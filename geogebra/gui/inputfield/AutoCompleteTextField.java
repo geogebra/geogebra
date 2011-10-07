0abe1e441034326382ba02ed467aa1d02e3d6548
Johannes Renner <rejo4850@yahoo.de>
2011-10-07 14:55:30 +0000

    Deleted the clearSelection() method since it isn't needed anymore.
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@13069 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/gui/inputfield/AutoCompleteTextField.java
+++ geogebra/gui/inputfield/AutoCompleteTextField.java
@@ -416,8 +416,6 @@ AutoComplete, KeyListener, GeoElementSelectionListener {
 				(ctrlC && Application.MAC_OS) // don't want selection cleared
 		) return;        
 
-		clearSelection();
-
 		// handle alt-p etc
 		super.keyReleased(e);
 
@@ -447,21 +445,6 @@ AutoComplete, KeyListener, GeoElementSelectionListener {
 				setCaretPosition(caretPos - decrease);
 			}
 		}		
-	}
-
-	private void clearSelection() {
-		int start = getSelectionStart();
-		int end = getSelectionEnd();        
-		//    clear selection if there is one
-		if (start != end) {
-			int pos = getCaretPosition();
-			String oldText = getText();
-			StringBuilder sb = new StringBuilder();
-			sb.append(oldText.substring(0, start));
-			sb.append(oldText.substring(end));            
-			setText(sb.toString());
-			if (pos < sb.length()) setCaretPosition(pos);
-		}
 	}
 
 	/**
@@ -496,7 +479,6 @@ AutoComplete, KeyListener, GeoElementSelectionListener {
 			return;
 		}
 
-		clearSelection();
 		caretPos = getCaretPosition();
 
 
