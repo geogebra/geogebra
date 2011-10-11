3e2675f944db670ba9039bb0910e6819587d25c0
Arnoud Delobelle <arnodel@gmail.com>
2011-10-11 21:32:58 +0000

    Removed unused code
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@13149 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/plugin/jython/PythonBridge.java
+++ geogebra/plugin/jython/PythonBridge.java
@@ -4,15 +4,11 @@ import geogebra.kernel.GeoElement;
 import geogebra.kernel.View;
 import geogebra.main.Application;
 
-import org.python.core.Py;
-import org.python.core.PySystemState;
 import org.python.util.PythonInterpreter;
 
 public class PythonBridge implements View {
 	private Application application;
 	private PythonInterpreter interpreter;
-	private String eventType;
-	private GeoElement eventTarget;
 	private PythonScriptInterface pyInterface;
 	
 	public PythonBridge(Application app) {
@@ -22,15 +18,8 @@ public class PythonBridge implements View {
 	
 	private void init() {
 		if (interpreter == null) {
-			
-			//final PySystemState engineSys = new PySystemState();
-			//engineSys.path.append( Py.newString( "__pyclasspath__/Lib" ) );
-			//Py.setSystemState(engineSys);
-
 			interpreter = new PythonInterpreter();
-			interpreter.exec("import sys; sys.path.append('__pyclasspath__/geogebra/plugin/jython')");
-			interpreter.exec("import sys; sys.path.append('__pyclasspath__/Lib')");
-			interpreter.exec("import sys; print sys.path");
+			interpreter.exec("import sys; sys.path.extend(['__pyclasspath__/geogebra/plugin/jython', '__pyclasspath__/Lib'])");
 			interpreter.exec("from pyggb import interface");
 			pyInterface = (PythonScriptInterface)interpreter.get("interface").__tojava__(PythonScriptInterface.class);
 			pyInterface.init(application);
