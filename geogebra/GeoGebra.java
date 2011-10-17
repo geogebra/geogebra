51775d7c07d4cead0ea70a743089fef333527fe3
Michael Borcherds <michael@geogebra.org>
2011-10-17 20:11:41 +0000

    4.9.6.0
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@13243 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/GeoGebra.java
+++ geogebra/GeoGebra.java
@@ -19,9 +19,9 @@ import java.net.URL;
 public class GeoGebra extends Object {
 	
 	// GeoGebra version
-	public static final String BUILD_DATE = "13 October 2011";
-	public static final String VERSION_STRING = "4.1.13.0"; // <- update lines below when this is updated
-	//current 3D: "4.9.5.0"
+	public static final String BUILD_DATE = "17 October 2011";
+	public static final String VERSION_STRING = "4.9.6.0"; // <- update lines below when this is updated
+	//current 3D: "4.9.6.0"
 	//current ggb42: "4.1.13.0"
 	
 	public static final String PREFERENCES_ROOT = VERSION_STRING.startsWith("4.9") ? 
