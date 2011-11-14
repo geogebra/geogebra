99fff2fc0e593241092b39e96ae646562756b488
Ancsin Gábor <ustakraharez@gmail.com>
2011-11-14 00:10:41 +0000

    First commit to GeoGebraCommon
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@13568 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common/src/geogebra/common/Common.java
@@ -0,0 +1,16 @@
+package geogebra.common;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Common implements EntryPoint {
+
+	public void onModuleLoad() {
+	
+		GWT.log("Debug Message from GeoGebraCommon");
+		
+	}
+}
