ffb179fc0e3e6a33fadfba9e3b9bb280f827d79b
Ancsin Gábor <ustakraharez@gmail.com>
2011-11-14 00:16:31 +0000

    First Commit to GeoGebraWeb
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@13569 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/geogebra/web/Web.java
@@ -0,0 +1,18 @@
+package geogebra.web;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Web implements EntryPoint {
+
+	public void onModuleLoad() {
+		
+		GWT.log("Debug Message from GeoGebraWeb");
+	
+	}
+}
