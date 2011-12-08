ae60e4a089e3144793625ba66e0483589ec5f328
Judit Elias <eliasjudit@gmail.com>
2011-12-08 12:32:37 +0000

    replace ColorFactory with AwtFactory (it missed from my previous commit)
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@14025 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ desktop/geogebra/factories/AwtFactory.java
@@ -0,0 +1,23 @@
+package geogebra.factories;
+
+import geogebra.common.awt.Color;
+
+public class AwtFactory extends geogebra.common.factories.AwtFactory{
+
+	public static AwtFactory prototype = null;
+	
+	@Override
+	public Color newColor(int red, int green, int blue) {
+		return new geogebra.awt.Color(red, green, blue);
+	}
+	
+	@Override
+	public Color newColor(int red, int green, int blue, int alpha) {
+		return new geogebra.awt.Color(red, green, blue, alpha);
+	}
+	
+	@Override
+	public Color newColor(float red, float green, float blue, float alpha) {
+		return new geogebra.awt.Color(red, green, blue, alpha);
+	}
+}
