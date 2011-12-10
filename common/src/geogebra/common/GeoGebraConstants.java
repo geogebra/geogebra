bcada334e620aec77c5247cc3a561f6dd23c1bbb
Michael Borcherds <michael@geogebra.org>
2011-12-10 20:28:16 +0000

    4.1.27.0
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@14108 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/GeoGebraConstants.java
+++ common/src/geogebra/common/GeoGebraConstants.java
@@ -6,11 +6,13 @@ public interface GeoGebraConstants {
 	// DO NOT CHANGE the format of VERSION_STRING (or add commented out version)
 	// as it is read by the build system
 	// update lines below when this is updated
-	public static final String BUILD_DATE = "04 December 2011";
-    public static final String VERSION_STRING = "4.1.26.0"; // <- update lines below when this is updated 
-    //current 3D: "4.9.9.0" 
-    //current ggb42: "4.1.26.0" 
-	
+	public static final String BUILD_DATE = "10 December 2011";
+	public static final String VERSION_STRING = "4.1.27.0"; // <- update lines
+															// below when this
+															// is updated
+	// current 3D: "4.9.9.0"
+	// current ggb42: "4.1.27.0"
+
 	/* start hacks TODO remove it when release candidate */
 	public static final String PREFERENCES_ROOT = VERSION_STRING
 			.startsWith("4.9") ? "/geogebra50" : "/geogebra42";
@@ -18,14 +20,15 @@ public interface GeoGebraConstants {
 	public static final String XML_FILE_FORMAT = VERSION_STRING
 			.startsWith("4.9") ? "5.0" : "4.2";
 	/* end hacks */
-	
+
 	public static final String SPLASH_STRING = "splash42beta.png";
-	public static final String SHORT_VERSION_STRING = "4.2"; // used for online archive
+	public static final String SHORT_VERSION_STRING = "4.2"; // used for online
+																// archive
 	public static final boolean CAS_VIEW_ENABLED = true;
-	public static final boolean IS_PRE_RELEASE = true; //!VERSION_STRING.endsWith(".0");
+	public static final boolean IS_PRE_RELEASE = true; // !VERSION_STRING.endsWith(".0");
 	// File format versions
 	public static final String GGB_XSD_FILENAME = "ggb.xsd"; // for ggb files
-	public static final String GGT_XSD_FILENAME = "ggt.xsd"; // for macro files 
+	public static final String GGT_XSD_FILENAME = "ggt.xsd"; // for macro files
 	public static final String I2G_FILE_FORMAT = "0.1.20080731";
 	// pre-releases and I2G
 	public static final boolean DISABLE_I2G = !IS_PRE_RELEASE;
