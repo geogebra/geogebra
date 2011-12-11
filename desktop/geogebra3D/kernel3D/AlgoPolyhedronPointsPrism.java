3f7e4545e187478bc9d721449121d07adcc11b0a
Mathieu Blossier <mathieublossier@free.fr>
2011-12-11 19:42:11 +0000

    Fix imports.
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@14138 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra3D/kernel3D/AlgoPolyhedronPointsPrism.java
+++ desktop/geogebra3D/kernel3D/AlgoPolyhedronPointsPrism.java
@@ -1,12 +1,10 @@
 package geogebra3D.kernel3D;
 
 import geogebra.common.kernel.Construction;
-import geogebra.common.kernel.ConstructionElementCycle;
 import geogebra.common.kernel.Matrix.Coords;
 import geogebra.common.kernel.arithmetic.NumberValue;
 import geogebra.common.kernel.geos.GeoPolygon;
 import geogebra.common.kernel.kernelND.GeoPointND;
-import geogebra.common.kernel.kernelND.GeoSegmentND;
 import geogebra.main.Application;
 
 /**
