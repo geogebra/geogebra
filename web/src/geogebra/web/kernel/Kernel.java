08888e1e2a90f68026cb0cfeb2e782f828b57c01
Zbyněk Konečný <zbynek@geogebra.org>
2011-12-11 11:49:52 +0000

    Web Kernel updated
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@14127 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/kernel/Kernel.java
+++ web/src/geogebra/web/kernel/Kernel.java
@@ -7,12 +7,13 @@ import geogebra.common.kernel.AbstractKernel;
 import geogebra.common.kernel.AbstractUndoManager;
 import geogebra.common.kernel.Construction;
 import geogebra.common.kernel.EquationSolverInterface;
+import geogebra.common.kernel.MacroInterface;
 import geogebra.common.kernel.algos.AlgoElement;
-import geogebra.common.kernel.arithmetic.ExpressionNodeEvaluatorInterface;
-import geogebra.common.kernel.arithmetic.Function;
+import geogebra.common.kernel.arithmetic.Equation;
 import geogebra.common.kernel.arithmetic.MyList;
+import geogebra.common.kernel.arithmetic.Polynomial;
 import geogebra.common.kernel.cas.GeoGebraCasInterface;
-import geogebra.common.kernel.commands.AbstractAlgebraProcessor;
+import geogebra.common.kernel.commands.AbstractCommandDispatcher;
 import geogebra.common.kernel.geos.AbstractGeoElementSpreadsheet;
 import geogebra.common.kernel.geos.GeoConicInterface;
 import geogebra.common.kernel.geos.GeoConicPartInterface;
@@ -40,376 +41,264 @@ import geogebra.web.main.Application;
 public class Kernel extends AbstractKernel {
 
 	public Kernel(Application application) {
-	    // TODO Auto-generated constructor stub
-    }
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
-    public Color getColorAdapter(int red, int green, int blue) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public Color getColorAdapter(int red, int green, int blue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public Color getColorAdapter(int red, int green, int blue, int alpha) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public Color getColorAdapter(int red, int green, int blue, int alpha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public Color getColorAdapter(float red, float green, float blue, float alpha) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public Color getColorAdapter(float red, float green, float blue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public NumberFormatAdapter getNumberFormat() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public Color getColorAdapter(float red, float green, float blue, float alpha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public NumberFormatAdapter getNumberFormat(String s) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public NumberFormatAdapter getNumberFormat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public GeoElementGraphicsAdapter newGeoElementGraphicsAdapter() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public NumberFormatAdapter getNumberFormat(String s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public ScientificFormatAdapter getScientificFormat(int a, int b, boolean c) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public GeoElementGraphicsAdapter newGeoElementGraphicsAdapter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    protected void notifyEuclidianViewCE() {
-	    // TODO Auto-generated method stub
-	    
-    }
+	public ScientificFormatAdapter getScientificFormat(int a, int b, boolean c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public LaTeXCache newLaTeXCache() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	protected void notifyEuclidianViewCE() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	@Override
-    public GeoElement lookupLabel(String label) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public LaTeXCache newLaTeXCache() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public GeoElement lookupLabel(String label, boolean b) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public AbstractApplication getApplication() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public Construction getConstruction() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public GeoGebraCasInterface newGeoGebraCAS() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public AbstractApplication getApplication() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public AbstractAnimationManager getAnimatonManager() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public void notifyRepaint() {
-	    // TODO Auto-generated method stub
-	    
-    }
+	public GeoElementInterface Semicircle(String label, GeoPoint2 geoPoint,
+			GeoPoint2 geoPoint2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public void initUndoInfo() {
-	    // TODO Auto-generated method stub
-	    
-    }
+	public AbstractGeoElementSpreadsheet getGeoElementSpreadsheet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public GeoGebraCasInterface newGeoGebraCAS() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public GgbMat getGgbMat(MyList myList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public int getConstructionStep() {
-	    // TODO Auto-generated method stub
-	    return 0;
-    }
+	public GeoPoint2[] RootMultiple(String[] labels, GeoFunction f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public void storeUndoInfo() {
-	    // TODO Auto-generated method stub
-	    
-    }
+	public GeoNumeric getDefaultNumber(boolean geoAngle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public double[] getViewBoundsForGeo(GeoElementInterface geo) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public GeoSegmentND SegmentND(String label, GeoPointND P, GeoPointND Q) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public void notifyUpdate(GeoElementInterface geo) {
-	    // TODO Auto-generated method stub
-	    
-    }
+	public Object Ray(String label, GeoPoint2 P, GeoPoint2 Q) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public AbstractAnimationManager getAnimatonManager() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public Object RayND(String label, GeoPointND P, GeoPointND Q) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public void notifyRename(GeoElementInterface geoElement) {
-	    // TODO Auto-generated method stub
-	    
-    }
+	public Object Ray(String label, GeoPoint2 P, GeoVector v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public void notifyRemove(GeoElementInterface geoElement) {
-	    // TODO Auto-generated method stub
-	    
-    }
+	public GeoElement[] PolygonND(String[] labels, GeoPointND[] P) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public void notifyUpdateVisualStyle(GeoElementInterface geoElement) {
-	    // TODO Auto-generated method stub
-	    
-    }
+	public GeoElement[] PolyLineND(String[] labels, GeoPointND[] P) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public void notifyUpdateAuxiliaryObject(GeoElementInterface geoElement) {
-	    // TODO Auto-generated method stub
-	    
-    }
+	public GeoConicPartInterface newGeoConicPart(Construction cons, int type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public void notifyAdd(GeoElementInterface geoElement) {
-	    // TODO Auto-generated method stub
-	    
-    }
+	public GeoLocusInterface newGeoLocus(Construction cons) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public AbstractAlgebraProcessor getAlgebraProcessor() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public GeoImplicitPolyInterface newGeoImplicitPoly(Construction cons) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public GeoElementInterface lookupCasCellLabel(String cmdName) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public String temporaryGetInterGeoStringForAlgoPointOnPath(
+			String classname, AlgoElement algo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public GeoElementInterface lookupCasRowReference(String cmdName) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public ParserInterface getParser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public GeoElementInterface Semicircle(String label, GeoPoint2 geoPoint,
-            GeoPoint2 geoPoint2) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public GeoConicInterface getGeoConic() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public AbstractGeoElementSpreadsheet getGeoElementSpreadsheet() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public ExtremumFinder getExtremumFinder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public GgbMat getGgbMat(MyList myList) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public GeoPoint2 getGeoPoint(double d, double e, int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public boolean isInsertLineBreaks() {
-	    // TODO Auto-generated method stub
-	    return false;
-    }
+	public EquationSolverInterface getEquationSolver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public ExpressionNodeEvaluatorInterface getExpressionNodeEvaluator() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public void resetGeoGebraCAS() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	@Override
-    public String[] getPolynomialCoeffs(String function, String var) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public void getKernelXML(StringBuilder sb, boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	@Override
-    public GeoPoint2[] RootMultiple(String[] labels, GeoFunction f) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public AbstractUndoManager getUndoManager(Construction cons) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public double getViewsXMax(GeoElement geo) {
-	    // TODO Auto-generated method stub
-	    return 0;
-    }
+	public AbstractConstructionDefaults getConstructionDefaults(
+			Construction construction) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public double getViewsXMin(GeoElement geo) {
-	    // TODO Auto-generated method stub
-	    return 0;
-    }
+	public GeoImplicitPolyInterface ImplicitPoly(String label, Polynomial lhs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public GeoNumeric getDefaultNumber(boolean geoAngle) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public GeoElement DependentImplicitPoly(String label, Equation equ) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public boolean isNotifyViewsActive() {
-	    // TODO Auto-generated method stub
-	    return false;
-    }
+	public AbstractCommandDispatcher getCommandDispatcher() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public GeoFunction DependentFunction(String label, Function fun) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public MacroInterface getMacro(String cmdName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-    public GeoSegmentND SegmentND(String label, GeoPointND P, GeoPointND Q) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
+	public GeoElement[] useMacro(String[] labels, MacroInterface macro,
+			GeoElement[] arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-	@Override
-    public String temporaryGetInterGeoStringForAlgoPointOnPath(
-            String classname, AlgoElement algo) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public ParserInterface getParser() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public GeoConicInterface getGeoConic() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public ExtremumFinder getExtremumFinder() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public GeoPoint2 getGeoPoint(double d, double e, int i) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public void updateConstruction() {
-	    // TODO Auto-generated method stub
-	    
-    }
-
-	@Override
-    public void notifyReset() {
-	    // TODO Auto-generated method stub
-	    
-    }
-
-	@Override
-    public EquationSolverInterface getEquationSolver() {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public void resetGeoGebraCAS() {
-	    // TODO Auto-generated method stub
-	    
-    }
-
-	@Override
-    public void getKernelXML(StringBuilder sb, boolean b) {
-	    // TODO Auto-generated method stub
-	    
-    }
-
-	@Override
-    public Color getColorAdapter(float red, float green, float blue) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public Object Ray(String label, GeoPoint2 P, GeoPoint2 Q) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public Object RayND(String label, GeoPointND P, GeoPointND Q) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public Object Ray(String label, GeoPoint2 P, GeoVector v) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public GeoConicPartInterface newGeoConicPart(Construction cons, int type) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public GeoLocusInterface newGeoLocus(Construction cons) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public AbstractUndoManager getUndoManager(Construction cons) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public AbstractConstructionDefaults getConstructionDefaults(
-            Construction construction) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
-	@Override
-    public GeoImplicitPolyInterface newGeoImplicitPoly(Construction cons) {
-	    // TODO Auto-generated method stub
-	    return null;
-    }
-
+	
 }
