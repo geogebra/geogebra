0ec0cd14d52d3898ff68bdff82c5db4f615afd35
Michael Borcherds <michael@geogebra.org>
2011-12-12 09:20:14 +0000

    undone [14145] - textfield can't keep caret
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@14157 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra/euclidian/DrawTextField.java
+++ desktop/geogebra/euclidian/DrawTextField.java
@@ -13,8 +13,8 @@
 package geogebra.euclidian;
 
 import geogebra.common.euclidian.EuclidianConstants;
-import geogebra.common.kernel.arithmetic.FunctionalNVar;
 import geogebra.common.kernel.arithmetic.ExpressionNodeConstants.StringType;
+import geogebra.common.kernel.arithmetic.FunctionalNVar;
 import geogebra.common.kernel.geos.GeoElement;
 import geogebra.common.kernel.geos.GeoPoint2;
 import geogebra.common.kernel.geos.GeoText;
@@ -51,7 +51,7 @@ import javax.swing.JTextField;
  */
 public final class DrawTextField extends Drawable {
 
-	private GeoButton geoButton;
+	private final GeoButton geoButton;
 
 	private boolean isVisible;
 
@@ -61,8 +61,7 @@ public final class DrawTextField extends Drawable {
 	JTextField textField;
 	JLabel label;
 	ButtonListener bl;
-    Container box = Box.createHorizontalBox();
-
+	Container box = Box.createHorizontalBox();
 
 	public DrawTextField(EuclidianView view, GeoTextField geoButton) {
 		this.view = view;
@@ -71,7 +70,7 @@ public final class DrawTextField extends Drawable {
 
 		// action listener for checkBox
 		bl = new ButtonListener();
-		textField = geoButton.getTextField();//new JTextField(20);	
+		textField = geoButton.getTextField();// new JTextField(20);
 		label = new JLabel("Label");
 		label.setLabelFor(textField);
 		textField.setVisible(true);
@@ -80,43 +79,35 @@ public final class DrawTextField extends Drawable {
 		label.addMouseListener(bl);
 		label.addMouseMotionListener(bl);
 		textField.addKeyListener(bl);
-	    box.add(label);
-	    box.add(textField);
+		box.add(label);
+		box.add(textField);
 		view.add(box);
-		
-		
-		// Add mouse listeners to textField so that it becomes draggable 
-		// on a right click. These listeners are registered first to prevent 
+
+		// Add mouse listeners to textField so that it becomes draggable
+		// on a right click. These listeners are registered first to prevent
 		// the JTextField listeners from initiating editing.
-		
-		MouseListener[] ml = textField.getMouseListeners();
-		for(int i = 0; i<ml.length; i++){
-			textField.removeMouseListener(ml[i]);
-		}
-		MouseMotionListener[] mml = textField.getMouseMotionListeners();
-		for(int i = 0; i<mml.length; i++){
-			textField.removeMouseMotionListener(mml[i]);
-		}
-
-		textField.addMouseListener(bl);
-		for(int i = 0; i<mml.length; i++){
-			textField.addMouseMotionListener(mml[i]);
-		}
-		
-		textField.addMouseMotionListener(bl);
-		for(int i = 0; i<ml.length; i++){
-			textField.addMouseListener(ml[i]);
-		}
-
+		/*
+		 * MouseListener[] ml = textField.getMouseListeners(); for(int i = 0;
+		 * i<ml.length; i++){ textField.removeMouseListener(ml[i]); }
+		 * MouseMotionListener[] mml = textField.getMouseMotionListeners();
+		 * for(int i = 0; i<mml.length; i++){
+		 * textField.removeMouseMotionListener(mml[i]); }
+		 * 
+		 * textField.addMouseListener(bl); for(int i = 0; i<mml.length; i++){
+		 * textField.addMouseMotionListener(mml[i]); }
+		 * 
+		 * textField.addMouseMotionListener(bl); for(int i = 0; i<ml.length;
+		 * i++){ textField.addMouseListener(ml[i]); }
+		 */
 
 		update();
 	}
 
-	private class ButtonListener implements 
-			MouseListener, MouseMotionListener, FocusListener, KeyListener {
+	private class ButtonListener implements MouseListener, MouseMotionListener,
+			FocusListener, KeyListener {
 
 		private boolean dragging = false;
-		private EuclidianController ec = view.getEuclidianController();
+		private final EuclidianController ec = view.getEuclidianController();
 
 		/**
 		 * Handles click on check box. Changes value of GeoBoolean.
@@ -126,63 +117,67 @@ public final class DrawTextField extends Drawable {
 			// TODO delete?
 		}
 
-		public void mouseDragged(MouseEvent e) {	
-			
-			dragging = true;			
+		public void mouseDragged(MouseEvent e) {
+
+			dragging = true;
 			e.translatePoint(box.getX(), box.getY());
 			ec.mouseDragged(e);
 			view.setToolTipText(null);
 		}
 
-		public void mouseMoved(MouseEvent e) {			
-			
+		public void mouseMoved(MouseEvent e) {
+
 			e.translatePoint(box.getX(), box.getY());
 			ec.mouseMoved(e);
 			view.setToolTipText(null);
 		}
 
 		public void mouseClicked(MouseEvent e) {
-			
-			if (e.getClickCount() > 1) return;
-			
+
+			if (e.getClickCount() > 1) {
+				return;
+			}
+
 			e.translatePoint(box.getX(), box.getY());
 			ec.mouseClicked(e);
 		}
 
 		public void mousePressed(MouseEvent e) {
-			
+
 			// prevent textField editing on right click
-			if(Application.isRightClick(e)) e.consume();
-			
-			dragging = false;	
+			if (Application.isRightClick(e)) {
+				e.consume();
+			}
+
+			dragging = false;
 			e.translatePoint(box.getX(), box.getY());
-			ec.mousePressed(e);		
+			ec.mousePressed(e);
 		}
 
-		public void mouseReleased(MouseEvent e) {	
-			
+		public void mouseReleased(MouseEvent e) {
+
 			// prevent textField editing on right click
-			if(Application.isRightClick(e)) e.consume();
-			
-			if (!dragging && !e.isMetaDown() && !e.isPopupTrigger()
-					&& view.getMode() == EuclidianConstants.MODE_MOVE) 
-			{
-				// handle LEFT CLICK
-				//geoBool.setValue(!geoBool.getBoolean());
-				//geoBool.updateRepaint();
-				//geo.runScript();
-				//
-				
-				// make sure itemChanged does not change
-		    	// the value back my faking a drag
-		    	dragging = true;				
+			if (Application.isRightClick(e)) {
+				e.consume();
 			}
-			else {
+
+			if (!dragging && !e.isMetaDown() && !e.isPopupTrigger()
+					&& (view.getMode() == EuclidianConstants.MODE_MOVE)) {
+				// handle LEFT CLICK
+				// geoBool.setValue(!geoBool.getBoolean());
+				// geoBool.updateRepaint();
+				// geo.runScript();
+				//
+
+				// make sure itemChanged does not change
+				// the value back my faking a drag
+				dragging = true;
+			} else {
 				// handle right click and dragging
 				e.translatePoint(box.getX(), box.getY());
-				ec.mouseReleased(e);	
+				ec.mouseReleased(e);
 			}
-			
+
 		}
 
 		public void mouseEntered(MouseEvent arg0) {
@@ -198,117 +193,126 @@ public final class DrawTextField extends Drawable {
 		public void focusGained(FocusEvent e) {
 			view.getEuclidianController().textfieldHasFocus(true);
 			updateText();
-			
+
 		}
 
 		public void focusLost(FocusEvent e) {
 			view.getEuclidianController().textfieldHasFocus(false);
-			
-			GeoElement linkedGeo = ((GeoTextField)geo).getLinkedGeo();
-			
-			
+
+			GeoElement linkedGeo = ((GeoTextField) geo).getLinkedGeo();
+
 			if (linkedGeo != null) {
 
-				String defineText = textField.getText();			
-				
+				String defineText = textField.getText();
+
 				if (linkedGeo.isGeoLine()) {
 					String prefix = linkedGeo.getLabel() + ":";
-					// need a: in front of 
+					// need a: in front of
 					// X = (-0.69, 0) + \lambda (1, -2)
-					if (!defineText.startsWith(prefix))
+					if (!defineText.startsWith(prefix)) {
 						defineText = prefix + defineText;
+					}
 				} else if (linkedGeo.isGeoText()) {
-					defineText = "\"" +  defineText + "\"";
+					defineText = "\"" + defineText + "\"";
 				} else if (linkedGeo.isGeoPoint()) {
-					if (((GeoPoint2)linkedGeo).toStringMode == Kernel.COORD_COMPLEX) {
-						// z=2 doesn't work for complex numbers (parses to GeoNumeric)
-						defineText = defineText + "+0"+Unicode.IMAGINARY;
+					if (((GeoPoint2) linkedGeo).toStringMode == Kernel.COORD_COMPLEX) {
+						// z=2 doesn't work for complex numbers (parses to
+						// GeoNumeric)
+						defineText = defineText + "+0" + Unicode.IMAGINARY;
 					}
 				} else if (linkedGeo instanceof FunctionalNVar) {
 					// string like f(x,y)=x^2
 					// or f(\theta) = \theta
-					defineText = linkedGeo.getLabel() + "(" + ((FunctionalNVar)linkedGeo).getVarString() + ")=" + defineText;
-				} 
-				
+					defineText = linkedGeo.getLabel() + "("
+							+ ((FunctionalNVar) linkedGeo).getVarString()
+							+ ")=" + defineText;
+				}
+
 				try {
-					linkedGeo = ((Kernel) geo.getKernel()).getAlgebraProcessor().changeGeoElementNoExceptionHandling(linkedGeo, defineText, false, true);
+					linkedGeo = ((Kernel) geo.getKernel())
+							.getAlgebraProcessor()
+							.changeGeoElementNoExceptionHandling(linkedGeo,
+									defineText, false, true);
 				} catch (Exception e1) {
 					geo.getKernel().getApplication().showError(e1.getMessage());
 					updateText();
 					return;
-				}			
-				((GeoTextField)geo).setLinkedGeo(linkedGeo);
-				
+				}
+				((GeoTextField) geo).setLinkedGeo(linkedGeo);
+
 				updateText();
 
-				
 			}
-			
+
 			geo.runScripts(textField.getText());
-			
+
 		}
 
 		public void keyPressed(KeyEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		public void keyReleased(KeyEvent e) {
 			if (e.getKeyChar() == '\n') {
-				//geo.runScripts(textField.getText());
-				
+				// geo.runScripts(textField.getText());
+
 				// this should be enough to trigger script event
 				// ie in focusLost
 				view.requestFocus();
 			}
-			
+
 		}
 
 		public void keyTyped(KeyEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
-		
 	}
-	
+
 	private void updateText() {
-		
-		GeoElement linkedGeo = ((GeoTextField)geo).getLinkedGeo();
+
+		GeoElement linkedGeo = ((GeoTextField) geo).getLinkedGeo();
 		if (linkedGeo != null) {
-			
+
 			String text;
-			
+
 			if (linkedGeo.isGeoText()) {
-				text = ((GeoText)linkedGeo).getTextString();
+				text = ((GeoText) linkedGeo).getTextString();
 			} else {
-			
-				// want just a number for eg a=3 but we want variables for eg y=m x + c
-				boolean substituteNos = linkedGeo.isGeoNumeric() && linkedGeo.isIndependent();
-				text = linkedGeo.getFormulaString(StringType.GEOGEBRA, substituteNos);
+
+				// want just a number for eg a=3 but we want variables for eg
+				// y=m x + c
+				boolean substituteNos = linkedGeo.isGeoNumeric()
+						&& linkedGeo.isIndependent();
+				text = linkedGeo.getFormulaString(StringType.GEOGEBRA,
+						substituteNos);
 			}
-			
-			if (linkedGeo.isGeoText() && text.indexOf("\n") > -1) {
+
+			if (linkedGeo.isGeoText() && (text.indexOf("\n") > -1)) {
 				// replace linefeed with \\n
-				while (text.indexOf("\n") > -1)
+				while (text.indexOf("\n") > -1) {
 					text = text.replaceAll("\n", "\\\\\\\\n");
+				}
 			}
 			textField.setText(text);
-			
+
 		}
-		
-		((GeoTextField)geo).setText(textField.getText());
+
+		((GeoTextField) geo).setText(textField.getText());
 
 	}
 
-
+	@Override
 	final public void update() {
 		isVisible = geo.isEuclidianVisible();
-		//textField.setVisible(isVisible);
-		//label.setVisible(isVisible);
+		// textField.setVisible(isVisible);
+		// label.setVisible(isVisible);
 		box.setVisible(isVisible);
-		if (!isVisible)
-			return;		
+		if (!isVisible) {
+			return;
+		}
 
 		// show hide label by setting text
 		if (geo.isLabelVisible()) {
@@ -317,66 +321,68 @@ public final class DrawTextField extends Drawable {
 			if (!caption.equals(oldCaption)) {
 				oldCaption = caption;
 				labelDesc = GeoElement.indicesToHTML(caption, true);
-			}	
+			}
 			box.setVisible(false); // avoid redraw error
 			label.setText(labelDesc);
 			box.setVisible(true);
 		} else {
 			label.setText("");
-		}			
-		
-		
+		}
+
 		int fontSize = view.fontSize + geoButton.getFontSize();
 		Application app = view.getApplication();
-		
+
 		Font vFont = view.getFont();
-		Font font = app.getFontCanDisplay(textField.getText(), false, vFont.getStyle(), fontSize);
-		
-		textField.setOpaque(true);		
-		label.setOpaque(false);		
-		textField.setFont(font);				
+		Font font = app.getFontCanDisplay(textField.getText(), false,
+				vFont.getStyle(), fontSize);
+
+		textField.setOpaque(true);
+		label.setOpaque(false);
+		textField.setFont(font);
 		label.setFont(font);
-		textField.setForeground(geogebra.awt.Color.getAwtColor((geogebra.awt.Color) geo.getObjectColor()));
-		label.setForeground(geogebra.awt.Color.getAwtColor((geogebra.awt.Color) geo.getObjectColor()));
-		Color bgCol = geogebra.awt.Color.getAwtColor((geogebra.awt.Color) geo.getBackgroundColor());
+		textField.setForeground(geogebra.awt.Color.getAwtColor(geo
+				.getObjectColor()));
+		label.setForeground(geogebra.awt.Color.getAwtColor(geo.getObjectColor()));
+		Color bgCol = geogebra.awt.Color.getAwtColor(geo.getBackgroundColor());
 		textField.setBackground(bgCol != null ? bgCol : view.getBackground());
-		
+
 		textField.setFocusable(true);
 		textField.setEditable(true);
 		updateText();
-		// set checkbox state		
-		//jButton.removeItemListener(bl);
-		//jButton.setSelected(geo.getBoolean());
-		//jButton.addItemListener(bl);
-		
+		// set checkbox state
+		// jButton.removeItemListener(bl);
+		// jButton.setSelected(geo.getBoolean());
+		// jButton.addItemListener(bl);
+
 		xLabel = geo.labelOffsetX;
-		yLabel = geo.labelOffsetY;		
+		yLabel = geo.labelOffsetY;
 		Dimension prefSize = box.getPreferredSize();
 		labelRectangle.setBounds(xLabel, yLabel, prefSize.width,
 				prefSize.height);
-		box.setBounds(labelRectangle);	}
+		box.setBounds(labelRectangle);
+	}
 
 	@SuppressWarnings("unused")
 	private void updateLabel() {
 		// TODO delete?
 		/*
-		xLabel = geo.labelOffsetX;
-		yLabel = geo.labelOffsetY;		
-
-		labelRectangle.setBounds(xLabel, yLabel,
-				 ((textSize == null) ? 0 : textSize.x),
-				12);*/
+		 * xLabel = geo.labelOffsetX; yLabel = geo.labelOffsetY;
+		 * 
+		 * labelRectangle.setBounds(xLabel, yLabel, ((textSize == null) ? 0 :
+		 * textSize.x), 12);
+		 */
 
 	}
 
+	@Override
 	final public void draw(Graphics2D g2) {
 		if (isVisible) {
 			if (geo.doHighlighting()) {
-				label.setOpaque(true);		
+				label.setOpaque(true);
 				label.setBackground(Color.lightGray);
-				
+
 			} else {
-				label.setOpaque(false);		
+				label.setOpaque(false);
 			}
 		}
 	}
@@ -387,15 +393,17 @@ public final class DrawTextField extends Drawable {
 	final public void remove() {
 		view.remove(box);
 	}
-	
+
 	/**
-	 * was this object clicked at? (mouse pointer
-	 * location (x,y) in screen coords)
+	 * was this object clicked at? (mouse pointer location (x,y) in screen
+	 * coords)
 	 */
+	@Override
 	final public boolean hit(int x, int y) {
-		return hit;				      
+		return hit;
 	}
 
+	@Override
 	final public boolean isInside(Rectangle rect) {
 		return rect.contains(labelRectangle);
 	}
@@ -403,17 +411,19 @@ public final class DrawTextField extends Drawable {
 	/**
 	 * Returns false
 	 */
+	@Override
 	public boolean hitLabel(int x, int y) {
 		return false;
 	}
 
+	@Override
 	final public GeoElement getGeoElement() {
 		return geo;
 	}
 
+	@Override
 	final public void setGeoElement(GeoElement geo) {
 		this.geo = geo;
 	}
-
 
 }
