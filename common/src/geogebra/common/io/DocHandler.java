c8c8147b4325c7ad2211d4022c9944311611c041
Ancsin Gábor <ustakraharez@gmail.com>
2011-12-13 08:56:18 +0000

    DocHandler.java moved to common
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@14183 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common/src/geogebra/common/io/DocHandler.java
@@ -0,0 +1,13 @@
+package geogebra.common.io;
+
+import java.util.Map;
+
+public interface DocHandler {
+
+	void startElement(String tagName, Map<String, String> attributes);
+	void endElement(String tagName);
+
+	void startDocument();
+	void endDocument();
+
+}
