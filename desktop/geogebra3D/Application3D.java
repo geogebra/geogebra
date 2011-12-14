f91e6378fa92be11acc5e9bc2843b0b7a11b1eff
Michael Borcherds <michael@geogebra.org>
2011-12-14 22:58:07 +0000

    changes for ggbtrans (.Syntax3D)
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@14279 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra3D/Application3D.java
+++ desktop/geogebra3D/Application3D.java
@@ -8,7 +8,7 @@ This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by 
 the Free Software Foundation.
 
-*/
+ */
 
 /**
  * GeoGebra Application
@@ -47,81 +47,80 @@ import java.io.File;
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 
+public class Application3D extends Application {
 
-public class Application3D extends Application{
-	
-    private EuclidianView3D euclidianView3D;
-    private EuclidianController3D euclidianController3D;      
-    protected Kernel3D kernel3D;
-    
-    private EuclidianViewForPlane euclidianViewForPlane;
+	private EuclidianView3D euclidianView3D;
+	private EuclidianController3D euclidianController3D;
+	protected Kernel3D kernel3D;
 
+	private EuclidianViewForPlane euclidianViewForPlane;
 
-    public Application3D(CommandLineArguments args, JFrame frame, boolean undoActive) {
-        this(args, frame, null, undoActive);
-    }
+	public Application3D(CommandLineArguments args, JFrame frame,
+			boolean undoActive) {
+		this(args, frame, null, undoActive);
+	}
 
-    public Application3D(CommandLineArguments args, AppletImplementation applet, boolean undoActive) {
-    	this(args, null, applet, undoActive);
-    }
-    
-    
-    private Application3D(CommandLineArguments args, JFrame frame, AppletImplementation applet, boolean undoActive) { 
-    	
-    	super(args, frame, applet, null, undoActive);
-    	
-    	//euclidianView3D.initAxisAndPlane();
-    	
-        
-	    //TODO remove 3D test : just comment following line        
-        //new Test3D(kernel3D,euclidianView,euclidianView3D,this);
-        
-        
-    }
-    
+	public Application3D(CommandLineArguments args,
+			AppletImplementation applet, boolean undoActive) {
+		this(args, null, applet, undoActive);
+	}
+
+	private Application3D(CommandLineArguments args, JFrame frame,
+			AppletImplementation applet, boolean undoActive) {
+
+		super(args, frame, applet, null, undoActive);
+
+		// euclidianView3D.initAxisAndPlane();
+
+		// TODO remove 3D test : just comment following line
+		// new Test3D(kernel3D,euclidianView,euclidianView3D,this);
+
+	}
+
 	@Override
-	public void initKernel(){
+	public void initKernel() {
 		kernel3D = new Kernel3D(this);
 		kernel = kernel3D;
 	}
-	
+
 	@Override
-	protected void initImageManager(Component component){
+	protected void initImageManager(Component component) {
 		imageManager = new ImageManager3D(component);
 	}
-	
+
 	/**
 	 * init the EuclidianView (and EuclidianView3D for 3D)
 	 */
 	@Override
-	public void initEuclidianViews(){
-		
-		//init the 2D euclidian view
+	public void initEuclidianViews() {
+
+		// init the 2D euclidian view
 		super.initEuclidianViews();
-		
-		//init the 3D euclidian view
+
+		// init the 3D euclidian view
 		euclidianController3D = new EuclidianController3D(kernel3D);
-        euclidianView3D = new EuclidianView3D(euclidianController3D); 
-        
+		euclidianView3D = new EuclidianView3D(euclidianController3D);
+
 	}
-	
+
 	@Override
-	protected EuclidianController newEuclidianController(Kernel kernel){
+	protected EuclidianController newEuclidianController(Kernel kernel) {
 		return new EuclidianControllerFor3D(kernel);
 	}
-	
+
 	@Override
-	protected EuclidianView newEuclidianView(boolean[] showAxes, boolean showGrid){
-		return new EuclidianViewFor3D(euclidianController, showAxes,
-				showGrid, 1);
+	protected EuclidianView newEuclidianView(boolean[] showAxes,
+			boolean showGrid) {
+		return new EuclidianViewFor3D(euclidianController, showAxes, showGrid,
+				1);
 	}
-	
+
 	@Override
 	public void setMode(int mode) {
 		super.setMode(mode);
-		
-		//if (euclidianView3D != null)
-			euclidianView3D.setMode(mode);
+
+		// if (euclidianView3D != null)
+		euclidianView3D.setMode(mode);
 	}
 
 	@Override
@@ -134,207 +133,210 @@ public class Application3D extends Application{
 		// save euclidianView3D settings
 		sb.append(euclidianView3D.getXML());
 
-
 		return sb.toString();
 	}
-	
-	/** return the 3D euclidian view
+
+	/**
+	 * return the 3D euclidian view
+	 * 
 	 * @return the 3D euclidian view
 	 */
-	public EuclidianView3D getEuclidianView3D(){
+	public EuclidianView3D getEuclidianView3D() {
 		return euclidianView3D;
 	}
-	
+
 	@Override
-	public void getEuclidianViewXML(StringBuilder sb,boolean asPreference){
+	public void getEuclidianViewXML(StringBuilder sb, boolean asPreference) {
 		super.getEuclidianViewXML(sb, asPreference);
 		sb.append(getEuclidianView3D().getXML());
 	}
-	
+
 	@Override
-	public BufferedImage getExportImage(double maxX, double maxY) throws OutOfMemoryError {
-		//TODO use maxX, maxY values
+	public BufferedImage getExportImage(double maxX, double maxY)
+			throws OutOfMemoryError {
+		// TODO use maxX, maxY values
 		return getEuclidianView3D().getRenderer().getExportImage();
 	}
-	
+
 	@Override
-	public boolean saveGeoGebraFile(File file) {		
-		//TODO generate it before
+	public boolean saveGeoGebraFile(File file) {
+		// TODO generate it before
 		getEuclidianView3D().getRenderer().needExportImage();
-		
+
 		return super.saveGeoGebraFile(file);
 	}
-	
-	/////////////////////////////////
+
+	// ///////////////////////////////
 	// EUCLIDIAN VIEW FOR PLANE
-	/////////////////////////////////
-	
-		
+	// ///////////////////////////////
+
 	/**
 	 * @param plane
 	 * @return create a new euclidian view for the plane
 	 */
 	@Override
-	public EuclidianView createEuclidianViewForPlane(Object o){
+	public EuclidianView createEuclidianViewForPlane(Object o) {
 		GeoCoordSys2D plane = (GeoCoordSys2D) o;
 		// create new view for plane and controller
 		EuclidianController ec = new EuclidianControllerForPlane(kernel3D);
 		euclidianViewForPlane = new EuclidianViewForPlane(ec, plane);
 		euclidianViewForPlane.updateFonts();
 		euclidianViewForPlane.addExistingGeos();
-		
+
 		// create dock panel
-		EuclidianDockPanelForPlane panel = new EuclidianDockPanelForPlane(this,euclidianViewForPlane);
+		EuclidianDockPanelForPlane panel = new EuclidianDockPanelForPlane(this,
+				euclidianViewForPlane);
 		getGuiManager().getLayout().registerPanel(panel);
-		
-		//panel.setToolbarString(dpInfo[i].getToolbarString());
+
+		// panel.setToolbarString(dpInfo[i].getToolbarString());
 		panel.setFrameBounds(new Rectangle(600, 400));
-		//panel.setEmbeddedDef(dpInfo[i].getEmbeddedDef());
-		//panel.setEmbeddedSize(dpInfo[i].getEmbeddedSize());
-		//panel.setShowStyleBar(dpInfo[i].showStyleBar());
-		//panel.setOpenInFrame(dpInfo[i].isOpenInFrame());
+		// panel.setEmbeddedDef(dpInfo[i].getEmbeddedDef());
+		// panel.setEmbeddedSize(dpInfo[i].getEmbeddedSize());
+		// panel.setShowStyleBar(dpInfo[i].showStyleBar());
+		// panel.setOpenInFrame(dpInfo[i].isOpenInFrame());
 		panel.setVisible(true);
 		panel.toggleStyleBar();
-		
-		
+
 		getGuiManager().getLayout().getDockManager().show(panel);
-		
+
 		return euclidianViewForPlane;
 	}
-	
-	/////////////////////////////////
+
+	// ///////////////////////////////
 	// GUI
-	/////////////////////////////////
-	
+	// ///////////////////////////////
 
 	@Override
 	public void refreshViews() {
 		getEuclidianView3D().reset();
 		super.refreshViews();
 	}
-	
-	public void toggleAxis3D(){
+
+	public void toggleAxis3D() {
 		// toggle axis
 		getEuclidianView3D().toggleAxis();
 	}
-	
-	public void togglePlane(){
+
+	public void togglePlane() {
 		// toggle xOy plane
 		getEuclidianView3D().togglePlane();
 	}
-	
-	public void toggleGrid3D(){
+
+	public void toggleGrid3D() {
 		// toggle xOy grid
 		getEuclidianView3D().toggleGrid();
 	}
-	
+
 	@Override
-	public void setShowAxesSelected(JCheckBoxMenuItem cb){
+	public void setShowAxesSelected(JCheckBoxMenuItem cb) {
 		cb.setSelected(getEuclidianView3D().axesAreAllVisible());
 	}
-	
-	/** set the show plane combo box selected if the plane is visible
+
+	/**
+	 * set the show plane combo box selected if the plane is visible
+	 * 
 	 * @param cb
 	 */
-	public void setShowPlaneSelected(JCheckBoxMenuItem cb){
+	public void setShowPlaneSelected(JCheckBoxMenuItem cb) {
 		GeoPlane3D p = getEuclidianView3D().getxOyPlane();
 		cb.setSelected(p.isPlateVisible());
 	}
-	
-	/** set the show grid combo box selected if the plane is visible
+
+	/**
+	 * set the show grid combo box selected if the plane is visible
+	 * 
 	 * @param cb
 	 */
 	@Override
-	public void setShowGridSelected(JCheckBoxMenuItem cb){
+	public void setShowGridSelected(JCheckBoxMenuItem cb) {
 		GeoPlane3D p = getEuclidianView3D().getxOyPlane();
 		cb.setSelected(p.isGridVisible());
 	}
-	
+
 	@Override
-	protected GuiManager newGuiManager(){
+	protected GuiManager newGuiManager() {
 		return new GuiManager3D(this);
 	}
-	
+
 	@Override
 	public void updateFonts() {
 
 		super.updateFonts();
-		
-		if (euclidianViewForPlane!=null) {
+
+		if (euclidianViewForPlane != null) {
 			euclidianViewForPlane.updateFonts();
 		}
 	}
-    
-	///////////////////////////////////////
+
+	// /////////////////////////////////////
 	// COMMANDS
-	///////////////////////////////////////
-	
+	// /////////////////////////////////////
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see geogebra.main.Application#getCommandSyntax(java.lang.String) check
+	 * if there's a Command.Syntax3D key. If not, return Command.Syntax key
+	 */
 	@Override
 	public String getCommandSyntax(String key) {
+		String command = getCommand(key);
 		String key3D = key + syntax3D;
-		String command3D = getCommand(key3D);
-		if (!command3D.equals(key3D)) return command3D;
-		
+		String syntax = getCommand(key3D);
+		if (!syntax.equals(key3D)) {
+			syntax = syntax.replace("[", command + '[');
+			return syntax;
+		}
+
 		return super.getCommandSyntax(key);
 	}
 
 	@Override
-	public void addToEuclidianView(GeoElement geo){
+	public void addToEuclidianView(GeoElement geo) {
 		super.addToEuclidianView(geo);
 		geo.addView(Application.VIEW_EUCLIDIAN3D);
 		getEuclidianView3D().add(geo);
 	}
 
 	@Override
-	public void removeFromEuclidianView(GeoElement geo){
+	public void removeFromEuclidianView(GeoElement geo) {
 		super.removeFromEuclidianView(geo);
 		geo.removeView(Application.VIEW_EUCLIDIAN3D);
 		getEuclidianView3D().remove(geo);
-	}	
-	
+	}
+
 	@Override
-	public void updateStyleBars(){		
+	public void updateStyleBars() {
 		super.updateStyleBars();
 		getEuclidianView3D().getStyleBar().updateStyleBar();
 	}
-	
-	
-	/////////////////////////////////
+
+	// ///////////////////////////////
 	// FOR TESTING : TODO remove all
 
 	@Override
-	protected GlobalKeyDispatcher newGlobalKeyDispatcher(){
+	protected GlobalKeyDispatcher newGlobalKeyDispatcher() {
 		return new GlobalKeyDispatcher3D(this);
 	}
-	
+
 	/*
-	private static final int WIREFRAME_OFF =0;
-	private static final int WIREFRAME_ON =1;
-	private int wireframe = WIREFRAME_OFF ;
-	
-	public void toggleWireframe(){
-		switch (wireframe){
-		case WIREFRAME_OFF:
-			wireframe = WIREFRAME_ON;
-			Application.debug("wireframe on");
-			break;
-		case WIREFRAME_ON:
-			wireframe = WIREFRAME_OFF;
-			Application.debug("wireframe off");
-			break;
-			
-		}
-		
-	}
-	
-	public boolean drawWireFrame(){
-		return (wireframe == WIREFRAME_ON);
-	}
-	
-	*/
-	
+	 * private static final int WIREFRAME_OFF =0; private static final int
+	 * WIREFRAME_ON =1; private int wireframe = WIREFRAME_OFF ;
+	 * 
+	 * public void toggleWireframe(){ switch (wireframe){ case WIREFRAME_OFF:
+	 * wireframe = WIREFRAME_ON; Application.debug("wireframe on"); break; case
+	 * WIREFRAME_ON: wireframe = WIREFRAME_OFF;
+	 * Application.debug("wireframe off"); break;
+	 * 
+	 * }
+	 * 
+	 * }
+	 * 
+	 * public boolean drawWireFrame(){ return (wireframe == WIREFRAME_ON); }
+	 */
+
 	@Override
-	public boolean is3D(){
+	public boolean is3D() {
 		return true;
 	}
 }
