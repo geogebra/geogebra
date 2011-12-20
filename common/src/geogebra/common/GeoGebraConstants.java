472bad04810b58f17567cce31af3698ffde6f8e2
Michael Borcherds <michael@geogebra.org>
2011-12-20 12:12:39 +0000

    4.9.10.0
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@14366 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/GeoGebraConstants.java
+++ common/src/geogebra/common/GeoGebraConstants.java
@@ -6,11 +6,11 @@ public interface GeoGebraConstants {
 	// DO NOT CHANGE the format of VERSION_STRING (or add commented out version)
 	// as it is read by the build system
 	// update lines below when this is updated
-	public static final String BUILD_DATE = "14 December 2011";
-	public static final String VERSION_STRING = "4.1.30.0"; // <- update lines
+	public static final String BUILD_DATE = "20 December 2011";
+	public static final String VERSION_STRING = "4.9.10.0"; // <- update lines
 															// below when this
 															// is updated
-	// current 3D: "4.9.9.0"
+	// current 3D: "4.9.10.0"
 	// current ggb42: "4.1.30.0"
 
 	/* start hacks TODO remove it when release candidate */
