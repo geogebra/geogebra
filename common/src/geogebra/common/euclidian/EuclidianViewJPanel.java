bc6c8382aaa30429750483dd8c54aec02e104dc0
Ancsin Gábor <ustakraharez@gmail.com>
2011-12-20 12:53:38 +0000

    EuclidianViewJpanel in common
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@14369 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common/src/geogebra/common/euclidian/EuclidianViewJPanel.java
@@ -0,0 +1,8 @@
+package geogebra.common.euclidian;
+
+public interface EuclidianViewJPanel {
+	public boolean hasFocus();
+	public void repaint();
+	public void setFocusable(boolean f);
+	public void removeAll();
+}
