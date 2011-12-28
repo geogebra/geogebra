30ce729993bd9c1baf8be30b8b8b72ba382d3668
Michael Borcherds <michael@geogebra.org>
2011-12-28 09:17:42 +0000

    CreateNew2DViewFromA
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@14455 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra/gui/ContextMenuGeoElement.java
+++ desktop/geogebra/gui/ContextMenuGeoElement.java
@@ -523,7 +523,7 @@ public class ContextMenuGeoElement extends JPopupMenu {
 
 		AbstractAction action;
 
-		action = new AbstractAction(app.getPlain("Create2DViewFrom")) {
+		action = new AbstractAction(app.getPlain("CreateNew2DViewFromA")) {
 			/**
 			 * 
 			 */
