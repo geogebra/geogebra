3aaba28b185eab14881b646de3b7c1402f871211
Arnoud Delobelle <arnodel@gmail.com>
2012-01-09 22:21:44 +0000

    Java docs for PythonBridge
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@14820 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra/plugin/jython/PythonBridge.java
+++ desktop/geogebra/plugin/jython/PythonBridge.java
@@ -2,16 +2,33 @@ package geogebra.plugin.jython;
 
 import geogebra.common.kernel.View;
 import geogebra.common.kernel.geos.GeoElement;
+import geogebra.common.main.AbstractApplication;
 import geogebra.common.main.GeoElementSelectionListener;
 import geogebra.main.Application;
 
 import org.python.util.PythonInterpreter;
 
+/**
+ * @author arno
+ * This class manages the Python interpreter.
+ * 
+ * It also listens to GeoGebra events (click, update, etc) and passes them
+ * on to a PythonScriptInterface object (implemented in Python in pyggb.py)
+ * which handles them.
+ * 
+ * It also listens to selection events on GeoElements and passes them on to
+ * the PythonScriptInterface object as well.
+ */
 public class PythonBridge implements View, GeoElementSelectionListener {
 	private Application application;
 	private PythonInterpreter interpreter;
 	private PythonScriptInterface pyInterface;
 	
+	/**
+	 * This constructor actually starts off the python interpreter
+	 * so it can take a while...
+	 * @param app the GeoGebra application
+	 */
 	public PythonBridge(Application app) {
 		application = app;
 		interpreter = null;
@@ -29,17 +46,34 @@ public class PythonBridge implements View, GeoElementSelectionListener {
 		}
 	}
 	
+	/**
+	 * Open / close the Python window
+	 */
 	public void toggleWindow() {
 		pyInterface.toggleWindow();
 	}
 	
+	/**
+	 * Check the visibility of the Python window
+	 * @return true if the Python window is currently visible
+	 */
 	public boolean isWindowVisible() {
 		return pyInterface.isWindowVisible();
 	}
 	
+	/**
+	 * This should be called when a geo is clicked.
+	 * (For now this is done in EuclidianController.switchModeForMouseReleased)
+	 * @param geo the clicked GeoElement
+	 */
 	public void click(GeoElement geo) {
 		pyInterface.handleEvent("click", geo);
 	}
+	
+	/*
+  	 * Implementation of View
+  	 * used to dispatch events to the PythonScriptInterface object
+	 */
 	
 	public void add(GeoElement geo) {
 		pyInterface.handleEvent("add", geo);
@@ -66,16 +100,19 @@ public class PythonBridge implements View, GeoElementSelectionListener {
 	}
 	
 	public void repaintView() {
+		/* not needed */
 	}
 	
 	public void reset() {
-		
+		/* not needed */		
 	} 
 	
 	public void clearView() {
+		/* not needed */
 	}
 	
 	public void setMode(int mode) {
+		/* not needed */
 	}
 	
 	public int getViewID() {
@@ -86,8 +123,12 @@ public class PythonBridge implements View, GeoElementSelectionListener {
 		pyInterface.notifySelected(geo, addToSelection);
 	}
 
+	/**
+	 * Evaluate a Python script
+	 * @param script script to evaluate
+	 */
 	public void eval(String script) {
-		Application.debug("python: evaluating:: "+script);
+		AbstractApplication.debug("python: evaluating:: "+script);
 		//interpreter.exec(script);
 		
 		pyInterface.execute(script);
