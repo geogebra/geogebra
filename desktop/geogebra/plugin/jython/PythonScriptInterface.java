795cd49e80ad0b5d8ed2cb3de9105fcbc4d71e7e
Arnoud Delobelle <arnodel@gmail.com>
2012-01-09 22:01:03 +0000

    Java docs for PythonScriptInterface
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@14817 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra/plugin/jython/PythonScriptInterface.java
+++ desktop/geogebra/plugin/jython/PythonScriptInterface.java
@@ -1,16 +1,47 @@
 package geogebra.plugin.jython;
 
-import geogebra.common.kernel.Construction;
-import geogebra.common.kernel.Kernel;
-import geogebra.common.kernel.commands.AlgebraProcessor;
 import geogebra.common.kernel.geos.GeoElement;
 import geogebra.main.Application;
 
+/**
+ * @author arno
+ * pyggb.Interface implements this interface so that its methods can be called
+ * from PythonBridge (see PythonBridge implementation).
+ */
 public interface PythonScriptInterface {
+	
+	/**
+	 * @param app the application object
+	 */
 	public void init(Application app);
+	/**
+	 * This method is called every time an event is triggered on a GeoElement
+	 * @param eventType string describing the type of the event
+	 * @param eventTarget geo that is the target of the event
+	 */
+	
 	public void handleEvent(String eventType, GeoElement eventTarget);
+	/**
+	 * This method is called every time a geo is selected
+	 * @param geo the selected geo
+	 * @param addToSelection true if it was added to the current selection
+	 */
+	
 	public void notifySelected(GeoElement geo, boolean addToSelection);
+	
+	/**
+	 * Open / close the Python window
+	 */
 	public void toggleWindow();
+	/**
+	 * Check the visibility of the Python window
+	 * @return true if the Python window is currently visible
+	 */
 	public boolean isWindowVisible();
+	
+	/**
+	 * Run a Python script
+	 * @param script script to execute
+	 */
 	public void execute(String script);
 }
