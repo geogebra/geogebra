cb97abb8ca1b4bb02f52e1d705e2b542c4c380f8
Ancsin Gábor <ustakraharez@gmail.com>
2012-02-01 07:16:58 +0000

    Web Workers "Theorem proving" for file loading: the beginning
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@15350 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/geogebra/web/ggbloaderworker/client/GgbLoaderWorker.java
@@ -0,0 +1,13 @@
+package geogebra.web.ggbloaderworker.client;
+
+import com.google.gwt.webworker.client.DedicatedWorkerEntryPoint;
+
+public class GgbLoaderWorker extends DedicatedWorkerEntryPoint {
+
+	@Override
+	public void onWorkerLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
