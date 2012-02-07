e1b1cc94e000f1cfd177cba044021aa5bb738c64
Zbyněk Konečný <zbynek@geogebra.org>
2012-02-07 15:19:22 +0000

    Fix for #1802 -- second attempt
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@15476 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/cas/CasParserToolsImpl.java
+++ common/src/geogebra/common/cas/CasParserToolsImpl.java
@@ -12,12 +12,11 @@ public class CasParserToolsImpl implements CasParserTools {
 	
 	public CasParserToolsImpl(char foreignExpChar) {
 		//e.g. ggbcasvar1e cannot be matched
-		pattern = RegExp.compile("((^|[^W])[0-9]+)" + foreignExpChar,"g");
+		pattern = RegExp.compile("((^|\\W)[0-9]+)" + foreignExpChar,"g");
 	}
 	
 	// convert MathPiper's scientific notation from e.g. 3.24e-4 to 3.2E-4
 	public String convertScientificFloatNotation(String input) {
-		AbstractApplication.debug(input);
 		return pattern.replace(input,"$1"+DEFAULT_EXP_CHAR);
 	}
 	
