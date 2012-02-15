8f7c6e1e7fec544708c3beca395c967d449df528
Ancsin Gábor <ustakraharez@gmail.com>
2012-02-15 15:32:42 +0000

    Key events captured in GlobalKeyDispatcher from EuclidianView
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@15596 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/main/GlobalKeyDispatcher.java
+++ web/src/geogebra/web/main/GlobalKeyDispatcher.java
@@ -1,9 +1,13 @@
 package geogebra.web.main;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+
 import geogebra.common.kernel.geos.GeoElement;
 
 public class GlobalKeyDispatcher extends
-        geogebra.common.main.GlobalKeyDispatcher {
+        geogebra.common.main.GlobalKeyDispatcher implements KeyPressHandler {
 
 	private Application app;
 
@@ -16,5 +20,9 @@ public class GlobalKeyDispatcher extends
 		// TODO Auto-generated method stub
 
 	}
+
+	public void onKeyPress(KeyPressEvent event) {
+	    GWT.log(event.getCharCode()+"");
+    }
 
 }
