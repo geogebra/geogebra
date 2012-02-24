ccd5ee01863e0a5d027c8b347049a472415a2023
Rana Issa <rana@geogebra.org>
2012-02-24 09:55:29 +0000

    Internationalization: creation of symbols interface.
    git-svn-id: http://www.geogebra.org/svn/trunk/geogebra@15751 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/geogebra/web/properties/SymbolsConstants.java
@@ -0,0 +1,89 @@
+package geogebra.web.properties;
+
+import com.google.gwt.i18n.client.ConstantsWithLookup;
+
+@SuppressWarnings("javadoc")
+/**
+ * Colors interface
+ * GWT implementation of Symbols' internationalization.
+ * @author Rana
+ *
+ */
+public interface SymbolsConstants extends ConstantsWithLookup {
+	
+	String T_1();
+
+	String T_8();
+
+	String S_12();
+
+	String T_9();
+
+	String S_13();
+
+	String T_6();
+
+	String S_14();
+
+	String T_7();
+
+	String S_15();
+
+	String T_4();
+
+	String T_5();
+
+	String T_2();
+
+	String S_10();
+
+	String T_3();
+
+	String S_11();
+
+	String S_8();
+
+	String S_7();
+
+	String S_9();
+
+	String S_4();
+
+	String S_19();
+
+	String S_3();
+
+	String S_18();
+
+	String S_6();
+
+	String S_17();
+
+	String S_5();
+
+	String S_16();
+
+	String T_11();
+
+	String T_19();
+
+	String T_12();
+
+	String S_2();
+
+	String T_17();
+
+	String S_1();
+
+	String T_10();
+
+	String T_18();
+
+	String T_15();
+
+	String T_16();
+
+	String T_13();
+
+	String T_14();
+}
