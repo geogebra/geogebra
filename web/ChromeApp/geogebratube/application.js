054327e8b2ce2696a79ce85d80250aa31aca537d
Ancsin Gábor <ustakraharez@gmail.com>
2012-02-26 15:09:11 +0000

    cookies used instead of redirect
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@15781 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/ChromeApp/geogebratube/application.js
+++ web/ChromeApp/geogebratube/application.js
@@ -1,7 +1,4 @@
-/* for now only modfiy the url to mobile
- * because this script run only on GeoGebraTube, we can assume that url already contains nice url.
- * */
 
-if (location.href.indexOf("mobile=true") === -1) {
-    location.href = location.href+"?mobile=true";
+if (document.cookie.indexOf("mobile=true") === -1) {
+	document.cookie = "mobile=true; "+document.cookie;
 }
