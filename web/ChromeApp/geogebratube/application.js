d4f860ae3ff62713d7042d2c9e506ab8ce939ef4
Ancsin Gábor <ustakraharez@gmail.com>
2012-02-27 14:45:01 +0000

    Modifications: (cookies not used, so mobile decided by the browser user agent string, and version number follows Chrome suggestions, options page empty).
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@15793 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/ChromeApp/geogebratube/application.js
+++ web/ChromeApp/geogebratube/application.js
@@ -1,4 +1,10 @@
 
-if (document.cookie.indexOf("mobile=true") === -1) {
+/*App should just go to GeoGebraTube, mobile content not 
+ * served automatically
+ * 
+ * Uncomment this if we want mobile content served from GeoGebraTube,
+ * if App installed
+ * 
+ * if (document.cookie.indexOf("mobile=true") === -1) {
 	document.cookie = "mobile=true; "+document.cookie;
-}
+}*/
