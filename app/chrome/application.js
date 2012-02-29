b1edf0f81780cff23f9fe3cf6e0a525d1b396683
Ancsin Gábor <ustakraharez@gmail.com>
2012-02-29 14:44:33 +0000

    some css
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@15850 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- app/chrome/application.js
+++ app/chrome/application.js
@@ -1,15 +1,30 @@
 /*
  * 
- * Collect links and article elements
+ * Decorate correct links
  * 
  * */
-var geogebratubeLinks = [];
+function decorateLinks(links) {
+	var link;
+	for (var i = 0, l = links.length; i < l; i++) {
+		link = $(links.get(i));
+		if (!link.hasClass("geogebraweblink")) {
+			link.addClass("geogebraweblink");
+		}
+	}
+}
 
-$(document).on("DOMSubtreeModified",function() {
+function collectLinks() {
+	//see document
+	decorateLinks($('a[href*="geogebratube.org/student/"]'));
+	//see iframes
 	$('iframe').each(function() {
 		var as = $(this).contents().find('a[href*="geogebratube.org/student/"]');
 		if (as && as.length && as.length > 0) {
-			console.log(as.length);
+			decorateLinks(as);
 		}
-	});
+	});	
+}
+
+$(document).on("DOMSubtreeModified",function() {
+	collectLinks();
 });
