b46ff108e78c53bc2c27299dd55207c804dc62fc
Ancsin Gábor <ustakraharez@gmail.com>
2012-02-29 07:31:36 +0000

    find links with $("a[href^http://www.geogebratube.org/student/") but not good yet, as ajax content loaded after document ready in gmail...
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@15843 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- app/chrome/application.js
+++ app/chrome/application.js
@@ -3,3 +3,7 @@
  * Collect links and article elements
  * 
  * */
+$(document).ready(function(){
+	var links = $('a[href^="http://www.geogebratube.org/student/"]');
+	console.log(links.length);
+});
