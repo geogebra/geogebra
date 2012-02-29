321de0243272c6d824283d42a30ed448c0a9d289
Ancsin Gábor <ustakraharez@gmail.com>
2012-02-29 15:49:19 +0000

    links decorated, tomorrow continued :-)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@15852 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- app/chrome/application.js
+++ app/chrome/application.js
@@ -3,12 +3,39 @@
  * Decorate correct links
  * 
  * */
+//ugly hack, but css not inserted somehow...
+
+var styles = {
+		"border":"1px solid #9999ff",
+		"border-radius":"3px",
+		"background":"white url("+ chrome.extension.getURL("icon_19.png")+") no-repeat right center",
+		"display":"inline-block",
+		"padding":" 2px 30px 2px 5px",
+		"color":"#666"
+}
+
+function handleMouseOver() {
+	$(this).css({
+		"color" : "black",
+		"text-decoration" : "none"
+	});
+}
+
+function handleMouseOut() {
+	$(this).css({
+		"color" : "#666",
+		"text-decoration" : "underline"
+	});
+}
+
+
 function decorateLinks(links) {
 	var link;
 	for (var i = 0, l = links.length; i < l; i++) {
 		link = $(links.get(i));
 		if (!link.hasClass("geogebraweblink")) {
-			link.addClass("geogebraweblink");
+			link.addClass("geogebraweblink").css(styles)
+			.on("mouseover",handleMouseOver).on("mouseout",handleMouseOut);
 		}
 	}
 }
