32a74cee96c7b5b7a86c4ed254fa74e0229af0f0
Zbyněk Konečný <zbynek@geogebra.org>
2012-03-02 20:44:46 +0000

    Fix FillColumn for functions in trunk
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@15908 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/geos/GeoElementSpreadsheet.java
+++ common/src/geogebra/common/kernel/geos/GeoElementSpreadsheet.java
@@ -3,6 +3,7 @@ package geogebra.common.kernel.geos;
 import geogebra.common.awt.Point;
 import geogebra.common.kernel.Construction;
 import geogebra.common.kernel.StringTemplate;
+import geogebra.common.kernel.arithmetic.FunctionalNVar;
 import geogebra.common.kernel.geos.GeoElement;
 import geogebra.common.main.AbstractApplication;
 
@@ -170,8 +171,12 @@ public class GeoElementSpreadsheet {
 			sb.setLength(0);
 
 		sb.append(cellName);
-		if (cellGeo.isGeoFunction())
-			sb.append("(x)");
+		if (cellGeo instanceof FunctionalNVar){
+			sb.append("(");
+			sb.append(((FunctionalNVar)cellGeo).getVarString(StringTemplate.defaultTemplate));
+			sb.append(")");
+		}
+		
 
 		// getLabel() returns algoParent.getCommandDescription() or
 		// toValueString()
