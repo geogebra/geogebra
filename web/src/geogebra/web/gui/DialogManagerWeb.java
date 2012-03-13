6ed702c85b22fdfa7a97137de06994c930b1e347
Michael Borcherds <michael@geogebra.org>
2012-03-13 12:24:35 +0000

    use Window.prompt()/confirm() rather than native method
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@16063 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/gui/DialogManagerWeb.java
+++ web/src/geogebra/web/gui/DialogManagerWeb.java
@@ -1,22 +1,18 @@
 package geogebra.web.gui;
 
 import geogebra.common.awt.Point;
-import geogebra.common.euclidian.AbstractEuclidianView;
 import geogebra.common.gui.dialog.DialogManager;
-import geogebra.common.kernel.Kernel;
 import geogebra.common.kernel.arithmetic.NumberValue;
 import geogebra.common.kernel.geos.GeoBoolean;
 import geogebra.common.kernel.geos.GeoElement;
 import geogebra.common.kernel.geos.GeoFunction;
-import geogebra.common.kernel.geos.GeoPoint2;
-import geogebra.common.kernel.geos.GeoPolygon;
-import geogebra.common.kernel.geos.GeoSegment;
 import geogebra.common.kernel.geos.GeoText;
 import geogebra.common.kernel.kernelND.GeoPointND;
 import geogebra.common.main.AbstractApplication;
-import geogebra.common.util.Unicode;
 
 import java.util.ArrayList;
+
+import com.google.gwt.user.client.Window;
 
 public class DialogManagerWeb extends DialogManager {
 
@@ -41,15 +37,6 @@ public class DialogManagerWeb extends DialogManager {
 	    // TODO Auto-generated method stub
 	    
     }
-
-	public static native String promptNative(String question, String def) /*-{
-	return $wnd.prompt(question, def);
-}-*/;
-
-	public static native boolean confirmNative(String question) /*-{
-	return $wnd.confirm(question);
-}-*/;
-
 
 	@Override
     public void showBooleanCheckboxCreationDialog(Point loc, GeoBoolean bool) {
@@ -79,12 +66,12 @@ public class DialogManagerWeb extends DialogManager {
 
 	@Override
     protected String prompt(String message, String def) {
-	    return promptNative(message, def);
+	    return Window.prompt(message, def);
     }
 
 	@Override
     protected boolean confirm(String string) {
-	    return confirmNative(string);
+	    return Window.confirm(string);
     }
 
 	@Override
