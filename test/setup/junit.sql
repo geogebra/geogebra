3c97b461063b013b9e85a95c91a67d25774ee799
Zoltán Kovács <zoltan@geogebra.org>
2012-03-22 21:24:25 +0000

    Saving JUnit test XML output into an SQLite3 database
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@16264 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ test/setup/junit.sql
@@ -0,0 +1,14 @@
+create table revisions (
+ id varchar(50) not null primary key,
+ tested timestamp not null);
+
+create table names (
+ id varchar(50) not null primary key);
+
+create table tests (
+ classname varchar(50),
+ name varchar(50),
+ message text,
+ type varchar(100),
+ revision varchar(50) not null,
+ unique (name, revision));
