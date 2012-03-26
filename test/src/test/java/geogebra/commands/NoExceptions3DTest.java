82ed27e1cb8e04bb670a9c62d3c12aa30041def1
Zbyněk Konečný <zbynek@geogebra.org>
2012-03-26 00:02:19 +0000

    Updated NoExceptionsTest
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@16343 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ test/src/test/java/geogebra/commands/NoExceptions3DTest.java
@@ -0,0 +1,116 @@
+package geogebra.commands;
+
+import org.junit.Test;
+
+public class NoExceptions3DTest {
+	@Test
+	public void cmdSurfaceCartesian() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdEnds() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdCone() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdCube() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdBottom() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdDodecahedron() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdCylinder() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdIcosahedron() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdInfiniteCylinder() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdInfiniteCone() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdOctahedron() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdPerpendicularPlane() {
+		// TODO write test
+	}
+	
+	@Test
+	public void cmdPlane() {
+		// TODO write test
+	}
+	
+	@Test
+	public void cmdPlaneBisector() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdPolyhedron() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdPyramid() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdQuadricSide() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdPrism() {
+		// TODO write test
+	}
+	
+	@Test
+	public void cmdSphere() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdTetrahedron() {
+		// TODO write test
+	}
+
+	@Test
+	public void cmdTop() {
+		// TODO write test
+	}
+	
+	@Test
+	public void cmdIntersectionPaths() {
+		// TODO write test
+	}
+
+}
