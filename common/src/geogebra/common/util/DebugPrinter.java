13ab01b86f06a8f7558edb56bf6a4a4cb781e7da
Zoltán Kovács <zoltan@geogebra.org>
2012-04-10 14:09:02 +0000

    Adding javadoc
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@16581 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/util/DebugPrinter.java
+++ common/src/geogebra/common/util/DebugPrinter.java
@@ -1,17 +1,18 @@
 package geogebra.common.util;
 
 /**
- * @author Zoltan Kovacs <zoltan@geogebra.org>
  * Common logging class
+ * @author Zoltan Kovacs <zoltan@geogebra.org>
  */
 public abstract class DebugPrinter {
 	
 	/**
-	 *  
+	 * Sets if debug messages must be shown in production mode 
 	 */
 	public static boolean DEBUG_IN_PRODUCTION = false;
 	
 	/**
+	 * Prints a debug message
 	 * @param s The string to be displayed by the logger
 	 * @param info Leading info string
 	 * @param level Logging level
@@ -19,11 +20,13 @@ public abstract class DebugPrinter {
 	public abstract void print(String s, String info, int level);
 	
 	/**
+	 * Prints a simple debug message
 	 * @param s The string to be displayed by the logger
 	 */
 	public abstract void print(String s);
 	
 	/**
+	 * Prints a detailed debug message
 	 * @param s The string to be displayed by the logger
 	 * @param showMemory if memory info to be shown
 	 * @param showTime if time details to be shown 
