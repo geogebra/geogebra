804f54bc05eb031a80a399117443da02d76547da
Mathieu Blossier <mathieublossier@free.fr>
2012-04-14 21:14:00 +0000

    If Java>=1.6, no accents used in dictionary. Use Class.forName() and so on...
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@16650 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/util/Normalizer6.java
+++ common/src/geogebra/common/util/Normalizer6.java
@@ -1,5 +1,7 @@
 package geogebra.common.util;
 
+import java.lang.reflect.Method;
+
 /**
  * Normalizer to get string to lower case and without accents (if Java >= 1.6)
  * 
@@ -9,11 +11,31 @@ package geogebra.common.util;
 public class Normalizer6 extends Normalizer {
 
 
+	private static final Method normalize;
+	private static final Object NFD;
+	
+	static {
+		try {
+			Class<?> normalizerClass = Class.forName("java.text.Normalizer");
+			Class<?>[] classes = normalizerClass.getClasses();
+			Class<?> formClass = classes[0];
+			NFD = formClass.getField("NFD").get(null);
+			normalize = normalizerClass.getMethod("normalize", CharSequence.class, formClass);
+		} catch (final Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
 	@Override
 	public String transform(String s) {
 		String ret = s.toLowerCase();	 
-		//remove accents
-		return java.text.Normalizer.normalize(ret, java.text.Normalizer.Form.NFD).replaceAll("[\u0300-\u036F]", "");
+		try {
+			return ((String) normalize.invoke(null, ret, NFD)).replaceAll("[\u0300-\u036F]", "");
+		} catch (Exception e) {
+			//should work, if not
+			return ret;
+		}
 		
 	}
 
