4fe001e5674cc1f260aa29c5b493f711468eca2b
Simon Weitzhofer <simon@geogebra.org>
2012-05-02 17:28:48 +0000

    Adding symbolic parameters to AlgoMidpoint
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@17009 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/algos/AlgoMidpoint.java
+++ common/src/geogebra/common/kernel/algos/AlgoMidpoint.java
@@ -18,10 +18,17 @@ the Free Software Foundation.
 
 package geogebra.common.kernel.algos;
 
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.HashSet;
+
 import geogebra.common.kernel.Construction;
 import geogebra.common.kernel.geos.GeoPoint2;
 import geogebra.common.kernel.kernelND.AlgoMidpointND;
 import geogebra.common.kernel.kernelND.GeoPointND;
+import geogebra.common.kernel.prover.FreeVariable;
+import geogebra.common.kernel.prover.NoSymbolicParametersException;
+import geogebra.common.kernel.prover.Polynomial;
 
 
 /**
@@ -29,9 +36,11 @@ import geogebra.common.kernel.kernelND.GeoPointND;
  * @author  Markus
  * @version 
  */
-public class AlgoMidpoint extends AlgoMidpointND {
+public class AlgoMidpoint extends AlgoMidpointND implements SymbolicParametersAlgo{
       
-    public AlgoMidpoint(Construction cons, String label, GeoPoint2 P, GeoPoint2 Q) {
+    private Polynomial[] polynomials;
+
+	public AlgoMidpoint(Construction cons, String label, GeoPoint2 P, GeoPoint2 Q) {
     	this(cons, P, Q);
     	getPoint().setLabel(label);
     }
@@ -67,5 +76,72 @@ public class AlgoMidpoint extends AlgoMidpointND {
                 (P.inhomY + Q.inhomY) / 2.0d,
                 1.0);
     }
+
+	public SymbolicParameters getSymbolicParameters() {
+		return new SymbolicParameters(this);
+	}
+
+	public int[] getFreeVariablesAndDegrees(HashSet<FreeVariable> freeVariables)
+			throws NoSymbolicParametersException {
+		GeoPoint2 P=(GeoPoint2) getP();
+		GeoPoint2 Q=(GeoPoint2) getQ();
+		if (P != null && Q != null) {
+			int[] degreeP = P.getFreeVariablesAndDegrees(freeVariables);
+			int[] degreeQ = Q.getFreeVariablesAndDegrees(freeVariables);
+			
+			int[] result =new int[3];
+			result[0]=Math.max(degreeP[0],degreeQ[0]);
+			result[1] = Math.max(degreeP[1],degreeQ[1]);
+			result[2] = Math.max(degreeP[2],degreeQ[2]);
+			return result;
+		}
+		throw new NoSymbolicParametersException();
+	}
+
+	public BigInteger[] getExactCoordinates(
+			HashMap<FreeVariable, BigInteger> values)
+			throws NoSymbolicParametersException {
+		GeoPoint2 P=(GeoPoint2) getP();
+		GeoPoint2 Q=(GeoPoint2) getQ();
+		if (P != null && Q != null) {
+			BigInteger[] pP = P.getExactCoordinates(values);
+			BigInteger[] pQ = Q.getExactCoordinates(values);
+			BigInteger[] coords = new BigInteger[3];
+			coords[0] = pP[0].add(pQ[0]);
+			coords[1] = pP[1].add(pQ[1]);
+			coords[2] = pP[2].add(pQ[2]);
+			return coords;
+		}
+		throw new NoSymbolicParametersException();
+	}
+
+	public Polynomial[] getPolynomials() throws NoSymbolicParametersException {
+		if (polynomials != null) {
+			return polynomials;
+		}
+		GeoPoint2 P=(GeoPoint2) getP();
+		GeoPoint2 Q=(GeoPoint2) getQ();
+		if (P != null && Q != null) {
+			Polynomial[] pP = P.getPolynomials();
+			Polynomial[] pQ = Q.getPolynomials();
+			polynomials = new Polynomial[3];
+			polynomials[0] = pP[0].add(pQ[0]);
+			polynomials[1] = pP[1].add(pQ[1]);
+			polynomials[2] = pP[2].add(pQ[2]);
+			return polynomials;
+		}
+		throw new NoSymbolicParametersException();
+	}
+
+	public FreeVariable[] getBotanaVars() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Polynomial[] getBotanaPolynomials()
+			throws NoSymbolicParametersException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
