ef6784db6813f3c3c5ec7a1e9c6c83558710339c
Árpád Fekete <fekarp@gmail.com>
2012-05-11 15:00:14 +0000

    Start porting AngleInputDialog - InputDialog is needed for it, so it doesn't work and is not used yet
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@17192 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/geogebra/web/gui/dialog/AngleInputDialog.java
@@ -0,0 +1,110 @@
+/* 
+GeoGebra - Dynamic Mathematics for Everyone
+http://www.geogebra.org
+
+This file is part of GeoGebra.
+
+This program is free software; you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by 
+the Free Software Foundation.
+
+*/
+package geogebra.web.gui.dialog;
+
+import geogebra.web.main.Application;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class AngleInputDialog extends PopupPanel implements ClickHandler {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected RadioButton rbCounterClockWise, rbClockWise;
+	Button btOK, btCancel;
+
+	public boolean success = true;
+	protected String inputText = null;
+
+	protected Application app;
+	protected String initString;
+	
+	/**
+	 * Input Dialog for a GeoAngle object.
+	 */
+	public AngleInputDialog(Application app,  String message, String title, String initString,
+					boolean autoComplete, /*InputHandler handler,*/ boolean modal) {
+		super(false, true);
+		//super(app.getFrame(), modal);
+		this.app = app;
+		//inputHandler = handler;
+		this.initString = initString;
+
+		// create radio buttons for "clockwise" and "counter clockwise"
+		String id = DOM.createUniqueId();
+		rbCounterClockWise = new RadioButton(id, app.getPlain("counterClockwise"));
+		rbClockWise = new RadioButton(id, app.getPlain("clockwise"));
+		rbCounterClockWise.setChecked(true);
+
+		//createGUI(title, message, autoComplete, DEFAULT_COLUMNS, 1, true, false, false, false, DialogType.GeoGebraEditor);
+
+		VerticalPanel centerPanel = new VerticalPanel();
+		//centerPanel.add(inputPanel);
+		centerPanel.add(rbCounterClockWise);
+		centerPanel.add(rbClockWise);
+		HorizontalPanel btPanel = new HorizontalPanel();
+		centerPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
+		centerPanel.add(btPanel);
+
+		btOK = new Button("OK");
+		btOK.addClickHandler(this);
+		btCancel = new Button("Cancel");
+		btCancel.addClickHandler(this);
+		//btApply = new Button("Apply");
+		//btApply.addActionListener(this);
+
+		btPanel.add(btOK);
+		btPanel.add(btCancel);
+
+		setWidget(centerPanel);
+		center();
+	}
+
+	public boolean isCounterClockWise() {
+		return rbCounterClockWise.getValue();
+	}
+
+	public void onClick(ClickEvent e) {
+		Object source = e.getSource();
+
+		boolean finished = false;
+		success=true;
+		try {
+
+				if (source == btOK /*|| source == inputPanel.getTextComponent()*/) {
+			//	inputText = inputPanel.getText();
+
+				// negative orientation ?
+				if (rbClockWise.getValue()) {
+					inputText = "-(" + inputText + ")";
+				}
+
+				//finished = inputHandler.processInput(inputText);
+			} else if (source == btCancel) {
+				finished = true;		
+				success=false;
+			}
+		} catch (Exception ex) {
+			// do nothing on uninitializedValue		
+			success=false;
+		}
+		if (finished) hide();
+		else show();
+	}
+}
