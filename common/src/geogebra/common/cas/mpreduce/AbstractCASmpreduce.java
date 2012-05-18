0c98415a70a543aba2363c22cb5e0c360b87db7d
George Sturr <gsturr@santarosa.edu>
2012-05-18 16:17:24 +0000

    reverted back to default MPReduce switches for now
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@17382 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/cas/mpreduce/AbstractCASmpreduce.java
+++ common/src/geogebra/common/cas/mpreduce/AbstractCASmpreduce.java
@@ -171,7 +171,10 @@ public abstract class AbstractCASmpreduce extends CASgeneric {
 		
 		// set default switches 
 		// (note: off factor turns on exp, so off exp must be placed later)
-		sb.append("$ numeric!!:=0$ precision 30$ print\\_precision 16$ on pri, rationalize  $ off complex, rounded, numval, factor, exp, allfac, div, combinelogs, expandlogs, revpri $ currentx!!:= ");
+		
+		sb.append("$ numeric!!:=0$ precision 30$ print\\_precision 16$ off complex, rounded, numval, factor, div, combinelogs, expandlogs, pri$ currentx!!:= "); 
+	
+	//	sb.append("$ numeric!!:=0$ precision 30$ print\\_precision 16$ on pri, rationalize  $ off complex, rounded, numval, factor, exp, allfac, div, combinelogs, expandlogs, revpri $ currentx!!:= ");
 		
 		sb.append(casPrefix);
 		sb.append("x; currenty!!:= ");
