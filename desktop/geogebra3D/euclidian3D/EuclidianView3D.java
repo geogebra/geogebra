7ff34dc168ba325081d5f8977af8ace756d10ae8
Michael Borcherds <michael@geogebra.org>
2012-05-26 20:54:10 +0000

    3D: show & use clipping cube by default
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@17577 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra3D/euclidian3D/EuclidianView3D.java
+++ desktop/geogebra3D/euclidian3D/EuclidianView3D.java
@@ -1354,8 +1354,8 @@ public class EuclidianView3D extends EuclidianViewND implements Printable {
 	}
 	
 	
-	private boolean useClippingCube = false;
-	private boolean showClippingCube = false;
+	private boolean useClippingCube = true;
+	private boolean showClippingCube = true;
 	
 	/**
 	 * 
