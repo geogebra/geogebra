206c0b2ccc68a6552526494f7cf1da1120a318d4
Zoltán Kovács <zoltan@geogebra.org>
2012-05-29 19:00:02 +0000

    Typo fix
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@17641 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/algos/AlgoAreEqual.java
+++ common/src/geogebra/common/kernel/algos/AlgoAreEqual.java
@@ -187,7 +187,7 @@ public class AlgoAreEqual extends AlgoElement implements
 
 			// We want to prove: 1) ABC collinear, 2) ABD collinear
 			botanaPolynomials[0][0] = Polynomial.collinear(v1[0], v1[1], v1[2], v1[3], v2[0], v2[1]); 
-			botanaPolynomials[1][0] = Polynomial.collinear(v1[0], v1[1], v1[2], v1[3], v2[0], v2[1]); 
+			botanaPolynomials[1][0] = Polynomial.collinear(v1[0], v1[1], v1[2], v1[3], v2[2], v2[3]); 
 			return botanaPolynomials;
 		}
 		
