746619dbf979b34b0f01e4a0c8062d4720e6cf34
Michael Borcherds <michael@geogebra.org>
2012-06-04 07:27:26 +0000

    getBase64() works #2072 (updated zipjs from github, also extendedASCII encoding fixed)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@17779 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/resources/js/zipjs/zip.js
+++ web/src/geogebra/resources/js/zipjs/zip.js
@@ -103,7 +103,7 @@
 		function init(callback, onerror) {
 			var blobBuilder = new BlobBuilder();
 			blobBuilder.append(text);
-			blobReader = new BlobReader(blobBuilder.getBlob());
+			blobReader = new BlobReader(blobBuilder.getBlob("text/plain"));
 			blobReader.init(function() {
 				that.size = blobReader.size;
 				callback();
@@ -289,7 +289,7 @@
 				callback(e.target.result);
 			};
 			reader.onerror = onerror;
-			reader.readAsText(blobBuilder.getBlob());
+			reader.readAsText(blobBuilder.getBlob("text/plain"));
 		}
 
 		that.init = init;
@@ -299,11 +299,11 @@
 	TextWriter.prototype = new Writer();
 	TextWriter.prototype.constructor = TextWriter;
 
-	function Data64URIWriter(mimeString) {
+	function Data64URIWriter(contentType) {
 		var that = this, data = "", pending = "";
 
 		function init(callback, onerror) {
-			data += "data:" + (mimeString || "") + ";base64,";
+			data += "data:" + (contentType || "") + ";base64,";
 			callback();
 		}
 
@@ -314,7 +314,10 @@
 				dataString += String.fromCharCode(array[i]);
 			for (; i < array.length; i++)
 				pending += String.fromCharCode(array[i]);
-			data += obj.btoa(dataString);
+			if (dataString.length > 2)
+				data += obj.btoa(dataString);
+			else
+				pending = dataString;
 			callback();
 		}
 
@@ -329,7 +332,7 @@
 	Data64URIWriter.prototype = new Writer();
 	Data64URIWriter.prototype.constructor = Data64URIWriter;
 
-	function FileWriter(fileEntry) {
+	function FileWriter(fileEntry, contentType) {
 		var writer, that = this;
 
 		function init(callback, onerror) {
@@ -347,7 +350,7 @@
 				callback();
 			};
 			writer.onerror = onerror;
-			writer.write(blobBuilder.getBlob());
+			writer.write(blobBuilder.getBlob(contentType));
 		}
 
 		function getData(callback) {
@@ -361,7 +364,7 @@
 	FileWriter.prototype = new Writer();
 	FileWriter.prototype.constructor = FileWriter;
 
-	function BlobWriter() {
+	function BlobWriter(contentType) {
 		var blobBuilder, that = this;
 
 		function init(callback, onerror) {
@@ -375,7 +378,7 @@
 		}
 
 		function getData(callback) {
-			callback(blobBuilder.getBlob());
+			callback(blobBuilder.getBlob(contentType));
 		}
 
 		that.init = init;
@@ -555,11 +558,11 @@
 	// ZipReader
 
 	function decodeASCII(str) {
-		var i, out = "", charCode, extendedASCII = [ '\u00C7', '\u00FC', '\u00E9', '\u00E2', '\u00E4', '\u00E0', '\u00E5', '\u00E7', '\u00EA', '\u00EB', '\u00E8', '\u00EF', '\u00EE', '\u00EC', '\u00C4', '\u00C5', '\u00C9', '\u00E6', '\u00C6', '\u00F4', '\u00F6',
-		                                             '\u00F2', '\u00FB', '\u00F9', '\u00FF', '\u00D6', '\u00DC', '\u00F8', '\u00A3', '\u00D8', '\u00D7', '\u0192', '\u00E1', '\u00ED', '\u00F3', '\u00FA', '\u00F1', '\u00D1', '\u00AA', '\u00BA', '\u00BF', '\u00AE', '\u00AC', '\u00BD', '\u00BC', '\u00A1', '\u00AB', '\u00BB', '_',
-		                                             '_', '_', '\u00A6', '\u00A6', '\u00C1', '\u00C2', '\u00C0', '\u00A9', '\u00A6', '\u00A6', '+', '+', '\u00A2', '\u00A5', '+', '+', '-', '-', '+', '-', '+', '\u00E3', '\u00C3', '+', '+', '-', '-', '\u00A6',
-		                                             '-', '+', '\u00A4', '\u00F0', '\u00D0', '\u00CA', '\u00CB', '\u00C8', 'i', '\u00CD', '\u00CE', '\u00CF', '+', '+', '_', '_', '\u00A6', '\u00CC', '_', '\u00D3', '\u00DF', '\u00D4', '\u00D2', '\u00F5', '\u00D5', '\u00B5', '\u00FE', '\u00DE',
-		                                             '\u00DA', '\u00DB', '\u00D9', '\u00FD', '\u00DD', '\u00AF', '\u00B4', '\u00AD', '\u00B1', '_', '\u00BE', '\u00B6', '\u00A7', '\u00F7', '\u00B8', '\u00B0', '\u00A8', '\u00B7', '\u00B9', '\u00B3', '\u00B2', '_', ' ' ];
+		extendedASCII = [ '\u00C7', '\u00FC', '\u00E9', '\u00E2', '\u00E4', '\u00E0', '\u00E5', '\u00E7', '\u00EA', '\u00EB', '\u00E8', '\u00EF', '\u00EE', '\u00EC', '\u00C4', '\u00C5', '\u00C9', '\u00E6', '\u00C6', '\u00F4', '\u00F6',
+		                  '\u00F2', '\u00FB', '\u00F9', '\u00FF', '\u00D6', '\u00DC', '\u00F8', '\u00A3', '\u00D8', '\u00D7', '\u0192', '\u00E1', '\u00ED', '\u00F3', '\u00FA', '\u00F1', '\u00D1', '\u00AA', '\u00BA', '\u00BF', '\u00AE', '\u00AC', '\u00BD', '\u00BC', '\u00A1', '\u00AB', '\u00BB', '_',
+		                  '_', '_', '\u00A6', '\u00A6', '\u00C1', '\u00C2', '\u00C0', '\u00A9', '\u00A6', '\u00A6', '+', '+', '\u00A2', '\u00A5', '+', '+', '-', '-', '+', '-', '+', '\u00E3', '\u00C3', '+', '+', '-', '-', '\u00A6',
+		                  '-', '+', '\u00A4', '\u00F0', '\u00D0', '\u00CA', '\u00CB', '\u00C8', 'i', '\u00CD', '\u00CE', '\u00CF', '+', '+', '_', '_', '\u00A6', '\u00CC', '_', '\u00D3', '\u00DF', '\u00D4', '\u00D2', '\u00F5', '\u00D5', '\u00B5', '\u00FE', '\u00DE',
+		                  '\u00DA', '\u00DB', '\u00D9', '\u00FD', '\u00DD', '\u00AF', '\u00B4', '\u00AD', '\u00B1', '_', '\u00BE', '\u00B6', '\u00A7', '\u00F7', '\u00B8', '\u00B0', '\u00A8', '\u00B7', '\u00B9', '\u00B3', '\u00B2', '_', ' ' ];
 		for (i = 0; i < str.length; i++) {
 			charCode = str.charCodeAt(i) & 0xFF;
 			if (charCode > 127)
@@ -924,7 +927,7 @@
 				callback(createZipWriter(writer, onerror, dontDeflate));
 			}, onerror);
 		},
-		workerScriptsPath : "web/js/zipjs/",
+		workerScriptsPath : "",
 		useWebWorkers : true
 	};
 
