3d1167149a7fdd820d00a4ff708f20fca0133d51
Michael Borcherds <michael@geogebra.org>
2012-06-12 10:37:56 +0000

    web: remove languages from menu
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@17985 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/util/Language.java
+++ common/src/geogebra/common/util/Language.java
@@ -14,11 +14,11 @@ import geogebra.common.main.AbstractApplication;
 public enum Language {
 	
 	// need to be in Alphabetical order so they appear in the menu in the right order
-	Afrikaans(false, "af","af", "Afrikaans", Country.SouthAfrica),
-	Armenian(true, "hy","hy", "Armenian / \u0540\u0561\u0575\u0565\u0580\u0565\u0576", Country.Armenia),
+	Afrikaans(false, false, "af","af", "Afrikaans", Country.SouthAfrica),
+	Armenian(true, false, "hy","hy", "Armenian / \u0540\u0561\u0575\u0565\u0580\u0565\u0576", Country.Armenia),
 
-	Albanian(false, "sq","sq", "Albanian / Gjuha Shqipe", Country.Albania),
-	Arabic(true, "ar", "ar", "Arabic / \u0627\u0644\u0639\u0631\u0628\u064A\u0629",
+	Albanian(false, false, "sq","sq", "Albanian / Gjuha Shqipe", Country.Albania),
+	Arabic(true, true, "ar", "ar", "Arabic / \u0627\u0644\u0639\u0631\u0628\u064A\u0629",
 			Country.Egypt, Country.Algeria, Country.Bahrain, Country.Chad,
 			Country.Comoros, Country.Djibouti, Country.Eritrea, Country.Iraq,
 			Country.Jordan, Country.Kuwait, Country.Lebanon, Country.Libya,
@@ -26,17 +26,17 @@ public enum Language {
 			Country.Palestine, Country.Qatar, Country.SaudiArabia,
 			Country.Somalia, Country.Sudan, Country.Syria, Country.Tunisia,
 			Country.UnitedArabEmirates, Country.WesternSahara, Country.Yemen),
-	Basque(true, "eu","eu", "Basque / Euskara", "basque"), // fudge to get right flag
-	Bosnian(true, "bs","bs", "Bosnian / \u0431\u043E\u0441\u0430\u043D\u0441\u043A\u0438", Country.BosniaandHerzegovina),
-	Bulgarian(false, "bg","bg", "Bulgarian / \u0431\u044A\u043B\u0433\u0430\u0440\u0441\u043A\u0438 \u0435\u0437\u0438\u043A", Country.Bulgaria),
-	Catalan(true, "ca","ca", "Catalan / Catal\u00E0", "catalonia"), // fudge to get right flag
-	Chinese_Simplified(true, "zhCN","zhCN", "Chinese Simplified / \u7B80\u4F53\u4E2D\u6587", Country.China, Country.Singapore),
-	Chinese_Traditional(true, "zhTW","zhTW", "Chinese Traditional / \u7E41\u9AD4\u4E2D\u6587", Country.TaiwanProvinceofChina),
-	Croatian(true, "hr","hr", "Croatian / Hrvatska", Country.Croatia, Country.BosniaandHerzegovina),
-	Czech(true, "cs","cs", "Czech / \u010Ce\u0161tina", Country.CzechRepublic),
-	Danish(true, "da","da", "Danish / Dansk", Country.Denmark),
-	Dutch(true, "nl","nl", "Dutch / Nederlands", Country.Netherlands, Country.Belgium, Country.Suriname),
-	English_US(true, "en", "en",
+	Basque(true, false, "eu","eu", "Basque / Euskara", "basque"), // fudge to get right flag
+	Bosnian(true, false, "bs","bs", "Bosnian / \u0431\u043E\u0441\u0430\u043D\u0441\u043A\u0438", Country.BosniaandHerzegovina),
+	Bulgarian(false, false, "bg","bg", "Bulgarian / \u0431\u044A\u043B\u0433\u0430\u0440\u0441\u043A\u0438 \u0435\u0437\u0438\u043A", Country.Bulgaria),
+	Catalan(true, false, "ca","ca", "Catalan / Catal\u00E0", "catalonia"), // fudge to get right flag
+	Chinese_Simplified(true, false, "zhCN","zhCN", "Chinese Simplified / \u7B80\u4F53\u4E2D\u6587", Country.China, Country.Singapore),
+	Chinese_Traditional(true, false, "zhTW","zhTW", "Chinese Traditional / \u7E41\u9AD4\u4E2D\u6587", Country.TaiwanProvinceofChina),
+	Croatian(true, false, "hr","hr", "Croatian / Hrvatska", Country.Croatia, Country.BosniaandHerzegovina),
+	Czech(true, false, "cs","cs", "Czech / \u010Ce\u0161tina", Country.CzechRepublic),
+	Danish(true, false, "da","da", "Danish / Dansk", Country.Denmark),
+	Dutch(true, false, "nl","nl", "Dutch / Nederlands", Country.Netherlands, Country.Belgium, Country.Suriname),
+	English_US(true, true, "en", "en",
 			"English (US)", Country.UnitedStatesofAmerica,
 			Country.AntiguaBarbuda, Country.Bahamas, Country.Barbados,
 			Country.Belize, Country.Botswana, Country.Cameroon, Country.Canada,
@@ -55,12 +55,12 @@ public enum Language {
 			Country.Swaziland, Country.Tanzania, Country.Tonga,
 			Country.TrinidadTobago, Country.Tuvalu, Country.Uganda,
 			Country.Vanuatu, Country.Zambia, Country.Zimbabwe),
-	English_UK(true, "enGB", "en_GB", "English (UK)", Country.UnitedKingdom),
-	English_Australia(true, "en_AU", "enAU", "English (Australia)", Country.Australia),
-	Estonian(true, "et","et", "Estonian / Eesti keel", Country.Estonia),
-	Filipino(true, "tl","tl", "Filipino", Country.Philippines),
-	Finnish(true, "fi","fi", "Finnish / Suomi", Country.Finland),
-	French(true, "fr", "fr",
+	English_UK(true, true, "enGB", "en_GB", "English (UK)", Country.UnitedKingdom),
+	English_Australia(true, false, "en_AU", "enAU", "English (Australia)", Country.Australia),
+	Estonian(true, false, "et","et", "Estonian / Eesti keel", Country.Estonia),
+	Filipino(true, false, "tl","tl", "Filipino", Country.Philippines),
+	Finnish(true, false, "fi","fi", "Finnish / Suomi", Country.Finland),
+	French(true, true, "fr", "fr",
 			"French / Fran\u00E7ais", Country.France,
 			Country.CongoDemocraticRepublicOf, Country.Canada,
 			Country.Madagascar, Country.Cameroon, Country.CoteDivoire,
@@ -71,59 +71,59 @@ public enum Language {
 			Country.Gabon, Country.Comoros, Country.EquatorialGuinea,
 			Country.Djibouti, Country.Luxembourg, Country.Vanuatu,
 			Country.Seychelles, Country.Monaco),
-	Galician(true, "gl","gl", "Galician / Galego", "Galician"), // fudge to get right flag
-	Georgian(true, "ka","ka", "Georgian / \u10E5\u10D0\u10E0\u10D7\u10E3\u10DA\u10D8 \u10D4\u10DC\u10D0", Country.Georgia),
+	Galician(true, false, "gl","gl", "Galician / Galego", "Galician"), // fudge to get right flag
+	Georgian(true, false, "ka","ka", "Georgian / \u10E5\u10D0\u10E0\u10D7\u10E3\u10DA\u10D8 \u10D4\u10DC\u10D0", Country.Georgia),
 	// German must be before German_Austria
-	German(true, "de","de", "German / Deutsch", Country.Germany, Country.Liechtenstein, Country.Luxembourg, Country.Switzerland, Country.Belgium),
-	German_Austria(true, "deAT","de_AT", "German (Austria) / Deutsch (\u00D6sterreich)", Country.Austria),
+	German(true, false, "de","de", "German / Deutsch", Country.Germany, Country.Liechtenstein, Country.Luxembourg, Country.Switzerland, Country.Belgium),
+	German_Austria(true, false, "deAT","de_AT", "German (Austria) / Deutsch (\u00D6sterreich)", Country.Austria),
 	
-	Greek(true, "el","el", "Greek / \u0395\u03BB\u03BB\u03B7\u03BD\u03B9\u03BA\u03AC", Country.Greece, Country.Cyprus),
-	Hebrew(true, "iw","iw", "Hebrew / \u05E2\u05B4\u05D1\u05B0\u05E8\u05B4\u05D9\u05EA", Country.Israel),
-	Hindi(false, "hi","hi", "Hindi / \u092E\u093E\u0928\u0915 \u0939\u093F\u0928\u094D\u0926\u0940", Country.India),
-	Hungarian(true, "hu","hu", "Hungarian / Magyar", Country.Hungary),
-	Icelandic(true, "is","is", "Icelandic / \u00CDslenska", Country.Iceland),
-	Indonesian(false, "in","in", "Indonesian / Bahasa Indonesia", Country.Indonesia),
-	Italian(true, "it","it", "Italian / Italiano", Country.Italy, Country.Switzerland, Country.SanMarino, Country.VaticanCityState),
-	Japanese(true, "ja","ja", "Japanese / \u65E5\u672C\u8A9E", Country.Japan),
-	Kazakh(true, "kk","kk", "Kazakh / \u049A\u0430\u0437\u0430\u049B \u0442\u0456\u043B\u0456", Country.Kazakhstan),
-	Korean(true, "ko","ko", "Korean / \uD55C\uAD6D\uB9D0", Country.KoreaRepublicof, Country.KoreaDemocraticPeoplesRepublicof),
-	Lithuanian(true, "lt","lt", "Lithuanian / Lietuvi\u0173 kalba", Country.Lithuania),
-	Malay(true, "ms","ms", "Malay / Bahasa Malaysia", Country.Malaysia, Country.Singapore, Country.Indonesia, Country.BruneiDarussalam),
-	Malayalam(false, "ml","ml", "Malayalam / \u0D2E\u0D32\u0D2F\u0D3E\u0D33\u0D02", Country.India),
-	Macedonian(true, "mk","mk", "Macedonian / \u041C\u0430\u043A\u0435\u0434\u043E\u043D\u0441\u043A\u0438 \u0458\u0430\u0437\u0438\u043A", Country.Macedonia),
-	Marathi(false, "mr","mr", "Marathi / \u092E\u0930\u093E\u0920\u0940", Country.India),
-	Mongolian(false, "mn","mn", "Mongolian / \u041C\u043E\u043D\u0433\u043E\u043B \u0445\u044D\u043B", Country.Mongolia),
-	Nepalese(false, "ne","ne", "Nepalese / \u0928\u0947\u092A\u093E\u0932\u0940", Country.Nepal),
-	Norwegian_Bokmal(true, "noNO","no_NB", "Norwegian / Bokm\u00e5l", Country.Norway ),
-	Norwegian_Nynorsk(true, "noNONY","no_NN", "Norwegian / Nynorsk", Country.Norway),
-	Persian(true, "fa","fa", "Persian / \u0641\u0627\u0631\u0633\u06CC", Country.IranIslamicRepublicof, Country.Afghanistan, Country.Tajikistan),
-	Polish(true, "pl","pl", "Polish / J\u0119zyk polski", Country.Poland),
+	Greek(true, false, "el","el", "Greek / \u0395\u03BB\u03BB\u03B7\u03BD\u03B9\u03BA\u03AC", Country.Greece, Country.Cyprus),
+	Hebrew(true, false, "iw","iw", "Hebrew / \u05E2\u05B4\u05D1\u05B0\u05E8\u05B4\u05D9\u05EA", Country.Israel),
+	Hindi(false, false, "hi","hi", "Hindi / \u092E\u093E\u0928\u0915 \u0939\u093F\u0928\u094D\u0926\u0940", Country.India),
+	Hungarian(true, true, "hu","hu", "Hungarian / Magyar", Country.Hungary),
+	Icelandic(true, false, "is","is", "Icelandic / \u00CDslenska", Country.Iceland),
+	Indonesian(false, false, "in","in", "Indonesian / Bahasa Indonesia", Country.Indonesia),
+	Italian(true, false, "it","it", "Italian / Italiano", Country.Italy, Country.Switzerland, Country.SanMarino, Country.VaticanCityState),
+	Japanese(true, false, "ja","ja", "Japanese / \u65E5\u672C\u8A9E", Country.Japan),
+	Kazakh(true, false, "kk","kk", "Kazakh / \u049A\u0430\u0437\u0430\u049B \u0442\u0456\u043B\u0456", Country.Kazakhstan),
+	Korean(true, false, "ko","ko", "Korean / \uD55C\uAD6D\uB9D0", Country.KoreaRepublicof, Country.KoreaDemocraticPeoplesRepublicof),
+	Lithuanian(true, false, "lt","lt", "Lithuanian / Lietuvi\u0173 kalba", Country.Lithuania),
+	Malay(true, false, "ms","ms", "Malay / Bahasa Malaysia", Country.Malaysia, Country.Singapore, Country.Indonesia, Country.BruneiDarussalam),
+	Malayalam(false, false, "ml","ml", "Malayalam / \u0D2E\u0D32\u0D2F\u0D3E\u0D33\u0D02", Country.India),
+	Macedonian(true, false, "mk","mk", "Macedonian / \u041C\u0430\u043A\u0435\u0434\u043E\u043D\u0441\u043A\u0438 \u0458\u0430\u0437\u0438\u043A", Country.Macedonia),
+	Marathi(false, false, "mr","mr", "Marathi / \u092E\u0930\u093E\u0920\u0940", Country.India),
+	Mongolian(false, false, "mn","mn", "Mongolian / \u041C\u043E\u043D\u0433\u043E\u043B \u0445\u044D\u043B", Country.Mongolia),
+	Nepalese(false, false, "ne","ne", "Nepalese / \u0928\u0947\u092A\u093E\u0932\u0940", Country.Nepal),
+	Norwegian_Bokmal(true, false, "noNO","no_NB", "Norwegian / Bokm\u00e5l", Country.Norway ),
+	Norwegian_Nynorsk(true, false, "noNONY","no_NN", "Norwegian / Nynorsk", Country.Norway),
+	Persian(true, false, "fa","fa", "Persian / \u0641\u0627\u0631\u0633\u06CC", Country.IranIslamicRepublicof, Country.Afghanistan, Country.Tajikistan),
+	Polish(true, false, "pl","pl", "Polish / J\u0119zyk polski", Country.Poland),
 	// use Brazilian as the root (ie not ptBR) as there are more speakers
-	Portuguese_Brazil(true, "pt","pt", "Portuguese (Brazil) / Portugu\u00EAs (Brasil)", Country.Brazil),
-	Portuguese_Portugal(true, "ptPT","pt_PT", "Portuguese (Portugal) / Portugu\u00EAs (Portugal)", Country.Portugal, Country.Mozambique, Country.Angola, Country.CapeVerde, Country.GuineaBissau, Country.SaoTomePrincipe, Country.Macau, Country.EastTimor),
-	Romanian(false, "ro","ro", "Romanian /  Rom\u00E2n\u0103", Country.Romania, Country.MoldovaRepublicof),
-	Russian(true, "ru","ru", "Russian / \u0420\u0443\u0441\u0441\u043A\u0438\u0439 \u044F\u0437\u044B\u043A", Country.RussianFederation, Country.Kazakhstan, Country.Belarus, Country.Kyrgyzstan, Country.Tajikistan),
-	Sinhala(false, "si","si", "Sinhala / \u0DC3\u0DD2\u0D82\u0DC4\u0DBD", Country.SriLanka),
-	Serbian(true, "sr","sr", "Serbian / \u0441\u0440\u043F\u0441\u043A\u0438", Country.Serbia, Country.BosniaandHerzegovina),
-	Slovakian(true, "sk","sk", "Slovakian / Slovensk\u00FD jazyk", Country.Slovakia),
-	Slovenian(true, "sl","sl", "Slovenian / Sloven\u0161\u010Dina", Country.Slovenia),
-	Spanish(true, "es", "es", "Spanish / Espa\u00F1ol", Country.Spain,
+	Portuguese_Brazil(true, false, "pt","pt", "Portuguese (Brazil) / Portugu\u00EAs (Brasil)", Country.Brazil),
+	Portuguese_Portugal(true, false, "ptPT","pt_PT", "Portuguese (Portugal) / Portugu\u00EAs (Portugal)", Country.Portugal, Country.Mozambique, Country.Angola, Country.CapeVerde, Country.GuineaBissau, Country.SaoTomePrincipe, Country.Macau, Country.EastTimor),
+	Romanian(false, false, "ro","ro", "Romanian /  Rom\u00E2n\u0103", Country.Romania, Country.MoldovaRepublicof),
+	Russian(true, false, "ru","ru", "Russian / \u0420\u0443\u0441\u0441\u043A\u0438\u0439 \u044F\u0437\u044B\u043A", Country.RussianFederation, Country.Kazakhstan, Country.Belarus, Country.Kyrgyzstan, Country.Tajikistan),
+	Sinhala(false, false, "si","si", "Sinhala / \u0DC3\u0DD2\u0D82\u0DC4\u0DBD", Country.SriLanka),
+	Serbian(true, false, "sr","sr", "Serbian / \u0441\u0440\u043F\u0441\u043A\u0438", Country.Serbia, Country.BosniaandHerzegovina),
+	Slovakian(true, false, "sk","sk", "Slovakian / Slovensk\u00FD jazyk", Country.Slovakia),
+	Slovenian(true, false, "sl","sl", "Slovenian / Sloven\u0161\u010Dina", Country.Slovenia),
+	Spanish(true, true, "es", "es", "Spanish / Espa\u00F1ol", Country.Spain,
 			Country.EquatorialGuinea, Country.Argentina, Country.Bolivia,
 			Country.Chile, Country.Colombia, Country.CostaRica, Country.Cuba,
 			Country.DominicanRepublic, Country.ElSalvador, Country.Guatemala,
 			Country.Honduras, Country.Mexico, Country.Nicaragua,
 			Country.Panama, Country.Paraguay, Country.Ecuador, Country.Peru,
 			Country.Uruguay, Country.Venezuela, Country.PuertoRico),
-	Swedish(true, "sv","sv", "Swedish / Svenska", Country.Sweden, Country.Finland),
-	Tamil(true, "ta","ta", "Tamil / \u0BA4\u0BAE\u0BBF\u0BB4\u0BCD", Country.India, Country.Singapore),
-	Thai(false, "th","th", "Thai / \u0E20\u0E32\u0E29\u0E32\u0E44\u0E17\u0E22", Country.Thailand),
-	Turkish(false, "tr","tr", "Turkish / T\u00FCrk\u00E7e", Country.Turkey, Country.Cyprus),
-	Ukrainian(false, "uk","uk", "Ukrainian / \u0423\u043A\u0440\u0430\u0457\u043D\u0441\u044C\u043A\u0430 \u043C\u043E\u0432\u0430", Country.Ukraine),
-	Vietnamese(false, "vi","vi", "Vietnamese / Ti\u1EBFng Vi\u1EC7t", Country.VietNam),
-	Welsh(true, "cy","cy", "Welsh / Cymraeg", "wales"), // fudge to get right flag
-	Yiddish(false, "ji","ji", "Yiddish / \u05D9\u05D9\u05B4\u05D3\u05D9\u05E9", Country.Israel),
+	Swedish(true, false, "sv","sv", "Swedish / Svenska", Country.Sweden, Country.Finland),
+	Tamil(true, false, "ta","ta", "Tamil / \u0BA4\u0BAE\u0BBF\u0BB4\u0BCD", Country.India, Country.Singapore),
+	Thai(false, false, "th","th", "Thai / \u0E20\u0E32\u0E29\u0E32\u0E44\u0E17\u0E22", Country.Thailand),
+	Turkish(false, false, "tr","tr", "Turkish / T\u00FCrk\u00E7e", Country.Turkey, Country.Cyprus),
+	Ukrainian(false, false, "uk","uk", "Ukrainian / \u0423\u043A\u0440\u0430\u0457\u043D\u0441\u044C\u043A\u0430 \u043C\u043E\u0432\u0430", Country.Ukraine),
+	Vietnamese(false, false, "vi","vi", "Vietnamese / Ti\u1EBFng Vi\u1EC7t", Country.VietNam),
+	Welsh(true, false, "cy","cy", "Welsh / Cymraeg", "wales"), // fudge to get right flag
+	Yiddish(false, false, "ji","ji", "Yiddish / \u05D9\u05D9\u05B4\u05D3\u05D9\u05E9", Country.Israel),
 	
-	Interlingua(false, "ia", "ia", "Interlingua", Country.UnitedStatesofAmerica);
+	Interlingua(false, false, "ia", "ia", "Interlingua", Country.UnitedStatesofAmerica);
 
 
 	public String localeGWT;
@@ -134,23 +134,26 @@ public enum Language {
 	public String flagName;
 	// used to determine whether to put in release versions
 	public  boolean fullyTranslated;
+	public boolean enableInGWT;
 	
-	Language(boolean fullyTranslated, String locale,String localeGWT, String name, Country ... countries) {
+	Language(boolean fullyTranslated, boolean enableInGWT, String locale,String localeGWT, String name, Country ... countries) {
 		this.locale = locale;
 		this.localeGWT = localeGWT;
 		this.name = name;
 		this.countries = countries;
 		this.flagName = null;
 		this.fullyTranslated = fullyTranslated;
+		this.enableInGWT = enableInGWT;
 		
 	}
-	Language(boolean fullyTranslated, String locale,String localeGWT, String name, String flagName) {
+	Language(boolean fullyTranslated, boolean enableInGWT, String locale,String localeGWT, String name, String flagName) {
 		this.locale = locale;
 		this.localeGWT = localeGWT;
 		this.name = name;
 		this.countries = null;
 		this.flagName = flagName;
 		this.fullyTranslated = fullyTranslated;
+		this.enableInGWT = enableInGWT;
 		
 	}
 	
@@ -241,5 +244,21 @@ public enum Language {
 		AbstractApplication.error("language not found: "+ggbLangCode);
 		
 		return null;
+	}
+	public static boolean isEnabledInGWT(String ggbLangCode) {
+		
+		// eg change en_GB to enGB
+		ggbLangCode = ggbLangCode.replaceAll("_", "");
+		
+		AbstractApplication.debug("looking for: "+ggbLangCode);
+		for (Language l : Language.values()) {
+			if (l.locale.equals(ggbLangCode)) {
+				return l.enableInGWT;
+			}
+		}
+		
+		AbstractApplication.error("language not found: "+ggbLangCode);
+		
+		return false;
 	}
 }
--- web/src/geogebra/web/gui/menubar/LanguageMenu.java
+++ web/src/geogebra/web/gui/menubar/LanguageMenu.java
@@ -59,27 +59,32 @@ public class LanguageMenu extends MenuBar {
 	
 	
 	private void addItems() {
-		
+
 		for(int i=0; i < Application.getSupportedLanguages().size(); i++) {			
 			String languageCode = Application.getSupportedLanguages().get(i);
-			
-			String languageName = Language.getDisplayName(Application.languageCodeVariationCrossReferencing(languageCode.replace(Application.AN_UNDERSCORE, "")));
-			
-			if(languageName != null) {
-				
-				char ch = languageName.toUpperCase().charAt(0);
-				
-				if(ch <= 'D') {
-					
-					atoDMenuBar.addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE.empty().getSafeUri().asString(),languageName),true,new LanguageCommand(languageCode));
-				} else if(ch <= 'I') {
-					etoIMenuBar.addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE.empty().getSafeUri().asString(),languageName),true,new LanguageCommand(languageCode));
-				} else if(ch <= 'Q') {
-					jtoQMenuBar.addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE.empty().getSafeUri().asString(),languageName),true,new LanguageCommand(languageCode));
-				} else {
-					rtoZMenuBar.addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE.empty().getSafeUri().asString(),languageName),true,new LanguageCommand(languageCode));
+
+			String lang = Application.languageCodeVariationCrossReferencing(languageCode.replace(Application.AN_UNDERSCORE, ""));
+
+			if (Language.isEnabledInGWT(lang)) {
+
+				String languageName = Language.getDisplayName(lang);
+
+				if(languageName != null) {
+
+					char ch = languageName.toUpperCase().charAt(0);
+
+					if(ch <= 'D') {
+
+						atoDMenuBar.addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE.empty().getSafeUri().asString(),languageName),true,new LanguageCommand(languageCode));
+					} else if(ch <= 'I') {
+						etoIMenuBar.addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE.empty().getSafeUri().asString(),languageName),true,new LanguageCommand(languageCode));
+					} else if(ch <= 'Q') {
+						jtoQMenuBar.addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE.empty().getSafeUri().asString(),languageName),true,new LanguageCommand(languageCode));
+					} else {
+						rtoZMenuBar.addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE.empty().getSafeUri().asString(),languageName),true,new LanguageCommand(languageCode));
+					}
+
 				}
-				
 			}
 		}
 
