0edf8896a2bcc5e2c9cc6ff356cd5f3947bfd0d1
Judit Elias <eliasjudit@gmail.com>
2012-06-18 17:33:28 +0000

    New items for "Edit" menu in web: copy, paste, select all.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@18098 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/gui/menubar/EditMenu.java
+++ web/src/geogebra/web/gui/menubar/EditMenu.java
@@ -1,54 +1,146 @@
 package geogebra.web.gui.menubar;
 
 import geogebra.common.main.AbstractApplication;
+import geogebra.common.util.CopyPaste;
 import geogebra.web.gui.images.AppResources;
 import geogebra.web.main.Application;
 
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.MenuBar;
 
+/**
+ * The "Edit" menu.
+ */
 public class EditMenu extends MenuBar {
-	
-	private AbstractApplication app;
 
+	/**
+	 * Application instance
+	 */
+	AbstractApplication app;
+
+	/**
+	 * Constructs the "Edit" menu
+	 * @param app Application instance
+	 */
 	public EditMenu(AbstractApplication app) {
 
 		super(true);
-	    this.app = app;
-	    addStyleName("GeoGebraMenuBar");
-	    initActions();
+		this.app = app;
+		addStyleName("GeoGebraMenuBar");
+		initActions();
 	}
 
 	private void initActions() {
-		
-//		if (enabled_undo)
-			addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE.edit_undo().getSafeUri().asString(), app.getMenu("Undo")),true,new Command() {
+
+		// undo menu
+//		if (app.getKernel().undoPossible())
+			addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE
+			        .edit_undo().getSafeUri().asString(), app.getMenu("Undo")),
+			        true, new Command() {
+				        public void execute() {
+					        app.getGuiManager().undo();
+				        }
+			        });
+//		else
+//			addItem(GeoGebraMenubar.getMenuBarHtmlGrayout(AppResources.INSTANCE
+//			        .edit_undo().getSafeUri().asString(), app.getMenu("Undo")),
+//			        true, new Command() {
+//				public void execute() {
+//					// do nothing
+//				}
+//			});
+
+
+		// redo menu
+//		if (app.getKernel().redoPossible())
+			addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE
+			        .edit_redo().getSafeUri().asString(), app.getMenu("Redo")),
+			        true, new Command() {
+				        public void execute() {
+					        app.getGuiManager().redo();
+				        }
+			        });
+//		else
+//			addItem(GeoGebraMenubar.getMenuBarHtmlGrayout(AppResources.INSTANCE
+//			        .edit_redo().getSafeUri().asString(), app.getMenu("Redo")),
+//			        true, new Command() {
+//				public void execute() {
+//					// do nothing
+//				}
+//			});
+
+		// separator
+		addSeparator();
 			
+		// copy menu
+//		if (!app.getSelectedGeos().isEmpty())
+			addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE
+			        .edit_copy().getSafeUri().asString(), app.getMenu("Copy")),
+			        true, new Command() {
+				        public void execute() {
+					        app.setWaitCursor();
+					        CopyPaste.copyToXML(app, app.getSelectedGeos());
+					        app.updateMenubar();
+					        app.setDefaultCursor();
+				        }
+			        });
+//		else
+//			addItem(GeoGebraMenubar.getMenuBarHtmlGrayout(AppResources.INSTANCE
+//			        .edit_copy().getSafeUri().asString(), app.getMenu("Copy")),
+//			        true, new Command() {
+//				        public void execute() {
+//					        // do nothing
+//				        }
+//			        });
+
+		// paste menu
+//		if (!CopyPaste.isEmpty())
+			addItem(GeoGebraMenubar
+			        .getMenuBarHtml(AppResources.INSTANCE.edit_paste()
+			                .getSafeUri().asString(), app.getMenu("Paste")),
+			        true, new Command() {
+				        public void execute() {
+					        app.setWaitCursor();
+					        CopyPaste.pasteFromXML(app);
+					        app.setDefaultCursor();
+				        }
+			        });
+//		else
+//			addItem(GeoGebraMenubar
+//			        .getMenuBarHtmlGrayout(AppResources.INSTANCE.edit_paste()
+//			                .getSafeUri().asString(), app.getMenu("Paste")),
+//			        true, new Command() {
+//				        public void execute() {
+//					        // do nothing
+//				        }
+//			        });
+
+		// copy graphics view menu
+		addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE
+		        .edit_copy().getSafeUri().asString(), app.getMenu("CopyImage")),
+		        true, new Command() {
+			        public void execute() {
+				        ((Application) app).copyEVtoClipboard();
+			        }
+		        });
+	
+		// separator
+		addSeparator();
+
+		// select all menu
+//		if (app.getKernel().isEmpty())
+			addItem(GeoGebraMenubar.getMenuBarHtml(null,
+			        app.getMenu("SelectAll")), true, new Command() {
 				public void execute() {
-					app.getGuiManager().undo();
+					app.selectAll(-1);
 				}
 			});
 //		else
-//			addItem(GeoGebraMenubar.getMenuBarHtmlGrayout(AppResources.INSTANCE.document_save().getSafeUri().asString(), app.getMenu("SaveAs")),true,new Command() {
-//				public void execute() {	}
+//			addItem(GeoGebraMenubar.getMenuBarHtmlGrayout(null,
+//			        app.getMenu("SelectAll")), true, new Command() {
+//				public void execute() {
+//					// do nothing
+//				}
 //			});
-//		
-//		if (enabled_redo)
-			addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE.edit_redo().getSafeUri().asString(), app.getMenu("Redo")),true,new Command() {
-			
-				public void execute() {
-					app.getGuiManager().redo();
-				}
-			});
-//		else
-//			addItem(GeoGebraMenubar.getMenuBarHtmlGrayout(AppResources.INSTANCE.document_save().getSafeUri().asString(), app.getMenu("SaveAs")),true,new Command() {
-//				public void execute() {	}
-//			});
-		
-	    addItem(GeoGebraMenubar.getMenuBarHtml(AppResources.INSTANCE.edit_copy().getSafeUri().asString(),app.getMenu("CopyImage")),true,new Command() {
-	    	public void execute() {
-	    		((Application)app).copyEVtoClipboard();
-	    	}
-	    });
 	}
 }
