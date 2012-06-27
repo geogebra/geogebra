b41a4aedb3c44dcbc118b45dc0f9eb5b22e30d15
Judit Elias <eliasjudit@gmail.com>
2012-06-27 13:15:46 +0000

    web: selection doesn't get stuck in last item at radiobutton menubar.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@18287 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/gui/menubar/RadioButtonMenuBar.java
+++ web/src/geogebra/web/gui/menubar/RadioButtonMenuBar.java
@@ -1,10 +1,6 @@
 package geogebra.web.gui.menubar;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import geogebra.web.gui.images.AppResources;
-import geogebra.web.main.Application;
 
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.MenuBar;
@@ -15,6 +11,7 @@ public class RadioButtonMenuBar extends MenuBar {
 
 	final private String menubarID;
 	private ArrayList<RadioButton> radioButtons;
+	private RadioButtonCommand lastCommand;
 
 	public RadioButtonMenuBar() {
 		super(true);
@@ -25,16 +22,14 @@ public class RadioButtonMenuBar extends MenuBar {
 	public MenuItem addItem(String text, RadioButtonCommand com) {
 		RadioButton radioButton = new RadioButton(menubarID, text, true);		
 		if (radioButtons.size()==0) radioButton.setValue(true);		
-		radioButtons.add(radioButton);	
+		radioButtons.add(radioButton);
+		lastCommand = com; //temp
 		return super.addItem(radioButton.toString(), true, com);
 	}
 
 	public void setSelected(int itemIndex) {		
-		boolean val;
-		for(int i=0; i<radioButtons.size(); i++){
-			val = (itemIndex == i) ? true : false;
-			radioButtons.get(itemIndex).setValue(val);	
-			this.getItems().get(i).setHTML(radioButtons.get(i).toString());
-		}		    
+		boolean val;		
+		radioButtons.get(itemIndex).setValue(true);
+		this.getItems().get(itemIndex).setHTML(radioButtons.get(itemIndex).toString());
 	}
 }
