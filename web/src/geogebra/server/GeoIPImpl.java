f8c7905ebc6304720b320e1677dcf0afe08dc379
Rana Issa <rana@geogebra.org>
2012-06-28 06:43:52 +0000

    Server-side code enhancement
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@18307 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/server/GeoIPImpl.java
+++ web/src/geogebra/server/GeoIPImpl.java
@@ -15,19 +15,9 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 public class GeoIPImpl extends RemoteServiceServlet implements GeoIPService {
-	
-	HttpServletRequest aReq;
-	HttpServletResponse aRes;
-	
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		aReq = req;
-		aRes = resp;
-		resp.setContentType("text/plain");
-	}
-	
+
 	public String getCountry() {
-		
+
 		String country = "";
 		try {
 			URL url = new URL("http://www.geogebra.org/geoip/");
@@ -42,13 +32,14 @@ public class GeoIPImpl extends RemoteServiceServlet implements GeoIPService {
 		}
 		return country;
 	}
-	
+
 	public String getLanguage() {
-//		String acceptCountry = req.getHeader("X-AppEngine-Country");
-//		String acceptCharset = req.getHeader("Accept-Charset");
-		String str = aReq.getHeader("Accept-Language");
+		//		String acceptCountry = req.getHeader("X-AppEngine-Country");
+		//		String acceptCharset = req.getHeader("Accept-Charset");
+		String str = getThreadLocalRequest().getHeader("Accept-Language");
+
 		return str.substring(0, str.indexOf(","));
-		
+
 	}
 
 }
