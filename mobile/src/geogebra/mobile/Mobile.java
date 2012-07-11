ab021d5fcd56b2eefa43adb8ffbc3a714a191593
Ancsin Gábor <ustakraharez@gmail.com>
2012-07-11 11:56:44 +0000

    Initial commit for mobile project
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@18612 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ mobile/src/geogebra/mobile/Mobile.java
@@ -0,0 +1,37 @@
+package geogebra.mobile;
+
+import geogebra.common.main.App;
+import geogebra.mobile.gui.GeoGebraMobileFrame;
+import geogebra.web.Web;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Mobile implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		/* the Common.java and the Web.java onlmoduleLoad() called 
+		 * before of this. So, any script injecting, etc is done there already
+		 * concerning the <body data-param-mobile="true"> parameter>
+		 */
+		loadMobileAsync();
+	}
+	
+	private void loadMobileAsync() {
+		   GWT.runAsync(new RunAsyncCallback() {
+			
+			public void onSuccess() {
+				GeoGebraMobileFrame app = new GeoGebraMobileFrame();
+				
+			}
+			
+			public void onFailure(Throwable reason) {
+				App.debug(reason);
+			}
+		   });
+	    }
+}
---
+++ mobile/src/geogebra/mobile/gui/GeoGebraMobileFrame.java
@@ -0,0 +1,14 @@
+package geogebra.mobile.gui;
+
+import geogebra.common.main.App;
+
+import com.google.gwt.user.client.Window;
+
+public class GeoGebraMobileFrame {
+	
+	public GeoGebraMobileFrame() {
+		Window.alert("Lord Matthias, start here");
+		App.debug("Lord Matthias, start here");
+	}
+
+}
