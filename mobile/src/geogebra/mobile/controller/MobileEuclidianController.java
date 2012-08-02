b6f6ab1c34c591bed29f33e7cb05ecb0fcd8754a
Matthias Meisinger <matthias.meisinger@geogebra.org>
2012-08-02 12:19:55 +0000

    introducing MobileEuclidianController
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@19229 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ mobile/src/geogebra/mobile/controller/MobileEuclidianController.java
@@ -0,0 +1,39 @@
+package geogebra.mobile.controller;
+
+import geogebra.common.euclidian.EuclidianController;
+import geogebra.common.kernel.geos.GeoElement;
+import geogebra.common.kernel.kernelND.GeoPointND;
+import geogebra.common.main.App;
+
+public class MobileEuclidianController extends EuclidianController
+{
+
+	@Override
+	public void setApplication(App app)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initToolTipManager()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected GeoElement[] createCircle2ForPoints3D(GeoPointND p0, GeoPointND p1)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void resetToolTipManager()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
