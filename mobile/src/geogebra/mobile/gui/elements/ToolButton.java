58e7a7b700353ef29278c7340bf47c2d4b5ab614
Thomas Krismayer <thomas.krismayer@geogebra.org>
2012-08-02 07:24:26 +0000

    button
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@19214 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- mobile/src/geogebra/mobile/gui/elements/ToolButton.java
+++ mobile/src/geogebra/mobile/gui/elements/ToolButton.java
@@ -2,10 +2,18 @@ package geogebra.mobile.gui.elements;
 
 import geogebra.mobile.utils.ToolBarCommand;
 
+import com.googlecode.mgwt.dom.client.event.touch.TouchCancelEvent;
+import com.googlecode.mgwt.dom.client.event.touch.TouchEndEvent;
+import com.googlecode.mgwt.dom.client.event.touch.TouchHandler;
+import com.googlecode.mgwt.dom.client.event.touch.TouchMoveEvent;
+import com.googlecode.mgwt.dom.client.event.touch.TouchStartEvent;
+
 import com.google.gwt.user.client.Window;
 import com.googlecode.mgwt.dom.client.event.tap.TapEvent;
 import com.googlecode.mgwt.dom.client.event.tap.TapHandler;
+
 import com.googlecode.mgwt.ui.client.widget.buttonbar.ButtonBarButtonBase;
+
 
 /**
  * A Button for the ToolBar, allowing an SVG graphic to be set as background <br>
@@ -21,9 +29,10 @@ public class ToolButton extends ButtonBarButtonBase
 
 	public ToolButton(final ToolBarCommand cmd)
 	{		
+
 		super(null);
 		this.addStyleName("toolbutton");
-
+		
 		super.getElement().getStyle().setBackgroundImage(cmd.getIconUrlAsString());
 
 		this.addTapHandler(new TapHandler()
@@ -34,5 +43,34 @@ public class ToolButton extends ButtonBarButtonBase
 				Window.alert("Mode: " + cmd.getMode());
 			}
 		});
+		
+		this.addTouchHandler(new TouchHandler() {
+			@Override
+			public void onTouchStart(TouchStartEvent event) {
+				click();
+			}
+
+			@Override
+			public void onTouchMove(TouchMoveEvent event) {
+			}
+
+			@Override
+			public void onTouchEnd(TouchEndEvent event) {
+				clickEnd();
+			}
+
+			@Override
+			public void onTouchCanceled(TouchCancelEvent event) {
+			}
+		});
+
 	}
+	private void click() {
+		this.addStyleName("button-active");
+	}
+
+	private void clickEnd() {
+		this.removeStyleName("button-active");
+	}
+
 }
