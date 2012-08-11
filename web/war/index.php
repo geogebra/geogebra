f690f5230ff13dbe005dd528c306ae3556ac490c
Zoltán Kovács <zoltan@geogebra.org>
2012-08-11 07:33:32 +0000

    Fixing direct calls like http://www.geogebra.org/web/test42/?f=_dashed.ggb
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@19434 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/war/index.php
+++ web/war/index.php
@@ -1,22 +1,33 @@
 <?php
-  
-  $languaes = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-  if(isset($languaes)) {
-    //find the first comma, get a substring from location 0 up to the location of the comma.
-    $comma_pos = strpos($languaes, ',');
-	$locale = substr($languaes, 0, $comma_pos);
+
+  if ($_SERVER['f']!="") {
+    // This is a web call ("web applet mode") and test.php must be used:
+    $dir=rtrim($_SERVER['SCRIPT_URL'],'index.php');
+    $dir=rtrim($dir,"/");
+    header("Location: $dir/test.php?".$_SERVER['QUERY_STRING']);
+    die();
+  }
 	
-	//The accepted character among the locale variable are a-z or A-Z or _ or - only
-	if(preg_match('/^[a-z_\-]+$/i', $locale)) {
+  // This is a web_gui call.
+  $languages = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+  if (isset($languages)) {
+    // Find the first comma, get a substring from location 0 up to the location of the comma.
+    $comma_pos = strpos($languages, ',');
+    $locale = substr($languages, 0, $comma_pos);
+	
+    // The accepted character among the locale variable are a-z or A-Z or _ or - only.
+    if (preg_match('/^[a-z_\-]+$/i', $locale)) {
 	  $gwtLocale = str_replace('-', '_', $locale);
-	} else {
-	  $gwtLocale = 'en';
-	} 
-	} else {
-	  $gwtLocale = 'en';
-	}
-	
-	//redirect the application to GeoGebra and pass the language parameter
-	//If language is not recognised then en is the default
-    header('Location: http://www.geogebra.org/web/web_gui/?locale=' . $gwtLocale);
-  ?>
+    } else {
+	$gwtLocale = 'en';
+    } 
+  } else {
+    $gwtLocale = 'en';
+  }
+
+  // Redirect the application to GeoGebra and pass the language parameter.
+  // If language is not recognised then en is the default.
+  header('Location: http://www.geogebra.org/web/web_gui/?locale=' . $gwtLocale);
+  // Instead of this, we should build up a full HTML page
+  // as it is present on this URL. FIXME! 
+?>
