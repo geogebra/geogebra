64911d4689b0a493ef56627549f1266720dc5151
Michael Borcherds <michael@geogebra.org>
2012-08-22 13:53:58 +0000

    fix for CmdZProportion2Estimate (copy & paste error)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@19737 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/commands/CmdZProportion2Estimate.java
+++ common/src/geogebra/common/kernel/commands/CmdZProportion2Estimate.java
@@ -3,13 +3,12 @@ package geogebra.common.kernel.commands;
 import geogebra.common.kernel.Kernel;
 import geogebra.common.kernel.arithmetic.Command;
 import geogebra.common.kernel.geos.GeoElement;
-import geogebra.common.kernel.geos.GeoList;
 import geogebra.common.kernel.geos.GeoNumeric;
-import geogebra.common.kernel.statistics.AlgoZMean2Estimate;
+import geogebra.common.kernel.statistics.AlgoZProportion2Estimate;
 import geogebra.common.main.MyError;
 
 /**
- * ZProportionTest
+ * ZProportion2Estimate[ <Sample Proportion 1 >, <Sample Size 1>, <Sample Proportion 2 >, <Sample Size 2>, <Level> ]
  */
 public class CmdZProportion2Estimate extends CommandProcessor {
 	/**
@@ -32,46 +31,19 @@ public class CmdZProportion2Estimate extends CommandProcessor {
 		switch (n) {
 
 		case 5:
-			if ((ok[0] = arg[0].isGeoList()) 
-					&& (ok[1] = arg[1].isGeoList())
-					&& (ok[2] = arg[2].isGeoNumeric())
-					&& (ok[3] = arg[3].isGeoNumeric())
-					&& (ok[4] = arg[4].isGeoNumeric())
-			) {
-				
-				AlgoZMean2Estimate algo = new AlgoZMean2Estimate(cons, c.getLabel(),
-						(GeoList) arg[0], 
-						(GeoList) arg[1],
-						(GeoNumeric) arg[2],
-						(GeoNumeric) arg[3],
-						(GeoNumeric) arg[4]
-								);
-
-				GeoElement[] ret = { algo.getResult() };
-				return ret;
-
-			} 
-			
-			throw argErr(app, c.getName(), getBadArg(ok, arg));
-			
-		case 7:
 			if ((ok[0] = arg[0].isGeoNumeric()) 
 					&& (ok[1] = arg[1].isGeoNumeric())
 					&& (ok[2] = arg[2].isGeoNumeric())
 					&& (ok[3] = arg[3].isGeoNumeric())
 					&& (ok[4] = arg[4].isGeoNumeric())
-					&& (ok[5] = arg[5].isGeoNumeric())
-					&& (ok[6] = arg[6].isGeoNumeric())
 			) {
 				
-				AlgoZMean2Estimate algo = new AlgoZMean2Estimate(cons, c.getLabel(),
+				AlgoZProportion2Estimate algo = new AlgoZProportion2Estimate(cons, c.getLabel(),
 						(GeoNumeric) arg[0], 
 						(GeoNumeric) arg[1],
 						(GeoNumeric) arg[2],
 						(GeoNumeric) arg[3],
-						(GeoNumeric) arg[4],
-						(GeoNumeric) arg[5],
-						(GeoNumeric) arg[6]
+						(GeoNumeric) arg[4]
 								);
 
 				GeoElement[] ret = { algo.getResult() };
