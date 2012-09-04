3fbcef29e4701a6812dd28ff90ee55e53596750e
Thomas Krismayer <thomas.krismayer@geogebra.org>
2012-09-04 10:07:28 +0000

    title
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@20100 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- mobile/src/geogebra/mobile/gui/elements/TabletHeaderPanel.java
+++ mobile/src/geogebra/mobile/gui/elements/TabletHeaderPanel.java
@@ -1,6 +1,16 @@
 package geogebra.mobile.gui.elements;
 
+import geogebra.mobile.gui.elements.toolbar.ToolBar;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.TextBox;
+import com.googlecode.mgwt.dom.client.event.tap.TapEvent;
+import com.googlecode.mgwt.dom.client.event.tap.TapHandler;
+import com.googlecode.mgwt.ui.client.dialog.PopinDialog;
+import com.googlecode.mgwt.ui.client.widget.Button;
 import com.googlecode.mgwt.ui.client.widget.HeaderPanel;
+import com.googlecode.mgwt.ui.client.widget.RoundPanel;
 
 /**
  * Extends from {@link HeaderPanel}.
@@ -13,6 +23,39 @@ public class TabletHeaderPanel extends HeaderPanel
 	 */
 	public TabletHeaderPanel()
 	{
-		this.setCenter("GeoGebra");
+		this.setCenter("Title");
+		
+		this.addDomHandler(new ClickHandler()
+		{
+			@Override
+			public void onClick(ClickEvent event)
+			{
+				event.preventDefault();
+				final PopinDialog dialog = new PopinDialog();
+				dialog.setHideOnBackgroundClick(true);
+				dialog.setCenterContent(true);
+
+				RoundPanel roundPanel = new RoundPanel();
+
+				final TextBox inputBar = new TextBox();
+				roundPanel.add(inputBar);
+
+				Button button = new Button("ok");
+				button.addDomHandler(new ClickHandler()
+				{
+					@Override
+					public void onClick(ClickEvent e)
+					{
+						TabletHeaderPanel.this.setCenter(inputBar.getText()); 
+						dialog.hide();
+					}
+				}, ClickEvent.getType());
+				button.addStyleName("popinButton");
+				roundPanel.add(button);
+
+				dialog.add(roundPanel);
+				dialog.show();
+			}
+		}, ClickEvent.getType());
 	}
 }
