67b9db23c1524491ee4ad7c0d1bb2806096eecd7
Philipp Birklbauer <philipp@geogebra.org>
2012-09-04 12:37:45 +0000

    Getting rid of the code duplication in GeoGebra/GeoGebra3D main.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@20102 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra/GeoGebra.java
+++ desktop/geogebra/GeoGebra.java
@@ -8,7 +8,7 @@ This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by 
 the Free Software Foundation.
 
-*/
+ */
 
 package geogebra;
 
@@ -17,56 +17,64 @@ import geogebra.common.GeoGebraConstants;
 import java.awt.Frame;
 import java.awt.Toolkit;
 import java.net.URL;
- 
-public class GeoGebra extends Object implements GeoGebraConstants {
 
-	/* WARNING: This code is the same as in GeoGebra3D.java.
-	 * TODO: Make the same code common.
-	 */
-	
+public class GeoGebra {
+
 	public static Frame splashFrame = null;
-	
-    public static void main(String[] cmdArgs) {  
+
+	protected GeoGebra() {
+	}
+
+	public static void main(String[] cmdArgs) {
+		(new GeoGebra()).doMain(cmdArgs);
+	}
+
+	protected void doMain(String[] cmdArgs) {
 		CommandLineArguments args = new CommandLineArguments(cmdArgs);
-				
-    	boolean showSplash = true;
-    	if (!args.getBooleanValue("showSplash", true)) {
-    		showSplash = false;
-    	}
-    	
-    	if (args.containsArg("help") || args.containsArg("proverhelp")
-    			|| args.containsArg("v") || args.containsArg("regressionFile")) {
-    		showSplash = false;
-    	}
-    	
-    	if (showSplash) {
-    	  // Show splash screen
-		  URL imageURL = GeoGebra.class.getResource("/geogebra/"+SPLASH_STRING);
-		  if (imageURL != null) {
-		      splashFrame = SplashWindow.splash(
-		          Toolkit.getDefaultToolkit().createImage(imageURL)
-		      );
-		  } else {
-		      System.err.println("Splash image not found");
-		  }
-    	}
-		  
-		  // Start GeoGebra
-		  try {        			  		
-			  startGeoGebra(args);               	
-		  } catch (Throwable e) {
-	      e.printStackTrace();
-	      System.err.flush();
-	      System.exit(10);
-		  }
-		  
-		  // Hide splash screen
-		  if (splashFrame != null) splashFrame.setVisible(false);
-    }
-    
-    private static void startGeoGebra(CommandLineArguments args) {
-    	// create and open first GeoGebra window        	
-    	geogebra.gui.app.GeoGebraFrame.main(args);
-    }
-    
+
+		boolean showSplash = true;
+		if (!args.getBooleanValue("showSplash", true)) {
+			showSplash = false;
+		}
+
+		if (args.containsArg("help") || args.containsArg("proverhelp")
+				|| args.containsArg("v") || args.containsArg("regressionFile")) {
+			showSplash = false;
+		}
+
+		if (showSplash) {
+			// Show splash screen
+			URL imageURL = GeoGebra.class.getResource(getSplashString());
+			if (imageURL != null) {
+				splashFrame = SplashWindow.splash(Toolkit.getDefaultToolkit()
+						.createImage(imageURL));
+			} else {
+				System.err.println("Splash image not found");
+			}
+		}
+
+		// Start GeoGebra
+		try {
+			startGeoGebra(args);
+		} catch (Throwable e) {
+			e.printStackTrace();
+			System.err.flush();
+			System.exit(10);
+		}
+
+		// Hide splash screen
+		if (splashFrame != null) {
+			splashFrame.setVisible(false);
+		}
+	}
+
+	protected void startGeoGebra(CommandLineArguments args) {
+		// create and open first GeoGebra window
+		geogebra.gui.app.GeoGebraFrame.main(args);
+	}
+
+	protected String getSplashString() {
+		return "/geogebra/" + GeoGebraConstants.SPLASH_STRING;
+	}
+
 }
--- desktop/geogebra/GeoGebra3D.java
+++ desktop/geogebra/GeoGebra3D.java
@@ -19,71 +19,30 @@ package geogebra;
 
 import geogebra3D.euclidian3D.opengl.RendererJogl;
 
-import java.awt.Frame;
-import java.awt.Toolkit;
-import java.net.URL;
+public class GeoGebra3D extends GeoGebra {
 
-
-public class GeoGebra3D extends GeoGebra
-{
-	
 	// File format versions
 	public static final String XML_FILE_FORMAT = "5.0";
-	
-	
+
 	static {
 		RendererJogl.initSingleton();
 	}
 
-	/* WARNING: This code is the same as in GeoGebra.java.
-	 * TODO: Make the same code common.
-	 */
-	
-	public static Frame splashFrame = null;
-	
-	public static void main(String[] cmdArgs) {  
-		CommandLineArguments args = new CommandLineArguments(cmdArgs);
-				
-    	boolean showSplash = true;
-    	if (!args.getBooleanValue("showSplash", true)) {
-    		showSplash = false;
-    	}
-    	
-    	if (args.containsArg("help") || args.containsArg("proverhelp")
-    			|| args.containsArg("v") || args.containsArg("regressionFile")) {
-    		showSplash = false;
-    	}
-    	
-    	if (showSplash) {
-    	  // Show splash screen
-		  URL imageURL = GeoGebra.class.getResource("/geogebra3D/splash5beta.png");
-		  if (imageURL != null) {
-		      splashFrame = SplashWindow.splash(
-		          Toolkit.getDefaultToolkit().createImage(imageURL)
-		      );
-		  } else {
-		      System.err.println("Splash image not found");
-		  }
-    	}
-		  
-		  // Start GeoGebra
-		  try {        			  		
-			  startGeoGebra(args);                	
-		  } catch (Throwable e) {
-		      e.printStackTrace();
-		      System.err.flush();
-		      System.exit(10);
-		  }
-		  
-		  // Hide splash screen
-		  if (splashFrame != null) splashFrame.setVisible(false);
-    }
+	public static void main(String[] cmdArgs) {
+		(new GeoGebra3D()).doMain(cmdArgs);
+	}
 
-    private static void startGeoGebra(CommandLineArguments args) {
-    	// create and open first GeoGebra window        	
-    	geogebra.gui.app.GeoGebraFrame3D.main(args);
-    }
+	protected GeoGebra3D() {
+	}
 
-	
+	protected void startGeoGebra(CommandLineArguments args) {
+		// create and open first GeoGebra window
+		geogebra.gui.app.GeoGebraFrame3D.main(args);
+	}
+
+	@Override
+	protected String getSplashString() {
+		return "/geogebra3D/splash5beta.png";
+	}
 
 }
