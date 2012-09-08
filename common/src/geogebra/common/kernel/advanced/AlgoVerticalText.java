7b7a88e488ea79055770c3a4c521dc55b328623f
George Sturr <gsturr@santarosa.edu>
2012-09-08 16:22:35 +0000

    fixed point parameter for VerticalText[]
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@20225 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/advanced/AlgoVerticalText.java
+++ common/src/geogebra/common/kernel/advanced/AlgoVerticalText.java
@@ -12,10 +12,12 @@ the Free Software Foundation.
 
 package geogebra.common.kernel.advanced;
 
+import geogebra.common.kernel.CircularDefinitionException;
 import geogebra.common.kernel.Construction;
 import geogebra.common.kernel.algos.AlgoElement;
 import geogebra.common.kernel.algos.Algos;
 import geogebra.common.kernel.geos.GeoElement;
+import geogebra.common.kernel.geos.GeoPoint;
 import geogebra.common.kernel.geos.GeoText;
 
 
@@ -27,8 +29,10 @@ public class AlgoVerticalText extends AlgoElement {
 
 	private GeoText text; //output	
     private GeoText args; //input	
+    private GeoPoint startPoint, startPointCopy; // optional input
     
     private StringBuilder sb = new StringBuilder();
+    
     
     /**
      * Creates new algo for vertical text
@@ -37,21 +41,58 @@ public class AlgoVerticalText extends AlgoElement {
      * @param args input text
      */
     public AlgoVerticalText(Construction cons, String label, GeoText args) {
-    	this(cons,  args);
+    	this(cons,  args, null);
+        text.setLabel(label);
+    }
+    
+    /**
+     * Creates new unlabeled algo for vertical text
+     * @param cons construction
+     * @param label label for output
+     * @param args input text
+     */
+    public AlgoVerticalText(Construction cons, GeoText args) {
+    	this(cons,  args, null);
+    }
+    
+    
+    
+    /**
+     * Creates new algo for vertical text, with fixed position point
+     * @param cons construction
+     * @param label label for output
+     * @param args input text
+     */
+    public AlgoVerticalText(Construction cons, String label, GeoText args,  GeoPoint p) {
+    	this(cons,  args, p);
         text.setLabel(label);
     }
 
     /**
-     * Creates new unlabeled algo for vertical text
+     * Creates new unlabeled algo for vertical text, with fixed position point
      * @param cons construction
      * @param args input text
      */
-    AlgoVerticalText(Construction cons, GeoText args) {
+    public AlgoVerticalText(Construction cons, GeoText args,  GeoPoint p) {
         super(cons);
+        
         this.args = args;
+        this.startPoint = p;
                
         text = new GeoText(cons);
 		text.setIsTextCommand(true); // stop editing as text
+		
+		// set startpoint
+		if (startPoint != null) {
+			startPointCopy = (GeoPoint) startPoint.copyInternal(cons);
+
+			try {
+				text.setStartPoint(startPointCopy);
+			} catch (CircularDefinitionException e) {
+				e.printStackTrace();
+			}
+			text.setAlwaysFixed(true); // disable dragging if p != null
+		}
 		
         setInputOutput();
         compute();    
@@ -62,15 +103,25 @@ public class AlgoVerticalText extends AlgoElement {
         return Algos.AlgoVerticalText;
     }
 
-    @Override
-	protected void setInputOutput(){
-	    input = new GeoElement[1];
-	    input[0] = args;
+	@Override
+	protected void setInputOutput() {
+		input = new GeoElement[1];
+		input[0] = args;
 
-        super.setOutputLength(1);
-        super.setOutput(0, text);
-        setDependencies(); // done by AlgoElement
-    }
+		if (startPoint == null) {
+			input = new GeoElement[1];
+			input[0] = args;
+
+		} else {
+			input = new GeoElement[2];
+			input[0] = args;
+			input[1] = startPoint;
+		}
+
+		super.setOutputLength(1);
+		super.setOutput(0, text);
+		setDependencies(); // done by AlgoElement
+	}
 
     /**
      * @return resulting text
@@ -81,16 +132,23 @@ public class AlgoVerticalText extends AlgoElement {
 
     @Override
 	public final void compute() {
-    	if (!args.isDefined()) {
-    		text.setTextString("");
-    		return;
-    	}
+		if (!args.isDefined()
+				|| (startPoint != null && !startPoint.isDefined())) {
+			text.setTextString("");
+			return;
+		}
     	
     	sb.setLength(0);
     	AlgoRotateText.appendRotatedText(sb, args, 90);
     	
     	text.setTextString(sb.toString());
     	text.setLaTeX(true,false);
+
+		// update startpoint position of text
+		if (startPointCopy != null) {
+			startPointCopy.setCoords(startPoint);
+		}
+    			
     }
 
 	@Override
--- common/src/geogebra/common/kernel/advanced/CmdVerticalText.java
+++ common/src/geogebra/common/kernel/advanced/CmdVerticalText.java
@@ -4,6 +4,7 @@ import geogebra.common.kernel.Kernel;
 import geogebra.common.kernel.arithmetic.Command;
 import geogebra.common.kernel.commands.CommandProcessor;
 import geogebra.common.kernel.geos.GeoElement;
+import geogebra.common.kernel.geos.GeoPoint;
 import geogebra.common.kernel.geos.GeoText;
 import geogebra.common.main.MyError;
 
@@ -24,7 +25,7 @@ public class CmdVerticalText extends CommandProcessor {
 	@Override
 	public GeoElement[] process(Command c) throws MyError {
 		int n = c.getArgumentNumber();
-		
+		boolean[] ok = new boolean[n];
 		GeoElement[] arg;
 		arg = resArgs(c);
 
@@ -39,6 +40,18 @@ public class CmdVerticalText extends CommandProcessor {
 				return ret;
 			}
 			throw argErr(app, c.getName(), arg[0]);
+			
+		case 2:
+			
+			if ((ok[0] = (arg[0].isGeoText()))
+					&& (ok[1] = arg[1].isGeoPoint())) {
+				AlgoVerticalText algo = new AlgoVerticalText(cons, c.getLabel(),
+						(GeoText) arg[0],  (GeoPoint) arg[1]);
+
+				GeoElement[] ret = { algo.getResult() };
+				return ret;
+			}
+			throw argErr(app, c.getName(), getBadArg(ok, arg));     
 
 		default:
 			throw argNumErr(app, c.getName(), n);
