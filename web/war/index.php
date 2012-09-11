9b9fe4fdc20bc470edb1eb5c4480c0c546cb9975
Zoltán Kovács <zoltan@geogebra.org>
2012-09-11 13:19:32 +0000

    Fixing unconditional redirect to the web_gui directory on the www server
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@20294 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/war/index.php
+++ web/war/index.php
@@ -7,27 +7,34 @@
     header("Location: $dir/test.php?".$_SERVER['QUERY_STRING']);
     die();
   }
-	
+
   // This is a web_gui call.
+  $forced_locale=$_GET['locale'];
+  if ($forced_locale!="") {
+    // The forced language setting is used:
+    header('Location: app.html?locale=' . $forced_locale);
+    die();
+  }
+
+  // No language is forced, then we use the browser's settings:
   $languages = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
   if (isset($languages)) {
     // Find the first comma, get a substring from location 0 up to the location of the comma.
     $comma_pos = strpos($languages, ',');
     $locale = substr($languages, 0, $comma_pos);
-	
+
     // The accepted character among the locale variable are a-z or A-Z or _ or - only.
     if (preg_match('/^[a-z_\-]+$/i', $locale)) {
-	  $gwtLocale = str_replace('-', '_', $locale);
+      $gwtLocale = str_replace('-', '_', $locale);
     } else {
-	$gwtLocale = 'en';
-    } 
+      $gwtLocale = 'en';
+    }
   } else {
     $gwtLocale = 'en';
   }
 
   // Redirect the application to GeoGebra and pass the language parameter.
-  // If language is not recognised then en is the default.
-  header('Location: http://www.geogebra.org/web/web_gui/?locale=' . $gwtLocale);
-  // Instead of this, we should build up a full HTML page
-  // as it is present on this URL. FIXME! 
+  // If language is not recognized then en is the default.
+  header('Location: app.html?locale=' . $gwtLocale);
+
 ?>
