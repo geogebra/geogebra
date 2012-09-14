962d646266d5962a925ddcb6d55c38e116a29cbe
Michael Borcherds <michael@geogebra.org>
2012-09-14 19:01:07 +0000

    wrong latex brackets in CAS for A:=(1/2,1/2)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@20381 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/arithmetic/MyVecNode.java
+++ common/src/geogebra/common/kernel/arithmetic/MyVecNode.java
@@ -20,6 +20,7 @@ package geogebra.common.kernel.arithmetic;
 
 import geogebra.common.kernel.Kernel;
 import geogebra.common.kernel.StringTemplate;
+import geogebra.common.kernel.arithmetic.ExpressionNodeConstants.StringType;
 import geogebra.common.kernel.geos.GeoElement;
 import geogebra.common.kernel.geos.GeoVec2D;
 import geogebra.common.main.MyParseError;
@@ -140,7 +141,7 @@ public class MyVecNode extends ValidExpression implements VectorValue {
 	public String toString(StringTemplate tpl) {
 		StringBuilder sb = new StringBuilder();
 		double[] coords;
-
+		
 		switch (tpl.getStringType()) {
 		case MATH_PIPER:
 			coords = getCoords();
@@ -180,9 +181,9 @@ public class MyVecNode extends ValidExpression implements VectorValue {
 				sb.append(')');
 			}
 			break;
-
+			
 		default: // continue below
-			sb.append('(');
+			sb.append(tpl.getStringType().equals(StringType.LATEX) ? " \\left( " : "(");
 			sb.append(x.isGeoElement() ? ((GeoElement) x).getLabel(tpl) : x
 					.toString(tpl));
 			if (mode == Kernel.COORD_CARTESIAN)
@@ -191,7 +192,7 @@ public class MyVecNode extends ValidExpression implements VectorValue {
 				sb.append("; ");
 			sb.append(y.isGeoElement() ? ((GeoElement) y).getLabel(tpl) : y
 					.toString(tpl));
-			sb.append(')');
+			sb.append(tpl.getStringType().equals(StringType.LATEX) ? " \\right) " : ")");
 			break;
 		}
 
