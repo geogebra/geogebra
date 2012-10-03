f004bb27113c6f06b33177eda9a8afd2d001201f
Arnoud Delobelle <arnodel@gmail.com>
2012-10-03 12:21:03 +0000

    Documenting EventListener
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@20858 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/plugin/EventListener.java
+++ common/src/geogebra/common/plugin/EventListener.java
@@ -1,7 +1,27 @@
 package geogebra.common.plugin;
 
+/**
+ * @see EventDispatcher
+ * @author arno
+ * 
+ */
 public interface EventListener {
-	
+
+	/**
+	 * This method is called by the event dispatcher every time an event is
+	 * triggered
+	 * 
+	 * @param evt
+	 *            the event
+	 */
 	public void sendEvent(Event evt);
-	
+
+	/**
+	 * This method is called every time the construction is cleared. This is a
+	 * cue to the event listener to get rid of the scripts attached to
+	 * particular objects.
+	 * 
+	 * TODO check how to make this work
+	 */
+	// public void clearObjects();
 }
