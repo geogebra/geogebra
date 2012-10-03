7622e1ccb22d19c8a99ee116cecf63eab6bebaf7
Arnoud Delobelle <arnodel@gmail.com>
2012-10-03 09:19:31 +0000

    Some documenting of EventDispatcher
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@20855 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/plugin/EventDispatcher.java
+++ common/src/geogebra/common/plugin/EventDispatcher.java
@@ -7,40 +7,68 @@ import geogebra.common.main.App;
 import java.util.ArrayList;
 
 /**
+ * Every event scripting machinery must implement the EventListener interface
+ * and register with the application's event dispatcher (via
+ * app.getEventDispatcher().addEventListener(...)). It will then obtain events
+ * via the sendEvent() method.
+ * 
  * @author arno
- *
+ * 
  */
 public class EventDispatcher implements View {
-	
+
 	private App app;
 	private ArrayList<EventListener> listeners = new ArrayList<EventListener>();
-	
+
+	/**
+	 * @param app
+	 */
 	public EventDispatcher(App app) {
 		this.app = app;
 	}
-	
+
+	/**
+	 * Add a new EventListener object
+	 * @param listener the object that wants to receive notifications
+	 * of events
+	 */
 	public void addEventListener(EventListener listener) {
 		listeners.add(listener);
 	}
-	
+
+	/**
+	 * Dispatch an event to all registered event listeners
+	 * @param evt the event to be dispatched
+	 */
 	public void dispatchEvent(Event evt) {
 		for (EventListener listener : listeners) {
 			listener.sendEvent(evt);
 		}
 	}
-	
+
+	/**
+	 * Convenience method for dispatching an event
+	 * @param evtType the type of the event
+	 * @param geo the target of the event
+	 * @param arg an extra argument
+	 */
 	public void dispatchEvent(EventType evtType, GeoElement geo, String arg) {
 		dispatchEvent(new Event(evtType, geo, arg));
 	}
-	
+
+	/**
+	 * Convenience method for dispatching an event
+	 * @param evtType the type of the event
+	 * @param geo the target of the event
+	 */
 	public void dispatchEvent(EventType evtType, GeoElement geo) {
 		dispatchEvent(new Event(evtType, geo));
 	}
-	
+
 	/*
-	 * Implementation of View	
+	 * Implementation of View
 	 */
-	
+
 	public void add(GeoElement geo) {
 		dispatchEvent(EventType.ADD, geo);
 	}
@@ -58,38 +86,35 @@ public class EventDispatcher implements View {
 	}
 
 	public void updateVisualStyle(GeoElement geo) {
-		// TODO Auto-generated method stub
-		
+		// Ignore
 	}
 
 	public void updateAuxiliaryObject(GeoElement geo) {
-		// TODO Auto-generated method stub
-		
+		// Ignore
 	}
 
 	public void repaintView() {
-		// TODO Auto-generated method stub
-		
+		// Ignore
+
 	}
 
 	public void reset() {
-		// TODO Auto-generated method stub
-		
+		// Ignore
+
 	}
 
 	public void clearView() {
-		// TODO Auto-generated method stub
-		
+		// TODO Check what to do
+
 	}
 
 	public void setMode(int mode) {
-		// TODO Auto-generated method stub
-		
+		// TODO Could be useful?
+
 	}
 
 	public int getViewID() {
-		// TODO Auto-generated method stub
-		return 0;
+		return App.VIEW_NONE;
 	}
 
 	public boolean hasFocus() {
@@ -97,13 +122,12 @@ public class EventDispatcher implements View {
 	}
 
 	public void repaint() {
-		// TODO Auto-generated method stub
-		
+		// Ignore
+
 	}
 
 	public boolean isShowing() {
 		return false;
 	}
-	
-	
+
 }
