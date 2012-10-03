9e2ebf258ad3df31ce36f0a1a75a8f4b0e8e2698
Arnoud Delobelle <arnodel@gmail.com>
2012-10-03 12:32:22 +0000

    Unused imports
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@20859 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/parser/cashandlers/ParserFunctions.java
+++ common/src/geogebra/common/kernel/parser/cashandlers/ParserFunctions.java
@@ -8,8 +8,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
