geogebra/geogebra

commit 9bb7c98ca3446968eb4c8fad395f5274e42818c3

applet null-pointer fix
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@21124 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/geogebra/common/kernel/Construction.java
@@ -2736,19 +2736,10 @@ public class Construction {
 	 * @return StringBuilder with xml of this construction.
 	 */
 	public StringBuilder getCurrentUndoXML() {
-		return xmlio.getUndoXML(this);
+		return kernel.getApplication().getXMLio().getUndoXML(this);
 	}
 
-	private MyXMLio xmlio;
 	private GeoElement outputGeo;
-
-	/**
-	 * @param xmlio
-	 *            XMLio object
-	 */
-	public void setXMLio(MyXMLio xmlio) {
-		this.xmlio = xmlio;
-	}
 
 	/**
 	 * Clears the undo info list of this construction and adds the current

common/src/geogebra/common/main/App.java
@@ -4279,4 +4279,6 @@ public abstract class App {
 		return optionsMenu;
 	}
 
+	public abstract MyXMLio getXMLio();
+
 }

desktop/geogebra/kernel/UndoManagerD.java
@@ -65,16 +65,12 @@ public class UndoManagerD extends UndoManager {
 
 	private static final String TEMP_FILE_PREFIX = "GeoGebraUndoInfo";
 	
-	private MyXMLio xmlio;
-
 	/**
 	 * Creates a new UndowManager for the given Construction.
 	 * @param cons construction
 	 */
 	public UndoManagerD(Construction cons) {
 		super(cons);
-		xmlio = new MyXMLio(cons.getKernel(), cons);
-		cons.setXMLio(xmlio);
 	}
 
 	/**
@@ -194,7 +190,7 @@ public class UndoManagerD extends UndoManager {
 
 			// load undo info
 			((AppD)app).getScriptManager().disableListeners();
-			xmlio.readZipFromMemory(is);
+			((AppD)app).getXMLio().readZipFromMemory(is);
 			((AppD)app).getScriptManager().enableListeners();
 
 			is.close();
@@ -216,7 +212,7 @@ public class UndoManagerD extends UndoManager {
 	@Override
 	public synchronized void processXML(String strXML) throws Exception {
 		construction.setFileLoading(true);
-		xmlio.processXMLString(strXML, true, false, false);
+		((AppD)app).getXMLio().processXMLString(strXML, true, false, false);
 		construction.setFileLoading(false);
 	}
 	

web/src/geogebra/web/kernel/UndoManagerW.java
@@ -32,17 +32,13 @@ public class UndoManagerW extends UndoManager {
 		}
 	}
 
-	private MyXMLio xmlio;
-
 	public UndoManagerW(Construction cons) {
 	    super(cons);
-		xmlio = new MyXMLio(cons.getKernel(), cons);
-		cons.setXMLio(xmlio);
     }
 
 	@Override
 	public void processXML(String xml) throws Exception {
-		xmlio.processXMLString(xml, true, false);
+		app.getXMLio().processXMLString(xml, true, false);
 
 	}
 

web/src/geogebra/web/main/AppW.java
@@ -2396,6 +2396,7 @@ public class AppW extends App {
 	}
 
 	private NormalizerMinimal normalizerMinimal;
+	private MyXMLio xmlio;
 
 	@Override
 	public NormalizerMinimal getNormalizer() {
@@ -2466,5 +2467,13 @@ public class AppW extends App {
 	public static void setDownKeys(KeyEvent ev) {
 		controlDown = ev.isControlKeyDown();
 		shiftDown = ev.isShiftKeyDown();
+	}
+
+	@Override 
+	public MyXMLio getXMLio() { 
+		if (xmlio == null) { 
+			xmlio = new MyXMLio(kernel, kernel.getConstruction()); 
+		} 
+		return xmlio; 
 	}
 }

web/src/geogebra/resources/js/test.html
@@ -1,0 +1,3 @@
+<html>
+<script src="properties_keys.js"></script>
+</html>

commit ce0d1f513450c58c45df2761dea6174a6ad07b4d

Fixed the bug caused by r21124 (ticket #2735)
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@21162 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/geogebra/common/kernel/Construction.java
@@ -2736,8 +2736,16 @@ public class Construction {
 	 * @return StringBuilder with xml of this construction.
 	 */
 	public StringBuilder getCurrentUndoXML() {
-		return kernel.getApplication().getXMLio().getUndoXML(this);
+		return getXMLio().getUndoXML(this);
 	}
+
+	public MyXMLio getXMLio() {
+		if (xmlio == null)
+			xmlio = kernel.getApplication().createXMLio(this);
+		return xmlio;
+	}
+
+	private MyXMLio xmlio;
 
 	private GeoElement outputGeo;
 

common/src/geogebra/common/main/App.java
@@ -4280,5 +4280,6 @@ public abstract class App {
 	}
 
 	public abstract MyXMLio getXMLio();
+	public abstract MyXMLio createXMLio(Construction cons);
 
 }

desktop/geogebra/kernel/UndoManagerD.java
@@ -190,7 +190,7 @@ public class UndoManagerD extends UndoManager {
 
 			// load undo info
 			((AppD)app).getScriptManager().disableListeners();
-			((AppD)app).getXMLio().readZipFromMemory(is);
+			((geogebra.io.MyXMLio)construction.getXMLio()).readZipFromMemory(is);
 			((AppD)app).getScriptManager().enableListeners();
 
 			is.close();
@@ -212,7 +212,7 @@ public class UndoManagerD extends UndoManager {
 	@Override
 	public synchronized void processXML(String strXML) throws Exception {
 		construction.setFileLoading(true);
-		((AppD)app).getXMLio().processXMLString(strXML, true, false, false);
+		((geogebra.io.MyXMLio)construction.getXMLio()).processXMLString(strXML, true, false, false);
 		construction.setFileLoading(false);
 	}
 	

desktop/geogebra/main/AppD.java
@@ -3561,6 +3561,11 @@ public class AppD extends App implements
 		return (MyXMLio)myXMLio;
 	}
 
+	@Override
+	public MyXMLio createXMLio(Construction cons) {
+		return new MyXMLio(cons.getKernel(), cons);
+	}
+
 	public boolean isSaved() {
 		return isSaved;
 	}

web/src/geogebra/web/kernel/UndoManagerW.java
@@ -38,8 +38,7 @@ public class UndoManagerW extends UndoManager {
 
 	@Override
 	public void processXML(String xml) throws Exception {
-		app.getXMLio().processXMLString(xml, true, false);
-
+		construction.getXMLio().processXMLString(xml, true, false);
 	}
 
 	@Override

web/src/geogebra/web/main/AppW.java
@@ -2480,11 +2480,16 @@ public class AppW extends App {
 	@Override 
 	public MyXMLio getXMLio() { 
 		if (xmlio == null) { 
-			xmlio = new MyXMLio(kernel, kernel.getConstruction()); 
+			xmlio = createXMLio(kernel.getConstruction()); 
 		} 
 		return xmlio; 
 	}
-	
+
+	@Override
+	public MyXMLio createXMLio(Construction cons) {
+		return new MyXMLio(cons.getKernel(), cons);
+	}
+
 	public void setShowToolBar(boolean toolbar, boolean help) {
 		if (toolbar) {
 			JavaScriptInjector.inject(GuiResources.INSTANCE.propertiesKeysJS().getText());