5d7993a0876dcce0f571494ac819d3679739eeef
Zoltán Kovács <zoltan@geogebra.org>
2012-11-05 15:16:54 +0000

    Removing unused imports
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@21513 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/gui/inputfield/AutoCompleteTextFieldW.java
+++ web/src/geogebra/web/gui/inputfield/AutoCompleteTextFieldW.java
@@ -39,8 +39,6 @@ import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.event.dom.client.MouseDownEvent;
-import com.google.gwt.event.dom.client.MouseDownHandler;
 import com.google.gwt.event.dom.client.MouseUpEvent;
 import com.google.gwt.event.dom.client.MouseUpHandler;
 import com.google.gwt.event.logical.shared.SelectionEvent;
