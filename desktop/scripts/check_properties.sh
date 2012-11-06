333e37f45f8401b1fdd9b69a4af59c9066c1b465
Zbyněk Konečný <zbynek@geogebra.org>
2012-11-06 17:23:00 +0000

    Script for checking properties files
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@21548 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ desktop/scripts/check_properties.sh
@@ -0,0 +1,17 @@
+#!/bin/sh/
+#
+# GeoGebra - Dynamic Mathematics for Everyone
+# http://www.geogebra.org
+#
+# This file is part of GeoGebra.
+#
+# This program is free software; you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by 
+# the Free Software Foundation.
+#
+# Script for checking duplicate command names
+#
+# @author: Zbynek Konecny
+#
+
+for i in `ls command_*`; do echo "**Checking $i"; grep -v Syntax $i |sed "s/.*=\(.*\)/\1/" | sort > f$i; cat f$i | uniq | diff - f$i | grep '>' | sed "s/./$i/" |awk '{print "grep \"=" $2 "$\"", $1}' |sh; done
