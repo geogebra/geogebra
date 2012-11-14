36381ac0a851632a4a34e3280d66d90c9b1b4ab2
Zbyněk Konečný <zbynek@geogebra.org>
2012-11-14 16:41:37 +0000

    Fixed CAS cell identification to match properties
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@21733 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/plugin/GeoClass.java
+++ common/src/geogebra/common/plugin/GeoClass.java
@@ -3,7 +3,7 @@ package geogebra.common.plugin;
 public enum GeoClass {
 	ANGLE("Angle"), AXIS("Axis"), BOOLEAN("Boolean"), BUTTON("Button"), TEXTFIELD("TextField"), CONIC("Conic"), CONICPART("ConicPart"), FUNCTION("Function"), INTERVAL("Interval"), FUNCTIONCONDITIONAL("FunctionConditional"), IMAGE("Image"), LINE("Line"), LIST("List"), LOCUS("Locus"), NUMERIC("Numeric"), POINT("Point"), POLYGON("Polygon"), RAY("Ray"), SEGMENT("Segment"), TEXT("Text"), VECTOR("Vector"), CURVE_CARTESIAN("CurveCartesian"), CURVE_POLAR("CurvePolar"), IMPLICIT_POLY("ImplicitPoly"), FUNCTION_NVAR("FunctionNVar"), POLYLINE("PolyLine"), PENSTROKE("PenStroke"), LINEAR_INEQUALITY("LinearInequality"),
 	TURTLE("Turtle"),
-	CAS_CELL("CASCell"),
+	CAS_CELL("CasCell"),
 
 	ANGLE3D("Angle3D"), POINT3D("Point3D"), VECTOR3D("Vector3D"), SEGMENT3D("Segment3D"), LINE3D("Line3D"), RAY3D("Ray3D"), CONIC3D("Conic3D"), AXIS3D("Axis3D"), CURVECARTESIAN3D("CurveCartesian3D"), POLYGON3D("Polygon3D"), PLANE3D("Plane3D"), QUADRIC("Quadric"), QUADRIC_PART("QuadricPart"), QUADRIC_LIMITED("QuadricLimited"), POLYLINE3D("PolyLine3D"), POLYHEDRON("Polyhedron"),
 
