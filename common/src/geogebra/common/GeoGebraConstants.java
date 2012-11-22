ec112cbb3c032859d666fe23004bd9474436c1ab
Zoltán Kovács <zoltan@geogebra.org>
2012-11-22 08:45:28 +0000

    4.9.71.0 + changing SHORT_VERSION_STRING to 5.0
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@21915 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/GeoGebraConstants.java
+++ common/src/geogebra/common/GeoGebraConstants.java
@@ -9,9 +9,9 @@ public interface GeoGebraConstants {
 	// as it is read by the build system
 	// update lines below when this is updated
 	/** last build date */
-	public static final String BUILD_DATE = "21 November 2012";
+	public static final String BUILD_DATE = "22 November 2012";
 	/** complete version string */
-	public static final String VERSION_STRING = "4.9.70.0"; 
+	public static final String VERSION_STRING = "4.9.71.0"; 
 	
 	/** used by version checker, so that sys admins can disable version checking for *all* ggb versions with
 	 * HKEY_LOCAL_MACHINE/Software/JavaSoft/Prefs/geogebra/version_check_allow = false
@@ -29,7 +29,7 @@ public interface GeoGebraConstants {
 	public static final String SPLASH_STRING = "splash.png";
 	// archive
 	/** short version, for online archive */
-	public static final String SHORT_VERSION_STRING = "4.2";
+	public static final String SHORT_VERSION_STRING = "5.0";
 	/** true if CAS is enabled*/
 	public static final boolean CAS_VIEW_ENABLED = true;
 	/** true for beta versions/release candidates*/
