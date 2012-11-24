eedc58d702f3071f8a5b36daee26b6b4cd414e53
Zbyněk Konečný <zbynek@geogebra.org>
2012-11-24 16:05:51 +0000

    Fix for multivariable functions [needs backport]
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@22013 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/cas/mpreduce/CASmpreduce.java
+++ common/src/geogebra/common/cas/mpreduce/CASmpreduce.java
@@ -311,7 +311,7 @@ public abstract class CASmpreduce implements CASGenericInterface {
 			
 			replacements.append("l");
 			replacements.append(parameters[i]);
-			processedBody = body.replaceAll("("+parameters[i]+"\\b)", "l$1");
+			processedBody = processedBody.replaceAll("("+parameters[i]+"\\b)", "l$1");
 			//we need this for x as FitPoly produces function of currentx!!
 			//we also do that for y, just to be sure
 			if(parameters[i].equals(casPrefix+"x"))
