f3a4ac3c31a2b2c7cbbba22404a6811e99f0aaef
Ancsin Gábor <ustakraharez@gmail.com>
2012-11-27 22:28:32 +0000

    Fix for false null
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@22096 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/util/MyDictionary.java
+++ web/src/geogebra/web/util/MyDictionary.java
@@ -78,16 +78,19 @@ public final class MyDictionary {
     }
     this.label = "Dictionary " + section+language;
     attach(section, language);
-    if (dict == null) {
+    if (isNullDict()) {
     	// this is not working on the second call, don't know why
-      //throw new MissingResourceException(
-      //    "Cannot find JavaScript object with the name '__GGB__keysVar["+language+"]["+section+"]'", section+language,
-      //    null);
-    	App.debug("Problem finding JavaScript object with the name '__GGB__keysVar["+language+"]["+section+"]'");
+      throw new MissingResourceException(
+      "Cannot find JavaScript object with the name '__GGB__keysVar["+language+"]["+section+"]'", section+language,
+       null);
     }
   }
 
-  /**
+  private native boolean isNullDict() /*-{
+		return this.@com.google.gwt.i18n.client.Dictionary::dict === null;
+	}-*/;
+
+/**
    * Get the value associated with the given Dictionary key.
    * 
    * We have to call Object.hasOwnProperty to verify that the value is
