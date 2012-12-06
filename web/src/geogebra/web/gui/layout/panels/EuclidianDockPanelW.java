geogebra/geogebra

commit 800ef616bb7040091bb80519022fa86187fc2370

Unify EuclidianPanel, GGWGraphicsView and EuclidianView1 to be EuclidianDockPanelW instead (and meanwhile fix part of a resize bug)
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@22286 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/geogebra/web/gui/layout/panels/EuclidianDockPanelW.java
@@ -1,0 +1,90 @@
+package geogebra.web.gui.layout.panels;
+
+import geogebra.common.main.App;
+import geogebra.web.euclidian.EuclidianViewW;
+import geogebra.web.euclidian.EuclidianStyleBarW;
+import geogebra.web.gui.app.EuclidianStyleBarPanel;
+import geogebra.web.gui.app.VerticalPanelSmart;
+import geogebra.web.gui.view.spreadsheet.SpreadsheetView;
+
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import com.google.gwt.user.client.ui.ResizeComposite;
+import com.google.gwt.user.client.ui.SimpleLayoutPanel;
+
+public class EuclidianDockPanelW extends ResizeComposite /*DockPanelW*/ {
+
+	App application = null;
+
+	SimpleLayoutPanel toplevel;
+
+	VerticalPanelSmart ancestor;
+	EuclidianStyleBarPanel espanel;
+	AbsolutePanel euclidianpanel;
+	Canvas eview1 = null;
+
+	public EuclidianDockPanelW(boolean stylebar) {
+		super();
+		initWidget(toplevel = new SimpleLayoutPanel());
+		ancestor = new VerticalPanelSmart();
+
+		if (stylebar)
+			ancestor.add(espanel = new EuclidianStyleBarPanel());
+
+		ancestor.add(euclidianpanel = new AbsolutePanel());
+		toplevel.add(ancestor);
+		eview1 = Canvas.createIfSupported();
+		euclidianpanel.add(eview1);
+	}
+
+	public Canvas getCanvas() {
+	    return eview1;
+    }
+
+	public AbsolutePanel getAbsolutePanel() {
+	    return euclidianpanel;
+    }
+
+	public void onResize() {
+		super.onResize();
+		//App.debug("resized");
+		/*if (application != null) {
+
+			//if (sview != null) {
+				// If this is resized, we may know its width and height
+
+				int width = this.getOffsetWidth();
+				int height = this.getOffsetHeight();
+				ancestor.setWidth(width+"px");
+				ancestor.setHeight(height+"px");
+
+				height -=
+					((EuclidianStyleBarW)application.getActiveEuclidianView().getStyleBar()).
+					getOffsetHeight();
+
+				eview1.setWidth(width+"px");
+				eview1.setHeight(height+"px");
+				((EuclidianViewW)application.getActiveEuclidianView()).setPreferredSize(width, height);
+			//}
+		}*/
+    }
+
+	public void add(Widget w, int x, int y) {
+	    euclidianpanel.add(w,x,y);
+    }
+
+	public void remove(Widget w) {
+		euclidianpanel.remove(w);
+    }
+
+	public void attachApp(App app) {
+		this.application = app;
+		espanel.attachApp(app);
+	}
+
+	public EuclidianDockPanelW getEuclidianView1Wrapper() {
+		return this;
+	}
+}

web/src/geogebra/web/main/AppW.java
@@ -45,7 +45,6 @@ import geogebra.web.euclidian.EuclidianControllerW;
 import geogebra.web.euclidian.EuclidianViewW;
 import geogebra.web.gui.GuiManagerW;
 import geogebra.web.gui.SplashDialog;
-import geogebra.web.gui.app.EuclidianPanel;
 import geogebra.web.gui.app.GGWCommandLine;
 import geogebra.web.gui.app.GGWMenuBar;
 import geogebra.web.gui.app.GGWToolBar;
@@ -55,6 +54,7 @@ import geogebra.web.gui.dialog.DialogManagerW;
 import geogebra.web.gui.images.AppResources;
 import geogebra.web.gui.infobar.InfoBarW;
 import geogebra.web.gui.inputbar.AlgebraInputW;
+import geogebra.web.gui.layout.panels.EuclidianDockPanelW;
 import geogebra.web.gui.menubar.GeoGebraMenubarW;
 import geogebra.web.gui.menubar.LanguageCommand;
 import geogebra.web.gui.view.algebra.AlgebraViewW;
@@ -154,7 +154,7 @@ public class AppW extends App {
 
 	protected ImageManager imageManager;
 
-	private EuclidianPanel euclidianViewPanel;
+	private EuclidianDockPanelW euclidianViewPanel;
 	private Canvas canvas;
 	private geogebra.common.plugin.GgbAPI ggbapi;
 	private HashMap<String, String> currentFile = null;
@@ -199,7 +199,7 @@ public class AppW extends App {
 		        + Window.Navigator.getUserAgent());
 		initCommonObjects();
 
-		euclidianViewPanel = new EuclidianPanel();
+		euclidianViewPanel = new EuclidianDockPanelW(false);
 		this.canvas = euclidianViewPanel.getCanvas();
 		canvas.setWidth("1px");
 		canvas.setHeight("1px");
@@ -1177,7 +1177,7 @@ public class AppW extends App {
 		return canvas;
 	}
 
-	public EuclidianPanel getEuclidianViewpanel() {
+	public EuclidianDockPanelW getEuclidianViewpanel() {
 		return euclidianViewPanel;
 	}
 
@@ -2235,10 +2235,15 @@ public class AppW extends App {
 	 *            Resets the width of the Canvas converning the Width of its
 	 *            wrapper (splitlayoutpanel center)
 	 */
-	public void ggwGraphicsViewWidthChanged(int ggwGraphicsViewWidth) {
+	public void ggwGraphicsViewDimChanged(int width, int height) {
 		getSettings().getEuclidian(1).setPreferredSize(
 		        geogebra.common.factories.AwtFactory.prototype.newDimension(
-		                ggwGraphicsViewWidth, appCanvasHeight));
+		                width, height));
+
+		// simple setting temp.
+		appCanvasHeight = height;
+		appCanvasWidth = width;
+
 		getEuclidianView1().setDisableRepaint(false);
 		getEuclidianView1().synCanvasSize();
 		getEuclidianView1().doRepaint();

web/src/geogebra/web/euclidian/EuclidianViewW.java
@@ -19,7 +19,7 @@ import geogebra.common.main.App;
 import geogebra.common.main.settings.EuclidianSettings;
 import geogebra.common.plugin.EuclidianStyleConstants;
 import geogebra.web.awt.GBasicStrokeW;
-import geogebra.web.gui.app.EuclidianPanel;
+import geogebra.web.gui.layout.panels.EuclidianDockPanelW;
 import geogebra.web.gui.applet.GeoGebraFrame;
 import geogebra.web.main.AppW;
 
@@ -62,7 +62,7 @@ public class EuclidianViewW extends EuclidianView {
 	protected ImageElement resetImage, playImage, pauseImage, upArrowImage,
 	downArrowImage;
 
-	public EuclidianViewW(EuclidianPanel euclidianViewPanel,
+	public EuclidianViewW(EuclidianDockPanelW euclidianViewPanel,
             EuclidianController euclidiancontroller, boolean[] showAxes,
             boolean showGrid, EuclidianSettings settings) {		
 		super(euclidiancontroller, settings);

web/src/geogebra/web/gui/app/EuclidianPanel.java
@@ -1,51 +1,0 @@
-package geogebra.web.gui.app;
-
-import geogebra.common.main.App;
-
-import com.google.gwt.canvas.client.Canvas;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.AbsolutePanel;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.RequiresResize;
-import com.google.gwt.user.client.ui.Widget;
-
-public class EuclidianPanel extends Composite implements RequiresResize {
-
-	private static EuclidianPanelUiBinder uiBinder = GWT
-	        .create(EuclidianPanelUiBinder.class);
-
-	interface EuclidianPanelUiBinder extends UiBinder<AbsolutePanel, EuclidianPanel> {
-	}
-	
-	@UiField AbsolutePanel euclidianpanel;
-	Canvas eview1 = null;
-
-	public EuclidianPanel() {
-		initWidget(uiBinder.createAndBindUi(this));
-		eview1 = Canvas.createIfSupported();	
-		euclidianpanel.add(eview1);
-	}
-
-	public Canvas getCanvas() {
-	    return eview1;
-    }
-
-	public AbsolutePanel getAbsolutePanel() {
-	    return euclidianpanel;
-    }
-
-	public void onResize() {
-		App.debug("resized");
-    }
-
-	public void add(Widget w, int x, int y) {
-	    euclidianpanel.add(w,x,y);
-    }
-
-	public void remove(Widget w) {
-	   euclidianpanel.remove(w);
-    }
-
-}

web/src/geogebra/web/gui/app/EuclidianPanel.ui.xml
@@ -1,14 +1,0 @@
-<!DOCTYPE ui:UiBinder SYSTEM "http://dl.google.com/gwt/DTD/xhtml.ent">
-<ui:UiBinder xmlns:ui="urn:ui:com.google.gwt.uibinder"
-	xmlns:g="urn:import:com.google.gwt.user.client.ui">
-	<ui:style>
-		.absolutepanel {
-			position:relative;
-			background-color:blue;
-			width:1px;
-			height:1px;
-		}
-	</ui:style>
-	<g:AbsolutePanel ui:field="euclidianpanel" styleName="{style.absolutepanel}">
-	</g:AbsolutePanel>
-</ui:UiBinder> 

web/src/geogebra/web/gui/app/EuclidianView1.java
@@ -1,72 +1,0 @@
-package geogebra.web.gui.app;
-
-import geogebra.common.main.App;
-
-import com.google.gwt.canvas.client.Canvas;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.ResizeComposite;
-import com.google.gwt.user.client.ui.VerticalPanel;
-
-/**
- * @author gabor
- * 
- * The markup for EuclidianView in UIBinder
- *
- */
-public class EuclidianView1 extends ResizeComposite {
-
-	App application = null;
-
-	private static EuclidianView1UiBinder uiBinder = GWT
-	        .create(EuclidianView1UiBinder.class);
-
-	/**
-	 * @author gabor
-	 * 
-	 * Binds EuclidianView as UIBinder
-	 *
-	 */
-	interface EuclidianView1UiBinder extends UiBinder<VerticalPanel, EuclidianView1> {
-	}
-
-	/**
-	 * EuclidianPanel for textfields
-	 */
-	@UiField VerticalPanelSmart ancestor;
-	@UiField EuclidianStyleBarPanel espanel;
-	@UiField EuclidianPanel euclidianpanel;
-
-	public EuclidianView1() {
-		initWidget(uiBinder.createAndBindUi(this));
-	}
-	
-	/**
-	 * @return Canvas corresponding eview1
-	 */
-	public Canvas getCanvas() {
-		return euclidianpanel.getCanvas();
-	}
-	
-	/**
-	 * @return euclidianPanel (needed for wrap for textfields)
-	 */
-	public EuclidianPanel getEuclidianPanel() {
-		return euclidianpanel;
-	}
-
-	public void onResize() {
-		App.debug("Resized");
-    }
-	
-	public void onLoad() {
-		//getEuclidianPanel().getElement().getStyle().setPosition(Position.RELATIVE);
-	}
-
-	public void attachApp(App app) {
-	   this.application = app;
-	   espanel.attachApp(app);
-	}
-
-}

web/src/geogebra/web/gui/app/EuclidianView1.ui.xml
@@ -1,14 +1,0 @@
-<!DOCTYPE ui:UiBinder SYSTEM "http://dl.google.com/gwt/DTD/xhtml.ent">
-<ui:UiBinder xmlns:ui="urn:ui:com.google.gwt.uibinder"
-	xmlns:g="urn:import:com.google.gwt.user.client.ui"
-	xmlns:gc="urn:import:com.google.gwt.canvas.client" 
-	xmlns:web="urn:import:geogebra.web.gui.app">
-	<ui:style>
-		
-	</ui:style>
-
-	<web:VerticalPanelSmart ui:field="ancestor">
-		<web:EuclidianStyleBarPanel ui:field="espanel"></web:EuclidianStyleBarPanel>
-		<web:EuclidianPanel ui:field="euclidianpanel" ></web:EuclidianPanel>
-	</web:VerticalPanelSmart>
-</ui:UiBinder>

web/src/geogebra/web/gui/app/GGWGraphicsView.java
@@ -1,52 +1,0 @@
-package geogebra.web.gui.app;
-
-import geogebra.common.main.App;
-
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Widget;
-
-/**
- * @author gabor
- * 
- * Wrapper for the Two EuclidianView with UIBinder
- *
- */
-public class GGWGraphicsView extends Composite {
-
-	App application = null;
-	
-	private static GGWGraphicsViewUiBinder uiBinder = GWT
-	        .create(GGWGraphicsViewUiBinder.class);
-
-	interface GGWGraphicsViewUiBinder extends UiBinder<Widget, GGWGraphicsView> {
-	}
-	
-	/**
-	 * 
-	 */
-	@UiField EuclidianView1 eview1;
-
-	/**
-	 * 	Wrapper for the two EuclidianView (one is active only)
-	 */
-	public GGWGraphicsView() {
-		initWidget(uiBinder.createAndBindUi(this));
-	}
-	
-	/**
-	 * @return Euclidianview1,
-	 * 
-	 * The wrapper of EuclidianView in UIBinder
-	 */
-	public EuclidianView1 getEuclidianView1Wrapper() {
-		return eview1;
-	}
-
-	public void attachApp(App app) {
-	   this.application = app;
-	   eview1.attachApp(app);
-	}
-}

commit 9d7d4db575cc728b6f11058641811e1b8ba7a15a

Fixed the bug caused by r22286 (this area will be refactored afterwards)
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@22300 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/geogebra/web/gui/layout/panels/EuclidianDockPanelW.java
@@ -4,6 +4,7 @@ import geogebra.common.main.App;
 import geogebra.web.euclidian.EuclidianViewW;
 import geogebra.web.euclidian.EuclidianStyleBarW;
 import geogebra.web.gui.app.EuclidianStyleBarPanel;
+import geogebra.web.gui.app.AbsolutePanelSmart;
 import geogebra.web.gui.app.VerticalPanelSmart;
 import geogebra.web.gui.view.spreadsheet.SpreadsheetView;
 
@@ -22,19 +23,21 @@ public class EuclidianDockPanelW extends ResizeComposite /*DockPanelW*/ {
 
 	VerticalPanelSmart ancestor;
 	EuclidianStyleBarPanel espanel;
-	AbsolutePanel euclidianpanel;
+	AbsolutePanelSmart euclidianpanel;
 	Canvas eview1 = null;
 
 	public EuclidianDockPanelW(boolean stylebar) {
 		super();
-		initWidget(toplevel = new SimpleLayoutPanel());
-		ancestor = new VerticalPanelSmart();
-
-		if (stylebar)
+		if (stylebar) {
+			initWidget(toplevel = new SimpleLayoutPanel());
+			ancestor = new VerticalPanelSmart();
 			ancestor.add(espanel = new EuclidianStyleBarPanel());
+			ancestor.add(euclidianpanel = new AbsolutePanelSmart());
+			toplevel.add(ancestor);
+		} else {
+			initWidget(euclidianpanel = new AbsolutePanelSmart());
+		}
 
-		ancestor.add(euclidianpanel = new AbsolutePanel());
-		toplevel.add(ancestor);
 		eview1 = Canvas.createIfSupported();
 		euclidianpanel.add(eview1);
 	}
@@ -86,5 +89,9 @@ public class EuclidianDockPanelW extends ResizeComposite /*DockPanelW*/ {
 
 	public EuclidianDockPanelW getEuclidianView1Wrapper() {
 		return this;
+	}
+
+	public AbsolutePanel getEuclidianPanel() {
+		return euclidianpanel;
 	}
 }

web/src/geogebra/web/main/AppW.java
@@ -1968,7 +1968,9 @@ public class AppW extends App {
 		}
 		
 		//return euclidianViewPanel;
+		App.debug("buildAppPanel start");
 		frame.add(euclidianViewPanel);
+		App.debug("buildAppPanel done");
 
 		if (showAlgebraInput){
 			attachAlgebraInput();