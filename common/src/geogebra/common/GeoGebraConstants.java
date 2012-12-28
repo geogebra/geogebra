e27013a654a536a6cf5514eb91913101386ba69d
Michael Borcherds <michael@geogebra.org>
2012-12-28 22:09:56 +0000

    4.9.85.0
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@22713 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/GeoGebraConstants.java
+++ common/src/geogebra/common/GeoGebraConstants.java
@@ -9,9 +9,9 @@ public interface GeoGebraConstants {
 	// as it is read by the build system
 	// update lines below when this is updated
 	/** last build date */
-	public static final String BUILD_DATE = "27 December 2012";
+	public static final String BUILD_DATE = "28 December 2012";
 	/** complete version string */
-	public static final String VERSION_STRING = "4.9.84.0"; 
+	public static final String VERSION_STRING = "4.9.85.0"; 
 	
 	/** used by version checker, so that sys admins can disable version checking for *all* ggb versions with
 	 * HKEY_LOCAL_MACHINE/Software/JavaSoft/Prefs/geogebra/version_check_allow = false
