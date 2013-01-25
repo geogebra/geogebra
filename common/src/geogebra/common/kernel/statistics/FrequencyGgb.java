d9a0e2a7ea5508ce84290c8559a6d067c88284ae
George Sturr <gsturr@santarosa.edu>
2013-01-25 15:46:26 +0000

    fixed FrequencyGgb to correctly override methods in Frequency
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@23236 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/statistics/FrequencyGgb.java
+++ common/src/geogebra/common/kernel/statistics/FrequencyGgb.java
@@ -5,40 +5,33 @@ import geogebra.common.kernel.Kernel;
 import org.apache.commons.math.stat.Frequency;
 
 /**
- * Extension of the Apache Commons Frequency class. Adds methods for handling
- * comparisons with double values that may contain rounding errors and are
- * nearly, but not exactly equal.
+ * Extension of the Apache Commons Frequency class. Modifies some methods to
+ * handle comparisons with double values that may contain rounding errors and
+ * are nearly, but not exactly equal.
  * 
  * @author G. Sturr
  * 
  */
 public class FrequencyGgb extends Frequency {
 
-	/**
-	 * Returns the number of values = v.
-	 * 
-	 * @param v
-	 *            the value to lookup.
-	 * @return the frequency of v.
-	 */
-	public long getCount(double v) {
-		Double v2 = Kernel.checkDecimalFraction(v);
-		return getCount(v2);
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public long getCount(Comparable<?> v) {
+		if (v instanceof Double) {
+			Double v2 = Kernel.checkDecimalFraction((Double) v);
+			return super.getCount(v2);
+		}
+		return super.getCount(v);
 	}
 
-	/**
-	 * Returns the cumulative frequency of values less than or equal to v.
-	 * <p>
-	 * Returns 0 if v is not comparable to the values set.
-	 * </p>
-	 * 
-	 * @param v
-	 *            the value to lookup
-	 * @return the proportion of values equal to v
-	 */
-	public long getCumFreq(double v) {
-		Double v2 = Kernel.checkDecimalFraction(v);
-		return getCumFreq(v2);
+	@Override
+	public long getCumFreq(Comparable<?> v) {
+		if (v instanceof Double) {
+			Double v2 = Kernel.checkDecimalFraction((Double) v);
+			return super.getCumFreq(v2);
+		}
+		return super.getCumFreq(v);
 	}
 
 }
