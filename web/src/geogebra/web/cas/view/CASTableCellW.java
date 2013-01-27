2643fcb2f83d562f5e6aecd8f75e6023017b792c
Zbyněk Konečný <zbynek@geogebra.org>
2013-01-27 15:55:57 +0000

    NPE fix for Web's CAS cells
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@23264 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/cas/view/CASTableCellW.java
+++ web/src/geogebra/web/cas/view/CASTableCellW.java
@@ -16,15 +16,17 @@ public class CASTableCellW extends VerticalPanel {
 	public CASTableCellW(GeoCasCell casCell) {
 		this.casCell = casCell;
 		Label inputPanel = new Label();
-		inputPanel.setText(casCell.getInput(StringTemplate.defaultTemplate));
-		inputPanel.getElement().getStyle().setPadding(2, Style.Unit.PX);
-		inputPanel.getElement().getStyle().setOverflow(Style.Overflow.HIDDEN);
-		inputPanel.getElement().getStyle().setHeight(100, Style.Unit.PCT);
+		if(casCell!=null){
+			inputPanel.setText(casCell.getInput(StringTemplate.defaultTemplate));
+			inputPanel.getElement().getStyle().setPadding(2, Style.Unit.PX);
+			inputPanel.getElement().getStyle().setOverflow(Style.Overflow.HIDDEN);
+			inputPanel.getElement().getStyle().setHeight(100, Style.Unit.PCT);
+		}
 		add(inputPanel);
 
 		Label outputPanel = new Label();
 		outputPanel.getElement().getStyle().setPadding(2, Style.Unit.PX);
-		if (casCell.showOutput()) {
+		if (casCell!=null && casCell.showOutput()) {
 			if (casCell.getLaTeXOutput() != null && !casCell.isError()) {
 				SpanElement outputSpan = DOM.createSpan().cast();
 				DrawEquationWeb.drawEquationMathQuill(outputSpan,
