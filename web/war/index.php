3712b4d1ba44bf933d20dc5e48d65b77c2dc396f
Zoltán Kovács <zoltan@geogebra.org>
2013-02-06 15:31:39 +0000

    No locale parameter used any longer
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@23419 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/war/index.php
+++ web/war/index.php
@@ -8,33 +8,7 @@
     die();
   }
 
-  // This is a web_gui call.
-  $forced_locale=$_GET['locale'];
-  if ($forced_locale!="") {
-    // The forced language setting is used:
-    header('Location: app.html?locale=' . $forced_locale);
-    die();
-  }
-
-  // No language is forced, then we use the browser's settings:
-  $languages = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-  if (isset($languages)) {
-    // Find the first comma, get a substring from location 0 up to the location of the comma.
-    $comma_pos = strpos($languages, ',');
-    $locale = substr($languages, 0, $comma_pos);
-
-    // The accepted character among the locale variable are a-z or A-Z or _ or - only.
-    if (preg_match('/^[a-z_\-]+$/i', $locale)) {
-      $gwtLocale = str_replace('-', '_', $locale);
-    } else {
-      $gwtLocale = 'en';
-    }
-  } else {
-    $gwtLocale = 'en';
-  }
-
-  // Redirect the application to GeoGebra and pass the language parameter.
-  // If language is not recognized then en is the default.
-  header('Location: app.html?locale=' . $gwtLocale);
+  // Redirect the application to GeoGebra.
+  header('Location: app.html');
 
 ?>
