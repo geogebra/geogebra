51e4e955a6bcf196e8a592bcc3e79b8249688214
Zbyněk Konečný <zbynek@geogebra.org>
2013-02-12 22:28:01 +0000

    Marble click listener
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@23509 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/cas/view/RowHeaderWidget.java
+++ web/src/geogebra/web/cas/view/RowHeaderWidget.java
@@ -4,6 +4,8 @@ import geogebra.common.kernel.geos.GeoCasCell;
 import geogebra.common.kernel.geos.MarbleRenderer;
 import geogebra.web.gui.images.AppResources;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -19,8 +21,9 @@ public class RowHeaderWidget extends VerticalPanel implements MarbleRenderer {
 		oldValue = false;
 		add(label);
 		add(marble);
-		if(cell!=null)
+		if (cell != null)
 			cell.handleMarble(this);
+		marble.addClickHandler(new MarbleClickHandler(cell));
 	}
 
 	public void setMarbleValue(boolean value) {
@@ -34,6 +37,19 @@ public class RowHeaderWidget extends VerticalPanel implements MarbleRenderer {
 	public void setMarbleVisible(boolean visible) {
 		marble.setVisible(visible);
 
+	}
+
+	protected class MarbleClickHandler implements ClickHandler {
+		private GeoCasCell cell;
+
+		protected MarbleClickHandler(GeoCasCell cell) {
+			this.cell = cell;
+		}
+
+		public void onClick(ClickEvent event) {
+			cell.toggleTwinGeoEuclidianVisible();
+			event.stopPropagation();
+		}
 	}
 
 }
