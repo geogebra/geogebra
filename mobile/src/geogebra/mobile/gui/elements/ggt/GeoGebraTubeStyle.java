0316b3c4798b4740ca102f70d5dacabb9e04fbcc
Matthias Meisinger <matthias.meisinger@geogebra.org>
2013-02-22 07:17:43 +0000

    MaterialPreview refinements finished
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@23687 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ mobile/src/geogebra/mobile/gui/elements/ggt/GeoGebraTubeStyle.java
@@ -0,0 +1,14 @@
+package geogebra.mobile.gui.elements.ggt;
+
+public class GeoGebraTubeStyle
+{
+	public static final String BorderColor = "#477aac";
+	public static final int BorderWidth = 2;
+	public static final String TitleColor = "#0d3a64";
+	public static final String TextColor = "#666";
+
+	// TODO USE I18N
+	public static final String SharedBy = "Shared by:";
+	public static final String InfoBackground = "#FFFFFF";
+	public static final double InfoBackgroundOpacity = 0.8;
+}
--- mobile/src/geogebra/mobile/gui/elements/ggt/MaterialPreview.java
+++ mobile/src/geogebra/mobile/gui/elements/ggt/MaterialPreview.java
@@ -2,11 +2,17 @@ package geogebra.mobile.gui.elements.ggt;
 
 import geogebra.mobile.utils.ggtapi.Material;
 
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.dom.client.Style.BorderStyle;
+import com.google.gwt.dom.client.Style.Overflow;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.layout.client.Layout.Alignment;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment.VerticalAlignmentConstant;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * GUI Element showing a Material
@@ -16,6 +22,8 @@ import com.google.gwt.dom.client.Style.Unit;
  */
 public class MaterialPreview extends LayoutPanel
 {
+	private VerticalPanel infos;
+	private HorizontalPanel shared;
 	private Label title, author, sharedBy;
 
 	public MaterialPreview(Material m)
@@ -23,30 +31,42 @@ public class MaterialPreview extends LayoutPanel
 		this.setWidth("200px");
 		this.setHeight("100px");
 
+		this.infos = new VerticalPanel();
+		this.infos.setVerticalAlignment(HasVerticalAlignment.ALIGN_BOTTOM);
+		this.infos.getElement().getStyle().setBackgroundColor(GeoGebraTubeStyle.InfoBackground);
+		this.infos.getElement().getStyle().setOpacity(GeoGebraTubeStyle.InfoBackgroundOpacity);
+		this.infos.setSize("100%", "50%");
+
+		this.shared = new HorizontalPanel();
+		this.shared.setWidth("100%");
+
 		this.getElement().getStyle().setBackgroundImage("url(http:" + m.getThumbnail() + ")");
-		this.getElement().getStyle().setBorderColor("#477aac");
+		this.getElement().getStyle().setBorderColor(GeoGebraTubeStyle.BorderColor);
 		this.getElement().getStyle().setBorderStyle(BorderStyle.SOLID);
-		this.getElement().getStyle().setBorderWidth(2, Unit.PX);
+		this.getElement().getStyle().setBorderWidth(GeoGebraTubeStyle.BorderWidth, Unit.PX);
 		this.getElement().getStyle().setMarginLeft(5, Unit.PX);
 		this.getElement().getStyle().setMarginRight(5, Unit.PX);
 
 		this.title = new Label(m.getTitle());
+		this.title.setWordWrap(true);
+		this.title.getElement().getStyle().setOverflow(Overflow.HIDDEN);
 		this.title.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
+		this.title.getElement().getStyle().setColor(GeoGebraTubeStyle.TitleColor);
 
-		this.sharedBy = new Label("Shared by:");
-		this.sharedBy.setSize("70px", "20px");
+		this.sharedBy = new Label(GeoGebraTubeStyle.SharedBy);
+		this.sharedBy.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
+		this.sharedBy.getElement().getStyle().setColor(GeoGebraTubeStyle.TextColor);
 
 		this.author = new Label(m.getAuthor());
-		this.author.setWidth("100px");
+		this.author.getElement().getStyle().setColor(GeoGebraTubeStyle.TextColor);
 
-		this.add(this.title);
-		setWidgetLeftRight(this.title, 0.0, Unit.PX, 0.0, Unit.PX);
-		setWidgetBottomHeight(this.title, 25.0, Unit.PX, 20.0, Unit.PX);
-		this.add(this.sharedBy);
-		setWidgetLeftWidth(this.sharedBy, 0.0, Unit.PX, 75.0, Unit.PX);
-		setWidgetBottomHeight(this.sharedBy, -1.0, Unit.PX, 20.0, Unit.PX);
-		this.add(this.author);
-		setWidgetRightWidth(this.author, 0.0, Unit.PX, 125.0, Unit.PX);
-		setWidgetTopHeight(this.author, 81.0, Unit.PX, 20.0, Unit.PX);
+		this.infos.add(this.title);
+		this.shared.add(this.sharedBy);
+		this.shared.add(this.author);
+		this.infos.add(this.shared);
+		this.add(this.infos);
+		this.setWidgetVerticalPosition(this.infos, Alignment.END);
+		
+		
 	}
 }
