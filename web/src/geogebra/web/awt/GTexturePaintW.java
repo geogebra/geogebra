9dd6fc1995dc443bd42265ec78e4d5615769a582
Zbyněk Konečný <zbynek@geogebra.org>
2013-03-14 09:41:18 +0000

    Simplified GTexturePaintW
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@24049 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/awt/GTexturePaintW.java
+++ web/src/geogebra/web/awt/GTexturePaintW.java
@@ -4,27 +4,27 @@ import com.google.gwt.dom.client.ImageElement;
 
 public class GTexturePaintW implements geogebra.common.awt.GPaint {
 	
-	GRectangleW anchor = null;
-	GBufferedImageW img = null;
+	//GRectangleW anchor = null;
+	ImageElement img = null;
 
 	public GTexturePaintW(GTexturePaintW tp) {
-		this.anchor = new GRectangleW();
-		this.anchor.setRect(tp.anchor.getX(), tp.anchor.getY(), tp.anchor.getWidth(), tp.anchor.getHeight());
-		this.img = new GBufferedImageW((ImageElement)tp.img.getImageElement().cloneNode(true));
+		//this.anchor = new GRectangleW();
+		//this.anchor.setRect(tp.anchor.getX(), tp.anchor.getY(), tp.anchor.getWidth(), tp.anchor.getHeight());
+		this.img = (ImageElement)tp.img.cloneNode(true);
 	}
 
 	public GTexturePaintW(GBufferedImageW subimage, GRectangleW rect) {
-	    img = subimage;
-	    anchor = rect;
+	    img = subimage.getImageElement();
+	    //anchor = rect;
     }
 
 	public GTexturePaintW(GBufferedImageW copy, GRectangle2DW tr) {
-	    img = copy;
-	    anchor  = new GRectangleW((int) tr.getX(),(int) tr.getY(),(int) tr.getWidth(),(int) tr.getHeight());
+	    img = copy.getImageElement();
+	    //anchor  = new GRectangleW((int) tr.getX(),(int) tr.getY(),(int) tr.getWidth(),(int) tr.getHeight());
     }
 	
 	public ImageElement getImg() {
-		return img.getImageElement();
+		return img;
 	}
 	
 	
