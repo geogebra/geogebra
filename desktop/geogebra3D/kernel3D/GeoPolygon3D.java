7c7e7344600b7ee2fdf70fe49fae4edf067be316
Mathieu Blossier <mathieublossier@free.fr>
2013-03-15 12:00:08 +0000

    ApplyMatrix on 3D polygon : update coord sys for normal direction
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@24090 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra3D/kernel3D/GeoPolygon3D.java
+++ desktop/geogebra3D/kernel3D/GeoPolygon3D.java
@@ -623,5 +623,20 @@ public class GeoPolygon3D extends GeoPolygon implements GeoElement3DInterface,
 		}
 		super.doRemove();
 	}
+	
+	
+	@Override
+	public void matrixTransform(double a00, double a01, double a10, double a11) {
+		super.matrixTransform(a00, a01, a10, a11);
+		updateCoordSys();
+	}
+
+	@Override
+	public void matrixTransform(double a00, double a01, double a02, double a10,
+			double a11, double a12, double a20, double a21, double a22) {
+
+		super.matrixTransform(a00, a01, a02, a10, a11, a12, a20, a21, a22);
+		updateCoordSys();
+	}
 
 }
