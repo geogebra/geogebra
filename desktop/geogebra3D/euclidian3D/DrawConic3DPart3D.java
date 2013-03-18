f5a0dcadd14e0308c744994406bd102ee17a8cb5
Mathieu Blossier <mathieublossier@free.fr>
2013-03-18 14:01:25 +0000

    Intersect plane/limited quadric (part 1) : outline drawing (ellipse part)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@24153 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra3D/euclidian3D/DrawConic3DPart3D.java
+++ desktop/geogebra3D/euclidian3D/DrawConic3DPart3D.java
@@ -2,7 +2,6 @@ package geogebra3D.euclidian3D;
 
 import geogebra.common.kernel.Matrix.Coords;
 import geogebra.common.kernel.kernelND.GeoConicND;
-import geogebra.common.kernel.kernelND.GeoConicNDConstants;
 import geogebra3D.euclidian3D.opengl.PlotterBrush;
 import geogebra3D.euclidian3D.opengl.PlotterSurface;
 import geogebra3D.kernel3D.GeoConic3DPart;
@@ -19,56 +18,76 @@ public class DrawConic3DPart3D extends DrawConic3D {
 	
 	@Override
 	protected double getStart(){
-		//return 0;
 		return ((GeoConic3DPart) getGeoElement()).getParameterStart(0);
 	}
 	
 	@Override
 	protected double getExtent(){
 		return ((GeoConic3DPart) getGeoElement()).getParameterExtent(0);
+	}	
+	
+	protected double getStart(int i){
+		return ((GeoConic3DPart) getGeoElement()).getParameterStart(i);
+	}
+	
+	protected double getExtent(int i){
+		return ((GeoConic3DPart) getGeoElement()).getParameterExtent(i);
 	}
 	
 
 	@Override
 	protected void updateCircle(PlotterBrush brush){
 		
-		GeoConic3DPart conic = (GeoConic3DPart) getGeoElement();
-		Coords m = conic.getMidpoint3D();
-		Coords ev0 = conic.getEigenvec3D(0); 
-		Coords ev1 = conic.getEigenvec3D(1);
-		double radius = conic.getHalfAxis(0);
-		double start = getStart();
-		double extent = getExtent();
-		brush.arc(m, ev0, ev1, radius,start,extent);
-		
-		updateSectorSegments(brush, conic.getConicPartType(), m, ev0, ev1, radius, radius, start, start+extent);
+		updateEllipse(brush);
 	}
 	
 	@Override
 	protected void updateEllipse(PlotterBrush brush){
 		
-		GeoConic3DPart conic = (GeoConic3DPart) getGeoElement();
-		Coords m = conic.getMidpoint3D();
-		Coords ev0 = conic.getEigenvec3D(0); 
-		Coords ev1 = conic.getEigenvec3D(1);
-		double r0 = conic.getHalfAxis(0);
-		double r1 = conic.getHalfAxis(1);
-		double start = getStart();
-		double extent = getExtent();
-		brush.arcEllipse(m, ev0, ev1, r0, r1,start,extent);
 		
-		updateSectorSegments(brush, conic.getConicPartType(), m, ev0, ev1, r0, r1, start, start+extent);
+		double start0 = getStart();
+		double extent0 = getExtent();
+		double start1 = getStart(1);
+		double extent1 = getExtent(1);
+		
+		
+		if(!Double.isNaN(start0)){ // there is at least one hole
+			
+			GeoConic3DPart conic = (GeoConic3DPart) getGeoElement();
+			Coords m = conic.getMidpoint3D();
+			Coords ev0 = conic.getEigenvec3D(0); 
+			Coords ev1 = conic.getEigenvec3D(1);
+			double r0 = conic.getHalfAxis(0);
+			double r1 = conic.getHalfAxis(1);
+			
+			brush.arcEllipse(m, ev0, ev1, r0, r1,start0,extent0);
+			
+			
+			if(!Double.isNaN(start1)){ // there is two holes
+				brush.setAffineTexture(0.5f,  0.25f);
+				brush.segment(ellipsePoint(m, ev0, ev1, r0, r1, start0+extent0),
+						ellipsePoint(m, ev0, ev1, r0, r1, start1));
+				brush.arcEllipse(m, ev0, ev1, r0, r1,start1,extent1);
+				brush.setAffineTexture(0.5f,  0.25f);
+				brush.segment(ellipsePoint(m, ev0, ev1, r0, r1, start1+extent1),
+						ellipsePoint(m, ev0, ev1, r0, r1, start0));
+			}else{
+				brush.setAffineTexture(0.5f,  0.25f);
+				brush.segment(ellipsePoint(m, ev0, ev1, r0, r1, start0+extent0),
+						ellipsePoint(m, ev0, ev1, r0, r1, start0));
+			}
+			
+		}else{ // no hole
+			super.updateEllipse(brush);
+		}
+		
+		//updateSectorSegments(brush, conic.getConicPartType(), m, ev0, ev1, r0, r1, start0, start0+extent0);
+	}
+	
+	private Coords ellipsePoint(Coords m, Coords ev0, Coords ev1, double r0, double r1, double parameter){
+		return m.add(ev0.mul(r0*Math.cos(parameter))).add(ev1.mul(r1*Math.sin(parameter)));
 	}
 
-	private static void updateSectorSegments(PlotterBrush brush, int type, Coords m, Coords ev0, Coords ev1, double r0, double r1, double start, double end){
-			
-		//if sector draws segments
-		if (type==GeoConicNDConstants.CONIC_PART_SECTOR){
-			brush.setAffineTexture(0.5f,  0.25f);
-			brush.segment(m, m.add(ev0.mul(r0*Math.cos(start))).add(ev1.mul(r1*Math.sin(start))));
-			brush.segment(m, m.add(ev0.mul(r0*Math.cos(end))).add(ev1.mul(r1*Math.sin(end))));
-		}
-	}
 	
 	@Override
 	protected void updateEllipse(PlotterSurface surface){
