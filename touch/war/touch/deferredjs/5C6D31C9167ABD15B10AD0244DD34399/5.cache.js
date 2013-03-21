e2a9724b553b9b25e2d4816dcf166a431e6bfa88
Zbyněk Konečný <zbynek@geogebra.org>
2013-03-21 14:06:43 +0000

    Removed compiled Touch files from SVN, attempt to ignore them
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@24199 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- touch/war/touch/deferredjs/5C6D31C9167ABD15B10AD0244DD34399/5.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.touch.runAsyncCallback5('cQc(4780,1,a5p);_.Td=function n0n(){this.a.g=new Lci;nci(this.a);s5f(this.a.k).vI().it()};h8p(qt)(5);\n//@ sourceURL=5.js\n')
--- touch/war/touch/deferredjs/5C6D31C9167ABD15B10AD0244DD34399/8.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.touch.runAsyncCallback8('cQc(4783,1,a5p);_.Td=function C0n(){this.a.d=new Hci;nci(this.a);s5f(this.a.k).vI().it()};h8p(qt)(8);\n//@ sourceURL=8.js\n')
