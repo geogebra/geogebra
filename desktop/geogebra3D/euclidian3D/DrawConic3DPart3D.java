50e895f713c59b7da4bee955c90d3828251cbe75
Mathieu Blossier <mathieublossier@free.fr>
2013-04-09 19:05:27 +0000

    Use local fields
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@24601 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra3D/euclidian3D/DrawConic3DPart3D.java
+++ desktop/geogebra3D/euclidian3D/DrawConic3DPart3D.java
@@ -101,23 +101,17 @@ public class DrawConic3DPart3D extends DrawConic3D {
 		
 		if(!Double.isNaN(start0)){ // there is at least one hole
 			
-			GeoConic3DPart conic = (GeoConic3DPart) getGeoElement();
-			Coords m = conic.getMidpoint3D();
-			Coords ev0 = conic.getEigenvec3D(0); 
-			Coords ev1 = conic.getEigenvec3D(1);
-			double r0 = conic.getHalfAxis(0);
-			double r1 = conic.getHalfAxis(1);
 			
-			surface.ellipsePart(m, ev0, ev1, r0, r1, start0, extent0, false);
+			surface.ellipsePart(m, ev1, ev2, e1, e2, start0, extent0, false);
 
 			
 			if(!Double.isNaN(start1)){ // there is two holes
-				surface.ellipsePart(m, ev0, ev1, r0, r1, start1, extent1, false);
+				surface.ellipsePart(m, ev1, ev2, e1, e2, start1, extent1, false);
 				surface.drawQuad(
-						ellipsePoint(m, ev0, ev1, r0, r1, start0), 
-						ellipsePoint(m, ev0, ev1, r0, r1, start0+extent0), 
-						ellipsePoint(m, ev0, ev1, r0, r1, start1), 
-						ellipsePoint(m, ev0, ev1, r0, r1, start1+extent1) 
+						ellipsePoint(m, ev1, ev2, e1, e2, start0), 
+						ellipsePoint(m, ev1, ev2, e1, e2, start0+extent0), 
+						ellipsePoint(m, ev1, ev2, e1, e2, start1), 
+						ellipsePoint(m, ev1, ev2, e1, e2, start1+extent1) 
 						);
 			}
 			
