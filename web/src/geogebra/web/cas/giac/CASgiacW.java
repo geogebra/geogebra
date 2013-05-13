ac0a8cb758e65e7a54331415ec0ecd78ceef6ad1
Michael Borcherds <michael@geogebra.org>
2013-05-13 14:32:19 +0000

    [giac] remove 3e-2 -> 3E-2 hack (no longer needed)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@25351 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/cas/giac/CASgiacW.java
+++ web/src/geogebra/web/cas/giac/CASgiacW.java
@@ -49,7 +49,9 @@ public class CASgiacW extends CASgiac implements geogebra.common.cas.Evaluate {
 
 			// convert Giac's scientific notation from e.g. 3.24e-4 to
 			// 3.2E-4
-			ret = parserTools.convertScientificFloatNotation(ret);
+			
+			// not needed, Giac now outputs E
+			//ret = parserTools.convertScientificFloatNotation(ret);
 
 			return ret;
 		//} catch (TimeoutException toe) {
