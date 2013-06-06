85549028975bfc13b921f21d66088f8ab04e58b3
Zoltán Kovács <zoltan@geogebra.org>
2013-06-06 21:18:19 +0000

    Fixing typo
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@25812 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- test/src/test/java/geogebra/cas/GeoGebraCasIntegrationTest.java
+++ test/src/test/java/geogebra/cas/GeoGebraCasIntegrationTest.java
@@ -1275,7 +1275,7 @@ public class GeoGebraCasIntegrationTest {
 	public void GCD_Variables_1() {
 		// Disabling temporarily for giac Linux jni:
 		// t("GCD[{x^2 + 4 * x + 4, x^2 - x - 6, x^3 - 4 * x^2 - 3 * x + 18}]",
-				"x + 2");
+		//		"x + 2");
 	}
 	
 
