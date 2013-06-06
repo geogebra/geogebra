910586cdc73fc8ba7cad8be3a5f047eb11e7b7da
Zbyněk Konečný <zbynek@geogebra.org>
2013-06-06 14:57:35 +0000

    Fix for PreviousPrime[2] in GIAC
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@25798 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/cas/giac/Ggb2giac.java
+++ common/src/geogebra/common/cas/giac/Ggb2giac.java
@@ -302,7 +302,7 @@ public class Ggb2giac {
 						"exp(-(%0))*sum ((%0)^k/k!,k,0,floor(%1)) " +
 				"else (%0)^(%1)/factorial(floor(%1))*exp(-%0) fi");
 		p("PreviousPrime.1",
-				"if (%0 > 1) then prevprime(%0) else 0/0 fi");
+				"if (%0 > 2) then prevprime(%0) else 0/0 fi");
 		p("PrimeFactors.1",
 				"ifactors(%0)");
 		// normal() makes sure answer is expanded
