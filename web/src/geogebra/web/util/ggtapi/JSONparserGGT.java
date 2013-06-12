2be249f5985cd14f2c6d5659a43f111b94105a2d
Zbyněk Konečný <zbynek@geogebra.org>
2013-06-12 14:54:38 +0000

    Hack GGBAPI parser to accept single material
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@25947 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/util/ggtapi/JSONparserGGT.java
+++ web/src/geogebra/web/util/ggtapi/JSONparserGGT.java
@@ -5,9 +5,9 @@ import geogebra.common.main.App;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONValue;
 
 /**
  * Utility Class for Parsing POJO's to JSON and vice versa
@@ -21,7 +21,7 @@ public class JSONparserGGT
 	{
 		List<Material> result = new ArrayList<Material>();
 
-		JSONArray materialsArray = null;
+		JSONValue materialsArray = null;
 
 		if (response != null)
 		{
@@ -38,7 +38,7 @@ public class JSONparserGGT
 
 				if (materialsObject.containsKey(("item")))
 				{
-					materialsArray = materialsObject.get("item").isArray();
+					materialsArray = materialsObject.get("item");
 				}
 				else
 				{
@@ -55,33 +55,49 @@ public class JSONparserGGT
 		{
 			// Response String was null
 		}
-
-		if (materialsArray != null)
+		//0  materials
+		if(materialsArray == null){
+			return result;
+		}
+		// >1 materials
+		if (materialsArray.isArray() != null)
 		{
-			for (int i = 0; i < materialsArray.size(); i++)
+			for (int i = 0; i < materialsArray.isArray().size(); i++)
 			{
 				JSONObject obj = materialsArray.isArray().get(i).isObject();
-
-				Material.MaterialType type = Material.MaterialType.valueOf(obj.get("type").isString().stringValue());
-				int ID = Integer.parseInt(obj.get("id").isString().stringValue());
-
-				Material material = new Material(ID, type);
-
-				material.setTitle(obj.get("title").isString().stringValue());
-				material.setDescription(obj.get("description").isString().stringValue());
-				material.setTimestamp(Long.parseLong(obj.get("timestamp").isString().stringValue()));
-				material.setAuthor(obj.get("author").isString().stringValue());
-				material.setAuthorURL(obj.get("author_url").isString().stringValue());
-				material.setURL(obj.get("url").isString().stringValue());
-				material.setURLdirect(obj.get("url_direct").isString().stringValue());
-				material.setThumbnail(obj.get("thumbnail").isString().stringValue());
-				material.setLanguage(obj.get("language").isString().stringValue());
-				material.setFeatured(Boolean.parseBoolean((obj.get("featured").isString().stringValue())));
-				material.setLikes(Integer.parseInt(obj.get("likes").isString().stringValue()));
-
-				result.add(material);
+				addToArray(result, obj);
+				
 			}
+		}
+		//1 material
+		else if(materialsArray.isObject()!=null){
+			addToArray(result,materialsArray.isObject());
 		}
 		return result;
 	}
+
+	private static void addToArray(List<Material> result, JSONObject obj) {
+		if(obj == null){
+			return;
+		}
+		Material.MaterialType type = Material.MaterialType.valueOf(obj.get("type").isString().stringValue());
+		int ID = Integer.parseInt(obj.get("id").isString().stringValue());
+
+		Material material = new Material(ID, type);
+
+		material.setTitle(obj.get("title").isString().stringValue());
+		material.setDescription(obj.get("description").isString().stringValue());
+		material.setTimestamp(Long.parseLong(obj.get("timestamp").isString().stringValue()));
+		material.setAuthor(obj.get("author").isString().stringValue());
+		material.setAuthorURL(obj.get("author_url").isString().stringValue());
+		material.setURL(obj.get("url").isString().stringValue());
+		material.setURLdirect(obj.get("url_direct").isString().stringValue());
+		material.setThumbnail(obj.get("thumbnail").isString().stringValue());
+		material.setLanguage(obj.get("language").isString().stringValue());
+		material.setFeatured(Boolean.parseBoolean((obj.get("featured").isString().stringValue())));
+		material.setLikes(Integer.parseInt(obj.get("likes").isString().stringValue()));
+
+		result.add(material);
+	    
+    }
 }
