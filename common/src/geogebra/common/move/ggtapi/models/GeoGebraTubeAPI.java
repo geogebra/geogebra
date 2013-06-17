f0ae7ac571c6aefc7b30b1d42fd823c013f00500
Zbyněk Konečný <zbynek@geogebra.org>
2013-06-17 10:05:52 +0000

    Make Touch compile again
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@26061 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/move/ggtapi/models/GeoGebraTubeAPI.java
+++ common/src/geogebra/common/move/ggtapi/models/GeoGebraTubeAPI.java
@@ -15,6 +15,10 @@ public class GeoGebraTubeAPI {
 	 */
 	protected static final String secure_test_url = "https://test.geogebratube.org:8084/api/json.php";
 	/**
+	 * Public url (no SSL)
+	 */
+	protected static final String url = "http://www.geogebratube.org/api/json.php";
+	/**
 	 * Instance of the new GeoGebraTube API D/W/T
 	 */
 	protected static GeoGebraTubeAPI instance;
