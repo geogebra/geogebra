382bafffa814cc99902f0352a09f775d50c17b7a
Michael Borcherds <michael@geogebra.org>
2013-06-19 07:14:08 +0000

    [giac] removing wrapping with caseval("...")
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@26133 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra/cas/giac/CASgiacD.java
+++ desktop/geogebra/cas/giac/CASgiacD.java
@@ -270,7 +270,7 @@ public class CASgiacD extends CASgiac implements Evaluate {
 	 * @return String from Giac
 	 */
 	String evalRaw(String exp) {
-		gen g = new gen("caseval(\"" + exp + "\")", C);
+		gen g = new gen(exp, C);
 		g = g.eval(1, C);
 		String ret = g.print(C);
 		
