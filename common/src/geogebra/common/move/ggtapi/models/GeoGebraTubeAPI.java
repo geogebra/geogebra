2d1edaddc886447d9ee2c65efbfaa5385244bde7
Matthias Meisinger <matthias.meisinger@geogebra.org>
2013-06-24 07:05:02 +0000

    fixed duplicate field url
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@26246 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/move/ggtapi/models/GeoGebraTubeAPI.java
+++ common/src/geogebra/common/move/ggtapi/models/GeoGebraTubeAPI.java
@@ -10,12 +10,15 @@ public class GeoGebraTubeAPI {
 	 * The Standard Result Quantity
 	 */
 	public static final int STANDARD_RESULT_QUANTITY = 10;
+
 	/**
 	 * Secure test url
 	 */
-	protected static final String secure_test_url = "https://test.geogebratube.org:8084/api/json.php";
+//	protected static final String secure_test_url = "https://test.geogebratube.org:8084/api/json.php";
+
 	/**
 	 * Public url (no SSL)
+	 * DO NOT CHANGE!
 	 */
 	protected static final String url = "http://www.geogebratube.org/api/json.php";
 	/**
