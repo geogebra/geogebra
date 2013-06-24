e3c362285cc40801320c9b2a2b4e813190e2cf04
Matthias Meisinger <matthias.meisinger@geogebra.org>
2013-06-24 15:39:43 +0000

    removed dialog to change worksheet title, instead direct manipulation is possible (gui design pending).
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@26258 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- touch/src/geogebra/touch/gui/elements/header/TabletHeaderPanel.java
+++ touch/src/geogebra/touch/gui/elements/header/TabletHeaderPanel.java
@@ -3,24 +3,27 @@ package geogebra.touch.gui.elements.header;
 import geogebra.touch.TouchApp;
 import geogebra.touch.gui.ResizeListener;
 import geogebra.touch.gui.TabletGUI;
-import geogebra.touch.gui.dialogs.InputDialog;
-import geogebra.touch.gui.dialogs.InputDialog.DialogType;
 import geogebra.touch.model.TouchModel;
 import geogebra.touch.utils.TitleChangedListener;
 
 import com.google.gwt.dom.client.Style.BorderStyle;
 import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.logical.shared.CloseEvent;
-import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
+import com.google.gwt.event.dom.client.FocusEvent;
+import com.google.gwt.event.dom.client.FocusHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HeaderPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.ValueBoxBase.TextAlignment;
 
 /**
  * Extends from {@link HeaderPanel}.
@@ -28,10 +31,9 @@ import com.google.gwt.user.client.ui.PopupPanel;
 public class TabletHeaderPanel extends HorizontalPanel implements ResizeListener
 {
 	private TabletHeaderPanelLeft leftHeader;
-	Button titleButton;
+	TextBox worksheetTitle;
 	private TabletHeaderPanelRight rightHeader;
 
-	protected InputDialog dialog;
 	TouchApp app;
 
 	public TabletHeaderPanel(TabletGUI tabletGUI, final TouchApp app, TouchModel touchModel)
@@ -39,62 +41,64 @@ public class TabletHeaderPanel extends HorizontalPanel implements ResizeListener
 		this.setStyleName("headerbar");
 		this.setWidth(Window.getClientWidth() + "px");
 
-		//this.getElement().getStyle().setBackgroundColor(TabletGUI.getBackgroundColor().toString());
-		//this.getElement().getStyle().setBorderColor(GColor.BLACK.toString());
-		//this.getElement().getStyle().setBorderWidth(TabletGUI.FOOTER_BORDER_WIDTH, Unit.PX);
-		//this.getElement().getStyle().setBorderStyle(BorderStyle.SOLID);
-
 		this.app = app;
-		this.dialog = new InputDialog(this.app, DialogType.Title, tabletGUI);
 		this.leftHeader = new TabletHeaderPanelLeft(tabletGUI, app, touchModel);
 		this.leftHeader.setStyleName("headerLeft");
 
-		this.titleButton = new Button(app.getConstructionTitle());
+		this.worksheetTitle = new TextBox();
+		this.worksheetTitle.setText(app.getConstructionTitle());
+		this.worksheetTitle.setAlignment(TextAlignment.CENTER);
 
-		app.addTitleChangedListener(new TitleChangedListener()
+		this.app.addTitleChangedListener(new TitleChangedListener()
 		{
 			@Override
 			public void onTitleChange(String title)
 			{
-				TabletHeaderPanel.this.titleButton.setText(title);
+				TabletHeaderPanel.this.worksheetTitle.setText(title);
 			}
 		});
 
 		this.rightHeader = new TabletHeaderPanelRight(app);
 		this.rightHeader.setStyleName("headerRight");
 
-		this.titleButton.addDomHandler(new ClickHandler()
+		this.worksheetTitle.addKeyDownHandler(new KeyDownHandler()
 		{
 			@Override
-			public void onClick(ClickEvent event)
+			public void onKeyDown(KeyDownEvent event)
 			{
-				event.preventDefault();
-
-				TabletHeaderPanel.this.dialog.setText(TabletHeaderPanel.this.titleButton.getText());
-				TabletHeaderPanel.this.dialog.show();
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER)
+				{
+					TabletHeaderPanel.this.app.setConstructionTitle(TabletHeaderPanel.this.worksheetTitle.getText());
+					TabletHeaderPanel.this.worksheetTitle.setFocus(false);
+				}
+				else if(event.getNativeKeyCode() == KeyCodes.KEY_ESCAPE)
+				{
+					TabletHeaderPanel.this.worksheetTitle.setText(TabletHeaderPanel.this.app.getConstructionTitle());
+					TabletHeaderPanel.this.worksheetTitle.setFocus(false);
+				}
 			}
-		}, ClickEvent.getType());
-
-		this.dialog.addCloseHandler(new CloseHandler<PopupPanel>()
-		{
-
+		});
+		
+		this.worksheetTitle.addMouseOutHandler(new MouseOutHandler()
+		{			
 			@Override
-			public void onClose(CloseEvent<PopupPanel> event)
+			public void onMouseOut(MouseOutEvent event)
 			{
-				TabletHeaderPanel.this.app.setConstructionTitle(TabletHeaderPanel.this.dialog.getInput());
+				TabletHeaderPanel.this.worksheetTitle.setText(TabletHeaderPanel.this.app.getConstructionTitle());
+				TabletHeaderPanel.this.worksheetTitle.setFocus(false);
 			}
 		});
 
 		this.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
 		this.add(this.leftHeader);
 
-		this.titleButton.setPixelSize(Window.getClientWidth() - 396, 61);
+		this.worksheetTitle.setPixelSize(Window.getClientWidth() - 396, 61);
 		this.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
-		this.add(this.titleButton);
+		this.add(this.worksheetTitle);
 
-		this.titleButton.getElement().getStyle().setBackgroundImage("none");
-		this.titleButton.getElement().getStyle().setBorderStyle(BorderStyle.NONE);
-		this.titleButton.getElement().getStyle().setFontSize(35, Unit.PX);
+		this.worksheetTitle.getElement().getStyle().setBackgroundImage("none");
+		this.worksheetTitle.getElement().getStyle().setBorderStyle(BorderStyle.NONE);
+		this.worksheetTitle.getElement().getStyle().setFontSize(35, Unit.PX);
 
 		this.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
 		this.add(this.rightHeader);
@@ -105,15 +109,11 @@ public class TabletHeaderPanel extends HorizontalPanel implements ResizeListener
 	public void onResize(ResizeEvent event)
 	{
 		this.setWidth(event.getWidth() + "px");
-		this.titleButton.setPixelSize(Window.getClientWidth() - 396, 61);
-		// this.titleButton.setWidth(Window.getClientWidth() -
-		// this.leftHeader.getOffsetWidth() - this.rightHeader.getOffsetWidth() +
-		// "px");
+		this.worksheetTitle.setPixelSize(Window.getClientWidth() - 396, 61);
 	}
 
 	public void setLabels()
 	{
-		this.dialog.setLabels();
 		this.leftHeader.setLabels();
 	}
 }
