cf315143cbba0186eedff7aeafa48169c2531efd
Zbyněk Konečný <zbynek@geogebra.org>
2013-07-03 14:36:50 +0000

    Whitespace changes in Touch stylebar
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@26468 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- touch/src/geogebra/touch/utils/StylingBarEntries.java
+++ touch/src/geogebra/touch/utils/StylingBarEntries.java
@@ -8,12 +8,16 @@ import org.vectomatic.dom.svg.ui.SVGResource;
 
 public enum StylingBarEntries
 {
-	Point(GColor.blue, new SVGResource[] { CommonResources.INSTANCE.color(), CommonResources.INSTANCE.label() }), DependentPoints(GColor.darkGray,
-	    new SVGResource[] { CommonResources.INSTANCE.color(), CommonResources.INSTANCE.label() }), Line(GColor.black,
-	    new SVGResource[] { CommonResources.INSTANCE.color(), CommonResources.INSTANCE.properties_default() }), Polygon(new GColorW(153, 51, 0),
-	    new SVGResource[] { CommonResources.INSTANCE.color(), CommonResources.INSTANCE.properties_default() }), Move(null, 
+	Point(GColor.blue, new SVGResource[] { CommonResources.INSTANCE.color(), CommonResources.INSTANCE.label() }), 
+	DependentPoints(GColor.darkGray,
+	    new SVGResource[] { CommonResources.INSTANCE.color(), CommonResources.INSTANCE.label() }), 
+	Line(GColor.black,
+	    new SVGResource[] { CommonResources.INSTANCE.color(), CommonResources.INSTANCE.properties_default() }), 
+	Polygon(new GColorW(153, 51, 0),
+	    new SVGResource[] { CommonResources.INSTANCE.color(), CommonResources.INSTANCE.properties_default() }), 
+	Move(null, 
 	    new SVGResource[] { CommonResources.INSTANCE.show_or_hide_the_axes(),  CommonResources.INSTANCE.show_or_hide_the_grid()}), 
-	    Angle(GColor.green, new SVGResource[] { CommonResources.INSTANCE.color(), CommonResources.INSTANCE.properties_default() });
+	Angle(GColor.green, new SVGResource[] { CommonResources.INSTANCE.color(), CommonResources.INSTANCE.properties_default() });
 
 	GColor defaultColor;
 	SVGResource[] entry;
