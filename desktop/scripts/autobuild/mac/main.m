38d93215f4615eb8cf0da2bd7e4c63abd1de0487
Zoltán Kovács <zoltan@geogebra.org>
2013-07-04 09:11:07 +0000

    Attempt to make the Mac installers work (almost fully tested)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@26491 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ desktop/scripts/autobuild/mac/main.m
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2012, Oracle and/or its affiliates. All rights reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+// Original code is modified by International GeoGebra Institute (IGI)
+// to make external libraries JOGL and giac work properly.
+// Modifications by Zoltan Kovacs <zoltan@geogebra.org>
+
+#import <Cocoa/Cocoa.h>
+#include <dlfcn.h>
+#include <jni.h>
+
+#define JAVA_LAUNCH_ERROR "JavaLaunchError"
+
+#define JVM_RUNTIME_KEY "JVMRuntime"
+#define JVM_MAIN_CLASS_NAME_KEY "JVMMainClassName"
+#define JVM_OPTIONS_KEY "JVMOptions"
+#define JVM_ARGUMENTS_KEY "JVMArguments"
+
+#define UNSPECIFIED_ERROR "An unknown error occurred."
+
+#define APP_ROOT_PREFIX "$APP_ROOT"
+
+#define LIBJLI_DYLIB "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/lib/jli/libjli.dylib"
+
+typedef int (JNICALL *JLI_Launch_t)(int argc, char ** argv,
+                                    int jargc, const char** jargv,
+                                    int appclassc, const char** appclassv,
+                                    const char* fullversion,
+                                    const char* dotversion,
+                                    const char* pname,
+                                    const char* lname,
+                                    jboolean javaargs,
+                                    jboolean cpwildcard,
+                                    jboolean javaw,
+                                    jint ergo);
+
+int launch(char *);
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+    int result;
+    @try {
+        launch(argv[0]);
+        result = 0;
+    } @catch (NSException *exception) {
+        NSAlert *alert = [[NSAlert alloc] init];
+        [alert setAlertStyle:NSCriticalAlertStyle];
+        [alert setMessageText:[exception reason]];
+        [alert runModal];
+
+        result = 1;
+    }
+
+    [pool drain];
+
+    return result;
+}
+
+int launch(char *commandName) {
+    // Get the main bundle
+    NSBundle *mainBundle = [NSBundle mainBundle];
+
+    // Get the main bundle's info dictionary
+    NSDictionary *infoDictionary = [mainBundle infoDictionary];
+
+    // Locate the JLI_Launch() function
+    NSString *runtime = [infoDictionary objectForKey:@JVM_RUNTIME_KEY];
+
+    const char *libjliPath = NULL;
+    if (runtime != nil) {
+        NSString *runtimePath = [[[NSBundle mainBundle] builtInPlugInsPath] stringByAppendingPathComponent:runtime];
+        libjliPath = [[runtimePath stringByAppendingPathComponent:@"Contents/Home/jre/lib/jli/libjli.dylib"] fileSystemRepresentation];
+    } else {
+        libjliPath = LIBJLI_DYLIB;
+    }
+
+    void *libJLI = dlopen(libjliPath, RTLD_LAZY);
+
+    JLI_Launch_t jli_LaunchFxnPtr = NULL;
+    if (libJLI != NULL) {
+        jli_LaunchFxnPtr = dlsym(libJLI, "JLI_Launch");
+    }
+
+    if (jli_LaunchFxnPtr == NULL) {
+        [[NSException exceptionWithName:@JAVA_LAUNCH_ERROR
+            reason:NSLocalizedString(@"JRELoadError", @UNSPECIFIED_ERROR)
+            userInfo:nil] raise];
+    }
+
+    // Get the main class name
+    NSString *mainClassName = [infoDictionary objectForKey:@JVM_MAIN_CLASS_NAME_KEY];
+    if (mainClassName == nil) {
+        [[NSException exceptionWithName:@JAVA_LAUNCH_ERROR
+            reason:NSLocalizedString(@"MainClassNameRequired", @UNSPECIFIED_ERROR)
+            userInfo:nil] raise];
+    }
+
+    // Set the class path
+    NSString *mainBundlePath = [mainBundle bundlePath];
+    NSString *javaPath = [mainBundlePath stringByAppendingString:@"/Contents/Java"];
+    NSString *classPath = @"-Djava.class.path=geogebra-jogl2.jar";
+    NSLog ( @"Setting hardcoded classpath");
+
+    // Set the working directory to the Java path
+    chdir([javaPath UTF8String]);
+
+    NSFileManager *defaultFileManager = [NSFileManager defaultManager];
+    NSArray *javaDirectoryContents = [defaultFileManager contentsOfDirectoryAtPath:javaPath error:nil];
+    if (javaDirectoryContents == nil) {
+        [[NSException exceptionWithName:@JAVA_LAUNCH_ERROR
+            reason:NSLocalizedString(@"JavaDirectoryNotFound", @UNSPECIFIED_ERROR)
+            userInfo:nil] raise];
+    }
+
+    // Set the library path (not really required)
+    NSString *libraryPath = @"-Djava.library.path=.";
+
+    // Get the VM options
+    NSArray *options = [infoDictionary objectForKey:@JVM_OPTIONS_KEY];
+    if (options == nil) {
+        options = [NSArray array];
+    }
+
+    // Get the application arguments
+    NSArray *arguments = [infoDictionary objectForKey:@JVM_ARGUMENTS_KEY];
+    if (arguments == nil) {
+        arguments = [NSArray array];
+    }
+
+    // Initialize the arguments to JLI_Launch()
+    int argc = 1 + [options count] + 2 + [arguments count] + 1;
+    char *argv[argc];
+
+    int i = 0;
+    argv[i++] = commandName;
+    argv[i++] = strdup([classPath UTF8String]);
+    argv[i++] = strdup([libraryPath UTF8String]);
+
+    for (NSString *option in options) {
+        option = [option stringByReplacingOccurrencesOfString:@APP_ROOT_PREFIX withString:[mainBundle bundlePath]];
+        argv[i++] = strdup([option UTF8String]);
+    }
+
+    argv[i++] = strdup([mainClassName UTF8String]);
+
+    for (NSString *argument in arguments) {
+        argument = [argument stringByReplacingOccurrencesOfString:@APP_ROOT_PREFIX withString:[mainBundle bundlePath]];
+        argv[i++] = strdup([argument UTF8String]);
+    }
+
+    int j;
+    for (j = 0; j < i; ++j) {
+        NSLog( @"%i %s", j, argv[j]);
+    }
+
+    // Invoke JLI_Launch()
+    return jli_LaunchFxnPtr(argc, argv,
+                            0, NULL,
+                            0, NULL,
+                            "",
+                            "",
+                            "java",
+                            "java",
+                            FALSE,
+                            FALSE,
+                            FALSE,
+                            0);
+}
