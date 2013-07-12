4ac9ea748fb430eba11beb9414cb56ad327c6ffc
Zbyněk Konečný <zbynek@geogebra.org>
2013-07-12 13:47:59 +0000

    Code format
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@26785 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- touch/src/geogebra/touch/gui/laf/DefaultLAF.java
+++ touch/src/geogebra/touch/gui/laf/DefaultLAF.java
@@ -1,6 +1,5 @@
 package geogebra.touch.gui.laf;
 
-
 import geogebra.touch.FileManagerM;
 import geogebra.touch.TouchApp;
 import geogebra.touch.gui.TabletGUI;
@@ -16,90 +15,79 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.DomEvent.Type;
 import com.google.gwt.event.shared.EventHandler;
 
-public class DefaultLAF implements LookAndFeel
-{
+public class DefaultLAF implements LookAndFeel {
 	private TabletHeaderPanel hp;
 
 	@Override
-	public void buildHeader(TabletGUI gui, TouchApp app, TouchModel touchModel, FileManagerM fm)
-	{
+	public void buildHeader(TabletGUI gui, TouchApp app, TouchModel touchModel,
+			FileManagerM fm) {
 		this.hp = new TabletHeaderPanel(gui, app, touchModel, fm);
 		gui.setHeaderWidget(this.hp);
 		gui.addResizeListener(this.hp);
 	}
 
 	@Override
-	public void setTitle(String title)
-	{
+	public void setTitle(String title) {
 		this.hp.setTitle(title);
 	}
 
 	@Override
-	public int getPanelsHeight()
-	{
+	public int getPanelsHeight() {
 		return 122;
 	}
 
 	@Override
-  public TabletHeaderPanel getTabletHeaderPanel()
-	{
+	public TabletHeaderPanel getTabletHeaderPanel() {
 		return this.hp;
 	}
 
 	@Override
-	public int getAppBarHeight()
-	{
+	public int getAppBarHeight() {
 		return 62;
 	}
 
 	@Override
-	public DefaultIcons getIcons()
-	{
+	public DefaultIcons getIcons() {
 		return DefaultIcons.INSTANCE;
 	}
-	
+
 	@Override
-  public Type getStylBarEventType()
-  {
-	  return ClickEvent.getType();
-  }
-	
+	public Type getStylBarEventType() {
+		return ClickEvent.getType();
+	}
+
 	@Override
-  public EventHandler getStyleBarHandlerShowHide(final StylingBar stylingBar, final EuclidianViewPanel euclidianViewPanel)
-  {
-	  return new ClickHandler()
-		{			
+	public EventHandler getStyleBarHandlerShowHide(final StylingBar stylingBar,
+			final EuclidianViewPanel euclidianViewPanel) {
+		return new ClickHandler() {
 			@Override
-			public void onClick(final ClickEvent event)
-			{
+			public void onClick(final ClickEvent event) {
 				stylingBar.onTouchStartShowHide(event, euclidianViewPanel);
 			}
 		};
-  }
+	}
 
 	@Override
-  public EventHandler getStyleBarButtonHandler(final StylingBar stylingBar, final StandardImageButton newButton, final String process)
-  {
-	  return new ClickHandler()
-		{			
+	public EventHandler getStyleBarButtonHandler(final StylingBar stylingBar,
+			final StandardImageButton newButton, final String process) {
+		return new ClickHandler() {
 			@Override
-			public void onClick(final ClickEvent event)
-			{
-				stylingBar.onTouchStartStyleBarButton(event, newButton, process);
+			public void onClick(final ClickEvent event) {
+				stylingBar
+						.onTouchStartStyleBarButton(event, newButton, process);
 			}
 		};
-  }
+	}
 
 	@Override
-  public EventHandler getOptionalButtonHandler(final StylingBar stylingBar, final StandardImageButton standardImageButton, final OptionType type)
-  {
-		return new ClickHandler()
-		{			
+	public EventHandler getOptionalButtonHandler(final StylingBar stylingBar,
+			final StandardImageButton standardImageButton, final OptionType type) {
+		return new ClickHandler() {
 			@Override
-			public void onClick(final ClickEvent event)
-			{
-				stylingBar.onTouchStartOptionalButton(event, standardImageButton, type);
+			public void onClick(final ClickEvent event) {
+				stylingBar.onTouchStartOptionalButton(event,
+						standardImageButton, type);
 			}
 		};
-  }
+	}
 }
