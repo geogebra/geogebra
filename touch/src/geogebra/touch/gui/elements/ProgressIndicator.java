3789cf4d5adc0fe794da820b86636fab66313dc0
Matthias Meisinger <matthias.meisinger@geogebra.org>
2013-07-18 22:54:58 +0000

    progressIndicator
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@26995 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- touch/src/geogebra/touch/gui/elements/ProgressIndicator.java
+++ touch/src/geogebra/touch/gui/elements/ProgressIndicator.java
@@ -1,17 +1,22 @@
 package geogebra.touch.gui.elements;
 
-import geogebra.touch.TouchEntryPoint;
+import geogebra.touch.gui.laf.DefaultResources;
 
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.PopupPanel;
 
 public class ProgressIndicator extends PopupPanel {
 
-	private static Image progressIndicator = new Image(TouchEntryPoint
-			.getLookAndFeel().getIcons().progressIndicator());
+	private static Image progressIndicator = new Image(DefaultResources.INSTANCE.progressIndicator());
 
+	static
+	{
+		progressIndicator.setPixelSize(50, 50);
+	}
+	
 	@Override
 	public void show() {
+		this.setPixelSize(50, 50);
 		this.setWidget(ProgressIndicator.progressIndicator);
 		super.show();
 		this.center();
