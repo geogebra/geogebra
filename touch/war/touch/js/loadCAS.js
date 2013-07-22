7cb0b1ddbe8b96dd02c9c5a2cb508acc2095ae3a
Matthias Meisinger <matthias.meisinger@geogebra.org>
2013-07-22 08:28:36 +0000

    accidentally commited output folder
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@27045 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- touch/war/touch/js/loadCAS.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("message", function(event) {
-//	var msgobject = JSON.parse(msg);
-//	if (typeof msgObject.loadCAS !== 'undefined') {
-//	  msgObject.loadCAS();
-//	}
-    self.postMessage('TODO: load CAS msg from worker ' + event.data);
-});
