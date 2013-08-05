2a6284423237628bd0453ba69d67c8e744d32fb5
Zbyněk Konečný <zbynek@geogebra.org>
2013-08-05 14:50:53 +0000

    Worksheet GUI working with test worksheet
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@27367 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- touch/src/geogebra/touch/TouchEntryPoint.java
+++ touch/src/geogebra/touch/TouchEntryPoint.java
@@ -35,171 +35,181 @@ import com.googlecode.gwtphonegap.client.event.BackButtonPressedHandler;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class TouchEntryPoint implements EntryPoint {
-    public static ProgressIndicator progressIndicator = new ProgressIndicator();
+	public static ProgressIndicator progressIndicator = new ProgressIndicator();
 
-    static TabletDeckLayoutPanel appWidget = new TabletDeckLayoutPanel();
-    static TabletGUI tabletGUI = new TabletGUI();
-    static BrowseGUI browseGUI;
-    static WorksheetGUI worksheetGUI;
+	static TabletDeckLayoutPanel appWidget = new TabletDeckLayoutPanel();
+	static TabletGUI tabletGUI = new TabletGUI();
+	static BrowseGUI browseGUI;
+	static WorksheetGUI worksheetGUI;
 
-    static final PhoneGap phoneGap = (PhoneGap) GWT.create(PhoneGap.class);
-    private static LookAndFeel laf;
+	static final PhoneGap phoneGap = (PhoneGap) GWT.create(PhoneGap.class);
+	private static LookAndFeel laf;
 
-    final static TouchApp app = new TouchApp(TouchEntryPoint.tabletGUI);
+	final static TouchApp app = new TouchApp(TouchEntryPoint.tabletGUI);
 
-    public static void allowEditing(boolean b) {
-	tabletGUI.allowEditing(b);
-    }
-
-    public static LookAndFeel getLookAndFeel() {
-	return TouchEntryPoint.laf;
-    }
-
-    public static void goBack() {
-	// is Dialog open? -> close dialog
-	if (tabletGUI.getTouchModel().getGuiModel().isDialogShown()) {
-	    tabletGUI.getTouchModel().getGuiModel().closeActiveDialog();
-	} else {
-	    // else go to last view in history
-	    if (!appWidget.goBack()) {
-		// if history is empty -> close app
-		phoneGap.exitApp();
-	    }
+	public static void allowEditing(boolean b) {
+		tabletGUI.allowEditing(b);
 	}
 
-	laf.updateUndoSaveButtons();
+	public static LookAndFeel getLookAndFeel() {
+		return TouchEntryPoint.laf;
+	}
 
-	Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-	    @Override
-	    public void execute() {
-		tabletGUI.updateViewSizes(tabletGUI.isAlgebraShowing());
-		tabletGUI.onResize();
-	    }
-	});
-    }
-
-    static void loadMobileAsync() {
-	GWT.runAsync(new RunAsyncCallback() {
-	    private void initPhoneGap() {
-		phoneGap.initializePhoneGap();
-		phoneGap.getEvent().getBackButton().addBackButtonPressedHandler(new BackButtonPressedHandler() {
-
-		    @Override
-		    public void onBackButtonPressed(BackButtonPressedEvent event) {
-			goBack();
-		    }
-		});
-	    }
-
-	    @Override
-	    public void onFailure(Throwable reason) {
-		// App.debug(reason);
-		reason.printStackTrace();
-	    }
-
-	    @Override
-	    public void onSuccess() {
-		ResourcesInjector.injectResources();
-		setLookAndFeel();
-
-		getLookAndFeel().setApp(app);
-		final FileManagerM fm = new FileManagerM();
-		app.setFileManager(fm);
-		app.registerSavedStateListener(TouchEntryPoint.getLookAndFeel());
-		browseGUI = new BrowseGUI(app);
-		worksheetGUI = new WorksheetGUI(app, tabletGUI);
-		TouchEntryPoint.appWidget.add(TouchEntryPoint.tabletGUI);
-		TouchEntryPoint.appWidget.add(TouchEntryPoint.browseGUI);
-		TouchEntryPoint.appWidget.add(TouchEntryPoint.worksheetGUI);
-
-		RootLayoutPanel.get().add(TouchEntryPoint.appWidget);
-
-		app.start();
-
-		TouchEntryPoint.showTabletGUI();
-
-		Window.addResizeHandler(new ResizeHandler() {
-
-		    @Override
-		    public void onResize(ResizeEvent event) {
-			// TouchEntryPoint.appWidget.setPixelSize(event.getWidth(),
-			// event.getHeight());
-		    }
-		});
-
-		tabletGUI.getContentWidget().getElement().getStyle().setOverflow(Overflow.VISIBLE);
-		app.getScriptManager().ggbOnInit();
-		// needed for testing
-		if (RootPanel.getBodyElement().getAttribute("data-param-ggbbase64").length() > 0) {
-		    app.getGgbApi().setBase64(RootPanel.getBodyElement().getAttribute("data-param-ggbbase64"));
+	public static void goBack() {
+		// is Dialog open? -> close dialog
+		if (tabletGUI.getTouchModel().getGuiModel().isDialogShown()) {
+			tabletGUI.getTouchModel().getGuiModel().closeActiveDialog();
+		} else {
+			// else go to last view in history
+			if (!appWidget.goBack()) {
+				// if history is empty -> close app
+				phoneGap.exitApp();
+			}
 		}
 
-		this.initPhoneGap();
+		laf.updateUndoSaveButtons();
 
-		Window.enableScrolling(false);
-
-		progressIndicator.hide();
-
-	    }
-	});
-    }
-
-    public static void reloadLocalFiles() {
-	TouchEntryPoint.browseGUI.reloadLocalFiles();
-    }
-
-    protected static void setLookAndFeel() {
-	final String param = RootPanel.getBodyElement().getAttribute("data-param-laf");
-
-	if ("android".equals(param)) {
-	    laf = new AndroidLAF(TouchEntryPoint.app);
-	} else if ("apple".equals(param)) {
-	    laf = new AppleLAF(TouchEntryPoint.app);
-	} else if ("win".equals(param)) {
-	    laf = new WinLAF(TouchEntryPoint.app);
-	} else {
-	    laf = new DefaultLAF(TouchEntryPoint.app);
+		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+			@Override
+			public void execute() {
+				tabletGUI.updateViewSizes(tabletGUI.isAlgebraShowing());
+				tabletGUI.onResize();
+			}
+		});
 	}
-    }
 
-    public static void showBrowseGUI() {
-	TouchEntryPoint.appWidget.showWidget(TouchEntryPoint.browseGUI);
-	TouchEntryPoint.browseGUI.onResize();
-    }
+	static void loadMobileAsync() {
+		GWT.runAsync(new RunAsyncCallback() {
+			private void initPhoneGap() {
+				phoneGap.initializePhoneGap();
+				phoneGap.getEvent()
+						.getBackButton()
+						.addBackButtonPressedHandler(
+								new BackButtonPressedHandler() {
 
-    public static void showTabletGUI() {
-	TouchEntryPoint.appWidget.showWidget(TouchEntryPoint.tabletGUI);
+									@Override
+									public void onBackButtonPressed(
+											BackButtonPressedEvent event) {
+										goBack();
+									}
+								});
+			}
 
-	Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+			@Override
+			public void onFailure(Throwable reason) {
+				// App.debug(reason);
+				reason.printStackTrace();
+			}
 
-	    @Override
-	    public void execute() {
-		tabletGUI.updateViewSizes(tabletGUI.isAlgebraShowing());
-		tabletGUI.onResize();
-	    }
-	});
-	laf.updateUndoSaveButtons();
-    }
+			@Override
+			public void onSuccess() {
+				ResourcesInjector.injectResources();
+				setLookAndFeel();
 
-    public static void showWorksheetGUI(Material material) {
-	TouchEntryPoint.worksheetGUI.loadWorksheet(material);
-	TouchEntryPoint.appWidget.showWidget(TouchEntryPoint.worksheetGUI);
-    }
+				getLookAndFeel().setApp(app);
+				final FileManagerM fm = new FileManagerM();
+				app.setFileManager(fm);
+				app.registerSavedStateListener(TouchEntryPoint.getLookAndFeel());
+				browseGUI = new BrowseGUI(app);
+				worksheetGUI = new WorksheetGUI(app, tabletGUI);
+				TouchEntryPoint.appWidget.add(TouchEntryPoint.tabletGUI);
+				TouchEntryPoint.appWidget.add(TouchEntryPoint.browseGUI);
+				TouchEntryPoint.appWidget.add(TouchEntryPoint.worksheetGUI);
 
-    @Override
-    public void onModuleLoad() {
+				RootLayoutPanel.get().add(TouchEntryPoint.appWidget);
 
-	AnimationScheduler.get().requestAnimationFrame(new AnimationCallback() {
+				app.start();
 
-	    @Override
-	    public void execute(double timestamp) {
-		loadMobileAsync();
+				TouchEntryPoint.showTabletGUI();
 
-		// insert mathquill css
-		final String mathquillcss = GuiResources.INSTANCE.mathquillCss().getText();
-		StyleInjector.inject(mathquillcss);
-	    }
-	});
+				Window.addResizeHandler(new ResizeHandler() {
 
-    }
+					@Override
+					public void onResize(ResizeEvent event) {
+						// TouchEntryPoint.appWidget.setPixelSize(event.getWidth(),
+						// event.getHeight());
+					}
+				});
+
+				tabletGUI.getContentWidget().getElement().getStyle()
+						.setOverflow(Overflow.VISIBLE);
+				app.getScriptManager().ggbOnInit();
+				// needed for testing
+				if (RootPanel.getBodyElement()
+						.getAttribute("data-param-ggbbase64").length() > 0) {
+					app.getGgbApi().setBase64(
+							RootPanel.getBodyElement().getAttribute(
+									"data-param-ggbbase64"));
+				}
+
+				this.initPhoneGap();
+
+				Window.enableScrolling(false);
+
+				progressIndicator.hide();
+
+			}
+		});
+	}
+
+	public static void reloadLocalFiles() {
+		TouchEntryPoint.browseGUI.reloadLocalFiles();
+	}
+
+	protected static void setLookAndFeel() {
+		final String param = RootPanel.getBodyElement().getAttribute(
+				"data-param-laf");
+
+		if ("android".equals(param)) {
+			laf = new AndroidLAF(TouchEntryPoint.app);
+		} else if ("apple".equals(param)) {
+			laf = new AppleLAF(TouchEntryPoint.app);
+		} else if ("win".equals(param)) {
+			laf = new WinLAF(TouchEntryPoint.app);
+		} else {
+			laf = new DefaultLAF(TouchEntryPoint.app);
+		}
+	}
+
+	public static void showBrowseGUI() {
+		TouchEntryPoint.appWidget.showWidget(TouchEntryPoint.browseGUI);
+		TouchEntryPoint.browseGUI.onResize();
+	}
+
+	public static void showTabletGUI() {
+		TouchEntryPoint.appWidget.showWidget(TouchEntryPoint.tabletGUI);
+
+		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+
+			@Override
+			public void execute() {
+				tabletGUI.updateViewSizes(tabletGUI.isAlgebraShowing());
+				tabletGUI.onResize();
+			}
+		});
+		laf.updateUndoSaveButtons();
+	}
+
+	public static void showWorksheetGUI(Material material) {
+		TouchEntryPoint.appWidget.showWidget(TouchEntryPoint.worksheetGUI);	
+		TouchEntryPoint.worksheetGUI.loadWorksheet(material);
+	}
+
+	@Override
+	public void onModuleLoad() {
+
+		AnimationScheduler.get().requestAnimationFrame(new AnimationCallback() {
+
+			@Override
+			public void execute(double timestamp) {
+				loadMobileAsync();
+
+				// insert mathquill css
+				final String mathquillcss = GuiResources.INSTANCE
+						.mathquillCss().getText();
+				StyleInjector.inject(mathquillcss);
+			}
+		});
+
+	}
 }
