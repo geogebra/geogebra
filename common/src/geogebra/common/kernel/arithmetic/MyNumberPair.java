af2550fce67f9413d70c634625ca00aac3d475cf
Zbyněk Konečný <zbynek@geogebra.org>
2013-08-09 06:53:38 +0000

    Code format
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@27548 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/arithmetic/MyNumberPair.java
+++ common/src/geogebra/common/kernel/arithmetic/MyNumberPair.java
@@ -8,7 +8,7 @@ This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by 
 the Free Software Foundation.
 
-*/
+ */
 
 /*
  * GeoVec2D.java
@@ -21,69 +21,75 @@ package geogebra.common.kernel.arithmetic;
 import geogebra.common.kernel.Kernel;
 import geogebra.common.kernel.StringTemplate;
 
-/** 
+/**
  * 
- * @author  Michael
+ * @author Michael
  */
 public class MyNumberPair extends MyVecNode {
-    /**
-     * Creates new number pair
-     * @param kernel kernel
-     * @param en first number
-     * @param en2 second number
-     */
-    public MyNumberPair(Kernel kernel, ExpressionValue en, ExpressionValue en2) {
+	/**
+	 * Creates new number pair
+	 * 
+	 * @param kernel
+	 *            kernel
+	 * @param en
+	 *            first number
+	 * @param en2
+	 *            second number
+	 */
+	public MyNumberPair(Kernel kernel, ExpressionValue en, ExpressionValue en2) {
 		super(kernel, en, en2);
 	}
 
-    @Override
+	@Override
 	public ExpressionValue deepCopy(Kernel kernel1) {
-        return new MyNumberPair(kernel1, x.deepCopy(kernel1), y.deepCopy(kernel1));
-    }
-    
+		return new MyNumberPair(kernel1, x.deepCopy(kernel1),
+				y.deepCopy(kernel1));
+	}
 
-    @Override
-	final public String toString(StringTemplate tpl) {         
-        StringBuilder sb = new StringBuilder();  
-				sb.append(x.toString(tpl));
-				sb.append(", ");
-				sb.append(y.toString(tpl));
-		        
-		 return sb.toString();      
-    }
-    
-    @Override
-	final public String toValueString(StringTemplate tpl) {         
-        StringBuilder sb = new StringBuilder();  
-				sb.append(x.toValueString(tpl));
-				sb.append(", ");
-				sb.append(y.toValueString(tpl));
-		        
-		 return sb.toString();      
-    }
-    
-    @Override
-	final public ExpressionValue traverse(Traversing t){
-    	ExpressionValue v = t.process(this);
-    	if(v!=this) 
-    		return v;
-    	x = x.traverse(t);
-    	y = y.traverse(t);
-    	return this;
-    }
+	@Override
+	final public String toString(StringTemplate tpl) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(x.toString(tpl));
+		sb.append(", ");
+		sb.append(y.toString(tpl));
+
+		return sb.toString();
+	}
+
+	@Override
+	final public String toValueString(StringTemplate tpl) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(x.toValueString(tpl));
+		sb.append(", ");
+		sb.append(y.toValueString(tpl));
+
+		return sb.toString();
+	}
+
+	@Override
+	final public ExpressionValue traverse(Traversing t) {
+		ExpressionValue v = t.process(this);
+		if (v != this)
+			return v;
+		x = x.traverse(t);
+		y = y.traverse(t);
+		return this;
+	}
 
 	/**
-	 * @param x x coordinate
+	 * @param x
+	 *            x coordinate
 	 */
 	public void setX(ExpressionValue x) {
-		this.x = x;		
+		this.x = x;
 	}
-	
+
 	/**
-	 * @param y y cordinate
+	 * @param y
+	 *            y cordinate
 	 */
 	public void setY(ExpressionValue y) {
-		this.y = y;		
+		this.y = y;
 	}
-	
+
 }
