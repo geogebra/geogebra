cfdd94a6749105babddb16cc9e5c282c980b5bfe
Zbyněk Konečný <zbynek@geogebra.org>
2013-08-21 12:11:35 +0000

    Null checks in authentication
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@27860 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/html5/move/ggtapi/models/AuthenticationModelWeb.java
+++ web/src/geogebra/html5/move/ggtapi/models/AuthenticationModelWeb.java
@@ -24,32 +24,50 @@ public class AuthenticationModelWeb extends AuthenticationModel {
 
 	@Override
 	public void storeLoginToken(String token) {
+		if(storage == null){
+			return;
+		}
 		storage.setItem(GGB_TOKEN_KEY_NAME, token);
 	}
 
 	@Override
 	public String getLoginToken() {
+		if(storage == null){
+			return "";
+		}
 		return storage.getItem(GGB_TOKEN_KEY_NAME);
 	}
 
 	@Override
 	public void clearLoginToken() {
+		if(storage == null){
+			return;
+		}
 		storage.removeItem(GGB_TOKEN_KEY_NAME);
 	}
 
 	@Override
     public JSONObject getStoredLoginData() {
+		if(storage == null){
+			return null;
+		}
 		JSONObject resp = JSONParser.parseToJSONObject(JSON.parse(storage.getItem(GGB_LOGIN_DATA_KEY_NAME)));
 		return resp;
 	}
 
 	@Override
     public void storeLoginData(JSONObject info) {
+		if(storage == null){
+			return;
+		}
 	    storage.setItem(GGB_LOGIN_DATA_KEY_NAME, info.toString());
     }
 
 	@Override
     public void removeStoredLoginData() {
+		if(storage == null){
+			return;
+		}
 	    storage.removeItem(GGB_LOGIN_DATA_KEY_NAME);
     }
 
