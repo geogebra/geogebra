f1299b25cf93c7bf7ed985fb88c4484140d47e3a
Michael Borcherds <michael@geogebra.org>
2013-09-06 12:53:42 +0000

    #3610 testing Java 6 building
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@28376 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra/gui/dialog/PropertiesPanelD.java
+++ desktop/geogebra/gui/dialog/PropertiesPanelD.java
@@ -1823,7 +1823,7 @@ public class PropertiesPanelD extends JPanel implements SetLabels, UpdateFonts {
 		private static final long serialVersionUID = 1L;
 		private Object[] geos; // currently selected geos
 		private JCheckBox showLabelCB;
-		private JComboBox labelModeCB;
+		private JComboBox<String> labelModeCB;
 		private boolean showNameValueComboBox;
 
 		public LabelPanel() {
