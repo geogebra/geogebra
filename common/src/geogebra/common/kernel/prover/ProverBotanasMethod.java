1b135972e8b1c10a59b13be5675ba96a14fcfed1
Zoltán Kovács <zoltan@geogebra.org>
2013-09-13 13:34:24 +0000

    Attempt to switch off forcing freePointsNeverCollinear for Giac
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@28579 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/prover/ProverBotanasMethod.java
+++ common/src/geogebra/common/kernel/prover/ProverBotanasMethod.java
@@ -300,7 +300,7 @@ public class ProverBotanasMethod {
 					// SingularWS will use Cox' method
 					ProverSettings.freePointsNeverCollinear = false;
 				} else {
-					ProverSettings.freePointsNeverCollinear = true;
+					ProverSettings.freePointsNeverCollinear = false;
 				}		
 			}
 			
