926da1cb892879170621946c64bb342631090d97
Balazs Bencze <megette_a@yahoo.com>
2013-09-17 09:31:26 +0000

    fix for #3657
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@28678 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/gui/inputfield/ColorProvider.java
+++ common/src/geogebra/common/gui/inputfield/ColorProvider.java
@@ -127,7 +127,7 @@ public class ColorProvider {
 	}
 	
 	private static RegExp createAssignmentRegExp(boolean isCasInput) {
-		return RegExp.compile(LABEL_REGEX_STRING + "(\\(((" + STRING + ",)*)" + STRING + "\\))" + "\\p{Z}*" + (!isCasInput ? "(\\:\\=|\\=)" : "(\\:\\=)"));
+		return RegExp.compile("^\\p{Z}" + LABEL_REGEX_STRING + "(\\(((" + STRING + ",)*)" + STRING + "\\))" + "\\p{Z}*" + (!isCasInput ? "(\\:\\=|\\=)" : "(\\:\\=)"));
 	}
 
 	private void getIntervals() {
