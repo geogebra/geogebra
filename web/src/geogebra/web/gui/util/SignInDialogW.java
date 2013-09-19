fa6ddaa41caef7f2ec94ede71bb17aa17593145a
Ancsin Gábor <ustakraharez@gmail.com>
2013-09-19 09:40:46 +0000

    #3631 signindialog appears as Desktop
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@28763 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/gui/util/SignInDialogW.java
+++ web/src/geogebra/web/gui/util/SignInDialogW.java
@@ -1,37 +1,19 @@
 package geogebra.web.gui.util;
 
-import geogebra.common.GeoGebraConstants;
 import geogebra.common.main.App;
 import geogebra.common.move.events.BaseEvent;
-import geogebra.common.move.ggtapi.models.AuthenticationModel;
-import geogebra.common.move.ggtapi.models.json.JSONObject;
-import geogebra.common.move.ggtapi.models.json.JSONString;
 import geogebra.common.move.views.SuccessErrorRenderable;
-import geogebra.html5.move.ggtapi.models.GeoGebraTubeAPIW;
-import geogebra.html5.util.JSON;
-import geogebra.html5.util.JavaScriptObjectWrapper;
-import geogebra.web.gui.images.AppResources;
 
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.core.shared.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyDownEvent;
-import com.google.gwt.event.dom.client.KeyDownHandler;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.http.client.Request;
-import com.google.gwt.http.client.RequestCallback;
-import com.google.gwt.http.client.Response;
-import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.event.dom.client.LoadEvent;
+import com.google.gwt.event.dom.client.LoadHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Frame;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.PasswordTextBox;
-import com.google.gwt.user.client.ui.TextBox;
 
 /**
  * @author gabor
@@ -46,16 +28,7 @@ public class SignInDialogW extends DialogBox implements SuccessErrorRenderable {
 	/**
 	 * userName for forum textfield
 	 */
-	TextBox forumUserName;
-	private PasswordTextBox forumPassword;
-	private Button submitButton;
-	private Button googleLogin;
-	private Button facebookLogin;
-	private Button twitterLogin;
-	private	Button openidLogin;
-	private Label errormsg;
-	
-	private KeyUpHandler enableSubmit;
+
 
 	/**
 	 * creates a SignInDialog for log in to different
@@ -89,127 +62,18 @@ public class SignInDialogW extends DialogBox implements SuccessErrorRenderable {
 		ggtLoginHeader.add(ggtLogin);
 		ggtLoginPanel.add(ggtLoginHeader);
 		
-		FlexTable logins = new FlexTable();
-		logins.addStyleName("loginspanel");
+		Frame ggbFrame = new Frame(app.getLoginOperation().getLoginURL());
+		ggbFrame.addStyleName("ggtFrame");
 		
-		Label forumAccount = new Label(app.getMenu("UsingYourForumAccount"));
-		logins.getFlexCellFormatter().setColSpan(0, 0, 2);
-		logins.setWidget(0, 0, forumAccount);
-		
-		enableSubmit = new KeyUpHandler() {
+		ggbFrame.addLoadHandler(new LoadHandler() {
 			
-			public void onKeyUp(KeyUpEvent event) {
-				errormsg.setText("");
-				submitButton.setEnabled(!isloginFieldsAreEmpty());
-			}
-		};
-		
-		forumUserName = new TextBox();
-		forumUserName.getElement().setAttribute("placeholder", app.getMenu("username"));
-		forumUserName.setTabIndex(1);
-		
-		forumUserName.addKeyUpHandler(enableSubmit);
-		
-		logins.getFlexCellFormatter().setColSpan(1, 0, 2);
-		logins.setWidget(1, 0, forumUserName);
-		
-		forumPassword = new PasswordTextBox();
-		forumPassword.getElement().setAttribute("placeholder", app.getMenu("password"));
-		forumPassword.setTabIndex(2);
-		
-		forumPassword.addKeyUpHandler(enableSubmit);
-		forumPassword.addKeyDownHandler(new KeyDownHandler() {
-			
-			public void onKeyDown(KeyDownEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					submitButton.click();
-				}
+			@Override
+			public void onLoad(LoadEvent event) {
+				
+				Window.alert("frame loaded");
 			}
 		});
-		
-		logins.getFlexCellFormatter().setColSpan(2, 0, 2);
-		logins.setWidget(2, 0, forumPassword);
-		
-		FlowPanel anchorsContainer = new FlowPanel();
-		anchorsContainer.addStyleName("anchorsContainer");
-		
-		Anchor forgotPassword = new Anchor(app.getMenu("forgottenPassword"));
-		forgotPassword.setHref(GeoGebraConstants.GGT_FORGOT_PWD_URL);
-		forgotPassword.setTarget("_blank");
-		anchorsContainer.add(forgotPassword);
-		
-		Anchor register = new Anchor(app.getMenu("register"));
-		register.setHref(GeoGebraConstants.GGT_REGISTER_URL);
-		register.setTarget("_blank");
-		anchorsContainer.add(register);
-		
-		logins.getFlexCellFormatter().setColSpan(3, 0, 2);
-		logins.setWidget(3, 0, anchorsContainer);
-		
-		errormsg = new Label();
-		errormsg.addStyleName("loginerror");
-		
-		logins.setWidget(4, 0, errormsg);
-		
-		submitButton = new Button(app.getMenu("SignIn"));
-		submitButton.getElement().setTabIndex(3);
-		logins.setWidget(4, 1, submitButton);
-		
-		submitButton.addClickHandler(new ClickHandler() {
-			
-			public void onClick(ClickEvent event) {
-				final String userName = forumUserName.getText();
-				String passwd = forumPassword.getText();
-				(GeoGebraTubeAPIW.getInstance(geogebra.common.move.ggtapi.models.GeoGebraTubeAPI.test_url)).logIn(userName, passwd, new RequestCallback() {
-					
-					public void onResponseReceived(Request request, Response response) {
-						JavaScriptObjectWrapper json = (JavaScriptObjectWrapper) JSON.parse(response.getText());
-						
-						JSONObject resp = new JSONObject();
-
-						if (json.getKeyAsString("error") != null) {
-							resp.put("error", new JSONString(json.getKeyAsString("error")));
-						} else {					
-							String token_value = json.getKeyAsObject("responses")
-									.getKeyAsObject("response")
-									.getKeyAsObject("token")
-									.getKeyAsString("-value");
-							resp.put(AuthenticationModel.GGB_TOKEN_KEY_NAME, new JSONString(token_value));
-						}
-						
-//						((AppW) app).getLoginOperation().onLoginResult(resp);
-					}
-					
-					public void onError(Request request, Throwable exception) {
-						GWT.log(exception.getLocalizedMessage());
-					}
-				});
-			}
-		});
-		
-		
-		
-		Label otherWebSites = new Label(app.getMenu("UsingOtherWebSites"));
-		logins.getFlexCellFormatter().setColSpan(0, 2, 2);
-		logins.setWidget(0, 2, otherWebSites);
-		
-		googleLogin = new Button("<span class=\"loginbutton\"><img src=\"" + AppResources.INSTANCE.social_google().getSafeUri().asString() + "\"/>" + app.getMenu("LoginToGoogle") + "</span>");
-		googleLogin.setTabIndex(4);
-		logins.setWidget(1, 2, googleLogin);
-		
-		facebookLogin = new Button("<span class=\"loginbutton\"><img src=\"" + AppResources.INSTANCE.social_facebook().getSafeUri().asString() + "\"/>" + app.getMenu("LoginToFaceBook") + "</span>");
-		facebookLogin.setTabIndex(5);
-		logins.setWidget(1, 3, facebookLogin);
-		
-		twitterLogin = new Button("<span class=\"loginbutton\"><img src=\"" + AppResources.INSTANCE.social_twitter().getSafeUri().asString() + "\"/>" + app.getMenu("LoginToTwitter") + "</span>");
-		twitterLogin.setTabIndex(6);
-		logins.setWidget(2, 2, twitterLogin);
-		
-		openidLogin = new Button("<span class=\"loginbutton\"><img src=\"" + AppResources.INSTANCE.social_openid().getSafeUri().asString() + "\"/>" + app.getMenu("LoginToOpenId") + "</span>");
-		openidLogin.setTabIndex(7);
-		logins.setWidget(2, 3, openidLogin);		
-		
-		ggtLoginPanel.add(logins);
+		ggtLoginPanel.add(ggbFrame);
 		container.add(ggtLoginPanel);
 		
 //		((AppW) app).getLoginOperation().getView().add(this);		
@@ -220,31 +84,14 @@ public class SignInDialogW extends DialogBox implements SuccessErrorRenderable {
 	@Override
     public void show() {
 		super.show();
-		clearLoginFields();
-		submitButton.setEnabled(!isloginFieldsAreEmpty());
-		Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand () {
-	        public void execute () {
-	            forumUserName.setFocus(true);
-	        }
-	    });
 	}
 
-	private boolean isloginFieldsAreEmpty() {
-		return (forumUserName.getText().length() == 0 || forumPassword.getText().length() == 0);
-    }
-
-	private void clearLoginFields() {
-		forumUserName.setText("");
-	    forumPassword.setText("");
-	    errormsg.setText("");
-    }
 
 	public void success(BaseEvent event) {
 	    this.hide();
     }
 
 	public void fail(BaseEvent event) {
-	   errormsg.setText(app.getMenu("LoginFailed"));
     }
 
 }
