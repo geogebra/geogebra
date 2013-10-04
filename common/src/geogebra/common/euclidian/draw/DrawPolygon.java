04abf5dbe3086f3163e53859b2356e1681ba1582
Zbyněk Konečný <zbynek@geogebra.org>
2013-10-04 08:04:44 +0000

    Removed debugging
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@29151 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/euclidian/draw/DrawPolygon.java
+++ common/src/geogebra/common/euclidian/draw/DrawPolygon.java
@@ -29,7 +29,6 @@ import geogebra.common.kernel.geos.GeoPolygon;
 import geogebra.common.kernel.geos.GeoVec3D;
 import geogebra.common.kernel.kernelND.GeoPointND;
 import geogebra.common.util.MyMath;
-import geogebra.common.util.debug.Log;
 
 import java.util.ArrayList;
 
@@ -338,8 +337,6 @@ public class DrawPolygon extends Drawable implements Previewable {
 
 	@Override
 	final public boolean isInside(geogebra.common.awt.GRectangle rect) {
-		Log.debug(gp.getBounds());
-		Log.debug(rect);
 		return gp != null && gp.getBounds() != null
 				&& rect.contains(gp.getBounds());
 	}
