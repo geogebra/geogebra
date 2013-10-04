351412924ae41f474638009309ed2b31037c4823
Mathieu Blossier <mathieublossier@free.fr>
2013-10-04 18:37:05 +0000

    Interface for Input3D
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@29189 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common/src/geogebra3D/input3D/Input3D.java
@@ -0,0 +1,25 @@
+package geogebra3D.input3D;
+
+
+/**
+ * interface for specific 3D inputs
+ * @author mathieu
+ *
+ */
+public interface Input3D {
+
+	
+	
+	/**
+	 * update values
+	 * @return true if the update worked
+	 */
+	public boolean update();
+	
+	/**
+	 * 
+	 * @return 3D mouse position
+	 */
+	public double[] getMouse3DPosition();
+	
+}
