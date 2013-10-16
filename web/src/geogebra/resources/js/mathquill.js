eb9235ac4f42a2e8d4b8a15eec12c38c357cec29
Árpád Fekete <fekarp@gmail.com>
2013-10-16 08:28:30 +0000

    Adapting mathquill.css to the Roboto fonts
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@29511 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/resources/js/mathquill.js
+++ web/src/geogebra/resources/js/mathquill.js
@@ -2211,7 +2211,6 @@ LatexCmds.fraction = P(MathCommand, function(_, _super) {
       '<span class="fraction non-leaf">'
     +   '<span class="numerator">&0</span>'
     +   '<span class="denominator">&1</span>'
-    +   '<span style="display:inline-block;width:0">&nbsp;</span>'
     + '</span>'
   ;
   _.textTemplate = ['(', '/', ')'];
