00bb75efe8b9ac0de0bb617487f3bcc8c8c91173
Zbyněk Konečný <zbynek@geogebra.org>
2013-10-31 20:58:50 +0000

    Removed debugging
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@29920 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/geos/GeoElement.java
+++ common/src/geogebra/common/kernel/geos/GeoElement.java
@@ -75,7 +75,6 @@ import geogebra.common.util.NumberFormatAdapter;
 import geogebra.common.util.SpreadsheetTraceSettings;
 import geogebra.common.util.StringUtil;
 import geogebra.common.util.Unicode;
-import geogebra.common.util.debug.Log;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -1504,7 +1503,6 @@ public abstract class GeoElement extends ConstructionElement implements
 	public void setAdvancedVisualStyleCopy(final GeoElement geo) {
 		// copy color function
 		if(geo.getColorFunction() != null){
-			Log.debug(geo.getColorFunction());
 			setColorFunction(geo.getColorFunction().deepCopyGeo());
 			setColorSpace(geo.getColorSpace());
 		}
