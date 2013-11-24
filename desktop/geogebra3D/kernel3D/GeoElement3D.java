2a8200caa40023258b15523dec7188eba80a57d3
Mathieu Blossier <mathieublossier@free.fr>
2013-11-24 14:27:42 +0000

    Remove unused "Test3D.java"
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@30576 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra3D/kernel3D/GeoElement3D.java
+++ desktop/geogebra3D/kernel3D/GeoElement3D.java
@@ -21,7 +21,6 @@ package geogebra3D.kernel3D;
 import geogebra.common.kernel.Construction;
 import geogebra.common.kernel.Matrix.Coords;
 import geogebra.common.kernel.geos.GeoElement;
-import geogebra3D.Test3D;
 import geogebra3D.euclidian3D.Drawable3D;
 
 
