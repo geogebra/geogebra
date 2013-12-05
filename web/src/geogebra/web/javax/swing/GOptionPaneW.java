b5fc2ae228acf5f81edec60db3ab1690ade11399
Judit Elias <eliasjudit@gmail.com>
2013-12-05 13:49:26 +0000

    Implementation of option dialog started... (#3490)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@30893 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/javax/swing/GOptionPaneW.java
+++ web/src/geogebra/web/javax/swing/GOptionPaneW.java
@@ -6,6 +6,7 @@ import geogebra.common.main.App;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PopupPanel;
@@ -58,4 +59,63 @@ public class GOptionPaneW implements GOptionPane{
 
     }
 
+	public void showOptionDialog(App app, String message,
+            String title, String[] options, final OptionHandler handler) {
+		
+		DialogBox dialogbox = new DialogBox();
+	
+		final PopupPanel dialog = new PopupPanel(false, true);
+		
+		FlowPanel buttonPanel = new FlowPanel();
+		buttonPanel.addStyleName("DialogButtonPanel");
+		for (int i = 0; i<options.length; i++){
+			Button bt = new Button(options[i]);
+			final int selectedOption = i;
+			bt.addClickHandler(new ClickHandler(){
+				public void onClick(ClickEvent event){
+					handler.selectOption(selectedOption);
+					dialog.hide();
+				}
+			});
+			buttonPanel.add(bt);
+		}
+		
+		
+//		if (optionType == GOptionPane.DEFAULT_OPTION) {
+//
+//			Button ok = new Button("OK");
+//			ok.addClickHandler(new ClickHandler() {
+//
+//				public void onClick(ClickEvent event) {
+//					dialog.hide();
+//				}
+//
+//			});
+//			buttonPanel.add(ok);
+//			ok.getElement().focus();
+//		} else {
+//			App.debug("Option type other then DEFAULT_OPTION - implementation needed");
+//		}
+//
+//		VerticalPanel panel = new VerticalPanel();
+//		String[] lines = message.split("\n");
+//		for (String item : lines) {
+//			panel.add(new Label(item));
+//		}
+//
+		
+		FlowPanel panel = new FlowPanel();
+		panel.add(new Label(message));
+		panel.add(buttonPanel);
+
+		dialog.setWidget(panel);
+		dialog.center();
+		dialog.show();
+	
+    }
+	
+	public abstract class OptionHandler{
+		public abstract void selectOption(int i);
+	}
+	
 }
