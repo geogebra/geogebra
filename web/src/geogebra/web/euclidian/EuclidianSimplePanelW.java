023bd5f8b474b613a041fb8aa70a8ceacf31eca7
Árpád Fekete <fekarp@gmail.com>
2013-12-06 15:06:53 +0000

    Ticket #3485: apparently EuclidianSimplePanelW never used onResize
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@30935 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/euclidian/EuclidianSimplePanelW.java
+++ web/src/geogebra/web/euclidian/EuclidianSimplePanelW.java
@@ -5,25 +5,18 @@ import geogebra.common.main.App;
 import geogebra.web.main.AppW;
 
 import com.google.gwt.canvas.client.Canvas;
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Panel;
-import com.google.gwt.user.client.ui.RequiresResize;
 import com.google.gwt.user.client.ui.Widget;
 
-public class EuclidianSimplePanelW extends AbsolutePanel implements EuclidianPanelWAbstract, RequiresResize {
+public class EuclidianSimplePanelW extends AbsolutePanel implements EuclidianPanelWAbstract {
 
 	AppW app;
 	int oldHeight = 0;
 	int oldWidth = 0;
-	//EuclidianStyleBar espanel;
-	//EuclidianPanel euclidianpanel;
 
 	Canvas eview1 = null;// static foreground
-
-	//private ConstructionProtocolNavigationW consProtNav;
 
 	/**
 	 * This constructor is used by the Application
@@ -45,109 +38,17 @@ public class EuclidianSimplePanelW extends AbsolutePanel implements EuclidianPan
 	public EuclidianSimplePanelW(AppW application, boolean stylebar) {
 		this(stylebar);
 		app = application;
-		//addNavigationBar();
 	}
 
 	protected Widget loadComponent() {
-		/*if (euclidianpanel == null) {
-			euclidianpanel = new EuclidianPanel(this);*/
-			eview1 = Canvas.createIfSupported();
-			eview1.getElement().getStyle().setPosition(Style.Position.RELATIVE);
-			eview1.getElement().getStyle().setZIndex(0);
-		/*	euclidianpanel.getAbsolutePanel().add(eview1);
-		}*/
+		eview1 = Canvas.createIfSupported();
+		eview1.getElement().getStyle().setPosition(Style.Position.RELATIVE);
+		eview1.getElement().getStyle().setZIndex(0);
 		add(eview1);
 
-		//return euclidianpanel;
 		return this;
 	}
 	
-	/*public void addNavigationBar(){
-		consProtNav = (ConstructionProtocolNavigationW)(app
-		        .getConstructionProtocolNavigation());
-		consProtNav.getImpl().addStyleName("consProtNav");
-		euclidianpanel.add(consProtNav.getImpl()); // may be invisible, but made visible later		
-		updateNavigationBar();
-	}*/
-	
-	/*public void updateNavigationBar(){
-//		ConstructionProtocolSettings cps = app.getSettings()
-//		        .getConstructionProtocol();
-//		((ConstructionProtocolNavigationW) consProtNav).settingsChanged(cps);
-//		cps.addListener((ConstructionProtocolNavigation)consProtNav);
-
-		
-		if (app.getShowCPNavNeedsUpdate()) {
-			app.setShowConstructionProtocolNavigation(app
-			        .showConsProtNavigation());
-		}
-
-		consProtNav.update();
-		consProtNav.setVisible(app.showConsProtNavigation());
-	}*/
-	
-	/*class EuclidianPanel extends FlowPanel implements RequiresResize {
-
-		EuclidianSimplePanelW dockPanel;
-		AbsolutePanel absoluteEuclidianPanel;
-
-		int oldHeight = 0;
-		int oldWidth = 0;
-		
-		public EuclidianPanel(EuclidianSimplePanelW dockPanel) {
-			super();
-			this.dockPanel = dockPanel;
-			add(absoluteEuclidianPanel = new AbsolutePanel());
-		}
-
-		public void onResize() {
-		
-			if (app != null) {
-
-				Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-					public void execute() {
-
-						int h = dockPanel.getComponentInteriorHeight();
-						int w = dockPanel.getComponentInteriorWidth();
-
-						// TODO handle this better?
-						// exit if new size cannot be determined
-						if (h < 0 || w < 0) {
-							return;
-						}
-						if (h != oldHeight || w != oldWidth) {
-							app.ggwGraphicsViewDimChanged(w, h);
-							oldHeight = h;
-							oldWidth = w;
-						}
-					}
-				});
-
-			}
-		}
-
-		public void add(Widget w, int x, int y) {
-	        absoluteEuclidianPanel.add(w,x,y);
-        }
-
-		public boolean remove(Widget w) {
-			return absoluteEuclidianPanel.remove(w);
-        }
-
-		public AbsolutePanel getAbsolutePanel() {
-			return absoluteEuclidianPanel;
-        }
-	}*/
-
-	/*protected Widget loadStyleBar() {
-
-		if (espanel == null) {
-			espanel = app.getEuclidianView1().getStyleBar();
-		}
-
-		return (Widget) espanel;
-	}*/
-
 	public Canvas getCanvas() {
 	    return eview1;
     }
@@ -156,20 +57,7 @@ public class EuclidianSimplePanelW extends AbsolutePanel implements EuclidianPan
 	    return this;
     }
 
-	/*public void onResize() {
-		// super.onResize();
-    }*/
-
-	/*public void add(Widget w, int x, int y) {
-	    euclidianpanel.add(w,x,y);
-    }
-
-	public void remover(Widget w) {
-		euclidianpanel.remove(w);
-    }*/
-
 	public void attachApp(App app) {
-		//super.attachApp(app);
 		this.app = (AppW)app;
 	}
 
@@ -188,33 +76,11 @@ public class EuclidianSimplePanelW extends AbsolutePanel implements EuclidianPan
 	}
 
 	public void onResize() {
-		if (app != null) {
-
-			Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-				public void execute() {
-
-					int h = getOffsetHeight();
-					int w = getOffsetWidth();
-
-					// TODO handle this better?
-					// exit if new size cannot be determined
-					if (h < 0 || w < 0) {
-						return;
-					}
-					if (h != oldHeight || w != oldWidth) {
-						app.ggwGraphicsViewDimChanged(w, h);
-						oldHeight = h;
-						oldWidth = w;
-					}
-				}
-			});
-
-		}
+		// no call
     }
 
 	public void deferredOnResize() {
-		// TODO: onResize should not be deferred!
-		onResize();
+		// no call
 	}
 
 	public void updateNavigationBar() { }
