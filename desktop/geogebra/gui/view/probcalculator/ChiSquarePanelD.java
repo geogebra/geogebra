23232eb1859994877b183ca36e4a4b28ac7602b7
Ancsin Gábor <ustakraharez@gmail.com>
2013-12-10 08:38:46 +0000

    #3882 ChiSqueareCell better to have as inner class
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@30996 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra/gui/view/probcalculator/ChiSquarePanelD.java
+++ desktop/geogebra/gui/view/probcalculator/ChiSquarePanelD.java
@@ -1,12 +1,16 @@
 package geogebra.gui.view.probcalculator;
 
+import geogebra.common.gui.view.probcalculator.ChiSquareCell;
 import geogebra.common.gui.view.probcalculator.StatisticsCalculator;
 import geogebra.common.gui.view.probcalculator.StatisticsCalculator.Procedure;
+import geogebra.common.gui.view.probcalculator.StatisticsCollection;
+import geogebra.common.main.GeoGebraColorConstants;
 import geogebra.gui.inputfield.MyTextField;
 import geogebra.gui.util.LayoutUtil;
 import geogebra.main.AppD;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,6 +18,7 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -152,8 +157,6 @@ public class ChiSquarePanelD extends geogebra.common.gui.view.probcalculator.Chi
 			JPanel row = new JPanel(new FlowLayout(FlowLayout.LEFT, 1, 1));
 			for (int c = 0; c < sc.columns + 2; c++) {
 				cell[r][c] = new ChiSquareCellD(sc, r, c);
-				cell[r][c].setApp(app);
-				cell[r][c].setStatCalc(statCalc);
 				cell[r][c].getInputField().addActionListener(this);
 				cell[r][c].getInputField().addFocusListener(this);
 
@@ -347,6 +350,187 @@ public class ChiSquarePanelD extends geogebra.common.gui.view.probcalculator.Chi
 	public void focusLost(FocusEvent e) {
 		if (e.getSource() instanceof MyTextField)
 			doTextFieldActionPerformed((MyTextField) e.getSource());
+
+	}
+
+	/*****************************************************************
+	 * 
+	 * Class ChiSquareCell: extended JPanel to hold cell components
+	 * 
+	 ***************************************************************** 
+	 */
+	public class ChiSquareCellD extends ChiSquareCell implements ActionListener,
+			FocusListener {
+
+		private static final long serialVersionUID = 1L;
+
+		
+		
+
+		private JPanel wrappedPanel;
+		
+		private MyTextField fldInput;
+		private JLabel[] label;
+
+		/**
+		 * Construct ChiSquareCell with given row, column
+		 */
+		public ChiSquareCellD(StatisticsCollection sc, int row, int column) {
+			this(sc);
+			this.row = row;
+			this.column = column;
+		}
+
+		/**
+		 * Construct ChiSquareCell
+		 */
+		public ChiSquareCellD(StatisticsCollection sc) {
+
+			this.sc = sc;
+			this.wrappedPanel = new JPanel();
+			wrappedPanel.setOpaque(true);
+			wrappedPanel.setLayout(new BoxLayout(this.wrappedPanel, BoxLayout.Y_AXIS));
+
+			fldInput = new MyTextField((AppD) app);
+			fldInput.addActionListener(this);
+			fldInput.addFocusListener(this);
+			wrappedPanel.add(LayoutUtil.flowPanelCenter(0, 0, 0, fldInput));
+
+			label = new JLabel[5];
+			for (int i = 0; i < label.length; i++) {
+				label[i] = new JLabel();
+				label[i].setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+				wrappedPanel.add(LayoutUtil.flowPanelCenter(0, 0, 0, label[i]));
+			}
+			setColumns(4);
+			setVisualStyle();
+			hideAllLabels();
+
+		}
+
+		public void setColumns(int columns) {
+			fldInput.setColumns(columns);
+
+			// force a minimum width for margin cells
+			wrappedPanel.add(Box.createHorizontalStrut(fldInput.getPreferredSize().width));
+
+		}
+
+		/**
+		 * hide all labels
+		 */
+		public void hideAllLabels() {
+			for (int i = 0; i < label.length; i++) {
+				label[i].setVisible(false);
+			}
+		}
+
+		/**
+		 * hide all
+		 */
+		public void hideAll() {
+			hideAllLabels();
+			fldInput.setVisible(false);
+			wrappedPanel.setBorder(BorderFactory.createEmptyBorder());
+		}
+
+		/**
+		 * @return input field
+		 */
+		public MyTextField getInputField() {
+			return fldInput;
+		}
+
+		/**
+		 * @return label array
+		 */
+		public JLabel[] getLabel() {
+			return label;
+		}
+
+		public void setLabelText(int index, String s) {
+			label[index].setText(s);
+		}
+
+		public void setLabelVisible(int index, boolean isVisible) {
+			label[index].setVisible(isVisible);
+		}
+
+		public void setMarginCell(boolean isMarginCell) {
+			this.isMarginCell = isMarginCell;
+			setVisualStyle();
+		}
+
+		public void setHeaderCell(boolean isHeaderCell) {
+			this.isHeaderCell = isHeaderCell;
+			setVisualStyle();
+		}
+
+		private void setVisualStyle() {
+			wrappedPanel.setBackground(null);
+			fldInput.setVisible(false);
+
+			if (isMarginCell) {
+				wrappedPanel.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+				setLabelVisible(0, true);
+
+			} else if (isHeaderCell) {
+				wrappedPanel.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+				fldInput.setVisible(true);
+				fldInput.setBackground(geogebra.awt.GColorD
+						.getAwtColor(GeoGebraColorConstants.TABLE_BACKGROUND_COLOR_HEADER));
+
+			} else {
+				fldInput.setVisible(true);
+				wrappedPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+				fldInput.setBackground(geogebra.awt.GColorD
+						.getAwtColor(GeoGebraColorConstants.WHITE));
+			}
+
+		}
+
+		public int getRow() {
+			return row;
+		}
+
+		public void setRow(int row) {
+			this.row = row;
+		}
+
+		public int getColumn() {
+			return column;
+		}
+
+		public void setColumn(int column) {
+			this.column = column;
+		}
+
+		private void updateCellData() {
+			sc.chiSquareData[row][column] = fldInput.getText();
+		}
+
+		public void focusGained(FocusEvent e) {
+			if (e.getSource() instanceof MyTextField) {
+				((MyTextField) e.getSource()).selectAll();
+			}
+		}
+
+		public void focusLost(FocusEvent e) {
+			updateCellData();
+			statCalc.updateResult();
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			updateCellData();
+			statCalc.updateResult();
+
+		}
+		
+		public JPanel getWrappedPanel() {
+			return wrappedPanel;
+		}
+		
+		
 
 	}
 
