geogebra/geogebra

commit e8e5c48c8c3475d0888ded1b6f6ece7e7b0f47fd

Ticket #3705: on resize, the divider locations should be updated to the correct value
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@31136 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/geogebra/web/gui/layout/DockSplitPaneW.java
@@ -576,7 +576,12 @@ public class DockSplitPaneW extends SplitLayoutPanel implements DockComponent {
 
 	@Override
 	public void onResize() {
-		//super.onResize();
+
+		if (orientation == HORIZONTAL_SPLIT) {
+			setDividerLocationSilent(getLeftComponent().getOffsetWidth());
+		} else {
+			setDividerLocationSilent(getLeftComponent().getOffsetHeight());
+		}
 
 		// it's only important to resize components so that
 		// the divider should be inside
@@ -586,8 +591,8 @@ public class DockSplitPaneW extends SplitLayoutPanel implements DockComponent {
 		}
 
 		if (getRightComponent() instanceof DockSplitPaneW) {
-			((DockSplitPaneW)getRightComponent()).checkDividerIsOutside();
 			((RequiresResize)getRightComponent()).onResize();
+			((DockSplitPaneW)getRightComponent()).checkDividerIsOutside();
 		} else if (getRightComponent() instanceof RequiresResize) {
 			((RequiresResize)getRightComponent()).onResize();
 		}
@@ -630,11 +635,13 @@ public class DockSplitPaneW extends SplitLayoutPanel implements DockComponent {
 		int h = this.getElement().getClientHeight();
 
 		if (orientation == HORIZONTAL_SPLIT) {
-			// vertical split not considered yet
 			if (getDividerLocation() >= w && (w > 0)) {
 				setDividerLocation(0.5);
 			}
-			//saveDividerLocation();
+		} else {
+			if (getDividerLocation() >= h && (h > 0)) {
+				setDividerLocation(0.5);
+			}
 		}
 	}
 }

commit 957eee4ba7fc073b25ebbd4cd5372d17d3a15778

Ticket #3705: fixing bug caused by r31136
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@31141 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/geogebra/web/gui/layout/DockSplitPaneW.java
@@ -577,12 +577,6 @@ public class DockSplitPaneW extends SplitLayoutPanel implements DockComponent {
 	@Override
 	public void onResize() {
 
-		if (orientation == HORIZONTAL_SPLIT) {
-			setDividerLocationSilent(getLeftComponent().getOffsetWidth());
-		} else {
-			setDividerLocationSilent(getLeftComponent().getOffsetHeight());
-		}
-
 		// it's only important to resize components so that
 		// the divider should be inside
 
@@ -595,6 +589,16 @@ public class DockSplitPaneW extends SplitLayoutPanel implements DockComponent {
 			((DockSplitPaneW)getRightComponent()).checkDividerIsOutside();
 		} else if (getRightComponent() instanceof RequiresResize) {
 			((RequiresResize)getRightComponent()).onResize();
+		}
+
+		if (orientation == HORIZONTAL_SPLIT) {
+			if (getLeftComponent() != null && getLeftComponent().getOffsetWidth() > 0) {
+				setDividerLocationSilent(getLeftComponent().getOffsetWidth());
+			}
+		} else {
+			if (getLeftComponent() != null && getLeftComponent().getOffsetHeight() > 0) {
+				setDividerLocationSilent(getLeftComponent().getOffsetHeight());
+			}
 		}
 
 		if (app.getArticleElement().getScaleX() != 1.0 ||