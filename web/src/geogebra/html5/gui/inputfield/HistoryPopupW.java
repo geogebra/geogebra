70caa8783527d8060975426df7c650eae1300698
George Sturr <gsturr@santarosa.edu>
2013-12-31 07:17:48 +0000

    changed history popup to use ListBox, better  key handling
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@31400 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/html5/gui/inputfield/HistoryPopupW.java
+++ web/src/geogebra/html5/gui/inputfield/HistoryPopupW.java
@@ -4,104 +4,95 @@ import geogebra.common.main.GWTKeycodes;
 
 import java.util.ArrayList;
 
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.PopupPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class HistoryPopupW extends PopupPanel implements ClickHandler, KeyUpHandler {
+public class HistoryPopupW extends PopupPanel implements ClickHandler,
+        KeyUpHandler, ChangeHandler {
 
 	private AutoCompleteTextFieldW textField;
 	private boolean isDownPopup;
-	private VerticalPanel historyList;
-	private int highlighted = 0;
+	private ListBox historyList;
+	private String originalTextEditorContent;
 
 	public HistoryPopupW(AutoCompleteTextFieldW autoCompleteTextField) {
-		 this.textField = autoCompleteTextField;
-		 historyList = new VerticalPanel();
-		 historyList.addStyleName("historyList");
-		 historyList.addHandler(this, KeyUpEvent.getType());
-		 add(historyList);
-    }
+
+		this.textField = autoCompleteTextField;
+
+		historyList = new ListBox();
+		historyList.addChangeHandler(this);
+		historyList.addKeyUpHandler(this);
+		historyList.addClickHandler(this);
+		historyList.addStyleName("historyList");
+
+		add(historyList);
+		addStyleName("GeoGebraPopup");
+		setAutoHideEnabled(true);
+	}
 
 	public void setDownPopup(boolean isDownPopup) {
 		this.isDownPopup = isDownPopup;
-    }
+	}
 
 	public void showPopup() {
 		ArrayList<String> list = textField.getHistory();
 		if (list.isEmpty()) {
 			return;
 		}
+
+		originalTextEditorContent = textField.getText();
 		historyList.clear();
+		historyList.setVisibleItemCount(Math.min(Math.max(list.size(),2), 10));
+
 		for (String link : list) {
-			
-			//With this constructor, there is no default href added to anchor,
-			//in this way, there is no "Javascript:;" popup on the left bottom corner of Chrome.
-	        Anchor a = new Anchor(false);	        
-	        a.setText(link);
-	        a.addClickHandler(this);
-	        //sadly, it is not so nice, but I can't attach it to historyList :-(
-	        a.addKeyUpHandler(this);
-	        historyList.add(a);
-        }
+			historyList.addItem(link);
+		}
+
 		show();
-		setPopupPosition(textField.getAbsoluteLeft(), textField.getAbsoluteTop()-getOffsetHeight());
-		int lastItem = historyList.getWidgetCount()-1;
-		historyList.getWidget(lastItem).getElement().focus();
-		highlighted = lastItem;
-    }
+		setPopupPosition(textField.getAbsoluteLeft(),
+		        textField.getAbsoluteTop() - getOffsetHeight());
+
+		historyList.setSelectedIndex(list.size() - 1);
+		textField.setText(historyList.getItemText(historyList
+		        .getSelectedIndex()));
+
+		historyList.setFocus(true);
+	}
 
 	public boolean isDownPopup() {
 		return isDownPopup;
-    }
+	}
 
-	public void onClick(ClickEvent event) {
-	  Anchor target = (Anchor) event.getSource();
-	  textField.setText(target.getText());
-	  hide();
-    }
+	@Override
+	public void onChange(ChangeEvent event) {
+		textField.setText(historyList.getItemText(historyList
+		        .getSelectedIndex()));
+	}
 
 	public void onKeyUp(KeyUpEvent event) {
-	    int charCode = event.getNativeKeyCode();
-	    if (historyList.getWidgetCount() == 0) {
-	    	return;
-	    }
-	    for (int i = 0; i < historyList.getWidgetCount(); i++) {
-	    	historyList.getWidget(i).removeStyleName("highlight");
-	    }
-	    Anchor a = null;
-	    switch (charCode) {
-		case GWTKeycodes.KEY_DOWN:
-			highlighted++;
-			if (highlighted >= historyList.getWidgetCount()) {
-				highlighted = historyList.getWidgetCount() -1;
-			}
-			a = (Anchor) historyList.getWidget(highlighted);
-			a.addStyleName("highlight");
-			a.getElement().focus();
-		break;
-		case GWTKeycodes.KEY_UP:
-			highlighted--;
-			if (highlighted < 0) {
-				highlighted = 0;
-			}
-			a = (Anchor) historyList.getWidget(highlighted);
-			a.addStyleName("highlight");
-			a.getElement().focus();
-		break;
-		case GWTKeycodes.KEY_ENTER: 
-			a = (Anchor) historyList.getWidget(highlighted);
-			 textField.setText(a.getText());
-			 hide();
-		break;
+		int charCode = event.getNativeKeyCode();
+		switch (charCode) {
 		case GWTKeycodes.KEY_ESCAPE:
 			hide();
-		break;
-	    }
-    }
+			textField.setText(originalTextEditorContent);
+			textField.setFocus(true);
+			break;
+		case GWTKeycodes.KEY_ENTER:
+			hide();
+			textField.setFocus(true);
+			break;
+		}
+
+	}
+
+	public void onClick(ClickEvent event) {
+		hide();
+	}
 
 }
