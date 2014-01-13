4abb085a3ec578d0d5a9675245bbe490dbb05bf3
Zoltán Kovács <zoltan@geogebra.org>
2014-01-13 16:48:12 +0000

    Removing unnecessary output line (LocusEquation)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@31677 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/locusequ/CASTranslator.java
+++ common/src/geogebra/common/kernel/locusequ/CASTranslator.java
@@ -275,7 +275,7 @@ public class CASTranslator extends EquationTranslator<StringBuilder> {
 					append("),dp;").
 					append("short=0;ideal I=" + convertFloatsToRationals(CASTranslator.constructRestrictions(restrictions))).
 					append(";def Gp=grobcov(I);list l=" + SingularWebService.getLocusCommand() + "(Gp);").
-					append("poly pp=1; int i; for (i=1; i<=size(l); i++) { pp=pp*l[i][1][1]; } pp;").
+					append("poly pp=1; int i; for (i=1; i<=size(l); i++) { pp=pp*l[i][1][1]; }").
 					append("string s=string(pp);int sl=size(s);string pg=\"poly p=\"+s[2,sl-2];").
 					append("ring rr=0,(x,y),dp;execute(pg);").
 					append("printf(\"%s,%s,%s\",size(coeffs(p,x)),size(coeffs(p,y)),").
