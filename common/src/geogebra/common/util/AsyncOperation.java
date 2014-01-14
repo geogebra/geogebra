92c41ce4ef527becf2636b6c7e0da3547f92e88c
Judit Elias <eliasjudit@gmail.com>
2014-01-14 12:10:08 +0000

    AsyncOperation class cleaning + bugfix (#3490)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@31704 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/util/AsyncOperation.java
+++ common/src/geogebra/common/util/AsyncOperation.java
@@ -3,27 +3,26 @@ package geogebra.common.util;
 import java.util.HashMap;
 
 public abstract class AsyncOperation {
-	protected Object data=null;
 	protected HashMap<String, Object> properties = new HashMap<String, Object>();
 	
 	
 	public abstract void callback(Object obj);
 	
 	public Object getData(){
-		return data;
+		return this.properties.get("data");
 	}
 	
 	public void setData(Object d){
-		data = d;
+		this.properties.put("data", d);
 	}
 	
 	
 	public void setProperty(String propertyName, Object prop){
-		this.properties.put("propertyName", prop);
+		this.properties.put(propertyName, prop);
 	}
 	
 	public Object getProperty(String propertyName){
-		return this.properties.get("propertyName");
+		return this.properties.get(propertyName);
 	}
 
 }
