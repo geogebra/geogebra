dd3895772af8edfd5fafea286b92f29a37a238c4
Mathieu Blossier <mathieublossier@free.fr>
2014-01-30 14:46:57 +0000

    Don't put again not labeled segments to output
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@32091 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/geogebra3D/kernel3D/algos/AlgoPolyhedron.java
+++ common/src/geogebra/common/geogebra3D/kernel3D/algos/AlgoPolyhedron.java
@@ -231,8 +231,11 @@ public abstract class AlgoPolyhedron extends AlgoElement3D{
 		//append XML for polygon and segments linked once more, to avoid override of specific properties		
 		for (GeoPolygon polygon : polyhedron.getPolygonsLinked())
 			polygon.getXML(false, sb);
-		for (GeoSegmentND segment : polyhedron.getSegmentsLinked())
-			((GeoElement) segment).getXML(false, sb);
+		for (GeoSegmentND segment : polyhedron.getSegmentsLinked()){
+			if (segment.isLabelSet()){
+				((GeoElement) segment).getXML(false, sb);
+			}
+		}
 		
 	}
 	
