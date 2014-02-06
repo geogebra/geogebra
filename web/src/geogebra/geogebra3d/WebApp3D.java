d635f11323a9fa1d6ff6833fd6da86b88cb2489c
Ancsin Gábor <ustakraharez@gmail.com>
2014-02-06 09:48:36 +0000

    Beginning webapp3d
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@32204 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/geogebra/geogebra3d/WebApp3D.java
@@ -0,0 +1,14 @@
+package geogebra.geogebra3d;
+
+import geogebra.web.Web;
+
+import com.google.gwt.user.client.Window;
+
+public class WebApp3D extends Web {
+		@Override
+		public void onModuleLoad() {
+		    // TODO Auto-generated method stub
+		    //super.onModuleLoad();
+			Window.alert("I will be threeD :-)");
+		}
+}
