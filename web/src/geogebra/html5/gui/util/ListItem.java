40777c12e0c3c73f0b0687816908e20a7683468d
Judit Elias <eliasjudit@gmail.com>
2014-02-28 17:26:19 +0000

    Some cleaning and avoid using of deprecated method. (#4134)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@32608 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/html5/gui/util/ListItem.java
+++ web/src/geogebra/html5/gui/util/ListItem.java
@@ -1,45 +1,19 @@
 package geogebra.html5.gui.util;
 
 import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.ComplexPanel;
 import com.google.gwt.user.client.ui.Widget;
-
-//public class ListItem extends Widget{
-//	
-//	public ListItem(){
-//		Element li = DOM.createElement("li");
-//		setElement(li);
-//	}
-//	
-//	public void add(Widget w){
-//		this.getElement().appendChild(w.getElement());
-//	}
-//}
-
-
 
 public class ListItem extends ComplexPanel {
 	
 	public ListItem(){
-		//Element li = DOM.createElement("li");
-		//setElement(li);
 		setElement(Document.get().createLIElement());
 	}
 	
 	@Override
 	  public void add(Widget w) {
-	    add(w, getElement());
+		Element el = getElement();
+	    add(w, el);
 	  }
-	
-//	@Override
-//	  public void insert(Widget w) {
-//		//insert(Widget child, Element container, int beforeIndex,
-//	     // boolean domInsert)
-//	    insert(w, getElement());
-//	  }
-	
-	
-	//public void add(Widget w){
-	//	this.getElement().appendChild(w.getElement());
-	//}
 }
