39ec10cc610dcafdde70f344e56933efc9c37915
Zbyněk Konečný <zbynek@geogebra.org>
2014-03-26 12:17:36 +0000

    Changing perspective programaticaly - part 2
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@33136 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/scripting/CmdPerspective.java
+++ common/src/geogebra/common/kernel/scripting/CmdPerspective.java
@@ -5,13 +5,19 @@ import geogebra.common.gui.toolbar.ToolBar;
 import geogebra.common.io.layout.DockPanelData;
 import geogebra.common.io.layout.DockSplitPaneData;
 import geogebra.common.io.layout.Perspective;
-import geogebra.common.javax.swing.GSplitPane;
 import geogebra.common.kernel.Kernel;
+import geogebra.common.kernel.StringTemplate;
 import geogebra.common.kernel.arithmetic.Command;
+import geogebra.common.kernel.arithmetic.ExpressionNode;
+import geogebra.common.kernel.arithmetic.ExpressionValue;
+import geogebra.common.kernel.arithmetic.Variable;
 import geogebra.common.kernel.commands.CmdScripting;
 import geogebra.common.kernel.geos.GeoElement;
 import geogebra.common.kernel.geos.GeoText;
+import geogebra.common.kernel.parser.ParseException;
 import geogebra.common.main.App;
+import geogebra.common.plugin.Operation;
+import geogebra.common.util.debug.Log;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,7 +30,11 @@ public class CmdPerspective extends CmdScripting {
 		viewCodes.put("A", new DockPanelData(App.VIEW_ALGEBRA, null, false, false, false, AwtFactory.prototype.newRectangle(100, 100, 250, 400), "3,3", 200));
 		viewCodes.put("S", new DockPanelData(App.VIEW_SPREADSHEET, null, true, false, false, AwtFactory.prototype.newRectangle(100, 100, 600, 400), "3", 300));
 		viewCodes.put("C", new DockPanelData(App.VIEW_CAS, null, false, false, false, AwtFactory.prototype.newRectangle(100, 100, 600, 400), "3,1", 300));
-		viewCodes.put("P", new DockPanelData(App.VIEW_PROPERTIES, null, false, true, true, AwtFactory.prototype.newRectangle(100, 100, 700, 550), "1,1", 400));
+		viewCodes.put("P", new DockPanelData(App.VIEW_PROPERTIES, null, false, false, true, AwtFactory.prototype.newRectangle(100, 100, 700, 550), "1,1", 400));
+		
+		viewCodes.put("L", new DockPanelData(App.VIEW_CONSTRUCTION_PROTOCOL, null, false, false, true, AwtFactory.prototype.newRectangle(100, 100, 700, 550), "1,1", 400));
+		viewCodes.put("D", new DockPanelData(App.VIEW_EUCLIDIAN2, null, false, false, true, AwtFactory.prototype.newRectangle(100, 100, 700, 550), "1,1", 400));
+		viewCodes.put("T", new DockPanelData(App.VIEW_EUCLIDIAN3D, null, false, false, true, AwtFactory.prototype.newRectangle(100, 100, 700, 550), "1,1", 400));
 	}
 	public CmdPerspective(Kernel kernel) {
 		super(kernel);
@@ -40,33 +50,101 @@ public class CmdPerspective extends CmdScripting {
 			String defToolbar = ToolBar.getAllToolsNoMacros(true, app.isHTML5Applet());
 			
 			String code  = ((GeoText)args[0]).getTextString();
-			int views = 0;
+			String longCode = "";
+			for(int i = 0;i<code.length();i++){
+				longCode += code.charAt(i)+" ";
+			}
+			ExpressionValue expr;
+			try {
+				expr = kernelA.getParser().parseGeoGebraExpression(longCode).wrap();
+			} catch (ParseException e) {				
+				e.printStackTrace();
+				return;
+			}
 			ArrayList<DockPanelData> panelList = new ArrayList<DockPanelData>();
-			String[] paths = new String[]{"3","1,3","1,1"};
-			for(int i=0;i<code.length();i++){
-				if(viewCodes.get(code.charAt(i)+"")!=null){
-					viewCodes.get(code.charAt(i)+"").setVisible(true);
-					viewCodes.get(code.charAt(i)+"").setLocation(paths[i]);
-					panelList.add(viewCodes.get(code.charAt(i)+""));
-					views++;
-				}
-			}
-			DockSplitPaneData[] spData = new DockSplitPaneData[views];
-			for(int i=0;i<views;i++){
-				spData[i] = new DockSplitPaneData("1,1,1,1,1,1,1".substring(0,2*i),0.3,GSplitPane.HORIZONTAL_SPLIT);
-			}
+			ArrayList<DockSplitPaneData> splitList = new ArrayList<DockSplitPaneData>();
+			buildPerspective(expr.unwrap(),"","",panelList,splitList,1,0.8);
+			
+			DockSplitPaneData[] spData = new DockSplitPaneData[splitList.size()];
+			splitList.toArray(spData);
+			
 			
 			DockPanelData[] dpData = new DockPanelData[panelList.size()];
 			panelList.toArray(dpData);
 		
 			
-			Perspective ps =  new Perspective("TableAndGraphics", spData, dpData, defToolbar, true, false, true, true, true, false);
+			Perspective ps =  new Perspective("Custom", spData, dpData, defToolbar, true, false, true, true, true, false);
+			try{
 			app.getGuiManager().getLayout().applyPerspective(ps);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
 			return;
 		}
 		throw this.argErr(app, c.getName(),args[0]);
 		
 		
+		
+	}
+
+	private void buildPerspective(ExpressionValue expr, String panelPath,
+			String splitPath,
+			ArrayList<DockPanelData> panelList,
+			ArrayList<DockSplitPaneData> splitList, double totalWidth, double totalHeight) {
+		
+		if(expr instanceof Variable){
+			String code = ((Variable)expr).getName(StringTemplate.defaultTemplate);
+			if(viewCodes.get(code)!=null){
+				viewCodes.get(code).setVisible(true);
+				viewCodes.get(code).setLocation(panelPath.length()>0?panelPath.substring(1):"");
+				panelList.add(viewCodes.get(code));
+			}
+		}
+		else
+		if(expr instanceof ExpressionNode){
+			Log.debug(expr);
+			App.debug("HEIGHT"+totalHeight);
+			ExpressionNode en = (ExpressionNode)expr;
+			boolean horizontal = ((ExpressionNode) expr).getOperation() == Operation.MULTIPLY;
+
+			double ratio = size(en.getLeft(),horizontal)/size(en,horizontal);
+			App.debug("RATIO"+ratio);
+			double height1 = totalHeight;
+			double width1 = totalWidth;
+			double height2 = totalHeight;
+			double width2 = totalWidth;
+			if(horizontal){
+				width1 = width1 * ratio;
+				width2 = width2 * (1-ratio);
+			}else{
+				height1 = height1 * ratio;
+				height2 = height2 * (1-ratio);
+			}
+			App.debug("HEIGHT1"+height1);
+
+			splitList.add(new DockSplitPaneData(splitPath.length()>0?splitPath.substring(1):"", horizontal ? width1 : height1, horizontal ? 1 : 0));
+			this.buildPerspective(en.getRight().unwrap(), panelPath+(horizontal ? ",1" : ",2"), splitPath+",1", panelList, splitList, width2, height2);
+			this.buildPerspective(en.getLeft().unwrap(), panelPath+(horizontal ? ",3" : ",0"), splitPath+",0", panelList, splitList, width1, height1);
+		}else{
+			App.error("Wrong type"+expr.getClass().getName());
+		}
+	}
+
+	private double size(ExpressionValue expr, boolean horizontal) {
+		if(expr instanceof Variable && horizontal){
+			String name = ((Variable)expr).getName(StringTemplate.defaultTemplate);
+			if("A".equals(name) || "C".equals(name)){
+				return 0.5;
+			}
+		}
+		if(!(expr instanceof ExpressionNode)){
+			return 1;
+		}
+		ExpressionNode en = (ExpressionNode)expr;
+		if(en.getOperation()!=Operation.NO_OPERATION && en.getRight()!=null){
+			return size(en.getLeft(), horizontal)+size(en.getRight(), horizontal);
+		}
+		return size(en.getLeft(),horizontal);
 		
 	}
 
