46f6a2fe3eb47f3d06696374a402cb01d2624a1e
Laszlo Gal <laszlo@geogebra.org>
2014-04-08 14:21:16 +0000

    #4212 Putting it back.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@33371 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/geos/GeoElement.java
+++ common/src/geogebra/common/kernel/geos/GeoElement.java
@@ -7154,9 +7154,9 @@ public abstract class GeoElement extends ConstructionElement implements
 	 */
 	public boolean showLineProperties(){
 		return isPath()
-//				|| (isGeoNumeric() && (((GeoNumeric) this)
-//						.isDrawable() 
-//				|| this.getDefaultGeoType() >=0 ))
+				|| (isGeoNumeric() && (((GeoNumeric) this)
+						.isDrawable() 
+				|| this.getDefaultGeoType() >=0 ))
 				|| ((this instanceof GeoFunctionNVar) && ((GeoFunctionNVar) this)
 				.isInequality());
 	}
