388718619d524608551bb754ca7fd86ecff5a7dd
Mathieu Blossier <mathieublossier@free.fr>
2014-04-10 10:27:16 +0000

    Comment out "real embedded dimensions" (need better method to have also correct mouse position, etc.)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@33405 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/gui/layout/DockManagerW.java
+++ web/src/geogebra/web/gui/layout/DockManagerW.java
@@ -397,8 +397,7 @@ public class DockManagerW extends DockManager {
 		//		}
 		//	}
 			
-			setEmbeddedDim(rootPane, windowWidth, windowHeight);
-			
+			//setEmbeddedDim(rootPane, windowWidth, windowHeight);
 			
 		}
 		
@@ -407,48 +406,48 @@ public class DockManagerW extends DockManager {
 	}
 	
 	
-	private void setEmbeddedDim(DockSplitPaneW pane, int width, int height){
-		
-		/*
-		App.debug("----------------------------");
-		App.debug(width+" x "+height);
-		App.debug(pane.getOrientation()+" - "+pane.getDividerLocation());
-		App.debug("pane.getSplitterSize() = "+pane.getSplitterSize());
-		*/
-		
-		int divLoc = pane.getDividerLocation();
-		
-		Widget left = pane.getLeftComponent();
-		Widget right = pane.getRightComponent();
-		
-		int w1, h1, w2, h2;
-		if (pane.getOrientation() == DockSplitPaneW.HORIZONTAL_SPLIT){
-			w1 = divLoc;
-			w2 = width - divLoc - pane.getSplitterSize();
-			h1 = height;
-			h2 = height;
-		}else{
-			w1 = width;
-			w2 = width;
-			h1 = divLoc;
-			h2 = height - divLoc - pane.getSplitterSize();
-		}
-		
-		if (left instanceof DockSplitPaneW){
-			setEmbeddedDim((DockSplitPaneW) left, w1, h1);
-		}else{
-			((DockPanelW) left).setEmbeddedDim(w1, h1);
-		}
-		
-		if (right instanceof DockSplitPaneW){
-			setEmbeddedDim((DockSplitPaneW) right, w2, h2);
-		}else{
-			((DockPanelW) right).setEmbeddedDim(w2, h2);
-		}
-
-		
-	}
-	
+//	private void setEmbeddedDim(DockSplitPaneW pane, int width, int height){
+//		
+//		/*
+//		App.debug("----------------------------");
+//		App.debug(width+" x "+height);
+//		App.debug(pane.getOrientation()+" - "+pane.getDividerLocation());
+//		App.debug("pane.getSplitterSize() = "+pane.getSplitterSize());
+//		*/
+//		
+//		int divLoc = pane.getDividerLocation();
+//		
+//		Widget left = pane.getLeftComponent();
+//		Widget right = pane.getRightComponent();
+//		
+//		int w1, h1, w2, h2;
+//		if (pane.getOrientation() == DockSplitPaneW.HORIZONTAL_SPLIT){
+//			w1 = divLoc;
+//			w2 = width - divLoc - pane.getSplitterSize();
+//			h1 = height;
+//			h2 = height;
+//		}else{
+//			w1 = width;
+//			w2 = width;
+//			h1 = divLoc;
+//			h2 = height - divLoc - pane.getSplitterSize();
+//		}
+//		
+//		if (left instanceof DockSplitPaneW){
+//			setEmbeddedDim((DockSplitPaneW) left, w1, h1);
+//		}else{
+//			((DockPanelW) left).setEmbeddedDim(w1, h1);
+//		}
+//		
+//		if (right instanceof DockSplitPaneW){
+//			setEmbeddedDim((DockSplitPaneW) right, w2, h2);
+//		}else{
+//			((DockPanelW) right).setEmbeddedDim(w2, h2);
+//		}
+//
+//		
+//	}
+//	
 	/**
 	 * Sets split pane divider locations 
 	 * @param spData
