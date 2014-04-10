4ae5c170ef7085a35ef58a9babf54c4717db8684
Zbyněk Konečný <zbynek@geogebra.org>
2014-04-10 12:16:06 +0000

    Refresh 3D view size on SetPerspective / file reload etc.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@33409 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/geogebra3D/web/gui/GuiManager3DW.java
+++ web/src/geogebra/geogebra3D/web/gui/GuiManager3DW.java
@@ -2,6 +2,7 @@ package geogebra.geogebra3D.web.gui;
 
 import geogebra.geogebra3D.web.gui.layout.panels.EuclidianDockPanel3DW;
 import geogebra.web.gui.GuiManagerW;
+import geogebra.web.gui.layout.DockPanelW;
 import geogebra.web.main.AppW;
 
 /**
@@ -11,6 +12,7 @@ import geogebra.web.main.AppW;
  */
 public class GuiManager3DW extends GuiManagerW {
 
+	private DockPanelW euclidian3Dpanel;
 	/**
 	 * constructor
 	 * @param app application
@@ -24,11 +26,16 @@ public class GuiManager3DW extends GuiManagerW {
     protected boolean initLayoutPanels() {
 
 		if (super.initLayoutPanels()){
-			layout.registerPanel(new EuclidianDockPanel3DW((AppW) app));
+			this.euclidian3Dpanel = new EuclidianDockPanel3DW((AppW) app);
+			layout.registerPanel(this.euclidian3Dpanel);
 			return true;
 		}
 		
 		return false;
 		
+	}
+	
+	public DockPanelW getEuclidian3DPanel(){
+		return this.euclidian3Dpanel;
 	}
 }
