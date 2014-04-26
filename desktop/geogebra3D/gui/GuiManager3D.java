df3717ee1603abfe4d7cd9350df4ea6c40800023
Michael Borcherds <michael@geogebra.org>
2014-04-26 20:26:36 +0000

    bugfix: settings not set for EuclidianView2 (with 3D kernel)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@33610 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra3D/gui/GuiManager3D.java
+++ desktop/geogebra3D/gui/GuiManager3D.java
@@ -6,6 +6,7 @@ import geogebra.common.euclidian3D.EuclidianView3DInterface;
 import geogebra.common.kernel.geos.GeoElement;
 import geogebra.common.kernel.kernelND.ViewCreator;
 import geogebra.common.main.App;
+import geogebra.common.main.settings.EuclidianSettings;
 import geogebra.euclidian.EuclidianViewD;
 import geogebra.euclidianND.EuclidianViewInterfaceDesktop;
 import geogebra.gui.ContextMenuChooseGeoD;
@@ -35,29 +36,30 @@ import javax.swing.AbstractAction;
  * Extending DefaultGuiManager class for 3D
  * 
  * @author matthieu
- *
+ * 
  */
 public class GuiManager3D extends GuiManagerD {
 
-	
 	private AbstractAction showAxes3DAction, showGrid3DAction, showPlaneAction;
-	
-	/** 
+
+	/**
 	 * default constructor
-	 * @param app application
+	 * 
+	 * @param app
+	 *            application
 	 */
 	public GuiManager3D(AppD app) {
 		super(app);
 		javax.swing.JPopupMenu.setDefaultLightWeightPopupEnabled(false);
-		
+
 		dialogManagerFactory = new DialogManager3D.Factory();
 	}
-	
+
 	@Override
 	public void initialize() {
 		super.initialize();
 	}
-	
+
 	/**
 	 * Add 3D euclidian view to layout.
 	 */
@@ -67,15 +69,14 @@ public class GuiManager3D extends GuiManagerD {
 		EuclidianDockPanel3D panel = new EuclidianDockPanel3D(getApp());
 		getLayout().registerPanel(panel);
 	}
-	
-	
-	//////////////////////////////
+
+	// ////////////////////////////
 	// ACTIONS
-	//////////////////////////////
-	
+	// ////////////////////////////
+
 	@Override
 	protected boolean initActions() {
-		
+
 		if (!super.initActions())
 			return false;
 		showAxes3DAction = new AbstractAction(getApp().getMenu("Axes"),
@@ -85,10 +86,10 @@ public class GuiManager3D extends GuiManagerD {
 			public void actionPerformed(ActionEvent e) {
 				// toggle axes
 				((App3D) getApp()).toggleAxis3D();
-				//getApp().getEuclidianView().repaint();
+				// getApp().getEuclidianView().repaint();
 				getApp().storeUndoInfo();
 				getApp().updateMenubar();
-				
+
 			}
 		};
 
@@ -99,13 +100,13 @@ public class GuiManager3D extends GuiManagerD {
 			public void actionPerformed(ActionEvent e) {
 				// toggle grid
 				((App3D) getApp()).toggleGrid3D();
-				//getApp().getEuclidianView().repaint();
+				// getApp().getEuclidianView().repaint();
 				getApp().storeUndoInfo();
 				getApp().updateMenubar();
-				
+
 			}
 		};
-		
+
 		showPlaneAction = new AbstractAction(getApp().getMenu("Plane"),
 				(getApp()).getImageIcon("plane.gif")) {
 			private static final long serialVersionUID = 1L;
@@ -118,11 +119,10 @@ public class GuiManager3D extends GuiManagerD {
 			}
 		};
 
-		
 		return true;
-		
+
 	}
-	
+
 	public AbstractAction getShowAxes3DAction() {
 		initActions();
 		return showAxes3DAction;
@@ -132,158 +132,155 @@ public class GuiManager3D extends GuiManagerD {
 		initActions();
 		return showGrid3DAction;
 	}
-	
+
 	public AbstractAction getShowPlaneAction() {
 		initActions();
 		return showPlaneAction;
 	}
-	
-	
-	
-	//////////////////////////////
+
+	// ////////////////////////////
 	// POPUP MENU
-	//////////////////////////////
-	
+	// ////////////////////////////
+
 	/**
 	 * Displays the zoom menu at the position p in the coordinate space of
 	 * euclidianView
 	 */
 	/*
-	public void showDrawingPadPopup(Component invoker, Point p) {
-		// clear highlighting and selections in views		
-		app.getEuclidianView().resetMode();
-		
-		// menu for drawing pane context menu
-		ContextMenuGraphicsWindow3D popupMenu = new ContextMenuGraphicsWindow3D(
-				app, p.x, p.y);
-		popupMenu.show(invoker, p.x, p.y);
-	}
-	*/
-	
+	 * public void showDrawingPadPopup(Component invoker, Point p) { // clear
+	 * highlighting and selections in views app.getEuclidianView().resetMode();
+	 * 
+	 * // menu for drawing pane context menu ContextMenuGraphicsWindow3D
+	 * popupMenu = new ContextMenuGraphicsWindow3D( app, p.x, p.y);
+	 * popupMenu.show(invoker, p.x, p.y); }
+	 */
+
 	/**
 	 * Displays the zoom menu at the position p in the coordinate space of
 	 * euclidianView
-	 * @param view view
-	 * @param p zoom point
+	 * 
+	 * @param view
+	 *            view
+	 * @param p
+	 *            zoom point
 	 */
-	public void showDrawingPadPopup3D(EuclidianViewInterfaceCommon view, geogebra.common.awt.GPoint p) {
-		// clear highlighting and selections in views		
+	public void showDrawingPadPopup3D(EuclidianViewInterfaceCommon view,
+			geogebra.common.awt.GPoint p) {
+		// clear highlighting and selections in views
 		((App3D) getApp()).getEuclidianView3D().resetMode();
-		
+
 		// menu for drawing pane context menu
 		ContextMenuGraphicsWindow3D popupMenu = new ContextMenuGraphicsWindow3D(
 				getApp(), p.x, p.y);
-		popupMenu.getWrappedPopup().show(((EuclidianViewInterfaceDesktop) view).getJPanel(), p.x, p.y);
+		popupMenu.getWrappedPopup().show(
+				((EuclidianViewInterfaceDesktop) view).getJPanel(), p.x, p.y);
 	}
-	
-	
-	
+
 	/**
 	 * Displays the popup menu for geo at the position p in the coordinate space
 	 * of the component invoker
-	 * @param selectedGeos first geos
-	 * @param geos list of geos
-	 * @param view view calling
-	 * @param p place to show the popup menue
+	 * 
+	 * @param selectedGeos
+	 *            first geos
+	 * @param geos
+	 *            list of geos
+	 * @param view
+	 *            view calling
+	 * @param p
+	 *            place to show the popup menue
 	 */
 	@Override
 	public void showPopupChooseGeo(ArrayList<GeoElement> selectedGeos,
 			ArrayList<GeoElement> geos, EuclidianView view,
 			geogebra.common.awt.GPoint p) {
-		
+
 		if (selectedGeos == null || selectedGeos.get(0) == null)
 			return;
 
 		// clear highlighting and selections in views
 		getApp().getActiveEuclidianView().resetMode();
-		
+
 		Component invoker = ((EuclidianViewInterfaceDesktop) view).getJPanel();
 
 		Point screenPos = (invoker == null) ? new Point(0, 0) : invoker
 				.getLocationOnScreen();
 		screenPos.translate(p.x, p.y);
 
-
-		ContextMenuGeoElementD popupMenu = new ContextMenuChooseGeoD(getApp(), view, selectedGeos, geos, screenPos, p);
+		ContextMenuGeoElementD popupMenu = new ContextMenuChooseGeoD(getApp(),
+				view, selectedGeos, geos, screenPos, p);
 		popupMenu.getWrappedPopup().show(invoker, p.x, p.y);
 
-
 	}
 
-
-	
-	
-	//////////////////////////////
+	// ////////////////////////////
 	// ALGEBRA VIEW
-	//////////////////////////////
-	
+	// ////////////////////////////
+
 	@Override
-	protected AlgebraViewD newAlgebraView(AlgebraControllerD algc){
+	protected AlgebraViewD newAlgebraView(AlgebraControllerD algc) {
 		return new AlgebraView3D(algc);
 	}
-	
+
 	@Override
-	protected EuclidianViewD newEuclidianView(boolean[] showAxis, boolean showGrid, int id){
-		return new EuclidianViewFor3D(new EuclidianControllerFor3DD(kernel), showAxis, showGrid, id, null);
+	protected EuclidianViewD newEuclidianView(boolean[] showAxis,
+			boolean showGrid, int viewId) {
+
+		EuclidianSettings settings = app.getSettings().getEuclidian(viewId);
+
+		return new EuclidianViewFor3D(new EuclidianControllerFor3DD(kernel),
+				showAxis, showGrid, viewId, settings);
 	}
-	
-	
-	
-	
-	//////////////////////////////
+
+	// ////////////////////////////
 	// 3D VIEW
-	//////////////////////////////
-	
+	// ////////////////////////////
+
 	@Override
 	public void attachView(int viewId) {
-		
+
 		switch (viewId) {
 		case App.VIEW_EUCLIDIAN3D:
 			App.debug("TODO: attach 3D view?");
 			break;
-		default: 
+		default:
 			/*
-			if (viewId>=App.VIEW_EUCLIDIAN_FOR_PLANE_START && viewId<=App.VIEW_EUCLIDIAN_FOR_PLANE_END)
-				App.debug("TODO: attach view for plane?");
-			else
-			*/
-				super.attachView(viewId);
+			 * if (viewId>=App.VIEW_EUCLIDIAN_FOR_PLANE_START &&
+			 * viewId<=App.VIEW_EUCLIDIAN_FOR_PLANE_END)
+			 * App.debug("TODO: attach view for plane?"); else
+			 */
+			super.attachView(viewId);
 			break;
 		}
 	}
-	
-	
+
 	@Override
-	protected PropertiesViewD newPropertiesViewD(AppD appD){
+	protected PropertiesViewD newPropertiesViewD(AppD appD) {
 		return new PropertiesView3D(appD);
 	}
-	
+
 	@Override
-	public DockPanel createEuclidianDockPanelForPlane(int id, String plane){
-		
+	public DockPanel createEuclidianDockPanelForPlane(int id, String plane) {
+
 		GeoElement geo = kernel.lookupLabel(plane);
-		if (geo==null)
+		if (geo == null)
 			return null;
 		if (!(geo instanceof ViewCreator))
 			return null;
-		
-		ViewCreator vc = (ViewCreator) geo;//getViewCreator(id);
-		vc.setEuclidianViewForPlane(((App3D)getApp()).createEuclidianViewForPlane(vc,false));
-		return ((App3D)getApp()).getPanelForPlane();
+
+		ViewCreator vc = (ViewCreator) geo;// getViewCreator(id);
+		vc.setEuclidianViewForPlane(((App3D) getApp())
+				.createEuclidianViewForPlane(vc, false));
+		return ((App3D) getApp()).getPanelForPlane();
 	}
-	
-	
-	
+
 	@Override
 	public void setLabels() {
-		
-		super.setLabels();
-		
-		EuclidianView3DInterface view = app.getEuclidianView3D();
-		if (view != null
-				&& ((EuclidianView) view).hasStyleBar())
-			((EuclidianView) view).getStyleBar().setLabels();
 
+		super.setLabels();
+
+		EuclidianView3DInterface view = app.getEuclidianView3D();
+		if (view != null && ((EuclidianView) view).hasStyleBar())
+			((EuclidianView) view).getStyleBar().setLabels();
 
 	}
 }
