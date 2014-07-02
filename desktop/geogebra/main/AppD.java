1019f3c6578b1c7785746201f2abaf3a4d678b6c
Zbyněk Konečný <zbynek@geogebra.org>
2014-07-02 15:43:11 +0000

    Less debugging
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@34707 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra/main/AppD.java
+++ desktop/geogebra/main/AppD.java
@@ -3600,7 +3600,6 @@ public class AppD extends App implements KeyEventDispatcher {
 	@Override
 	public void storeUndoInfo() {
 		if (isUndoActive()) {
-			App.printStacktrace("");
 			kernel.storeUndoInfo();
 			setUnsaved();
 		}
