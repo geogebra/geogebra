3539adb2f45991dcb09af3c7d77f6fac09d19994
Melanie Tomaschko <melanie.tomaschko@geogebra.org>
2014-07-03 09:15:34 +0000

    NOT IN USE YET - for phoneGUI
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@34726 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/geogebra/html5/gui/browser/BrowseViewPanel.java
@@ -0,0 +1,151 @@
+package geogebra.html5.gui.browser;
+
+import geogebra.common.main.App;
+import geogebra.common.move.ggtapi.models.Material;
+import geogebra.common.move.views.BooleanRenderable;
+import geogebra.html5.gui.ResizeListener;
+import geogebra.html5.gui.browser.SearchPanel.SearchListener;
+import geogebra.html5.main.AppWeb;
+import geogebra.html5.move.ggtapi.models.GeoGebraTubeAPIW;
+import geogebra.html5.move.ggtapi.models.MaterialCallback;
+
+import java.util.List;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.FlowPanel;
+
+/**
+ * NOT IN USE YET
+ * 
+ * The container for the {@link SearchPanel} and the {@link MaterialListPanel}
+ */
+public class BrowseViewPanel extends FlowPanel implements BooleanRenderable, ResizeListener {
+	
+	protected final int SEARCH_PANEL_HEIGHT = 40;
+	
+	protected AppWeb app;
+	
+	protected String lastQuery;
+	/**
+	 * last selected {@link MaterialListElement material}
+	 */
+	protected MaterialListElement lastSelected;
+	/**
+	 * a panel with available {@link MaterialListElement materials}
+	 */
+	protected MaterialListPanel filePanel;
+	/** */
+	protected FlowPanel container;
+	
+	protected SearchPanel searchPanel;
+	
+	public BrowseViewPanel(AppWeb app) {
+		this.setStyleName("contentPanel");
+		this.app = app;
+		
+		this.container = new FlowPanel();
+		this.add(this.container);
+		
+		addSearchPanel();
+		addFilePanel();
+	}
+
+	private void addSearchPanel() {
+		this.searchPanel = new SearchPanel(app);
+		this.searchPanel.addSearchListener(new SearchListener() {
+          @Override
+			public void onSearch(final String query) {
+        	  	System.out.println("onSearch");
+				BrowseViewPanel.this.displaySearchResults(query);
+			}
+		});
+		this.container.add(this.searchPanel);
+	}
+
+	protected void addFilePanel() {
+		this.filePanel = new MaterialListPanel(this.app);
+		this.container.add(this.filePanel);
+	}
+
+	public void loadFeatured() {
+		this.lastQuery = null;
+		MaterialCallback rc = new MaterialCallback() {
+			@Override
+			public void onError(final Throwable exception) {
+				// FIXME implement Error Handling!
+				exception.printStackTrace();
+				App.debug(exception.getMessage());
+			}
+
+			@Override
+			public void onLoaded(final List<Material> response) {
+				onSearchResults(response);
+			}
+		};
+		GeoGebraTubeAPIW api = (GeoGebraTubeAPIW) app.getLoginOperation().getGeoGebraTubeAPI();
+		
+		if(app.getLoginOperation().isLoggedIn()){
+			api.getUsersMaterials(app.getLoginOperation().getModel().getUserId(), rc);
+		}else{
+			api.getFeaturedMaterials(rc);
+		}
+	}
+
+	public void onSearchResults(final List<Material> response) {
+		this.filePanel.setMaterials(response);
+	}
+	
+	public MaterialListElement getChosenMaterial() {
+		return this.lastSelected;
+	}
+
+	public void unselectMaterials() {
+		if (this.lastSelected != null) {
+			this.lastSelected.markUnSelected();
+		}
+	}
+
+	public void rememberSelected(final MaterialListElement materialElement) {
+		this.lastSelected = materialElement;
+	}
+	
+	public void displaySearchResults(final String query) {
+		System.out.println("displaySearchResults BrowseViewPanel");
+		this.lastQuery = query;
+		((GeoGebraTubeAPIW) this.app.getLoginOperation().getGeoGebraTubeAPI()).search(
+				query, new MaterialCallback() {
+					@Override
+					public void onError(final Throwable exception) {
+						// FIXME implement Error Handling!
+						exception.printStackTrace();
+						App.debug(exception.getMessage());
+					}
+
+					@Override
+					public void onLoaded(final List<Material> response) {
+						onSearchResults(response);
+					}
+				});
+	}
+	
+	@Override
+	public void render(final boolean b) {
+		if (!b) {
+			this.filePanel.clearList();
+		} else if (this.lastQuery != null) {
+			this.displaySearchResults(this.lastQuery);
+		} else {
+			this.loadFeatured();
+		}
+	}
+
+	@Override
+	public void onResize() {
+		this.setPixelSize(Window.getClientWidth()-70, Window.getClientHeight()-61);
+		this.filePanel.onResize();
+	}
+
+	public void setLabels() {
+		this.filePanel.setLabels();
+	}
+}
---
+++ web/src/geogebra/html5/gui/browser/MaterialListPanel.java
@@ -0,0 +1,93 @@
+package geogebra.html5.gui.browser;
+
+import geogebra.common.move.ggtapi.models.Material;
+import geogebra.html5.gui.ResizeListener;
+import geogebra.html5.main.AppWeb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.FlowPanel;
+
+/**
+ * NOT IN USE YET
+ * 
+ * The preview panel for the {@link MaterialListElement materials}
+ *
+ */
+public class MaterialListPanel extends FlowPanel implements ResizeListener {
+	
+	protected List<MaterialListElement> materials = new ArrayList<MaterialListElement>();
+	protected AppWeb app;
+	
+	/**
+	 * @param app AppWeb
+	 */
+	public MaterialListPanel(AppWeb app) {
+		this.app = app;
+//		this.setStyleName("materialListPanel");
+		this.setStyleName("filePanel");
+		this.setHeight(Window.getClientHeight() - 61 - 65 +"px");
+	}
+	
+	/**
+	 * clears the existing list of materials and adds the new materials (matList)
+	 * @param matList List<Material>
+	 */
+	public void setMaterials(final List<Material> matList) {
+		this.materials.clear();
+
+		for (final Material mat : matList) {
+			addMaterial(mat);
+		}
+	}
+	
+	/**
+	 * adds the given material to the list of {@link MaterialListElement materials} and the preview-panel
+	 * 
+	 * @param mat {@link Material}
+	 */
+	public void addMaterial(Material mat) {
+		final MaterialListElement preview = new MaterialListElement(mat, this.app);
+		this.materials.add(preview);
+		this.add(preview);
+	}
+
+	/**
+	 * removes the given material from the list of {@link MaterialListElement materials} and the preview-panel
+	 * @param mat {@link Material}
+	 */
+	public void removeMaterial(Material mat) {
+		for(MaterialListElement matElem : this.materials) {
+			if (matElem.getMaterial().equals(mat)) {
+				this.materials.remove(matElem);
+				this.remove(matElem);
+			}
+		}
+	}
+
+	/**
+	 * clears the list of materials shown in the preview-panel
+	 */
+	public void clearList() {
+		this.materials.clear();
+	}
+	
+	/**
+	 * 
+	 */
+	public void setLabels() {
+		for (final MaterialListElement e : this.materials) {
+			e.setLabels();
+		}
+	}
+
+	@Override
+	public void onResize() {
+		this.setHeight(Window.getClientHeight() - 61 - 65 +"px");
+		for (MaterialListElement elem : this.materials) {
+			elem.onResize();
+		}
+	}
+}
