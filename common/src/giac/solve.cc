84e0392bb34249a5214567bee62b5c7027b7c62f
Michael Borcherds <michael@geogebra.org>
2014-07-05 19:34:59 +0000

    [giac] dummy commit
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@34804 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/giac/solve.cc
+++ common/src/giac/solve.cc
@@ -4,7 +4,7 @@
 /*
  *  Copyright (C) 2001,14 B. Parisse, R. De Graeve
  *  Institut Fourier, 38402 St Martin d'Heres
- *
+ * 
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 3 of the License, or
