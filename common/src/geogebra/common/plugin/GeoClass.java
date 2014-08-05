1938aabfdfaca72f62ee8c5fb7f5990b58d7c4a4
Michael Borcherds <michael@geogebra.org>
2014-08-05 05:56:32 +0000

    correct Algebra View names for more 3D objects
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@35314 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/plugin/GeoClass.java
+++ common/src/geogebra/common/plugin/GeoClass.java
@@ -1,13 +1,18 @@
 package geogebra.common.plugin;
 
+
+/**
+ * name MUST be in ggbtrans/properties
+ *
+ */
 public enum GeoClass {
 	ANGLE("Angle"), AXIS("Axis"), BOOLEAN("Boolean"), BUTTON("Button"), TEXTFIELD("TextField"), CONIC("Conic"), CONICPART("ConicPart"), FUNCTION("Function"), INTERVAL("Interval"), FUNCTIONCONDITIONAL("FunctionConditional"), IMAGE("Image"), LINE("Line"), LIST("List"), LOCUS("Locus"), NUMERIC("Numeric"), POINT("Point"), POLYGON("Polygon"), RAY("Ray"), SEGMENT("Segment"), TEXT("Text"), VECTOR("Vector"), CURVE_CARTESIAN("CurveCartesian"), CURVE_POLAR("CurvePolar"), IMPLICIT_POLY("ImplicitPoly"), FUNCTION_NVAR("FunctionNVar"), POLYLINE("PolyLine"), PENSTROKE("PenStroke"), SPLINE("CurveCartesian"),
 	TURTLE("Turtle"),
 	CAS_CELL("CasCell"),
 
-	ANGLE3D("Angle","Angle3D"), POINT3D("Point","Point3D"), VECTOR3D("Vector","Vector3D"), SEGMENT3D("Segment","Segment3D"), LINE3D("Line","Line3D"), RAY3D("Ray","Ray3D"), CONIC3D("Conic","Conic3D"), CONICSECTION("Conic3DPart"), AXIS3D("Axis","Axis3D"), CURVE_CARTESIAN3D("CurveCartesian","CurveCartesian3D"), POLYGON3D("Polygon","Polygon3D"), PLANE3D("Plane3D"), QUADRIC("Quadric"), QUADRIC_PART("QuadricPart"), QUADRIC_LIMITED("QuadricLimited"), POLYLINE3D("PolyLine","PolyLine3D"), POLYHEDRON("Polyhedron"), NET("Net"),
+	ANGLE3D("Angle", "Angle3D"), POINT3D("Point", "Point3D"), VECTOR3D("Vector", "Vector3D"), SEGMENT3D("Segment", "Segment3D"), LINE3D("Line", "Line3D"), RAY3D("Ray", "Ray3D"), CONIC3D("Conic", "Conic3D"), CONICSECTION("ConicPart", "Conic3DPart"), AXIS3D("Axis", "Axis3D"), CURVE_CARTESIAN3D("CurveCartesian", "CurveCartesian3D"), POLYGON3D("Polygon", "Polygon3D"), PLANE3D("Plane", "Plane3D"), QUADRIC("Quadric"), QUADRIC_PART("Quadric", "QuadricPart"), QUADRIC_LIMITED("Quadric", "QuadricLimited"), POLYLINE3D("PolyLine", "PolyLine3D"), POLYHEDRON("Polyhedron"), NET("Net"),
 
-	SURFACECARTESIAN3D("SurfaceCartesian3D"),
+	SURFACECARTESIAN3D("Surface", "SurfaceCartesian3D"),
 	
 	CLIPPINGCUBE3D("ClippingCube3D"),
 
@@ -15,6 +20,9 @@ public enum GeoClass {
 
 	DEFAULT("Default");
 	
+	/**
+	 * name MUST be in ggbtrans/properties
+	 */
 	public String name, xmlName;
 
 	GeoClass(String name) {
