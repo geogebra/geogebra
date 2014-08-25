ba30938c75eb31606027cbd50bf324fead60a2c3
Michael Borcherds <michael@geogebra.org>
2014-08-25 13:50:13 +0000

    testing - fix for missing 3D Tool icons in applets
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@35789 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/gui/app/GGWToolBar.java
+++ web/src/geogebra/web/gui/app/GGWToolBar.java
@@ -590,6 +590,84 @@ public class GGWToolBar extends Composite implements RequiresResize{
 
 		case EuclidianConstants.MODE_ZOOM_OUT:
 			return myIconResourceBundle.mode_zoomout_32().getSafeUri().asString();
+			
+			
+			
+			
+			/*
+			 * 3D
+			 */
+			
+		case EuclidianConstants.MODE_CIRCLE_AXIS_POINT:
+			return myIconResourceBundle.mode_circleaxispoint_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_CIRCLE_POINT_RADIUS_DIRECTION:
+			return myIconResourceBundle.mode_circlepointradiusdirection_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_CONE_TWO_POINTS_RADIUS:
+			return myIconResourceBundle.mode_cone_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_CONIFY:
+			return myIconResourceBundle.mode_conify_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_CUBE:
+			return myIconResourceBundle.mode_cube_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_CYLINDER_TWO_POINTS_RADIUS:
+			return myIconResourceBundle.mode_cylinder_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_EXTRUSION:
+			return myIconResourceBundle.mode_extrusion_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_MIRROR_AT_PLANE:
+			return myIconResourceBundle.mode_mirroratplane_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_NET:
+			return myIconResourceBundle.mode_net_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_ORTHOGONAL_PLANE:
+			return myIconResourceBundle.mode_orthogonalplane_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_PARALLEL_PLANE:
+			return myIconResourceBundle.mode_parallelplane_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_PLANE_THREE_POINTS:
+			return myIconResourceBundle.mode_planethreepoint_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_PLANE:
+			return myIconResourceBundle.mode_plane_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_PRISM:
+			return myIconResourceBundle.mode_prism_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_PYRAMID:
+			return myIconResourceBundle.mode_pyramid_32().getSafeUri().asString();
+			
+		case EuclidianConstants.MODE_ROTATE_AROUND_LINE:
+			return myIconResourceBundle.mode_rotatearoundline_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_ROTATEVIEW:
+			return myIconResourceBundle.mode_rotateview_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_SPHERE_TWO_POINTS:
+			return myIconResourceBundle.mode_sphere2_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_SPHERE_POINT_RADIUS:
+			return myIconResourceBundle.mode_spherepointradius_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_TETRAHEDRON:
+			return myIconResourceBundle.mode_tetrahedron_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_VIEW_IN_FRONT_OF:
+			return myIconResourceBundle.mode_viewinfrontof_32().getSafeUri().asString();
+
+		case EuclidianConstants.MODE_VOLUME:
+			return myIconResourceBundle.mode_volume_32().getSafeUri().asString();
+			
+		case EuclidianConstants.MODE_ORTHOGONAL_THREE_D:
+			return myIconResourceBundle.mode_orthogonalthreed_32().getSafeUri().asString();
+			
+		
 
 		default:
 			return "";
