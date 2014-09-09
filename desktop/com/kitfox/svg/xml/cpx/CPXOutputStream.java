a9dd8bcae4d5a19f75d804210924ce0a8934a887
Michael Borcherds <michael@geogebra.org>
2014-09-09 11:32:16 +0000

    fix compile error (wrong imports)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@36200 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/com/kitfox/svg/xml/cpx/CPXOutputStream.java
+++ desktop/com/kitfox/svg/xml/cpx/CPXOutputStream.java
@@ -36,10 +36,10 @@
 
 package com.kitfox.svg.xml.cpx;
 
-import java.io.*;
-import java.util.zip.*;
-import java.security.*;
-import javax.crypto.*;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.Deflater;
 
 /**
  * @author Mark McKay
@@ -47,137 +47,140 @@ import javax.crypto.*;
  */
 public class CPXOutputStream extends FilterOutputStream implements CPXConsts {
 
-    Deflater deflater = new Deflater(Deflater.BEST_COMPRESSION);
+	Deflater deflater = new Deflater(Deflater.BEST_COMPRESSION);
 
-    /** Creates a new instance of CPXOutputStream */
-    public CPXOutputStream(OutputStream os) throws IOException {
-        super(os);
+	/** Creates a new instance of CPXOutputStream */
+	public CPXOutputStream(OutputStream os) throws IOException {
+		super(os);
 
-        //Write magic number
-        os.write(MAGIC_NUMBER);
-    }
-
-    /**
-     * Writes the specified <code>byte</code> to this output stream.
-     * <p>
-     * The <code>write</code> method of <code>FilterOutputStream</code>
-     * calls the <code>write</code> method of its underlying output stream,
-     * that is, it performs <tt>out.write(b)</tt>.
-     * <p>
-     * Implements the abstract <tt>write</tt> method of <tt>OutputStream</tt>.
-     *
-     * @param      b   the <code>byte</code>.
-     * @exception  IOException  if an I/O error occurs.
-     */
-    public void write(int b) throws IOException {
-        final byte[] buf = new byte[1];
-        buf[0] = (byte)b;
-	write(buf, 0, 1);
-    }
-
-    /**
-     * Writes <code>b.length</code> bytes to this output stream.
-     * <p>
-     * The <code>write</code> method of <code>FilterOutputStream</code>
-     * calls its <code>write</code> method of three arguments with the
-     * arguments <code>b</code>, <code>0</code>, and
-     * <code>b.length</code>.
-     * <p>
-     * Note that this method does not call the one-argument
-     * <code>write</code> method of its underlying stream with the single
-     * argument <code>b</code>.
-     *
-     * @param      b   the data to be written.
-     * @exception  IOException  if an I/O error occurs.
-     * @see        java.io.FilterOutputStream#write(byte[], int, int)
-     */
-    public void write(byte b[]) throws IOException {
-	write(b, 0, b.length);
-    }
-
-    byte[] deflateBuffer = new byte[2048];
-
-    /**
-     * Writes <code>len</code> bytes from the specified
-     * <code>byte</code> array starting at offset <code>off</code> to
-     * this output stream.
-     * <p>
-     * The <code>write</code> method of <code>FilterOutputStream</code>
-     * calls the <code>write</code> method of one argument on each
-     * <code>byte</code> to output.
-     * <p>
-     * Note that this method does not call the <code>write</code> method
-     * of its underlying input stream with the same arguments. Subclasses
-     * of <code>FilterOutputStream</code> should provide a more efficient
-     * implementation of this method.
-     *
-     * @param      b     the data.
-     * @param      off   the start offset in the data.
-     * @param      len   the number of bytes to write.
-     * @exception  IOException  if an I/O error occurs.
-     * @see        java.io.FilterOutputStream#write(int)
-     */
-    public void write(byte b[], int off, int len) throws IOException
-    {
-        deflater.setInput(b, off, len);
-
-        processAllData();
-        /*
-        int numDeflatedBytes;
-        while ((numDeflatedBytes = deflater.deflate(deflateBuffer)) != 0)
-        {
-//            byte[] cipherBuf = cipher.update(deflateBuffer, 0, numDeflatedBytes);
-//            out.write(cipherBytes);
-out.write(deflateBuffer, 0, numDeflatedBytes);
-        }
-        */
-    }
-
-    protected void processAllData() throws IOException
-    {
-        int numDeflatedBytes;
-        while ((numDeflatedBytes = deflater.deflate(deflateBuffer)) != 0)
-        {
-//            byte[] cipherBuf = cipher.update(deflateBuffer, 0, numDeflatedBytes);
-//            out.write(cipherBytes);
-out.write(deflateBuffer, 0, numDeflatedBytes);
-        }
-    }
-
-    /**
-     * Flushes this output stream and forces any buffered output bytes
-     * to be written out to the stream.
-     * <p>
-     * The <code>flush</code> method of <code>FilterOutputStream</code>
-     * calls the <code>flush</code> method of its underlying output stream.
-     *
-     * @exception  IOException  if an I/O error occurs.
-     * @see        java.io.FilterOutputStream#out
-     */
-    public void flush() throws IOException {
-	out.flush();
-    }
-
-    /**
-     * Closes this output stream and releases any system resources
-     * associated with the stream.
-     * <p>
-     * The <code>close</code> method of <code>FilterOutputStream</code>
-     * calls its <code>flush</code> method, and then calls the
-     * <code>close</code> method of its underlying output stream.
-     *
-     * @exception  IOException  if an I/O error occurs.
-     * @see        java.io.FilterOutputStream#flush()
-     * @see        java.io.FilterOutputStream#out
-     */
-    public void close() throws IOException {
-        deflater.finish();
-        processAllData();
-
-	try {
-	  flush();
-	} catch (IOException ignored) {
+		// Write magic number
+		os.write(MAGIC_NUMBER);
 	}
-	out.close();
-    }
+
+	/**
+	 * Writes the specified <code>byte</code> to this output stream.
+	 * <p>
+	 * The <code>write</code> method of <code>FilterOutputStream</code> calls
+	 * the <code>write</code> method of its underlying output stream, that is,
+	 * it performs <tt>out.write(b)</tt>.
+	 * <p>
+	 * Implements the abstract <tt>write</tt> method of <tt>OutputStream</tt>.
+	 *
+	 * @param b
+	 *            the <code>byte</code>.
+	 * @exception IOException
+	 *                if an I/O error occurs.
+	 */
+	public void write(int b) throws IOException {
+		final byte[] buf = new byte[1];
+		buf[0] = (byte) b;
+		write(buf, 0, 1);
+	}
+
+	/**
+	 * Writes <code>b.length</code> bytes to this output stream.
+	 * <p>
+	 * The <code>write</code> method of <code>FilterOutputStream</code> calls
+	 * its <code>write</code> method of three arguments with the arguments
+	 * <code>b</code>, <code>0</code>, and <code>b.length</code>.
+	 * <p>
+	 * Note that this method does not call the one-argument <code>write</code>
+	 * method of its underlying stream with the single argument <code>b</code>.
+	 *
+	 * @param b
+	 *            the data to be written.
+	 * @exception IOException
+	 *                if an I/O error occurs.
+	 * @see java.io.FilterOutputStream#write(byte[], int, int)
+	 */
+	public void write(byte b[]) throws IOException {
+		write(b, 0, b.length);
+	}
+
+	byte[] deflateBuffer = new byte[2048];
+
+	/**
+	 * Writes <code>len</code> bytes from the specified <code>byte</code> array
+	 * starting at offset <code>off</code> to this output stream.
+	 * <p>
+	 * The <code>write</code> method of <code>FilterOutputStream</code> calls
+	 * the <code>write</code> method of one argument on each <code>byte</code>
+	 * to output.
+	 * <p>
+	 * Note that this method does not call the <code>write</code> method of its
+	 * underlying input stream with the same arguments. Subclasses of
+	 * <code>FilterOutputStream</code> should provide a more efficient
+	 * implementation of this method.
+	 *
+	 * @param b
+	 *            the data.
+	 * @param off
+	 *            the start offset in the data.
+	 * @param len
+	 *            the number of bytes to write.
+	 * @exception IOException
+	 *                if an I/O error occurs.
+	 * @see java.io.FilterOutputStream#write(int)
+	 */
+	public void write(byte b[], int off, int len) throws IOException {
+		deflater.setInput(b, off, len);
+
+		processAllData();
+		/*
+		 * int numDeflatedBytes; while ((numDeflatedBytes =
+		 * deflater.deflate(deflateBuffer)) != 0) { // byte[] cipherBuf =
+		 * cipher.update(deflateBuffer, 0, numDeflatedBytes); //
+		 * out.write(cipherBytes); out.write(deflateBuffer, 0,
+		 * numDeflatedBytes); }
+		 */
+	}
+
+	protected void processAllData() throws IOException {
+		int numDeflatedBytes;
+		while ((numDeflatedBytes = deflater.deflate(deflateBuffer)) != 0) {
+			// byte[] cipherBuf = cipher.update(deflateBuffer, 0,
+			// numDeflatedBytes);
+			// out.write(cipherBytes);
+			out.write(deflateBuffer, 0, numDeflatedBytes);
+		}
+	}
+
+	/**
+	 * Flushes this output stream and forces any buffered output bytes to be
+	 * written out to the stream.
+	 * <p>
+	 * The <code>flush</code> method of <code>FilterOutputStream</code> calls
+	 * the <code>flush</code> method of its underlying output stream.
+	 *
+	 * @exception IOException
+	 *                if an I/O error occurs.
+	 * @see java.io.FilterOutputStream#out
+	 */
+	public void flush() throws IOException {
+		out.flush();
+	}
+
+	/**
+	 * Closes this output stream and releases any system resources associated
+	 * with the stream.
+	 * <p>
+	 * The <code>close</code> method of <code>FilterOutputStream</code> calls
+	 * its <code>flush</code> method, and then calls the <code>close</code>
+	 * method of its underlying output stream.
+	 *
+	 * @exception IOException
+	 *                if an I/O error occurs.
+	 * @see java.io.FilterOutputStream#flush()
+	 * @see java.io.FilterOutputStream#out
+	 */
+	public void close() throws IOException {
+		deflater.finish();
+		processAllData();
+
+		try {
+			flush();
+		} catch (IOException ignored) {
+		}
+		out.close();
+	}
 }
