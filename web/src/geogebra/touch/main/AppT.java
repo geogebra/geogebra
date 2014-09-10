2c4ea5a53bf4c84795f0f6e5c9862150530e77e0
Thomas Krismayer <thomas.krismayer@geogebra.org>
2014-09-10 08:48:28 +0000

    Menu for Tablet (no 3D)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@36222 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/touch/main/AppT.java
+++ web/src/geogebra/touch/main/AppT.java
@@ -1,5 +1,7 @@
 package geogebra.touch.main;
 
+import geogebra.common.gui.Layout;
+import geogebra.common.io.layout.Perspective;
 import geogebra.common.main.DialogManager;
 import geogebra.html5.gui.History;
 import geogebra.html5.main.FileManager;
@@ -9,40 +11,58 @@ import geogebra.web.gui.app.GeoGebraAppFrame;
 import geogebra.web.gui.laf.GLookAndFeel;
 import geogebra.web.main.AppWapplication;
 
+import java.util.ArrayList;
+
 /**
  * App for tablets and phones
- *
+ * 
  */
 public class AppT extends AppWapplication {
 
 	private History history;
-	
-	public AppT(final ArticleElement article, final GeoGebraAppFrame geoGebraAppFrame,
-            final boolean undoActive, final int dimension, final GLookAndFeel laf) {
-	    super(article, geoGebraAppFrame, undoActive, dimension, laf);
 
-	    this.setLabelDragsEnabled(false);
-    }
-	
-	public AppT(final ArticleElement article, final GeoGebraAppFrame geoGebraAppFrame, final int dimension, final GLookAndFeel laf) {
-		super(article, geoGebraAppFrame, dimension, laf);
-
+	public AppT(final ArticleElement article,
+	        final GeoGebraAppFrame geoGebraAppFrame, final boolean undoActive,
+	        final int dimension, final GLookAndFeel laf) {
+		super(article, geoGebraAppFrame, undoActive, dimension, laf);
 		this.setLabelDragsEnabled(false);
 	}
-	
+
+	public AppT(final ArticleElement article,
+	        final GeoGebraAppFrame geoGebraAppFrame, final int dimension,
+	        final GLookAndFeel laf) {
+		super(article, geoGebraAppFrame, dimension, laf);
+		this.setLabelDragsEnabled(false);
+	}
+
+	@Override
+	protected void afterCoreObjectsInited() {
+		super.afterCoreObjectsInited();
+
+		ArrayList<Perspective> list = new ArrayList<Perspective>();
+		for (Perspective p : Layout.defaultPerspectives) {
+			if (!p.getId().equals("Perspective.3DGraphics")) {
+				list.add(p);
+			}
+		}
+		Layout.defaultPerspectives = list.toArray(new Perspective[0]);
+		setTmpPerspectives(list);
+	}
+
 	public History getHistory() {
-	    if(this.history == null){
-	    	this.history = new History();
-	    }
-	    return this.history;
-    }
-	
+		if (this.history == null) {
+			this.history = new History();
+		}
+		return this.history;
+	}
+
 	/**
 	 * different behavior for phone and tablet
+	 * 
 	 * @return FileManagerInterface
 	 */
 	@Override
-    public FileManager getFileManager() {
+	public FileManager getFileManager() {
 		return null;
 	}
 
