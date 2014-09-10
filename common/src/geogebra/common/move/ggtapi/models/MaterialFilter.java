4d1f2568778e44fb990cdb4915d811c7108eb743
Melanie Tomaschko <melanie.tomaschko@geogebra.org>
2014-09-10 10:22:48 +0000

    new filter
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@36229 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/move/ggtapi/models/MaterialFilter.java
+++ common/src/geogebra/common/move/ggtapi/models/MaterialFilter.java
@@ -14,6 +14,11 @@ public abstract class MaterialFilter {
 		};
 	}
 	
+	/**
+	 * searches for "query" in: title and description of material
+	 * @param query String
+	 * @return {@link MaterialFilter}
+	 */
 	public static MaterialFilter getSearchFilter(final String query){
 		return new MaterialFilter(){
 			@Override
@@ -26,6 +31,20 @@ public abstract class MaterialFilter {
 					ret |= StringUtil.toLowerCase(m.getDescription()).contains(StringUtil.toLowerCase(query));
 				}
 				return ret;
+			}
+		};
+	}
+	
+	/**
+	 * Filter materials from specific user
+	 * @param author String
+	 * @return {@link MaterialFilter}
+	 */
+	public static MaterialFilter getAuthorFilter(final String author) {
+		return new MaterialFilter() {
+			@Override
+			public boolean check(Material m) {
+				return m.getAuthor().equals(author);
 			}
 		};
 	}
