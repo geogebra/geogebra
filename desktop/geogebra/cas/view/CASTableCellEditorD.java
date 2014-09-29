ff4aa46e3e6188d14ce9a67636d3c21da70e2572
Balazs Bencze <megette_a@yahoo.com>
2014-09-29 11:18:50 +0000

    CAS - Don't insert error from previous cell
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@36609 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra/cas/view/CASTableCellEditorD.java
+++ desktop/geogebra/cas/view/CASTableCellEditorD.java
@@ -18,25 +18,27 @@ import javax.swing.table.TableCellEditor;
 import javax.swing.text.JTextComponent;
 
 /**
- * Cell editor; handles keystrokes 
+ * Cell editor; handles keystrokes
  */
-public class CASTableCellEditorD extends CASTableCell implements TableCellEditor, KeyListener, CASTableCellEditor {
-	
+public class CASTableCellEditorD extends CASTableCell implements
+		TableCellEditor, KeyListener, CASTableCellEditor {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private JTable table;
 	private GeoCasCell cellValue;
-	
+
 	private boolean editing = false;
 	private int editingRow;
-	
+
 	private String inputOnEditingStart;
 	private boolean isUseAsTextOnEditingStart;
-		
+
 	private ArrayList<CellEditorListener> listeners = new ArrayList<CellEditorListener>();
 
 	/**
-	 * @param view CAS view
+	 * @param view
+	 *            CAS view
 	 */
 	public CASTableCellEditorD(CASViewD view) {
 		super(view);
@@ -44,153 +46,162 @@ public class CASTableCellEditorD extends CASTableCell implements TableCellEditor
 		getInputArea().addKeyListener(this);
 	}
 
-	public Component getTableCellEditorComponent(JTable casTable, Object value, boolean isSelected, int row, int column) {
-		if (value instanceof GeoCasCell) {						
+	public Component getTableCellEditorComponent(JTable casTable, Object value,
+			boolean isSelected, int row, int column) {
+		if (value instanceof GeoCasCell) {
 			editing = true;
 			editingRow = row;
-									
+
 			// set CASTableCell value
 			this.cellValue = (GeoCasCell) value;
 			this.table = casTable;
-			inputOnEditingStart = cellValue.getInput(StringTemplate.defaultTemplate);
+			inputOnEditingStart = cellValue
+					.getInput(StringTemplate.defaultTemplate);
 			isUseAsTextOnEditingStart = cellValue.isUseAsText();
 			setValue(cellValue);
-			
+
 			boolean isUseAsText = cellValue.isUseAsText();
 			getInputArea().enableColoring(!isUseAsText);
 			getInputArea().setAutoComplete(!isUseAsText);
-							
+
 			// update font and row height
 			setFont(view.getCASViewComponent().getFont());
 			updateTableRowHeight(casTable, row);
-			
+
 			// Set width of editor to the width of the table column.
-			// This will allow scrolling of strings that are wider than the cell. 
-			setInputPanelWidth(casTable.getParent().getWidth());				
-		} 
+			// This will allow scrolling of strings that are wider than the
+			// cell.
+			setInputPanelWidth(casTable.getParent().getWidth());
+		}
 		return this;
-	}	
-	
+	}
+
 	/**
 	 * @return input text
 	 */
-	public String getInputText() {	
+	public String getInputText() {
 		return getInputArea().getText();
 	}
-	
-	public String getInputSelectedText() {	
+
+	public String getInputSelectedText() {
 		return getInputArea().getSelectedText();
 	}
-	
-	public int getInputSelectionStart() {	
+
+	public int getInputSelectionStart() {
 		return getInputArea().getSelectionStart();
 	}
-	
-	public int getInputSelectionEnd() {	
+
+	public int getInputSelectionEnd() {
 		return getInputArea().getSelectionEnd();
-	}	
-	
-	public void setInputSelectionStart(int pos) {	
+	}
+
+	public void setInputSelectionStart(int pos) {
 		getInputArea().setSelectionStart(pos);
 	}
-	
+
 	public void setInputSelectionEnd(int pos) {
 		getInputArea().setSelectionEnd(pos);
-	}	
-	public int getCaretPosition(){
+	}
+
+	public int getCaretPosition() {
 		return getInputArea().getCaretPosition();
 	}
-	public void setCaretPosition(int i){
+
+	public void setCaretPosition(int i) {
 		getInputArea().setCaretPosition(i);
 	}
-	
+
 	/**
 	 * Replaces selection with given text
-	 * @param text text
+	 * 
+	 * @param text
+	 *            text
 	 */
 	public void insertText(String text) {
 		getInputArea().replaceSelection(text);
-		//getInputArea().requestFocusInWindow();
+		// getInputArea().requestFocusInWindow();
 	}
-	
+
 	/**
 	 * Clears input area
 	 */
 	public void clearInputSelectionText() {
 		getInputArea().setText(null);
 	}
-		
-	public boolean stopCellEditing() {	
+
+	public boolean stopCellEditing() {
 		// update cellValue's input using editor content
 		if (editing && cellValue != null) {
 			String newInput = getInput();
-			if (!newInput.equals(inputOnEditingStart) 
+			if (!newInput.equals(inputOnEditingStart)
 					|| cellValue.isUseAsText() != isUseAsTextOnEditingStart)
 				cellValue.setInput(getInput());
 			fireEditingStopped();
-		}				
-		
+		}
+
 		return true;
 	}
-	
+
 	public void cancelCellEditing() {
 		// update cellValue's input using editor content
 		if (editing && cellValue != null) {
 			String newInput = getInput();
 			if (!newInput.equals(inputOnEditingStart)
 					|| cellValue.isUseAsText() != isUseAsTextOnEditingStart)
-				cellValue.setInput(getInput());	
+				cellValue.setInput(getInput());
 			fireEditingCanceled();
 		}
 	}
-	
+
 	/**
 	 * @return whether this cell is being edited
 	 */
 	public boolean isEditing() {
-		return editing; //&& hasFocus();
+		return editing; // && hasFocus();
 	}
-	
-	public Object getCellEditorValue() {		
+
+	public Object getCellEditorValue() {
 		return cellValue;
 	}
+
 	/**
 	 * Editing canceled
 	 */
-	protected void fireEditingCanceled() {				
-		if (editing && editingRow < table.getRowCount()) {	
+	protected void fireEditingCanceled() {
+		if (editing && editingRow < table.getRowCount()) {
 			ChangeEvent ce = new ChangeEvent(this);
-			for (int i=0; i < listeners.size(); i++) {
+			for (int i = 0; i < listeners.size(); i++) {
 				CellEditorListener l = listeners.get(i);
 				l.editingCanceled(ce);
 			}
 		}
-		
+
 		editing = false;
 	}
+
 	/**
 	 * Editing stopped
 	 */
-	protected void fireEditingStopped() {		
-		if (editing && editingRow < table.getRowCount()) {	
+	protected void fireEditingStopped() {
+		if (editing && editingRow < table.getRowCount()) {
 			ChangeEvent ce = new ChangeEvent(this);
-			for (int i=0; i < listeners.size(); i++) {
+			for (int i = 0; i < listeners.size(); i++) {
 				CellEditorListener l = listeners.get(i);
 				l.editingStopped(ce);
 			}
 		}
-		
-		editing = false;		
+
+		editing = false;
 	}
 
-	public boolean isCellEditable(EventObject anEvent) {	
+	public boolean isCellEditable(EventObject anEvent) {
 		return true;
 	}
 
 	public void removeCellEditorListener(CellEditorListener l) {
 		listeners.remove(l);
 	}
-	
+
 	public void addCellEditorListener(CellEditorListener l) {
 		if (!listeners.contains(l))
 			listeners.add(l);
@@ -199,40 +210,40 @@ public class CASTableCellEditorD extends CASTableCell implements TableCellEditor
 	public boolean shouldSelectCell(EventObject anEvent) {
 		return true;
 	}
-	
+
 	/**
 	 * @return index of editing row
 	 */
 	public final int getEditingRow() {
 		return editingRow;
 	}
-	
+
 	public void keyPressed(KeyEvent e) {
 		int keyCode = e.getKeyCode();
 
 		switch (keyCode) {
-			case KeyEvent.VK_ESCAPE:
+		case KeyEvent.VK_ESCAPE:
+			e.consume();
+			getInputArea().setText("");
+			break;
+
+		case KeyEvent.VK_V:
+			if (AppD.isControlDown(e)) {
+				// make sure Ctrl-V isn't passed on to Euclidian View
+				getInputArea().paste();
 				e.consume();
-				getInputArea().setText("");				
-				break;
-				
-			case KeyEvent.VK_V: 
-				if (AppD.isControlDown(e)) { 
-					// make sure Ctrl-V isn't passed on to Euclidian View 
-					getInputArea().paste(); 
-					e.consume(); 
-				} 
-				break; 
-			 	
-//			case KeyEvent.VK_ENTER:
-//				e.consume();
-//				stopCellEditing();				
-//				break;				
+			}
+			break;
+
+		// case KeyEvent.VK_ENTER:
+		// e.consume();
+		// stopCellEditing();
+		// break;
 		}
 	}
 
 	public void keyReleased(KeyEvent arg0) {
-		//do nothing
+		// do nothing
 	}
 
 	public void keyTyped(KeyEvent e) {
@@ -240,37 +251,35 @@ public class CASTableCellEditorD extends CASTableCell implements TableCellEditor
 		JTextComponent inputArea = getInputArea();
 		String text = inputArea.getText();
 		
-		// if closing paranthesis is typed and there is no opening parenthesis for it
-		// add one in the beginning
-		switch (ch){
-		case ' ':
-		case '|':
+		// check for special characters, which insert some text
+		// from the previous cell
+		if (editingRow == 0 || text.length() != 0) {
+			return;
+		}
+		
+		GeoCasCell selCellValue = view.getConsoleTable().getGeoCasCell(editingRow - 1);
+
+		if (!selCellValue.isError()) {
+			switch (ch) {
+			case ' ':
+			case '|':
 				// insert output of previous row (not in parentheses)
-				if (editingRow > 0 && text.length() == 0) {
-					GeoCasCell selCellValue = view.getConsoleTable().getGeoCasCell(editingRow - 1);				
-					inputArea.setText(selCellValue.getOutputRHS(StringTemplate.defaultTemplate) + " ");
-					e.consume();
-				}
+				inputArea.setText(selCellValue.getOutputRHS(StringTemplate.defaultTemplate) + " ");
+				e.consume();
 				break;
-				
 			case ')':
-				// insert output of previous row in parentheses		
-				if (editingRow > 0 && text.length() == 0) {
-					GeoCasCell selCellValue = view.getConsoleTable().getGeoCasCell(editingRow - 1);				
-					String prevOutput = selCellValue.getOutputRHS(StringTemplate.defaultTemplate);
-					inputArea.setText("(" +  prevOutput + ")");
-					e.consume();
-				}
-				break;		
-				
-			case '=':
-				// insert input of previous row
-				if (editingRow > 0 && text.length() == 0) {
-					GeoCasCell selCellValue = view.getConsoleTable().getGeoCasCell(editingRow - 1);				
-					inputArea.setText(selCellValue.getInput(StringTemplate.defaultTemplate));
-					e.consume();
-				}
+				// insert output of previous row in parentheses
+				String prevOutput = selCellValue.getOutputRHS(StringTemplate.defaultTemplate);
+				inputArea.setText("(" + prevOutput + ")");
+				e.consume();
 				break;
+			}
+		}
+		// insert input of previous row
+		// should work on errors also
+		if ('=' == ch) {
+			inputArea.setText(selCellValue.getInput(StringTemplate.defaultTemplate));
+			e.consume();
 		}
 	}
 
