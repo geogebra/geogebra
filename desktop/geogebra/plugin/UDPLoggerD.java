11cf55b845ff2d16880b7f2bc0e277913560e5ab
Árpád Fekete <fekarp@gmail.com>
2014-10-01 13:55:43 +0000

    Commiting Zbynek's patch instead of mine, although I have a quicker solution with the synchronized keyword that works as well (other than I commited yesterday) and runs faster, but they do not like the synchronized now
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@36672 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/geogebra/plugin/UDPLoggerD.java
+++ desktop/geogebra/plugin/UDPLoggerD.java
@@ -14,6 +14,8 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.HashMap;
 
+import javax.swing.SwingUtilities;
+
 /**
  * @author michael
  * 
@@ -84,6 +86,170 @@ public class UDPLoggerD implements UDPLogger {
 
 		listeners.clear();
 		listenersL.clear();
+	}
+
+	private void log(Types type, double val) {
+		log(type, val, true);
+	}
+
+	private void log(Types type, double val, boolean repaint) {
+		GeoNumeric geo = listeners.get(type);
+
+		if (geo != null) {
+
+			// if (repaint)
+			App.debug(type + ": " + val);
+
+			// If we do not want to repaint, probably logging
+			// should be avoided as well...
+
+			geo.setValue(val);
+
+			if (repaint)
+				geo.updateRepaint();
+			else
+				geo.updateCascade();
+		} else {
+			GeoList list = listenersL.get(type);
+			if (list != null) {
+				// synchronized (this) {
+				// if (repaint)
+				App.debug(type + ": " + val);
+
+				geo = new GeoNumeric(list.getConstruction(), val);
+
+				Integer ll = listLimits.get(type);
+				if (ll == null || ll == 0 || ll > list.size()) {
+					list.add(geo);
+				} else {
+					list.addQueue(geo);
+				}
+
+				if (repaint)
+					list.updateRepaint();
+				else
+					list.updateCascade();
+				// }
+			}
+		}
+	}
+
+	private float getFloat(byte[] buffer1, int i) {
+		byte[] bytes = { buffer1[i], buffer1[i + 1], buffer1[i + 2],
+				buffer1[i + 3] };
+		return ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+	}
+
+	public void handle(byte[] buffer, int length, String address) {
+		byte c0 = buffer[0];
+		byte c1 = buffer[1];
+		byte c2 = buffer[2];
+		byte c3 = buffer[3];
+		byte c4 = buffer[4];
+		if (c0 == 'F' && c1 == 'S' && c2 == 0x01) {
+
+			// https://itunes.apple.com/gb/app/sensor-data-streamer/id608278214?mt=8
+			App.debug("data is from 'Sensor Streamer' (c) 2013 FNI Co LTD");
+			log(Types.ACCELEROMETER_X, getFloat(buffer, 4));
+			log(Types.ACCELEROMETER_Y, getFloat(buffer, 8));
+			log(Types.ACCELEROMETER_Z, getFloat(buffer, 12));
+
+			log(Types.ORIENTATION_X, getFloat(buffer, 16));
+			log(Types.ORIENTATION_Y, getFloat(buffer, 20));
+			log(Types.ORIENTATION_Z, getFloat(buffer, 24));
+
+			log(Types.MAGNETIC_FIELD_X, getFloat(buffer, 28));
+			log(Types.MAGNETIC_FIELD_Y, getFloat(buffer, 36));
+			log(Types.MAGNETIC_FIELD_Z, getFloat(buffer, 44));
+
+		} else if (c0 == 'E' && c1 == 'D' && c2 == 'A' && c3 == 'Q') {
+			// EDAQ 530
+
+			// "EDAQ;{sensor1},{doublebits8};{sensor},{doublebits};{sensor},{doublebits}"...
+			// we could even spare the ; and , but still left
+
+			for (int bp = 5; bp < length; bp += 11) {
+				// "{sensor1},{doublebits8};"
+				// ,,23456789
+
+				if (buffer[bp - 1] != ';') {
+					App.error("error in UDP transmission");
+				}
+
+				long gotit = 0;
+				for (int place = bp + 2, shift = 56; place < bp + 10; place++, shift -= 8) {
+					gotit |= ((buffer[place] & 0xFFL) << shift);
+				}
+
+				if (buffer[bp + 1] != ',') {
+					App.error("error in UDP transmission");
+				}
+
+				switch (buffer[bp]) {
+				case 0:
+					log(Types.EDAQ0, Double.longBitsToDouble(gotit), false);
+					break;
+				case 1:
+					log(Types.EDAQ1, Double.longBitsToDouble(gotit), false);
+					break;
+				case 2:
+					log(Types.EDAQ2, Double.longBitsToDouble(gotit), false);
+					break;
+
+				default:
+					App.error("unknown EDAQ port!");
+				}
+			}
+
+			// flush repainting of logs!
+			kernel.notifyRepaint();
+
+		} else {
+
+			// https://play.google.com/store/apps/details?id=jp.ac.ehime_u.cite.sasaki.SensorUdp
+			App.debug("Assume data is from Android/SensorUDP");
+
+			String msg = new String(buffer, 0, length);
+
+			String[] split = msg.split(",");
+
+			switch (buffer[0]) {
+			case 'A':
+
+				App.debug("received" + msg);
+
+				log(Types.ACCELEROMETER_X, Double.parseDouble(split[3]));
+				log(Types.ACCELEROMETER_Y, Double.parseDouble(split[4]));
+				log(Types.ACCELEROMETER_Z, Double.parseDouble(split[5]));
+
+				break;
+			case 'M':
+
+				log(Types.MAGNETIC_FIELD_X, Double.parseDouble(split[3]));
+				log(Types.MAGNETIC_FIELD_Y, Double.parseDouble(split[4]));
+				log(Types.MAGNETIC_FIELD_Z, Double.parseDouble(split[5]));
+
+				break;
+			case 'O':
+				log(Types.ORIENTATION_X, Double.parseDouble(split[3]));
+				log(Types.ORIENTATION_Y, Double.parseDouble(split[4]));
+				log(Types.ORIENTATION_Z, Double.parseDouble(split[5]));
+
+				break;
+
+			default:
+				App.debug("unknown data type: " + buffer[0]);
+				break;
+			}
+
+			// for (int i = 1; i < split.length; i++) {
+			// App.debug(split[i]);
+			// }
+
+			// Convert the contents to a string, and display
+			// them
+			App.debug(address + ": " + msg);
+		}
 	}
 
 	@Override
@@ -179,169 +345,20 @@ public class UDPLoggerD implements UDPLogger {
 					}
 
 					if (socketCopy == dsocket) {
+						final byte[] bufferCopy = buffer.clone();
+						final int length = packet.getLength();
+						SwingUtilities.invokeLater(new Runnable() {
+							public void run() {
+								handle(bufferCopy, length, packet.getAddress()
+										.getHostAddress()
+										+ " "
+										+ packet.getAddress().getHostName());
 
-						byte c0 = buffer[0];
-						byte c1 = buffer[1];
-						byte c2 = buffer[2];
-						byte c3 = buffer[3];
-						byte c4 = buffer[4];
-						if (c0 == 'F' && c1 == 'S' && c2 == 0x01) {
-
-							// https://itunes.apple.com/gb/app/sensor-data-streamer/id608278214?mt=8
-							App.debug("data is from 'Sensor Streamer' (c) 2013 FNI Co LTD");
-							log(Types.ACCELEROMETER_X, getFloat(buffer, 4));
-							log(Types.ACCELEROMETER_Y, getFloat(buffer, 8));
-							log(Types.ACCELEROMETER_Z, getFloat(buffer, 12));
-
-							log(Types.ORIENTATION_X, getFloat(buffer, 16));
-							log(Types.ORIENTATION_Y, getFloat(buffer, 20));
-							log(Types.ORIENTATION_Z, getFloat(buffer, 24));
-
-							log(Types.MAGNETIC_FIELD_X, getFloat(buffer, 28));
-							log(Types.MAGNETIC_FIELD_Y, getFloat(buffer, 36));
-							log(Types.MAGNETIC_FIELD_Z, getFloat(buffer, 44));
-
-							/*
-							 * 
-							 * App.debug("accelerometer x = " + getFloat(buffer,
-							 * 4)); App.debug("accelerometer y = " +
-							 * getFloat(buffer, 8));
-							 * App.debug("accelerometer z = " + getFloat(buffer,
-							 * 12)); App.debug("gyro x = " + getFloat(buffer,
-							 * 16)); App.debug("gyro y = " + getFloat(buffer,
-							 * 20)); App.debug("gyro z = " + getFloat(buffer,
-							 * 24)); App.debug("teslameter x = " +
-							 * getDouble(buffer, 28));
-							 * App.debug("teslameter y = " + getDouble(buffer,
-							 * 36)); App.debug("teslameter z = " +
-							 * getDouble(buffer, 44));
-							 * App.debug("magnetic heading = " +
-							 * getDouble(buffer, 52));
-							 * App.debug("true heading = " + getDouble(buffer,
-							 * 60)); App.debug("latitude = " + getDouble(buffer,
-							 * 68)); App.debug("longitude = " +
-							 * getDouble(buffer, 76)); App.debug("altitude = " +
-							 * getDouble(buffer, 84)); App.debug("proximity = "
-							 * + buffer[92]); App.debug("touched1? = " +
-							 * buffer[93]); App.debug("touch1 x = " +
-							 * getInt(buffer, 94)); App.debug("touch1 y = " +
-							 * getInt(buffer, 98)); App.debug("touched2? = " +
-							 * buffer[102]); App.debug("touch2 x = " +
-							 * getInt(buffer, 103)); App.debug("touch2 y = " +
-							 * getInt(buffer, 107));
-							 */
-
-						} else if (c0 == 'E' && c1 == 'D' && c2 == 'A'
-								&& c3 == 'Q') {
-							// EDAQ 530
-
-							// "EDAQ;{sensor1},{doublebits8};{sensor},{doublebits};{sensor},{doublebits}"...
-							// we could even spare the ; and , but still left
-
-							for (int bp = 5; bp < packet.getLength(); bp += 11) {
-								// "{sensor1},{doublebits8};"
-								// ,,23456789
-
-								if (buffer[bp - 1] != ';') {
-									App.error("error in UDP transmission");
-								}
-
-								long gotit = 0;
-								for (int place = bp + 2, shift = 56; place < bp + 10; place++, shift -= 8) {
-									gotit |= ((buffer[place] & 0xFFL) << shift);
-								}
-
-								if (buffer[bp + 1] != ',') {
-									App.error("error in UDP transmission");
-								}
-
-								switch (buffer[bp]) {
-								case 0:
-									log(Types.EDAQ0,
-											Double.longBitsToDouble(gotit),
-											false);
-									break;
-								case 1:
-									log(Types.EDAQ1,
-											Double.longBitsToDouble(gotit),
-											false);
-									break;
-								case 2:
-									log(Types.EDAQ2,
-											Double.longBitsToDouble(gotit),
-											false);
-									break;
-
-								default:
-									App.error("unknown EDAQ port!");
-								}
 							}
-
-							// flush repainting of logs!
-							kernel.notifyRepaint();
-
-						} else {
-
-							// https://play.google.com/store/apps/details?id=jp.ac.ehime_u.cite.sasaki.SensorUdp
-							App.debug("Assume data is from Android/SensorUDP");
-
-							String msg = new String(buffer, 0,
-									packet.getLength());
-
-							String[] split = msg.split(",");
-
-							switch (buffer[0]) {
-							case 'A':
-
-								App.debug("received" + msg);
-
-								log(Types.ACCELEROMETER_X,
-										Double.parseDouble(split[3]));
-								log(Types.ACCELEROMETER_Y,
-										Double.parseDouble(split[4]));
-								log(Types.ACCELEROMETER_Z,
-										Double.parseDouble(split[5]));
-
-								break;
-							case 'M':
-
-								log(Types.MAGNETIC_FIELD_X,
-										Double.parseDouble(split[3]));
-								log(Types.MAGNETIC_FIELD_Y,
-										Double.parseDouble(split[4]));
-								log(Types.MAGNETIC_FIELD_Z,
-										Double.parseDouble(split[5]));
-
-								break;
-							case 'O':
-								log(Types.ORIENTATION_X,
-										Double.parseDouble(split[3]));
-								log(Types.ORIENTATION_Y,
-										Double.parseDouble(split[4]));
-								log(Types.ORIENTATION_Z,
-										Double.parseDouble(split[5]));
-
-								break;
-
-							default:
-								App.debug("unknown data type: " + buffer[0]);
-								break;
-							}
-
-							// for (int i = 1; i < split.length; i++) {
-							// App.debug(split[i]);
-							// }
-
-							// Convert the contents to a string, and display
-							// them
-							App.debug(packet.getAddress().getHostAddress()
-									+ " " + packet.getAddress().getHostName()
-									+ ": " + msg);
-						}
-
-						// Reset the length of the packet before reusing it.
+						});
+						// Reset the length of the packet before reusing
+						// it.
 						packet.setLength(buffer.length);
-
 					}
 
 				}
@@ -349,59 +366,6 @@ public class UDPLoggerD implements UDPLogger {
 				dsocket.close();
 				App.debug("thread ending");
 
-			}
-
-			private void log(Types type, double val) {
-				log(type, val, true);
-			}
-
-			private void log(Types type, double val, boolean repaint) {
-				GeoNumeric geo = listeners.get(type);
-
-				if (geo != null) {
-
-					// if (repaint)
-					App.debug(type + ": " + val);
-
-					// If we do not want to repaint, probably logging
-					// should be avoided as well...
-
-					geo.setValue(val);
-
-					if (repaint)
-						geo.updateRepaint();
-					else
-						geo.updateCascade();
-				} else {
-					GeoList list = listenersL.get(type);
-					if (list != null) {
-						// synchronized (this) {
-						// if (repaint)
-						App.debug(type + ": " + val);
-
-						geo = new GeoNumeric(list.getConstruction(), val);
-
-						Integer ll = listLimits.get(type);
-						if (ll == null || ll == 0 || ll > list.size()) {
-							list.add(geo);
-						} else {
-							list.addQueue(geo);
-						}
-
-						if (repaint)
-							list.updateRepaint();
-						else
-							list.updateCascade();
-						// }
-					}
-				}
-			}
-
-			private float getFloat(byte[] buffer1, int i) {
-				byte[] bytes = { buffer1[i], buffer1[i + 1], buffer1[i + 2],
-						buffer1[i + 3] };
-				return ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN)
-						.getFloat();
 			}
 
 			private float getInt(byte[] buffer1, int i) {
