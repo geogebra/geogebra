c694b429cfd32fb994045fe398d77bf63e502df2
Balazs Bencze <megette_a@yahoo.com>
2014-10-09 13:59:33 +0000

    [web] timer created for long touches
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@36803 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/geogebra/html5/gui/util/LongTouchTimer.java
@@ -0,0 +1,73 @@
+package geogebra.html5.gui.util;
+
+import com.google.gwt.user.client.Timer;
+
+/**
+ * Class used in view controllers to handle long touches.
+ */
+public class LongTouchTimer extends Timer {
+	
+	private static final int SHOW_CONTEXT_MENU_DELAY = 1000;
+	
+	/**
+	 * Interface for handling long touches.
+	 */
+	public interface LongTouchHandler {
+		/**
+		 * Handles the long touch event.
+		 * @param x the x coordinate of the long touch
+		 * @param y the y coordinate of the long touch
+		 */
+		public void handleLongTouch(int x, int y);
+	}
+	
+	private LongTouchHandler handler;
+	private int xCoordinate;
+	private int yCoordinate;
+	
+	/**
+	 * @param handler used when the timer elapsed.
+	 */
+	public LongTouchTimer(LongTouchHandler handler) {
+	    this.handler = handler;
+	    this.xCoordinate = 0;
+	    this.yCoordinate = 0;
+    }
+
+	@Override
+    public void run() {
+	    if (handler == null) {
+	    	return;
+	    }
+	    handler.handleLongTouch(xCoordinate, yCoordinate);
+    }
+	
+	/**
+	 * Schedules the timer with a default delay value.
+	 * @param x the x coordinate passed to the handler
+	 * @param y the y coordinate passed to the handler
+	 */
+	public void schedule(int x, int y) {
+		schedule(x, y, SHOW_CONTEXT_MENU_DELAY);
+	}
+	
+	/**
+	 * Schedules the timer with {@code delayMillis} ms.
+	 * @param x the x coordinate passed to the handler
+	 * @param y the y coordinate passed to the handler
+	 * @param delayMillis how long to wait before the timer elapses, in milliseconds
+	 */
+	public void schedule(int x, int y, int delayMillis) {
+		xCoordinate = x;
+		yCoordinate = y;
+		schedule(delayMillis);
+	}
+	
+	@Override
+	public void cancel() {
+		xCoordinate = 0;
+		yCoordinate = 0;
+	    super.cancel();
+	}
+
+}
