1589cb5ccda4767384894988668ba0d65f6ed635
Laszlo Gal <laszlo@geogebra.org>
2014-10-16 11:40:24 +0000

    #4635 two flat list of menuitems (no tree yet) and selection
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@36952 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/gui/CustomizeToolbarGUI.java
+++ web/src/geogebra/web/gui/CustomizeToolbarGUI.java
@@ -2,19 +2,26 @@ package geogebra.web.gui;
 
 import geogebra.common.gui.CustomizeToolbarModel;
 import geogebra.common.main.App;
+import geogebra.html5.gui.util.LayoutUtil;
 import geogebra.html5.main.AppW;
 import geogebra.web.gui.CustomizeToolbarHeaderPanel.CustomizeToolbarListener;
+import geogebra.web.gui.app.GGWToolBar;
 import geogebra.web.gui.layout.DockPanelW;
 import geogebra.web.gui.toolbar.ToolBarW;
 
 import java.util.Vector;
+
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
 
 public class CustomizeToolbarGUI extends MyHeaderPanel
 	implements CustomizeToolbarListener {
 
 	private AppW app;
 	private CustomizeToolbarHeaderPanel header;
-
+	private FlowPanel usedToolsPanel;
+	private FlowPanel allToolsPanel;
 	public CustomizeToolbarGUI(AppW app) {
 		this.app = app;
 		addHeader();
@@ -22,6 +29,17 @@ public class CustomizeToolbarGUI extends MyHeaderPanel
 	}
 
 	private void addContent() {
+		FlowPanel main = new FlowPanel();
+		
+		usedToolsPanel = new FlowPanel();
+		usedToolsPanel.setStyleName("usedToolsPanel");
+		
+		allToolsPanel = new FlowPanel();
+		allToolsPanel.setStyleName("allToolsPanel");
+		
+		main.add(usedToolsPanel);
+		main.add(allToolsPanel);
+		setContentWidget(main);
 		update(-1);
     }
 
@@ -34,15 +52,20 @@ public class CustomizeToolbarGUI extends MyHeaderPanel
 		
 		String toolbarDefinition = null;
 		if (id == -1) {
-			toolbarDefinition = app.getGuiManager().getToolbarDefinition(); 
+			toolbarDefinition = ((GuiManagerW)app.getGuiManager()).getToolbarDefinition(); 
 		} else  {
-			DockPanelW panel = (DockPanelW) app.getGuiManager().getLayout().getDockManager().getPanel(id);
+			DockPanelW panel = ((GuiManagerW)app.getGuiManager()).getLayout().getDockManager().getPanel(id);
 			toolbarDefinition = panel.getDefaultToolbarString();
 		}
 		
-		App.debug("[CUSTOMIZE] "); 
 		Vector<Integer> usedTools = CustomizeToolbarModel
 				.generateToolsVector(toolbarDefinition);	    
+		
+		usedToolsPanel.clear();
+		for (Integer mode: usedTools) {
+			usedToolsPanel.add(buildItem(mode));
+		}
+		
 		App.debug("[CUSTOMIZE] " + usedTools);
 	}
 
@@ -50,7 +73,13 @@ public class CustomizeToolbarGUI extends MyHeaderPanel
 		Vector<Integer> allTools = CustomizeToolbarModel
 				.generateToolsVector(ToolBarW.getAllTools(app));
 		 
+		allToolsPanel.clear();
 		
+//		allToolsPanel.add(buildItem(ToolBar.SEPARATOR));
+		
+		for (Integer mode: allTools) {
+			allToolsPanel.add(buildItem(mode));
+		}
 	}
 
 	private void addHeader() {
@@ -65,4 +94,21 @@ public class CustomizeToolbarGUI extends MyHeaderPanel
 		}
 	}
 
+	private FlowPanel buildItem(int mode) {
+		FlowPanel item = new FlowPanel();
+		FlowPanel btn = new FlowPanel();
+
+
+		item.addStyleName("customizableToolbarItem");
+		btn.addStyleName("toolbar_button");
+		Image toolbarImg = new Image(((GGWToolBar)app.getToolbar()).getImageURL(mode));
+		toolbarImg.addStyleName("toolbar_icon");
+		btn.add(toolbarImg);
+		Label text = new Label(app.getMenu(app.getToolName(mode)));
+	
+		item.add(LayoutUtil.panelRow(btn, text));
+		
+		item.getElement().setAttribute("mode", mode + " ");
+		return item;
+	}
 }
