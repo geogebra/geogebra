1c03d3b2e5c40621cfe68229b6ac4456e423747e
Laszlo Gal <laszlo@geogebra.org>
2014-10-18 12:45:17 +0000

    #4635 tools can be "dropped out" from the tree
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@36983 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/gui/CustomizeToolbarGUI.java
+++ web/src/geogebra/web/gui/CustomizeToolbarGUI.java
@@ -14,6 +14,15 @@ import geogebra.web.gui.toolbar.ToolBarW;
 
 import java.util.Vector;
 
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.DragLeaveEvent;
+import com.google.gwt.event.dom.client.DragLeaveHandler;
+import com.google.gwt.event.dom.client.DragOverEvent;
+import com.google.gwt.event.dom.client.DragOverHandler;
+import com.google.gwt.event.dom.client.DragStartEvent;
+import com.google.gwt.event.dom.client.DragStartHandler;
+import com.google.gwt.event.dom.client.DropEvent;
+import com.google.gwt.event.dom.client.DropHandler;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Image;
@@ -29,8 +38,11 @@ implements CustomizeToolbarListener {
 	private FlowPanel usedToolsPanel;
 	private FlowPanel allToolsPanel;
 	private Vector<Integer> usedTools;
+	private Vector<Integer> allTools;
 	private ToolTree toolTree;
-
+	private static ToolItem dragging = null;
+	private static TreeItem allToolsRoot = new TreeItem();
+	
 	private class ToolTreeResources implements Tree.Resources {
 
 		public ToolTreeResources() {
@@ -64,19 +76,158 @@ implements CustomizeToolbarListener {
 	}
 
 	private class ToolItem extends FlowPanel {
-		public ToolItem(Integer mode) {
+		
+		private Integer mode;
+		private TreeItem parent;
+		private TreeItem treeItem;
+		public ToolItem(Integer mode, TreeItem parent) {
+			this.mode = mode;
+			this.parent = parent;
+			treeItem = null; 
 			FlowPanel btn = new FlowPanel();
 			addStyleName("customizableToolbarItem");
 			btn.addStyleName("toolbar_button");
 			Image toolbarImg = new Image(((GGWToolBar)app.getToolbar()).getImageURL(mode));
 			toolbarImg.addStyleName("toolbar_icon");
 			btn.add(toolbarImg);
-			Label text = new Label(app.getMenu(mode == ToolBar.SEPARATOR ? "Separator": app.getToolName(mode)));
+			String str = app.getMenu(mode == ToolBar.SEPARATOR ? "Separator": app.getToolName(mode));
+			setTitle(str);
+			Label text = new Label(str);
 			add(LayoutUtil.panelRow(btn, text));
 			getElement().setAttribute("mode", mode + " ");
-
+		    getElement().setDraggable(Element.DRAGGABLE_TRUE);
+	       
+		    initDrag();
+		    initDrop();
 		}
+
+		private void initDrag() {
+	        addDomHandler(new DragStartHandler() {
+				
+				public void onDragStart(DragStartEvent event) {
+					App.debug("!DRAG START!");
+					dragging = ToolItem.this;
+					event.setData("text", "draggginggg");
+ 	                event.getDataTransfer().setDragImage(getElement(), 10, 10);
+		
+				}
+			}, DragStartEvent.getType());
+        }
+
 	}
+	
+
+    private void initDrop()
+    {
+        addDomHandler(new DragOverHandler()
+        {
+            @Override
+            public void onDragOver(DragOverEvent event)
+            {
+                addStyleName("dropping");
+            }
+        }, DragOverEvent.getType());
+
+        addDomHandler(new DragLeaveHandler()
+        {
+            @Override
+            public void onDragLeave(DragLeaveEvent event)
+            {
+                removeStyleName("dropping");
+            }
+        }, DragLeaveEvent.getType());
+
+        addDomHandler(new DropHandler()
+        {
+            @Override
+            public void onDrop(DropEvent event)
+            {
+                event.preventDefault();
+                if (dragging != null)
+                {
+                	App.debug("Drop " + dragging.getTitle());
+                	TreeItem parent = dragging.parent;
+                	
+                	if (parent == null) {
+                		return;
+                	}
+                	
+                	if (parent != allToolsRoot) {
+                		usedTools.remove(usedTools.indexOf(dragging.mode));
+             			allToolsPanel.add(new ToolItem(dragging.mode, allToolsRoot));
+             			dragging.treeItem.remove();
+                	}
+                 dragging = null;
+                }
+            }
+        }, DropEvent.getType());
+    };
+    
+//                    // Target treeitem is found via 'this';
+//                    // Dragged treeitem is found via 'dragging'.
+//
+//                    TreeItem dragTarget = null;
+//                    TreeItem dragSource = null;
+//                    // The parent of 'this' is not the TreeItem, as that's not a Widget.
+//                    // The parent is the tree containing the treeitem.
+//                    Tree tree = (Tree)DragDropLabel.this.getParent();
+//
+//                    // Visit the entire tree, searching for the drag and drop TreeItems
+//                    List<TreeItem> stack = new ArrayList<TreeItem>();
+//                    stack.add(tree.getItem(0));
+//                    while(!stack.isEmpty())
+//                    {
+//                        TreeItem item = stack.remove(0);
+//                        for(int i=0;i<item.getChildCount();i++)
+//                        {
+//                            stack.add(item.getChild(i));
+//                        }
+//
+//                        Widget w = item.getWidget();
+//                        if (w != null)
+//                        {
+//                            if (w == dragging)
+//                            {
+//                                dragSource = item;
+//                                if (dragTarget != null)
+//                                {
+//                                    break;
+//                                }
+//                            }
+//                            if (w == DragDropLabel.this)
+//                            {
+//                                dragTarget = item;
+//                                w.removeStyleName("dropping");
+//                                if (dragSource != null)
+//                                {
+//                                    break;
+//                                }
+//                            }
+//                        }
+//                    }
+//                    if (dragSource != null && dragTarget != null)
+//                    {
+//                        // Make sure that target is not a child of dragSource
+//
+//                        TreeItem test = dragTarget;
+//                        while(test != null)
+//                        {
+//                            if (test == dragSource)
+//                            {
+//                                return;
+//                            }
+//                            test = test.getParentItem();
+//                        }
+//                        dragTarget.addItem(dragSource);
+//                        dragTarget.setState(true);
+//                    }
+//                    ToolItem.dragging = null;
+//                }
+//            }
+//        }, DropEvent.getType());
+//    }
+
+	
 	public CustomizeToolbarGUI(AppW app) {
 		this.app = app;
 		addHeader();
@@ -126,7 +277,7 @@ implements CustomizeToolbarListener {
 	}
 
 	private void updateAllTools() {
-		Vector<Integer> allTools = CustomizeToolbarModel
+		allTools = CustomizeToolbarModel
 				.generateToolsVector(ToolBarW.getAllTools(app));
 
 		allToolsPanel.clear();
@@ -135,7 +286,8 @@ implements CustomizeToolbarListener {
 
 		for (Integer mode: allTools) {
 			if (!usedTools.contains(mode)) {
-				allToolsPanel.add(new ToolItem(mode));
+				ToolItem tool = new ToolItem(mode, allToolsRoot);
+				allToolsPanel.add(tool);
 			}
 		}
 	}
@@ -171,15 +323,16 @@ implements CustomizeToolbarListener {
 		
 			if (element.getMenu() != null) {
 				Vector<Integer> menu = element.getMenu();
-				
-				TreeItem current = toolTree.addItem(new ToolItem(menu.get(0)));
-				
-	 			for (int j = 0; j < menu.size(); j++) {
+				ToolItem tool = new ToolItem(menu.get(0), null);
+				TreeItem current = toolTree.addItem(tool);
+			 	for (int j = 0; j < menu.size(); j++) {
 					Integer modeInt = menu.get(j);
 					int mode = modeInt.intValue();
 					if (mode != -1)
 						usedTools.add(modeInt);
-						current.addItem(new ToolItem(modeInt));
+						ToolItem leaf = new ToolItem(modeInt, current);
+						TreeItem t = current.addItem(leaf);
+						leaf.treeItem = t;
 					}
 			}
 //			else {
