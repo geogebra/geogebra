37206e914149bb19f5328a71e1729526ee3063fd
Mathieu Blossier <mathieublossier@free.fr>
2014-10-20 21:58:57 +0000

    use zscale
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@37026 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/geogebra3D/euclidian3D/EuclidianView3D.java
+++ common/src/geogebra/common/geogebra3D/euclidian3D/EuclidianView3D.java
@@ -3748,7 +3748,7 @@ public abstract class EuclidianView3D extends EuclidianView implements
 		ret[0] = (p2[0] - p1[0]) * getXscale();
 		ret[1] = (p2[1] - p1[1]) * getYscale();
 		if (ret.length > 2) {
-			ret[2] = (p2[2] - p1[2]) * getYscale();
+			ret[2] = (p2[2] - p1[2]) * getZscale();
 		}
 		return ret;
 	}
