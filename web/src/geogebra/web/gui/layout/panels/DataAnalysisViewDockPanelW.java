c6906ca991d78aac8cd6d224cf90d37c5ee05383
Michael Borcherds <michael@geogebra.org>
2014-10-27 09:25:45 +0000

    [web] make sure MODE_SPREADSHEET_MULTIVARSTATS is in default spreadsheet toolbar
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@37109 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/gui/layout/panels/DataAnalysisViewDockPanelW.java
+++ web/src/geogebra/web/gui/layout/panels/DataAnalysisViewDockPanelW.java
@@ -1,6 +1,5 @@
 package geogebra.web.gui.layout.panels;
 
-import geogebra.common.GeoGebraConstants;
 import geogebra.common.euclidian.EuclidianConstants;
 import geogebra.common.main.App;
 import geogebra.html5.main.AppW;
@@ -17,20 +16,20 @@ import com.google.gwt.user.client.ui.Widget;
  *
  */
 public class DataAnalysisViewDockPanelW extends DockPanelW {
-	
-//	/**
-//	 * default width of this panel
-//	 */
-//	public static final int DEFAULT_WIDTH = 480;
+
+	//	/**
+	//	 * default width of this panel
+	//	 */
+	//	public static final int DEFAULT_WIDTH = 480;
 	private AppW app;
 
 	/**
 	 * @param app App
 	 * Creates panel
-	
+
 	 *
 	 */
-	
+
 	public DataAnalysisViewDockPanelW(AppW app) {
 		super(App.VIEW_DATA_ANALYSIS, // view id
 				"DataAnalysis", // view title phrase
@@ -38,7 +37,7 @@ public class DataAnalysisViewDockPanelW extends DockPanelW {
 				true, // style bar?
 				-1, // menu order
 				'-' // menu shortcut
-		);
+				);
 		this.app = app;
 		setShowStyleBar(true);
 		this.setEmbeddedSize(900);
@@ -59,7 +58,7 @@ public class DataAnalysisViewDockPanelW extends DockPanelW {
 		DataAnalysisViewW da = ((DataAnalysisViewW)((GuiManagerW)app.getGuiManager()).getDataAnalysisView());
 		return  da.getStyleBar();
 	}
-	
+
 	@Override
 	public boolean isStyleBarEmpty(){
 		return false;
@@ -67,19 +66,15 @@ public class DataAnalysisViewDockPanelW extends DockPanelW {
 
 	private static String getDefaultToolbar() {
 		StringBuilder sb = new StringBuilder();
-		
+
 		sb.append(EuclidianConstants.MODE_MOVE);
 		sb.append(" || ");
 		sb.append(EuclidianConstants.MODE_SPREADSHEET_ONEVARSTATS);
 		sb.append(" || ");
 		sb.append(EuclidianConstants.MODE_SPREADSHEET_TWOVARSTATS);
+		sb.append(" || ");
+		sb.append(EuclidianConstants.MODE_SPREADSHEET_MULTIVARSTATS);
 
-		if (GeoGebraConstants.IS_PRE_RELEASE) {
-			sb.append(" || ");
-			sb.append(EuclidianConstants.MODE_SPREADSHEET_MULTIVARSTATS);
-			
-
-	}
 		return sb.toString();
- }
+	}
 } 
