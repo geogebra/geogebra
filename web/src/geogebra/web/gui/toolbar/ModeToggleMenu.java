e8278405c7df7007b260a69cb7a5dab7525be251
Zbyněk Konečný <zbynek@geogebra.org>
2014-10-31 10:00:56 +0000

    CAS / spreadsheet tools bugfix
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@37200 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/gui/toolbar/ModeToggleMenu.java
+++ web/src/geogebra/web/gui/toolbar/ModeToggleMenu.java
@@ -280,7 +280,7 @@ TouchStartHandler, TouchEndHandler, MouseOutHandler, MouseOverHandler, KeyUpHand
 		ToolTipManagerW.sharedInstance().setBlockToolTip(false);
 		//if we click the toolbar button, only interpret it as real click if there is only one tool in this menu
 		app.setMode(Integer.parseInt(event.getRelativeElement().getAttribute("mode")), 
-				event.getSource() == tbutton && menu.size() < 2 ? ModeSetter.DOCK_PANEL : ModeSetter.TOOLBAR);
+				event.getSource() == tbutton && menu.size() > 1 ? ModeSetter.DOCK_PANEL : ModeSetter.TOOLBAR);
 		ToolTipManagerW.sharedInstance().setBlockToolTip(true);
 
 		tbutton.getElement().focus();
