8ac5ef537721aa1a92340b8baa28f446e927b13e
Balazs Bencze <megette_a@yahoo.com>
2014-10-31 11:05:11 +0000

    Removed unnecessary comment.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@37203 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/cas/view/RowHeaderHandler.java
+++ web/src/geogebra/web/cas/view/RowHeaderHandler.java
@@ -9,23 +9,13 @@ import com.google.gwt.event.dom.client.MouseUpEvent;
 import com.google.gwt.event.dom.client.MouseUpHandler;
 import com.google.gwt.user.client.Window;
 
-public class RowHeaderHandler implements MouseUpHandler{
+public class RowHeaderHandler implements MouseUpHandler {
 
-//	public void onClick(ClickEvent event) {
-//		App.debug("click on header");
-//	    if(event.getNativeEvent().getButton() == NativeEvent.BUTTON_RIGHT){
-//	    	App.debug("rightclick");
-//	    	RowHeaderPopupMenuW popupMenu = new RowHeaderPopupMenuW();
-//	    	popupMenu.show(new GPoint());
-//	    }
-//	    
-//    }
-	
 	private AppW app;
 	private RowHeaderWidget rowHeader;
 	private CASTableW table;
 
-	public RowHeaderHandler(AppW appl, CASTableW casTableW, RowHeaderWidget rowHeaderWidget){
+	public RowHeaderHandler(AppW appl, CASTableW casTableW, RowHeaderWidget rowHeaderWidget) {
 		super();
 		app = appl;
 		rowHeader = rowHeaderWidget;
@@ -35,18 +25,17 @@ public class RowHeaderHandler implements MouseUpHandler{
 	public void onMouseUp(MouseUpEvent event) {
 		int releasedRow = rowHeader.getIndex();
     	table.getCASView().getCASStyleBar().setSelectedRow(table.getGeoCasCell(releasedRow));
-	    if(event.getNativeEvent().getButton() == NativeEvent.BUTTON_RIGHT){
-	    	if(!table.isSelectedIndex(releasedRow)){
+	    if(event.getNativeEvent().getButton() == NativeEvent.BUTTON_RIGHT) {
+	    	if (!table.isSelectedIndex(releasedRow)) {
 	    		table.setSelectedRows(releasedRow,  releasedRow);
 	    	}
-			if(table.getSelectedRows().length>0){    	
+			if(table.getSelectedRows().length>0){
 		    	// Don't istantiate RowHeaderPopupMenuW() directly. Use guimanager for this,
 		    	// because it must store in GuiManagerW.currentPopup - in this way the popup will hide
 		    	// when a newer popup will be shown.
 		    	RowHeaderPopupMenuW popupMenu = ((GuiManagerW)app.getGuiManager()).getCASContextMenu(rowHeader, table);
 		    	popupMenu.show(new GPoint(event.getClientX() + Window.getScrollLeft(), event.getClientY() + Window.getScrollTop()));
 			}
-	    }    
-	    
+	    }
     }
 }
