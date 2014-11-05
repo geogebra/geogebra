c11f98a79ae42411d50c26197a78c59eb2ae1bd0
Ancsin Gábor <ustakraharez@gmail.com>
2014-11-05 09:50:42 +0000

    RealSense continued
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@37318 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/geogebra3D/web/realsense/RealSense.java
+++ web/src/geogebra/geogebra3D/web/realsense/RealSense.java
@@ -17,7 +17,7 @@ public class RealSense {
 	           if (isReady) {
 	        	   App.debug("Hurray, Supported! " + JSON.get(result, "msg"));
 	        	   ResourcesInjector.injectRealSenseResources();
-	        	   //TODO: continue here!
+	        	   
 	           } else {
 	        	   App.debug("Sadly, not Supported! " + JSON.get(result, "msg"));
 	           }
@@ -28,4 +28,35 @@ public class RealSense {
 		
 		RealSenseInfo.isSupported(clb);
     }
+	
+	public static native void createInstance() /*-{
+		$wnd.PXCMSenseManager_CreateInstance().then(function (result) {
+		$wnd.sense = result;
+		return $wnd.sense.EnableHand(onHandData);
+	}).then(function (result) {
+		@geogebra.html5.main.AppW::debug(Ljava/lang/String;)('Init started');
+		return $wnd.sense.Init(onConnect);
+	}).then(function (result) {
+		return $wnd.sense.CreateHandConfiguration();
+	}).then(function (result) {
+		handConfiguration = result;
+		return handConfiguration.EnableAllAlerts();
+	}).then(function (result) {
+		return handConfiguration.EnableAllGestures(false);
+	 }).then(function (result) {
+		return handConfiguration.ApplyChanges();
+	}).then(function (result) {
+		return $wnd.sense.QueryCaptureManager();
+	}).then(function (result) {
+		capture = result;
+		return capture.QueryImageSize(capture.STREAM_TYPE_DEPTH);
+	}).then(function (result) {
+		imageSize = result.size;
+		return $wnd.sense.StreamFrames();
+	}).then(function (result) {
+		@geogebra.html5.main.AppW::debug(Ljava/lang/String;)('Streaming ' + imageSize.width + 'x' + imageSize.height);
+	}); //dont compiles because gwt believes it is a try catch block .catch(function (error) {
+		//@geogebra.html5.main.AppW::debug(Ljava/lang/String;)('Init failed: ' + JSON.stringify(error));
+		//});
+	}-*/;
 }
