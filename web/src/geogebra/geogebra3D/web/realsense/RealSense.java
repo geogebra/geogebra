1432df605bda03345063c31c7aa0311862650344
Ancsin Gábor <ustakraharez@gmail.com>
2014-11-07 12:52:03 +0000

    Some realSense pluged in more
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@37397 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/geogebra3D/web/realsense/RealSense.java
+++ web/src/geogebra/geogebra3D/web/realsense/RealSense.java
@@ -7,13 +7,23 @@ import geogebra.html5.util.JSON;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+/**
+ * @author gabor
+ * 
+ * Class for making RealSense api working
+ *
+ */
 public class RealSense {
 	
 	private static JavaScriptObject sense;
 	private static JavaScriptObject handConfiguration;
 	private static JavaScriptObject capture;
 	private static JavaScriptObject imageSize;
+	private static JavaScriptObject handValues;
 
+	/**
+	 * inits if supported
+	 */
 	public static void initIfSupported() {
 		AsyncCallback clb = new AsyncCallback() {
 
@@ -34,33 +44,37 @@ public class RealSense {
 		RealSenseInfo.isSupported(clb);
     }
 	
+	/**
+	 * creates instance
+	 */
 	public static native void createInstance() /*-{
-		var t = this;
 		$wnd.PXCMSenseManager_CreateInstance().then(function (result) {
-		t.@geogebra.geogebra3D.web.realsense.RealSense::sense = result;
-		return t.@geogebra.geogebra3D.web.realsense.RealSense::sense.EnableHand(onHandData);
+		@geogebra.geogebra3D.web.realsense.RealSense::sense = result;
+		return @geogebra.geogebra3D.web.realsense.RealSense::sense.EnableHand(function(mid, module, data) {
+			@geogebra.geogebra3D.web.realsense.RealSense::onHandData(Lcom/google/gwt/core/client/JavaScriptObject;Lcom/google/gwt/core/client/JavaScriptObject;Lcom/google/gwt/core/client/JavaScriptObject;)(mid, module, data);
+		});
 	}).then(function (result) {
 		@geogebra.html5.main.AppW::debug(Ljava/lang/String;)('Init started');
-		return t.@geogebra.geogebra3D.web.realsense.RealSense::sense.Init(onConnect);
+		return @geogebra.geogebra3D.web.realsense.RealSense::sense.Init(onConnect);
 	}).then(function (result) {
-		return t.@geogebra.geogebra3D.web.realsense.RealSense::sense.CreateHandConfiguration();
+		return @geogebra.geogebra3D.web.realsense.RealSense::sense.CreateHandConfiguration();
 	}).then(function (result) {
-		t.@geogebra.geogebra3D.web.realsense.RealSense::handConfiguration = result;
-		return t.@geogebra.geogebra3D.web.realsense.RealSense::handConfiguration.EnableAllAlerts();
+		@geogebra.geogebra3D.web.realsense.RealSense::handConfiguration = result;
+		return @geogebra.geogebra3D.web.realsense.RealSense::handConfiguration.EnableAllAlerts();
 	}).then(function (result) {
-		return t.@geogebra.geogebra3D.web.realsense.RealSense::handConfiguration.EnableAllGestures(false);
+		return @geogebra.geogebra3D.web.realsense.RealSense::handConfiguration.EnableAllGestures(false);
 	 }).then(function (result) {
-		return t.@geogebra.geogebra3D.web.realsense.RealSense::handConfiguration.ApplyChanges();
+		return @geogebra.geogebra3D.web.realsense.RealSense::handConfiguration.ApplyChanges();
 	}).then(function (result) {
-		return t.@geogebra.geogebra3D.web.realsense.RealSense::sense.QueryCaptureManager();
+		return @geogebra.geogebra3D.web.realsense.RealSense::sense.QueryCaptureManager();
 	}).then(function (result) {
-		t.@geogebra.geogebra3D.web.realsense.RealSense::capture = result;
+		@geogebra.geogebra3D.web.realsense.RealSense::capture = result;
 		return capture.QueryImageSize(capture.STREAM_TYPE_DEPTH);
 	}).then(function (result) {
-		t.@geogebra.geogebra3D.web.realsense.RealSense::imageSize = result.size;
-		return t.@geogebra.geogebra3D.web.realsense.RealSense::sense.StreamFrames();
+		@geogebra.geogebra3D.web.realsense.RealSense::imageSize = result.size;
+		return @geogebra.geogebra3D.web.realsense.RealSense::sense.StreamFrames();
 	}).then(function (result) {
-		@geogebra.html5.main.AppW::debug(Ljava/lang/String;)('Streaming ' + imageSize.width + 'x' + imageSize.height);
+		@geogebra.html5.main.AppW::debug(Ljava/lang/String;)('Streaming ' + @geogebra.geogebra3D.web.realsense.RealSense::imageSize.width + 'x' + @geogebra.geogebra3D.web.realsense.RealSense::imageSize.height);
 	}); //dont compiles because gwt believes it is a try catch block .catch(function (error) {
 		//@geogebra.html5.main.AppW::debug(Ljava/lang/String;)('Init failed: ' + JSON.stringify(error));
 		//});
@@ -68,7 +82,7 @@ public class RealSense {
 	
 	private native void onConnect(JavaScriptObject device, JavaScriptObject data) /*-{
 		if (data.connected == false) {
-			this.@geogebra.geogebra3D.web.realsense.RealSense::status(Ljava/lang/String;)('Alert: ' + JSON.stringify(data));
+			@geogebra.geogebra3D.web.realsense.RealSense::status(Ljava/lang/String;)('Alert: ' + JSON.stringify(data));
 		}
 	
 	}-*/;
@@ -76,4 +90,41 @@ public class RealSense {
 	private static void status(String status) {
 		App.debug(status);
 	}
+	
+	private static native void onHandData(JavaScriptObject mid, JavaScriptObject module, JavaScriptObject data) /*-{
+		if (data.hands === undefined) { 
+			return;
+		}
+	
+		var hand = data.hands[0];
+		if (hand === undefined)  {
+			return;
+		}
+	
+		var name;
+		if (data.gestures === undefined){
+			name = "";
+		}else{
+			if (data.gestures[0] === undefined){
+			name = "";
+		}else{
+			name = data.gestures[0].name;
+		}
+	}
+	
+	@geogebra.geogebra3D.web.realsense.RealSense::handValues = {
+			mx : hand.massCenterWorld.x, my : hand.massCenterWorld.y, mz : hand.massCenterWorld.z, 
+			ox : hand.palmOrientation.x, oy : hand.palmOrientation.y, oz : hand.palmOrientation.z, ow: hand.palmOrientation.w,
+			gesture: name
+		};
+	}-*/;
+	
+	/**
+	 * closes realsense.
+	 */
+	public static native void realSenseClose() /*-{
+		@geogebra.geogebra3D.web.realsense.RealSense::sense.Close().then(function (result) {
+			@geogebra.geogebra3D.web.realsense.RealSense::status(Ljava/lang/String;)('Stopped');
+		});
+	}-*/;
 }
