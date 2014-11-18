72c600fea1404fa47ec76d8f3763a67722e28201
Balazs Bencze <megette_a@yahoo.com>
2014-11-18 16:23:10 +0000

    Android beta release
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@37630 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/GeoGebraConstants.java
+++ common/src/geogebra/common/GeoGebraConstants.java
@@ -13,7 +13,7 @@ public interface GeoGebraConstants {
 	/** last build date */
 	public static final String BUILD_DATE = "18 November 2014";
 	/** complete version string */
-	public static final String VERSION_STRING = "5.0.32.0"; 
+	public static final String VERSION_STRING = "5.0.33.0"; 
 	
 	/** true for beta versions/release candidates*/
 	public static final boolean IS_PRE_RELEASE = false;//VERSION_STRING.startsWith(XML_FILE_FORMAT);
