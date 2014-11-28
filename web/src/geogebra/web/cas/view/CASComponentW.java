8a4265b27fb464d650617e413a7ed612bb355428
Balazs Bencze <megette_a@yahoo.com>
2014-11-28 15:58:52 +0000

    Removed debugging messages
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@37813 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/cas/view/CASComponentW.java
+++ web/src/geogebra/web/cas/view/CASComponentW.java
@@ -1,7 +1,5 @@
 package geogebra.web.cas.view;
 
-import geogebra.common.main.App;
-
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ScrollEvent;
 import com.google.gwt.event.dom.client.ScrollHandler;
@@ -21,18 +19,14 @@ public class CASComponentW extends ScrollPanel implements ScrollHandler, NativeP
 	}
 
 	public void onScroll(ScrollEvent event) {
-		App.debug("CAS scroll happened");
 	    scrollHappened = true;
     }
 
 	public void onPreviewNativeEvent(NativePreviewEvent event) {
 		Element element = Element.as(event.getNativeEvent().getEventTarget());
 	    if (this.getElement().isOrHasChild(element)) {
-	    	App.debug("sorce is or has child");
 	    	if (event.getTypeInt() == Event.ONTOUCHEND) {
-	    		App.debug("touchend");
 	    		if (scrollHappened) {
-	    			App.debug("cancel event");
 	    			event.cancel();
 	    			scrollHappened = false;
 	    		}
