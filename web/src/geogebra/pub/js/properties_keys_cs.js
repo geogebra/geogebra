bbcd8678e856a3ebc8c447f54ff16ae8f2716907
Automatic update by the ggbtrans subsystem <geogebra-dev@googlegroups.com>
2014-12-02 19:16:10 +0000

    properties files and related resources automatically updated from ggbtrans
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@37877 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/pub/js/properties_keys_cs.js
+++ web/src/geogebra/pub/js/properties_keys_cs.js
@@ -2607,7 +2607,7 @@ __GGB__keysVar.cs.plain = {
     "PolarCoords": "Pol\xe1rn\xed sou\u0159adnice",
     "PolarLineOfARelativeToB": "Pol\xe1ra bodu %0 relative to %1",
     "Polarized": "Polarizovan\xe9",
-    "PoleOfLineARelativeToB": "Pole of Line %0 Relative to %1",
+    "PoleOfLineARelativeToB": "Pol\xe1ra %0 vzhledem ke %1",
     "PolyLine": "Lomen\xe1 \u010d\xe1ra",
     "Polygon": "Mnoho\xfaheln\xedk",
     "PolygonA": "Mnoho\xfaheln\xedk %0",
