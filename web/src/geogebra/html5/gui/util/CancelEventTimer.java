86362daa67c5b4f258b693ef3db14931ae9938e8
Thomas Krismayer <thomas.krismayer@geogebra.org>
2014-12-12 10:15:58 +0000

    Extend CancelEventTimer for showing on-screen-keyboards
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@38057 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/html5/gui/util/CancelEventTimer.java
+++ web/src/geogebra/html5/gui/util/CancelEventTimer.java
@@ -4,11 +4,19 @@ public class CancelEventTimer {
 
 	private static long lastTouchEvent = 0;
 
+	private static long lastKeyboardEvent = 0;
+
 	/**
 	 * amount of time (ms) in which all mouse events are ignored after a touch
 	 * event
 	 */
 	public static final int TIME_BETWEEN_TOUCH_AND_MOUSE = 500;
+
+	/**
+	 * amount of time (ms) in which background-clicks are not closing the
+	 * keyboard
+	 */
+	public static final int TIME_BEFORE_HIDING_KEYBOARD = 250;
 
 	/**
 	 * called at the end of any touch event
@@ -24,5 +32,22 @@ public class CancelEventTimer {
 	 */
 	public static boolean cancelMouseEvent() {
 		return System.currentTimeMillis() - lastTouchEvent < TIME_BETWEEN_TOUCH_AND_MOUSE;
+	}
+
+	/**
+	 * called after the keyboard is set to visible
+	 */
+	public static void keyboardSetVisible() {
+		lastKeyboardEvent = System.currentTimeMillis();
+	}
+
+	/**
+	 * called at the beginning of a background click
+	 * 
+	 * @return true if the actual event should not hide the keyboard; false
+	 *         otherwise
+	 */
+	public static boolean cancelKeyboardHide() {
+		return System.currentTimeMillis() - lastKeyboardEvent < TIME_BEFORE_HIDING_KEYBOARD;
 	}
 }
