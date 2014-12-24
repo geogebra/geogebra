a801c00ac408d992da8e2b74999a2e20bd0fe94c
Michael Borcherds <michael@geogebra.org>
2014-12-24 12:54:20 +0000

    [desktop] remove rosuda.REngine
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@38245 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/org/rosuda/REngine/REXPLanguage.java
+++
@@ -1,9 +0,0 @@
-package org.rosuda.REngine;
-
-/** represents a language object in R */
-public class REXPLanguage extends REXPList {
-	public REXPLanguage(RList list) { super(list); }
-	public REXPLanguage(RList list, REXPList attr) { super(list, attr); }
-	
-	public boolean isLanguage() { return true; }
-}
--- desktop/org/rosuda/REngine/REngineCallbacks.java
+++
@@ -1,5 +0,0 @@
-package org.rosuda.REngine;
-
-/** REngineCallbacks is a virtual interface that poses as a superclass of all callback delegate classes. */
-public interface REngineCallbacks {
-}
