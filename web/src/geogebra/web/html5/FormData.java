e61d6dd8afc3f4d20b74d3e1cf9f80f0cac1de94
Michael Borcherds <michael@geogebra.org>
2014-12-25 07:54:13 +0000

    formatting
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@38267 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/html5/FormData.java
+++ web/src/geogebra/web/html5/FormData.java
@@ -5,16 +5,16 @@ import com.google.gwt.core.client.JavaScriptObject;
 public class FormData extends JavaScriptObject {
 
 	public static native FormData create() /*-{
-	                                       var fd = new $wnd.FormData();
-	                                       return fd;
-	                                       }-*/;
+		var fd = new $wnd.FormData();
+		return fd;
+	}-*/;
 
 	protected FormData() {
 
 	}
 
 	public native final void append(String key, String value) /*-{
-	                                                          this.append(key, value);
-	                                                          }-*/;
+		this.append(key, value);
+	}-*/;
 
 }
