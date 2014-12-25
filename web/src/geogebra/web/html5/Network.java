84df10e2aa89359965fbcdecbfd0b2c5f07876a7
Michael Borcherds <michael@geogebra.org>
2014-12-25 07:55:08 +0000

    formatting
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@38268 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/html5/Network.java
+++ web/src/geogebra/web/html5/Network.java
@@ -1,13 +1,12 @@
 package geogebra.web.html5;
 
 /**
- * @author gabor some conveniencte method for checking how the app network
- *         state.
+ * @author gabor some convenience method for checking how the app network state.
  */
 public class Network {
 
 	public static native boolean isOffLine() /*-{
 	                                         
-	                                         }-*/;
+	}-*/;
 
 }
