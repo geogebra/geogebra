986ed7b939680d908ca86b360a56aa5064d94ce3
Thomas Krismayer <thomas.krismayer@geogebra.org>
2014-12-30 10:46:13 +0000

    ClickEndHandler and ClickMoveHandler
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@38291 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/html5/gui/util/ClickMoveHandler.java
+++ web/src/geogebra/html5/gui/util/ClickMoveHandler.java
@@ -22,6 +22,12 @@ public abstract class ClickMoveHandler {
 	public static void init(Widget w, final ClickMoveHandler handler) {
 		w.addDomHandler(new MouseMoveHandler() {
 			public void onMouseMove(MouseMoveEvent event) {
+				if (handler.preventDefault) {
+					event.preventDefault();
+				}
+				if (handler.stopPropagation) {
+					event.stopPropagation();
+				}
 				if (!CancelEventTimer.cancelMouseEvent()) {
 					handler.onClickMove(event.getX(), event.getY(),
 					        PointerEventType.MOUSE);
@@ -31,6 +37,12 @@ public abstract class ClickMoveHandler {
 
 		w.addDomHandler(new TouchMoveHandler() {
 			public void onTouchMove(TouchMoveEvent event) {
+				if (handler.preventDefault) {
+					event.preventDefault();
+				}
+				if (handler.stopPropagation) {
+					event.stopPropagation();
+				}
 				handler.onClickMove(event.getTouches().get(0).getClientX(),
 				        event.getTouches().get(0).getClientY(),
 				        PointerEventType.TOUCH);
@@ -39,18 +51,65 @@ public abstract class ClickMoveHandler {
 		}, TouchMoveEvent.getType());
 	}
 
+	boolean preventDefault = false;
+	boolean stopPropagation = false;
+
+	/**
+	 * creates the base version of a ClickEventHandler.
+	 */
 	public ClickMoveHandler() {
+	}
+
+	/**
+	 * {@link ClickMoveHandler} with preventDefault and stopPropagation set
+	 * explicitly. event.preventDefault() and event.stopPropagation() will also
+	 * be called, if the handling-method is canceled for the event.
+	 * 
+	 * @param preventDefault
+	 *            whether or not event.preventDefault() should be called for
+	 *            MouseMoveEvents and TouchMoveEvents
+	 * @param stopPropagation
+	 *            whether or not event.stopPropagation() should be called for
+	 *            MouseMoveEvents and TouchMoveEvents
+	 */
+	public ClickMoveHandler(boolean preventDefault, boolean stopPropagation) {
+		this.preventDefault = preventDefault;
+		this.stopPropagation = stopPropagation;
 	}
 
 	/**
 	 * Actual handler-method, needs to be overwritten in the instances.
 	 * 
 	 * @param x
-	 *            : x-coordinate of the event
+	 *            x-coordinate of the event
 	 * @param y
-	 *            : y-coordinate of the event
+	 *            y-coordinate of the event
 	 * @param type
-	 *            : type of the event
+	 *            type of the event
 	 */
 	public abstract void onClickMove(int x, int y, PointerEventType type);
+
+	/**
+	 * Set preventDefault explicitly. event.preventDefault() will also be
+	 * called, if the handling-method is canceled for the event.
+	 * 
+	 * @param preventDefault
+	 *            whether or not event.preventDefault() should be called for
+	 *            MouseMoveEvents and TouchMoveEvents
+	 */
+	public void setPreventDefault(boolean preventDefault) {
+		this.preventDefault = preventDefault;
+	}
+
+	/**
+	 * Set stopPropagation explicitly. event.stopPropagation() will also be
+	 * called, if the handling-method is canceled for the event.
+	 * 
+	 * @param stopPropagation
+	 *            whether or not event.stopPropagation() should be called for
+	 *            MouseMoveEvents and TouchMoveEvents
+	 */
+	public void setStopPropagation(boolean stopPropagation) {
+		this.stopPropagation = stopPropagation;
+	}
 }
