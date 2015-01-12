31da7e8ab3a00b39bc5dc28a1cd81431b3a9a0f9
Árpád Fekete <fekarp@gmail.com>
2015-01-12 14:37:57 +0000

    Ticket #4819: addressing other problems Balazs has found
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@38418 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/gui/menubar/GMenuBar.java
+++ web/src/geogebra/web/gui/menubar/GMenuBar.java
@@ -1,6 +1,8 @@
 package geogebra.web.gui.menubar;
 
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.MenuItem;
 import com.google.gwt.user.client.ui.MenuItemSeparator;
@@ -41,21 +43,56 @@ public class GMenuBar extends MenuBar{
 	        final MenuBar submenupopup) {
 		// this works, but it is still different from addItem in
 		// not following mouse movement, only following mouse clicks, etc
-		final MenuItem[] ait = new MenuItem[1];
+		final MenuBar parentMenu = this;
+		final Object[] ait = new Object[2];
+		ait[1] = null;
 		ait[0] = addItem(itemtext, textishtml, new ScheduledCommand() {
 			public void execute() {
 				if (ait[0] != null) {
-					PopupPanel pp = new PopupPanel(true, false);
-					submenupopup.addStyleName("subMenuLeftSide2");
-					pp.addStyleName("subMenuLeftSidePopup");
-					pp.add(submenupopup);
-					int left = ait[0].getElement().getAbsoluteLeft();
-					int top = ait[0].getElement().getAbsoluteTop();
-					pp.setPopupPosition(left, top);
-					pp.show();
+					if (ait[1] == null) {
+						// popuppanel still not present
+						final PopupPanel pp = new PopupPanel(true, false);
+						submenupopup.addStyleName("subMenuLeftSide2");
+						pp.addStyleName("subMenuLeftSidePopup");
+						pp.add(submenupopup);
+						int left = ((MenuItem) ait[0]).getElement()
+						        .getAbsoluteLeft();
+						int top = ((MenuItem) ait[0]).getElement()
+						        .getAbsoluteTop();
+						pp.setPopupPosition(left, top);
+
+						// TODO: maybe this solution is imperfect?
+						// but somehow the popup shall be hidden if
+						// clicked on it, or on its triggering element
+						pp.addDomHandler(new ClickHandler() {
+							public void onClick(ClickEvent ce) {
+								// on presuming that clicks will always trigger
+								// something
+
+								// TODO: if condition (later)
+								// if (submenupopup.getSelectedItem() != null) {
+									ait[1] = null;
+									pp.hide();
+								// }
+							}
+						}, ClickEvent.getType());
+
+						// TODO: more difficult to solve autoOpen
+						// sadly, the following thing can only work together
+						// with submenus created by the other method, in theory
+						// parentMenu.setAutoOpen(true);
+
+						ait[1] = pp;
+						pp.show();
+					} else {
+						// if popuppanel is present, it will be hidden
+						// due to autoHide, and no new one is created instead
+						// let's forget about it
+						ait[1] = null;
+					}
 				}
 			}
 		});
-		return ait[0];
+		return (MenuItem) ait[0];
 	}
 }
