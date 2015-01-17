39017955036a5c40099d4a6d4d0913bb71016b25
Automatic update by the ggbtrans subsystem <geogebra-dev@googlegroups.com>
2015-01-17 19:15:55 +0000

    properties files and related resources automatically updated from ggbtrans
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@38506 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/pub/js/properties_keys_fr.js
+++ web/src/geogebra/pub/js/properties_keys_fr.js
@@ -2761,7 +2761,7 @@ __GGB__keysVar.fr.plain = {
     "TotalPercent": "Total %",
     "TraceMode": "Trace :",
     "TraceOff": "Trace d\xe9sactiv\xe9e",
-    "TraceOn": "Trace activ\xe9e",
+    "TraceOn": "Activer Trace",
     "TraceToSpreadsheet": "Enregistrer dans Tableur",
     "TranslationOfAbyB": "Image de %0 dans la translation de vecteur %1",
     "TranslationOfAtoB": "Translat\xe9 de %0 \xe0 %1",
