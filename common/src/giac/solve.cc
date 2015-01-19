684aa4006920bae93cd38dc0220fa1ee520a5239
Bernard Parisse <bernard.parisse@ujf-grenoble.fr>
2015-01-19 14:03:41 +0000

    [giac] Improvement for solve(((3)*(log10((x)^(4))))-((5)*(log10((x)^(2))))=4)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@38515 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/giac/solve.cc
+++ common/src/giac/solve.cc
@@ -2325,8 +2325,11 @@ namespace giac {
 	}
 	vecteur lv=lvarx(makevecteur(a1,a2),v.back());
 	vecteur w=lop(lv,at_pow);
-	if (a2.type!=_VECT && a2!=0 && w.size()>1)
-	  arg1=lnexpand(ln(simplify(a1,contextptr),contextptr)-ln(simplify(a2,contextptr),contextptr),contextptr);
+	if (a2.type!=_VECT && a2!=0 && w.size()>1){
+	  arg1=ln(simplify(a1,contextptr),contextptr)-ln(simplify(a2,contextptr),contextptr);
+	  if (lvarx(arg1,v.back())>1)
+	    arg1=lnexpand(arg1,contextptr);
+	}
 	w=lop(lv,at_exp);
 	if (a2.type!=_VECT && a2!=0 && w.size()>1)
 	  arg1=lnexpand(ln(simplify(a1,contextptr),contextptr)-ln(simplify(a2,contextptr),contextptr),contextptr);
