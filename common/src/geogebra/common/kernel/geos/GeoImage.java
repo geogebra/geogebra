d9e1cf49b5672bf04ea6d243ec7f58a804486d70
Michael Borcherds <michael@geogebra.org>
2015-02-03 19:08:48 +0000

    fix NPE (applymatrix[image])
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@38749 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/kernel/geos/GeoImage.java
+++ common/src/geogebra/common/kernel/geos/GeoImage.java
@@ -935,6 +935,10 @@ public class GeoImage extends GeoElement implements Locateable,
 
 	public void matrixTransform(double a00, double a01, double a02, double a10,
 			double a11, double a12, double a20, double a21, double a22) {
+
+		// http://forum.geogebra.org/viewtopic.php?f=8&t=38230
+		initTempPoints();
+
 		for (int i = 0; i < corners.length; i++) {
 			GeoVec2D vec = tempPoints[i].getVector();
 			vec.matrixTransform(a00, a01, a02, a10, a11, a12, a20, a21, a22);
