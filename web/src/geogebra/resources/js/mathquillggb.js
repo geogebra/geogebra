geogebra/geogebra

commit 1f64ef9170254cdf6ba44046f8aa0a6fd3307289

Ticket #4810: pressing comma before comma should try to select the next syntax help in the suggestion (but for the time being, it also clears it for some reason, but at least, it is usable when the command is top-level)
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@38783 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/geogebra/resources/js/mathquillggb.js
@@ -1791,8 +1791,93 @@ var RootMathBlock = P(MathBlock, function(_, _super) {
     return false;
   };
   _.onText = function(ch) {
+	if (ch === ',') {
+      if (this.cursor[R]) {
+        if ((this.cursor[R] instanceof VanillaSymbol) && (this.cursor[R].ctrlSeq === ',')) {
+          // TODO: it would be good to make a heuristic whether
+          // this.selectNextSyntaxHelp will work or not, e.g. by searching
+          // in this.text()... however, in the GeoGebraWeb command case it further slows it down
+          var successful = this.selectNextSyntaxHelp();
+          if (successful) {
+            return false;
+          }
+          // if not successful we should just continue with the default case
+          // for the same reason, this.selectNextSyntaxHelp() should not have
+          // any ill effect on the formula in case it is unsuccessful!
+        }
+      }
+	}
     this.cursor.write(ch);
     return false;
+  };
+  _.selectNextSyntaxHelp = function() {
+    var numberOfMoveRights = 0;// at the end, the cursor should move left this many times
+	var numberOfSelectRights = 0;
+	var cursor = this.cursor;
+	var checkpoint1 = false;
+	var undoEverything = false;
+	while(cursor[R]) {
+	  // the ] sign in the end is actually a pair of another,
+	  // and these make the parent of the parent (this.cursor.parent.parent)
+	  // so we only need to check for (cursor[R])
+	  if ((cursor[R] instanceof BinaryOperator) &&
+	      (cursor[R].ctrlSeq === '<')) {
+	   	if (checkpoint1) {
+	   	  // wrong syntax, let's undo everything!
+	      undoEverything = true;
+	      break;
+	  	} else {
+	   	  // in theory, selectRight will also move the cursor
+	   	  // to the right... but it should not go out of the
+	      // block because we check (cursor[R]) in this loop
+	      cursor.selectRight();
+	      numberOfSelectRights++;
+	 	  checkpoint1 = true;
+	    }
+	  } else if ((cursor[R] instanceof BinaryOperator) &&
+	             (cursor[R].ctrlSeq === '>')) {
+	  	if (checkpoint1) {
+	   	  // the only way to finish this without undo!
+	      cursor.selectRight();
+	      numberOfSelectRights++;
+	      if (cursor[R]) {
+	       	break;
+	      } else {
+	       	// if (cursor[R]) is false in the end,
+	       	// then undoEverything should mean its opposite
+	       	undoEverything = true;
+	       	//break;// although it can be omitted
+	      }
+	  	} else {
+	   	  // wrong syntax, let's undo everything!
+	   	  undoEverything = true;
+	  	  break;
+	   	}
+	  } else {
+	  	if (checkpoint1) {
+	   	  cursor.selectRight();
+	   	  numberOfSelectRights++;
+	  	} else {
+	      cursor.moveRight();
+	      numberOfMoveRights++;
+	 	}
+	  }
+	}
+	if (cursor[R]) {
+	  // as undefined or null would give run-time-error
+      // on (!cursor[R]) in theory, let's put code in else branch
+	} else {
+	  undoEverything = !undoEverything;
+	}
+	if (undoEverything) {
+      cursor.clearSelection();
+      while(numberOfMoveRights) {
+    	cursor.moveLeft();
+    	numberOfMoveRights--;
+      }
+      return false;
+	}
+	return true;
   };
 });
 

commit 7a215fcfca3592e42bff752ea6f6609254b6a28c

Ticket #4810: fixing the other bug described in comment:145 caused by r38783
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@38801 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/geogebra/resources/js/mathquillggb.js
@@ -1799,6 +1799,10 @@ var RootMathBlock = P(MathBlock, function(_, _super) {
           // in this.text()... however, in the GeoGebraWeb command case it further slows it down
           var successful = this.selectNextSyntaxHelp();
           if (successful) {
+            if (this.common !== undefined) {
+              // but note that in case of Up and Down, we should not hide them!
+              this.common.GeoGebraSuggestionPopupCanShow = false; // or undefined;
+            }
             return false;
           }
           // if not successful we should just continue with the default case