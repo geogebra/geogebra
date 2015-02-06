b6cd589b0546de1713a4057db15808295bcd846b
Zbyněk Konečný <zbynek@geogebra.org>
2015-02-06 15:34:11 +0000

    Temporarily switch to HTTP for tube-beta
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@38812 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/geogebra/common/move/ggtapi/models/GeoGebraTubeAPI.java
+++ common/src/geogebra/common/move/ggtapi/models/GeoGebraTubeAPI.java
@@ -28,9 +28,9 @@ public abstract class GeoGebraTubeAPI {
 	 * DO NOT CHANGE!
 	 */
 	public static String url = "https://tube.geogebra.org/api/json.php";
-	public static String urlBeta = "https://tube-beta.geogebra.org/api/json.php";
+	public static String urlBeta = "http://tube-beta.geogebra.org/api/json.php";
 	public static String login_url = "https://accounts.geogebra.org/api/index.php";
-	public static String login_urlBeta = "https://login-beta.geogebra.org/api/index.php";
+	public static String login_urlBeta = "http://login-beta.geogebra.org/api/index.php";
 	/**
 	 * Instance of the new GeoGebraTube API D/W/T
 	 */
