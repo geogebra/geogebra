7ae45bb18f77cc02fd5936d1047ac746e21b36c7
Árpád Fekete <fekarp@gmail.com>
2015-03-02 11:27:41 +0000

    #4905 preparations for implementing new kind of formula creation/edition
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@39211 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/geogebra/web/gui/view/algebra/SpecialRadioButtonTreeItem.java
@@ -0,0 +1,18 @@
+package geogebra.web.gui.view.algebra;
+
+import geogebra.common.kernel.Kernel;
+import geogebra.web.gui.images.AppResources;
+
+/**
+ * SpecialRadioButtonTreeItem for creating special formulas in the algebra view
+ * e.g. matrix, piecewise function, parametric curve
+ * 
+ * File created by Arpad Fekete
+ */
+public class SpecialRadioButtonTreeItem extends RadioButtonTreeItem {
+
+	public SpecialRadioButtonTreeItem(Kernel kern) {
+		super(kern, AppResources.INSTANCE.shown().getSafeUri(),
+		        AppResources.INSTANCE.hidden().getSafeUri());
+	}
+}
