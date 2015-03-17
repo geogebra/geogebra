a0d8b7dabe4ba7dc5b0a3ad837f679a9218aa458
Árpád Fekete <fekarp@gmail.com>
2015-03-17 11:21:44 +0000

    #4810 fixing right-arrow issues in r39321 and comment:379
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@39420 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/resources/js/mathquillggb.js
+++ web/src/geogebra/resources/js/mathquillggb.js
@@ -4058,18 +4058,13 @@ LatexCmds.uArr = LatexCmds.Uparrow = bind(VanillaSymbol,'\\Uparrow ','&uArr;');
 
 LatexCmds.to = bind(BinaryOperator,'\\to ','&rarr;');
 
-LatexCmds.rarr = LatexCmds.rightarrow = bind(VanillaSymbol,'\\rightarrow ','&rarr;');
-
 //merged with GeoGebra "\u2192", maybe it's not necessary to put it in LatexCmds, but used to
-//it's strange that LatexCmds.and and LatexCmds.or were VanillaSymbol's, and this was
-//a BinaryOperator... but logically, this is really it, and cannot be combined with fractions,
-// so it's almost good, but this way operator precedence may appear, which is not clear for
-// and/or and implies, I think, but I think it'll be good to make .and and .or BinaryOperators too,
-// and hereby I also merge .implies and .rArr and .Rightarrow (which were VanillaSymbols),
-// because it will be easier for GeoGebra development (there are no two things with same notation)
+// and VanillaSymbol changed to BinaryOperator (in case of .and, .or, etc too)
 LatexCmds.implies = LatexCmds['\u2192'] =
+LatexCmds.rarr = LatexCmds.rightarrow = bind(BinaryOperator,'\\rightarrow ','&rarr;', '\u2192');
+
 LatexCmds.rArr = LatexCmds.Rightarrow =
-  bind(BinaryOperator, '\\Rightarrow ', '&rArr;', '\u2192');
+  bind(BinaryOperator, '\\Rightarrow ', '&rArr;');
 
 LatexCmds.gets = bind(BinaryOperator,'\\gets ','&larr;');
 
