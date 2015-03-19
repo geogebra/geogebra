500b3c9e42e3227773173ec4a6c9885274e00943
Árpád Fekete <fekarp@gmail.com>
2015-03-19 08:44:58 +0000

    #4810 my first attempt of implementing selection scroll did not work, but the code may be useful
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@39478 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/html5/main/DrawEquationWeb.java
+++ web/src/geogebra/html5/main/DrawEquationWeb.java
@@ -790,7 +790,12 @@ public class DrawEquationWeb extends DrawEquation {
 					event4.stopPropagation();
 				}).click(function(event6) {
 					event6.stopPropagation();
-				});
+				})
+		//.select(
+		//		function(event7) {
+		//			@geogebra.html5.main.DrawEquationWeb::scrollSelectionIntoView(Lgeogebra/html5/gui/view/algebra/RadioButtonTreeItem;Lcom/google/gwt/dom/client/Element;Z)(rbti,parentElement,newCreationMode);
+		//		})
+		;
 		// could be: mouseover, mouseout, mousemove, mouseup, but this seemed to be enough
 
 		// hacking to deselect the editing when the user does something else like in Desktop
@@ -1338,6 +1343,17 @@ public class DrawEquationWeb extends DrawEquation {
 		return null;
 	}-*/;
 
+	public static native JavaScriptObject grabSelectionFocusForScrollIntoView() /*-{
+		return $wnd.getSelection().focusNode;
+	}-*/;
+
+	public static void scrollSelectionIntoView(RadioButtonTreeItem rbti,
+	        Element parentElement, boolean newCreationMode) {
+		JavaScriptObject jo = grabSelectionFocusForScrollIntoView();
+		if (jo != null)
+			scrollJSOIntoView(jo, rbti, parentElement, newCreationMode);
+	}
+
 	/**
 	 * This is an autoScroll to the edited formula in theory, so it could be
 	 * just a _scrollToBottom_ in practice, but there is a case when the
@@ -1353,47 +1369,52 @@ public class DrawEquationWeb extends DrawEquation {
 	        Element parentElement, boolean newCreationMode) {
 		JavaScriptObject jo = grabCursorForScrollIntoView(parentElement);
 		if (jo != null) {
-			Element joel = Element.as(jo);
-			joel.scrollIntoView();
-
-			// Note: the following hacks should only be made in
-			// new creation mode! so boolean introduced...
-			if (newCreationMode) {
-
-				// if the cursor is on the right or on the left,
-				// it would be good to scroll some more, to show the "X" closing
-				// sign and the blue border of the window! How to know that?
-				// let's compare their places, and if the difference is little,
-				// scroll to the left/right!
-				if (joel.getAbsoluteLeft() - parentElement.getAbsoluteLeft() < 50) {
-					// NewRadioButtonTreeItem class in theory
-					rbti.getElement().setScrollLeft(0);
-				} else if (parentElement.getAbsoluteRight()
-				        - joel.getAbsoluteRight() < 50) {
-					// NewRadioButtonTreeItem class in theory
-					rbti.getElement().setScrollLeft(Integer.MAX_VALUE);
-				} else if (joel.getAbsoluteLeft()
-				        - rbti.getElement().getAbsoluteLeft() < 50) {
-					// we cannot show the "X" sign all the time anyway!
-					// but it would be good not to keep the cursor on the
-					// edge...
-					// so if it is around the edge by now, scroll!
-					rbti.getElement().setScrollLeft(
-					        rbti.getElement().getScrollLeft() - 50
-					                + joel.getAbsoluteLeft()
-					                - rbti.getElement().getAbsoluteLeft());
-				} else if (rbti.getElement().getAbsoluteRight()
-				        - joel.getAbsoluteRight() < 50) {
-					// similarly
-					rbti.getElement().setScrollLeft(
-					        rbti.getElement().getScrollLeft() + 50
-					                - rbti.getElement().getAbsoluteRight()
-					                + joel.getAbsoluteRight());
-				}
-			}
+			scrollJSOIntoView(jo, rbti, parentElement, newCreationMode);
 		} else {
-			// otherwise do the old functionality
 			rbti.scrollIntoView();
+		}
+	}
+
+	private static void scrollJSOIntoView(JavaScriptObject jo,
+	        RadioButtonTreeItem rbti, Element parentElement,
+	        boolean newCreationMode) {
+
+		Element joel = Element.as(jo);
+		joel.scrollIntoView();
+
+		// Note: the following hacks should only be made in
+		// new creation mode! so boolean introduced...
+		if (newCreationMode) {
+			// if the cursor is on the right or on the left,
+			// it would be good to scroll some more, to show the "X" closing
+			// sign and the blue border of the window! How to know that?
+			// let's compare their places, and if the difference is little,
+			// scroll to the left/right!
+			if (joel.getAbsoluteLeft() - parentElement.getAbsoluteLeft() < 50) {
+				// NewRadioButtonTreeItem class in theory
+				rbti.getElement().setScrollLeft(0);
+			} else if (parentElement.getAbsoluteRight()
+			        - joel.getAbsoluteRight() < 50) {
+				// NewRadioButtonTreeItem class in theory
+				rbti.getElement().setScrollLeft(Integer.MAX_VALUE);
+			} else if (joel.getAbsoluteLeft()
+			        - rbti.getElement().getAbsoluteLeft() < 50) {
+				// we cannot show the "X" sign all the time anyway!
+				// but it would be good not to keep the cursor on the
+				// edge...
+				// so if it is around the edge by now, scroll!
+				rbti.getElement().setScrollLeft(
+				        rbti.getElement().getScrollLeft() - 50
+				                + joel.getAbsoluteLeft()
+				                - rbti.getElement().getAbsoluteLeft());
+			} else if (rbti.getElement().getAbsoluteRight()
+			        - joel.getAbsoluteRight() < 50) {
+				// similarly
+				rbti.getElement().setScrollLeft(
+				        rbti.getElement().getScrollLeft() + 50
+				                - rbti.getElement().getAbsoluteRight()
+				                + joel.getAbsoluteRight());
+			}
 		}
 	}
 
