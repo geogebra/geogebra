37ff74851571898a2b4e13f0c0eb8484b244a613
Melanie Tomaschko <melanie.tomaschko@geogebra.org>
2015-03-25 05:09:30 +0000

    Korean keyboard
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@39599 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/geogebra/web/util/keyboard/OnScreenKeyBoard.java
+++ web/src/geogebra/web/util/keyboard/OnScreenKeyBoard.java
@@ -77,7 +77,7 @@ public class OnScreenKeyBoard extends PopupPanel implements ClickHandler {
 		supportedLocales.put(Language.Italian.localeGWT, "it");
 		// supportedLocales.put(Language.Japanese.localeGWT, value);
 		// supportedLocales.put(Language.Kazakh.localeGWT, "kk");
-		// supportedLocales.put(Language.Korean.localeGWT, "ko");
+		supportedLocales.put(Language.Korean.localeGWT, "ko");
 		supportedLocales.put(Language.Latvian.localeGWT, "en");
 		supportedLocales.put(Language.Lithuanian.localeGWT, "lt");
 		// supportedLocales.put(Language.Macedonian.localeGWT, "mk");
