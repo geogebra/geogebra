13823041fb119b8f87ca0fc0b805679ac8ed096f
Mathieu Blossier <mathieublossier@free.fr>
2015-04-12 21:15:34 +0000

    Delete[B3:C7] deletes cells from B3 to C7
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@39915 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/kernel/scripting/CmdDelete.java
+++ common/src/main/java/org/geogebra/common/kernel/scripting/CmdDelete.java
@@ -1,9 +1,15 @@
 package org.geogebra.common.kernel.scripting;
 
+import org.geogebra.common.awt.GPoint;
+import org.geogebra.common.gui.view.spreadsheet.CopyPasteCut;
+import org.geogebra.common.gui.view.spreadsheet.MyTable;
 import org.geogebra.common.kernel.Kernel;
+import org.geogebra.common.kernel.algos.AlgoElement;
 import org.geogebra.common.kernel.arithmetic.Command;
 import org.geogebra.common.kernel.commands.CmdScripting;
 import org.geogebra.common.kernel.geos.GeoElement;
+import org.geogebra.common.kernel.geos.GeoElementSpreadsheet;
+import org.geogebra.common.kernel.statistics.AlgoCellRange;
 import org.geogebra.common.main.MyError;
 
 /**
@@ -25,7 +31,7 @@ public class CmdDelete extends CmdScripting {
 	protected final void perform(Command c) throws MyError {
 		int n = c.getArgumentNumber();
 		boolean[] ok = new boolean[n];
-
+		
 		switch (n) {
 		case 1:
 			try {
@@ -36,13 +42,27 @@ public class CmdDelete extends CmdScripting {
 			ok[0] = arg[0].isGeoElement();
 			if (ok[0]) {
 				GeoElement geo = arg[0];
-
-				if (geo.isLabelSet())
+				
+				AlgoElement algoParent = geo.getParentAlgorithm();
+				if (algoParent != null && algoParent instanceof AlgoCellRange){
+					// delete cells
+					AlgoCellRange algo = (AlgoCellRange) algoParent;
+					GPoint startCoords = GeoElementSpreadsheet
+							.getSpreadsheetCoordsForLabel(algo.getStart());
+					GPoint endCoords = GeoElementSpreadsheet.getSpreadsheetCoordsForLabel(algo.getEnd());
+					CopyPasteCut.delete(app, startCoords.x, startCoords.y, endCoords.x, endCoords.y,
+							MyTable.CELL_SELECT);
+					
+				}else if (geo.isLabelSet()){
 					// delete object
 					geo.removeOrSetUndefinedIfHasFixedDescendent();
+				}
+
 				return;
 			}
 			throw argErr(app, c.getName(), arg[0]);
+			
+		
 
 		default:
 			throw argNumErr(app, c.getName(), n);
