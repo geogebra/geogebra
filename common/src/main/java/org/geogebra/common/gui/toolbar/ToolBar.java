c3ff9e8905f7c458bc1594316ef3ff3e5b21f770
Mathieu Blossier <mathieublossier@free.fr>
2015-04-22 14:28:29 +0000

    Add circle arc/sector to 3D toolbar
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@40126 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/gui/toolbar/ToolBar.java
+++ common/src/main/java/org/geogebra/common/gui/toolbar/ToolBar.java
@@ -454,13 +454,13 @@ public class ToolBar {
 		sb.append(EuclidianConstants.MODE_CIRCLE_THREE_POINTS);
 		sb.append(" , ");
 		// sb.append(EuclidianView.MODE_SEMICIRCLE);
-		// sb.append("  ");
-		// sb.append(EuclidianView.MODE_CIRCLE_ARC_THREE_POINTS);
-		// sb.append(" ");
+		sb.append(" ");
+		sb.append(EuclidianConstants.MODE_CIRCLE_ARC_THREE_POINTS);
+		sb.append(" ");
 		sb.append(EuclidianConstants.MODE_CIRCUMCIRCLE_ARC_THREE_POINTS);
-		sb.append(" , ");
-		// sb.append(EuclidianView.MODE_CIRCLE_SECTOR_THREE_POINTS);
-		// sb.append(" ");
+		sb.append(" ");
+		sb.append(EuclidianConstants.MODE_CIRCLE_SECTOR_THREE_POINTS);
+		sb.append(" ");
 		sb.append(EuclidianConstants.MODE_CIRCUMCIRCLE_SECTOR_THREE_POINTS);
 
 		// sb.append( " | ");
