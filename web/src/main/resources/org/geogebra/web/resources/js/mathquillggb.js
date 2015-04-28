c80d1515b6dc5ce7ffce947c7d646f127c4067ae
Zbyněk Konečný <zbynek@geogebra.org>
2015-04-28 19:14:50 +0000

    Removing the touch event hack for now
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@40259 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/resources/org/geogebra/web/resources/js/mathquillggb.js
+++ web/src/main/resources/org/geogebra/web/resources/js/mathquillggb.js
@@ -1995,9 +1995,9 @@ function createRoot(jQ, root, textbox, editable) {
 	// but still in this case, textarea.blur may be called too
 	// late, setTimeout is not the best solution:
 	if(window.mqTouchEvents){
-		textarea.attr("disabled","true");
+		//textarea.attr("disabled","true");
 	}else if(window.mqTouchEvents === false){
-		textarea.attr("disabled","false");
+		//textarea.attr("disabled","false");
 	}
 	setTimeout(function() { textarea.focus(); });
 
