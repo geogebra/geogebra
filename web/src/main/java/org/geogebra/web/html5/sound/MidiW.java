369ea979b8799f1030a00b8d3877e1ff4567edb8
Laszlo Gal <laszlo@geogebra.org>
2015-05-06 09:22:26 +0000

    #5094 wmaw object creation
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@40423 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/html5/sound/MidiW.java
+++ web/src/main/java/org/geogebra/web/html5/sound/MidiW.java
@@ -1,48 +1,50 @@
 package org.geogebra.web.html5.sound;
 
 import org.geogebra.common.main.App;
+import org.geogebra.web.html5.js.JavaScriptInjector;
+import org.geogebra.web.web.css.GuiResources;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.RunAsyncCallback;
 
 public class MidiW {
 	public static final MidiW INSTANCE = new MidiW();
-
+	protected static final String PREFIX = "[MIDIW] ";
+	protected boolean jsLoaded;
+	JavaScriptObject mwaw;
 	public MidiW() {
-		init();
+		initialize();
 	}
 
-	public static native void init() /*-{
-	//navigator.requestMIDIAccess() callbacks
-	$wnd.onRequestMidiSuccess = $entry(@org.geogebra.web.html5.sound.MidiW::onRequestMidiSuccess(Lcom/google/gwt/core/client/JavaScriptObject;));
-	$wnd.onRequestMidiError = $entry(@org.geogebra.web.html5.sound.MidiW::onRequestMidiError(Lcom/google/gwt/core/client/JavaScriptObject;));
-	//init Web MIDI
-	$wnd.navigator.requestMIDIAccess().then($wnd.onRequestMidiSuccess, $wnd.onRequestMidiError);
-}-*/;
+	public void initialize() {
+		GWT.runAsync(new RunAsyncCallback() {
+			public void onSuccess() {
+				App.debug(PREFIX + "WebMIDIAPIWrapper.js loading success");
+				JavaScriptInjector.inject(GuiResources.INSTANCE
+						.webMidiAPIWrapperJs());
+				MidiW.this.jsLoaded = true;
+				init();
 
-	/**
-	 * Success callback for <code>navigator.requestMIDIAccess()</code>.
-	 */
-	public static void onRequestMidiSuccess(JavaScriptObject midiAccess) {
-		App.debug("[MIDI] initialized: ");
-		getMidiOutput(midiAccess);
+			}
 
+			public void onFailure(Throwable reason) {
+				App.debug(PREFIX + "WebMIDIAPIWrapper.js loading failure");
+			}
+		});
 	}
 
-	public static native void getMidiOutput(JavaScriptObject midiAccess) /*-{
-		var out = midiAccess.outputs.get(0);
-		$wnd.console.log(out);
+	public native JavaScriptObject init() /*-{
+		this@org.geogebra.web.html5.sound.MidiW::wmaw = new $wnd.WebMIDIAPIWrapper(
+				true);
 	}-*/;
 
-	/**
-	 * Error callback for <code>navigator.requestMIDIAccess()</code>.
-	 */
-	public static void onRequestMidiError(JavaScriptObject error) {
-		App.debug("[MIDI] initializer faiure.");
-
-	}
 
 	public void playNote() {
-		App.debug("[MIDI] foo");
+		if (!jsLoaded) {
+			return;
+		}
 
+		App.debug(PREFIX + "MWAW: " + mwaw);
 	}
 }
