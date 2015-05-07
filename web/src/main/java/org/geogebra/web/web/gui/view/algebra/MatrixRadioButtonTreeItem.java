f71b96e77b3ec042b0b9a890e158fe7d0354fc06
Árpád Fekete <fekarp@gmail.com>
2015-05-07 15:08:42 +0000

    #4905 preparations before making it possible to create new matrices from the GUI
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@40464 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/web/gui/view/algebra/MatrixRadioButtonTreeItem.java
+++ web/src/main/java/org/geogebra/web/web/gui/view/algebra/MatrixRadioButtonTreeItem.java
@@ -2,6 +2,8 @@ package org.geogebra.web.web.gui.view.algebra;
 
 import org.geogebra.common.kernel.Kernel;
 import org.geogebra.common.kernel.geos.GeoElement;
+import org.geogebra.common.kernel.geos.GeoList;
+import org.geogebra.common.kernel.geos.GeoNumeric;
 import org.geogebra.web.web.gui.images.AppResources;
 
 import com.google.gwt.safehtml.shared.SafeUri;
@@ -20,8 +22,18 @@ public class MatrixRadioButtonTreeItem extends RadioButtonTreeItem {
 	 * this purpose - this should be empty and editable
 	 */
 	public MatrixRadioButtonTreeItem(Kernel kern) {
-		super(kern, null, AppResources.INSTANCE.shown().getSafeUri(),
-		        AppResources.INSTANCE.hidden().getSafeUri());
+		// note that this should create a 2x2 Zero matrix by default!
+		// so what about creating the same matrix first, and reuse
+		// existing code for constructor / editing?
+
+		// super(kern, create2x2ZeroMatrix(kern), AppResources.INSTANCE.shown()
+		// .getSafeUri(),
+		// AppResources.INSTANCE.hidden().getSafeUri());
+
+		// or... call even more general code!
+		this(create2x2ZeroMatrix(kern), AppResources.INSTANCE.shown()
+				.getSafeUri(),
+				AppResources.INSTANCE.hidden().getSafeUri());
 	}
 
 	/**
@@ -33,6 +45,20 @@ public class MatrixRadioButtonTreeItem extends RadioButtonTreeItem {
 	public MatrixRadioButtonTreeItem(GeoElement ge, SafeUri showUrl,
 			SafeUri hiddenUrl) {
 		super(ge, showUrl, hiddenUrl);
+	}
+
+	public static GeoList create2x2ZeroMatrix(Kernel kern) {
+		// this works in a similar was as AlgoIdentity
+		GeoList ret = new GeoList(kern.getConstruction());
+		GeoList row = new GeoList(kern.getConstruction());
+		row.add(new GeoNumeric(kern.getConstruction(), 0));
+		row.add(new GeoNumeric(kern.getConstruction(), 0));
+		ret.add(row);
+		row = new GeoList(kern.getConstruction());
+		row.add(new GeoNumeric(kern.getConstruction(), 0));
+		row.add(new GeoNumeric(kern.getConstruction(), 0));
+		ret.add(row);
+		return ret;
 	}
 
 	public void increaseRows() {
