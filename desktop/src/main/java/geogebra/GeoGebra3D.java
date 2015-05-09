bc88bcb8aca3e674b717419b70453db183c01c5a
Zoltán Kovács <zoltan@geogebra.org>
2015-05-09 15:13:19 +0000

    Intentional error introduced to test error reporting from Jenkins (to be reverted)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@40492 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/main/java/geogebra/GeoGebra3D.java
+++ desktop/src/main/java/geogebra/GeoGebra3D.java
@@ -3,6 +3,8 @@ package geogebra;
 // Wrapper for backward compatibility for auto-updating under Windows.
 // TODO: Remove it in GeoGebra 5.2
 
+test
+
 public class GeoGebra3D {
 	public static void main(String[] cmdArgs) {	
 		org.geogebra.desktop.GeoGebra3D.main(cmdArgs);
