20ed2e5e2dbff6c8a4d5c00d8e64e1dc062002b6
Zoltán Kovács <zoltan@geogebra.org>
2015-05-09 15:15:49 +0000

    Reverting intentionally introduced error
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@40493 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/main/java/geogebra/GeoGebra3D.java
+++ desktop/src/main/java/geogebra/GeoGebra3D.java
@@ -3,8 +3,6 @@ package geogebra;
 // Wrapper for backward compatibility for auto-updating under Windows.
 // TODO: Remove it in GeoGebra 5.2
 
-test
-
 public class GeoGebra3D {
 	public static void main(String[] cmdArgs) {	
 		org.geogebra.desktop.GeoGebra3D.main(cmdArgs);
