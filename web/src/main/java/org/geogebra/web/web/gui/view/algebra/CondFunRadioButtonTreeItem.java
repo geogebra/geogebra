578203d402be052196ef0b925c7aee66b865536e
Árpád Fekete <fekarp@gmail.com>
2015-05-12 10:41:35 +0000

    #4905 these changes are just for reference - do not seem to fix the problem with piecewise functions in themselves
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@40536 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/web/gui/view/algebra/CondFunRadioButtonTreeItem.java
+++ web/src/main/java/org/geogebra/web/web/gui/view/algebra/CondFunRadioButtonTreeItem.java
@@ -7,6 +7,24 @@ import org.geogebra.web.web.gui.images.AppResources;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.DoubleClickEvent;
+import com.google.gwt.event.dom.client.DoubleClickHandler;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.gwt.event.dom.client.MouseMoveEvent;
+import com.google.gwt.event.dom.client.MouseMoveHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
+import com.google.gwt.event.dom.client.MouseUpEvent;
+import com.google.gwt.event.dom.client.MouseUpHandler;
+import com.google.gwt.event.dom.client.TouchEndEvent;
+import com.google.gwt.event.dom.client.TouchEndHandler;
+import com.google.gwt.event.dom.client.TouchMoveEvent;
+import com.google.gwt.event.dom.client.TouchMoveHandler;
+import com.google.gwt.event.dom.client.TouchStartEvent;
+import com.google.gwt.event.dom.client.TouchStartHandler;
 import com.google.gwt.safehtml.shared.SafeUri;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Image;
@@ -31,6 +49,58 @@ public class CondFunRadioButtonTreeItem extends RadioButtonTreeItem {
 	 * AppResources.INSTANCE.hidden().getSafeUri()); }
 	 */
 
+	static class CancelEvents extends Object implements MouseDownHandler,
+			MouseUpHandler, MouseOverHandler, MouseOutHandler,
+			MouseMoveHandler, DoubleClickHandler, TouchStartHandler,
+			TouchEndHandler, TouchMoveHandler {
+		public void onDoubleClick(DoubleClickEvent me) {
+			me.preventDefault();
+			me.stopPropagation();
+		}
+
+		public void onMouseDown(MouseDownEvent me) {
+			me.preventDefault();
+			me.stopPropagation();
+		}
+
+		public void onMouseUp(MouseUpEvent mue) {
+			mue.preventDefault();
+			mue.stopPropagation();
+		}
+
+		public void onMouseOver(MouseOverEvent me) {
+			me.preventDefault();
+			me.stopPropagation();
+		}
+
+		public void onMouseOut(MouseOutEvent mue) {
+			mue.preventDefault();
+			mue.stopPropagation();
+		}
+
+		public void onMouseMove(MouseMoveEvent mue) {
+			mue.preventDefault();
+			mue.stopPropagation();
+		}
+
+		public void onTouchStart(TouchStartEvent tse) {
+			tse.preventDefault();
+			tse.stopPropagation();
+		}
+
+		public void onTouchEnd(TouchEndEvent tee) {
+			tee.preventDefault();
+			tee.stopPropagation();
+		}
+
+		public void onTouchMove(TouchMoveEvent tee) {
+			tee.preventDefault();
+			tee.stopPropagation();
+		}
+	}
+
+	static CancelEvents cancelEvents = new CancelEvents();
+
 	/**
 	 * Creating a SpecialRadioButtonTreeItem from existing construction as we
 	 * should allow special buttons for them, too... see
@@ -44,11 +114,24 @@ public class CondFunRadioButtonTreeItem extends RadioButtonTreeItem {
 		PushButton btnRow = new PushButton(new Image(
 				AppResources.INSTANCE.point_cross()), new ClickHandler() {
 			public void onClick(ClickEvent ce) {
-				addNewRow();
 				ce.preventDefault();
 				ce.stopPropagation();
+				addNewRow();
 			}
 		});
+
+		// basically, everything except onClick,
+		// static to prevent more instances
+		btnRow.addDoubleClickHandler(cancelEvents);
+		btnRow.addMouseDownHandler(cancelEvents);
+		btnRow.addMouseUpHandler(cancelEvents);
+		btnRow.addMouseMoveHandler(cancelEvents);
+		btnRow.addMouseOverHandler(cancelEvents);
+		btnRow.addMouseOutHandler(cancelEvents);
+		btnRow.addTouchStartHandler(cancelEvents);
+		btnRow.addTouchEndHandler(cancelEvents);
+		btnRow.addTouchMoveHandler(cancelEvents);
+
 		btnRow.addStyleName("RadioButtonTreeItemSpecButton");
 		FlowPanel auxPanel = new FlowPanel();
 		auxPanel.add(btnRow);
@@ -64,7 +147,9 @@ public class CondFunRadioButtonTreeItem extends RadioButtonTreeItem {
 
 				// it seems clicking elsewhere closes editing mode,
 				// but we need to be there in case of piecewise functions!
-				ensureEditing();
+				if (!commonEditingCheck()) {
+					ensureEditing();
+				}
 			}
 		});
 	}
