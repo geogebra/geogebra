32cec8c132190a92fb33347c748c69bdce115f5d
Zbyněk Konečný <zbynek@geogebra.org>
2015-05-20 15:04:23 +0000

    5.0.114
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@40691 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/GeoGebraConstants.java
+++ common/src/main/java/org/geogebra/common/GeoGebraConstants.java
@@ -10,9 +10,9 @@ public interface GeoGebraConstants {
 	// as it is read by the build system
 	// update lines below when this is updated
 	/** last build date */
-	public static final String BUILD_DATE = "19 May 2015";
+	public static final String BUILD_DATE = "20 May 2015";
 	/** complete version string */
-	public static final String VERSION_STRING = "5.0.113.0";
+	public static final String VERSION_STRING = "5.0.114.0";
 
 	/** true for beta versions/release candidates */
 	public static final boolean IS_PRE_RELEASE = false;// VERSION_STRING.startsWith(XML_FILE_FORMAT);
