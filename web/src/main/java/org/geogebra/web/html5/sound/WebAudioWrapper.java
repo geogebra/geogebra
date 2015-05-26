bf0558ef8e3c4f4d90f2d3458635dd20eda84d0d
Laszlo Gal <laszlo@geogebra.org>
2015-05-26 05:20:36 +0000

    #5094 new way to play buffered data (no ScriptProcessor)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@40767 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/html5/sound/WebAudioWrapper.java
+++ web/src/main/java/org/geogebra/web/html5/sound/WebAudioWrapper.java
@@ -29,34 +29,36 @@ public class WebAudioWrapper {
 
 
 	public native void start() /*-{
-		var source = $wnd.context.createBufferSource();
-		var processor = $wnd.context.createScriptProcessor(2048);
 		$wnd.ins = this;
-		processor.onaudioprocess = this
-				.@org.geogebra.web.html5.sound.WebAudioWrapper::audioProcess(Lcom/google/gwt/core/client/JavaScriptObject;);
-		source.connect(processor);
-		processor.connect($wnd.context.destination);
+		$wnd.gainNode = $wnd.context.createGain();
+		$wnd.gainNode.connect($wnd.context.destination);
+		$wnd.gainNode.gain.value = 0.1;
 	}-*/;
 
 
-	private native void audioProcess(JavaScriptObject e) /*-{
-		var leftOut = e.outputBuffer.getChannelData(0);
-		var rightOut = e.outputBuffer.getChannelData(1);
-		//		for (var i = 0; i < leftOut.length; i++) {
-		//			leftOut[i] += (Math.random() - 0.5) * 2;
-		//			rightOut[i] += (Math.random() - 0.5) * 2;
-		//
-		//		}
+	// private native void audioProcess(JavaScriptObject e) /*-{
+	// var leftOut = e.outputBuffer.getChannelData(0);
+	// var rightOut = e.outputBuffer.getChannelData(1);
+	// // for (var i = 0; i < leftOut.length; i++) {
+	// // leftOut[i] += (Math.random() - 0.5) * 0.05;
+	// // rightOut[i] += (Math.random() - 0.5) * 0.05;
+	// //
+	// // }
+	//
+	// var d = 0.5;
+	// if ($wnd.buf) {
+	// for (var i = 0; i < $wnd.buf.length; i++) {
+	// leftOut[i] = $wnd.buf[i] * d;
+	// rightOut[i] = $wnd.buf[i] * d;
+	//
+	// }
+	// }
+	// $wnd.ins.@org.geogebra.web.html5.sound.WebAudioWrapper::fill()();
+	//
+	// }-*/;
 
-		if ($wnd.buf) {
-			for (var i = 0; i < $wnd.buf.length; i++) {
-				leftOut[i] = $wnd.buf[i];
-				rightOut[i] = $wnd.buf[i];
-
-			}
-		}
+	private native void bufferEnded(JavaScriptObject e) /*-{
 		$wnd.ins.@org.geogebra.web.html5.sound.WebAudioWrapper::fill()();
-
 	}-*/;
 
 	public void fill() {
@@ -67,7 +69,7 @@ public class WebAudioWrapper {
 	}
 	public void write(byte[] buf, int length) {
 		JsArrayInteger arr = JsArrayUtils.readOnlyJsArray(buf);
-		toBuffer(arr);
+		createBufferSource(arr);
 	}
 
 	private native void toBuffer(JsArrayInteger buf) /*-{
@@ -78,6 +80,15 @@ public class WebAudioWrapper {
 		}
 	}-*/;
 
+	private native void createBufferSource(JsArrayInteger buf) /*-{
+		var source = $wnd.context.createBufferSource();
+		source.buffer = buf;
+		source.connect($wnd.gainNode);
+		source.onended = this
+				.@org.geogebra.web.html5.sound.WebAudioWrapper::bufferEnded(Lcom/google/gwt/core/client/JavaScriptObject;);
+		
+		source.start();
+	}-*/;
 	public void stop() {
 		// TODO Auto-generated method stub
 
