12ccf50e39a829b25b01e7c55648add48b78c7ed
Mathieu Blossier <mathieublossier@free.fr>
2015-06-06 13:02:25 +0000

    Quick fix for removeFromGL() bad behaviour with lists
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@41008 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/geogebra3D/euclidian3D/draw/Drawable3DLists.java
+++ common/src/main/java/org/geogebra/common/geogebra3D/euclidian3D/draw/Drawable3DLists.java
@@ -92,7 +92,7 @@ public class Drawable3DLists {
 		if (drawable != null) {
 			// App.debug(drawable.getGeoElement()+"");
 			drawable.removeFromDrawable3DLists(this);
-			drawable.removeFromGL();
+			// drawable.removeFromGL();
 		}
 
 	}
