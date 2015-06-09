bea4d80ebc5d04468e08f76a581e69f0f01cb1a5
Zbyněk Konečný <zbynek@geogebra.org>
2015-06-09 13:15:22 +0000

    Fix for #5239
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@41060 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/kernel/cas/CmdSimplify.java
+++ common/src/main/java/org/geogebra/common/kernel/cas/CmdSimplify.java
@@ -1,11 +1,13 @@
 package org.geogebra.common.kernel.cas;
 
 import org.geogebra.common.kernel.Kernel;
+import org.geogebra.common.kernel.algos.AlgoElement;
 import org.geogebra.common.kernel.arithmetic.Command;
 import org.geogebra.common.kernel.commands.CommandProcessor;
 import org.geogebra.common.kernel.commands.Commands;
 import org.geogebra.common.kernel.geos.CasEvaluableFunction;
 import org.geogebra.common.kernel.geos.GeoElement;
+import org.geogebra.common.kernel.geos.GeoFunctionable;
 import org.geogebra.common.kernel.geos.GeoText;
 import org.geogebra.common.main.MyError;
 
@@ -29,27 +31,32 @@ public class CmdSimplify extends CommandProcessor {
 		int n = c.getArgumentNumber();
 		GeoElement[] arg;
 		arg = resArgs(c);
-
+		AlgoElement algo;
 		switch (n) {
 		case 1:
-			if ((arg[0].isCasEvaluableObject())) {
+			if ((arg[0] instanceof CasEvaluableFunction)) {
 
-				AlgoCasBaseSingleArgument algo = new AlgoCasBaseSingleArgument(
+				algo = new AlgoCasBaseSingleArgument(
 						cons, c.getLabel(), (CasEvaluableFunction) arg[0],
 						Commands.Simplify);
 
-				GeoElement[] ret = { algo.getResult() };
-				return ret;
-			} else if ((arg[0].isGeoText())) {
 
-				AlgoSimplifyText algo = new AlgoSimplifyText(cons,
+			} else if (arg[0] instanceof GeoFunctionable) {
+				algo = new AlgoCasBaseSingleArgument(
+						cons, c.getLabel(),
+						((GeoFunctionable) arg[0]).getGeoFunction(),
+						Commands.Simplify);
+
+
+			} else if ((arg[0].isGeoText())) {
+				algo = new AlgoSimplifyText(cons,
 						c.getLabel(), (GeoText) arg[0]);
 
-				GeoElement[] ret = { algo.getGeoText() };
-				return ret;
-			} else
+			} else {
 				throw argErr(app, c.getName(), arg[0]);
-
+			}
+			GeoElement[] ret = { algo.getOutput(0) };
+			return ret;
 			// more than one argument
 		default:
 			throw argNumErr(app, c.getName(), n);
