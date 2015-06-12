2cf52baf9d87a7def559d69775473dced9de00e0
Michael Borcherds <michael@geogebra.org>
2015-06-12 13:23:35 +0000

    [cas] Extremum.1
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@41139 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
+++ common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
@@ -167,6 +167,9 @@ public class Ggb2giac {
 		p("Expand.1", "normal(%0)");
 		p("Exponential.2", "1-exp(-(%0)*(%1))");
 
+		p("Extremum.1",
+				"[[[ggbextremumfun:=%0], [ggbans:=extrema(%0)]], seq(point(ggbans[j],ggbextremumfun(ggbans[j])),j,0,size(ggbans)-1) ][1]");
+
 		// factor over rationals
 		// add x so that Factor[(-k x^2 + 4k x + x^3)] gives a nicer answer
 		p("Factor.1",
