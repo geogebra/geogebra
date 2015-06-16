a97b6bff9032a9cb531fbadb51f46bfa925eba60
Zbyněk Konečný <zbynek@geogebra.org>
2015-06-16 08:23:01 +0000

    #5258 make sure the tangent test works again
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@41185 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/kernel/arithmetic/MyVecNode.java
+++ common/src/main/java/org/geogebra/common/kernel/arithmetic/MyVecNode.java
@@ -176,11 +176,11 @@ public class MyVecNode extends ValidExpression implements VectorValue,
 				sb.append(print(y, values, tpl));
 				sb.append("))");
 			} else {
-				sb.append(isCASVector() ? "ggbvect[" : "point(re(");
-				sb.append(print(x, values, tpl));
-				sb.append(isCASVector() ? "," : "),re(");
-				sb.append(print(y, values, tpl));
-				sb.append(isCASVector() ? "]" : "))");
+				sb.append(isCASVector() ? "ggbvect[" : "point(");
+				printReGiac(sb, x, values, tpl);
+				sb.append(",");
+				printReGiac(sb, y, values, tpl);
+				sb.append(isCASVector() ? "]" : ")");
 			}
 			break;
 
@@ -210,6 +210,18 @@ public class MyVecNode extends ValidExpression implements VectorValue,
 		}
 
 		return sb.toString();
+	}
+
+	private void printReGiac(StringBuilder sb, ExpressionValue x2,
+			boolean values, StringTemplate tpl) {
+		if (x2.unwrap() instanceof Command) {
+			sb.append("re(");
+		}
+		sb.append(print(x2, values, tpl));
+		if (x2.unwrap() instanceof Command) {
+			sb.append(")");
+		}
+
 	}
 
 	@Override
