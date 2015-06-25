04937bf0a081a3a9eb4fdf2a974dd642b9038de0
Zbyněk Konečný <zbynek@geogebra.org>
2015-06-25 10:08:44 +0000

    #5285 compile error
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@41389 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/euclidian/DrawAxis.java
+++ common/src/main/java/org/geogebra/common/euclidian/DrawAxis.java
@@ -508,10 +508,10 @@ view.estimateNumberWidth(rw,
 							view.drawString(g2, sb.toString(), x, y);
 							// measure width, so grid line can avoid it
 							// use same (max) for all labels
-							if (sb.charAt(0) == '–'
+							if (sb.charAt(0) == Unicode.MINUS
 									&& width > view.yLabelMaxWidthNeg) {
 								view.yLabelMaxWidthNeg = width;
-							} else if (sb.charAt(0) != '–'
+							} else if (sb.charAt(0) != Unicode.MINUS
 									&& width > view.yLabelMaxWidthPos) {
 								view.yLabelMaxWidthPos = width;
 							}
