48e4c219fcbfc32af43185e0ed4de379779e303b
Balazs Bencze <megette_a@yahoo.com>
2015-07-01 07:10:09 +0000

    Inherit from HTML5 to compile AppW for android Separating keyboard from input
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@41498 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/main/java/org/geogebra/web/android/keyboard/KeyboardEntry.java
@@ -0,0 +1,53 @@
+package org.geogebra.web.android.keyboard;
+
+import org.geogebra.common.util.StringUtil;
+import org.geogebra.web.android.AppStub;
+import org.geogebra.web.android.MathKeyboardListenerStub;
+import org.geogebra.web.android.UpdateKeyboardListenerStub;
+import org.geogebra.web.html5.css.GuiResourcesSimple;
+import org.geogebra.web.html5.css.StyleInjector;
+import org.geogebra.web.html5.gui.view.algebra.MathKeyboardListener;
+import org.geogebra.web.html5.util.ScriptLoadCallback;
+import org.geogebra.web.web.util.keyboardBase.OnScreenKeyBoardBase;
+import org.geogebra.web.web.util.keyboardBase.TextFieldProcessing;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window.Location;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class KeyboardEntry implements EntryPoint, ScriptLoadCallback {
+
+	private AppStub app;
+
+	public void onModuleLoad() {
+		injectKeyboardStyles();
+		createFactories();
+
+		app = new AppStub();
+
+		String language = Location.getParameter("language");
+		language = language == null || "".equals(language) ? "en" : language;
+		app.setLanguage(language, this);
+	}
+
+	private void injectKeyboardStyles() {
+		StyleInjector.inject(GuiResourcesSimple.INSTANCE.modernStyle()
+				.getText());
+	}
+
+	private void createFactories() {
+		StringUtil.prototype = new StringUtil();
+	}
+
+	public void onLoad() {
+		MathKeyboardListener textField = new MathKeyboardListenerStub();
+		UpdateKeyboardListenerStub listener = new UpdateKeyboardListenerStub();
+		TextFieldProcessing processing = new KeyboardListener();
+		OnScreenKeyBoardBase oskb = OnScreenKeyBoardBase.getInstance(textField,
+				listener, app);
+		oskb.setProcessing(processing);
+		oskb.show();
+
+		RootPanel.get().add(oskb);
+	}
+}
---
+++ web/src/main/java/org/geogebra/web/android/keyboard/KeyboardListener.java
@@ -0,0 +1,60 @@
+package org.geogebra.web.android.keyboard;
+
+import org.geogebra.common.main.Localization;
+import org.geogebra.web.web.util.keyboardBase.TextFieldProcessing;
+
+public class KeyboardListener implements TextFieldProcessing {
+
+	public void setFocus(boolean focus) {
+		// ignored
+	}
+
+	public void onEnter() {
+		onEnterNative();
+	}
+
+	private native void onEnterNative() /*-{
+		$wnd.androidKeyboard.onEnter();
+	}-*/;
+
+	public void onBackSpace() {
+		onBackSpaceNative();
+	}
+
+	private native void onBackSpaceNative() /*-{
+		$wnd.androidKeyboard.onBackSpace();
+	}-*/;
+
+	public void onArrow(ArrowType type) {
+		onArrowNative(type.ordinal());
+	}
+
+	private native void onArrowNative(int arrow) /*-{
+		$wnd.androidKeyboard.onArrow(arrow);
+	}-*/;
+
+	public void insertString(String text) {
+		insertStringNative(text);
+	}
+
+	private native void insertStringNative(String text) /*-{
+		$wnd.androidKeyboard.insertString(text);
+	}-*/;
+
+	public void scrollCursorIntoView() {
+		scrollCursorIntoViewNative();
+	}
+
+	private native void scrollCursorIntoViewNative() /*-{
+		$wnd.androidKeyboard.scrollCursosIntoView();
+	}-*/;
+
+	public boolean resetAfterEnter() {
+		return false;
+	}
+
+	public void updateForNewLanguage(Localization localization) {
+		// ignored
+	}
+
+}
