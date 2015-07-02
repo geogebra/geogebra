1dc320bcb316a87a5c9803f8e5ef40c747d24ec1
Mathieu Blossier <mathieublossier@free.fr>
2015-07-02 12:09:56 +0000

    elements: back to canary
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@41543 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -3881,6 +3881,7 @@ public abstract class App implements UpdateSelection {
 	public final boolean has(Feature f) {
 		switch (f) {
 		case DATA_COLLECTION:
+		case GL_ELEMENTS:
 			return canary;
 		case ALL_LANGUAGES:
 		case AV_EXTENSIONS:
@@ -3900,7 +3901,6 @@ public abstract class App implements UpdateSelection {
 		case SF_DRAG:
 		case JLM_IN_WEB:
 		case DELETE_IN_ALGEBRA:
-		case GL_ELEMENTS:
 			return true;
 		default:
 			return false;
