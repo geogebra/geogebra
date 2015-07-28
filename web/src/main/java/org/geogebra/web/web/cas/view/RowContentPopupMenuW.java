a33d185317790fecf9f892a34316fc17f0bd0323
Árpád Fekete <fekarp@gmail.com>
2015-07-28 17:16:44 +0000

    #5163 this is how we used to port things - at first commit the class with the hard things commented out
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@41913 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/main/java/org/geogebra/web/web/cas/view/RowContentPopupMenuW.java
@@ -0,0 +1,147 @@
+package org.geogebra.web.web.cas.view;
+
+import org.geogebra.common.kernel.geos.GeoCasCell;
+import org.geogebra.web.html5.main.AppW;
+import org.geogebra.web.web.html5.AttachedToDOM;
+//import org.geogebra.web.html5.gui.util.ImageSelection;
+import org.geogebra.web.web.javax.swing.GPopupMenuW;
+
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.user.client.ui.MenuItem;
+
+public class RowContentPopupMenuW extends GPopupMenuW implements AttachedToDOM {
+
+	private static final long serialVersionUID = 1L;
+
+	private final GeoCasCell value;
+	private final CASTableCellEditorW editor;
+	private final CASTableW table;
+	private final AppW app;
+
+	public enum Panel {
+		OUTPUT, INPUT
+	}
+
+	private Panel clickedPanel;
+
+	/**
+	 * initializes the menu
+	 * 
+	 * @param value
+	 *            the {@link GeoCasCell} containing the value to copy
+	 * @param table
+	 *            needed to get the {@link AppD}
+	 */
+	public RowContentPopupMenuW(AppW app, GeoCasCell value,
+			CASTableCellEditorW editor, CASTableW table) {
+		super(app);
+		this.value = value;
+		this.table = table;
+		this.editor = editor;
+		this.app = app;
+		this.clickedPanel = Panel.OUTPUT;
+
+		initMenu();
+	}
+
+	public RowContentPopupMenuW(AppW app, GeoCasCell value,
+			CASTableCellEditorW editor, CASTableW table, Panel clickedPanel) {
+		super(app);
+		this.value = value;
+		this.table = table;
+		this.editor = editor;
+		this.app = app;
+		this.clickedPanel = clickedPanel;
+
+		initMenu();
+	}
+
+	private void initMenu() {
+		switch (clickedPanel) {
+		case OUTPUT:
+			MenuItem copyItem = new MenuItem(app.getMenu("Copy"),
+					new ScheduledCommand() {
+						public void execute() {
+							actionPerformed("copy");
+						}
+					});
+			addItem(copyItem);
+			copyItem.addStyleName("mi_no_image");
+
+			addSeparator();
+
+			MenuItem copyToLatexItem = new MenuItem(app.getMenu("CopyAsLaTeX"),
+					new ScheduledCommand() {
+						public void execute() {
+							// ??
+							// actionPerformed(app.getMenu("copyAsLatex"));
+							actionPerformed("copyAsLatex");
+						}
+					});
+			addItem(copyToLatexItem);
+			copyToLatexItem.addStyleName("mi_no_image");
+			break;
+		case INPUT:
+			// not implemented yet
+			/*JMenuItem pasteItem = new JMenuItem(table.getApplication().getMenu(
+					"Paste"));
+			pasteItem.setActionCommand("paste");
+			pasteItem.addActionListener(this);
+			add(pasteItem);*/
+			break;
+		}
+	}
+
+	/**
+	 * handles the {@link ActionEvent}s
+	 */
+	public void actionPerformed(String ac) {
+		handleCopy(ac);
+		// handlePaste(e);
+	}
+
+	private void handleCopy(String ac) {
+
+		// Clipboard sysClip = Toolkit.getDefaultToolkit().getSystemClipboard();
+		// Transferable data = null;
+
+		if (ac.equals("copy")) {
+			// data = new StringSelection(
+			// use xmlTemplate so that sin(2x) -> sin(2*x)
+			// so that it can be pasted into other software
+			// value.getOutput(StringTemplate.xmlTemplate));
+		} else if (ac.equals("copyAsLatex")) {
+			String latexOutput = value.getLaTeXOutput();
+			// data = new StringSelection(StringUtil.toLaTeXString(latexOutput,
+			// true));
+
+		}
+
+		// if (data != null) {
+		// sysClip.setContents(data, null);
+		// }
+	}
+
+	/*private void handlePaste(ActionEvent e) {
+		String ac = e.getActionCommand();
+		String data = "";
+		Clipboard sysClip = Toolkit.getDefaultToolkit().getSystemClipboard();
+
+		if (ac.equals("paste")) {
+			Transferable contents = sysClip.getContents(null);
+			boolean hasTransferableText = (contents != null)
+					&& contents.isDataFlavorSupported(DataFlavor.stringFlavor);
+			if (hasTransferableText) {
+				try {
+					data = (String) contents
+							.getTransferData(DataFlavor.stringFlavor);
+				} catch (Exception ex) {
+					System.out.println(ex);
+					ex.printStackTrace();
+				}
+			}
+			editor.insertText(data);
+			app.storeUndoInfo();
+		}
+	}*/
+}
