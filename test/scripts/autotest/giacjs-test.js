e0e34bb95a8d130117178c70291982f561902fa4
Zoltán Kovács <zoltan@geogebra.org>
2015-08-06 20:09:30 +0000

    Update to use current Jenkins based autotesting
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@42110 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- test/scripts/autotest/giacjs-test.js
+++ test/scripts/autotest/giacjs-test.js
@@ -1,6 +1,6 @@
 // This PhantomJS script is a slightly modified version of webtest-template.js.
-// Basically it loads the CasUnitTests.html (needed to pass as 1st argument)
-// within a timeout (2nd argument) and exports its results to fatal.html and out.html.
+// Basically it loads the CasUnitTests.html within a timeout
+// and exports its results to fatal.html and out.html.
 // See giacjs-test.sh for more details. Note that a recent PhantomJS may be required.
 
 // @author Zoltan Kovacs <zoltan@geogebra.org>
@@ -8,61 +8,95 @@
 var system = require('system'),
     fs = require('fs');
 
-if (system.args.length !== 3) {
-    console.log('Usage: giactest.js url timeout_seconds');
+if (system.args.length !== 4) {
+    console.log('Usage: phantomjs-2.0.0 --ignore-ssl-errors=true giacjs-test.js begin end timeout_seconds');
     phantom.exit();
 }
 
-url = system.args[1];
-timeout_seconds = system.args[2];
+begin = system.args[1];
+end = system.args[2];
+
+starting_test = "Starting test ";
+
+url = "https://autotest.geogebra.org/job/GeoGebra-autotest/lastSuccessfulBuild/artifact/web/war/CASUnitTests.html?_start=" +
+    begin + "&_end=" + end + "&cb=jenkins"
+
+// console.log("Running " + url);
+
+timeout_seconds = system.args[3];
+starting_test_len = starting_test.length;
+var sem_exit = false;
+
+function saveTables() {
+    var fatal = page.evaluate(function () {
+        // Mike returns fatal errors in this DIV:
+        return document.getElementById('table').innerHTML;
+    });
+    var out = page.evaluate(function () {
+        // Mike returns normal errors in this DIV:
+        return document.getElementById('table2').innerHTML;
+    });
+    f = fs.open("fatal.html", "w");
+    f.write(fatal);
+    f.close();
+
+    f = fs.open("out.html", "w");
+    f.write(out);
+    f.close();
+
+    phantom.exit(2);
+}
 
 function waitFor(testFx, onReady) {
     var maxtimeOutMillis = timeout_seconds * 1000,
         start = new Date().getTime(),
-        condition = false,
+        condition = sem_exit,
         interval = setInterval(function() {
             if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
                 // If not time-out yet and condition not yet fulfilled
-                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+		condition = sem_exit;
             } else {
-                if(!condition) {
-                    var fatal = page.evaluate(function () {
-                        // Mike returns fatal errors in this DIV:
-                        return document.getElementById('table').innerHTML;
-                    });
-                    var out = page.evaluate(function () {
-                        // Mike returns normal errors in this DIV:
-                        return document.getElementById('table2').innerHTML;
-                    });
-                    f = fs.open("fatal.html", "w");
-                    f.write(fatal);
-                    f.close();
-
-                    f = fs.open("out.html", "w");
-                    f.write(out);
-                    f.close();
-
-                    phantom.exit(2);
-                } else {
-                    // Condition fulfilled (timeout and/or condition is 'true')
-                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
-                    clearInterval(interval); //< Stop this interval
-                }
+		console.log("Saving tables for " + begin + ".." + end);
+		saveTables();
             }
-        }, 1000); //< repeat check every 1000ms
+        }, 1000); // repeat check every 1000ms
 };
 
 var page = require('webpage').create();
 
-var lastmsg = "";
+var curtest = 0;
 
 page.onConsoleMessage = function(msg) {
+// console.log(msg);
+if (msg.search(starting_test) == 0) {
+    curtest = msg.substring(starting_test_len);
+    console.log("Test #" + curtest);
+    return;
+    }
+if (msg.search("Cannot enlarge memory arrays.") == 0) {
+    // end = curtest;
+    // saveTables(); // output tables are not yet ready
+    console.log("Ending " + curtest + " without output");
+    if (curtest == end) {
+	sem_exit = true;
+	console.log("Assuming 'the end'");
+	}
+    }
+if (msg.search("js giac output:") == 0) {
+    // console.log("Ending " + curtest);
+    if (curtest == end) {
+	sem_exit = true;
+	console.log("Assuming 'the end'");
+	}
+    }
 }
 
 page.onResourceRequested = function (request) {
 };
 
 page.onError = function (msg, trace) {
+console.log("ERROR: " + msg);
+
 };
 
 page.open(url, function (status) {
--- test/scripts/autotest/giacjs-test.sh
+++ test/scripts/autotest/giacjs-test.sh
@@ -7,33 +7,26 @@
 # Here we use a recent version (>=1.9). Unfortunately we do not get the results
 # in the simplest form from PhantomJS, so we need to refine the <TABLE>
 # DOM object by using another external tool, xmlstarlet.
-# Output: giacjs-test.sql which is a list of SQLite3 insert commands.
-# They are also inserted into the ../../sqlite3db database at the end.
-
-# Call this script in ./junit-tests before running ../../warnings to make sure
-# that the giac Javascript entries will be put into the database.
+# Output: giacjs-test.txt.
 
 # @author Zoltan Kovacs <zoltan@geogebra.org>
 
 test -r autotest.conf && . ./autotest.conf
 
 # 0. You may fine tune the output here
-#PHANTOMJS=phantomjs-1.9
-PHANTOMJS=phantomjs-2pre
+PHANTOMJS=phantomjs-2.0.0
 TESTJS=giacjs-test.js
 TIMEOUT_SEC=60
-# URL=http://www.geogebra.org/web/giac/CASUnitTests.html #?_start=1&_end=10
-URL="$WEBTESTPROTOCOL://$WEBTESTSERVER:$WEBTESTPORT/$WEBTESTWARDIR/CASUnitTests.html"
-# URL="http://web.geogebra.org/beta/CASUnitTests.html"
 MYNAME=giacjs-test
-SQLFILE=giacjs-test.sql
-# TESTS="http://web.geogebra.org/beta/__giac.js"
-TESTS="$WEBTESTPROTOCOL://$WEBTESTSERVER:$WEBTESTPORT/$WEBTESTWARDIR/__giac.js"
+OUTPUTFILE=giacjs-test.txt
 LIMIT=100
+GIACJS=../../../web/war/__giac.js
 
-# Heuristics to find the number of tests:
-wget -N $TESTS
-NOTESTS=`cat __giac.js | grep [0\-9]\: | grep -v ^// | wc -l`
+# Set the number of tests here
+JSONSTART=`cat $GIACJS | nl | grep JSONSTART | awk '{print $1}'`
+JSONEND=`cat $GIACJS | nl | grep JSONEND | awk '{print $1}'`
+NOTESTS=$((JSONEND-JSONSTART+1))
+echo "Assuming $NOTESTS tests"
 
 # 1. Testing prerequisities
 XML=`which xml`
@@ -43,12 +36,6 @@ if [ "$XML" = "" ]; then
 if [ "$XML" = "" ]; then
  echo "No XMLStarlet executable found on path"
  exit 11
- fi
-
-SQLITE3=`which sqlite3`
-if [ "$SQLITE3" = "" ]; then
- echo "No sqlite3 executable found on path"
- exit 12
  fi
 
 which $PHANTOMJS >/dev/null || {
@@ -61,25 +48,20 @@ FATALRAW=fatal.html
 FATAL=fatal-edited.html
 OUTRAW=out.html
 OUT=out-edited.html
-REVISION=`./myrevision`
-TESTED=`date "+%Y-%m-%d %H:%M:%S"`
 
 # 3. Resetting output
-rm -f $SQLFILE
-echo "insert into revisions (id, tested) values ('$REVISION', '$TESTED');" >> $SQLFILE
+rm -f $OUTPUTFILE
 
-# classname, name, message, type, revision, error
-# -----------------------------------------------
-# giacjs-test, 8: Factor(x^2-1), ..., FATAL, revision, 1 for fatal (0 otherwise)
-
-create_sql() {
+create_txt() {
  FILE=$1
- TYPE=$2
- ERROR=$3
+ SIZE=`ls -l $FILE | awk '{print $5}'`
+ if [ $SIZE = 1 ]; then
+  return
+  fi
  N=`cat $FILE | $XML sel -t -v 'count(/table/tbody/tr)'`
  for i in `seq 1 $N`; do
   CLASSNAME=$MYNAME
-  # Apostrophes will be doubled for SQLite3 compliance.
+  # Apostrophes will be doubled for former SQLite3 compliance.
   NAME=`cat $FILE | $XML sel -t -v /table/tbody/tr[$i]/td[1] | sed s/\'/\'\'/g`
   # Sometimes ". NNNNms" is appended to the name which must be removed:
   NAME=`echo "$NAME" | sed s/"\. [0-9]\+ms"//`
@@ -88,9 +70,7 @@ create_sql() {
   REMARK=`cat $FILE | $XML sel -t -v /table/tbody/tr[$i]/td[4] | sed s/\'/\'\'/g`
   MESSAGE="Expected: $EXPECTED got: $GOT. $REMARK"
 
-  echo "insert into names (id) values ('$NAME');" >> $SQLFILE
-  echo "insert into tests (classname, name, message, type, revision, error) values " >> $SQLFILE
-  echo " ('$CLASSNAME', '$NAME', '$MESSAGE', '$TYPE', $REVISION, $ERROR);" >> $SQLFILE
+  echo "$NAME|$MESSAGE" >> $OUTPUTFILE
   done
  }
 
@@ -102,7 +82,7 @@ while [ $START -lt $NOTESTS ]; do
   END=$NOTESTS
   fi
  echo "Processing tests $START..$END..."
- $PHANTOMJS $TESTJS "$URL?_start=$START&_end=$END" $TIMEOUT_SEC
+ $PHANTOMJS --ignore-ssl-errors=true $TESTJS $START $END $TIMEOUT_SEC
 
  # 5. Processing HTML output from PhantomJS
 
@@ -111,13 +91,10 @@ while [ $START -lt $NOTESTS ]; do
  cat $FATALRAW | sed s/"<br>"/". "/g > $FATAL
  cat $OUTRAW | sed s/"<br>"/". "/g > $OUT
 
- # 6. Create SQL inserts
- create_sql $FATAL FATAL 1
- create_sql $OUT ERROR 0
+ # 6. Create output
+ create_txt $FATAL
+ create_txt $OUT
 
  START=$(($END+1))
 
  done
-
-# 7. Adding inserts into database
-cat $SQLFILE | $SQLITE3 ../../sqlite3db 2>&1
