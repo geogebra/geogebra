e3500a36b29ecdb2afb213b1d6d168dc9c093d2b
Stefanie Bogner <diiino@hotmail.com>
2015-08-26 13:54:54 +0000

    Slider in AV looks exactly the same as in EV now - missing black border from handle inserted
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@42451 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/resources/org/geogebra/web/resources/js/jquery-ui.js
+++ web/src/main/resources/org/geogebra/web/resources/js/jquery-ui.js
@@ -1108,7 +1108,7 @@ var slider = $.widget( "ui.slider", $.ui.mouse, {
 		var i, handleCount,
 			options = this.options,
 			existingHandles = this.element.find( ".ui-slider-handle" ).addClass( "ui-state-default ui-corner-all" ),
-			handle = "<span class='ui-slider-handle ui-state-default ui-corner-all' tabindex='0'></span>",
+			handle = "<span class='ui-slider-handle ui-state-default ui-corner-all' tabindex='0'><span></span></span>",
 			handles = [];
 
 		handleCount = ( options.values && options.values.length ) || 1;
