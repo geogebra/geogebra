309b5a3d51731b81c9d540bc437ee1af45caa50a
Csilla Solyom-Gecse <solyom_csilla@yahoo.com>
2015-08-27 11:02:42 +0000

    #4146
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@42471 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
+++ common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
@@ -620,7 +620,7 @@ public class Ggb2giac {
 		p("Root.1", root1);
 		p("Solve.1", root1);
 
-		p("Solve.2", "ggbsort(normal([op(solve(%0,%1))]))");
+		p("Solve.2", "flatten(ggbsort(normal([op(solve(%0,%1))])))");
 		p("SolveODE.1", "when((%0)[0]=='=',"
 				+ "normal(map(desolve(%0),x->y=x)[0])" + ","
 				// add y'= if it's missing
