8204dd71aa14e92e905a8cbda8fe588d2c895869
Michael Borcherds <michael@geogebra.org>
2015-08-28 12:27:24 +0000

    #5500 change < to <=
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@42527 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
+++ common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
@@ -166,7 +166,7 @@ public class Ggb2giac {
 
 		// idea for #5500
 		// p("Evaluate.1",
-		// "[[[ggbevalarg:=%0],[ggbevalans:=regroup(normal(ggbevalarg))],[ggbevalans2:=regroup(ggbevalarg)]], when(length(\"\"+ggbevalans)<length(\"\"+ggbevalans2),ggbevalans,ggbevalans2)][1]");
+		// "[[[ggbevalarg:=%0],[ggbevalans:=regroup(normal(ggbevalarg))],[ggbevalans2:=regroup(ggbevalarg)]], when(length(\"\"+ggbevalans)<=length(\"\"+ggbevalans2),ggbevalans,ggbevalans2)][1]");
 
 		p("Expand.1", "normal(%0)");
 		p("Exponential.2", "1-exp(-(%0)*(%1))");
