e67fadebe5fb9ede9844e03202edfec4207bcb5e
Zbyněk Konečný <zbynek@geogebra.org>
2015-09-07 12:18:25 +0000

    More toolbar images removed
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@42717 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/web/gui/toolbar/images/MyIconResourceBundle.java
+++ web/src/main/java/org/geogebra/web/web/gui/toolbar/images/MyIconResourceBundle.java
@@ -4,366 +4,366 @@ import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 
 public interface MyIconResourceBundle extends ClientBundle, ToolbarResources{
-//	@Source("mode_move.png")
+	// @Source("org/geogebra/common/icons_toolbar/p32/mode_move.png")
 //	MenuBar.Resources mode_move();
 //
-//	@Source("mode_segment.png")
+	// @Source("org/geogebra/common/icons_toolbar/p32/mode_segment.png")
 //	ClientBundle mode_segment();
 
-	@Source("mode_angle.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_angle.png")
 	ImageResource mode_angle_32();
 
-	@Source("mode_anglefixed.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_anglefixed.png")
 	ImageResource mode_anglefixed_32();
 
-	@Source("mode_angularbisector.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_angularbisector.png")
 	ImageResource mode_angularbisector_32();
 
-	@Source("mode_area.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_area.png")
 	ImageResource mode_area_32();
 
-	@Source("mode_attachdetachpoint.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_attachdetachpoint.png")
 	ImageResource mode_attachdetachpoint_32();
 
-	@Source("mode_buttonaction.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_buttonaction.png")
 	ImageResource mode_buttonaction_32();
 
-	@Source("mode_circle2.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_circle2.png")
 	ImageResource mode_circle2_32();
 
-	@Source("mode_circle3.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_circle3.png")
 	ImageResource mode_circle3_32();	
 	
-	@Source("mode_circlearc3.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_circlearc3.png")
 	ImageResource mode_circlearc3_32();
 
-	@Source("mode_circlepointradius.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_circlepointradius.png")
 	ImageResource mode_circlepointradius_32();
 
-	@Source("mode_circlesector3.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_circlesector3.png")
 	ImageResource mode_circlesector3_32();
 
-	@Source("mode_circumcirclearc3.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_circumcirclearc3.png")
 	ImageResource mode_circumcirclearc3_32();
 
-	@Source("mode_circumcirclesector3.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_circumcirclesector3.png")
 	ImageResource mode_circumcirclesector3_32();
 
-	@Source("mode_compasses.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_compasses.png")
 	ImageResource mode_compasses_32();
 
-	@Source("mode_complexnumber.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_complexnumber.png")
 	ImageResource mode_complexnumber_32();
 
-	@Source("mode_conic5.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_conic5.png")
 	ImageResource mode_conic5_32();
 	
-	@Source("mode_copyvisualstyle.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_copyvisualstyle.png")
 	ImageResource mode_copyvisualstyle_32();
 
-	@Source("mode_countcells.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_countcells.png")
 	ImageResource mode_countcells_32();
 	
-	@Source("mode_createlist.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_createlist.png")
 	ImageResource mode_createlist_32();
 
-	@Source("mode_createlistofpoints.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_createlistofpoints.png")
 	ImageResource mode_createlistofpoints_32();
 
-	@Source("mode_creatematrix.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_creatematrix.png")
 	ImageResource mode_creatematrix_32();
 
-	@Source("mode_createpolyline.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_createpolyline.png")
 	ImageResource mode_createpolyline_32();
 
-	@Source("mode_createtable.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_createtable.png")
 	ImageResource mode_createtable_32();
 
-	@Source("mode_delete.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_delete.png")
 	ImageResource mode_delete_32();
 
-	@Source("mode_derivative.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_derivative.png")
 	ImageResource mode_derivative_32();
 
-	@Source("mode_dilatefrompoint.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_dilatefrompoint.png")
 	ImageResource mode_dilatefrompoint_32();
 
-	@Source("mode_distance.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_distance.png")
 	ImageResource mode_distance_32();
 
-	@Source("mode_ellipse3.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_ellipse3.png")
 	ImageResource mode_ellipse3_32();
 	
-	@Source("mode_evaluate.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_evaluate.png")
 	ImageResource mode_evaluate_32();
 
-	@Source("mode_expand.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_expand.png")
 	ImageResource mode_expand_32();
 
-	@Source("mode_factor.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_factor.png")
 	ImageResource mode_factor_32();
 
-	@Source("mode_fitline.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_fitline.png")
 	ImageResource mode_fitline_32();
 
-	@Source("mode_freehandshape.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_freehandshape.png")
 	ImageResource mode_freehandshape_32();
 
-	@Source("mode_functioninspector.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_functioninspector.png")
 	ImageResource mode_functioninspector_32();
 
-	@Source("mode_hyperbola3.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_hyperbola3.png")
 	ImageResource mode_hyperbola3_32();
 
-	@Source("mode_image.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_image.png")
 	ImageResource mode_image_32();
 
-	@Source("mode_integral.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_integral.png")
 	ImageResource mode_integral_32();
 
-	@Source("mode_intersect.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_intersect.png")
 	ImageResource mode_intersect_32();
 
-	@Source("mode_intersectioncurve.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_intersectioncurve.png")
 	ImageResource mode_intersectioncurve_32();
 
-	@Source("mode_join.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_join.png")
 	ImageResource mode_join_32();
 
-	@Source("mode_keepinput.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_keepinput.png")
 	ImageResource mode_keepinput_32();
 
-	@Source("mode_linebisector.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_linebisector.png")
 	ImageResource mode_linebisector_32();
 
-	@Source("mode_locus.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_locus.png")
 	ImageResource mode_locus_32();
 
-	@Source("mode_maxcells.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_maxcells.png")
 	ImageResource mode_maxcells_32();
 
-	@Source("mode_meancells.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_meancells.png")
 	ImageResource mode_meancells_32();
 
-	@Source("mode_midpoint.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_midpoint.png")
 	ImageResource mode_midpoint_32();
 
-	@Source("mode_mincells.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_mincells.png")
 	ImageResource mode_mincells_32();
 
-	@Source("mode_mirroratcircle.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_mirroratcircle.png")
 	ImageResource mode_mirroratcircle_32();
 
-	@Source("mode_mirroratline.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_mirroratline.png")
 	ImageResource mode_mirroratline_32();
 
-	@Source("mode_mirroratpoint.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_mirroratpoint.png")
 	ImageResource mode_mirroratpoint_32();
 
-	@Source("mode_move.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_move.png")
 	ImageResource mode_move_32();
 
-	@Source("mode_moverotate.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_moverotate.png")
 	ImageResource mode_moverotate_32();
 
-	@Source("mode_multivarstats.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_multivarstats.png")
 	ImageResource mode_multivarstats_32();
 	
-	@Source("mode_nsolve.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_nsolve.png")
 	ImageResource mode_nsolve_32();
 
-	@Source("mode_numeric.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_numeric.png")
 	ImageResource mode_numeric_32();
 
-	@Source("mode_onevarstats.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_onevarstats.png")
 	ImageResource mode_onevarstats_32();
 
-	@Source("mode_orthogonal.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_orthogonal.png")
 	ImageResource mode_orthogonal_32();
 
-	@Source("mode_parabola.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_parabola.png")
 	ImageResource mode_parabola_32();
 
-	@Source("mode_parallel.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_parallel.png")
 	ImageResource mode_parallel_32();
 
-	@Source("mode_pen.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_pen.png")
 	ImageResource mode_pen_32();
 	
-	@Source("mode_point.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_point.png")
     ImageResource mode_point_32();
 
-	@Source("mode_pointonobject.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_pointonobject.png")
 	ImageResource mode_pointonobject_32();
 
-	@Source("mode_polardiameter.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_polardiameter.png")
 	ImageResource mode_polardiameter_32();
 
-	@Source("mode_polygon.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_polygon.png")
 	ImageResource mode_polygon_32();
 
-	@Source("mode_polyline.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_polyline.png")
 	ImageResource mode_polyline_32();
 
-	@Source("mode_probabilitycalculator.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_probabilitycalculator.png")
 	ImageResource mode_probabilitycalculator_32();
 
-	@Source("mode_ray.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_ray.png")
 	ImageResource mode_ray_32();
 
-	@Source("mode_recordtospreadsheet.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_recordtospreadsheet.png")
 	ImageResource mode_recordtospreadsheet_32();
 
-	@Source("mode_regularpolygon.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_regularpolygon.png")
 	ImageResource mode_regularpolygon_32();
 
-	@Source("mode_relation.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_relation.png")
 	ImageResource mode_relation_32();
 
-	@Source("mode_rigidpolygon.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_rigidpolygon.png")
 	ImageResource mode_rigidpolygon_32();
 
-	@Source("mode_rotatebyangle.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_rotatebyangle.png")
 	ImageResource mode_rotatebyangle_32();
 
-	@Source("mode_segment.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_segment.png")
 	ImageResource mode_segment_32();
 
-	@Source("mode_segmentfixed.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_segmentfixed.png")
 	ImageResource mode_segmentfixed_32();
 
-	@Source("mode_semicircle.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_semicircle.png")
 	ImageResource mode_semicircle_32();
 
-	@Source("mode_showcheckbox.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_showcheckbox.png")
 	ImageResource mode_showcheckbox_32();
 
-	@Source("mode_showhidelabel.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_showhidelabel.png")
 	ImageResource mode_showhidelabel_32();
 
-	@Source("mode_showhideobject.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_showhideobject.png")
 	ImageResource mode_showhideobject_32();
 
-	@Source("mode_slider.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_slider.png")
 	ImageResource mode_slider_32();
 
-	@Source("mode_slope.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_slope.png")
 	ImageResource mode_slope_32();
 
-	@Source("mode_solve.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_solve.png")
 	ImageResource mode_solve_32();
 
-	@Source("mode_substitute.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_substitute.png")
 	ImageResource mode_substitute_32();
 
-	@Source("mode_sumcells.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_sumcells.png")
 	ImageResource mode_sumcells_32();
 
-	@Source("mode_tangent.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_tangent.png")
 	ImageResource mode_tangent_32();
 
-	@Source("mode_text.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_text.png")
 	ImageResource mode_text_32();
 
-	@Source("mode_textfieldaction.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_textfieldaction.png")
 	ImageResource mode_textfieldaction_32();
 	
-	@Source("mode_tool.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_tool.png")
 	ImageResource mode_tool_32();	
 
-	@Source("mode_translatebyvector.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_translatebyvector.png")
 	ImageResource mode_translatebyvector_32();
 
-	@Source("mode_translateview.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_translateview.png")
 	ImageResource mode_translateview_32();
 
-	@Source("mode_twovarstats.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_twovarstats.png")
 	ImageResource mode_twovarstats_32();
 
-	@Source("mode_vector.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_vector.png")
 	ImageResource mode_vector_32();
 
-	@Source("mode_vectorfrompoint.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_vectorfrompoint.png")
 	ImageResource mode_vectorfrompoint_32();
 
-	@Source("mode_vectorpolygon.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_vectorpolygon.png")
 	ImageResource mode_vectorpolygon_32();
 
-	@Source("mode_zoomin.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_zoomin.png")
 	ImageResource mode_zoomin_32();
 
-	@Source("mode_zoomout.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_zoomout.png")
 	ImageResource mode_zoomout_32();
 	
 	// 3D
 	
-	@Source("mode_circleaxispoint.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_circleaxispoint.png")
 	ImageResource mode_circleaxispoint_32();
 
-	@Source("mode_circlepointradiusdirection.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_circlepointradiusdirection.png")
 	ImageResource mode_circlepointradiusdirection_32();
 
-	@Source("mode_cone.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_cone.png")
 	ImageResource mode_cone_32();
 
-	@Source("mode_conify.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_conify.png")
 	ImageResource mode_conify_32();
 
-	@Source("mode_cube.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_cube.png")
 	ImageResource mode_cube_32();
 
-	@Source("mode_cylinder.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_cylinder.png")
 	ImageResource mode_cylinder_32();
 
-	@Source("mode_extrusion.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_extrusion.png")
 	ImageResource mode_extrusion_32();
 
-	@Source("mode_mirroratplane.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_mirroratplane.png")
 	ImageResource mode_mirroratplane_32();
 
-	@Source("mode_net.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_net.png")
 	ImageResource mode_net_32();
 
-	@Source("mode_orthogonalplane.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_orthogonalplane.png")
 	ImageResource mode_orthogonalplane_32();
 
-	@Source("mode_parallelplane.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_parallelplane.png")
 	ImageResource mode_parallelplane_32();
 
-	@Source("mode_planethreepoint.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_planethreepoint.png")
 	ImageResource mode_planethreepoint_32();
 
-	@Source("mode_plane.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_plane.png")
 	ImageResource mode_plane_32();
 
-	@Source("mode_prism.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_prism.png")
 	ImageResource mode_prism_32();
 
-	@Source("mode_pyramid.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_pyramid.png")
 	ImageResource mode_pyramid_32();
 
-	@Source("mode_rotatearoundline.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_rotatearoundline.png")
 	ImageResource mode_rotatearoundline_32();
 
-	@Source("mode_rotateview.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_rotateview.png")
 	ImageResource mode_rotateview_32();
 
-	@Source("mode_sphere2.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_sphere2.png")
 	ImageResource mode_sphere2_32();
 
-	@Source("mode_spherepointradius.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_spherepointradius.png")
 	ImageResource mode_spherepointradius_32();
 
-	@Source("mode_tetrahedron.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_tetrahedron.png")
 	ImageResource mode_tetrahedron_32();
 
-	@Source("mode_viewinfrontof.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_viewinfrontof.png")
 	ImageResource mode_viewinfrontof_32();
 
-	@Source("mode_volume.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_volume.png")
 	ImageResource mode_volume_32();
 	
-	@Source("mode_orthogonalthreed.png")
+	@Source("org/geogebra/common/icons_toolbar/p32/mode_orthogonalthreed.png")
 	ImageResource mode_orthogonalthreed_32();
 	
 	
