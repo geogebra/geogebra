geogebra/geogebra

commit 09582035efb6acbd182e38b0c732e906ce94e3a8

related to #2185: better handling of show/hide navigation bar for 3D view (web)
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@42631 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/main/java/org/geogebra/web/geogebra3D/web/gui/layout/panels/EuclidianDockPanel3DW.java
@@ -83,7 +83,7 @@ public class EuclidianDockPanel3DW extends EuclidianDockPanelWAbstract {
 		if (consProtNav != null) {
 			consProtNav.update();
 			consProtNav.setVisible(app.showConsProtNavigation(id));
-			euclidianpanel.onResize();
+			updateEuclidianPanel();
 		}
 	}
 	
@@ -139,14 +139,13 @@ public class EuclidianDockPanel3DW extends EuclidianDockPanelWAbstract {
 				if (app.showConsProtNavigation(App.VIEW_EUCLIDIAN3D)) {
 					h -= dockPanel.navHeight();
 				}
-
 				// TODO handle this better?
 				// exit if new size cannot be determined
 				if (h <= 0 || w <= 0) {
 					return;
 				}
 				if (h != oldHeight || w != oldWidth) {
-					app.ggwGraphicsViewDimChanged(w, h);
+					app.ggwGraphicsView3DDimChanged(w, h);
 					oldHeight = h;
 					oldWidth = w;
 				} else {
@@ -169,6 +168,35 @@ public class EuclidianDockPanel3DW extends EuclidianDockPanelWAbstract {
 		public AbsolutePanel getAbsolutePanel() {
 			return absoluteEuclidianPanel;
         }
+	}
+
+	private int oldHeight = 0, oldWidth = 0;
+
+	private void updateEuclidianPanel() {
+
+		if (app != null) {
+
+			int h = getComponentInteriorHeight();
+			int w = getComponentInteriorWidth();
+			if (app.showConsProtNavigation(App.VIEW_EUCLIDIAN3D)) {
+				h -= navHeight();
+			}
+			// TODO handle this better?
+			// exit if new size cannot be determined
+			if (h <= 0 || w <= 0) {
+				return;
+			}
+			if (h != oldHeight || w != oldWidth) {
+				app.ggwGraphicsView3DDimChanged(w, h);
+				oldHeight = h;
+				oldWidth = w;
+			} else {
+				// it's possible that the width/height didn't change but the
+				// position of EV did
+				app.getEuclidianView3D().getEuclidianController()
+						.calculateEnvironment();
+			}
+		}
 	}
 
 

web/src/main/java/org/geogebra/web/geogebra3D/web/euclidian3D/EuclidianView3DW.java
@@ -39,6 +39,7 @@ import org.geogebra.web.html5.main.TimerSystemW;
 import com.google.gwt.animation.client.AnimationScheduler;
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.BlurEvent;
 import com.google.gwt.event.dom.client.BlurHandler;
 import com.google.gwt.event.dom.client.FocusEvent;
@@ -763,6 +764,31 @@ public class EuclidianView3DW extends EuclidianView3D implements
 		// needed in applets; in app we might want to avoid this because panel
 		// resizing does the same.
 		this.resizeView();
+	}
+
+	public void synCanvasSize() {
+		setCoordinateSpaceSize(g2p.getOffsetWidth(), g2p.getOffsetHeight());
+	}
+
+	public void setCoordinateSpaceSize(int width, int height) {
+
+		int oldWidth = g2p.getOffsetWidth();
+		int oldHeight = g2p.getOffsetHeight();
+		// no transform nor color set since it's a WebGL context
+		g2p.setCoordinateSpaceSizeNoTransformNoColor(width, height);
+		try {
+			((AppW) app).syncAppletPanelSize(width - oldWidth, height
+					- oldHeight, evNo);
+
+			// just resizing the AbsolutePanelSmart, not the whole of DockPanel
+			g2p.getCanvas().getElement().getParentElement().getStyle()
+					.setWidth(width, Style.Unit.PX);
+			g2p.getCanvas().getElement().getParentElement().getStyle()
+					.setHeight(height, Style.Unit.PX);
+			getEuclidianController().calculateEnvironment();
+		} catch (Exception exc) {
+			App.debug("Problem with the parent element of the canvas");
+		}
 	}
 
 }

web/src/main/java/org/geogebra/web/geogebra3D/web/main/App3DW.java
@@ -102,4 +102,31 @@ public class App3DW {
 		return new EuclidianView3DWnoWebGL(ec, settings);
 	}
 
+	/**
+	 * Resets the width of the Canvas converning the Width of its wrapper
+	 * (splitlayoutpanel center)
+	 * 
+	 * @param app
+	 *            application instance
+	 *
+	 * @param width
+	 *            new width
+	 * 
+	 * @param height
+	 *            new height
+	 */
+	static final public void ggwGraphicsView3DDimChanged(AppW app, int width,
+			int height) {
+		app.getSettings()
+				.getEuclidian(3)
+				.setPreferredSize(
+				org.geogebra.common.factories.AwtFactory.prototype
+						.newDimension(width, height));
+
+		EuclidianView3DW view = (EuclidianView3DW) app.getEuclidianView3D();
+		view.synCanvasSize();
+		view.doRepaint2();
+		app.stopCollectingRepaints();
+	}
+
 }

web/src/main/java/org/geogebra/web/geogebra3D/web/main/AppWapplet3D.java
@@ -194,4 +194,9 @@ public class AppWapplet3D extends AppWapplet {
 
 		return sb.toString();
 	}
+
+	@Override
+	public void ggwGraphicsView3DDimChanged(int width, int height) {
+		App3DW.ggwGraphicsView3DDimChanged(this, width, height);
+	}
 }

web/src/main/java/org/geogebra/web/geogebra3D/web/main/AppWapplication3D.java
@@ -211,4 +211,9 @@ public class AppWapplication3D extends AppWapplication {
 		return sb.toString();
 	}
 
+	@Override
+	public void ggwGraphicsView3DDimChanged(int width, int height) {
+		App3DW.ggwGraphicsView3DDimChanged(this, width, height);
+	}
+
 }

web/src/main/java/org/geogebra/web/html5/awt/GGraphics2DW.java
@@ -416,6 +416,13 @@ public class GGraphics2DW implements org.geogebra.common.awt.GGraphics2D {
 		this.updateCanvasColor();
 	}
 
+	public void setCoordinateSpaceSizeNoTransformNoColor(int width, int height) {
+		canvas.setCoordinateSpaceWidth(physicalPX(width));
+		canvas.setCoordinateSpaceHeight(physicalPX(height));
+		setWidth(width);
+		setHeight(height);
+	}
+
 	public int getOffsetWidth() {
 		int width = canvas.getOffsetWidth();
 		return width == 0 ? canvasWidth : width;

web/src/main/java/org/geogebra/web/html5/main/AppW.java
@@ -3172,6 +3172,17 @@ public abstract class AppW extends App implements SetLabels, HasKeyboard {
 		stopCollectingRepaints();
 	}
 
+	/**
+	 * Resets the width of the Canvas converning the Width of its wrapper
+	 * (splitlayoutpanel center)
+	 *
+	 * @param width
+	 *            , height
+	 */
+	public void ggwGraphicsView3DDimChanged(int width, int height) {
+		// only used for 3D
+	}
+
 	@Override
 	public void ensureTimerRunning() {
 		this.getTimerSystem().ensureRunning();

commit aab727b3b13f76b1c9de8ac172b77cb7acae4306

try to fix bug introduced by r42631
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@42788 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/main/java/org/geogebra/web/geogebra3D/web/gui/layout/panels/EuclidianDockPanel3DW.java
@@ -85,7 +85,7 @@ public class EuclidianDockPanel3DW extends EuclidianDockPanelWAbstract {
 		if (consProtNav != null) {
 			consProtNav.update();
 			consProtNav.setVisible(app.showConsProtNavigation(id));
-			updateEuclidianPanel();
+			// updateEuclidianPanel();
 			euclidianpanel.onResize();
 		}
 	}