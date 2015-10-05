c0e6635fa39212026a48594c8f43f1b7112bf8e9
Zbyněk Konečný <zbynek@geogebra.org>
2015-10-05 09:18:29 +0000

    GGB-93 cancel previous slider when another slider is touched.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@43254 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/resources/org/geogebra/web/resources/js/jquery-ui.js
+++ web/src/main/resources/org/geogebra/web/resources/js/jquery-ui.js
@@ -906,8 +906,9 @@ var mouse = $.widget("ui.mouse", {
 
 	_mouseDown: function(event) {
 		// don't let more than one widget handle mouseStart
-		if ( mouseHandled ) {
-			return;
+		if ( mouseHandled && mouseHandled._mouseCancel) {
+			mouseHandled._mouseCancel();
+			mouseHandled = false;
 		}
 
 		this._mouseMoved = false;
@@ -960,7 +961,7 @@ var mouse = $.widget("ui.mouse", {
 
 		event.preventDefault();
 
-		mouseHandled = true;
+		mouseHandled = this;
 		return true;
 	},
 
@@ -1197,11 +1198,14 @@ var slider = $.widget( "ui.slider", $.ui.mouse, {
 		this._mouseDestroy();
 	},
 
-	_mouseCapture: function( event ) {
+	_mouseCapture: function( event ) {	
 		var position, normValue, distance, closestHandle, index, allowed, offset, mouseOverHandle,
 			that = this,
 			o = this.options;
-
+		if ( mouseHandled && mouseHandled._mouseCancel) {
+			mouseHandled._mouseCancel();
+			mouseHandled = false;
+		}
 		if ( o.disabled ) {
 			return false;
 		}
@@ -1280,6 +1284,21 @@ var slider = $.widget( "ui.slider", $.ui.mouse, {
 		this._clickOffset = null;
 		this._animateOff = false;
 
+		return false;
+	},
+	
+	_mouseCancel: function( event ) {
+		this.document
+		.unbind( "mousemove." + this.widgetName, this._mouseMoveDelegate )
+		.unbind( "mouseup." + this.widgetName, this._mouseUpDelegate );
+		this.handles.removeClass( "ui-state-active" );
+		this._mouseSliding = false;
+
+
+		this._handleIndex = null;
+		this._clickOffset = null;
+		this._animateOff = false;
+		mouseHandled = false;
 		return false;
 	},
 
@@ -1747,4 +1766,16 @@ var slider = $.widget( "ui.slider", $.ui.mouse, {
  *  jquery.ui.widget.js
  *  jquery.ui.mouse.js
  */
-!function(a){function f(a,b){if(!(a.originalEvent.touches.length>1)){a.preventDefault();var c=a.originalEvent.changedTouches[0],d=document.createEvent("MouseEvents");d.initMouseEvent(b,!0,!0,window,1,c.screenX,c.screenY,c.clientX,c.clientY,!1,!1,!1,!1,0,null),a.target.dispatchEvent(d)}}if(a.support.touch="ontouchend"in document,a.support.touch){var e,b=a.ui.mouse.prototype,c=b._mouseInit,d=b._mouseDestroy;b._touchStart=function(a){var b=this;!e&&b._mouseCapture(a.originalEvent.changedTouches[0])&&(e=!0,b._touchMoved=!1,f(a,"mouseover"),f(a,"mousemove"),f(a,"mousedown"))},b._touchMove=function(a){e&&(this._touchMoved=!0,f(a,"mousemove"))},b._touchEnd=function(a){e&&(f(a,"mouseup"),f(a,"mouseout"),this._touchMoved||f(a,"click"),e=!1)},b._mouseInit=function(){var b=this;b.element.bind({touchstart:a.proxy(b,"_touchStart"),touchmove:a.proxy(b,"_touchMove"),touchend:a.proxy(b,"_touchEnd")}),c.call(b)},b._mouseDestroy=function(){var b=this;b.element.unbind({touchstart:a.proxy(b,"_touchStart"),touchmove:a.proxy(b,"_touchMove"),touchend:a.proxy(b,"_touchEnd")}),d.call(b)}}}(jQuery || $ggbQuery);
+!function(a){function f(a,b){if(!(a.originalEvent.touches.length>1)){
+	//a.preventDefault();
+	var c=a.originalEvent.changedTouches[0],
+	d=document.createEvent("MouseEvents");
+	d.initMouseEvent(b,!0,!0,window,1,c.screenX,c.screenY,c.clientX,c.clientY,!1,!1,!1,!1,0,null),
+	a.target.dispatchEvent(d)}
+}if(a.support.touch="ontouchend"in document,a.support.touch){
+	var e,b=a.ui.mouse.prototype,c=b._mouseInit,d=b._mouseDestroy;
+	b._touchStart=function(a){var b=this;!e&&b._mouseCapture(a.originalEvent.changedTouches[0])&&(e=!0,b._touchMoved=!1,f(a,"mouseover"),f(a,"mousemove"),f(a,"mousedown"))},
+	b._touchMove=function(a){e&&(this._touchMoved=!0,f(a,"mousemove"))},
+	b._touchEnd=function(a){e&&(f(a,"mouseup"),f(a,"mouseout"),this._touchMoved||f(a,"click"),e=!1)},
+	b._mouseInit=function(){var b=this;b.element.bind({touchstart:a.proxy(b,"_touchStart"),touchmove:a.proxy(b,"_touchMove"),touchend:a.proxy(b,"_touchEnd")}),c.call(b)},
+	b._mouseDestroy=function(){var b=this;b.element.unbind({touchstart:a.proxy(b,"_touchStart"),touchmove:a.proxy(b,"_touchMove"),touchend:a.proxy(b,"_touchEnd")}),d.call(b)}}}(jQuery || $ggbQuery);
