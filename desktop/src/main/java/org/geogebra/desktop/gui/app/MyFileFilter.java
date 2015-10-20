df7ad0a7f7047c188527b6629ac3fbdea540fe5d
Michael Borcherds <michael@geogebra.org>
2015-10-20 13:39:35 +0000

    fix for missing files on File -> Open
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@43562 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/main/java/org/geogebra/desktop/gui/app/MyFileFilter.java
+++ desktop/src/main/java/org/geogebra/desktop/gui/app/MyFileFilter.java
@@ -140,11 +140,15 @@ public class MyFileFilter extends FileFilter implements java.io.FileFilter {
 	@Override
 	public boolean accept(File f) {
 		if (f != null) {
-			if (f.isDirectory())
+			if (f.isDirectory()) {
 				return true;
-			String extension = getExtension(f);
-			if (extension != null && filters.contains(getExtension(f)))
+			}
+
+			FileExtensions extension = FileExtensions.get(getExtension(f));
+
+			if (filters.contains(extension)) {
 				return true;
+			}
 		}
 		return false;
 	}
