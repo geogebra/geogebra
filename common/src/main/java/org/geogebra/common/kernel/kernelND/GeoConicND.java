geogebra/geogebra

commit 3cd4cc90069e12dc5631c590bd4da6cda6df6e78

fix #5001
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@42167 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/main/java/org/geogebra/common/kernel/kernelND/GeoConicND.java
@@ -3368,7 +3368,13 @@ FromMeta
 
 	
 	public boolean isInRegion(GeoPointND PI) {
-		Coords coords = PI.getCoordsInD2(getCoordSys());		
+
+		Coords coords = PI.getCoordsInD2IfInPlane(getCoordSys());
+
+		if (coords == null) { // point is not in plane containing the polygon
+			return false;
+		}
+
 		return isInRegion(coords.getX(),coords.getY());
 		
 	}

common/src/main/java/org/geogebra/common/geogebra3D/kernel3D/geos/GeoPoint3D.java
@@ -375,9 +375,22 @@ public class GeoPoint3D extends GeoVec4D implements GeoPointND, PathOrPoint,
 	private CoordMatrix4x4 tmpMatrix4x4;
 	private Coords tmpCoordsLength3;
 
-	@Override
+	public Coords getCoordsInD2IfInPlane(CoordSys coordSys) {
+
+		if (setCoords2D(coordSys)) {
+			return tmpCoordsLength3;
+		}
+
+		return null;
+	}
+
 	public Coords getCoordsInD2(CoordSys coordSys) {
 
+		setCoords2D(coordSys);
+		return tmpCoordsLength3;
+	}
+
+	private boolean setCoords2D(CoordSys coordSys) {
 		Coords coords;
 		if (tmpCoords1 == null) {
 			tmpCoords1 = Coords.createInhomCoorsInD3();
@@ -416,7 +429,7 @@ public class GeoPoint3D extends GeoVec4D implements GeoPointND, PathOrPoint,
 		tmpCoordsLength3.setY(tmpCoords1.getY());
 		tmpCoordsLength3.setZ(tmpCoords1.getW());
 
-		return tmpCoordsLength3;
+		return Kernel.isZero(tmpCoords1.getZ());
 
 	}
 

common/src/main/java/org/geogebra/common/geogebra3D/kernel3D/geos/GeoPolygon3D.java
@@ -578,7 +578,12 @@ public class GeoPolygon3D extends GeoPolygon implements GeoPolygon3DInterface,
 	@Override
 	public boolean isInRegion(GeoPointND PI) {
 		
-		Coords coords = PI.getCoordsInD2(getCoordSys());
+		Coords coords = PI.getCoordsInD2IfInPlane(getCoordSys());
+
+		if (coords == null) { // point is not in plane containing the polygon
+			return false;
+		}
+
 		return isInRegion(coords.getX(), coords.getY());
 
 	}

common/src/main/java/org/geogebra/common/kernel/geos/GeoPoint.java
@@ -1797,31 +1797,51 @@ SymbolicParametersBotanaAlgo {
 	
 	private Coords tmpCoords;
 	
-	public Coords getCoordsInD2(CoordSys coordSys) { // TODO use coord sys ?
+	public Coords getCoordsInD2IfInPlane(CoordSys coordSys){
+		
+		if (setCoords2D(coordSys)) {
+			return coords2D;
+		}
+
+		return null;
+	}
+	
+	public Coords getCoordsInD2(CoordSys coordSys) { 
+		
+		setCoords2D(coordSys);
+		return coords2D;
+	}
+		
+	private boolean setCoords2D(CoordSys coordSys) {
 		
 		if(coords2D == null){
 			coords2D = new Coords(new double[] { x, y, z });
-		}else{
-			if (coordSys == null){
-				coords2D.set(1, x);
-				coords2D.set(2, y);
-				coords2D.set(3, z);
-			}else{ // this should happen only when we try to put a 2D point on a 3D path (e.g. GeoConic3D)
-				// matrix for projection
-				if (tmpMatrix4x4 == null){
-					tmpMatrix4x4 = new CoordMatrix4x4();
-				}
-				tmpMatrix4x4.set(coordSys.getMatrixOrthonormal());
-				if (tmpCoords == null){
-					tmpCoords = new Coords(4);
-				}
-				getCoordsInD3().projectPlaneInPlaneCoords(tmpMatrix4x4, tmpCoords);
-				coords2D.setX(tmpCoords.getX());
-				coords2D.setY(tmpCoords.getY());
-				coords2D.setZ(tmpCoords.getW());
-			}
 		}
-		return coords2D;
+
+		if (coordSys == null) {
+			coords2D.set(1, x);
+			coords2D.set(2, y);
+			coords2D.set(3, z);
+		} else { // this should happen only when we try to put a 2D point on a
+					// 3D path (e.g. GeoConic3D)
+					// matrix for projection
+			if (tmpMatrix4x4 == null) {
+				tmpMatrix4x4 = new CoordMatrix4x4();
+			}
+			tmpMatrix4x4.set(coordSys.getMatrixOrthonormal());
+			if (tmpCoords == null) {
+				tmpCoords = new Coords(4);
+			}
+			getCoordsInD3().projectPlaneInPlaneCoords(tmpMatrix4x4, tmpCoords);
+			coords2D.setX(tmpCoords.getX());
+			coords2D.setY(tmpCoords.getY());
+			coords2D.setZ(tmpCoords.getW());
+
+			// check if point is included in the plane
+			return Kernel.isZero(tmpCoords.getZ());
+		}
+
+		return true;
 	}
 	
 	public Coords getCoordsInD2(){

common/src/main/java/org/geogebra/common/kernel/kernelND/GeoPointND.java
@@ -216,6 +216,14 @@ public interface GeoPointND extends PointProperties, Translateable,
 	public Coords getCoordsInD2(CoordSys coordSys);
 
 	/**
+	 * @param coordSys
+	 *            coord system
+	 * @return the coords of the point in 2D (projected on coord sys) or null if
+	 *         not included in coord sys
+	 */
+	public Coords getCoordsInD2IfInPlane(CoordSys coordSys);
+
+	/**
 	 * @return path on which this point lies
 	 */
 	public Path getPath();

commit 60d5c2d054a3b5daa03cb6ef1ce146caac9060c6

Fix bug introduced by r42167 (point changed in region stuck on outline)
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@43572 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/main/java/org/geogebra/common/kernel/kernelND/GeoConicND.java
@@ -3426,10 +3426,11 @@ FromMeta
 
 		RegionParameters rp = PI.getRegionParameters();
 
-		if (!isInRegion(PI)){
+		Coords coords = PI.getCoordsInD2(getCoordSys());
+
+		if (!isInRegion(coords.getX(), coords.getY())) {
 			moveBackToRegion(PI,rp);
 		}else{
-			Coords coords = PI.getCoordsInD2(getCoordSys());	
 			rp.setIsOnPath(false);
 				
 			coordsRWtoEV(coords);