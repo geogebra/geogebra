7cbb97ea0c74484d6aa34eea552fbfab0b839441
Balazs Bencze <megette_a@yahoo.com>
2015-10-29 13:36:14 +0000

    add fields for the views formatting TUBE-383
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@43727 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/move/ggtapi/models/MaterialRequest.java
+++ common/src/main/java/org/geogebra/common/move/ggtapi/models/MaterialRequest.java
@@ -13,45 +13,43 @@ import org.geogebra.common.move.ggtapi.models.json.JSONString;
  * 
  * @author Matthias Meisinger
  */
-public class MaterialRequest implements Request
-{
-	enum Task
-	{
+public class MaterialRequest implements Request {
+
+	enum Task {
 		fetch;
 	}
 
-	public enum Fields
-	{
-		id, title, type, description, timestamp, author, author_id, url, url_direct, language, thumbnail, featured, likes,
- width, height, instructions_pre, instructions_post, ggbBase64, toolbar, menubar, inputbar, modified, visibility, favorite;
+	public enum Fields {
+		id, title, type, description, timestamp, author, author_id, url,
+		url_direct, language, thumbnail, featured, likes, width, height,
+		instructions_pre, instructions_post, ggbBase64, toolbar, menubar,
+		inputbar, modified, visibility, favorite, is3d, spreadsheet, cas,
+		graphics2, constprot, propcalc, dataanalysis, funcinsp, python, macro;
 	}
 
-	public enum Filters
-	{
-		id, title, search, type, description, timestamp, author, author_url, language, featured, likes, inbook, inws;
+	public enum Filters {
+		id, title, search, type, description, timestamp, author, author_url,
+		language, featured, likes, inbook, inws;
 	}
 
-	public enum Order
-	{
-		id, title, type, description, timestamp, author, language, featured, likes, relevance;
+	public enum Order {
+		id, title, type, description, timestamp, author, language, featured,
+		likes, relevance;
 	}
 
-	public enum Type
-	{
+	public enum Type {
 		asc, desc;
 	}
 
 	private static final String api = "1.1.0";
 	private Task task = Task.fetch;
 
-	public Fields[] fields = new Fields[]{Fields.id, Fields.title, Fields.type, Fields.timestamp,
- Fields.author, Fields.author_id,
-			Fields.url, Fields.url_direct,
- Fields.thumbnail, Fields.featured,
-			Fields.likes, Fields.modified, Fields.visibility, Fields.favorite
-			};
+	public Fields[] fields = new Fields[] { Fields.id, Fields.title,
+			Fields.type, Fields.timestamp, Fields.author, Fields.author_id,
+			Fields.url, Fields.url_direct, Fields.thumbnail, Fields.featured,
+			Fields.likes, Fields.modified, Fields.visibility, Fields.favorite };
 	public Filters[] filters = { Filters.search };
-	public Map<Filters,String> filterMap = new HashMap<Filters,String>();
+	public Map<Filters, String> filterMap = new HashMap<Filters, String>();
 	public Order by = Order.relevance;
 	public Type type = Type.desc;
 	public int limit = GeoGebraTubeAPI.STANDARD_RESULT_QUANTITY;
@@ -70,11 +68,11 @@ public class MaterialRequest implements Request
 	private final AuthenticationModel model;
 	private final ClientInfo client;
 	public TreeSet<Filters> negFilters = new TreeSet<Filters>();
+
 	/**
 	 * Constructor for a Featured Materials Request
 	 */
-	public MaterialRequest(ClientInfo client)
-	{
+	public MaterialRequest(ClientInfo client) {
 		this.client = client;
 		this.model = client.getModel();
 	}
@@ -82,18 +80,17 @@ public class MaterialRequest implements Request
 	/**
 	 * Constructor for a Search Request
 	 * 
-	 * @param query search term or #id
+	 * @param query
+	 *            search term or #id
 	 */
-	public MaterialRequest(String query, ClientInfo client)
-	{
+	public MaterialRequest(String query, ClientInfo client) {
 		this(client);
 		this.filterMap.put(Filters.type, "ggb");
-		if(query!=null && query.startsWith("#")){
+		if (query != null && query.startsWith("#")) {
 			this.filters = new Filters[] { Filters.id };
 			this.filterMap.put(Filters.id, query.substring(1));
 			this.by = Order.timestamp;
-		}
-		else{
+		} else {
 			this.filters = new Filters[] { Filters.search };
 			this.filterMap.put(Filters.search, query);
 		}
@@ -105,24 +102,21 @@ public class MaterialRequest implements Request
 	 * @param filters
 	 * @param by
 	 */
-	public static MaterialRequest forId(String id, ClientInfo client)
-	{
+	public static MaterialRequest forId(String id, ClientInfo client) {
 		MaterialRequest mr = new MaterialRequest(client);
 		mr.fields = Fields.values();
 		mr.by = Order.id;
 		mr.filters = new Filters[] { Filters.id };
 		mr.filterMap.put(Filters.type, "ggb");
-		mr.filterMap.put(Filters.id, id+"");
+		mr.filterMap.put(Filters.id, id + "");
 		return mr;
 	}
 
-	public String toJSONString(ClientInfo client)
-	{
+	public String toJSONString(ClientInfo client) {
 		this.apiJSON.put("-api", new JSONString(MaterialRequest.api));
 		this.taskJSON.put("-type", new JSONString(this.task.toString()));
 
-		for (int i = 0; i < this.fields.length; i++)
-		{
+		for (int i = 0; i < this.fields.length; i++) {
 			JSONObject current = new JSONObject();
 			current.put("-name", new JSONString(this.fields[i].toString()));
 			this.fieldJSON.set(i, current);
@@ -130,15 +124,15 @@ public class MaterialRequest implements Request
 
 		this.fieldsJSON.put("field", this.fieldJSON);
 
-		for (int i = 0; i < this.filters.length; i++)
-		{
+		for (int i = 0; i < this.filters.length; i++) {
 			JSONObject current = new JSONObject();
 			current.put("-name", new JSONString(this.filters[i].toString()));
-			if(this.negFilters.contains(filters[i])){
+			if (this.negFilters.contains(filters[i])) {
 				current.put("-comp", new JSONString("neq"));
 			}
-			if (this.filterMap.get(this.filters[i]) != null){
-				current.put("#text", new JSONString(this.filterMap.get(this.filters[i])));
+			if (this.filterMap.get(this.filters[i]) != null) {
+				current.put("#text", new JSONString(this.filterMap
+						.get(this.filters[i])));
 			}
 
 			this.filterJSON.set(i, current);
@@ -154,16 +148,16 @@ public class MaterialRequest implements Request
 		this.taskJSON.put("filters", this.filtersJSON);
 		this.taskJSON.put("order", this.orderJSON);
 		this.taskJSON.put("limit", this.limitJSON);
-		if(this.model != null && model.isLoggedIn()){
+		if (this.model != null && model.isLoggedIn()) {
 			JSONObject login = new JSONObject();
 			login.put("-token", model.getLoginToken());
 			this.apiJSON.put("login", login);
 		}
-		if(this.client != null){
+		if (this.client != null) {
 			JSONObject clientJSON = new JSONObject();
 			clientJSON.put("-id", client.getId());
-			clientJSON.put("-width", client.getWidth()+"");
-			clientJSON.put("-height", client.getHeight()+"");
+			clientJSON.put("-width", client.getWidth() + "");
+			clientJSON.put("-height", client.getHeight() + "");
 			clientJSON.put("-type", client.getType());
 			clientJSON.put("-language", client.getLanguage());
 			this.apiJSON.put("client", clientJSON);
@@ -212,7 +206,7 @@ public class MaterialRequest implements Request
 
 	public static MaterialRequest forFeaturedGgb(ClientInfo client) {
 		MaterialRequest req = new MaterialRequest(client);
-		req.filters = new Filters[] { Filters.featured, Filters.type};
+		req.filters = new Filters[] { Filters.featured, Filters.type };
 		req.filterMap.put(Filters.type, "ggb");
 		req.filterMap.put(Filters.featured, "true");
 		req.type = Type.desc;
@@ -221,7 +215,7 @@ public class MaterialRequest implements Request
 
 	public static MaterialRequest searchGgb(ClientInfo client, String query) {
 		MaterialRequest req = new MaterialRequest(client);
-		req.filters = new Filters[] { Filters.search, Filters.type};
+		req.filters = new Filters[] { Filters.search, Filters.type };
 		req.filterMap.put(Filters.search, query);
 		req.filterMap.put(Filters.type, "ggb");
 		return req;
@@ -232,7 +226,7 @@ public class MaterialRequest implements Request
 		req.filters = new Filters[] { Filters.inbook, Filters.type };
 		req.filterMap.put(Filters.type, "link");
 		req.negFilters.add(Filters.type);
-		req.filterMap.put(Filters.inbook, id+"");
+		req.filterMap.put(Filters.inbook, id + "");
 		req.by = Order.timestamp;
 		req.type = Type.desc;
 		return req;
