1ac7db11cb54b0dbc15a032651e29ae25aced393
Michael Borcherds <michael@geogebra.org>
2015-11-08 17:11:28 +0000

    GGB-22 DRAW_DROPDOWNLISTS_TO_CANVAS -> stable
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@43890 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -3906,7 +3906,6 @@ public abstract class App implements UpdateSelection {
 		case CP_NEW_COLUMNS:
 		case EXAM:
 		case FREE_3DCONICS:
-		case DRAW_DROPDOWNLISTS_TO_CANVAS:
 		case SHADERS_IN_DESKTOP:
 			return prerelease;
 		// return canary;
@@ -3921,6 +3920,7 @@ public abstract class App implements UpdateSelection {
 		case GL_ELEMENTS:
 		case AUTOMATIC_FONTSIZE:
 		case RETINA:
+		case DRAW_DROPDOWNLISTS_TO_CANVAS:
 			return true;
 		default:
 			return false;
