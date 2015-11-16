eaaf135b7baf697ecc2258a85bcf8ae1898699ef
Michael Borcherds <michael@geogebra.org>
2015-11-16 10:43:29 +0000

    fix test Tangent_PointOffConic_0
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@44011 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/test/java/org/geogebra/cas/GeoGebraCasIntegrationTest.java
+++ desktop/src/test/java/org/geogebra/cas/GeoGebraCasIntegrationTest.java
@@ -3812,7 +3812,7 @@ public class GeoGebraCasIntegrationTest {
   @Test
   public void Tangent_PointOffConic_0 () {
 	  t("c := Ellipse[(1, 1), (3, 2), (2, 3)]",
-				"c: ((8 * x^(2)) * sqrt(10)) + (12 * x^(2)) - ((32 * x) * sqrt(10)) - ((16 * x) * y) - (24 * x) + ((8 * sqrt(10)) * y^(2)) - ((24 * sqrt(10)) * y) + (32 * sqrt(10)) + (24 * y^(2)) - (40 * y) = 0");
+				"8 * x^(2) * sqrt(10) + 12 * x^(2) - 32 * x * sqrt(10) - 16 * x * y - 24 * x + 8 * sqrt(10) * y^(2) - 24 * sqrt(10) * y + 32 * sqrt(10) + 24 * y^(2) - 40 * y = 0");
 	  t("P := (0, (-3 * sqrt(10) * sqrt(224 * sqrt(10) + 687) * sqrt(31) + 672 * sqrt(10) - 11 * sqrt(224 * sqrt(10) + 687) * sqrt(31) + 2061) / (448 * sqrt(10) + 1374))",
 				"P:=(0, ((-sqrt((2 * sqrt(10)) + 3)) + 3) / 2)");
 		t("Tangent[P, c]",
