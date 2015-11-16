6b206c372bf4c88bd978ab7e6f728b535a15c890
Michael Borcherds <michael@geogebra.org>
2015-11-16 14:17:32 +0000

    fix test Tangent_PointOffConic_0
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@44024 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/test/java/org/geogebra/cas/GeoGebraCasIntegrationTest.java
+++ desktop/src/test/java/org/geogebra/cas/GeoGebraCasIntegrationTest.java
@@ -3816,7 +3816,7 @@ public class GeoGebraCasIntegrationTest {
 	  t("P := (0, (-3 * sqrt(10) * sqrt(224 * sqrt(10) + 687) * sqrt(31) + 672 * sqrt(10) - 11 * sqrt(224 * sqrt(10) + 687) * sqrt(31) + 2061) / (448 * sqrt(10) + 1374))",
 				"(0, (-sqrt(2 * sqrt(10) + 3) + 3) / 2)");
 		t("Tangent[P, c]",
-				"{y = ((-sqrt((2 * sqrt(10)) + 3)) + 3) / 2, y = (5.558213948645 * x) - 0.02680674287353}");
+				"{y = (-sqrt(2 * sqrt(10) + 3) + 3) / 2, y = 5.558213948645 * x - 0.02680674287353}");
   }
 
   // TODO Add tests for other conics.
