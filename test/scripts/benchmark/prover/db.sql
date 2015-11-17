5f560b07f7426e0e894b785d85ab676b330bd277
Zoltán Kovács <zoltan@geogebra.org>
2015-11-17 09:03:00 +0000

    Theorem Proving: use SQLite3 to save test results (first step), add Giac export
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@44032 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- test/scripts/benchmark/prover/db.sql
+++ test/scripts/benchmark/prover/db.sql
@@ -1,12 +1,12 @@
 create table engines (
- name text,
+ name text not null primary key,
  info text);
 
 insert into engines (name, info) values ('GG', 'GeoGebra''s internal prover');
 insert into engines (name, info) values ('OGP', 'OpenGeoProver');
 
 create table methods (
- name text,
+ name text not null primary key,
  info text);
 
 insert into methods (name, info) values ('Recio', 'Recio''s exact check method');
@@ -16,7 +16,7 @@ insert into methods (name, info) values ('Area', 'The area method');
 insert into methods (name, info) values ('Auto', 'Default prover');
 
 create table platforms (
- name text,
+ name text not null primary key,
  info text);
 
 insert into platforms (name, info) values ('desktop', 'Java native');
@@ -29,7 +29,8 @@ create table outsourcings (
 insert into outsourcings (name, info) values ('', 'no outsourcing: using internals');
 insert into outsourcings (name, info) values ('SingularWS', 'Singular WebService');
 
-create table environments (
+create table provers (
+ name text not null primary key,
  engine text,
  method text,
  outsourcing text,
@@ -37,13 +38,13 @@ create table environments (
  ndg boolean
 );
 
-insert into environments (engine, method, outsourcing, platform, ndg) values ('GG', 'Recio', '', 'desktop', 0);
-insert into environments (engine, method, outsourcing, platform, ndg) values ('GG', 'Botana', '', 'desktop', 1);
-insert into environments (engine, method, outsourcing, platform, ndg) values ('GG', 'Botana', 'SingularWS', 'desktop', 1);
-insert into environments (engine, method, outsourcing, platform, ndg) values ('GG', 'Auto', '', 'desktop', 1);
-insert into environments (engine, method, outsourcing, platform, ndg) values ('OGP', 'Wu', '', 'desktop', 1);
-insert into environments (engine, method, outsourcing, platform, ndg) values ('OGP', 'Area', '', 'desktop', 0);
-insert into environments (engine, method, outsourcing, platform, ndg) values ('GG', 'Auto', '', 'web', 1);
+insert into provers (name, engine, method, outsourcing, platform, ndg) values ('Recio', 'GG', 'Recio', '', 'desktop', 0);
+insert into provers (name, engine, method, outsourcing, platform, ndg) values ('BotanaGiac', 'GG', 'Botana', '', 'desktop', 1);
+insert into provers (name, engine, method, outsourcing, platform, ndg) values ('Botana', 'GG', 'Botana', 'SingularWS', 'desktop', 1);
+insert into provers (name, engine, method, outsourcing, platform, ndg) values ('Auto', 'GG', 'Auto', '', 'desktop', 1);
+insert into provers (name, engine, method, outsourcing, platform, ndg) values ('OpenGeoProver_Wu', 'OGP', 'Wu', '', 'desktop', 1);
+insert into provers (name, engine, method, outsourcing, platform, ndg) values ('OpenGeoProver_Area', 'OGP', 'Area', '', 'desktop', 0);
+insert into provers (name, engine, method, outsourcing, platform, ndg) values ('Web', 'GG', 'Auto', '', 'web', 1);
 
 -- Using Jenkins' environment settings, assuming SVN being used,
 -- see https://wiki.jenkins-ci.org/display/JENKINS/Building+a+software+project
@@ -54,12 +55,12 @@ create table builds (
  machine text);
 
 create table testcases (
- id text not null primary key);
+ name text not null primary key);
 
 create table tests (
- testcase text, -- references testcases.id
- engine text, -- references platforms.name
- platform text, -- references platforms.name
+ testcase text, -- references testcases.name
+ prover text, -- references provers.name
+ ndg boolean,
  build_number int not null, -- references builds.build_number
  start_unixtime int, -- when did the test start
  osresult int, -- exit code from OS (Linux)
@@ -67,6 +68,8 @@ create table tests (
  accepted bool,
  errortype int, -- 1: runtime error, 2: result error, 3: timeout
  speed int, -- in milliseconds
+ regressionfile text,
+ logfile text,
  stdout text,
  stderr text,
- unique (testcase, build_number));
+ unique (testcase, prover, ndg, build_number));
