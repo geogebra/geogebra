faa11db4555ea88938e0966c80bb11ff39012a25
Mathieu Blossier <mathieublossier@free.fr>
2015-11-19 10:34:49 +0000

    GGB-300: logical hitting for 3D curve
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@44092 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/geogebra3D/euclidian3D/draw/DrawCurve3D.java
+++ common/src/main/java/org/geogebra/common/geogebra3D/euclidian3D/draw/DrawCurve3D.java
@@ -2,11 +2,15 @@ package org.geogebra.common.geogebra3D.euclidian3D.draw;
 
 import org.geogebra.common.euclidian.plot.CurvePlotter;
 import org.geogebra.common.geogebra3D.euclidian3D.EuclidianView3D;
+import org.geogebra.common.geogebra3D.euclidian3D.Hitting;
 import org.geogebra.common.geogebra3D.euclidian3D.openGL.PlotterBrush;
 import org.geogebra.common.geogebra3D.euclidian3D.openGL.Renderer;
+import org.geogebra.common.geogebra3D.kernel3D.geos.GeoPoint3D;
+import org.geogebra.common.kernel.Matrix.Coords;
 import org.geogebra.common.kernel.geos.GeoElement;
 import org.geogebra.common.kernel.geos.GeoFunction;
 import org.geogebra.common.kernel.kernelND.CurveEvaluable;
+import org.geogebra.common.kernel.kernelND.GeoCurveCartesianND;
 
 /**
  * @author ggb3D
@@ -104,6 +108,67 @@ public class DrawCurve3D extends Drawable3DCurves {
 	@Override
 	public void removeFromDrawable3DLists(Drawable3DLists lists) {
 		removeFromDrawable3DLists(lists, DRAW_TYPE_CLIPPED_CURVES);
+	}
+
+	private GeoPoint3D hittingPoint;
+	private Coords project;
+	private double[] lineCoords;
+
+	@Override
+	public boolean hit(Hitting hitting) {
+
+		if (waitForReset) { // prevent NPE
+			return false;
+		}
+		
+		GeoCurveCartesianND curveND = (GeoCurveCartesianND) getGeoElement();
+
+		if (hittingPoint == null){
+			hittingPoint = new GeoPoint3D(curveND.cons);
+			project = new Coords(4);
+			lineCoords = new double[2];
+		}
+
+		hittingPoint.setWillingCoords(hitting.origin);
+		hittingPoint.setWillingDirection(hitting.direction);
+
+		double t = curveND.getClosestParameter(hittingPoint, hittingPoint
+				.getPathParameter().getT());
+		
+		// App.debug("" + t);
+		
+		hittingPoint.getPathParameter().setT(t);
+
+		curveND.pathChanged(hittingPoint);
+
+		Coords closestPoint = hittingPoint.getInhomCoordsInD3();
+		closestPoint.projectLine(hitting.origin, hitting.direction, project,
+				lineCoords);
+
+		// App.debug("\n" + hitting.origin + "\nclosest point:\n" + closestPoint
+		// + "\nclosest point on line:\n" + project);
+
+		double d = project.distance(closestPoint);
+		if (d * getView3D().getScale() <= getGeoElement().getLineThickness() + 2) {
+			double z = -lineCoords[0];
+			double dz = getGeoElement().getLineThickness()
+					/ getView3D().getScale();
+			setZPick(z + dz, z - dz);
+			return true;
+		}
+
+		return false;
+
+
+	}
+
+	@Override
+	public boolean hitForList(Hitting hitting) {
+		if (hasGeoElementVisible() && getGeoElement().isPickable()) {
+			return hit(hitting);
+		}
+
+		return false;
 	}
 
 }
