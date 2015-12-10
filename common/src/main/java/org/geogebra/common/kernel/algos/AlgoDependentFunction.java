geogebra/geogebra

commit 010aa7d55e2cc3f7a801ceeec7ebd9a29998b853

Invalid function syntax should throw exception before function is added to XML
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@44478 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/main/java/org/geogebra/common/kernel/algos/AlgoDependentFunction.java
@@ -62,10 +62,12 @@ public class AlgoDependentFunction extends AlgoElement implements DependentAlgo 
 	 */
 	public AlgoDependentFunction(Construction cons, Function fun) {
 		super(cons);
+		fun.initFunction();
+		cons.addToConstructionList(this, false);
 		this.fun = fun;
 		f = new GeoFunction(cons, false);
 		f.setFunction(fun);
-		f.initFunction();
+		f.setConstructionDefaults();
 
 		// look for FUNCTION or DERIVATIVE nodes in function
 		expression = fun.getExpression();
@@ -322,9 +324,13 @@ public class AlgoDependentFunction extends AlgoElement implements DependentAlgo 
 		return sb.toString();
 	}
 
-	/*
+	/***
 	 * checks to see if this is an nth derivative, and return an appropriate
 	 * label eg f''' for 3rd derivative
+	 * 
+	 * @param fun
+	 *            function
+	 * @return label
 	 */
 	public static String getDerivativeLabel(Function fun) {
 		ExpressionValue ev = fun.getExpression().getLeft();

common/src/main/java/org/geogebra/common/kernel/algos/AlgoDependentFunctionNVar.java
@@ -57,11 +57,13 @@ public class AlgoDependentFunctionNVar extends AlgoElement implements
 	 *            input function
 	 */
 	AlgoDependentFunctionNVar(Construction cons, FunctionNVar fun) {
-		super(cons);		
+		super(cons, false);
+		fun.initFunction();
+		cons.addToConstructionList(this, false);
 		this.fun = fun;
 		f = new GeoFunctionNVar(cons, false);
 		f.setFunction(fun);
-		fun.initFunction();
+
 		expression = fun.getExpression();
 		expContainsFunctions = AlgoDependentFunction
 				.containsFunctions(expression);

commit c7404f867c9d7a71e9c76491dc665d5c97c3e78d

GGB-357 fix for the bug in Mathieu's comment introduced in r44478
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@44483 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/main/java/org/geogebra/common/kernel/algos/AlgoDependentFunction.java
@@ -61,7 +61,7 @@ public class AlgoDependentFunction extends AlgoElement implements DependentAlgo 
 	 *            input function
 	 */
 	public AlgoDependentFunction(Construction cons, Function fun) {
-		super(cons);
+		super(cons, false);
 		fun.initFunction();
 		cons.addToConstructionList(this, false);
 		this.fun = fun;