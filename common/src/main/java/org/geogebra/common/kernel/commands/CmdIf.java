025e38daa263327fa72a89636b79ad3755afa847
Zbyněk Konečný <zbynek@geogebra.org>
2015-12-14 11:50:56 +0000

    Reverting the critical part of r44504
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@44540 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/kernel/commands/CmdIf.java
+++ common/src/main/java/org/geogebra/common/kernel/commands/CmdIf.java
@@ -43,25 +43,16 @@ public class CmdIf extends CommandProcessor {
 		super(kernel);
 	}
 
-	@Override
-	public ExpressionValue simplify(Command c) {
-		ExpressionNode ret = null;
-		if (c.getArgumentNumber() == 2) {
-			ret = new ExpressionNode(kernelA, c.getArgument(0), Operation.IF,
-					c.getArgument(1));
-		}
-		if (c.getArgumentNumber() == 3) {
-			ret = new ExpressionNode(kernelA, new MyNumberPair(kernelA,
-					c.getArgument(0), c.getArgument(1)),
-					Operation.IF_ELSE,
-					c.getArgument(2));
-		}
-		if (ret != null) {
-			ret.resolveVariables();
-			return ret;
-		}
-		return super.simplify(c);
-	}
+	// @Override
+	/*
+	 * public ExpressionValue simplify(Command c) { ExpressionNode ret = null;
+	 * if (c.getArgumentNumber() == 2) { ret = new ExpressionNode(kernelA,
+	 * c.getArgument(0), Operation.IF, c.getArgument(1)); } if
+	 * (c.getArgumentNumber() == 3) { ret = new ExpressionNode(kernelA, new
+	 * MyNumberPair(kernelA, c.getArgument(0), c.getArgument(1)),
+	 * Operation.IF_ELSE, c.getArgument(2)); } if (ret != null) {
+	 * ret.resolveVariables(); return ret; } return super.simplify(c); }
+	 */
 	@Override
 	public GeoElement[] process(Command c) throws MyError {
 		int n = c.getArgumentNumber();
