50b196345a13bc531e3f6b031f32a692a9eeb1bf
Mathieu Blossier <mathieublossier@free.fr>
2016-03-03 08:42:11 +0000

    turn MOBILE_PROPERITIES_VIEW feature to false
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@45731 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -1,12 +1,5 @@
 package org.geogebra.common.main;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
-import java.util.Vector;
-
 import org.geogebra.common.GeoGebraConstants;
 import org.geogebra.common.awt.GDimension;
 import org.geogebra.common.awt.GFont;
@@ -82,6 +75,13 @@ import org.geogebra.common.util.NormalizerMinimal;
 import org.geogebra.common.util.StringUtil;
 import org.geogebra.common.util.Util;
 import org.geogebra.common.util.debug.Log;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
 
 /**
  * Represents an application window, gives access to views and system stuff
@@ -3842,6 +3842,7 @@ public abstract class App implements UpdateSelection {
 			return prerelease;
 
 		case MOBILE_PROPERTIES_VIEW:
+			return false;
 		case MOBILE_WEB_VIEW:
 			return prerelease;
 
@@ -4121,6 +4122,14 @@ public abstract class App implements UpdateSelection {
 	}
 
 	/**
+	 * Creates a new Timer.
+	 *
+	 * @param delay Milliseconds to run timer after start()1.
+	 * @return GTimer descendant instance.
+	 */
+	public abstract GTimer newTimer(GTimerListener listener, int delay);
+
+	/**
 	 * possible positions for the inputBar (respective inputBox)
 	 */
 	public enum InputPositon {
@@ -4157,13 +4166,4 @@ public abstract class App implements UpdateSelection {
 	public interface ViewCallback {
 		public void run(int viewID, String viewName);
 	}
-
-	/**
-	 * Creates a new Timer.
-	 * 
-	 * @param delay
-	 *            Milliseconds to run timer after start()1.
-	 * @return GTimer descendant instance.
-	 */
-	public abstract GTimer newTimer(GTimerListener listener, int delay);
 }
