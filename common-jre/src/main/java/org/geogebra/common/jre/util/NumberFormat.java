e3b989abc36f352790a19ab7a27ff8ef95dc872d
Balazs Bencze <megette_a@yahoo.com>
2016-03-04 13:52:46 +0000

    add two classes to the project
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@45765 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common-jre/src/main/java/org/geogebra/common/jre/util/NumberFormat.java
@@ -0,0 +1,24 @@
+package org.geogebra.common.jre.util;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+import org.geogebra.common.util.NumberFormatAdapter;
+
+public class NumberFormat extends DecimalFormat implements NumberFormatAdapter {
+
+	private static final long serialVersionUID = 1L;
+
+	public NumberFormat() {
+		super();
+		this.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.US));
+	}
+
+	public NumberFormat(String pattern, int i) {
+		super(pattern);
+		setMaximumFractionDigits(i);
+		this.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.US));
+	}
+
+}
