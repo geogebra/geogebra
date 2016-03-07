83f5cb0ffdf67e0793d1fa8a929a28f810f0f996
Mathieu Blossier <mathieublossier@free.fr>
2016-03-07 14:03:50 +0000

    MOB-357: smooth pinch zooming
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@45827 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -1,12 +1,5 @@
 package org.geogebra.common.main;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
-import java.util.Vector;
-
 import org.geogebra.common.GeoGebraConstants;
 import org.geogebra.common.awt.GDimension;
 import org.geogebra.common.awt.GFont;
@@ -82,6 +75,13 @@ import org.geogebra.common.util.NormalizerMinimal;
 import org.geogebra.common.util.StringUtil;
 import org.geogebra.common.util.Util;
 import org.geogebra.common.util.debug.Log;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
 
 /**
  * Represents an application window, gives access to views and system stuff
@@ -3849,6 +3849,9 @@ public abstract class App implements UpdateSelection {
 
 		case MOBILE_INPUT_BAR_HELP_PANEL:
 			return prerelease;
+
+			case MOBILE_SMOOTH_PINCH:
+				return prerelease;
 
 		case AV_EXTENSIONS:
 		case DATA_COLLECTION:
