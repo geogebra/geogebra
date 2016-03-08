b4124f8dd3cf94255b7025c828b3977c7ce6b9e4
Zbyněk Konečný <zbynek@geogebra.org>
2016-03-08 14:40:21 +0000

    GGB-605 Free copy of NDerivative functions should stay NDerivative
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@45860 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/kernel/commands/CmdCopyFreeObject.java
+++ common/src/main/java/org/geogebra/common/kernel/commands/CmdCopyFreeObject.java
@@ -33,41 +33,14 @@ public class CmdCopyFreeObject extends CommandProcessor {
 		switch (n) {
 		// FunctionalNVar
 		case 1:
-
+			GeoElement geo;
 			String label = c.getLabel();
 			if (arg[0] instanceof FunctionalNVar) {
-				FunctionalNVar f = (FunctionalNVar) arg[0];
-				StringBuilder command = new StringBuilder();
 
-				// eg f(x,y)=
-				if (label != null) {
-					command.append(label);
-					command.append('(');
-					command.append(f
-							.getVarString(StringTemplate.defaultTemplate));
-					command.append(")=");
-				}
+				return copyFunction(arg[0], c, label);
 
-				StringTemplate highPrecision = StringTemplate.maxPrecision;
-				command.append(arg[0].toOutputValueString(highPrecision));
-
-				try {
-					GeoElement[] ret = kernelA.getAlgebraProcessor()
-							.processAlgebraCommandNoExceptions(
-									command.toString(), true);
-					ret[0].setVisualStyle(arg[0]);
-					if (!arg[0].isLabelSet())
-						arg[0].remove();
-					return ret;
-
-				} catch (Exception e) {
-					if (!arg[0].isLabelSet())
-						arg[0].remove();
-					e.printStackTrace();
-					throw argErr(app, c.getName(), arg[0]);
-				}
 			}
-			GeoElement geo;
+
 
 			if (arg[0] instanceof GeoSegmentND) {
 
@@ -94,5 +67,47 @@ public class CmdCopyFreeObject extends CommandProcessor {
 		default:
 			throw argNumErr(app, c.getName(), n);
 		}
+	}
+
+	private GeoElement[] copyFunction(GeoElement geoElement, Command c,
+			String label) {
+		FunctionalNVar f = (FunctionalNVar) geoElement;
+		StringBuilder command = new StringBuilder();
+
+		// eg f(x,y)=
+		if (label != null) {
+			command.append(label);
+			command.append('(');
+			command.append(f.getVarString(StringTemplate.defaultTemplate));
+			command.append(")=");
+		}
+		StringTemplate highPrecision = StringTemplate.maxPrecision;
+		if (f.getFunctionExpression().isSecret()) {
+			command.append(geoElement.getParentAlgorithm().getClassName());
+			command.append("[");
+			command.append(geoElement.getParentAlgorithm().getInput(0)
+					.toOutputValueString(highPrecision));
+			command.append("]");
+		} else {
+
+			command.append(geoElement.toOutputValueString(highPrecision));
+		}
+
+		try {
+			GeoElement[] ret = kernelA.getAlgebraProcessor()
+					.processAlgebraCommandNoExceptions(command.toString(),
+							true);
+			ret[0].setVisualStyle(geoElement);
+			if (!geoElement.isLabelSet())
+				geoElement.remove();
+			return ret;
+
+		} catch (Exception e) {
+			if (!geoElement.isLabelSet())
+				geoElement.remove();
+			e.printStackTrace();
+			throw argErr(geoElement, c);
+		}
+
 	}
 }
