67ea822e7b140f4f65ba4c7ba900744acab1cfc1
Alicia Hofstaetter <alicia.hofstaetter@geogebra.org>
2016-03-17 10:28:46 +0000

    remove console logs from exam
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@46084 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/web/gui/app/GGWToolBar.java
+++ web/src/main/java/org/geogebra/web/web/gui/app/GGWToolBar.java
@@ -325,7 +325,7 @@ pr.menu_header_undo(), null, 32);
 		//var focus;
 		//$wnd.console.log("focus 1: " + focus);
 		var fullscreen = true;
-		$wnd.console.log("fullscreen: " + fullscreen);
+		//$wnd.console.log("fullscreen: " + fullscreen);
 		if ($wnd.innerHeight < screen.height - 5
 				|| $wnd.innerWidth < screen.width - 5) {
 			fullscreen = false;
@@ -405,23 +405,24 @@ pr.menu_header_undo(), null, 32);
 		});
 
 		//$wnd.console.log("x= " + $wnd.screenX + "; y= " + $wnd.screenY);
-		var interval = setInterval(function() {
+		//var interval = setInterval(function() {
 
-			$wnd.console.log("innerHeight = " + $wnd.innerHeight
-					+ ", screen.height = " + screen.height + "; screenx= "
-					+ $wnd.screenX + "; screeny= " + $wnd.screenY
-					+ "; availHeight = " + screen.availHeight);
+		//	$wnd.console.log("innerHeight = " + $wnd.innerHeight
+		//			+ ", screen.height = " + screen.height + "; screenx= "
+		//			+ $wnd.screenX + "; screeny= " + $wnd.screenY
+		//			+ "; availHeight = " + screen.availHeight);
 
-			//if ($wnd.screenX != 0 || $wnd.screenY != 0) {
-			//	startCheating();
-			//	$wnd.console.log('moved! ' + "x= " + $wnd.screenX + "; y= "
-			//			+ $wnd.screenY);
-			//} else if ($wnd.innerHeight >= fullHeight && focus == true) {
-			//	stopCheating();
-			//$wnd.console.log('focus 4 ' + focus + ' stop!');
+		//if ($wnd.screenX != 0 || $wnd.screenY != 0) {
+		//	startCheating();
+		//	$wnd.console.log('moved! ' + "x= " + $wnd.screenX + "; y= "
+		//			+ $wnd.screenY);
+		//} else if ($wnd.innerHeight >= fullHeight && focus == true) {
+		//	stopCheating();
+		//$wnd.console.log('focus 4 ' + focus + ' stop!');
 
-			//}
-		}, 3000);
+		//}
+		//}, 3000);
+
 		// TODO find a way to stop interval on exit exam
 		//if (!examActive) {
 		//	$wnd.console.log("interval cleared");
