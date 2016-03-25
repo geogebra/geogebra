c31ebce81fca3ff1a0b7d911318b43ffc8373c9f
Mathieu Blossier <mathieublossier@free.fr>
2016-03-25 21:01:30 +0000

    HIT_PARAMETRIC_SURFACE back to prerelease
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@46251 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -3867,7 +3867,7 @@ public abstract class App implements UpdateSelection {
 			return true;
 
 		case HIT_PARAMETRIC_SURFACE:
-			return true;
+			return prerelease;
 
 		case PARAMETRIC_SURFACE_IS_REGION:
 			return prerelease;
