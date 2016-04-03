aac81c30f5603bfdfec56462668e37c32bdfea2e
Michael Borcherds <michael@geogebra.org>
2016-04-03 15:35:45 +0000

    GGB-690 fix registerFont() (remove reflection)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@46369 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- retex/renderer-desktop/src/main/java/com/himamis/retex/renderer/desktop/font/FontLoaderD.java
+++ retex/renderer-desktop/src/main/java/com/himamis/retex/renderer/desktop/font/FontLoaderD.java
@@ -55,47 +55,54 @@ import com.himamis.retex.renderer.share.platform.font.Font;
 import com.himamis.retex.renderer.share.platform.font.FontLoader;
 
 public class FontLoaderD implements FontLoader {
-	
-	private static boolean registerFontExceptionDisplayed = false; 
+
 	private static boolean shouldRegisterFonts = true;
 
 	public Font loadFont(Object base, String name) throws ResourceParseException {
-		
+
 		InputStream fontIn = (InputStream) new Resource().loadResource(base, name);
 		try {
-            java.awt.Font f = java.awt.Font.createFont(java.awt.Font.TRUETYPE_FONT, fontIn).deriveFont(PIXELS_PER_POINT);
-	    GraphicsEnvironment graphicEnv = GraphicsEnvironment.getLocalGraphicsEnvironment();
-	    /**
-	     * The following fails under java 1.5
-	     * graphicEnv.registerFont(f);
-	     * dynamic load then
-	     */
-	    if (shouldRegisterFonts) {
-		try {
-		    Method registerFontMethod = graphicEnv.getClass().getMethod("registerFont", new Class[] { Font.class });
-		    if ((Boolean) registerFontMethod.invoke(graphicEnv, new Object[] { f }) == Boolean.FALSE) {
-			System.err.println("Cannot register the font " + f.getFontName());
-		    }
-		} catch (Exception ex) {
-		    if (!registerFontExceptionDisplayed) {
-			System.err.println("Warning: Jlatexmath: Could not access to registerFont. Please update to java 6");
-			registerFontExceptionDisplayed = true;
-		    }
+			java.awt.Font f = java.awt.Font.createFont(java.awt.Font.TRUETYPE_FONT, fontIn)
+					.deriveFont(PIXELS_PER_POINT);
+			GraphicsEnvironment graphicEnv = GraphicsEnvironment.getLocalGraphicsEnvironment();
+
+			/**
+			 * The following fails under java 1.5 graphicEnv.registerFont(f);
+			 * dynamic load then
+			 */
+			if (shouldRegisterFonts) {
+
+				graphicEnv.registerFont(f);
+
+				// try {
+				// Method registerFontMethod =
+				// graphicEnv.getClass().getMethod("registerFont", new Class[] {
+				// Font.class });
+				// if ((Boolean) registerFontMethod.invoke(graphicEnv, new
+				// Object[] { f }) == Boolean.FALSE) {
+				// System.err.println("Cannot register the font " +
+				// f.getFontName());
+				// }
+				// } catch (Exception ex) {
+				// if (!registerFontExceptionDisplayed) {
+				// System.err.println("Warning: Jlatexmath: Could not access to
+				// registerFont. Please update to java 6");
+				// registerFontExceptionDisplayed = true;
+				// }
+				// }
+			}
+			return new FontD(f);
+		} catch (Exception e) {
+			throw new XMLResourceParseException(
+					"FontLoader" + ": FontLoader '" + name + "'. Error message: " + e.getMessage());
+		} finally {
+			try {
+				if (fontIn != null)
+					fontIn.close();
+			} catch (IOException ioex) {
+				throw new RuntimeException("Close threw exception", ioex);
+			}
 		}
-	    }
-	    return new FontD(f);
-        } catch (Exception e) {
-            throw new XMLResourceParseException("FontLoader"
-						+ ": FontLoader '" + name + "'. Error message: "
-						+ e.getMessage());
-        } finally {
-            try {
-                if (fontIn != null)
-                    fontIn.close();
-            } catch (IOException ioex) {
-                throw new RuntimeException("Close threw exception", ioex);
-            }
-        }
 	}
 
 }
