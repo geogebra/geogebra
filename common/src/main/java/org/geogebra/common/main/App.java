a91d2de6f94c0990dac799593f3718251324c5bd
Michael Borcherds <michael@geogebra.org>
2016-04-20 10:23:38 +0000

    GGB-737 add Feature.CAS_INTERSECT_3D
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@46775 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -1,5 +1,12 @@
 package org.geogebra.common.main;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
+
 import org.geogebra.common.GeoGebraConstants;
 import org.geogebra.common.awt.GDimension;
 import org.geogebra.common.awt.GFont;
@@ -75,13 +82,6 @@ import org.geogebra.common.util.NormalizerMinimal;
 import org.geogebra.common.util.StringUtil;
 import org.geogebra.common.util.Util;
 import org.geogebra.common.util.debug.Log;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
-import java.util.Vector;
 
 /**
  * Represents an application window, gives access to views and system stuff
@@ -4030,6 +4030,10 @@ public abstract class App implements UpdateSelection {
 
 		// GGB-766
 		case DESKTOP_EXPORT_BRAILLE:
+			return prerelease;
+
+		// GGB-737
+		case CAS_INTERSECT_3D:
 			return prerelease;
 
 		default:
