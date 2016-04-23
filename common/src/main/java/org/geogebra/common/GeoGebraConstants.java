f6b1bb4dd37c1a3c2ae3dd44c2a5b0c86864c43d
Zbyněk Konečný <zbynek@geogebra.org>
2016-04-23 14:19:49 +0000

    5.0.230
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@46855 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/GeoGebraConstants.java
+++ common/src/main/java/org/geogebra/common/GeoGebraConstants.java
@@ -12,7 +12,7 @@ public interface GeoGebraConstants {
 	/** last build date */
 	public static final String BUILD_DATE = "20 April 2016";
 	/** complete version string */
-	public static final String VERSION_STRING = "5.0.229.0";
+	public static final String VERSION_STRING = "5.0.230.0";
 
 	// proper noun, should NOT be translated / transliterated
 	public static final String APPLICATION_NAME = "GeoGebra";
