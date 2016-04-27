030ceb1560942be735ac7c8e2c78c48d4638698f
Mathieu Blossier <mathieublossier@free.fr>
2016-04-27 07:46:41 +0000

    MOB-571: show tooltip from active view mode
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@46914 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -1,12 +1,5 @@
 package org.geogebra.common.main;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
-import java.util.Vector;
-
 import org.geogebra.common.GeoGebraConstants;
 import org.geogebra.common.awt.GDimension;
 import org.geogebra.common.awt.GFont;
@@ -84,6 +77,13 @@ import org.geogebra.common.util.NormalizerMinimal;
 import org.geogebra.common.util.StringUtil;
 import org.geogebra.common.util.Util;
 import org.geogebra.common.util.debug.Log;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
 
 /**
  * Represents an application window, gives access to views and system stuff
@@ -1318,7 +1318,11 @@ public abstract class App implements UpdateSelection {
 	 * @return current mode
 	 */
 	final public int getMode() {
-		return this.createEuclidianView().getMode();
+		EuclidianView view = (EuclidianView) getActiveEuclidianView();
+		if (view == null) {
+			view = getEuclidianView1();
+		}
+		return view.getMode();
 	}
 
 	public void setMode(int mode) {
