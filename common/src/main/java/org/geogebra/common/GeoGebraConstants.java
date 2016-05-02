699ad042f73fe225a9276ed52e1c290f6ebd1b7a
Michael Borcherds <michael@geogebra.org>
2016-05-02 18:32:44 +0000

    5.0.232.1
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@47019 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/GeoGebraConstants.java
+++ common/src/main/java/org/geogebra/common/GeoGebraConstants.java
@@ -10,9 +10,9 @@ public interface GeoGebraConstants {
 	// as it is read by the build system
 	// update lines below when this is updated
 	/** last build date */
-	public static final String BUILD_DATE = "29 April 2016";
+	public static final String BUILD_DATE = "02 May 2016";
 	/** complete version string */
-	public static final String VERSION_STRING = "5.0.232.0";
+	public static final String VERSION_STRING = "5.0.232.1";
 
 	// proper noun, should NOT be translated / transliterated
 	public static final String APPLICATION_NAME = "GeoGebra";
