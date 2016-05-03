2e172ea1db2bdd78cf23d6fd5114f1bd271a6eda
Mathieu Blossier <mathieublossier@free.fr>
2016-05-03 11:35:37 +0000

    Use GLJPanel for mac osx
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@47028 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/main/java/org/geogebra/desktop/geogebra3D/euclidian3D/EuclidianView3DD.java
+++ desktop/src/main/java/org/geogebra/desktop/geogebra3D/euclidian3D/EuclidianView3DD.java
@@ -126,19 +126,32 @@ public class EuclidianView3DD extends EuclidianView3D implements
 		// we don't want shaders with win os < vista
 		if (app.has(Feature.SHADERS_IN_DESKTOP) && !app.isApplet()
 				&& !AppD.WINDOWS_VISTA_OR_EARLIER) {
-			return new RendererCheckGLVersionD(this, !app.isApplet());
+			return new RendererCheckGLVersionD(this, canUseCanvas());
 		}
 
 		if (app.useShaders()) {
-			return new RendererShadersElements(this, !app.isApplet());
+			return new RendererShadersElements(this, canUseCanvas());
 		}
 
-		return new RendererCheckGLVersionD(this, !app.isApplet(),
+		return new RendererCheckGLVersionD(this, canUseCanvas(),
 				RendererType.GL2);
 
 	}
 
 
+	private boolean canUseCanvas() {
+		if (app.isApplet()) {
+			return false;
+		}
+
+		// TODO remove that (quick fix for jogl 2.3.2)
+		if (AppD.MAC_OS) {
+			Log.debug("XXXXXXXXXXXXXXX mac osx");
+			return false;
+		}
+
+		return true;
+	}
 
 
 	protected void setBackground(GColor updatedColor, GColor applyedColor) {
