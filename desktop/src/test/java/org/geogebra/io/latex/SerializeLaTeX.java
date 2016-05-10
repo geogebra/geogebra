7f290cc5756d8956632b2037947e3376acaab4a4
Zbyněk Konečný <zbynek@geogebra.org>
2016-05-10 20:36:40 +0000

    Code for exponents taken from Desktop
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@47194 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/test/java/org/geogebra/io/latex/SerializeLaTeX.java
+++ desktop/src/test/java/org/geogebra/io/latex/SerializeLaTeX.java
@@ -3,6 +3,7 @@ package org.geogebra.io.latex;
 import org.geogebra.common.io.latex.GeoGebraSerializer;
 import org.geogebra.common.io.latex.ParseException;
 import org.geogebra.common.io.latex.Parser;
+import org.geogebra.common.util.Unicode;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -41,9 +42,21 @@ public class SerializeLaTeX {
 	}
 
 	@Test
+	public void testPower() {
+		checkCannon("x ^ 2", "x^(2)");
+		checkCannon("x" + Unicode.Superscript_2 + Unicode.Superscript_3,
+				"x^(23)");
+		checkCannon("x" + Unicode.Superscript_Minus + Unicode.Superscript_2
+				+ Unicode.Superscript_3, "x^(-23)");
+		checkCannon("1 + x" + Unicode.Superscript_Minus + Unicode.Superscript_2
+				+ Unicode.Superscript_3, "1+x^(-23)");
+
+	}
+
+	@Test
 	public void testPoint() {
 		checkCannon("(1, 2)", "(1,2)");
-		checkCannon("(1 ; 2)", "(1;2)");
+		checkCannon("(1; 2)", "(1;2)");
 
 	}
 
