d65a66290b479db8a878c6e37c8fa7aa866cb31a
Zbyněk Konečný <zbynek@geogebra.org>
2016-05-11 08:52:30 +0000

    Remove app.debu, gifshot
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@47204 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -440,17 +440,6 @@ public abstract class App implements UpdateSelection {
 	/* selection handling */
 
 	/**
-	 * @param s
-	 *            string to log
-	 *
-	 * @deprecated use Log.debug() instead
-	 */
-	@Deprecated
-	public static void debug(String s) {
-		Log.debug(s, 5);
-	}
-
-	/**
 	 * @param version string version, eg 4.9.38.0
 	 * @return version as list of ints, eg [4,9,38,0]
 	 */
