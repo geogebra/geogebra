80974d0d80f08df25c7d123de01a6e3062ee6412
Michael Borcherds <michael@geogebra.org>
2016-05-18 12:04:36 +0000

    add @SuppressWarnings("deprecation")
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@47353 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/main/java/org/geogebra/desktop/cas/giac/CASgiacD.java
+++ desktop/src/main/java/org/geogebra/desktop/cas/giac/CASgiacD.java
@@ -90,13 +90,12 @@ public class CASgiacD extends CASgiacJre {
 		return !AppD.LINUX;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	final protected void stopThread(Thread thread) {
 		// thread.interrupt() doesn't seem to stop it, so add this for
 		// good measure:
 		thread.stop();
-		// in fact, stop will do nothing (never implemented)
-		// Log.debug("giac: after interrupt/stop");
 	}
 
 }
