1ce2d26ff86534c6ab973d30eac945f5ad3983e2
Judit Elias <eliasjudit@gmail.com>
2016-06-06 13:38:08 +0000

    GGB-886 cleaning
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@47679 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/html5/util/tabpanel/MultiRowsTabPanel.java
+++ web/src/main/java/org/geogebra/web/html5/util/tabpanel/MultiRowsTabPanel.java
@@ -5,7 +5,6 @@ import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DeckPanel;
-import com.google.gwt.user.client.ui.SourcesTabEvents;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -24,14 +23,6 @@ public class MultiRowsTabPanel extends Composite implements TabPanelInterface {
 		panel.add(deck);
 		panel.setCellHeight(deck, "100%");
 		tabBar.setWidth("100%");
-		final MultiRowsTabPanel that = this;
-		tabBar.addSelectionHandler(new SelectionHandler<Integer>() {
-
-			public void onSelection(SelectionEvent<Integer> event) {
-				that.selectTab(event.getSelectedItem());
-			}
-
-		});
 		initWidget(panel);
 	}
 
@@ -42,17 +33,6 @@ public class MultiRowsTabPanel extends Composite implements TabPanelInterface {
 
 	public void selectTab(int index) {
 		tabBar.selectTab(index);
-		deck.showWidget(index);
-
-	}
-
-	public void selectTab(Widget w) {
-		for (int i = 0; i < tabBar.getWidgetCount(); i++) {
-			if (tabBar.getWidget(i) == w) {
-				selectTab(i);
-				return;
-			}
-		}
 	}
 
 	private int getTabCount() {
@@ -70,11 +50,6 @@ public class MultiRowsTabPanel extends Composite implements TabPanelInterface {
 
 	public int getWidgetIndex(Widget widget) {
 		return deck.getWidgetIndex(widget);
-	}
-
-	public boolean onBeforeTabSelected(SourcesTabEvents sender, int tabIndex) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
