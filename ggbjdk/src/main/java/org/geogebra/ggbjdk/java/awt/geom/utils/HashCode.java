18bd338e0ead9f9d90bc3390cdc2f1c76e7fdb65
Michael Borcherds <michael@geogebra.org>
2016-06-08 10:03:14 +0000

    fix HashCode
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@47743 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- ggbjdk/src/main/java/org/geogebra/ggbjdk/java/awt/geom/utils/HashCode.java
+++ ggbjdk/src/main/java/org/geogebra/ggbjdk/java/awt/geom/utils/HashCode.java
@@ -1,16 +1,18 @@
 package org.geogebra.ggbjdk.java.awt.geom.utils;
 
 public class HashCode {
-	int hashcode = 1837269811; 
+	int hashcode =  1837269851; 
 		
 	@Override
 	public int hashCode() {
 		return hashcode;
 	}
+	
+	// http://stackoverflow.com/questions/892618/create-a-hashcode-of-two-numbers
 	public void append(Double value) {
-		hashcode += (value.hashCode() * 32);
+		hashcode += hashcode * 31 + value.hashCode();
 	}
 	public void append(Integer value) {
-		hashcode += (value.hashCode() * 32);
+		hashcode += hashcode * 31 + value.hashCode();
 	}
 }
