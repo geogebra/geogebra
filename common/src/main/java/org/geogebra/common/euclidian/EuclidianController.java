ef2cc548ae35f1264414cd201b63c6af05afdf6e
Michael Borcherds <michael@geogebra.org>
2016-06-10 18:33:34 +0000

    remove debugging
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@47823 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/euclidian/EuclidianController.java
+++ common/src/main/java/org/geogebra/common/euclidian/EuclidianController.java
@@ -129,7 +129,6 @@ import org.geogebra.common.plugin.Operation;
 import org.geogebra.common.util.AsyncOperation;
 import org.geogebra.common.util.MyMath;
 import org.geogebra.common.util.Unicode;
-import org.geogebra.common.util.debug.Log;
 
 @SuppressWarnings("javadoc")
 public abstract class EuclidianController {
@@ -7272,8 +7271,6 @@ public abstract class EuclidianController {
 
 	protected void handleMovedElementFree(PointerEventType type) {
 
-		Log.debug("movedGeoElement = " + movedGeoElement.getClass());
-		Log.printStacktrace("");
 		if (handleMovedElementFreePoint()) {
 			return;
 		}
