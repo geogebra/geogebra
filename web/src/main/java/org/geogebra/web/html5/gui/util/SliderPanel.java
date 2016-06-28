f2728f85e6ea1e33ee2e9b6665b2a67bff31413d
Judit Elias <eliasjudit@gmail.com>
2016-06-28 09:58:41 +0000

    GGB-867 show actual value of slider instead of min and max (in properties)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@48047 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/html5/gui/util/SliderPanel.java
+++ web/src/main/java/org/geogebra/web/html5/gui/util/SliderPanel.java
@@ -3,7 +3,10 @@ package org.geogebra.web.html5.gui.util;
 import org.geogebra.common.util.debug.Log;
 import org.geogebra.web.html5.awt.GDimensionW;
 
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.dom.client.Document;
 import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.DomEvent;
 import com.google.gwt.event.dom.client.HasChangeHandlers;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
@@ -15,21 +18,27 @@ public class SliderPanel extends FlowPanel implements HasChangeHandlers,
         HasValue<Integer> {
 
 	private Slider slider;
-	private Label minLabel;
-	private Label maxLabel;
+	//private Label minLabel;
+	//private Label maxLabel;
+	public Label sliderLabel;
 
 	public SliderPanel() {
 		this(0, 100);
 	}
 
 	public SliderPanel(int min, int max) {
-		minLabel = new Label(String.valueOf(min));
-		add(minLabel);
+		//minLabel = new Label(String.valueOf(min));
+		// add(minLabel);
 		slider = new Slider(min, max);
 		add(slider);
-		maxLabel = new Label(String.valueOf(max));
-		add(maxLabel);
+		//maxLabel = new Label(String.valueOf(max));
+		// add(maxLabel);
+		sliderLabel = new Label();
+		sliderLabel.setText(this.getValue() + "");
+		add(sliderLabel);
+		sliderLabel.addStyleName("popupSliderLabel");
 		setStyleName("optionsSlider");
+		Slider.addInputHandler(slider.getElement(), getInputHandler(this));
 	}
 
 	public Integer getValue() {
@@ -38,12 +47,12 @@ public class SliderPanel extends FlowPanel implements HasChangeHandlers,
 
 	public void setMinimum(int min) {
 		slider.setMinimum(min);
-		minLabel.setText(String.valueOf(min));
+		// minLabel.setText(String.valueOf(min));
 	}
 
 	public void setMaximum(int max) {
 		slider.setMaximum(max);
-		maxLabel.setText(String.valueOf(max));
+		// maxLabel.setText(String.valueOf(max));
 	}
 
 	public void setMajorTickSpacing(int step) {
@@ -71,12 +80,26 @@ public class SliderPanel extends FlowPanel implements HasChangeHandlers,
 		return slider.addValueChangeHandler(handler);
 	}
 
+	private native JavaScriptObject getInputHandler(SliderPanel sp)/*-{
+		return function() {
+			sp.@org.geogebra.web.html5.gui.util.SliderPanel::doOninput()();
+		}
+	}-*/;
+
+	private void doOninput() {
+		DomEvent.fireNativeEvent(Document.get().createChangeEvent(),
+				this.slider);
+		sliderLabel.setText(this.getValue() + "");
+	}
+
 	public void setValue(Integer value) {
 		slider.setValue(value, false);
+		sliderLabel.setText(this.getValue() + "");
 	}
 
 	public void setValue(Integer value, boolean fireEvents) {
 		slider.setValue(value, fireEvents);
+		sliderLabel.setText(this.getValue() + "");
 	}
 
 	public HandlerRegistration addChangeHandler(ChangeHandler handler) {
--- web/src/main/java/org/geogebra/web/web/gui/view/algebra/InputTreeItem.java
+++ web/src/main/java/org/geogebra/web/web/gui/view/algebra/InputTreeItem.java
@@ -114,8 +114,16 @@ public class InputTreeItem extends RadioTreeItem implements
 		addDomHandlers(ihtml);
 		main.add(ihtml);
 
+
 		ihtml.getElement().appendChild(item.getElement());
 		ihtml.getElement().addClassName("hasCursorPermanent");
+
+		SimplePanel temp = new SimplePanel();
+		temp.setStyleName("temp");
+		temp.setWidth("40px");
+		ihtml.add(temp);
+		ihtml.add(new Label("valami"));
+
 
 		if (app.has(Feature.INPUTHELP_SHOWN_IN_AV)) {
 			ihtml.getElement().addClassName("tempHasCursorPermanent");
