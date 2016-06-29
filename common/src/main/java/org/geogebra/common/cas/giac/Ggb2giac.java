21d0977ae8d9c35fe3cfd8175433d88c43a0909a
Zbyněk Konečný <zbynek@geogebra.org>
2016-06-29 09:14:22 +0000

    CountIf working in CAS
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@48062 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
+++ common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
@@ -117,12 +117,12 @@ public class Ggb2giac {
 
 		p("CommonDenominator.2", "lcm(denom(%0),denom(%1))");
 
-		// TODO not working properly with == because of global evaluation
-		// CountIf[t>3,t,{1,2,3,4,5}]
+		// Use local function rather than subst to make sure t==5 is not
+		// evaluated to false before subst
 		p("CountIf.3",
-				"[[ggbcount(%1):=begin when(%0,1,0);end],map(%2,loc->ggbcount(loc))][1]");
-		p("CountIf.3",
-				"[[ggbcount(x):=begin when(%0,1,0);end],map(%1,loc->ggbcount(loc))][1]");
+				"[[ggbcount(%1):=begin when(%0,1,0);end],sum(map(%2,loc->ggbcount(loc)))][1]");
+		p("CountIf.2",
+				"[[ggbcount(x):=begin when(%0,1,0);end],sum(map(%1,loc->ggbcount(loc)))][1]");
 		// p("CountIf.2", "sum(map(%1,loc->when(subst(%0,x=loc),1,0))");
 
 		p("Covariance.2", "covariance(%0,%1)");
