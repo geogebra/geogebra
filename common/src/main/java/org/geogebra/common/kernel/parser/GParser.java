7c0595caa9a63a25763a532610b6f644cdeeb440
Zbyněk Konečný <zbynek@geogebra.org>
2016-07-29 10:55:25 +0000

    javadoc
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@48620 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/kernel/parser/GParser.java
+++ common/src/main/java/org/geogebra/common/kernel/parser/GParser.java
@@ -3,15 +3,31 @@ package org.geogebra.common.kernel.parser;
 import org.geogebra.common.kernel.Construction;
 import org.geogebra.common.kernel.Kernel;
 
+/**
+ * Faster exceptions
+ */
 public class GParser extends Parser {
-
+	/**
+	 * 
+	 * @param kernel
+	 *            kernel
+	 * @param cons
+	 *            construction
+	 */
 	public GParser(Kernel kernel, Construction cons) {
 		super(kernel, cons);
 	}
 
 	@Override
 	public ParseException generateParseException() {
-		return new ParseException("Unexpected token" + token.image);
+		if (jj_nt != null && jj_nt.image != null) {
+			return new ParseException("Unexpected next token: " + jj_nt.image);
+		}
+		if (token.image != null) {
+			return new ParseException("Unexpected token: " + token.image);
+		}
+
+		return new ParseException("Generic parse error.");
 
 	}
 
