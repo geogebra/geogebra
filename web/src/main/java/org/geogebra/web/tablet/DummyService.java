43e2865de6a8ef47f493df942b744732c744eae6
Zbyněk Konečný <zbynek@geogebra.org>
2016-08-02 14:08:22 +0000

    Superdev compile error
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@48653 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/tablet/DummyService.java
+++ web/src/main/java/org/geogebra/web/tablet/DummyService.java
@@ -3,12 +3,21 @@ package org.geogebra.web.tablet;
 import java.util.List;
 import java.util.logging.LogRecord;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.googlecode.gwtphonegap.client.log.shared.PhoneGapLogService;
+import com.googlecode.gwtphonegap.client.log.shared.PhoneGapLogServiceAsync;
 
-public class DummyService implements PhoneGapLogService {
+public class DummyService
+		implements PhoneGapLogService, PhoneGapLogServiceAsync {
 
 	public String logOnServer(String clientId, List<LogRecord> record) {
 		return "";
+	}
+
+	public void logOnServer(String clientId, List<LogRecord> record,
+			AsyncCallback<String> callback) {
+		// ignore
+
 	}
 
 }
