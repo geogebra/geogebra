8c1c94071d1f6713bf098a7ae6b711064b9f41e5
Michael Borcherds <michael@geogebra.org>
2016-08-14 07:34:32 +0000

    fix wrong comment
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@48782 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -1,5 +1,12 @@
 package org.geogebra.common.main;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
+
 import org.geogebra.common.GeoGebraConstants;
 import org.geogebra.common.awt.GDimension;
 import org.geogebra.common.awt.GFont;
@@ -74,13 +81,6 @@ import org.geogebra.common.util.NormalizerMinimal;
 import org.geogebra.common.util.StringUtil;
 import org.geogebra.common.util.Util;
 import org.geogebra.common.util.debug.Log;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
-import java.util.Vector;
 
 /**
  * Represents an application window, gives access to views and system stuff
@@ -4002,7 +4002,7 @@ public abstract class App implements UpdateSelection {
 
 		//MOB-695
 		case MOBILE_RELATION_TOOL:
-			return true; // 5.0.268
+			return true; // 5.0.269
 
 		default:
 			Log.debug("missing case in Feature: " + f);
