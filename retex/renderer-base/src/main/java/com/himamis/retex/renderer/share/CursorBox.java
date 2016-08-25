e273b209b5e70a2173cbb45627205b4186734fab
Balazs Bencze <megette_a@yahoo.com>
2016-08-25 10:46:13 +0000

    code formatting
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@48957 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- retex/renderer-base/src/main/java/com/himamis/retex/renderer/share/CursorBox.java
+++ retex/renderer-base/src/main/java/com/himamis/retex/renderer/share/CursorBox.java
@@ -1,15 +1,12 @@
 package com.himamis.retex.renderer.share;
 
-import java.util.ArrayList;
-
 import com.himamis.retex.renderer.share.platform.graphics.Graphics2DInterface;
 
 public class CursorBox extends Box {
-	private static final int DIAMETER = 10;
+
 	public static double startX, startY;
-	private Box content;
-	public static boolean touchSelection = true;
 	public static boolean blink = true;
+	private Box content;
 
 	public CursorBox(Box content, float coeff) {
 		this.content = content;
@@ -24,29 +21,16 @@ public class CursorBox extends Box {
 			content.draw(g2, (float) (x - content.width * 0.5), y);
 		}
 
-
 		CursorBox.startX = g2.getTransform().getScaleX() * x
 				+ g2.getTransform().getShearX() * y
 				+ g2.getTransform().getTranslateX();
 		CursorBox.startY = g2.getTransform().getScaleY() * y
 				+ g2.getTransform().getShearY() * x
 				+ g2.getTransform().getTranslateY();
-
-
-
-
-
 	}
 
 	@Override
 	public int getLastFontId() {
-		// TODO Auto-generated method stub
 		return content.getLastFontId();
-	}
-
-	@Override
-	public void getPath(float x, float y, ArrayList<Integer> list) {
-		super.getPath(x, y, list);
-
 	}
 }
