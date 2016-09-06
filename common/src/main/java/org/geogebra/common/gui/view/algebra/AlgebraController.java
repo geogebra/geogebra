d4f4d728ecd48cdd93006922a66d6fc06a7dabb4
Balazs Bencze <megette_a@yahoo.com>
2016-09-06 07:58:22 +0000

    Add common api for android and ios for evaluating algebra input
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@49079 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/gui/view/algebra/AlgebraController.java
+++ common/src/main/java/org/geogebra/common/gui/view/algebra/AlgebraController.java
@@ -14,11 +14,18 @@ package org.geogebra.common.gui.view.algebra;
 
 import java.util.ArrayList;
 
+import org.geogebra.common.euclidian.EuclidianView;
+import org.geogebra.common.euclidian.EuclidianViewInterfaceCommon;
+import org.geogebra.common.kernel.CircularDefinitionException;
+import org.geogebra.common.kernel.Construction;
 import org.geogebra.common.kernel.Kernel;
 import org.geogebra.common.kernel.StringTemplate;
 import org.geogebra.common.kernel.geos.GeoElement;
+import org.geogebra.common.kernel.geos.GeoPoint;
+import org.geogebra.common.kernel.geos.GeoText;
 import org.geogebra.common.main.App;
 import org.geogebra.common.main.SelectionManager;
+import org.geogebra.common.util.AsyncOperation;
 
 public class AlgebraController {
 
@@ -108,4 +115,103 @@ public class AlgebraController {
 		return latex;
 	}
 
+	/**
+	 * Evaluate the text entered in input. Used in Android and iOS.
+	 * @param input input string
+	 * @return evaluation was successful
+     */
+	public boolean onTextEntered(String input) {
+		GeoElement[] geos;
+		try {
+
+			AsyncOperation callback = new AsyncOperation() {
+
+				@Override
+				public void callback(Object obj) {
+
+					if (!(obj instanceof GeoElement[])) {
+						// no GeoElements were created
+						return;
+					}
+					GeoElement[] geos = (GeoElement[]) obj;
+
+					// need label if we type just eg lnx
+					if (geos.length == 1 && !geos[0].labelSet) {
+						geos[0].setLabel(geos[0].getDefaultLabel());
+					}
+
+					// create texts in the middle of the visible view
+					// we must check that size of geos is not 0 (ZoomIn,
+					// ZoomOut, ...)
+					if (geos.length > 0 && geos[0] != null
+							&& geos[0].isGeoText()) {
+						GeoText text = (GeoText) geos[0];
+						if (!text.isTextCommand()
+								&& text.getStartPoint() == null) {
+
+							Construction cons = text.getConstruction();
+							EuclidianViewInterfaceCommon ev = kernel.getApplication()
+									.getActiveEuclidianView();
+
+							boolean oldSuppressLabelsStatus = cons
+									.isSuppressLabelsActive();
+							cons.setSuppressLabelCreation(true);
+							GeoPoint p = new GeoPoint(text.getConstruction(),
+									null, (ev.getXmin() + ev.getXmax()) / 2,
+									(ev.getYmin() + ev.getYmax()) / 2, 1.0);
+							cons.setSuppressLabelCreation(oldSuppressLabelsStatus);
+
+							try {
+								text.setStartPoint(p);
+								text.update();
+							} catch (CircularDefinitionException e1) {
+								e1.printStackTrace();
+							}
+						}
+					}
+				}
+
+			};
+
+			geos = kernel.getAlgebraProcessor().processAlgebraCommandNoExceptionHandling(
+					input, true, app.getErrorHandler(), true, callback);
+
+			if (geos != null && geos.length == 1 && !geos[0].labelSet) {
+				geos[0].setLabel(geos[0].getDefaultLabel());
+			}
+
+		} catch (Exception ee) {
+			app.showError(ee, null);
+			return false;
+		} catch (Error ee) {
+			app.showError(ee.getMessage());
+			return false;
+		}
+
+		// create texts in the middle of the visible view
+		// we must check that size of geos is not 0 (ZoomIn, ZoomOut, ...)
+		if (geos != null && geos.length > 0 && geos[0] != null && geos[0].isGeoText()) {
+			GeoText text = (GeoText) geos[0];
+			if (!text.isTextCommand() && text.getStartPoint() == null) {
+
+				Construction cons = text.getConstruction();
+				EuclidianView ev = app.getActiveEuclidianView();
+
+				boolean oldSuppressLabelsStatus = cons.isSuppressLabelsActive();
+				cons.setSuppressLabelCreation(true);
+				GeoPoint p = new GeoPoint(text.getConstruction(), null,
+						(ev.getXmin() + ev.getXmax()) / 2, (ev.getYmin() + ev.getYmax()) / 2,
+						1.0);
+				cons.setSuppressLabelCreation(oldSuppressLabelsStatus);
+
+				try {
+					text.setStartPoint(p);
+					text.update();
+				} catch (CircularDefinitionException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		return true;
+	}
 }
