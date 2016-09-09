4ad17c2cdb1479b21eef99dd1c064b78fe6653eb
Zbyněk Konečný <zbynek@geogebra.org>
2016-09-09 12:15:13 +0000

    Changed exam fullscreen handler for Xwalk
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@49126 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/html5/main/ExamUtil.java
+++ web/src/main/java/org/geogebra/web/html5/main/ExamUtil.java
@@ -2,9 +2,9 @@ package org.geogebra.web.html5.main;
 
 public class ExamUtil {
 	public native static boolean toggleFullscreen(boolean full) /*-{
-		if ($wnd.ggbExamMode) {
+		if ($wnd.ggbExamMode && (full == !$wnd.ggbExamMode.running)) {
+			$wnd.ggbExamMode.running = full;
 			$wnd.ggbExamMode(full);
-			return true;
 		}
 		$wnd.toggleFullScreen(full);
 		return false;
