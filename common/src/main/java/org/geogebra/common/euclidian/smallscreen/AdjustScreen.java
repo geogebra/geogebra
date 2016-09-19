caea1933a76dd8632e99c01374d1c6a6410df922
Laszlo Gal <laszlo@geogebra.org>
2016-09-19 14:48:50 +0000

    GGB-334 TRAC-3401 Some kind of overlap handling with horizontal sliders.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@49321 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/euclidian/smallscreen/AdjustScreen.java
+++ common/src/main/java/org/geogebra/common/euclidian/smallscreen/AdjustScreen.java
@@ -1,5 +1,10 @@
 package org.geogebra.common.euclidian.smallscreen;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import org.geogebra.common.euclidian.EuclidianView;
 import org.geogebra.common.kernel.Kernel;
 import org.geogebra.common.kernel.geos.GeoElement;
@@ -8,14 +13,46 @@ import org.geogebra.common.main.App;
 import org.geogebra.common.main.Feature;
 import org.geogebra.common.util.debug.Log;
 
+/**
+ * Checks if the original screen was bigger when file was saved or not. If so,
+ * some widgets needs to be adjusted to fit the smaller screen.
+ * 
+ * @author laszlo
+ *
+ */
 public class AdjustScreen {
+	private static final int HSLIDER_OVERLAP_THRESOLD = 20;
 	private EuclidianView view;
 	private App app;
 	private Kernel kernel;
+	private boolean enabled;
+	private List<GeoNumeric> hSliders = new ArrayList<GeoNumeric>();
+	private List<GeoNumeric> vSliders = new ArrayList<GeoNumeric>();
+
+	private class HSliderComparator implements Comparator {
+		public int compare(Object o1, Object o2) {
+			double y1 = ((GeoNumeric) o1).getSliderY();
+			double y2 = ((GeoNumeric) o2).getSliderY();
+			if (y1 == y2) {
+				return 0;
+			}
+			return y1 < y2 ? -1 : 1;
+		}
+	}
+
 	public AdjustScreen(EuclidianView view) {
 		this.view = view;
 		app = view.getApplication();
 		kernel = app.getKernel();
+		enabled = true;// needsAdjusting();
+		apply();
+	}
+
+	private void apply() {
+		if (!enabled) {
+			return;
+		}
+
 		collectSliders();
 	}
 
@@ -25,14 +62,46 @@ public class AdjustScreen {
 			if (geo instanceof GeoNumeric) {
 				GeoNumeric num = (GeoNumeric) geo;
 				if (num.isSlider()) {
-					needsAdjusted(num);
-					Log.debug("[AS] a slider: " + num.getLabelSimple());
+					if (num.isSliderHorizontal()) {
+						hSliders.add(num);
+					} else {
+						vSliders.add(num);
+					}
+
+					view.ensureGeoOnScreen(num);
+
 				}
+			}
+		}
+		checkOvelappingHSliders();
+	}
+
+	private void checkOvelappingHSliders() {
+		Collections.sort(hSliders, new HSliderComparator());
+		int diff = HSLIDER_OVERLAP_THRESOLD;
+		for (int idx = hSliders.size() - 1; idx > 0; idx--) {
+			GeoNumeric num1 = hSliders.get(idx - 1);
+			GeoNumeric num2 = hSliders.get(idx);
+			Log.debug("[AS] :" + num1 + " - " + num2);
+			// double x1 = num1.getSliderX();
+			// double xEnd1 = num1.getSliderX() + num1.getSliderWidth();
+			double y1 = num1.getSliderY();
+			double x2 = num2.getSliderX();
+			// double xEnd2 = num2.getSliderX() + num2.getSliderWidth();
+			double y2 = num2.getSliderY();
+			if (y2 - y1 < HSLIDER_OVERLAP_THRESOLD) {
+				Log.debug("[AS] adjusting " + num2 + " to (" + x2 + ", "
+						+ (y2 + diff) + ")");
+				num2.setSliderLocation(x2, y2 + diff, true);
+				diff += HSLIDER_OVERLAP_THRESOLD;
 			}
 		}
 	}
 
-	protected boolean needsAdjusted(GeoElement geo) {
+	/**
+	 * @return if the original screen was bigger so adjusting is needed.
+	 */
+	protected boolean needsAdjusting() {
 		App app = view.getApplication();
 		int fileWidth = app.getSettings()
 				.getEuclidian(view.getEuclidianViewNo()).getFileWidth();
@@ -53,7 +122,6 @@ public class AdjustScreen {
 			return false;
 		}
 
-		view.ensureGeoOnScreen(geo);
 
 		return true;
 	}
