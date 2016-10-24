9e6379ae9f30d5ec338082d35392e4673b7555b2
Zbyněk Konečný <zbynek@geogebra.org>
2016-10-24 08:51:51 +0000

    Switch to GWT 2.8
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@49828 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- retex/renderer-web/src/main/resources/com/himamis/retex/renderer/gwt-patch/com/himamis/retex/renderer/share/Atom.java
+++ retex/renderer-web/src/main/resources/com/himamis/retex/renderer/gwt-patch/com/himamis/retex/renderer/share/Atom.java
@@ -126,8 +126,8 @@ public abstract class Atom implements Cloneable {
 		var r = {};
 
 		// prevents to use same hash code
-		@com.google.gwt.core.client.impl.Impl::getHashCode(Ljava/lang/Object;)(r);
-		//GWT 2.8 @javaemul.internal.HashCodes::getObjectIdentityHashCode(*)(r);
+		//GWT <=2.7: @com.google.gwt.core.client.impl.Impl::getHashCode(Ljava/lang/Object;)(r);
+		@javaemul.internal.HashCodes::getObjectIdentityHashCode(*)(r);
 		var o = this;
 		for ( var i in o) {
 			if (!(i in r)) {
