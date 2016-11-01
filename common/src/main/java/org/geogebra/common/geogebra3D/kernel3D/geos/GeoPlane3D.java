db89a56e469fa656d82089caa691f94f5172a7e4
Michael Borcherds <michael@geogebra.org>
2016-11-01 14:28:30 +0000

    remove redundant interface
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@49911 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/geogebra3D/kernel3D/geos/GeoPlane3D.java
+++ common/src/main/java/org/geogebra/common/geogebra3D/kernel3D/geos/GeoPlane3D.java
@@ -5,7 +5,6 @@ import org.geogebra.common.geogebra3D.euclidianForPlane.EuclidianViewForPlaneCom
 import org.geogebra.common.geogebra3D.kernel3D.transform.MirrorableAtPlane;
 import org.geogebra.common.kernel.Construction;
 import org.geogebra.common.kernel.Kernel;
-import org.geogebra.common.kernel.Region;
 import org.geogebra.common.kernel.RegionParameters;
 import org.geogebra.common.kernel.StringTemplate;
 import org.geogebra.common.kernel.Matrix.CoordMatrix;
@@ -35,7 +34,7 @@ import org.geogebra.common.plugin.GeoClass;
 public class GeoPlane3D extends GeoElement3D implements Functional2Var,
 		ViewCreator, GeoCoords4D, GeoPlaneND, Translateable, Traceable,
 		RotateableND, MirrorableAtPlane, Transformable, Dilateable,
-		EquationValue, Region {
+		EquationValue {
 
 	/** default labels */
 	private static final char[] Labels = { 'p', 'q', 'r' };
