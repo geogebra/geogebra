c198415cb0186ce0407a4c14e4b26e9e45bb6647
Michael Borcherds <michael@geogebra.org>
2016-11-07 09:30:59 +0000

    formatting
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@50011 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/kernel/optimization/ExtremumFinder.java
+++ common/src/main/java/org/geogebra/common/kernel/optimization/ExtremumFinder.java
@@ -58,158 +58,127 @@ package org.geogebra.common.kernel.optimization;
 import org.geogebra.common.kernel.roots.RealRootFunction;
 
 /**
-*
-*<p>
-*This class was translated by a statistician from the FORTRAN 
-*version of fmin.  It is NOT an official translation.  When 
-*public domain Java optimization routines become available 
-*from professional numerical analysts, then <b>THE CODE PRODUCED
-*BY THE NUMERICAL ANALYSTS SHOULD BE USED</b>.
-*
-*<p>
-*Meanwhile, if you have suggestions for improving this
-*code, please contact Steve Verrill at steve@www1.fpl.fs.fed.us.
-*
-*@author Steve Verrill
-*@version .5 --- March 24, 1998
-* 
-*/
+ *
+ * <p>
+ * This class was translated by a statistician from the FORTRAN version of fmin.
+ * It is NOT an official translation. When public domain Java optimization
+ * routines become available from professional numerical analysts, then <b>THE
+ * CODE PRODUCED BY THE NUMERICAL ANALYSTS SHOULD BE USED</b>.
+ *
+ * <p>
+ * Meanwhile, if you have suggestions for improving this code, please contact
+ * Steve Verrill at steve@www1.fpl.fs.fed.us.
+ *
+ * @author Steve Verrill
+ * @version .5 --- March 24, 1998
+ * 
+ */
 
 public class ExtremumFinder {
 
 	private int MAX_ITERATIONS = 100;
 
-	public ExtremumFinder() {}
+	public ExtremumFinder() {
+	}
 
 	public void setMaxIterations(int iterations) {
 		MAX_ITERATIONS = iterations;
 	}
 
 	/**
-	*
-	*<p>
-	*This method performs a 1-dimensional maximization.
-	*It implements Brent's method which combines a golden-section
-	*search and parabolic interpolation.  
-	*
-	*@param  a         Left endpoint of initial interval
-	*@param  b         Right endpoint of initial interval
-	*@param  maxfunction  an object that defines a method, evaluate,
-	*                  to maximize.  
-	*@param  tol       Desired length of the interval in which
-	*                  the minimum will be determined to lie
-	*                  (This should be greater than, roughly, 3.0e-8.)
-	*
-	*/
-	final public double findMaximum(
-		double a,
-		double b,
-		RealRootFunction maxfunction,
-		double tol) {
-		NegativeRealRootFunction minfunc =
-			new NegativeRealRootFunction(maxfunction);
+	 *
+	 * <p>
+	 * This method performs a 1-dimensional maximization. It implements Brent's
+	 * method which combines a golden-section search and parabolic
+	 * interpolation.
+	 *
+	 * @param a
+	 *            Left endpoint of initial interval
+	 * @param b
+	 *            Right endpoint of initial interval
+	 * @param maxfunction
+	 *            an object that defines a method, evaluate, to maximize.
+	 * @param tol
+	 *            Desired length of the interval in which the minimum will be
+	 *            determined to lie (This should be greater than, roughly,
+	 *            3.0e-8.)
+	 *
+	 */
+	final public double findMaximum(double a, double b,
+			RealRootFunction maxfunction, double tol) {
+		NegativeRealRootFunction minfunc = new NegativeRealRootFunction(
+				maxfunction);
 		return findMinimum(a, b, minfunc, tol);
 	}
 
 	/**
-	*
-	*<p>
-	*This method performs a 1-dimensional minimization.
-	*It implements Brent's method which combines a golden-section
-	*search and parabolic interpolation.  The introductory comments from
-	*the FORTRAN version are provided below.
-	*
-	*This method is a translation from FORTRAN to Java of the Netlib 
-	*function fmin.  In the Netlib listing no author is given.
-	*
-	*Translated by Steve Verrill, March 24, 1998.
-	*
-	*@param  a         Left endpoint of initial interval
-	*@param  b         Right endpoint of initial interval
-	*@param  minclass  A class that defines a method, f_to_minimize,
-	*                  to minimize.  The class must implement
-	*                  the Fmin_methods interface (see the definition
-	*                  in Fmin_methods.java).  See FminTest.java
-	*                  for an example of such a class.
-	*                  f_to_minimize must have one
-	*                  double valued argument.
-	*@param  tol       Desired length of the interval in which
-	*                  the minimum will be determined to lie
-	*                  (This should be greater than, roughly, 3.0e-8.)
-	*
-	*/
-	final public double findMinimum(
-		double a,
-		double b,
-		RealRootFunction minclass,
-		double tol) {
+	 *
+	 * <p>
+	 * This method performs a 1-dimensional minimization. It implements Brent's
+	 * method which combines a golden-section search and parabolic
+	 * interpolation. The introductory comments from the FORTRAN version are
+	 * provided below.
+	 *
+	 * This method is a translation from FORTRAN to Java of the Netlib function
+	 * fmin. In the Netlib listing no author is given.
+	 *
+	 * Translated by Steve Verrill, March 24, 1998.
+	 *
+	 * @param a
+	 *            Left endpoint of initial interval
+	 * @param b
+	 *            Right endpoint of initial interval
+	 * @param minclass
+	 *            A class that defines a method, f_to_minimize, to minimize. The
+	 *            class must implement the Fmin_methods interface (see the
+	 *            definition in Fmin_methods.java). See FminTest.java for an
+	 *            example of such a class. f_to_minimize must have one double
+	 *            valued argument.
+	 * @param tol
+	 *            Desired length of the interval in which the minimum will be
+	 *            determined to lie (This should be greater than, roughly,
+	 *            3.0e-8.)
+	 *
+	 */
+	final public double findMinimum(double a, double b,
+			RealRootFunction minclass, double tol) {
 
 		/*
-		
-		Here is a copy of the Netlib documentation:
-		
-		c
-		c      An approximation x to the point where f attains a minimum on
-		c  the interval (ax,bx) is determined.
-		c
-		c  input..
-		c
-		c  ax    left endpoint of initial interval
-		c  bx    right endpoint of initial interval
-		c  f     function subprogram which evaluates f(x) for any x
-		c        in the interval (ax,bx)
-		c  tol   desired length of the interval of uncertainty of the final
-		c        result (.ge.0.)
-		c
-		c  output..
-		c
-		c  fmin  abcissa approximating the point where  f  attains a
-		c        minimum
-		c
-		c     The method used is a combination of golden section search and
-		c  successive parabolic interpolation.  Convergence is never much slower
-		c  than that for a Fibonacci search.  If f has a continuous second
-		c  derivative which is positive at the minimum (which is not at ax or
-		c  bx), then convergence is superlinear, and usually of the order of
-		c  about 1.324.
-		c     The function f is never evaluated at two points closer together
-		c  than eps*abs(fmin)+(tol/3), where eps is approximately the square
-		c  root of the relative machine precision.  If f is a unimodal
-		c  function and the computed values of f are always unimodal when
-		c  separated by at least eps*abs(x)+(tol/3), then fmin approximates
-		c  the abcissa of the global minimum of f on the interval (ax,bx) with
-		c  an error less than 3*eps*abs(fmin)+tol.  If f is not unimodal,
-		c  then fmin may approximate a local, but perhaps non-global, minimum to
-		c  the same accuracy.
-		c      This function subprogram is a slightly modified version of the
-		c  Algol 60 procedure localmin given in Richard Brent, Algorithms For
-		c  Minimization Without Derivatives, Prentice-Hall, Inc. (1973).
-		c
-		
-		*/
+		 * 
+		 * Here is a copy of the Netlib documentation:
+		 * 
+		 * c c An approximation x to the point where f attains a minimum on c
+		 * the interval (ax,bx) is determined. c c input.. c c ax left endpoint
+		 * of initial interval c bx right endpoint of initial interval c f
+		 * function subprogram which evaluates f(x) for any x c in the interval
+		 * (ax,bx) c tol desired length of the interval of uncertainty of the
+		 * final c result (.ge.0.) c c output.. c c fmin abcissa approximating
+		 * the point where f attains a c minimum c c The method used is a
+		 * combination of golden section search and c successive parabolic
+		 * interpolation. Convergence is never much slower c than that for a
+		 * Fibonacci search. If f has a continuous second c derivative which is
+		 * positive at the minimum (which is not at ax or c bx), then
+		 * convergence is superlinear, and usually of the order of c about
+		 * 1.324. c The function f is never evaluated at two points closer
+		 * together c than eps*abs(fmin)+(tol/3), where eps is approximately the
+		 * square c root of the relative machine precision. If f is a unimodal c
+		 * function and the computed values of f are always unimodal when c
+		 * separated by at least eps*abs(x)+(tol/3), then fmin approximates c
+		 * the abcissa of the global minimum of f on the interval (ax,bx) with c
+		 * an error less than 3*eps*abs(fmin)+tol. If f is not unimodal, c then
+		 * fmin may approximate a local, but perhaps non-global, minimum to c
+		 * the same accuracy. c This function subprogram is a slightly modified
+		 * version of the c Algol 60 procedure localmin given in Richard Brent,
+		 * Algorithms For c Minimization Without Derivatives, Prentice-Hall,
+		 * Inc. (1973). c
+		 * 
+		 */
 
-		double c,
-			d,
-			e,
-			eps,
-			xm,
-			p,
-			q,
-			r,
-			tol1,
-			t2,
-			u,
-			v,
-			w,
-			fu,
-			fv,
-			fw,
-			fx,
-			x,
-			tol3;
+		double c, d, e, eps, xm, p, q, r, tol1, t2, u, v, w, fu, fv, fw, fx, x,
+				tol3;
 
-		// start value		
-		c = .5 * (3.0 - Math.sqrt(5.0));		
+		// start value
+		c = .5 * (3.0 - Math.sqrt(5.0));
 		d = 0.0;
 
 		// 1.1102e-16 is machine precision
@@ -218,7 +187,6 @@ public class ExtremumFinder {
 		tol1 = eps + 1.0;
 		eps = Math.sqrt(eps);
 
-		
 		v = a + c * (b - a);
 		w = v;
 		x = v;
@@ -274,9 +242,8 @@ public class ExtremumFinder {
 				// brace below corresponds to statement 50
 			}
 
-			if ((Math.abs(p) < Math.abs(.5 * q * r))
-				&& (p > q * (a - x))
-				&& (p < q * (b - x))) {
+			if ((Math.abs(p) < Math.abs(.5 * q * r)) && (p > q * (a - x))
+					&& (p < q * (b - x))) {
 
 				// a parabolic interpolation step
 
@@ -419,4 +386,4 @@ public class ExtremumFinder {
 
 }
 
-//use -f for maximum
+// use -f for maximum
