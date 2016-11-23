251c19b0ec7393c89d6789ef381b206a7111b08a
Zbyněk Konečný <zbynek@geogebra.org>
2016-11-23 14:37:23 +0000

    Double regroup for derivative
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@50432 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
+++ common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
@@ -170,19 +170,20 @@ public class Ggb2giac {
 		// when(count_eq(x,lname(%0))==0,lname(%0)[0],x)
 
 		p("Derivative.1",
-				"[[ggbderivarg0:=%0], regroup(diff(ggbderivarg0, "
+				"[[ggbderivarg0:=%0], regroup(regroup(diff(ggbderivarg0, "
 						+
 						// check for constant
 						"when(length(lname(ggbderivarg0))==0,x,"
-						+ "when(count_eq(x,lname(ggbderivarg0))==0,lname(ggbderivarg0)[0],x))))][1]");
+						+ "when(count_eq(x,lname(ggbderivarg0))==0,lname(ggbderivarg0)[0],x)))))][1]");
 		p("Derivative.2",
 				"[[[ggbderiv2arg0:=%0],[ggbderiv2arg1:=%1]],"
 						+ "when(type(ggbderiv2arg1)==DOM_INT,"
-						+ "regroup(diff(ggbderiv2arg0,when(count_eq(x,lname(ggbderiv2arg0))==0,lname(%0)[0],x),ggbderiv2arg1))"
-						+ "," + "regroup(diff(ggbderiv2arg0,ggbderiv2arg1))"
+						+ "regroup(regroup(diff(ggbderiv2arg0,when(count_eq(x,lname(ggbderiv2arg0))==0,lname(%0)[0],x),ggbderiv2arg1)))"
+						+ ","
+						+ "regroup(regroup(diff(ggbderiv2arg0,ggbderiv2arg1)))"
 						+ ")][1]");
 
-		p("Derivative.3", "regroup(diff(%0,%1,%2))");
+		p("Derivative.3", "regroup(regroup(diff(%0,%1,%2)))");
 
 		// det_minor for symbolic, see GGB-830
 		p("Determinant.1", "when(size(lname(%0))==0,det(%0),det_minor(%0))");
