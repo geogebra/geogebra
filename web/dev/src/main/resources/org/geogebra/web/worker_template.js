fc44b931814deb02623654bc6039b73519521e17
Ancsin Gábor <ustakraharez@gmail.com>
2016-12-13 13:29:48 +0000

    MAT-1547 done. ServiceWorker simplified.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@50858 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/dev/src/main/resources/org/geogebra/web/worker_template.js
+++ web/dev/src/main/resources/org/geogebra/web/worker_template.js
@@ -10,17 +10,16 @@ var urlsToCache = {"urls_to_cache":[%URLS% "https:\/\/ajax.googleapis.com\/ajax\
 
 self.addEventListener('install', function(event) {
     "use strict";
+    console.log("install");
     event.waitUntil(
         caches
             .open(urlsToCache.unique_id)
             .then(function(cache) {
-                console.log('[install] Caches opened, adding GeoGebraWeb js files to cache' +
-                    'to cache');
+                console.log('[install] Caches opened, adding GeoGebraWeb js files to cache');
                 return cache.addAll(urlsToCache.urls_to_cache);
             })
             .then(function() {
                 console.log('[install] All required resources have been cached');
-                return self.skipWaiting();
             })
     );
 });
@@ -30,45 +29,12 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
             .then(function(response) {
-                // Cache hit - return response
                 if (response) {
-                    console.log("from cache: " + response.url);
                     return response;
                 }
-
-                // IMPORTANT: Clone the request. A request is a stream and
-                // can only be consumed once. Since we are consuming this
-                // once by cache and once by the browser for fetch, we need
-                // to clone the response
-                    var fetchRequest = event.request.clone();
-
-                    return fetch(fetchRequest).then(
-                        function(response) {
-                            // Check if we received a valid response
-                            if(!response || response.status !== 200 || response.type !== 'basic') {
-                                return response;
-                            }
-
-                            // IMPORTANT: Clone the response. A response is a stream
-                            // and because we want the browser to consume the response
-                            // as well as the cache consuming the response, we need
-                            // to clone it so we have 2 stream.
-                            if (urlsToCache.urls_to_cache.indexOf(response.url) > -1) {
-                                var responseToCache = response.clone();
-
-                                caches.open(urlsToCache.unique_id)
-                                    .then(function (cache) {
-                                        cache.put(event.request, responseToCache);
-                                        console.log("put to cache: " + responseToCache.url);
-                                    });
-                            }
-
-                            return response;
-                        }
-                ).catch(function(reason) {
-                        console.log(reason);
-                        return response;
-                    });
+                return fetch(event.request);
+            }).catch(function(reason) {
+                    console.log(reason);
             })
     );
 });
@@ -90,6 +56,5 @@ self.addEventListener('activate', function(event) {
                     })
                 );
             })
-            .then(self.clients.claim())
     );
 });
