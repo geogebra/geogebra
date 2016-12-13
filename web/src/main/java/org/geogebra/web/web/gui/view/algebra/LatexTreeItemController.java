dd77b7cd8f6d68983e389308b89bab299ef64bae
Laszlo Gal <laszlo@geogebra.org>
2016-12-13 15:46:04 +0000

    GGB-1417 styleEditor() for inputTreeItem too.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@50865 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/web/gui/view/algebra/LatexTreeItemController.java
+++ web/src/main/java/org/geogebra/web/web/gui/view/algebra/LatexTreeItemController.java
@@ -52,6 +52,7 @@ public class LatexTreeItemController extends RadioTreeItemController
 	public void onEnter(final boolean keepFocus) {
 		if (app.has(Feature.AV_SINGLE_TAP_EDIT) && item.isInputTreeItem()
 				&& item.isEmpty()) {
+			item.styleEditor();
 			item.addDummyLabel();
 			return;
 		}
