e34cf14da797d10418b892c1476a52392b59cb04
Bernard Parisse <bernard.parisse@ujf-grenoble.fr>
2017-01-16 19:45:45 +0000

    [giac] reduce max algebraic order size to 4, should fix normal(regroup(surd(((x)+(1))/((x)^(6)),6)))
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@51376 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- giac/src/giac/cpp/global.cc
+++ giac/src/giac/cpp/global.cc
@@ -1666,7 +1666,11 @@ extern "C" void Sleep(unsigned int miliSecond);
   int INT_KARAMUL_SIZE=300;
   int FFTMUL_SIZE=100; 
   int FFTMUL_INT_MAXBITS=1024;
+#ifdef GIAC_GGB
+  int MAX_ALG_EXT_ORDER_SIZE = 4;
+#else
   int MAX_ALG_EXT_ORDER_SIZE = 6;
+#endif
 #if defined EMCC || defined NO_TEMPLATE_MULTGCD
   int MAX_COMMON_ALG_EXT_ORDER_SIZE = 16;
 #else
