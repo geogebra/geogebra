ad3515e00e257d00438db54127c4c3f13ef39048
Michael Borcherds <michael@geogebra.org>
2017-02-05 10:43:53 +0000

    [cas] try to improve ScientificText[] in web
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@51794 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
+++ common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
@@ -996,11 +996,11 @@ public class Ggb2giac {
 		// default 15, like Input Bar version
 		p("ScientificText.1",
 				" [[[ggbstinput:=%0],[ggbstans:=?],[ggbstabsans:=abs(ggbstinput)],[ggbstpower:=floor(log10(ggbstinput))],"
-						+ "[ggbstans:=evalf(ggbstinput / 10^ggbstpower, 15) + \" 10^ \" + ggbstpower]],when(ggbstinput==0,0,ggbstans)][1]");
+						+ "[ggbstans:=evalf(ggbstinput / 10^ggbstpower, 15) + \" * 10^ \" + ggbstpower]],when(ggbstinput==0,0,ggbstans)][1]");
 
 		p("ScientificText.2",
 				" [[[ggbstinput:=%0],[ggbstans:=?],[ggbstabsans:=abs(ggbstinput)],[ggbstpower:=floor(log10(ggbstinput))],"
-						+ "[ggbstans:=evalf(ggbstinput / 10^ggbstpower, %1) + \" 10^ \" + ggbstpower]],when(ggbstinput==0,0,ggbstans)][1]");
+						+ "[ggbstans:=evalf(ggbstinput / 10^ggbstpower, %1) + \" * 10^ \" + ggbstpower]],when(ggbstinput==0,0,ggbstans)][1]");
 
 		p("SD.1", "normal(stddev(%0))");
 
