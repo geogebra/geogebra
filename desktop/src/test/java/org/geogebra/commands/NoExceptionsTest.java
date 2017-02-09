2f11edf3c91e79d03b4c3f812b734ddfb0608d05
Zbyněk Konečný <zbynek@geogebra.org>
2017-02-09 12:19:00 +0000

    Testcase fix for surfaces
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@51918 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/test/java/org/geogebra/commands/NoExceptionsTest.java
+++ desktop/src/test/java/org/geogebra/commands/NoExceptionsTest.java
@@ -2772,6 +2772,11 @@ public class NoExceptionsTest {
 	}
 
 	@Test
+	public void implicitSurface() {
+		t("x^3+y^3+z^3=1");
+	}
+
+	@Test
 	public void localizationTest() {
 		Assert.assertNull(app.getLocalization().getReverseCommand("x"));
 	}
