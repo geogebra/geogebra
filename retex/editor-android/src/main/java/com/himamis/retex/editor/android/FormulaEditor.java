4e4334356a821eaedb6ecc21333e32e6238bd7a5
Michael Borcherds <michael@geogebra.org>
2017-02-13 11:56:48 +0000

    add getter/setter for FactoryProvider.INSTANCE
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@52012 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- retex/editor-android/src/main/java/com/himamis/retex/editor/android/FormulaEditor.java
+++ retex/editor-android/src/main/java/com/himamis/retex/editor/android/FormulaEditor.java
@@ -130,8 +130,8 @@ public class FormulaEditor extends View implements MathField {
     }
 
     private void initFactoryProvider() {
-        if (FactoryProvider.INSTANCE == null) {
-            FactoryProvider.INSTANCE = new FactoryProviderAndroid(getContext().getAssets());
+        if (FactoryProvider.getInstance() == null) {
+            FactoryProvider.setInstance(new FactoryProviderAndroid(getContext().getAssets()));
         }
     }
 
