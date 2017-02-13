b1ab56f3bccc0d4248434ce062b0d49ac9b3440e
Csilla Solyom-Gecse <solyom_csilla@yahoo.com>
2017-02-13 10:42:10 +0000

    MOW-158 enum for corner handlers
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@52001 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common/src/main/java/org/geogebra/common/euclidian/EuclidianBoundingBoxHandler.java
@@ -0,0 +1,32 @@
+package org.geogebra.common.euclidian;
+
+/**
+ * handlers type for bounding box
+ * 
+ * @author csilla
+ *
+ */
+public enum EuclidianBoundingBoxHandler {
+
+	/**
+	 * top right corner handler of bounding box
+	 */
+	TOP_RIGHT,
+	/**
+	 * bottom right corner handler of bounding box
+	 */
+	BOTTOM_RIGHT,
+	/**
+	 * bottom left corner handler of bounding box
+	 */
+	BOTTOM_LEFT,
+	/**
+	 * top left corner handler of bounding box
+	 */
+	TOP_LEFT,
+	/**
+	 * undefined handler
+	 */
+	UNDEFINED
+
+}
