geogebra/geogebra

commit ca7f42892d8edd8637a2cbd3ddb1a677c09d7bb3

MOW-133 sqrt and trig improvements
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@51895 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/main/java/org/geogebra/web/editor/MathFieldProcessing.java
@@ -2,6 +2,7 @@ package org.geogebra.web.editor;
 
 import org.geogebra.common.main.KeyboardLocale;
 import org.geogebra.common.util.Unicode;
+import org.geogebra.common.util.debug.Log;
 import org.geogebra.web.keyboard.KeyboardConstants;
 import org.geogebra.web.keyboard.KeyboardListener;
 
@@ -50,8 +51,16 @@ public class MathFieldProcessing implements KeyboardListener {
 		}
 		else if (text.equals(Unicode.DIVIDE)) {
 			mf.getKeyListener().onKeyTyped(new KeyEvent(0, 0, '/'));
+		} else if (text.charAt(0) == Unicode.SQUARE_ROOT) {
+			mf.insertFunction("sqrt");
 		} else {
-			for (int i = 0; i < text.length(); i++) {
+			int length = text.length();
+			if (length > 1 && Character.isLetter(text.charAt(0))) {
+				mf.insertFunction(text);
+				return;
+			}
+			Log.debug("TYPED" + text);
+			for (int i = 0; i < length; i++) {
 				mf.getKeyListener()
 						.onKeyTyped(new KeyEvent(0, 0, text.charAt(i)));
 			}

retex/editor-base/src/main/java/com/himamis/retex/editor/share/controller/InputController.java
@@ -133,7 +133,6 @@ public class InputController {
      * @param name function
      */
     public void newFunction(EditorState editorState, String name, int initial) {
-		System.out.println(name);
         MathSequence currentField = editorState.getCurrentField();
         int currentOffset = editorState.getCurrentOffset();
         // add extra braces for sqrt, nthroot and fraction
@@ -166,7 +165,7 @@ public class InputController {
             MetaFunction meta = metaModel.getFunction(name);
             function = new MathFunction(meta);
         }
-
+		mathField.debug("INSERT FN " + name + editorState.getSelectionEnd());
         // add sequences
         for (int i = 0; i < function.size(); i++) {
             MathSequence field = new MathSequence();
@@ -175,9 +174,7 @@ public class InputController {
 
         // pass characters for fraction and factorial only
 		if ("frac".equals(name)) {
-			System.out.println("cut frac?");
 			if (editorState.getSelectionEnd() != null) {
-				System.out.println("cut frac");
 				ArrayList<MathComponent> removed = cut(currentField,
 						currentOffset, -1,
 						editorState,
@@ -191,6 +188,17 @@ public class InputController {
 				return;
 			}
 			ArgumentHelper.passArgument(editorState, function);
+		} else {
+			if (editorState.getSelectionEnd() != null) {
+				ArrayList<MathComponent> removed = cut(currentField,
+						currentOffset, -1, editorState, function, true);
+				MathSequence field = new MathSequence();
+				function.setArgument(0, field);
+				insertReverse(field, -1, removed);
+				editorState.resetSelection();
+				editorState.incCurrentOffset();
+				return;
+			}
         }
         currentOffset = editorState.getCurrentOffset();
         currentField.addArgument(currentOffset, function);

retex/editor-base/src/main/java/com/himamis/retex/editor/share/editor/MathFieldInternal.java
@@ -528,4 +528,11 @@ public class MathFieldInternal implements KeyListener, FocusListener, ClickListe
 		}
 	}
 
+	public void insertFunction(String text) {
+		inputController.newFunction(editorState, text);
+		if (listener != null) {
+			listener.onKeyTyped();
+		}
+	}
+
 }

retex/editor-web/src/main/java/com/himamis/retex/editor/web/MathFieldW.java
@@ -667,4 +667,9 @@ public class MathFieldW implements MathField, IsWidget {
 
 	}
 
+	public void insertFunction(String text) {
+		mathFieldInternal.insertFunction(text);
+
+	}
+
 }

commit 2c7c53fbe1a3b3fce38b54ec2a6d5d3df634c4e3

Attempt to fix autocomplete, broken in r51895
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@52111 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/main/java/org/geogebra/web/editor/MathFieldProcessing.java
@@ -8,10 +8,18 @@ import org.geogebra.web.keyboard.KeyboardListener;
 import com.himamis.retex.editor.share.event.KeyEvent;
 import com.himamis.retex.editor.web.MathFieldW;
 
+/**
+ * Virtual keyboard bindings for ReTeX
+ *
+ */
 public class MathFieldProcessing implements KeyboardListener {
 
 	private MathFieldW mf;
 
+	/**
+	 * @param mf
+	 *            math input field
+	 */
 	public MathFieldProcessing(MathFieldW mf) {
 		this.mf = mf;
 	}
@@ -60,7 +68,8 @@ public class MathFieldProcessing implements KeyboardListener {
 			mf.insertFunction("log10");
 		} else {
 			int length = text.length();
-			if (length > 1 && Character.isLetter(text.charAt(0))) {
+			if (length > 1 && Character.isLetter(text.charAt(0))
+					&& !text.contains("[")) {
 				mf.insertFunction(text);
 				return;
 			}
@@ -96,6 +105,10 @@ public class MathFieldProcessing implements KeyboardListener {
 
 	}
 
+	/**
+	 * @param text
+	 *            text to be inserted
+	 */
 	public void autocomplete(String text) {
 		this.mf.deleteCurrentWord();
 		insertString(text);