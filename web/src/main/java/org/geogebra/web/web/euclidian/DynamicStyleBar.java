e80364b4a3f5ce76f7399a8bba8dd0483bf85eb2
Judit Elias <eliasjudit@gmail.com>
2017-02-17 13:16:41 +0000

    MOW-169 top fixed too
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@52143 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/web/euclidian/DynamicStyleBar.java
+++ web/src/main/java/org/geogebra/web/web/euclidian/DynamicStyleBar.java
@@ -15,24 +15,27 @@ public class DynamicStyleBar extends EuclidianStyleBarW {
 		((GeoGebraFrameW)((AppW) ev.getApplication()).getAppletFrame()).add(this);
 	}
 
-
 	/**
 	 * Sets the position of dynamic style bar. newPos position of right top
 	 * corner of bounding box of drawable
 	 */
 	@Override
 	public void setPosition(double[] newPos) {
+		boolean oldVisible = this.isVisible();
+		this.setVisible(true);
 
 		// Calculates the x param. of distance between the start of dynamic
 		// stylebar and the three dot button.
-		boolean oldVisible = this.isVisible();
-		this.setVisible(true);
 		this.getElement().getStyle().setTop(-10000, Unit.PX);
 		int move = this.getViewButton().getAbsoluteLeft()
 				- this.getAbsoluteLeft();
+
+		int height = this.getOffsetHeight();
 		this.setVisible(oldVisible);
 
 		newPos[0] -= move;
+		newPos[1] -= height + 5;
+
 		Log.debug("newpos: " + newPos[0] + " " + newPos[1]);
 
 		this.getElement().getStyle().setLeft(newPos[0], Unit.PX);
