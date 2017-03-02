c48503cd0fe451385e618f9a15065bad88735c4c
Zbyněk Konečný <zbynek@geogebra.org>
2017-03-02 15:20:09 +0000

    Removing unwanted comment
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@52478 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/euclidian/EuclidianController.java
+++ common/src/main/java/org/geogebra/common/euclidian/EuclidianController.java
@@ -6476,7 +6476,6 @@ public abstract class EuclidianController {
 		}
 	}
 
-	// clean testAll publishWebOnTubeBeta
 	protected void moveDependent(boolean repaint) {
 
 		translationVec.setX(xRW - getStartPointX());
