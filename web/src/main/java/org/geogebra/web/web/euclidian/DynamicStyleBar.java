0c636f75ea8cdec76cb13292f92f3a10e6ac4ec0
Judit Elias <eliasjudit@gmail.com>
2017-03-03 08:17:09 +0000

    MOW-169 Dynamic stylebar left position fix: won't go out from the EV.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@52488 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/web/euclidian/DynamicStyleBar.java
+++ web/src/main/java/org/geogebra/web/web/euclidian/DynamicStyleBar.java
@@ -49,12 +49,17 @@ public class DynamicStyleBar extends EuclidianStyleBarW {
 			if (top < 0){
 				top = gRectangle2D.getMaxY() + 5;
 			}
+			
 		} else { //line has no bounding box
 			left = gRectangle2D.getMaxX() - height / 2.0;
 			if(left + this.getOffsetWidth() > app.getActiveEuclidianView().getWidth()){
 				left = app.getActiveEuclidianView().getWidth() - this.getOffsetWidth();
 			}
 			top = gRectangle2D.getMinY();
+		}
+		
+		if(left<0){
+			left = 0;
 		}
 			
 		this.getElement().getStyle().setLeft(left, Unit.PX);
