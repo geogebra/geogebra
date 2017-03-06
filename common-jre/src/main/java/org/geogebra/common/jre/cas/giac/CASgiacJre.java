9de58cf9b5825eea6751178dd0359e4868c94f6c
Balazs Bencze <megette_a@yahoo.com>
2017-03-06 16:03:27 +0000

    Fix missing constructor
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@52557 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common-jre/src/main/java/org/geogebra/common/jre/cas/giac/CASgiacJre.java
+++ common-jre/src/main/java/org/geogebra/common/jre/cas/giac/CASgiacJre.java
@@ -3,6 +3,7 @@ package org.geogebra.common.jre.cas.giac;
 import org.geogebra.common.cas.CASparser;
 import org.geogebra.common.cas.giac.CASgiacB;
 import org.geogebra.common.cas.giac.binding.CASGiacBinding;
+import org.geogebra.common.kernel.Kernel;
 
 /**
  * Giac for Desktop and Android
@@ -11,6 +12,10 @@ public abstract class CASgiacJre extends CASgiacB {
 
     public CASgiacJre(CASparser casParser) {
         super(casParser);
+    }
+
+    public CASgiacJre(CASparser casParser, Kernel kernel) {
+        this(casParser);
     }
 
     @Override
