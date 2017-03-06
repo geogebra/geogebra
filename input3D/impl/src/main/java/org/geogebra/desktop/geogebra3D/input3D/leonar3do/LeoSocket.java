19e8f5cacc9e24f3117c3126eee31adddce4e469
Zbyněk Konečný <zbynek@geogebra.org>
2017-03-06 08:18:43 +0000

    Fixed warnings
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@52528 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- input3D/impl/src/main/java/org/geogebra/desktop/geogebra3D/input3D/leonar3do/LeoSocket.java
+++ input3D/impl/src/main/java/org/geogebra/desktop/geogebra3D/input3D/leonar3do/LeoSocket.java
@@ -26,20 +26,72 @@ public class LeoSocket {
 	static{
 		class NoLogging implements Logger {
 			@Override public String getName() { return "no"; }
-			@Override public void warn(String msg, Object... args) { }
-			@Override public void warn(Throwable thrown) { }
-			@Override public void warn(String msg, Throwable thrown) { }
-			@Override public void info(String msg, Object... args) { }
-			@Override public void info(Throwable thrown) { }
-			@Override public void info(String msg, Throwable thrown) { }
+
+			@Override
+			public void warn(String msg, Object... args) {
+				/* no logging */
+			}
+
+			@Override
+			public void warn(Throwable thrown) {
+				/* no logging */
+			}
+
+			@Override
+			public void warn(String msg, Throwable thrown) {
+				/* no logging */
+			}
+
+			@Override
+			public void info(String msg, Object... args) {
+				/* no logging */
+			}
+
+			@Override
+			public void info(Throwable thrown) {
+				/* no logging */
+			}
+
+			@Override
+			public void info(String msg, Throwable thrown) {
+				/* no logging */
+			}
 			@Override public boolean isDebugEnabled() { return false; }
-			@Override public void setDebugEnabled(boolean enabled) { }
-			@Override public void debug(String msg, Object... args) { }
-			@Override public void debug(Throwable thrown) { }
-			@Override public void debug(String msg, Throwable thrown) { }
-			@Override public Logger getLogger(String name) { return this; }
-			@Override public void ignore(Throwable ignored) { }
-			@Override public void debug(String msg, long l) { }
+
+			@Override
+			public void setDebugEnabled(boolean enabled) {
+				/* no logging */
+			}
+
+			@Override
+			public void debug(String msg, Object... args) {
+				/* no logging */
+			}
+
+			@Override
+			public void debug(Throwable thrown) {
+				/* no logging */
+			}
+
+			@Override
+			public void debug(String msg, Throwable thrown) {
+				/* no logging */
+			}
+
+			@Override
+			public Logger getLogger(String name) {
+				return this;
+			}
+
+			@Override
+			public void ignore(Throwable ignored) {
+				/* no logging */
+			}
+
+			@Override
+			public void debug(String msg, long l) {
+				/* no logging */
+			}
 		}
 
 		org.eclipse.jetty.util.log.Log.setLog(new NoLogging());
