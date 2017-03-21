d44a01a228b2110092d5177fe83ca304a87e6d65
Laszlo Gal <laszlo@geogebra.org>
2017-03-21 08:38:26 +0000

    MOW-183 fill pattern fix.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@52973 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/web/gui/color/MOWColorButton.java
+++ web/src/main/java/org/geogebra/web/web/gui/color/MOWColorButton.java
@@ -22,8 +22,8 @@ import com.google.gwt.event.dom.client.ClickHandler;
 public class MOWColorButton extends ColorPopupMenuButton {
 	private static final int FILL_TYPES_COUNT = 5;
 	private SelectionTableW fillTable;
-	private FillType fillTypes[] = { FillType.STANDARD, FillType.BRICK,
-			FillType.CHESSBOARD, FillType.DOTTED, FillType.HONEYCOMB };
+	private FillType fillTypes[] = { FillType.STANDARD, FillType.HATCH,
+			FillType.DOTTED, FillType.CROSSHATCHED, FillType.HONEYCOMB };
 	/**
 	 * @param app
 	 *            GGB application.
