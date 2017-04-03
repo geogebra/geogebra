7aa757598a8a142ef2e0547eb0cc9b63a4fd2f3c
Balazs Bencze <megette_a@yahoo.com>
2017-04-03 06:11:05 +0000

    AND-238 Add documentation to the public methods
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@53261 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- keyboard-base/src/main/java/org/geogebra/keyboard/base/linear/impl/factory/KeyboardFactory.java
+++ keyboard-base/src/main/java/org/geogebra/keyboard/base/linear/impl/factory/KeyboardFactory.java
@@ -2,6 +2,10 @@ package org.geogebra.keyboard.base.linear.impl.factory;
 
 import org.geogebra.keyboard.base.linear.LinearKeyboard;
 
+/**
+ * This class can create {@link LinearKeyboard}s of different types.
+ * It is not thread safe.
+ */
 public class KeyboardFactory {
 
     private MathKeyboardFactory mathKeyboardFactory;
@@ -9,6 +13,11 @@ public class KeyboardFactory {
     private FunctionKeyboardFactory functionKeyboardFactory;
     private LetterKeyboardFactory letterKeyboardFactory;
 
+    /**
+     * Creates a math keyboard with numbers and operators.
+     *
+     * @return math keyboard
+     */
     public LinearKeyboard createMathKeyboard() {
         if (mathKeyboardFactory == null) {
             mathKeyboardFactory = new MathKeyboardFactory();
@@ -16,6 +25,11 @@ public class KeyboardFactory {
         return mathKeyboardFactory.createMathKeyboard();
     }
 
+    /**
+     * Creates a greek keyboard with the greek letters and control buttons.
+     *
+     * @return greek keyboard
+     */
     public LinearKeyboard createGreekKeyboard() {
         if (greekKeyboardFactory == null) {
             greekKeyboardFactory = new GreekKeyboardFactory();
@@ -23,6 +37,11 @@ public class KeyboardFactory {
         return greekKeyboardFactory.createGreekKeyboard();
     }
 
+    /**
+     * Creates a function keyboard with the function buttons.
+     *
+     * @return function keyboard
+     */
     public LinearKeyboard createFunctionKeyboard() {
         if (functionKeyboardFactory == null) {
             functionKeyboardFactory = new FunctionKeyboardFactory();
@@ -30,6 +49,17 @@ public class KeyboardFactory {
         return functionKeyboardFactory.createFunctionKeyboard();
     }
 
+    /**
+     * Creates a letter (or ABC) keyboard with letters on it. There is a restriction on the
+     * row definitions that are passed as a String, namely
+     * he bottom row has to be shorter than the top or middle row.
+     * If the restrictions are not met, a {@link RuntimeException} is thrown.
+     *
+     * @param topRow    a list of characters that will be the buttons of the top row
+     * @param middleRow a list of characters that will the buttons of the middle row
+     * @param bottomRow a list of characters that will be the buttons of the last row
+     * @return
+     */
     public LinearKeyboard createLetterKeyboard(String topRow, String middleRow, String bottomRow) {
         if (letterKeyboardFactory == null) {
             letterKeyboardFactory = new LetterKeyboardFactory();
