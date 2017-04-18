a49267330711dc91edb6c40d10f0e05ad5a503c1
Zbyněk Konečný <zbynek@geogebra.org>
2017-04-18 17:20:05 +0000

    MOW-84 switch off by default
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@53479 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- retex/editor-base/src/main/java/com/himamis/retex/editor/share/editor/MathFieldInternal.java
+++ retex/editor-base/src/main/java/com/himamis/retex/editor/share/editor/MathFieldInternal.java
@@ -82,7 +82,7 @@ public class MathFieldInternal implements KeyListener, FocusListener, ClickListe
 
     public MathFieldInternal(MathField mathField) {
         this.mathField = mathField;
-		this.directFormulaBuilder = true;
+		this.directFormulaBuilder = false;
         cursorController = new CursorController();
 		inputController = new InputController(mathField.getMetaModel());
         keyListener = new KeyListenerImpl(cursorController, inputController);
