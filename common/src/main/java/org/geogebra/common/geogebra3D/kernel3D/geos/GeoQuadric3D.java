dcdf5671c36a56a01e292cfefca845f587dbeaa2
Csilla Solyom-Gecse <solyom_csilla@yahoo.com>
2017-04-20 06:42:08 +0000

    fix GGB-1629 classify quadric
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@53502 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/geogebra3D/kernel3D/geos/GeoQuadric3D.java
+++ common/src/main/java/org/geogebra/common/geogebra3D/kernel3D/geos/GeoQuadric3D.java
@@ -102,9 +102,8 @@ public class GeoQuadric3D extends GeoQuadricND implements Functional2Var,
 			matrix[i] = coeffs[i];
 		}
 
-		if (kernel.getConstruction() != null
-				&& !kernel.getConstruction().isFileLoading()) {
-			// will be done at XML end, by setMatrixFromXML()
+		// try to classify quadric
+		if (type == QUADRIC_NOT_CLASSIFIED) {
 			classifyQuadric();
 		}
 	}
