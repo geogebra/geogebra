cdc2352207b36d9b6233367a935b217482e30809
Balazs Bencze <megette_a@yahoo.com>
2017-05-03 08:56:33 +0000

    Update description
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@53747 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- keyboard-base/src/main/java/org/geogebra/keyboard/base/listener/KeyboardObserver.java
+++ keyboard-base/src/main/java/org/geogebra/keyboard/base/listener/KeyboardObserver.java
@@ -6,6 +6,7 @@ import org.geogebra.keyboard.base.Keyboard;
  * Classes implementing this can register themselves to observe keyboard model changes in
  * {@link Keyboard#registerKeyboardObserver(KeyboardObserver)} such as caps lock or accent toggle.
  * Controllers implementing this should refresh the view from the model {@link Keyboard#getModel()}.
+ * Internal use only.
  */
 public interface KeyboardObserver {
 
