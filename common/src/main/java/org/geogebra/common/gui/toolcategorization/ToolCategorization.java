57125e5e2ec77178f160ada4d4c016d4d73b831f
Mathieu Blossier <mathieublossier@free.fr>
2017-05-12 08:40:01 +0000

    MOB-1219, MOB-1220: ToolCategorization class & tools filtering
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@53936 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/gui/toolcategorization/ToolCategorization.java
+++ common/src/main/java/org/geogebra/common/gui/toolcategorization/ToolCategorization.java
@@ -5,7 +5,6 @@ import org.geogebra.common.gui.toolbar.ToolBar;
 import org.geogebra.common.main.App;
 
 import java.util.ArrayList;
-import java.util.TreeMap;
 import java.util.TreeSet;
 
 /**
@@ -30,28 +29,31 @@ public class ToolCategorization {
         }
     }
 
-    private TreeMap<Category, ArrayList<Integer>> categoriesMap;
+    private Type type;
+    private ArrayList<Category> customizedCategories;
+    private ArrayList<ArrayList<Integer>> toolsLists;
     private TreeSet<Integer> availableTools;
 
     private App app;
 
-    public ToolCategorization(App app) {
+    /**
+     * Creates a tool categorization for the give type
+     *
+     * @param app  App (for localization)
+     * @param type categorization type
+     */
+    public ToolCategorization(App app, Type type) {
         this.app = app;
-        categoriesMap = new TreeMap<Category, ArrayList<Integer>>();
+        toolsLists = new ArrayList<ArrayList<Integer>>();
+        customizedCategories = new ArrayList<Category>();
+        this.type = type;
     }
 
     /**
-     * @param type categorization type
-     * @return categories used for this categorization type
+     * @return categories used
      */
-    public Category[] getCategories(Type type) {
-        switch (type) {
-            case GEOMETRY:
-            case GRAPHING_CALCULATOR:
-            case GRAPHER_3D:
-            default:
-                return new Category[]{Category.BASIC, Category.LINES, Category.CIRCLES};
-        }
+    public ArrayList<Category> getCategories() {
+        return customizedCategories;
     }
 
     /**
@@ -62,65 +64,99 @@ public class ToolCategorization {
         return app.getLocalization().getPlain(category.getHeader());
     }
 
-    public ArrayList<Integer> getTools(Category category) {
-        ArrayList<Integer> ret = categoriesMap.get(category);
-        if (ret == null) {
-            ret = storeTools(category);
-        }
-        return ret;
+    /**
+     * categoryId is the rank of the category over the categories list
+     *
+     * @param categoryId category id
+     * @return list of tools for that category
+     */
+    public ArrayList<Integer> getTools(int categoryId) {
+        return toolsLists.get(categoryId);
     }
 
+    /**
+     * reset tools & categories list
+     */
+    public void resetTools() {
+        resetTools(null);
+    }
+
+    /**
+     * reset tools & categories list, keeping only tools present in toolbarDef
+     *
+     * @param toolbarDef toolbar definition
+     */
     public void resetTools(String toolbarDef) {
         if (toolbarDef == null) {
             this.availableTools = null;
         } else {
             this.availableTools = ToolBar.toSet(toolbarDef);
         }
-        categoriesMap.clear();
+        toolsLists.clear();
+        customizedCategories.clear();
+        buildTools();
     }
 
-    private ArrayList<Integer> storeTools(Category category) {
-        ArrayList<Integer> ret = new ArrayList<Integer>();
-        switch (category) {
-            case BASIC:
-                addToList(ret, EuclidianConstants.MODE_MOVE);
-                addToList(ret, EuclidianConstants.MODE_POINT);
-                addToList(ret, EuclidianConstants.MODE_SEGMENT);
-                addToList(ret, EuclidianConstants.MODE_JOIN);
-                addToList(ret, EuclidianConstants.MODE_CIRCLE_TWO_POINTS);
-                addToList(ret, EuclidianConstants.MODE_POLYGON);
-                addToList(ret, EuclidianConstants.MODE_INTERSECT);
-                break;
-            case LINES:
-                addToList(ret, EuclidianConstants.MODE_ORTHOGONAL);
-                addToList(ret, EuclidianConstants.MODE_LINE_BISECTOR);
-                addToList(ret, EuclidianConstants.MODE_PARALLEL);
-                addToList(ret, EuclidianConstants.MODE_ANGULAR_BISECTOR);
-                addToList(ret, EuclidianConstants.MODE_SEGMENT_FIXED);
-                addToList(ret, EuclidianConstants.MODE_SEGMENT);
-                addToList(ret, EuclidianConstants.MODE_JOIN);
-                addToList(ret, EuclidianConstants.MODE_RAY);
-                addToList(ret, EuclidianConstants.MODE_VECTOR);
-                addToList(ret, EuclidianConstants.MODE_POLYLINE);
-                addToList(ret, EuclidianConstants.MODE_TANGENTS);
-                addToList(ret, EuclidianConstants.MODE_VECTOR_FROM_POINT);
-                addToList(ret, EuclidianConstants.MODE_FITLINE);
-                addToList(ret, EuclidianConstants.MODE_POLAR_DIAMETER);
-                break;
-            case CIRCLES:
-                addToList(ret, EuclidianConstants.MODE_CIRCLE_TWO_POINTS);
-                addToList(ret, EuclidianConstants.MODE_COMPASSES);
-                addToList(ret, EuclidianConstants.MODE_CIRCLE_POINT_RADIUS);
-                addToList(ret, EuclidianConstants.MODE_CIRCLE_THREE_POINTS);
-                addToList(ret, EuclidianConstants.MODE_SEMICIRCLE);
-                addToList(ret, EuclidianConstants.MODE_CIRCLE_ARC_THREE_POINTS);
-                addToList(ret, EuclidianConstants.MODE_CIRCLE_SECTOR_THREE_POINTS);
-                addToList(ret, EuclidianConstants.MODE_CIRCUMCIRCLE_ARC_THREE_POINTS);
-                addToList(ret, EuclidianConstants.MODE_CIRCUMCIRCLE_SECTOR_THREE_POINTS);
+    private void buildTools() {
+        Category category;
+        ArrayList<Integer> tools;
+        switch (type) {
+            case GEOMETRY:
+            case GRAPHING_CALCULATOR:
+            case GRAPHER_3D:
+            default:
+                category = Category.BASIC;
+                tools = new ArrayList<Integer>();
+                addToList(tools, EuclidianConstants.MODE_MOVE);
+                addToList(tools, EuclidianConstants.MODE_POINT);
+                addToList(tools, EuclidianConstants.MODE_SEGMENT);
+                addToList(tools, EuclidianConstants.MODE_JOIN);
+                addToList(tools, EuclidianConstants.MODE_CIRCLE_TWO_POINTS);
+                addToList(tools, EuclidianConstants.MODE_POLYGON);
+                addToList(tools, EuclidianConstants.MODE_INTERSECT);
+                storeIfNotEmpty(category, tools);
+
+                category = Category.LINES;
+                tools = new ArrayList<Integer>();
+                addToList(tools, EuclidianConstants.MODE_ORTHOGONAL);
+                addToList(tools, EuclidianConstants.MODE_LINE_BISECTOR);
+                addToList(tools, EuclidianConstants.MODE_PARALLEL);
+                addToList(tools, EuclidianConstants.MODE_ANGULAR_BISECTOR);
+                addToList(tools, EuclidianConstants.MODE_SEGMENT_FIXED);
+                addToList(tools, EuclidianConstants.MODE_SEGMENT);
+                addToList(tools, EuclidianConstants.MODE_JOIN);
+                addToList(tools, EuclidianConstants.MODE_RAY);
+                addToList(tools, EuclidianConstants.MODE_VECTOR);
+                addToList(tools, EuclidianConstants.MODE_POLYLINE);
+                addToList(tools, EuclidianConstants.MODE_TANGENTS);
+                addToList(tools, EuclidianConstants.MODE_VECTOR_FROM_POINT);
+                addToList(tools, EuclidianConstants.MODE_FITLINE);
+                addToList(tools, EuclidianConstants.MODE_POLAR_DIAMETER);
+                storeIfNotEmpty(category, tools);
+
+                category = Category.CIRCLES;
+                tools = new ArrayList<Integer>();
+                addToList(tools, EuclidianConstants.MODE_CIRCLE_TWO_POINTS);
+                addToList(tools, EuclidianConstants.MODE_COMPASSES);
+                addToList(tools, EuclidianConstants.MODE_CIRCLE_POINT_RADIUS);
+                addToList(tools, EuclidianConstants.MODE_CIRCLE_THREE_POINTS);
+                addToList(tools, EuclidianConstants.MODE_SEMICIRCLE);
+                addToList(tools, EuclidianConstants.MODE_CIRCLE_ARC_THREE_POINTS);
+                addToList(tools, EuclidianConstants.MODE_CIRCLE_SECTOR_THREE_POINTS);
+                addToList(tools, EuclidianConstants.MODE_CIRCUMCIRCLE_ARC_THREE_POINTS);
+                addToList(tools, EuclidianConstants.MODE_CIRCUMCIRCLE_SECTOR_THREE_POINTS);
+                storeIfNotEmpty(category, tools);
                 break;
         }
-        categoriesMap.put(category, ret);
-        return ret;
+
+
+    }
+
+    private void storeIfNotEmpty(Category category, ArrayList<Integer> tools) {
+        if (!tools.isEmpty()) {
+            customizedCategories.add(category);
+            toolsLists.add(tools);
+        }
     }
 
     final private void addToList(ArrayList<Integer> toolList, int mode) {
