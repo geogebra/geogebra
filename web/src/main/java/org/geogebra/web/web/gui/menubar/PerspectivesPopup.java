af87c33930e759922237a98e2ebf421e3b2348e1
Zbyněk Konečný <zbynek@geogebra.org>
2017-05-22 15:51:21 +0000

    Blank target for store links
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@54148 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/web/gui/menubar/PerspectivesPopup.java
+++ web/src/main/java/org/geogebra/web/web/gui/menubar/PerspectivesPopup.java
@@ -112,12 +112,14 @@ public class PerspectivesPopup {
 			Anchor link_playstore = new Anchor(playStoreIcon.toString(), true,
 					"https://play.google.com/store/apps/details?id=org.geogebra.android");
 			link_playstore.addStyleName("linkPlaystore");
+			link_playstore.setTarget("_blank");
 
 			// creating app store icon
 			NoDragImage appStoreIcon = new NoDragImage(ImgResourceHelper
 					.safeURI(GuiResources.INSTANCE.app_store()), 34, true);
 			Anchor link_appstore = new Anchor(appStoreIcon.toString(), true,
 					"https://itunes.apple.com/us/app/geogebra-graphing-calculator/id1146717204");
+			link_appstore.setTarget("_blank");
 
 			// holder panel
 			FlowPanel holderPanel = new FlowPanel();
