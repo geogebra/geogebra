72dc21cd6815c5c9568e6599498119c1cfbbc58a
Zbyněk Konečný <zbynek@geogebra.org>
2017-05-30 13:51:37 +0000

    Javadoc
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@54293 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/html5/util/OfflineLoadingStrategy.java
+++ web/src/main/java/org/geogebra/web/html5/util/OfflineLoadingStrategy.java
@@ -5,8 +5,17 @@ import com.google.gwt.core.client.impl.ScriptTagLoadingStrategy.ScriptTagDownloa
 import com.google.gwt.core.client.impl.XhrLoadingStrategy.XhrDownloadStrategy;
 import com.google.gwt.user.client.Window.Location;
 
+/**
+ * Download strategy that works with appcache / service worker over HTTP and
+ * still supports file protocol (via script tag)
+ *
+ */
 public class OfflineLoadingStrategy extends LoadingStrategyBase {
 
+	/**
+	 * @param downloadStrategy
+	 *            internal download strategy
+	 */
 	public OfflineLoadingStrategy(DownloadStrategy downloadStrategy) {
 		super(downloadStrategy);
 	}
@@ -23,13 +32,23 @@ public class OfflineLoadingStrategy extends LoadingStrategyBase {
 
 	}
 
-	private static native void setAsyncCallback(int fragment,
+	/**
+	 * @param fragment
+	 *            fragment number
+	 * @param request
+	 *            request
+	 */
+	static native void setAsyncCallback(int fragment,
 			RequestData request) /*-{
     __gwtModuleFunction['runAsyncCallback' + fragment] = $entry(function(code, instance) {
       @com.google.gwt.core.client.impl.ScriptTagLoadingStrategy::asyncCallback(Lcom/google/gwt/core/client/impl/LoadingStrategyBase$RequestData;Ljava/lang/String;)(
         request, code);
     });
   }-*/;
+
+	/**
+	 * Create the strategy
+	 */
 	public OfflineLoadingStrategy() {
 		super(makeDownloadStrategy());
 	}
