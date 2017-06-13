geogebra/geogebra

commit 04080508697c86d7a73608c4fddbefeeaa279ef8

tmpCoords is already in used e.g. in https://www.geogebra.org/m/Er9e6mSV: create and use new temp instead
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@53561 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/main/java/org/geogebra/common/geogebra3D/kernel3D/geos/GeoQuadric3D.java
@@ -2497,6 +2497,8 @@ public class GeoQuadric3D extends GeoQuadricND implements Functional2Var,
 		getNormalProjectionParameters(coords, parameters);
 		evaluatePoint(parameters[0], parameters[1], ret);
 	}
+	
+	private Coords tmpCoords6;
 
 	/**
 	 * try with t1, then with t2
@@ -2513,15 +2515,15 @@ public class GeoQuadric3D extends GeoQuadricND implements Functional2Var,
 	 */
 	protected Coords[] getProjection(Coords willingCoords,
 			Coords willingDirection, double t1, double t2) {
-		return getNormalProjection(willingCoords.add(willingDirection.mul(t1)));
-		// TODO faster implementation breaks
-		// https://www.geogebra.org/m/Er9e6mSV; setW(0) might make it equivalent
-		// with the current one
 
-		// tmpCoords.setMul3(willingDirection, t1);
-		// tmpCoords.setAdd3(tmpCoords, willingCoords);
-		// tmpCoords.setW(1);
-		// return getNormalProjection(tmpCoords);
+		if (tmpCoords6 == null) {
+			tmpCoords6 = Coords.createInhomCoorsInD3();
+		}
+
+		tmpCoords6.setMul3(willingDirection, t1);
+		tmpCoords6.setAdd3(tmpCoords6, willingCoords);
+		tmpCoords6.setW(0);
+		return getNormalProjection(tmpCoords6);
 	}
 
 	@Override

commit 6bd11edbdd6ae6f73abe35443c5c06903eead16d

Fix bug introduced by r53561 (hit sphere)
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@54579 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/main/java/org/geogebra/common/geogebra3D/kernel3D/geos/GeoQuadric3D.java
@@ -2545,7 +2545,7 @@ public class GeoQuadric3D extends GeoQuadricND implements Functional2Var,
 
 		tmpCoords6.setMul3(willingDirection, t1);
 		tmpCoords6.setAdd3(tmpCoords6, willingCoords);
-		tmpCoords6.setW(0);
+		tmpCoords6.setW(1);
 		return getNormalProjection(tmpCoords6);
 	}
 