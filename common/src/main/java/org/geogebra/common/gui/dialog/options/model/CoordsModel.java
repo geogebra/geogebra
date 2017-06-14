db77af71e8cfa05e82163533af6d315f88cafd03
Zbyněk Konečný <zbynek@geogebra.org>
2017-06-14 13:10:22 +0000

    GGB-1875 missing update + repaint
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@54632 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/gui/dialog/options/model/CoordsModel.java
+++ common/src/main/java/org/geogebra/common/gui/dialog/options/model/CoordsModel.java
@@ -59,6 +59,7 @@ public class CoordsModel extends MultipleOptionsModel {
 	@Override
 	protected void apply(int index, int value) {
 		getCoordStyleAt(index).setMode(coordValues.get(value));
+		getGeoAt(index).updateRepaint();
 
 	}
 
