766e2f6491aeb9c46bc283d1f9a9d89acb74309f
Mathieu Blossier <mathieublossier@free.fr>
2017-06-20 17:51:03 +0000

    App.startNewExam()
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@54754 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -2,6 +2,7 @@ package org.geogebra.common.main;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
@@ -4491,6 +4492,11 @@ public abstract class App implements UpdateSelection {
 
 	public void setNewExam() {
 		setExam(new ExamEnvironment());
+	}
+	
+	public void startNewExam() {
+		setNewExam();
+		getExam().setStart((new Date()).getTime());
 	}
 
 	/**
