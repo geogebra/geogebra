ba6b00a38d76bacf967c9215d2f12839b3cd7e4a
Michael Borcherds <michael@geogebra.org>
2017-06-23 14:08:05 +0000

    revert r54817 GGB-1876 (doesn't work with AV Input)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@54823 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- keyboard-web/src/main/java/org/geogebra/keyboard/web/TabbedKeyboard.java
+++ keyboard-web/src/main/java/org/geogebra/keyboard/web/TabbedKeyboard.java
@@ -498,9 +498,6 @@ public class TabbedKeyboard extends FlowPanel {
 			if (wb.getActionName().equals("" + Unicode.LCEIL)) {
 				return new KeyBoardButtonBase(KeyboardConstants.CEIL, wb.getActionName(), bh);
 			}
-			if (wb.getActionName().equals("(")) {
-				return new KeyBoardButtonBase("(", "()", b);
-			}
 			return new KeyBoardButtonBase(wb.getActionName(),
 					wb.getActionName(), b);
 		}
