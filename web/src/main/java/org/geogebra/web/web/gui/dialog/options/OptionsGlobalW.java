dec53e148ac0d6f43a131fc3020583882f5699d4
Csilla Solyom-Gecse <solyom_csilla@yahoo.com>
2017-08-02 10:46:38 +0000

    GGB-1989 add save and restore settings buttons
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@55613 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/web/gui/dialog/options/OptionsGlobalW.java
+++ web/src/main/java/org/geogebra/web/web/gui/dialog/options/OptionsGlobalW.java
@@ -1,8 +1,11 @@
 package org.geogebra.web.web.gui.dialog.options;
 
+import org.geogebra.web.html5.gui.FastClickHandler;
 import org.geogebra.web.html5.gui.util.LayoutUtilW;
 import org.geogebra.web.html5.main.AppW;
 import org.geogebra.web.html5.util.tabpanel.MultiRowsTabPanel;
+import org.geogebra.web.web.gui.util.StandardButton;
+import org.geogebra.web.web.main.GeoGebraPreferencesW;
 
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -25,6 +28,9 @@ public class OptionsGlobalW implements OptionPanelW {
 	private ListBox labelingList;
 	private Label lblFontSize;
 	private ListBox fontSizeList;
+	private StandardButton saveSettingsBtn;
+	private StandardButton restoreSettingsBtn;
+	
 
 
 	/**
@@ -41,6 +47,12 @@ public class OptionsGlobalW implements OptionPanelW {
 	private void createGUI() {
 		optionsPanel = new FlowPanel();
 		optionsPanel.setStyleName("algebraOptions");
+		addLabelsWithComboBox();
+		addSaveSettingBtn();
+		addRestoreSettingsBtn();
+	}
+
+	private void addLabelsWithComboBox() {
 		lblGlobal = new Label(app.getLocalization().getMenu("Global"));
 		lblGlobal.addStyleName("panelTitle");
 		optionsPanel.add(lblGlobal);
@@ -56,6 +68,34 @@ public class OptionsGlobalW implements OptionPanelW {
 				app.getLocalization().getMenu("FontSize") + ":");
 		fontSizeList = new ListBox();
 		optionsPanel.add(LayoutUtilW.panelRowIndent(lblFontSize, fontSizeList));
+	}
+
+	private void addRestoreSettingsBtn() {
+		restoreSettingsBtn = new StandardButton(
+				app.getLocalization().getMenu("Settings.ResetDefault"));
+		restoreSettingsBtn.setStyleName("settingsBtn");
+		restoreSettingsBtn.addFastClickHandler(new FastClickHandler() {
+
+			@Override
+			public void onClick(Widget source) {
+				resetDefault();
+			}
+		});
+		optionsPanel.add(restoreSettingsBtn);
+	}
+
+	private void addSaveSettingBtn() {
+		saveSettingsBtn = new StandardButton(
+				app.getLocalization().getMenu("Settings.Save"));
+		saveSettingsBtn.setStyleName("settingsBtn");
+		saveSettingsBtn.addFastClickHandler(new FastClickHandler() {
+
+			@Override
+			public void onClick(Widget source) {
+				GeoGebraPreferencesW.getPref().saveXMLPreferences(app);
+			}
+		});
+		optionsPanel.add(saveSettingsBtn);
 	}
 
 	@Override
@@ -103,6 +143,43 @@ public class OptionsGlobalW implements OptionPanelW {
 	public MultiRowsTabPanel getTabPanel() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	/**
+	 * Reset defaults
+	 */
+	protected void resetDefault() {
+		GeoGebraPreferencesW.getPref().clearPreferences();
+
+		// reset defaults for GUI, views etc
+		// this has to be called before load XML preferences,
+		// in order to avoid overwrite
+		app.getSettings().resetSettings(app);
+
+		// for geoelement defaults, this will do nothing, so it is
+		// OK here
+		GeoGebraPreferencesW.getPref().resetPreferences(app);
+
+		// reset default line thickness etc
+		app.getKernel().getConstruction().getConstructionDefaults()
+				.resetDefaults();
+
+		// reset defaults for geoelements; this will create brand
+		// new objects
+		// so the options defaults dialog should be reset later
+		app.getKernel().getConstruction().getConstructionDefaults()
+				.createDefaultGeoElements();
+
+		// reset the stylebar defaultGeo
+		if (app.getEuclidianView1().hasStyleBar()) {
+			app.getEuclidianView1().getStyleBar().restoreDefaultGeo();
+		}
+		if (app.hasEuclidianView2EitherShowingOrNot(1)
+				&& app.getEuclidianView2(1).hasStyleBar()) {
+			app.getEuclidianView2(1).getStyleBar().restoreDefaultGeo();
+		}
+		// TODO needed to eg. update rounding, possibly too heavy
+		app.getKernel().updateConstruction();
 	}
 
 }
