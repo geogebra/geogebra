0f1f04880c903d78977c6dc6745a6e26c4a51984
Balazs Bencze <megette_a@yahoo.com>
2017-08-09 07:47:11 +0000

    IGR-429 Move IndexLaTeXBuilder to common.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@55758 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common/src/main/java/org/geogebra/common/util/IndexLaTeXBuilder.java
@@ -0,0 +1,19 @@
+package org.geogebra.common.util;
+
+
+public class IndexLaTeXBuilder extends IndexHTMLBuilder {
+
+    public IndexLaTeXBuilder() {
+        super(false);
+    }
+
+    @Override
+    public void startIndex() {
+        append("$_");
+    }
+
+    @Override
+    public void endIndex() {
+        append("$");
+    }
+}
