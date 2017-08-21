72ed7c323d35d6e4b6822d587a1fb2665e64d09a
Judit Elias <eliasjudit@gmail.com>
2017-08-21 15:30:12 +0000

    Unused import removed.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@55962 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/gui/view/algebra/AlgebraItem.java
+++ common/src/main/java/org/geogebra/common/gui/view/algebra/AlgebraItem.java
@@ -11,7 +11,6 @@ import org.geogebra.common.kernel.geos.GeoList;
 import org.geogebra.common.kernel.geos.GeoNumeric;
 import org.geogebra.common.kernel.geos.HasSymbolicMode;
 import org.geogebra.common.main.Feature;
-import org.geogebra.common.util.IndexHTMLBuilder;
 
 import com.himamis.retex.editor.share.util.Unicode;
 
