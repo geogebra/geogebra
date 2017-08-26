geogebra/geogebra

commit 9d6599b7e49ed258a5d8820e7c2e57da7965fd33

make sure macro object Hexagon(G_8) can be moved with the arrow keys
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@55597 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/main/java/org/geogebra/common/kernel/geos/GeoElement.java
@@ -7073,6 +7073,7 @@ public abstract class GeoElement extends ConstructionElement
 
 		else if (isTranslateable()
 				&& getParentAlgorithm() instanceof AlgoTranslate) {
+
 			AlgoElement algo = getParentAlgorithm();
 			GeoElement[] input = algo.getInput();
 			GeoElement in = input[1];
@@ -7099,12 +7100,29 @@ public abstract class GeoElement extends ConstructionElement
 		}
 
 		else {
-			ArrayList<GeoElement> tempMoveObjectList = kernel.getApplication()
-					.getSelectionManager().getTempMoveGeoList();
 
-			movedGeo = moveFromChangeableCoordParentNumbers(rwTransVec,
-					endPosition, viewDirection, updateGeos, tempMoveObjectList,
-					view);
+			ArrayList<GeoPointND> freeInputPoints = getFreeInputPoints(view);
+
+			// eg macro like Hexagon[G_8], if the hexagon is dragged then G_8
+			// needs updating
+			// handled for mouse drag in
+			// EuclidianController.addMovedGeoElementFreeInputPointsToTranslateableGeos
+			// needed here for moving with arrow keys
+			if (freeInputPoints.size() > 0) {
+				for (int i = 0; i < freeInputPoints.size(); i++) {
+					((GeoElement) freeInputPoints.get(i)).moveObject(rwTransVec,
+							endPosition, viewDirection, updateGeos, view);
+				}
+			} else {
+
+				ArrayList<GeoElement> tempMoveObjectList = kernel
+						.getApplication().getSelectionManager()
+						.getTempMoveGeoList();
+
+				movedGeo = moveFromChangeableCoordParentNumbers(rwTransVec,
+						endPosition, viewDirection, updateGeos,
+						tempMoveObjectList, view);
+			}
 		}
 
 		return movedGeo;

commit 3382619ffac3ba472df159b82b117264da251d0b

fix for double-speed dragging (caused by r55597)
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@56065 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/main/java/org/geogebra/common/kernel/geos/GeoElement.java
@@ -57,6 +57,7 @@ import org.geogebra.common.kernel.algos.AlgoDynamicCoordinatesInterface;
 import org.geogebra.common.kernel.algos.AlgoElement;
 import org.geogebra.common.kernel.algos.AlgoIntegralODE;
 import org.geogebra.common.kernel.algos.AlgoJoinPointsSegment;
+import org.geogebra.common.kernel.algos.AlgoMacro;
 import org.geogebra.common.kernel.algos.AlgoMacroInterface;
 import org.geogebra.common.kernel.algos.AlgoName;
 import org.geogebra.common.kernel.algos.AlgoTranslate;
@@ -7172,7 +7173,8 @@ public abstract class GeoElement extends ConstructionElement
 			// handled for mouse drag in
 			// EuclidianController.addMovedGeoElementFreeInputPointsToTranslateableGeos
 			// needed here for moving with arrow keys
-			if (freeInputPoints.size() > 0) {
+			if (getParentAlgorithm() instanceof AlgoMacro
+					&& freeInputPoints.size() > 0) {
 				for (int i = 0; i < freeInputPoints.size(); i++) {
 					((GeoElement) freeInputPoints.get(i)).moveObject(rwTransVec,
 							endPosition, viewDirection, updateGeos, view);