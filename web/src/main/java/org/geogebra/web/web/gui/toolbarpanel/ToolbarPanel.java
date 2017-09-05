ffd0f99b91b1e15222801b446aa8dcaf786ff965
Judit Elias <eliasjudit@gmail.com>
2017-09-05 09:25:01 +0000

    GGB-2039 comment
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@56219 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/web/gui/toolbarpanel/ToolbarPanel.java
+++ web/src/main/java/org/geogebra/web/web/gui/toolbarpanel/ToolbarPanel.java
@@ -493,6 +493,9 @@ public class ToolbarPanel extends FlowPanel implements MyModeChangedListener {
 						0, 0, 24, 24, false, false),
 				app);
 		moveBtn.setStyleName("moveFloatingBtn");
+
+		// moveMoveBtnDown style added for moveBtn to fix the position on tablet
+		// too
 		moveBtn.addStyleName("moveMoveBtnDown");
 		main.add(moveBtn);
 		hideMoveFloatingButton();
