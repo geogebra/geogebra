e1bc02183ac17cd2879f5414d98811b86a160a7b
Michael Borcherds <michael@geogebra.org>
2017-11-08 12:10:23 +0000

    add tests
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@57647 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/war/__giac.js
+++ web/war/__giac.js
@@ -2077,6 +2077,8 @@ var __giac = [ {},
 { cat:"Evaluate", cmd:"Evaluate[(sqrt(1 + x) * sqrt(1 - x)) - sqrt(1 - x^(2))]", result:"0", notes:"TRAC-5527" },
 { cat:"Solve", cmd:"Solve[{4=a*(2)^n,13.5=a*3^n},{a,n}]", result:"{{a = 1 / 2, n = 3}}", notes:"TRAC-5530" },
 { cat:"Factor", cmd:"Factor(x^2+x=x^2-1)", result:"x (x + 1) = (x - 1) (x + 1)" },
+{ cat:"Solve", cmd:"Solve((n^2)*(x/n)^ln(x)=x^2,x)", result:"{x = n, x = \u212F\u00B2}" },
+{ cat:"Solve", cmd:"Solve((n^2)*(x/n)^log(10,x)=x^2,x,n>0)", result:"{x = 100, x = n}" },
 //JSONEND
 // 3/44*sqrt(22)*pi*sign(sqrt(22))*sign(cbrt(3))-3/44*sqrt(22)*pi*sign(sqrt(22))+3/22*sqrt(22)*atan(11/(4*sqrt(22)))-3/22*sqrt(22)*atan(22/(sqrt(22)*cbrt(3)))+11*ln((cbrt(3))^2+22)-11*ln(86)-1/2*(cbrt(3))^2+32
 //{ cat:"Simplify", cmd:"Simplify((sqrt(22)*32-11*sqrt(22)*ln(86)-3*atan(8/sqrt(22)))/sqrt(22)-1/2*((3^(1/3))^2*sqrt(22)-sqrt(22)*22*ln((3^(1/3))^2+22)-6*atan(3^(1/3)/sqrt(22)))/sqrt(22))", result:"" },
