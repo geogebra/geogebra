615c9ce1bb343533461757f741cab5b7f4458ab1
Barna Karacsony <barna.karacsony@geogebra.org>
2017-11-30 15:49:43 +0000

    AND-535: AND_FOCUS_ON_BIND feature checks and related dead code removed
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@58227 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -3947,9 +3947,6 @@ public abstract class App implements UpdateSelection {
             case AND_KEEP_SIGNED_IN_WHEN_NO_CONNECTION:
                 return true; // 5.0.376
 
-            case AND_FOCUS_ON_BIND:
-                return isNativeMobileAppWithNewUI();
-
             case AND_SPEED_UP_AV:
                 return isNativeMobileAppWithNewUI();
 
--- common/src/main/java/org/geogebra/common/main/Feature.java
+++ common/src/main/java/org/geogebra/common/main/Feature.java
@@ -153,8 +153,6 @@ public enum Feature {
 	/** GGB-1881 */
 	MINOR_GRIDLINES,
 
-	AND_FOCUS_ON_BIND,
-
 	/** AND-423 */
 	AND_SPEED_UP_AV,
 
