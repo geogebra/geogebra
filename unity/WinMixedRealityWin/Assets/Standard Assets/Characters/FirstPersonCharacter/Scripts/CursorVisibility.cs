e09b29ed9ca23262d0b3a615307db25f0692d4fd
Jan Borovsky <jan.borovsky@geogebra.org>
2017-11-30 15:06:18 +0000

    Added beta Scene. FPS controller for testing created.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@58224 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ unity/WinMixedRealityWin/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/CursorVisibility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorVisibility : MonoBehaviour
+{
+    void OnLevelWasLoaded(int level)
+    {
+        if (FindObjectOfType<CharacterController>() != null)
+        {
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.visible = true;
+        }
+    }
+}
