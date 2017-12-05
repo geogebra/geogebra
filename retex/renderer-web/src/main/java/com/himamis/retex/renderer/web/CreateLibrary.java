6b1995525af49af44c7822303b60cb506f091722
Zbyněk Konečný <zbynek@geogebra.org>
2017-12-05 18:03:32 +0000

    Add jlmOnInit callback for JLM
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@58339 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- retex/renderer-web/src/main/java/com/himamis/retex/renderer/web/CreateLibrary.java
+++ retex/renderer-web/src/main/java/com/himamis/retex/renderer/web/CreateLibrary.java
@@ -68,20 +68,20 @@ public class CreateLibrary implements EntryPoint {
 			$wnd.console.log("JLM already installed");
 			return;
 		}
-		$wnd.jlmlib = {};
+		var api = {};
 
-		$wnd.jlmlib.initWith = $entry(function(str) {
+		api.initWith = $entry(function(str) {
 			library.@com.himamis.retex.renderer.web.JlmLib::initWith(Ljava/lang/String;)(str);
 		});
 
-		$wnd.jlmlib.SERIF = @com.himamis.retex.renderer.share.TeXFormula::SERIF;
-		$wnd.jlmlib.SANSSERIF = @com.himamis.retex.renderer.share.TeXFormula::SANSSERIF;
-		$wnd.jlmlib.BOLD = @com.himamis.retex.renderer.share.TeXFormula::BOLD;
-		$wnd.jlmlib.ITALIC = @com.himamis.retex.renderer.share.TeXFormula::ITALIC;
-		$wnd.jlmlib.ROMAN = @com.himamis.retex.renderer.share.TeXFormula::ROMAN;
-		$wnd.jlmlib.TYPEWRITER = @com.himamis.retex.renderer.share.TeXFormula::TYPEWRITER;
+		api.SERIF = @com.himamis.retex.renderer.share.TeXFormula::SERIF;
+		api.SANSSERIF = @com.himamis.retex.renderer.share.TeXFormula::SANSSERIF;
+		api.BOLD = @com.himamis.retex.renderer.share.TeXFormula::BOLD;
+		api.ITALIC = @com.himamis.retex.renderer.share.TeXFormula::ITALIC;
+		api.ROMAN = @com.himamis.retex.renderer.share.TeXFormula::ROMAN;
+		api.TYPEWRITER = @com.himamis.retex.renderer.share.TeXFormula::TYPEWRITER;
 
-		$wnd.jlmlib.drawLatex = $entry(function(opts) {
+		api.drawLatex = $entry(function(opts) {
 			//ctx, latex, size, style, x, y, fgColor, bgColor, cb
 			if (!opts.context) {
 				throw ("drawLatex(opts): opts.context must not be null");
@@ -89,22 +89,27 @@ public class CreateLibrary implements EntryPoint {
 			if (!(typeof opts.latex == "string")) {
 				throw ("drawLatex(opts): opts.latex must be of type string.");
 			}
-			var ctx = opts.context, latex = opts.latex, size = opts.size || 12, type = opts.type || 0, x = opts.x || 0, y = opts.y || 0, insets = opts.insets
-					|| {
-						top : 0,
-						bottom : 0,
-						left : 0,
-						right : 0
-					}, topInset = insets.top || 0, bottomInset = insets.bottom || 0, leftInset = insets.left || 0, rightInset = insets.right || 0, fgColor = opts.foregroundColor
+			var ctx = opts.context, latex = opts.latex, size = opts.size || 12, type = opts.type || 0;
+			var x = opts.x || 0, y = opts.y || 0, insets = opts.insets || {
+				top : 0,
+				bottom : 0,
+				left : 0,
+				right : 0
+			}, topInset = insets.top || 0, bottomInset = insets.bottom || 0, leftInset = insets.left || 0, rightInset = insets.right || 0, fgColor = opts.foregroundColor
 					|| "#000000", bgColor = opts.backgroundColor, // undefined === invisible
 			cb = opts.callback;
 
 			return library.@com.himamis.retex.renderer.web.JlmLib::drawLatex(Lcom/google/gwt/canvas/dom/client/Context2d;Ljava/lang/String;DIIIIIIILjava/lang/String;Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)(ctx, latex, size, type, x, y, topInset, leftInset, bottomInset, rightInset, fgColor, bgColor, cb);
 		});
 
-		$wnd.jlmlib.setFontBaseUrl = $entry(function(url) {
+		api.setFontBaseUrl = $entry(function(url) {
 			opentype.@com.himamis.retex.renderer.web.font.opentype.Opentype::setFontBaseUrl(Ljava/lang/String;)(url);
 		});
+		if (typeof $wnd.jlmOnInit == "function") {
+			$wnd.jlmOnInit(api);
+		} else {
+			$wnd.jlmlib = api;
+		}
 	}-*/;
 
 }
