8ef25751a48700103f300482f2afd18ad788a51d
Jan Borovsky <jan.borovsky@geogebra.org>
2017-12-05 13:32:30 +0000

    LookAtCamera Text script
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@58318 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ unity/WinMixedRealityWin/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour {
+
+    public Camera myCamera;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+
+
+    void Update()
+    {
+         if (myCamera == null)
+         {
+             myCamera = Camera.main;
+         }
+         transform.rotation = Quaternion.LookRotation(transform.position - myCamera.transform.position);
+    }
+        
+    
+
+    
+}
