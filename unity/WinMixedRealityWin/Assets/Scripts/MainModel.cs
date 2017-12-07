93e0f5e998e3e2bb0479a2d617ef27e4f382cd50
Jan Borovsky <jan.borovsky@geogebra.org>
2017-12-07 17:14:08 +0000

    Textures
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@58409 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- unity/WinMixedRealityWin/Assets/Scripts/MainModel.cs
+++ unity/WinMixedRealityWin/Assets/Scripts/MainModel.cs
@@ -4,69 +4,106 @@ using UnityEngine;
 using UnityEngine.XR.WSA.Input;
 using HoloToolkit.Unity.InputModule;
 
+
+
+namespace UnityEngine.XR.WSA.Input
+{ 
 public class MainModel : MonoBehaviour, IControllerInputHandler, IInputClickHandler, IFocusable, IControllerTouchpadHandler
-{
-
-
-    private float movementSpeedMultiplier = 0.001f;
-
-
-    public void OnFocusEnter()
     {
-        Debug.Log("you focused on Main Model");
-    }
 
-    public void OnFocusExit()
-    {
-        throw new System.NotImplementedException();
-    }
-
-
-
-    public void OnInputClicked(InputClickedEventData eventData)
-    {
-        //throw new System.NotImplementedException();
-        Debug.Log("You clicked on me");
-    }
-
-    public void OnInputPositionChanged(InputPositionEventData eventData)
-    {
- //       Debug.Log("OnInputPositionChanged");
- //       this.transform.localScale += this.transform.localScale + new Vector3(1,1,1)*movementSpeedMultiplier;
- //       Debug.Log("This.transform.localscale is: " + this.transform.localScale);
-    }
-    
-    public void OnInputPositionUpdated ()
-    {
         
-        Debug.Log("OnInputPosition updated");
+        private float movementSpeedMultiplier = 0.001f;
+
+        #region Members
+        [HideInInspector]
+        public InteractionSourceState InteractionSource;
+        public GameObject interatctionSourceState;
+        #endregion
+
+        public void OnFocusEnter()
+        {
+            Debug.Log("you focused on Main Model");
+        }
+
+        public void OnFocusExit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+
+        public void OnInputClicked(InputClickedEventData eventData)
+        {
+            //throw new System.NotImplementedException();
+            Debug.Log("You clicked on me");
+        }
+
+        public void OnInputPositionChanged(InputPositionEventData eventData)
+        {
+            //       Debug.Log("OnInputPositionChanged");
+            //       this.transform.localScale += this.transform.localScale + new Vector3(1,1,1)*movementSpeedMultiplier;
+            //       Debug.Log("This.transform.localscale is: " + this.transform.localScale);
+        }
+
+        public void OnInputPositionUpdated()
+        {
+
+            Debug.Log("OnInputPosition updated");
+
+        }
+        // IControllerTouchpadHandler
+        public void OnTouchpadReleased(InputEventData eventData)
+        {
+            Debug.Log("OnInputPositionChanged");
+            //throw new System.NotImplementedException();
+        }
+
+        public void OnTouchpadTouched(InputEventData eventData)
+        {
+            Debug.Log("OnInputPositionChanged");
+            //this.transform.localScale += this.transform.localScale + new Vector3(1, 1, 1) * movementSpeedMultiplier;
+            Debug.Log("This.transform.localscale is: " + this.transform.localScale);
+            //throw new System.NotImplementedException();
+
+
+
+            //controllerState.TouchpadPosition.x = 1;
+
+            //Vector2 primaryAxis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+            //InteractionSourceState.touchpadPostion;
+            //InteractionSourceState.touchpad
+
+            if (interatctionSourceState.GetComponent<InteractionSourceState>().touchpadPosition.x > 0.4f)
+            {
+                Debug.Log("Touchpad is aboe 0.4");
+                transform.localPosition = transform.localPosition * 0.5f;
+            }
+
+
+            var interactionSourceStates = InteractionManager.GetCurrentReading();
+
+            
+    }
+
+
+
+        void Start()
+        {
+            if (interatctionSourceState == null)
+            {
+                interatctionSourceState = GameObject.Find("GesturesInput");
+            }
+            
+        }
+
+  
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+
 
     }
-    // IControllerTouchpadHandler
-    public void OnTouchpadReleased(InputEventData eventData)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnTouchpadTouched(InputEventData eventData)
-    {
-        Debug.Log("OnInputPositionChanged");
-        this.transform.localScale += this.transform.localScale + new Vector3(1, 1, 1) * movementSpeedMultiplier;
-        Debug.Log("This.transform.localscale is: " + this.transform.localScale);
-        //throw new System.NotImplementedException();
-    }
-
-
-
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    
 }
