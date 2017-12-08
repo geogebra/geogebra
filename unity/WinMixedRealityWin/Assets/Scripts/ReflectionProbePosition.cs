d8a18fe52fb20b97ded68425451f5cac0eaaf8db
Jan Borovsky <jan.borovsky@geogebra.org>
2017-12-08 11:31:42 +0000

    Adding ReflectionProbePosition.cs XRInput.cs
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@58420 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ unity/WinMixedRealityWin/Assets/Scripts/ReflectionProbePosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectionProbePosition : MonoBehaviour {
+
+    public Camera myCamera;
+    public  float distanceyCamera = -1.21f;
+
+	// Use this for initialization
+	void Start () {
+
+
+        transform.position = transform.position + new Vector3(0, -1.23f, 0f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (myCamera = null)
+        {
+            myCamera = Camera.main;
+        }
+        //this.gameObject.transform.position = myCamera.transform.position;
+        //this.gameObject.transform.position.y = distanceyCamera;
+
+
+    }
+}
