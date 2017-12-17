8f1b212b54a15e33f0f857802798ddd4208f439c
Michael Borcherds <michael@geogebra.org>
2017-12-17 13:48:19 +0000

    CAS: add tests for Solve(3*4^(x/5)=48)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@58635 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/war/__giac.js
+++ web/war/__giac.js
@@ -2113,6 +2113,9 @@ var __giac = [ {},
 { cat:"Simplify", cmd:"Simplify(If(x>0,x))", result:"If(x>0, x)" },
 { cat:"LeftSide", cmd:"LeftSide({a,b}={1,2})", result:"{a,b}" },
 { cat:"RightSide", cmd:"RightSide({a,b}={1,2})", result:"{1,2}" },
+{ cat:"Solve", cmd:"Solve(4^(x/5)=16)", result:"{x = 10}" },
+{ cat:"Solve", cmd:"Solve(3*4^(x/5)=16)", result:"{x = (-ln(243) + 10ln(4)) / ln(4)}" },
+{ cat:"Solve", cmd:"Solve(3*4^(x/5)=48)", result:"{x = 10}" },
 //JSONEND
 // 3/44*sqrt(22)*pi*sign(sqrt(22))*sign(cbrt(3))-3/44*sqrt(22)*pi*sign(sqrt(22))+3/22*sqrt(22)*atan(11/(4*sqrt(22)))-3/22*sqrt(22)*atan(22/(sqrt(22)*cbrt(3)))+11*ln((cbrt(3))^2+22)-11*ln(86)-1/2*(cbrt(3))^2+32
 //{ cat:"Simplify", cmd:"Simplify((sqrt(22)*32-11*sqrt(22)*ln(86)-3*atan(8/sqrt(22)))/sqrt(22)-1/2*((3^(1/3))^2*sqrt(22)-sqrt(22)*22*ln((3^(1/3))^2+22)-6*atan(3^(1/3)/sqrt(22)))/sqrt(22))", result:"" },
