f1d582a6ae125b13b1658a3a4e124c5152d66f99
Alicia Hofstaetter <alicia.hofstaetter@geogebra.org>
2018-01-10 13:08:13 +0000

    MOW-347 icon opacity
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@58884 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/web/gui/toolbar/mow/SubMenuPanel.java
+++ web/src/main/java/org/geogebra/web/web/gui/toolbar/mow/SubMenuPanel.java
@@ -329,7 +329,8 @@ public abstract class SubMenuPanel extends FlowPanel
 		if ((mode < 101 && mode != EuclidianConstants.MODE_TEXT && mode != EuclidianConstants.MODE_IMAGE
 				&& mode != EuclidianConstants.MODE_PEN)
 				|| (mode > 110 && mode != EuclidianConstants.MODE_VIDEO && mode != EuclidianConstants.MODE_AUDIO
-						&& mode != EuclidianConstants.MODE_GEOGEBRA)) {
+						&& mode != EuclidianConstants.MODE_GEOGEBRA
+						&& mode != EuclidianConstants.MODE_CAMERA)) {
 			return true;
 		}
 		return false;
