9a49a62b81670c497878dcb478c73e42d7ca661f
Agoston Suto <suto.agoston@yahoo.com>
2018-02-02 13:26:28 +0000

    StepByStep: fix test
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@59361 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/test/java/org/geogebra/stepbystep/SolveStepTest.java
+++ desktop/src/test/java/org/geogebra/stepbystep/SolveStepTest.java
@@ -128,7 +128,7 @@ public class SolveStepTest {
 	@Test
 	public void cubicEquations() {
 		t("x^3", "x", "x", "-1", "0", "1");
-		t("x^3 + 3 * a * x^2 + 3 * a^2 * x + 1", "0", "x", "(nroot((-1 + (a)^(3)), 3)-a)");
+		t("x^3 + 3 * a * x^2 + 3 * a^2 * x + 1", "0", "x", "(nroot(((a)^(3)-1), 3)-a)");
 		t("x^3+1", "4", "x", "nroot(3, 3)");
 		t("x^3+3x^2+3x+2", "0", "x", "-2");
 		t("x^3-6x^2+12x+13", "0", "x", "(-nroot(21, 3) + 2)");
