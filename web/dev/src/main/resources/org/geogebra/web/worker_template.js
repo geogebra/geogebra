0a68864ed161ba577c3586fb305eff429dcc498c
Zbyněk Konečný <zbynek@geogebra.org>
2018-02-12 16:19:05 +0000

    Service worker: load jquery from our CDN
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@59574 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/dev/src/main/resources/org/geogebra/web/worker_template.js
+++ web/dev/src/main/resources/org/geogebra/web/worker_template.js
@@ -5,8 +5,8 @@ var silent = true, // disable output to console
     "unique_id": "#%ID%",
     "urls_to_cache": [
       %URLS%,
-      "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js",
-      "https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js",
+      "https://cdn.geogebra.org/files/lib/jquery/1.11.1/jquery.min.js",
+      "https://cdn.geogebra.org/files/lib/jqueryui/1.11.1/jquery-ui.min.js",
       "/scripts/jlatexmath/jlatexmath.js?v=1472544409"
     ]
   };
