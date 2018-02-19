270aaf4cfd13850adbc8ef6ef33c5d1aa8e6419b
Laszlo Gal <laszlo@geogebra.org>
2018-02-19 14:34:06 +0000

    MOW-336 save current page on click fixed.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@59763 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/full/gui/pagecontrolpanel/PageListController.java
+++ web/src/main/java/org/geogebra/web/full/gui/pagecontrolpanel/PageListController.java
@@ -372,6 +372,9 @@ public class PageListController implements PageListControllerInterface,
 
 	@Override
 	public void clickPage(int pageIdx, boolean select) {
+		if (selectedCard != null) {
+			savePreviewCard(selectedCard);
+		}
 		loadSlide(null, pageIdx, false);
 		if (select) {
 			setCardSelected(getCard(pageIdx));
