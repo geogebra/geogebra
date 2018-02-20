030560a03907b8d2f447495c8454ebed53a8d3d6
Alicia Hofstaetter <alicia.hofstaetter@geogebra.org>
2018-02-20 13:38:48 +0000

    MOW-384 log needed for testing
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@59792 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/full/gui/dialog/image/WebCamInputPanel.java
+++ web/src/main/java/org/geogebra/web/full/gui/dialog/image/WebCamInputPanel.java
@@ -109,7 +109,7 @@ public class WebCamInputPanel extends VerticalPanel {
 									} else {
 										that.@org.geogebra.web.full.gui.dialog.image.WebCamInputPanel::showPermissionDeniedDialog()();
 									}
-									@org.geogebra.common.util.debug.Log::debug(Ljava/lang/String;)("Error from WebCam: "+err);
+									@org.geogebra.common.util.debug.Log::debug(Ljava/lang/String;)("Error from WebCam: "+err +" || Name: " + err.name);
 								});
 				function accessRequest() {
 					if (!browserAlreadyAllowed && !deniedByUser) {
