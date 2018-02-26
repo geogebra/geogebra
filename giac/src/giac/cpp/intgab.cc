076e6f9e57457609508a4a7734344175ae129b9c
Bernard Parisse <bernard.parisse@ujf-grenoble.fr>
2018-02-26 07:37:33 +0000

    [giac] solve ∫(e^(-a*x^2+b*x+c)*polynomial,x,-∞,∞)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@59935 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- giac/src/giac/cpp/intgab.cc
+++ giac/src/giac/cpp/intgab.cc
@@ -579,8 +579,17 @@ namespace giac {
     // check if g may be integrated using the residue formula
     gen A,B,P;
     int typeint=rational?2:is_meromorphic(g,x,A,B,P,contextptr);
-    if (typeint==6 && a==minus_inf && b==plus_inf && is_zero(P) && is_constant_wrt(A,x,contextptr) && is_constant_wrt(B,x,contextptr) && is_strictly_positive(-A[0],contextptr)){
+    if (typeint==6 && a==minus_inf && b==plus_inf && is_zero(P) && is_constant_wrt(A,x,contextptr) && lvarxwithinv(B,x,contextptr).size()<2 && is_strictly_positive(-A[0],contextptr)){
       gen A_(A[0]),B_(A[1]),C_(A[2]);
+      gen der(2*A_*x+B_),tmp(B);
+      B=0;
+      while (!is_zero(tmp,contextptr)){
+	tmp=_quorem(makesequence(tmp,der,x),contextptr);
+	if (tmp.type!=_VECT || tmp._VECTptr->size()!=2)
+	  return false;
+	B += tmp._VECTptr->back();
+	tmp=-derive(tmp._VECTptr->front(),x,contextptr);
+      }
       // int(exp(A_*x^2+B_*x+C_)*B,x,-inf,inf)
       // =int(exp(A_*(x+B_/2/A_)^2+(-B_^2/4/A_+C_))*B,x,-inf,inf)
       // =sqrt(pi/A_)*B*exp(B_^2/4/A_-C_)
