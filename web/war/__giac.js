058ee0d86938e428bc987322135fe05b8ff77905
Michael Borcherds <michael@geogebra.org>
2018-03-06 11:39:21 +0000

    add tests
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@60110 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/war/__giac.js
+++ web/war/__giac.js
@@ -2155,6 +2155,8 @@ var __giac = [ {},
 { cat:"Evaluate", cmd:"Evaluate({{1,2},{3,4},{5,6}} * Vector((1,2)))", result:"ggbvect((5, 11, 17))" },
 { cat:"Evaluate", cmd:"Evaluate({{1,2,3},{4,5,6},{7,8,9},{10,11,12}} * Vector((1,2,3)))", result:"ggbvect(14,32,50,68)" },
 { cat:"Solve", cmd:"Solve(|x|^2 = |x^2|)", result:"{x= x}"},
+{ cat:"Integral", cmd:"Integral((-1) / 160 floor(x)² + 1 / 10 floor(x) + 2 / 5,15,16)", result:"79 / 160", round:"0.49" },
+{ cat:"Normal", cmd:"InverseNormal(1,2,0.4)", result:"0.4933057937284", round:"0.49" },
 //JSONEND
 // 3/44*sqrt(22)*pi*sign(sqrt(22))*sign(cbrt(3))-3/44*sqrt(22)*pi*sign(sqrt(22))+3/22*sqrt(22)*atan(11/(4*sqrt(22)))-3/22*sqrt(22)*atan(22/(sqrt(22)*cbrt(3)))+11*ln((cbrt(3))^2+22)-11*ln(86)-1/2*(cbrt(3))^2+32
 //{ cat:"Simplify", cmd:"Simplify((sqrt(22)*32-11*sqrt(22)*ln(86)-3*atan(8/sqrt(22)))/sqrt(22)-1/2*((3^(1/3))^2*sqrt(22)-sqrt(22)*22*ln((3^(1/3))^2+22)-6*atan(3^(1/3)/sqrt(22)))/sqrt(22))", result:"" },
