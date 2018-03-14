a43871dc7a235241e37e8245b23c66a9c83028ea
Balazs Bencze <megette_a@yahoo.com>
2018-03-14 07:27:11 +0000

    IGR-623 Add Angle Unit property
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@60335 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/properties/PropertiesFactory.java
+++ common/src/main/java/org/geogebra/common/properties/PropertiesFactory.java
@@ -2,11 +2,15 @@ package org.geogebra.common.properties;
 
 import org.geogebra.common.main.App;
 import org.geogebra.common.main.Localization;
+import org.geogebra.common.properties.impl.AngleUnitProperty;
 import org.geogebra.common.properties.impl.RoundingProperty;
 
 public class PropertiesFactory {
 
     public static Property[] createGeneralProperties(App app, Localization localization) {
-        return new Property[] { new RoundingProperty(app, localization) };
+        return new Property[] {
+                new RoundingProperty(app, localization),
+                new AngleUnitProperty(app.getKernel(), localization)
+        };
     }
 }
---
+++ common/src/main/java/org/geogebra/common/properties/impl/AngleUnitProperty.java
@@ -0,0 +1,34 @@
+package org.geogebra.common.properties.impl;
+
+import org.geogebra.common.kernel.Kernel;
+import org.geogebra.common.main.App;
+import org.geogebra.common.main.Localization;
+import org.geogebra.common.properties.AbstractEnumerableProperty;
+
+public class AngleUnitProperty extends AbstractEnumerableProperty {
+
+    private Kernel kernel;
+
+    public AngleUnitProperty(Kernel kernel, Localization localization) {
+        super(localization, "AngleUnit", "");
+        this.kernel = kernel;
+        setupValues(localization);
+    }
+
+    private void setupValues(Localization localization) {
+        String degree = localization.getMenu("Degree");
+        String radiant = localization.getMenu("Radiant");
+        setValues(new String[] { degree, radiant });
+    }
+
+    @Override
+    protected void setValueSafe(String value, int index) {
+        int angleUnit = index == 0 ? Kernel.ANGLE_DEGREE : Kernel.ANGLE_RADIANT;
+        kernel.setAngleUnit(angleUnit);
+    }
+
+    @Override
+    public int getCurrent() {
+        return kernel.getAngleUnit() == Kernel.ANGLE_DEGREE ? 0 : 1;
+    }
+}
