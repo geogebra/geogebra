2e8fb9bc9255fc546bfe615eeac2e87442866dd7
Automatic update by the ggbtrans subsystem <geogebra-dev@googlegroups.com>
2018-03-18 19:20:51 +0000

    properties files and related resources automatically updated from ggbtrans
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@60454 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/nonfree/resources/org/geogebra/web/pub/js/properties_keys_it.js
+++ web/src/nonfree/resources/org/geogebra/web/pub/js/properties_keys_it.js
@@ -3121,7 +3121,7 @@ __GGB__keysVar["it"].menu = {
     "exam_start_time": "Ora inizio",
     "exam_started": "Esame iniziato",
     "exam_views_deactivated": "Viste disattivate",
-    "exportImage": "Export  Image",
+    "exportImage": "Esporta immagine",
     "firstAxisLength": "lunghezza semiasse maggiore",
     "fixed": "Fisso",
     "fncInspector.Curvature": "Curvatura",
