fa2c753b98ba61b689d9d572b2e969162806114f
Balazs Bencze <megette_a@yahoo.com>
2018-04-10 08:20:52 +0000

    IGR-774 Create GridStyle property
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@61012 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/properties/PropertiesFactory.java
+++ common/src/main/java/org/geogebra/common/properties/PropertiesFactory.java
@@ -16,6 +16,7 @@ import org.geogebra.common.properties.impl.general.LabelingProperty;
 import org.geogebra.common.properties.impl.general.LanguageProperty;
 import org.geogebra.common.properties.impl.general.RoundingProperty;
 import org.geogebra.common.properties.impl.graphics.AxesVisibilityProperty;
+import org.geogebra.common.properties.impl.graphics.GridStyleProperty;
 import org.geogebra.common.properties.impl.graphics.GridVisibilityProperty;
 
 /**
@@ -71,7 +72,8 @@ public class PropertiesFactory {
         EuclidianSettings euclidianSettings = activeView.getSettings();
         return new Property[]{
                 new AxesVisibilityProperty(localization, euclidianSettings),
-                new GridVisibilityProperty(localization, euclidianSettings)
+                new GridVisibilityProperty(localization, euclidianSettings),
+                new GridStyleProperty(localization, euclidianSettings)
         };
     }
 }
---
+++ common/src/main/java/org/geogebra/common/properties/impl/graphics/GridStyleProperty.java
@@ -0,0 +1,30 @@
+package org.geogebra.common.properties.impl.graphics;
+
+import org.geogebra.common.main.Localization;
+import org.geogebra.common.main.settings.EuclidianSettings;
+import org.geogebra.common.properties.AbstractEnumerableProperty;
+
+public class GridStyleProperty extends AbstractEnumerableProperty {
+
+    private EuclidianSettings euclidianSettings;
+
+    public GridStyleProperty(Localization localization, EuclidianSettings euclidianSettings) {
+        super(localization, "GridType");
+        this.euclidianSettings = euclidianSettings;;
+    }
+
+    @Override
+    public int getCurrent() {
+        return 0;
+    }
+
+    @Override
+    protected void setValueSafe(String value, int index) {
+
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return euclidianSettings.getShowGrid();
+    }
+}
