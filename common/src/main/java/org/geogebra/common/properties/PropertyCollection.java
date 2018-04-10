ce121749a0e4d10f33e913d80233cdb1b2bb6048
Balazs Bencze <megette_a@yahoo.com>
2018-04-10 08:36:27 +0000

    IGR-775 Create a property collection
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@61014 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common/src/main/java/org/geogebra/common/properties/PropertyCollection.java
@@ -0,0 +1,15 @@
+package org.geogebra.common.properties;
+
+/**
+ * This property has a collection of properties. This can be though of as a container
+ * for sub properties that are related to eachother.
+ */
+public interface PropertyCollection extends Property {
+
+    /**
+     * Returns the properties that belong to this property group.
+     *
+     * @return an array of properties.
+     */
+    Property[] getProperties();
+}
