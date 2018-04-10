a0767d75cef9813b578fcf1652130db4e2772220
Balazs Bencze <megette_a@yahoo.com>
2018-04-10 09:18:03 +0000

    IGR-774 Implement Grid style
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@61018 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/properties/IconsEnumerableProperty.java
+++ common/src/main/java/org/geogebra/common/properties/IconsEnumerableProperty.java
@@ -4,4 +4,12 @@ package org.geogebra.common.properties;
  * A property which values are also represented as icons.
  */
 public interface IconsEnumerableProperty extends EnumerableProperty {
+
+    /**
+     * Returns an array with the identifiers of the icons. The identifiers are usually
+     * tied to a specific property
+     *
+     * @return an array of identifiers
+     */
+    int[] getIconIds();
 }
--- common/src/main/java/org/geogebra/common/properties/impl/graphics/GridStyleProperty.java
+++ common/src/main/java/org/geogebra/common/properties/impl/graphics/GridStyleProperty.java
@@ -1,5 +1,6 @@
 package org.geogebra.common.properties.impl.graphics;
 
+import org.geogebra.common.euclidian.EuclidianView;
 import org.geogebra.common.main.Localization;
 import org.geogebra.common.main.settings.EuclidianSettings;
 import org.geogebra.common.properties.AbstractEnumerableProperty;
@@ -10,7 +11,25 @@ import org.geogebra.common.properties.IconsEnumerableProperty;
  */
 public class GridStyleProperty extends AbstractEnumerableProperty implements IconsEnumerableProperty {
 
+    public static final int ICON_CARTESIAN = 0;
+    public static final int ICON_CARTESIAN_MINOR = 1;
+    public static final int ICON_POLAR = 2;
+    public static final int ICON_ISOMETRIC = 3;
+
     private EuclidianSettings euclidianSettings;
+
+    private int[] iconIds = new int[]{
+            ICON_CARTESIAN,
+            ICON_CARTESIAN_MINOR,
+            ICON_POLAR, ICON_ISOMETRIC
+    };
+
+    private int[] gridTypes = new int[]{
+            EuclidianView.GRID_CARTESIAN,
+            EuclidianView.GRID_CARTESIAN_WITH_SUBGRID,
+            EuclidianView.GRID_POLAR,
+            EuclidianView.GRID_ISOMETRIC
+    };
 
     /**
      * Controls a grid style property.
@@ -21,20 +40,42 @@ public class GridStyleProperty extends AbstractEnumerableProperty implements Ico
     public GridStyleProperty(Localization localization, EuclidianSettings euclidianSettings) {
         super(localization, "GridType");
         this.euclidianSettings = euclidianSettings;
+        setValuesAndLocalize(new String[]{
+                "Grid.Major",
+                "Grid.MajorAndMinor",
+                "Polar",
+                "Isometric"
+        });
     }
 
     @Override
     public int getCurrent() {
-        return 0;
+        switch (euclidianSettings.getGridType()) {
+            case EuclidianView.GRID_CARTESIAN:
+                return 0;
+            case EuclidianView.GRID_CARTESIAN_WITH_SUBGRID:
+                return 1;
+            case EuclidianView.GRID_POLAR:
+                return 2;
+            case EuclidianView.GRID_ISOMETRIC:
+                return 3;
+            default:
+                return -1;
+        }
     }
 
     @Override
     protected void setValueSafe(String value, int index) {
-
+        euclidianSettings.setGridType(gridTypes[index]);
     }
 
     @Override
     public boolean isEnabled() {
         return euclidianSettings.getShowGrid();
+    }
+
+    @Override
+    public int[] getIconIds() {
+        return iconIds;
     }
 }
