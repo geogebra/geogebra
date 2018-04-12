07de21d83a89400dcb0cd244f35899501b556d8f
Balazs Bencze <megette_a@yahoo.com>
2018-04-12 11:22:13 +0000

    MOB-1479 Remove MOB_INCREASE_TOUCHABLE_AREA flag
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@61081 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -3968,10 +3968,6 @@ public abstract class App implements UpdateSelection {
 		case MOB_AND_CHECKBOXES_AND_STATUS_BAR:
 			return true;
 
-		// MOB-1259
-		case MOB_INCREASE_TOUCHABLE_AREA:
-			return true;
-
 		// AND-875
 		case MOB_LOAD_SAVE_FOR_PRE_LOLLIPOP:
 			return true;
