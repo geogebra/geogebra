0bb38fc243b86936aaf70b66b6e7dd90193783c9
Jan Borovsky <jan.borovsky@geogebra.org>
2018-04-19 06:38:37 +0000

    MOB-1494 Fade in/out added. Added missing scripts.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@61208 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ unity/WinMixedRealityWin/Assets/Scripts/CloseWebviewIcon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class CloseWebviewIcon : MonoBehaviour, IFocusable, IInputClickHandler {
+
+    public GameObject webviewManager;
+    private WebviewManager webviewManagerScript;
+
+    // Use this for initialization
+    void Start () {
+		if (webviewManager == null)
+        {
+            webviewManager = GameObject.Find("GGBKeyboardDisplayCanvas");
+            webviewManagerScript = webviewManager.GetComponent<WebviewManager>();
+        }
+	}
+
+    public void OnFocusEnter()
+    {
+    }
+
+    public void OnFocusExit()
+    {
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        webviewManagerScript.DissapperWebview();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ unity/WinMixedRealityWin/Assets/Scripts/HighlightSizeOnFocuse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+
+public class HighlightSizeOnFocuse : MonoBehaviour, IFocusable {
+
+    public float highlightedSize = 1.2f;
+    private Vector3 _originTransform;
+
+    // Use this for initialization
+    void Start () {
+        _originTransform = this.transform.localScale;
+    }
+	
+    public void OnFocusEnter()
+    {
+        this.transform.localScale = transform.localScale * highlightedSize;
+    }
+
+    public void OnFocusExit()
+    {
+        this.transform.localScale = _originTransform;
+    }
+}
