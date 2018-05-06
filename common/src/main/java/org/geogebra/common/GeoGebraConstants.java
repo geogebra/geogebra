5c83443caf101273556d8e8e821d350497ea43f5
Automatic update by maintainer at dev.geogebra.org <geogebra-dev@googlegroups.com>
2018-05-06 14:24:26 +0000

    version bump
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@61586 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/GeoGebraConstants.java
+++ common/src/main/java/org/geogebra/common/GeoGebraConstants.java
@@ -10,9 +10,9 @@ public interface GeoGebraConstants {
 	// as it is read by the build system
 	// and updated automatically by the "Version Bump" task
 	/** last build date */
-	public static final String BUILD_DATE = "02 May 2018";
+	public static final String BUILD_DATE = "06 May 2018";
 	/** complete version string */
-	public static final String VERSION_STRING = "5.0.462.0";
+	public static final String VERSION_STRING = "5.0.463.0";
 
 	/** proper noun, should NOT be translated / transliterated */
 	public static final String APPLICATION_NAME = "GeoGebra";
