66e60fb46bb63bdfcc3bd8e6be8f7c62a4047ded
Automatic update by the ggbtrans subsystem <geogebra-dev@googlegroups.com>
2018-05-11 18:20:18 +0000

    properties files and related resources automatically updated from ggbtrans
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@61731 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/nonfree/resources/org/geogebra/web/pub/js/properties_keys_es-ES.js
+++ web/src/nonfree/resources/org/geogebra/web/pub/js/properties_keys_es-ES.js
@@ -3191,7 +3191,7 @@ __GGB__keysVar["es-ES"].menu = {
     "pdf": "Documento PDF",
     "permission.photos": "GeoGebra necesita acceder a tu galer\xeda de fotos para guardar una imagen.",
     "permission.photos.denied": "Denegado el permiso de acceso a las fotos",
-    "permission.photos.exam_screenshot": "GeoGebra necesita acceder a tu biblioteca de im\xe1genes para poder guardar una copia del registro al finalizar el examen.",
+    "permission.photos.exam_screenshot": "GeoGebra necesita acceder a tu galer\xeda de im\xe1genes para poder guardar una copia del registro al finalizar el examen.",
     "permission.request": "Por favor, permite el acceso en tu Configuraci\xf3n.",
     "phone_change_visibility": "Change Visibility",
     "phone_change_visibility_text": "If you share this private material, its visibility status will be changed to \"Shared with Link\".",
