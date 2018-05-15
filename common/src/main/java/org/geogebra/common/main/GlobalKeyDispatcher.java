geogebra/geogebra

commit 462dc770bbfc78b54a1ecce9af411c7f3d042188

tidy code
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@58738 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/main/java/org/geogebra/common/main/GlobalKeyDispatcher.java
@@ -137,13 +137,13 @@ public abstract class GlobalKeyDispatcher {
 		if (ch == '/') {
 			toggleSelectionVisibility();
 		}
-		if (selection.selectedGeosSize() == 1) {
-			if (ch == 13) {
-				startEdit(geo = selection.getSelectedGeos().get(0));
-			} else if (ch == '.') {
-				openSettingsInAV(selection.getSelectedGeos().get(0));
-			}
+
+		if (ch == '\n') {
+			startEdit(geo);
+		} else if (ch == '.') {
+			openSettingsInAV(geo);
 		}
+
 		// don't instantiate: could steal focus
 		if (app.getActiveEuclidianView().hasDynamicStyleBar()) {
 			app.getActiveEuclidianView().getDynamicStyleBar().setVisible(false);

commit c089f9d6ec3e61486225359b91158e7bf79f8e58

Fix Enter to edit geo, broken since r58738
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@61808 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/main/java/org/geogebra/common/main/GlobalKeyDispatcher.java
@@ -145,7 +145,7 @@ public abstract class GlobalKeyDispatcher {
 			toggleSelectionVisibility();
 		}
 
-		if (ch == '\n') {
+		if (ch == '\n' || ch == '\r') {
 			startEdit(geo);
 		} else if (ch == '.') {
 			openSettingsInAV(geo);