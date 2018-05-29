geogebra/geogebra

commit e6f07c68744effeabc1dbd98fd8468815c574c3e

MOW-375 Make sure we load images needed for current slide
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@62060 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/main/java/org/geogebra/web/html5/main/AppW.java
@@ -827,10 +827,13 @@ public abstract class AppW extends App implements SetLabels, HasLanguage {
 		} else {
 			kernel.setLibraryJavaScript(libraryJS);
 		}
-
-		if (!asSlide) {
-			for (Entry<String, String> entry : archive.entrySet()) {
-				maybeProcessImage(entry.getKey(), entry.getValue());
+		HashMap<String, String> toLoad = new HashMap<>();
+		for (Entry<String, String> entry : archive.entrySet()) {
+			if (getImageManager().getExternalImage(entry.getKey(),
+			 this) == null) {
+			if (maybeProcessImage(entry.getKey(), entry.getValue())) {
+				toLoad.put(entry.getKey(), entry.getValue());
+			}
 			}
 		}
 
@@ -881,13 +884,13 @@ public abstract class AppW extends App implements SetLabels, HasLanguage {
 			}
 
 		};
-		if (!getImageManager().hasImages() || asSlide) {
+		if (toLoad.isEmpty()) {
 			afterImages.run();
 			setCurrentFile(archiveContent);
 			// getKernel().setNotifyViewsActive(true);
 		} else {
 			// on images do nothing here: wait for callback when images loaded.
-			getImageManager().triggerImageLoading(this, afterImages);
+			getImageManager().triggerImageLoading(this, afterImages, toLoad);
 			setCurrentFile(archiveContent);
 		}
 	}
@@ -982,17 +985,17 @@ public abstract class AppW extends App implements SetLabels, HasLanguage {
 		}
 	}
 
-	private void maybeProcessImage(String filename0, String content) {
+	private boolean maybeProcessImage(String filename0, String content) {
 		String fn = filename0.toLowerCase();
 		if (fn.equals(MyXMLio.XML_FILE_THUMBNAIL)) {
-			return; // Ignore thumbnail
+			return false; // Ignore thumbnail
 		}
 
 		FileExtensions ext = StringUtil.getFileExtension(fn);
 
 		// Ignore non image files
 		if (!ext.isImage()) {
-			return;
+			return false;
 		}
 		String filename = filename0;
 		// bug in old versions (PNG saved with wrong extension)
@@ -1014,6 +1017,7 @@ public abstract class AppW extends App implements SetLabels, HasLanguage {
 		} else {
 			getImageManager().addExternalImage(filename, content);
 		}
+		return true;
 	}
 
 	@Override

web/src/main/java/org/geogebra/web/html5/util/ImageManagerW.java
@@ -2,6 +2,7 @@ package org.geogebra.web.html5.util;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeSet;
 
@@ -59,9 +60,10 @@ public class ImageManagerW extends ImageManager {
 		return externalImageSrcs.get(StringUtil.removeLeadingSlash(fileName));
 	}
 
-	protected void checkIfAllLoaded(AppW app1, Runnable run) {
+	protected void checkIfAllLoaded(AppW app1, Runnable run,
+			Map<String, String> toLoad) {
 		imagesLoaded++;
-		if (imagesLoaded == externalImageSrcs.size()) {
+		if (imagesLoaded == toLoad.size()) {
 			run.run();
 			imagesLoaded = 0;
 		}
@@ -165,19 +167,21 @@ public class ImageManagerW extends ImageManager {
 	 *            app
 	 * @param run
 	 *            image load callback
+	 * @param toLoad
+	 *            map of images to be loaded
 	 */
 	public void triggerImageLoading(final AppW app,
-			final Runnable run) {
-
-		if (externalImageSrcs.entrySet() != null) {
-			for (Entry<String, String> imgSrc : externalImageSrcs.entrySet()) {
-				ImageWrapper img = new ImageWrapper(
-						getExternalImage(imgSrc.getKey(), app));
+			final Runnable run, final Map<String, String> toLoad) {
+		this.imagesLoaded = 0;
+		if (toLoad.entrySet() != null) {
+			for (Entry<String, String> imgSrc : toLoad.entrySet()) {
+				ImageElement el = getExternalImage(imgSrc.getKey(), app);
+				ImageWrapper img = new ImageWrapper(el);
 				img.attachNativeLoadHandler(this, new ImageLoadCallback() {
 
 					@Override
 					public void onLoad() {
-						checkIfAllLoaded(app, run);
+						checkIfAllLoaded(app, run, toLoad);
 					}
 				});
 				img.getElement().setSrc(imgSrc.getValue());

commit f233048609a35eab9c203373160067aaf7fad5ba

Fix SVG loading in Web, broken in r62060
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@62106 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/main/java/org/geogebra/web/html5/main/AppW.java
@@ -831,9 +831,7 @@ public abstract class AppW extends App implements SetLabels, HasLanguage {
 		for (Entry<String, String> entry : archive.entrySet()) {
 			if (getImageManager().getExternalImage(entry.getKey(),
 			 this) == null) {
-			if (maybeProcessImage(entry.getKey(), entry.getValue())) {
-				toLoad.put(entry.getKey(), entry.getValue());
-			}
+				maybeProcessImage(entry.getKey(), entry.getValue(), toLoad);
 			}
 		}
 
@@ -985,7 +983,7 @@ public abstract class AppW extends App implements SetLabels, HasLanguage {
 		}
 	}
 
-	private boolean maybeProcessImage(String filename0, String content) {
+	private boolean maybeProcessImage(String filename0, String content, HashMap<String, String> toLoad) {
 		String fn = filename0.toLowerCase();
 		if (fn.equals(MyXMLio.XML_FILE_THUMBNAIL)) {
 			return false; // Ignore thumbnail
@@ -1011,11 +1009,14 @@ public abstract class AppW extends App implements SetLabels, HasLanguage {
 
 		if (ext.equals(FileExtensions.SVG)) {
 			// IE11/Edge needs SVG to be base64 encoded
-			getImageManager().addExternalImage(filename,
+			String fixedContent =
 					StringUtil.svgMarker + Browser
-							.encodeBase64(ImageManager.fixSVG(content)));
+							.encodeBase64(ImageManager.fixSVG(content));
+			getImageManager().addExternalImage(filename, fixedContent);
+			toLoad.put(filename, fixedContent);
 		} else {
 			getImageManager().addExternalImage(filename, content);
+			toLoad.put(filename, content);
 		}
 		return true;
 	}