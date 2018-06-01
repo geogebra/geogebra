4bc1be89af2ff4a41adf2c7d0307d8ef13b78fa7
Michael Borcherds <michael@geogebra.org>
2018-06-01 10:51:22 +0000

    add capturingThreshold
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@62167 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/HTML5Export.java
+++ common/src/main/java/org/geogebra/common/main/HTML5Export.java
@@ -64,9 +64,12 @@ public class HTML5Export {
 		sb.append("\"allowStyleBar\":false,\n");
 		sb.append("\"preventFocus\":false,\n");
 		sb.append("\"showZoomButtons\":true,\n");
-		sb.append("// add code here to run when rhe applet starts\n");
+		sb.append("\"capturingThreshold\":3,\n");
+
+		sb.append("// add code here to run when the applet starts\n");
 		sb.append("\"appletOnLoad\":function(api){");
 		sb.append(" /* api.evalCommand('Segment((1,2),(3,4))');*/ },\n");
+
 		sb.append("\"showFullscreenButton\":true,\n");
 		sb.append("\"scale\":1,\n");
 		sb.append("\"disableAutoScale\":false,\n");
