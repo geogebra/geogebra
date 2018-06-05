ffd6cdabd4a8fff1b2083b24a9e871c07d3a5aa2
Csilla Solyom-Gecse <solyom_csilla@yahoo.com>
2018-06-05 06:08:04 +0000

    MOW-422 add left/right buttons to dialog
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@62247 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/full/gui/dialog/PDFInputDialog.java
+++ web/src/main/java/org/geogebra/web/full/gui/dialog/PDFInputDialog.java
@@ -3,14 +3,18 @@ package org.geogebra.web.full.gui.dialog;
 import org.geogebra.common.euclidian.EuclidianConstants;
 import org.geogebra.common.kernel.ModeSetter;
 import org.geogebra.common.main.App;
+import org.geogebra.keyboard.web.KeyboardResources;
 import org.geogebra.web.full.css.MaterialDesignResources;
 import org.geogebra.web.html5.gui.FastClickHandler;
+import org.geogebra.web.html5.gui.inputfield.AutoCompleteTextFieldW;
+import org.geogebra.web.html5.gui.util.FormLabel;
 import org.geogebra.web.html5.gui.util.NoDragImage;
 import org.geogebra.web.html5.gui.util.StandardButton;
 import org.geogebra.web.html5.main.AppW;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -27,10 +31,17 @@ public class PDFInputDialog extends DialogBoxW implements FastClickHandler {
 	protected AppW appW;
 	private FlowPanel mainPanel;
 	private FlowPanel pdfContainerPanel;
+	private FlowPanel pdfPreviewPanel;
+	private FlowPanel pdfPageTextPanel;
 	private Label clickOrDragText;
 	private FlowPanel buttonPanel;
 	private StandardButton insertBtn;
 	private StandardButton cancelBtn;
+	private StandardButton leftBtn;
+	private StandardButton rightBtn;
+	private FormLabel pageLbl;
+	private AutoCompleteTextFieldW curPageNrField;
+	private Label ofPageLbl;
 
 	/**
 	 * @param app
@@ -76,6 +87,33 @@ public class PDFInputDialog extends DialogBoxW implements FastClickHandler {
 		setLabels();
 	}
 
+	private void buildPdfContainer() {
+		pdfContainerPanel.clear();
+		pdfPreviewPanel = new FlowPanel();
+		pdfPreviewPanel.addStyleName("pdfPreview");
+		leftBtn = createButton(pdfPreviewPanel,
+				KeyboardResources.INSTANCE.keyboard_arrowLeft_black(), 24,
+				"leftBtn");
+		// TODO add here the preview image of pdf MOW-424
+		rightBtn = createButton(pdfPreviewPanel,
+				KeyboardResources.INSTANCE.keyboard_arrowRight_black(), 24,
+				"rightBtn");
+		pdfPageTextPanel = new FlowPanel();
+		pdfPageTextPanel.addStyleName("pdfPageText");
+		pdfContainerPanel.add(pdfPreviewPanel);
+		pdfContainerPanel.add(pdfPageTextPanel);
+	}
+
+	private StandardButton createButton(FlowPanel root, ImageResource imgSource,
+			int size, String styleName) {
+		StandardButton btn = new StandardButton(
+				imgSource, null, size, size, appW);
+		btn.addStyleName(styleName);
+		btn.addFastClickHandler(this);
+		root.add(btn);
+		return btn;
+	}
+
 	private void initActions() {
 		getCaption().setText(appW.getLocalization().getMenu("pdfDialogTitle"));
 		insertBtn.addFastClickHandler(this);
@@ -85,7 +123,8 @@ public class PDFInputDialog extends DialogBoxW implements FastClickHandler {
 
 			@Override
 			public void onClick(ClickEvent event) {
-				// TODO add here code for open pdf MOW-424
+				// only for testing here
+				buildPdfContainer();
 			}
 
 		}, ClickEvent.getType());
@@ -106,7 +145,11 @@ public class PDFInputDialog extends DialogBoxW implements FastClickHandler {
 		if (source == cancelBtn) {
 			hide();
 		} else if (source == insertBtn) {
-			// TODO process page of pdf
+			// TODO process page of pdf MOW-428
+		} else if (source == leftBtn) {
+			// TODO handle left button MOW-426
+		} else if (source == rightBtn) {
+			// TODO handle left button MOW-426
 		}
 	}
 
