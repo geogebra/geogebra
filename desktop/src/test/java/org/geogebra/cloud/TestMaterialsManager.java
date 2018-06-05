55702c95f57221aeac097e92fbbc5900483f89cd
Michael Borcherds <michael@geogebra.org>
2018-06-05 16:23:22 +0000

    fix compile error
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@62272 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/test/java/org/geogebra/cloud/TestMaterialsManager.java
+++ desktop/src/test/java/org/geogebra/cloud/TestMaterialsManager.java
@@ -229,7 +229,7 @@ public class TestMaterialsManager extends MaterialsManager {
 	
 	
 	@Override
-	public void open(String url, String name, String features){
+	public void open(String url, String features) {
 		// TODO: implement this?
 	}
 	
