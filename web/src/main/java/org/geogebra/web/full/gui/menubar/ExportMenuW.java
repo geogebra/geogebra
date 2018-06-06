3d4b479d270bc1a3ce6f3ee0a3d02cc1000de5e1
Michael Borcherds <michael@geogebra.org>
2018-06-06 07:45:07 +0000

    remove debugging (NPE)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@62287 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/full/gui/menubar/ExportMenuW.java
+++ web/src/main/java/org/geogebra/web/full/gui/menubar/ExportMenuW.java
@@ -7,7 +7,6 @@ import org.geogebra.common.plugin.EventType;
 import org.geogebra.common.util.AsyncOperation;
 import org.geogebra.common.util.FileExtensions;
 import org.geogebra.common.util.StringUtil;
-import org.geogebra.common.util.debug.Log;
 import org.geogebra.web.full.gui.images.AppResources;
 import org.geogebra.web.html5.Browser;
 import org.geogebra.web.html5.euclidian.EuclidianViewWInterface;
@@ -99,8 +98,6 @@ public class ExportMenuW extends AriaMenuBar implements MenuBarI {
 
 			@Override
 			public void execute() {
-
-				Log.debug(app.getPageController().exportPDF());
 
 				menu.hide();
 				app.toggleMenu();
