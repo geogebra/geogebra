57e5ec7595d09262262a1a879b9caa0c1d34d5ad
Krisztina Frank <krisztina.frank@geogebra.org>
2018-06-21 11:58:11 +0000

    AND-1204 Hide tutorial menu when no tutorial exists for the app
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@62462 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common/src/main/java/org/geogebra/common/main/settings/AppConfigScientific.java
@@ -0,0 +1,15 @@
+package org.geogebra.common.main.settings;
+
+public class AppConfigScientific extends AppConfigGraphing {
+
+    @Override
+    public String getAppTitle() {
+        return "ScientificCalculator";
+    }
+
+    @Override
+    public String getTutorialKey() {
+        return "";
+    }
+
+}
