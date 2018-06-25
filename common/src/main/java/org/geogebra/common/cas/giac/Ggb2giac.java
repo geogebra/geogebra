8a26a4f3b5abf0d165631ca0555dbb4185dd4607
Michael Borcherds <michael@geogebra.org>
2018-06-25 11:41:42 +0000

    small fix for Eigenvectors() in web
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@62501 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
+++ common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
@@ -1863,7 +1863,9 @@ public class Ggb2giac {
 
 		p("JordanDiagonalization.1", "{jordan(%0)}");
 
-		p("Eigenvectors.1", "egv(%0)");
+		// giac.js / wasm gives "?" not "{}"
+		p("Eigenvectors.1",
+				"[[ggbenvas:=egv(%0)],when(ggbenvas==?,{},ggbenvas)][1]");
 
 		p("Eigenvalues.1", "{eigenvals(%0)}");
 
