796c599df22bc378a3a2f1dc39c0522267be69a5
Michael Borcherds <michael@geogebra.org>
2018-07-10 06:08:55 +0000

    remove changes to fix 2d line as input
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@62789 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/geogebra3D/kernel3D/commands/CmdCircle3D.java
+++ common/src/main/java/org/geogebra/common/geogebra3D/kernel3D/commands/CmdCircle3D.java
@@ -1,18 +1,14 @@
 package org.geogebra.common.geogebra3D.kernel3D.commands;
 
-import org.geogebra.common.geogebra3D.kernel3D.algos.AlgoPlaneThreePoints;
-import org.geogebra.common.geogebra3D.kernel3D.geos.GeoPoint3D;
 import org.geogebra.common.kernel.Kernel;
 import org.geogebra.common.kernel.arithmetic.Command;
 import org.geogebra.common.kernel.commands.CmdCircle;
 import org.geogebra.common.kernel.geos.GeoElement;
-import org.geogebra.common.kernel.geos.GeoLine;
 import org.geogebra.common.kernel.geos.GeoNumberValue;
 import org.geogebra.common.kernel.kernelND.GeoDirectionND;
 import org.geogebra.common.kernel.kernelND.GeoLineND;
 import org.geogebra.common.kernel.kernelND.GeoPointND;
 import org.geogebra.common.main.MyError;
-import org.geogebra.common.util.debug.Log;
 
 /**
  * Circle command
@@ -81,47 +77,6 @@ public class CmdCircle3D extends CmdCircle {
 
 		if ((ok[0] = (arg[0].isGeoPoint()))
 				&& (ok[2] = (arg[2] instanceof GeoDirectionND))) {
-
-			Log.error(arg[2].getParentAlgorithm() + "");
-
-			// x=0 is ambiguous so convert eg
-			// Circle((0,0,0), 1, x=0)
-			// to Circle((0, 0, 0), 1, Plane((0, 0, 0), (0, 0, 1), (1, 0, 0)))
-			// Circle((0, 0, 0), 1, 4x + 3y = 0)
-			// unambiguous, doesn't need converting
-			// Circle((0,0,0), 1, Line((0,0),(0,1)))
-			if (arg[2] instanceof GeoLine
-					&& arg[2].getParentAlgorithm() == null) {
-
-				GeoLine line = (GeoLine) arg[2];
-
-				double x = line.x;
-				double y = line.y;
-				double z = line.z;
-
-				GeoPoint3D pt1 = new GeoPoint3D(cons);
-				GeoPoint3D pt2 = new GeoPoint3D(cons);
-				GeoPoint3D pt3 = new GeoPoint3D(cons);
-
-				// convert eg 4x+3y=5 to vertical plane
-				// perpendicular to 4x+3y=5
-
-				// point on line
-				double x1 = -z * x / (x * x + y * y);
-				double y1 = -z * y / (x * x + y * y);
-
-				pt1.setCoords(x1, y1, 0, 1);
-				pt2.setCoords(x1, y1, 1, 1);
-				// second point on line
-				pt3.setCoords(x1 + x, y1 + y, 0, 1);
-
-				AlgoPlaneThreePoints algo = new AlgoPlaneThreePoints(cons, pt1,
-						pt2, pt3);
-
-				// converted plane
-				arg[2] = algo.getOutput(0);
-
-			}
 
 			if (arg[1] instanceof GeoNumberValue) {
 				GeoElement[] ret = { kernel.getManager3D().circle3D(
