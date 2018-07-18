a2c06bd9421acf0bd8de25a7923a995c92321916
Michael Borcherds <michael@geogebra.org>
2018-07-18 08:43:40 +0000

    quick fix for compile error
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@62978 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/editor/AppWsolver.java
+++ web/src/main/java/org/geogebra/web/editor/AppWsolver.java
@@ -79,7 +79,7 @@ public class AppWsolver extends AppW implements HasKeyboard {
 			public void onClickStart(int x, int y, PointerEventType type) {
 				String url = Location.getHref().replaceAll("\\?.*", "")
 						+ Solver.getRelativeURLforEqn(getMathField().getText());
-				ShareDialog sd = new ShareDialog(AppWsolver.this, url);
+				ShareDialog sd = new ShareDialog(AppWsolver.this, url, null);
 				sd.setVisible(true);
 				sd.center();
 			}
