9af460fddc8bfdd7e19d593b61f74b92a77c0366
Zbyněk Konečný <zbynek@geogebra.org>
2018-08-16 12:59:54 +0000

    Open File View (MOW) should listen to login/logout events
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@63484 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/full/gui/openfileview/OpenFileView.java
+++ web/src/main/java/org/geogebra/web/full/gui/openfileview/OpenFileView.java
@@ -5,11 +5,15 @@ import java.util.List;
 
 import org.geogebra.common.main.Feature;
 import org.geogebra.common.main.OpenFileListener;
+import org.geogebra.common.move.events.BaseEvent;
+import org.geogebra.common.move.ggtapi.events.LogOutEvent;
+import org.geogebra.common.move.ggtapi.events.LoginEvent;
 import org.geogebra.common.move.ggtapi.models.Chapter;
 import org.geogebra.common.move.ggtapi.models.Material;
 import org.geogebra.common.move.ggtapi.models.Material.Provider;
 import org.geogebra.common.move.ggtapi.models.MaterialRequest.Order;
 import org.geogebra.common.move.ggtapi.requests.MaterialCallbackI;
+import org.geogebra.common.move.views.EventRenderable;
 import org.geogebra.common.util.debug.Log;
 import org.geogebra.web.full.css.MaterialDesignResources;
 import org.geogebra.web.full.gui.MyHeaderPanel;
@@ -43,7 +47,7 @@ import com.google.gwt.user.client.ui.Widget;
  *
  */
 public class OpenFileView extends MyHeaderPanel
-		implements BrowseViewI, OpenFileListener {
+		implements BrowseViewI, OpenFileListener, EventRenderable {
 	/**
 	 * application
 	 */
@@ -90,6 +94,7 @@ public class OpenFileView extends MyHeaderPanel
 			this.app.initSignInEventFlow(new LoginOperationW(app),
 					!ArticleElement.isEnableUsageStats());
 		}
+		this.app.getLoginOperation().getView().add(this);
 		initGUI();
 	}
 
@@ -502,6 +507,13 @@ public class OpenFileView extends MyHeaderPanel
 					.setDisplay(visible ? Display.BLOCK : Display.NONE);
 		}
 		contentPanel.getElement().getStyle().setTop(visible ? 44 : 0, Unit.PX);
+	}
+
+	@Override
+	public void renderEvent(BaseEvent event) {
+		if (event instanceof LoginEvent || event instanceof LogOutEvent) {
+			loadAllMaterials();
+		}
 	}
 
 }
