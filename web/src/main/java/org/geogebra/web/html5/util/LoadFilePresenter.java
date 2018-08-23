geogebra/geogebra

commit ddbbfa9729669c4add005be847d1d6e3cc5f4c8e

NPE fix, simplify CSS
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@63492 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/main/java/org/geogebra/web/html5/util/LoadFilePresenter.java
@@ -205,7 +205,8 @@ public class LoadFilePresenter {
 	protected void deferredOpenEmpty(AppW app) {
 		String perspective = defaultPerspective(app);
 		finishEmptyLoading(app,
-				perspective == null ? null : getPerspective(app, perspective));
+				perspective.length() > 0 ? null
+						: getPerspective(app, perspective));
 	}
 
 	private static String defaultPerspective(AppW app) {
@@ -218,7 +219,7 @@ public class LoadFilePresenter {
 		} else if (app.isWhiteboardActive()) {
 			return Perspective.WHITEBOARD + "";
 		}
-		return null;
+		return "";
 	}
 
 	private static Perspective getPerspective(AppW app, String perspective) {

web/src/main/java/org/geogebra/web/full/gui/openfileview/OpenFileView.java
@@ -270,6 +270,7 @@ public class OpenFileView extends MyHeaderPanel
 	}
 
 	private void showEmptyListNotification() {
+		contentPanel.clear();
 		infoPanel = new FlowPanel();
 		infoPanel.setStyleName("emptyMaterialListInfo");
 		Image image = new NoDragImage(
@@ -513,7 +514,7 @@ public class OpenFileView extends MyHeaderPanel
 	@Override
 	public void renderEvent(BaseEvent event) {
 		if (event instanceof LoginEvent || event instanceof LogOutEvent) {
-			loadAllMaterials();
+			updateMaterials();
 		}
 	}
 

web/src/main/resources/org/geogebra/web/resources/html/splash-style.css
@@ -75,13 +75,9 @@ border-spacing:0;
     z-index: 501;
     /* box-shadow: 1px 1px 6px 1px #666666; */
     background-color: rgba(255, 255, 255, 0.9);
-    border-radius: 10px;
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
-    border-bottom-right-radius: 10px;
-    border-bottom-left-radius: 10px;
-    -moz-border-radius: 10px;
-    -webkit-border-radius: 10px;
+    border-radius: 2px;
+    -moz-border-radius: 2px;
+    -webkit-border-radius: 2px;
     box-shadow: 0px 0px 2px 1px rgba(102, 102, 102, 0.3);
     padding: 15px;
 }
@@ -154,15 +150,7 @@ margin: 0px 5px 0px 0px;
 }
 
 .GeoGebraFrame .toolbarPanel .toolBPanel .toolbar_button {
-    -webkit-border-radius: 5px;
-    -khtml-border-radius: 5px;
-    -moz-border-radius: 5px;
-    border-radius: 5px;
-    -webkit-border-radius: 5px;
-    -khtml-border-radius: 5px;
-    -moz-border-radius: 5px;
-    border-radius: 5px;
-    -webkit-border-radius: 5px;
+     -webkit-border-radius: 5px;
     -khtml-border-radius: 5px;
     -moz-border-radius: 5px;
     border-radius: 5px;

commit fb39dc6c53acdbd1cb7a4d883e2dd562c287fd66

MOW: Fix for New Note (broken in r63492)
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@63624 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

web/src/main/java/org/geogebra/web/html5/util/LoadFilePresenter.java
@@ -204,9 +204,8 @@ public class LoadFilePresenter {
 	 */
 	protected void deferredOpenEmpty(AppW app) {
 		String perspective = defaultPerspective(app);
-		finishEmptyLoading(app,
-				perspective.length() > 0 ? null
-						: getPerspective(app, perspective));
+		finishEmptyLoading(app, StringUtil.empty(perspective)
+				? getPerspective(app, perspective) : null);
 	}
 
 	private static String defaultPerspective(AppW app) {