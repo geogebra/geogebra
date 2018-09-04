96c93d8ef0183faa4124ca97b673ce87ac242f87
Jan Borovsky <jan.borovsky@geogebra.org>
2018-09-04 07:38:58 +0000

    MOB-1778: Translation Matrix for AR set.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@63875 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/geogebra3D/euclidian3D/EuclidianView3D.java
+++ common/src/main/java/org/geogebra/common/geogebra3D/euclidian3D/EuclidianView3D.java
@@ -871,13 +871,20 @@ public abstract class EuclidianView3D extends EuclidianView
 	 * Update translation matrix.
 	 */
 	public void updateTranslationMatrix() {
-		translationMatrixWithScale.set(1, 4, getXZero() * getXscale());
-		translationMatrixWithScale.set(2, 4, getYZero() * getYscale());
-		translationMatrixWithScale.set(3, 4, getZZero() * getZscale());
-		translationMatrixWithoutScale.set(1, 4, getXZero());
-		translationMatrixWithoutScale.set(2, 4, getYZero());
-		translationMatrixWithoutScale.set(3, 4, getZZero());
-
+		if (mIsAREnabled){
+			translationMatrixWithScale = CoordMatrix4x4.IDENTITY;
+			translationMatrixWithoutScale = CoordMatrix4x4.IDENTITY;
+			translationMatrixWithScale.set(3, 4, (-getZmin()) * getZscale());
+			translationMatrixWithoutScale.set(3, 4, -getZmin()* getZscale());
+		}
+		else {
+			translationMatrixWithScale.set(1, 4, getXZero() * getXscale());
+			translationMatrixWithScale.set(2, 4, getYZero() * getYscale());
+			translationMatrixWithScale.set(3, 4, getZZero() * getZscale());
+			translationMatrixWithoutScale.set(1, 4, getXZero());
+			translationMatrixWithoutScale.set(2, 4, getYZero());
+			translationMatrixWithoutScale.set(3, 4, getZZero());
+		}
 	}
 
 	/**
