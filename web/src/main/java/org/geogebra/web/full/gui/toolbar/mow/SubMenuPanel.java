932f981335310a46195bad40f90855764635fbb6
Csilla Solyom-Gecse <solyom_csilla@yahoo.com>
2018-09-21 09:25:02 +0000

    MOW-614 + MOW-615 update button width
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@64383 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/full/gui/toolbar/mow/SubMenuPanel.java
+++ web/src/main/java/org/geogebra/web/full/gui/toolbar/mow/SubMenuPanel.java
@@ -43,7 +43,7 @@ public abstract class SubMenuPanel extends FlowPanel
 	 * group panel
 	 */
 	protected static class GroupPanel extends FlowPanel {
-		private static final int BUTTON_WIDTH = 64;
+		private static final int BUTTON_WIDTH = 104;
 
 		/**
 		 * constructor
