3d4cad27184130852cd05559327b4cb1373b8ff5
Barna Karacsony <barna.karacsony@geogebra.org>
2018-09-26 15:27:17 +0000

    MOB-1783, MOB-1784, MOB-1786: SD, SampleSD and MAD cases removed from the switch of the Localization:getMainCommandName method
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@64546 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/Localization.java
+++ common/src/main/java/org/geogebra/common/main/Localization.java
@@ -1457,15 +1457,6 @@ public abstract class Localization {
 			case Binomial:
 			case nCr:
 				return Commands.nCr.name();
-			case SD:
-			case stdev:
-				return Commands.SD.name();
-			case SampleSD:
-			case stdevp:
-				return Commands.SampleSD.name();
-			case MAD:
-			case mad:
-				return Commands.MAD.name();
 			default:
 				return null;
 		}
