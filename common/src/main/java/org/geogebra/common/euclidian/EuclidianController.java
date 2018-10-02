ef4c7a232583e42a4ef27450dcd98f2fee72807c
Judit Elias <eliasjudit@gmail.com>
2018-10-02 13:22:17 +0000

    MOW-611 reverted change
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@64703 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/euclidian/EuclidianController.java
+++ common/src/main/java/org/geogebra/common/euclidian/EuclidianController.java
@@ -8114,7 +8114,7 @@ public abstract class EuclidianController implements SpecialPointsListener {
 					newMinY = newMinY
 							- (bounds.getHeight() - (newMaxY - newMinY));
 				}
-				if (bounds != null && bounds.getMinX() + bounds.getWidth() <= bbMinX + bbWidth) {
+				if (bounds != null) {
 					// calculate the difference between the new and old
 					// positions (minX) and then apply translate
 					double dx = thresholdXReached ? 0
