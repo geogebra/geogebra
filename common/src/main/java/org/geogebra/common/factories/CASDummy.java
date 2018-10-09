c491d5af561f4f63519cf6e885a4744c5152eaaa
Michael Borcherds <michael@geogebra.org>
2018-10-09 11:52:15 +0000

    MOB-1842 Dummy CAS
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@64818 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common/src/main/java/org/geogebra/common/factories/CASDummy.java
@@ -0,0 +1,33 @@
+package org.geogebra.common.factories;
+
+import org.geogebra.common.cas.CASparser;
+import org.geogebra.common.cas.giac.CASgiac;
+import org.geogebra.common.kernel.CASGenericInterface;
+
+public class CASDummy extends CASgiac implements CASGenericInterface {
+
+	public CASDummy(CASparser casParser) {
+		super(casParser);
+	}
+
+	public void clearResult() {
+		//
+	}
+
+	public boolean externalCAS() {
+		return false;
+	}
+
+	@Override
+	public String evaluateCAS(String exp) {
+		return "?";
+	}
+
+	@Override
+	protected String evaluate(String exp, long timeoutMilliseconds)
+			throws Throwable {
+		return "?";
+	}
+
+
+}
---
+++ common/src/main/java/org/geogebra/common/factories/CASFactoryDummy.java
@@ -0,0 +1,14 @@
+package org.geogebra.common.factories;
+
+import org.geogebra.common.cas.CASparser;
+import org.geogebra.common.kernel.CASGenericInterface;
+import org.geogebra.common.kernel.Kernel;
+
+public class CASFactoryDummy extends CASFactory {
+
+	@Override
+	public CASGenericInterface newGiac(CASparser p, Kernel k) {
+		return new CASDummy(p);
+	}
+
+}
