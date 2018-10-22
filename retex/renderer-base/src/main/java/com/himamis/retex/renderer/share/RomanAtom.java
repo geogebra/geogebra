7164ea31c8cba3b9ba689c0a5431023835eed211
Michael Borcherds <michael@geogebra.org>
2018-10-22 18:15:10 +0000

    GGB-2475 use correct version of RomanAtom
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@65083 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- retex/renderer-base/src/main/java/com/himamis/retex/renderer/share/RomanAtom.java
+++ retex/renderer-base/src/main/java/com/himamis/retex/renderer/share/RomanAtom.java
@@ -24,23 +24,23 @@
  * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA.
  *
- * Linking this library statically or dynamically with other modules 
- * is making a combined work based on this library. Thus, the terms 
- * and conditions of the GNU General Public License cover the whole 
+ * Linking this library statically or dynamically with other modules
+ * is making a combined work based on this library. Thus, the terms
+ * and conditions of the GNU General Public License cover the whole
  * combination.
- * 
- * As a special exception, the copyright holders of this library give you 
- * permission to link this library with independent modules to produce 
- * an executable, regardless of the license terms of these independent 
- * modules, and to copy and distribute the resulting executable under terms 
- * of your choice, provided that you also meet, for each linked independent 
- * module, the terms and conditions of the license of that module. 
- * An independent module is a module which is not derived from or based 
- * on this library. If you modify this library, you may extend this exception 
- * to your version of the library, but you are not obliged to do so. 
- * If you do not wish to do so, delete this exception statement from your 
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce
+ * an executable, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting executable under terms
+ * of your choice, provided that you also meet, for each linked independent
+ * module, the terms and conditions of the license of that module.
+ * An independent module is a module which is not derived from or based
+ * on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obliged to do so.
+ * If you do not wish to do so, delete this exception statement from your
  * version.
- * 
+ *
  */
 
 package com.himamis.retex.renderer.share;
@@ -52,31 +52,37 @@ public class RomanAtom extends Atom {
 
 	protected Atom base;
 
-	@Override
-	final public Atom duplicate() {
-		return setFields(new RomanAtom(base));
-	}
-
 	public RomanAtom(Atom base) {
 		this.base = base;
 	}
 
+	public Atom getTrueBase() {
+		return base;
+	}
+
 	@Override
-	public Box createBox(TeXEnvironment env0) {
+	public Box createBox(TeXEnvironment env) {
 		Box box;
 		if (base != null) {
-			TeXEnvironment env = env0.copy(env0.getTeXFont().copy());
-			env.getTeXFont().setRoman(true);
-			box = base.createBox(env);
+			final TeXFont tf = env.getTeXFont();
+			final boolean rm = tf.getRoman();
+			if (rm) {
+				box = base.createBox(env);
+			} else {
+				tf.setRoman(true);
+				box = base.createBox(env);
+				tf.setRoman(false);
+			}
 		} else {
-			box = new StrutBox(0, 0, 0, 0);
+			box = StrutBox.getEmpty();
 		}
 
 		return box;
 	}
 
 	@Override
-	public Atom getBase() {
-		return base;
+	final public Atom duplicate() {
+		return setFields(new RomanAtom(base));
 	}
+
 }
