3eb9317a394d7fc7f1f7954aad7e056784a986f8
Csilla Solyom-Gecse <solyom_csilla@yahoo.com>
2018-10-25 07:28:19 +0000

    APPS-56 add input text field component + build TV dialog
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@65134 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/main/java/org/geogebra/web/full/gui/components/ComponentInputField.java
@@ -0,0 +1,102 @@
+package org.geogebra.web.full.gui.components;
+
+import org.geogebra.common.gui.SetLabels;
+import org.geogebra.web.full.gui.view.algebra.InputPanelW;
+import org.geogebra.web.html5.gui.util.FormLabel;
+import org.geogebra.web.html5.main.AppW;
+
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+
+/**
+ * @author csilla
+ * 
+ *         input field material design component
+ *
+ */
+public class ComponentInputField extends FlowPanel implements SetLabels {
+	private AppW appW;
+	private String errorTxt;
+	private String labelTxt;
+	private FlowPanel contentPanel;
+	private FormLabel labelText;
+	private InputPanelW inputTextField;
+	private Label errorLabel;
+
+	/**
+	 * @param app
+	 *            see {@link AppW}
+	 * @param placeholder
+	 *            placeholder text (can be null)
+	 * @param labelTxt
+	 *            label of input field
+	 * @param errorTxt
+	 *            error label of input field
+	 * @param defaultValue
+	 *            default text of input text field
+	 */
+	public ComponentInputField(AppW app, String placeholder, String labelTxt,
+			String errorTxt, String defaultValue) {
+		this.appW = app;
+		this.labelTxt = labelTxt;
+		this.errorTxt = errorTxt;
+		buildGui(placeholder);
+		if (defaultValue != null && !defaultValue.isEmpty()) {
+			setInputText(defaultValue);
+		}
+	}
+
+	private void buildGui(String placeholder) {
+		contentPanel = new FlowPanel();
+		contentPanel.setStyleName("inputTextField");
+		// input text field
+		inputTextField = new InputPanelW("", appW, 1, 25, false);
+		inputTextField.addStyleName("textField");
+		// label of text field
+		labelText = new FormLabel().setFor(inputTextField.getTextComponent());
+		labelText.addStyleName("inputLabel");
+		// placeholder if there is any
+		if (placeholder != null && !placeholder.isEmpty()) {
+			inputTextField.getTextComponent().getTextBox().getElement()
+				.setAttribute("placeholder",
+						appW.getLocalization().getMenu(placeholder));
+		}
+		// build component
+		contentPanel.add(labelText);
+		contentPanel.add(inputTextField);
+		// add error label if there is any
+		if (errorTxt != null && !errorTxt.isEmpty()) {
+			addErrorLabel(contentPanel);
+		}
+		add(contentPanel);
+		setLabels();
+	}
+
+	private void addErrorLabel(FlowPanel root) {
+		errorLabel = new Label();
+		errorLabel.addStyleName("errorLabel");
+		root.add(errorLabel);
+	}
+
+	public void setLabels() {
+		labelText.setText(appW.getLocalization().getMenu(labelTxt));
+		if (errorLabel != null) {
+			errorLabel.setText(appW.getLocalization().getMenu(errorTxt));
+		}
+	}
+
+	/**
+	 * @return value of input text field
+	 */
+	public String getInputText() {
+		return inputTextField.getText();
+	}
+
+	/**
+	 * @param text
+	 *            should appear in the input text field
+	 */
+	public void setInputText(String text) {
+		inputTextField.getTextComponent().setText(text);
+	}
+}
--- web/src/main/java/org/geogebra/web/full/gui/dialog/InputDialogTableView.java
+++ web/src/main/java/org/geogebra/web/full/gui/dialog/InputDialogTableView.java
@@ -1,19 +1,29 @@
 package org.geogebra.web.full.gui.dialog;
 
 import org.geogebra.common.gui.SetLabels;
+import org.geogebra.web.full.gui.components.ComponentInputField;
+import org.geogebra.web.html5.gui.FastClickHandler;
 import org.geogebra.web.html5.gui.util.StandardButton;
 import org.geogebra.web.html5.main.AppW;
 import org.geogebra.web.shared.DialogBoxW;
 
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author csilla
+ * 
+ *         dialog opened from av context menu of functions and lines by clicking
+ *         on Table of values
  *
  */
-public class InputDialogTableView extends DialogBoxW implements SetLabels {
+public class InputDialogTableView extends DialogBoxW
+		implements SetLabels, FastClickHandler {
 	private AppW appW;
 	private FlowPanel contentPanel;
+	private ComponentInputField startValue;
+	private ComponentInputField endValue;
+	private ComponentInputField step;
 	private FlowPanel buttonPanel;
 	private StandardButton cancelBtn;
 	private StandardButton okBtn;
@@ -31,13 +41,32 @@ public class InputDialogTableView extends DialogBoxW implements SetLabels {
 	private void buildGui() {
 		addStyleName("tableOfValuesDialog");
 		contentPanel = new FlowPanel();
+		buildTextFiledPanel();
+		buildButtonPanel(contentPanel);
+		this.add(contentPanel);
+		setLabels();
+	}
+
+	private void buildTextFiledPanel() {
+		startValue = addTextField("Start value for x", "-2");
+		endValue = addTextField("End value for x", "2");
+		step = addTextField("Step", "1");
+	}
+
+	private ComponentInputField addTextField(String labelText, String defaultValue) {
+		ComponentInputField field = new ComponentInputField(appW, null,
+				labelText,
+				null, defaultValue);
+		contentPanel.add(field);
+		return field;
+	}
+
+	private void buildButtonPanel(FlowPanel root) {
 		buttonPanel = new FlowPanel();
 		buttonPanel.setStyleName("DialogButtonPanel");
 		cancelBtn = createTxtButton(buttonPanel, "cancelBtn", true);
-		okBtn = createTxtButton(buttonPanel, "okBtn", false);
-		contentPanel.add(buttonPanel);
-		this.add(contentPanel);
-		setLabels();
+		okBtn = createTxtButton(buttonPanel, "okBtn", true);
+		root.add(buttonPanel);
 	}
 
 	private StandardButton createTxtButton(FlowPanel root, String styleName,
@@ -45,12 +74,16 @@ public class InputDialogTableView extends DialogBoxW implements SetLabels {
 		StandardButton btn = new StandardButton("", appW);
 		btn.addStyleName(styleName);
 		btn.setEnabled(isEnabled);
+		btn.addFastClickHandler(this);
 		root.add(btn);
 		return btn;
 	}
 
 	public void setLabels() {
 		getCaption().setText(appW.getLocalization().getMenu("TableOfValues"));
+		startValue.setLabels();
+		endValue.setLabels();
+		step.setLabels();
 		cancelBtn.setText(appW.getLocalization().getMenu("Cancel"));
 		okBtn.setText(appW.getLocalization().getMenu("OK"));
 	}
@@ -59,5 +92,14 @@ public class InputDialogTableView extends DialogBoxW implements SetLabels {
 	public void show() {
 		super.show();
 		super.center();
+	}
+
+	public void onClick(Widget source) {
+		if (source == cancelBtn) {
+			hide();
+		} else if (source == okBtn) {
+			// TODO process input
+			hide();
+		}
 	}
 }
