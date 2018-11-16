72860a29764327cf9ea71b50b9d4f3493bd21b68
Zbyněk Konečný <zbynek@geogebra.org>
2018-11-16 08:23:04 +0000

    Disable assertions in test
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@65474 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/test/java/org/geogebra/web/full/gui/view/algebra/AVDescriptionTest.java
+++ web/src/test/java/org/geogebra/web/full/gui/view/algebra/AVDescriptionTest.java
@@ -4,6 +4,7 @@ import org.geogebra.web.full.main.AppWFull;
 import org.geogebra.web.html5.main.TestArticleElement;
 import org.geogebra.web.main.MockApp;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
