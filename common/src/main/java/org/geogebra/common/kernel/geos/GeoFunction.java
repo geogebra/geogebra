geogebra/geogebra

commit a2b97eaf43f186b2700984d1342e8d3018032e86

APPS-40: editing the freehand function doesn't crash anymore
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@65559 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/main/java/org/geogebra/common/kernel/geos/GeoFunction.java
@@ -904,13 +904,6 @@ public class GeoFunction extends GeoElement implements VarString, Translateable,
 
 	@Override
 	public String toValueString(StringTemplate tpl) {
-		// make sure Freehand Functions have different entries in drop-down
-		// lists
-		if (this.isFreehandFunction()) {
-			return this.getLabel(tpl) + tpl.leftBracket()
-					+ fun.getFunctionVariable() + tpl.rightBracket();
-		}
-
 		if (fun != null && isDefined()) {
 			return fun.toValueString(tpl);
 		}

common/src/main/java/org/geogebra/common/kernel/algos/AlgoFunctionFreehand.java
@@ -128,6 +128,15 @@ public class AlgoFunctionFreehand extends AlgoElement {
 						tpl));
 	}
 
+	@Override
+	public String getDefinition(StringTemplate tpl) {
+		if (tpl == StringTemplate.editorTemplate) {
+			return g.toValueString(tpl);
+		} else {
+			return super.getDefinition(tpl);
+		}
+	}
+
 	/**
 	 * @return input list
 	 */

common/src/main/java/org/geogebra/common/kernel/commands/AlgebraProcessor.java
@@ -1853,9 +1853,13 @@ public class AlgebraProcessor {
 			ret = doProcessValidExpression(ve, info);
 
 			if (ret == null) { // eg (1,2,3) running in 2D
-				Log.warn("Unhandled ValidExpression : " + ve);
-				throw new MyError(loc,
-						loc.getError("InvalidInput") + ":\n" + ve);
+				if (isFreehandFunction(ve)) {
+					return null;
+				} else {
+					Log.warn("Unhandled ValidExpression : " + ve);
+					throw new MyError(loc,
+							loc.getError("InvalidInput") + ":\n" + ve);
+				}
 			}
 		} finally {
 			cons.setSuppressLabelCreation(oldMacroMode);
@@ -1864,6 +1868,22 @@ public class AlgebraProcessor {
 		processReplace(replaceable, ret, ve, info);
 
 		return ret;
+	}
+
+	private boolean isFreehandFunction(ValidExpression expression) {
+		if (expression instanceof ExpressionNode) {
+			ExpressionNode expressionNode = (ExpressionNode) expression;
+			if (expressionNode.isLeaf()) {
+				ExpressionValue expressionValue = expressionNode.getLeft();
+				if (expressionValue instanceof Command) {
+					Command command = (Command) expressionValue;
+					if (command.getName().equals(loc.getFunction("freehand"))) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
 	}
 
 	/**

common/src/main/java/org/geogebra/common/kernel/commands/CommandDispatcher.java
@@ -180,9 +180,13 @@ public abstract class CommandDispatcher {
 	private GeoElement[] process(CommandProcessor cmdProc, Command c,
 			EvalInfo info) {
 		if (cmdProc == null) {
-			throw new MyError(app.getLocalization(),
-					app.getLocalization().getError("UnknownCommand") + " : "
-							+ app.getLocalization().getCommand(c.getName()));
+			if (c.getName().equals(app.getLocalization().getFunction("freehand"))) {
+				return null;
+			} else {
+				throw new MyError(app.getLocalization(),
+						app.getLocalization().getError("UnknownCommand") + " : "
+								+ app.getLocalization().getCommand(c.getName()));
+			}
 		}
 
 		// switch on macro mode to avoid labeling of output if desired

commit fbf2ddf51e2fbe96150537ef30841d36c3b0d936

Add different LaTeX labels to dropdowns (broken implementation was removed in r65559 for APPS-40)
git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@65568 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

common/src/main/java/org/geogebra/common/kernel/geos/GeoFunction.java
@@ -938,6 +938,11 @@ public class GeoFunction extends GeoElement implements VarString, Translateable,
 
 	@Override
 	public String toLaTeXString(boolean symbolic, StringTemplate tpl) {
+		// make sure Freehand Functions have different entries in drop-down
+		// lists
+		if (isFreehandFunction()) {
+			return getAssignmentLHS(tpl);
+		}
 		if (fun != null && isDefined()) {
 			return fun.toLaTeXString(symbolic, tpl);
 		}