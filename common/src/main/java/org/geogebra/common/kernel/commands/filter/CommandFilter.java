0a39df7878312a25c25eb45e70a12542eb3a6e44
Barna Karacsony <barna.karacsony@geogebra.org>
2018-11-23 11:33:18 +0000

    APPS-180: adding comments for the CommandFilter interface
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@65596 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/kernel/commands/filter/CommandFilter.java
+++ common/src/main/java/org/geogebra/common/kernel/commands/filter/CommandFilter.java
@@ -3,6 +3,18 @@ package org.geogebra.common.kernel.commands.filter;
 import org.geogebra.common.kernel.arithmetic.Command;
 import org.geogebra.common.kernel.commands.CommandProcessor;
 
+/**
+ * Filters out commands that are not allowed
+ */
 public interface CommandFilter {
+
+    /**
+     * @param command
+     *          the command that should be allowed or not
+     * @param commandProcessor
+     *          makes it possible to check the argument list of the command
+     * @return
+     *          true if the command is allowed otherwise false
+     */
     boolean isAllowed(Command command, CommandProcessor commandProcessor);
 }
