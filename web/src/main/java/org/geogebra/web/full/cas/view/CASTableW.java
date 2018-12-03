c34aab5d171a3156929dcd361d2830e1b1c12a6f
Zbyněk Konečný <zbynek@geogebra.org>
2018-12-03 15:42:49 +0000

    Remove unused JLM/MathQuill abstraction layer
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@65742 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/full/cas/view/CASTableW.java
+++ web/src/main/java/org/geogebra/web/full/cas/view/CASTableW.java
@@ -11,7 +11,6 @@ import org.geogebra.common.kernel.geos.GeoCasCell;
 import org.geogebra.common.kernel.geos.GeoNumeric;
 import org.geogebra.common.main.App;
 import org.geogebra.common.util.debug.Log;
-import org.geogebra.web.full.util.ReTeXHelper;
 import org.geogebra.web.html5.gui.util.ClickStartHandler;
 import org.geogebra.web.html5.main.AppW;
 
@@ -232,8 +231,7 @@ public class CASTableW extends Grid implements CASTable {
 	@Override
 	public CASTableCellEditor getEditor() {
 		if (editor == null) {
-			editor = new ReTeXHelper()
-					.getCASEditor(app, ml);
+			editor = new CASLaTeXEditor(app, ml);
 		}
 		return editor;
 	}
