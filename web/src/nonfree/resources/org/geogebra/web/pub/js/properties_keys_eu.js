b70eca00a7d7dfa40667fe07815bf06a01cceac9
Automatic update by the ggbtrans subsystem <geogebra-dev@googlegroups.com>
2018-12-19 19:23:10 +0000

    properties files and related resources automatically updated from ggbtrans
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@66060 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/nonfree/resources/org/geogebra/web/pub/js/properties_keys_eu.js
+++ web/src/nonfree/resources/org/geogebra/web/pub/js/properties_keys_eu.js
@@ -1698,7 +1698,7 @@ __GGB__keysVar["eu"].menu = {
     "DropDownEmptyItemSelected": "Item hutsa aukeratu da. Goitiberako menua itxi egin da.",
     "DropDownItemSelected": "%0 Itema aukeratu da. Goitiberako menu itxi egin da.",
     "DropDownOpened": "Goitiberako %0 ireki da.",
-    "DropDownSelected": "Goitibeherako %0 menua aukeratua.",
+    "DropDownSelected": "Goitibeherako %0 menua aukeratuta.",
     "Duplicate": "Bikoiztu",
     "Duration": "Iraupena",
     "DynamicColors": "Kolore Dinamikoak",
