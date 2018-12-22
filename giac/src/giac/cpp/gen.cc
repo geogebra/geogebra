cf6768ec155db876de9473458ff373321fd9b37e
Bernard Parisse <bernard.parisse@ujf-grenoble.fr>
2018-12-22 07:29:45 +0000

    [giac] fix for abs((-1/2)^(-1/2))
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@66083 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- giac/src/giac/cpp/gen.cc
+++ giac/src/giac/cpp/gen.cc
@@ -3861,10 +3861,9 @@ namespace giac {
       if (u==at_ln)
 	return real_abs(s,contextptr);
       if (!has_i(s)){
-	if (u==at_exp || u==at_sqrt || (u==at_pow && f[1]==plus_one_half))
+	if (u==at_exp || ( (u==at_sqrt && is_positive(f,contextptr)) || (u==at_pow && f[1]==plus_one_half && is_positive(f[0],contextptr)) ) )
 	  return s;
-	if (calc_mode(contextptr)==1 && u==at_pow && f.type==_VECT && f._VECTptr->size()==2 && f._VECTptr->back()==plus_one_half)
-	  return s;
+	// if (calc_mode(contextptr)==1 && u==at_pow && f.type==_VECT && f._VECTptr->size()==2 && f._VECTptr->back()==plus_one_half && is_positive(f[0],contextptr)) return s;
       }
     }
     else {
