484bcb31ffe88edc603e55773869e995f2b101bf
Mathieu Blossier <mathieublossier@free.fr>
2019-01-02 19:07:44 +0000

    Javadoc
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@66193 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/SchedulerFactory.java
+++ common/src/main/java/org/geogebra/common/main/SchedulerFactory.java
@@ -1,9 +1,29 @@
 package org.geogebra.common.main;
 
+/**
+ * factory for creating schedulers
+ *
+ */
 abstract public class SchedulerFactory {
 
+	/**
+	 * scheduler interface
+	 *
+	 */
     public interface Scheduler {
+		/**
+		 * schedule a runnable to run after a delay
+		 * 
+		 * @param runnable
+		 *            runnable
+		 * @param delay
+		 *            delay
+		 */
         void schedule(Runnable runnable, int delay);
+
+		/**
+		 * cancel the scheduled action
+		 */
         void cancel();
     }
 
@@ -25,6 +45,10 @@ abstract public class SchedulerFactory {
         return prototype;
     }
 
+	/**
+	 * 
+	 * @return a new scheduler
+	 */
     abstract public Scheduler createScheduler();
 
 }
