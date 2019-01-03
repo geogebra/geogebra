e6f9d4b9b67164aedaf58a0b2fed734f7d1cc098
Zoltán Kovács <zoltan@geogebra.org>
2019-01-03 16:24:50 +0000

    Add testing options for Node and Wasm
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@66206 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- test/scripts/benchmark/art-plotter/db.sql
+++ test/scripts/benchmark/art-plotter/db.sql
@@ -4,6 +4,7 @@ create table platforms (
 
 insert into platforms (name, info) values ('desktop', 'Java native');
 insert into platforms (name, info) values ('web', 'JavaScript');
+insert into platforms (name, info) values ('desktop6', 'JavaScript via Electron');
 
 create table outsourcings (
  name text,
@@ -21,6 +22,8 @@ create table provers (
 insert into provers (name, outsourcing, platform) values ('DesktopInternal', '', 'desktop');
 insert into provers (name, outsourcing, platform) values ('DesktopGrobcov', 'SingularWS', 'desktop');
 insert into provers (name, outsourcing, platform) values ('Web', '', 'web');
+insert into provers (name, outsourcing, platform) values ('Node', '', 'desktop6');
+insert into provers (name, outsourcing, platform) values ('Wasm', '', 'desktop6');
 
 -- Using Jenkins' environment settings, assuming SVN being used,
 -- see https://wiki.jenkins-ci.org/display/JENKINS/Building+a+software+project
