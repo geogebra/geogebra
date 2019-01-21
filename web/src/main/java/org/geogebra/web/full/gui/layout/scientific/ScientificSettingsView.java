4f299fd97c3d8a9cfb9cba134c528dc0f3db19d2
Zbyněk Konečný <zbynek@geogebra.org>
2019-01-21 22:19:38 +0000

    remove unused imports
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@66491 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/full/gui/layout/scientific/ScientificSettingsView.java
+++ web/src/main/java/org/geogebra/web/full/gui/layout/scientific/ScientificSettingsView.java
@@ -6,9 +6,7 @@ import org.geogebra.web.full.gui.MyHeaderPanel;
 import org.geogebra.web.html5.gui.FastClickHandler;
 import org.geogebra.web.html5.gui.view.button.StandardButton;
 import org.geogebra.web.html5.main.AppW;
-import org.geogebra.web.html5.util.Dom;
 
-import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
