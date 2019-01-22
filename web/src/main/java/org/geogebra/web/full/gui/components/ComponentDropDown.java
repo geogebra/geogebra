8a8098a18b8f1a210d22f4f7981476f080711549
Laszlo Gal <laszlo@geogebra.org>
2019-01-22 09:34:25 +0000

    APPS-267 code cleaning
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@66494 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/full/gui/components/ComponentDropDown.java
+++ web/src/main/java/org/geogebra/web/full/gui/components/ComponentDropDown.java
@@ -14,6 +14,7 @@ import org.geogebra.web.html5.main.AppW;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author kriszta
@@ -23,6 +24,9 @@ import com.google.gwt.user.client.ui.Label;
  */
 
 public class ComponentDropDown extends FlowPanel {
+
+	// for checking item position. It is 0 in normal case.
+	private static final int POPUP_X_OFFSET = 32;
 
     private Label titleLabel;
     private Label selectedOptionLabel;
@@ -75,7 +79,7 @@ public class ComponentDropDown extends FlowPanel {
 
             @Override
             public void onClickStart(int x, int y, PointerEventType type) {
-				openAtSelectedItem(view);
+				openAt(view, titleLabel);
             }
         });
     }
@@ -84,36 +88,44 @@ public class ComponentDropDown extends FlowPanel {
 	 * Opens DropDown at the top of the widget positioning selected item at the
 	 * center.
 	 */
-	void openAtSelectedItem(EuclidianView view) {
-		int anchorTop = titleLabel.getAbsoluteTop();
-		int anchorLeft = titleLabel.getAbsoluteLeft() + 32;
+	void openAt(EuclidianView view, Widget anchor) {
 		int itemTop = (getSelectedIndex() + 1) * getItemHeight();
-		if (itemTop < anchorTop) {
-			// everything fits fine, no scrollbar
-			dropDownMenu.showAtPoint(anchorLeft, anchorTop - itemTop);
-		} else {
 
-			int allHeight = getAllItemsHeight();
-			int popupHeight = view.getHeight() / 2;
-			int h2 = popupHeight / 2;
-			int scrollTop = itemTop - h2;
-			int diff = allHeight - itemTop;
-			if (diff < h2) {
-				if (diff + h2 < anchorTop) {
-					// many items, but there is space to go up;
-					dropDownMenu.showAtPoint(anchorLeft, anchorTop - h2 - diff);
-				} else {
-					dropDownMenu.showAtPoint(anchorLeft, 0);
-					// no space: put at 0 and scroll
-					// what if the popup is longer than anchortop??
-					setVerticalScrollPosition(itemTop);
-				}
-			} else {
-				// center popup and scroll;
-				dropDownMenu.showAtPoint(anchorLeft, anchorTop - h2);
-				setVerticalScrollPosition(scrollTop);
-			}
+		if (itemTop < anchor.getAbsoluteTop()) {
+			// everything fits fine, no scrollbar
+			dropDownMenu.showAtPoint(anchor.getAbsoluteLeft(),
+					(anchor.getAbsoluteTop() - itemTop) + POPUP_X_OFFSET);
+		} else {
+			openScrollablePopup(anchor, itemTop, view.getHeight() / 2);
 		}
+	}
+
+	private void openScrollablePopup(Widget anchor, int itemTop, int popupHeight) {
+		int anchorTop = anchor.getAbsoluteTop();
+		int anchorLeft = anchor.getAbsoluteLeft() + POPUP_X_OFFSET;
+		int allHeight = getAllItemsHeight();
+		int h2 = popupHeight / 2;
+		int scrollTop = itemTop - h2;
+		int diff = allHeight - itemTop;
+		if (diff < h2) {
+			if (diff + h2 < anchorTop) {
+				// many items, but there is space to go up;
+				dropDownMenu.showAtPoint(anchorLeft, anchorTop - h2 - diff);
+			} else {
+				// no space: put at 0 and scroll
+				// what if the popup is longer than anchortop??
+				openAndScrollTo(anchorLeft, 0, itemTop);
+			}
+		} else {
+			// center popup and scroll;
+			openAndScrollTo(anchorLeft, anchorTop - h2, scrollTop);
+		}
+
+	}
+
+	private void openAndScrollTo(int left, int top, int position) {
+		dropDownMenu.showAtPoint(left, top);
+		setVerticalScrollPosition(position);
 	}
 
 	private void setVerticalScrollPosition(int pos) {
