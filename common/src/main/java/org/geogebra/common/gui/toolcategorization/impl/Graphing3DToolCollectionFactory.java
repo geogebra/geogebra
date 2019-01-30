2555e4cb3ca27f321cfc7560bfedb32a02f21dbc
Mathieu Blossier <mathieublossier@free.fr>
2019-01-30 17:16:04 +0000

    G3D-197: advanced level toolbar for 3D app fills categories from scratch (except basic)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@66683 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/gui/toolcategorization/impl/Graphing3DToolCollectionFactory.java
+++ common/src/main/java/org/geogebra/common/gui/toolcategorization/impl/Graphing3DToolCollectionFactory.java
@@ -112,92 +112,200 @@ public class Graphing3DToolCollectionFactory extends AbstractToolCollectionFacto
 
         impl.extendCategory(CATEGORY_BASIC);
 
-        impl.extendCategory(CATEGORY_EDIT,
-                EuclidianConstants.MODE_VIEW_IN_FRONT_OF);
+        if (app.has(Feature.G3D_TOOLS_SIMPLE_MORE_LESS)) {
+            impl.addCategory(CATEGORY_EDIT,
+                    EuclidianConstants.MODE_SHOW_HIDE_LABEL,
+                    EuclidianConstants.MODE_SHOW_HIDE_OBJECT,
+                    EuclidianConstants.MODE_DELETE,
+                    EuclidianConstants.MODE_VIEW_IN_FRONT_OF);
 
-        impl.extendCategory(CATEGORY_POINTS,
-                EuclidianConstants.MODE_POINT_ON_OBJECT,
-                EuclidianConstants.MODE_ATTACH_DETACH);
+            impl.addCategory(CATEGORY_POINTS,
+                    EuclidianConstants.MODE_POINT,
+                    EuclidianConstants.MODE_INTERSECT,
+                    EuclidianConstants.MODE_MIDPOINT,
+                    EuclidianConstants.MODE_POINT_ON_OBJECT,
+                    EuclidianConstants.MODE_ATTACH_DETACH);
 
-        if (app.has(Feature.G3D_IMPROVE_SOLID_TOOLS)) {
-            impl.addCategory(CATEGORY_LINES_AND_POLYGONS,
-                    EuclidianConstants.MODE_SEGMENT,
-                    EuclidianConstants.MODE_SEGMENT_FIXED,
-                    EuclidianConstants.MODE_JOIN,
-                    EuclidianConstants.MODE_RAY,
-                    EuclidianConstants.MODE_VECTOR,
-                    EuclidianConstants.MODE_POLYGON,
-                    EuclidianConstants.MODE_REGULAR_POLYGON,
-                    EuclidianConstants.MODE_ORTHOGONAL_THREE_D,
-                    EuclidianConstants.MODE_PARALLEL,
-                    EuclidianConstants.MODE_ANGULAR_BISECTOR,
-                    EuclidianConstants.MODE_TANGENTS);
+            if (app.has(Feature.G3D_IMPROVE_SOLID_TOOLS)) {
+                impl.addCategory(CATEGORY_LINES_AND_POLYGONS,
+                        EuclidianConstants.MODE_SEGMENT,
+                        EuclidianConstants.MODE_SEGMENT_FIXED,
+                        EuclidianConstants.MODE_JOIN,
+                        EuclidianConstants.MODE_RAY,
+                        EuclidianConstants.MODE_VECTOR,
+                        EuclidianConstants.MODE_POLYGON,
+                        EuclidianConstants.MODE_REGULAR_POLYGON,
+                        EuclidianConstants.MODE_ORTHOGONAL_THREE_D,
+                        EuclidianConstants.MODE_PARALLEL,
+                        EuclidianConstants.MODE_ANGULAR_BISECTOR,
+                        EuclidianConstants.MODE_TANGENTS);
+            } else {
+                impl.addCategory(CATEGORY_LINES_AND_POLYGONS,
+                        EuclidianConstants.MODE_SEGMENT,
+                        EuclidianConstants.MODE_SEGMENT_FIXED,
+                        EuclidianConstants.MODE_JOIN,
+                        EuclidianConstants.MODE_RAY,
+                        EuclidianConstants.MODE_VECTOR,
+                        EuclidianConstants.MODE_POLYGON,
+                        EuclidianConstants.MODE_ORTHOGONAL_THREE_D,
+                        EuclidianConstants.MODE_PARALLEL,
+                        EuclidianConstants.MODE_ANGULAR_BISECTOR,
+                        EuclidianConstants.MODE_TANGENTS);
+            }
+
+            impl.addCategory(CATEGORY_SOLIDS,
+                    EuclidianConstants.MODE_PYRAMID,
+                    EuclidianConstants.MODE_PRISM,
+                    EuclidianConstants.MODE_TETRAHEDRON,
+                    EuclidianConstants.MODE_CUBE,
+                    EuclidianConstants.MODE_SPHERE_TWO_POINTS,
+                    EuclidianConstants.MODE_SPHERE_POINT_RADIUS,
+                    EuclidianConstants.MODE_CONE_TWO_POINTS_RADIUS,
+                    EuclidianConstants.MODE_CYLINDER_TWO_POINTS_RADIUS,
+                    EuclidianConstants.MODE_CONIFY,
+                    EuclidianConstants.MODE_EXTRUSION,
+                    EuclidianConstants.MODE_NET);
+
+            impl.addCategory(CATEGORY_PLANES,
+                    EuclidianConstants.MODE_PLANE_THREE_POINTS,
+                    EuclidianConstants.MODE_PLANE,
+                    EuclidianConstants.MODE_PARALLEL_PLANE,
+                    EuclidianConstants.MODE_ORTHOGONAL_PLANE);
+
+            impl.addCategory(CATEGORY_CIRCLES,
+                    EuclidianConstants.MODE_CIRCLE_AXIS_POINT,
+                    EuclidianConstants.MODE_CIRCLE_POINT_RADIUS_DIRECTION,
+                    EuclidianConstants.MODE_CIRCLE_THREE_POINTS,
+                    EuclidianConstants.MODE_CIRCLE_ARC_THREE_POINTS,
+                    EuclidianConstants.MODE_CIRCUMCIRCLE_ARC_THREE_POINTS,
+                    EuclidianConstants.MODE_CIRCLE_SECTOR_THREE_POINTS,
+                    EuclidianConstants.MODE_CIRCUMCIRCLE_SECTOR_THREE_POINTS);
+
+            impl.addCategory(CATEGORY_CURVES,
+                    EuclidianConstants.MODE_ELLIPSE_THREE_POINTS,
+                    EuclidianConstants.MODE_CONIC_FIVE_POINTS,
+                    EuclidianConstants.MODE_PARABOLA,
+                    EuclidianConstants.MODE_HYPERBOLA_THREE_POINTS,
+                    EuclidianConstants.MODE_LOCUS,
+                    EuclidianConstants.MODE_INTERSECTION_CURVE);
+
+            impl.addCategory(CATEGORY_TRANSFORM,
+                    EuclidianConstants.MODE_MIRROR_AT_PLANE,
+                    EuclidianConstants.MODE_MIRROR_AT_POINT,
+                    EuclidianConstants.MODE_ROTATE_AROUND_LINE,
+                    EuclidianConstants.MODE_TRANSLATE_BY_VECTOR,
+                    EuclidianConstants.MODE_DILATE_FROM_POINT,
+                    EuclidianConstants.MODE_MIRROR_AT_LINE);
+
+            impl.addCategory(CATEGORY_MEASURE,
+                    EuclidianConstants.MODE_ANGLE,
+                    EuclidianConstants.MODE_DISTANCE,
+                    EuclidianConstants.MODE_AREA,
+                    EuclidianConstants.MODE_VOLUME);
+
+            List<Integer> others = new ArrayList<>(Arrays.asList(
+                    EuclidianConstants.MODE_ROTATEVIEW,
+                    EuclidianConstants.MODE_TRANSLATEVIEW,
+                    EuclidianConstants.MODE_COPY_VISUAL_STYLE
+            ));
+            if (!isPhoneApp) {
+                others.add(EuclidianConstants.MODE_TEXT);
+            }
+            impl.addCategory(CATEGORY_OTHERS, others);
+
+            impl.addCategory(CATEGORY_SPECIAL_LINES,
+                    EuclidianConstants.MODE_VECTOR_FROM_POINT,
+                    EuclidianConstants.MODE_POLYLINE,
+                    // EuclidianConstants.MODE_FITLINE,
+                    EuclidianConstants.MODE_POLAR_DIAMETER);
         } else {
-            impl.addCategory(CATEGORY_LINES_AND_POLYGONS,
-                    EuclidianConstants.MODE_SEGMENT,
-                    EuclidianConstants.MODE_SEGMENT_FIXED,
-                    EuclidianConstants.MODE_JOIN,
-                    EuclidianConstants.MODE_RAY,
-                    EuclidianConstants.MODE_VECTOR,
-                    EuclidianConstants.MODE_POLYGON,
-                    EuclidianConstants.MODE_ORTHOGONAL_THREE_D,
-                    EuclidianConstants.MODE_PARALLEL,
-                    EuclidianConstants.MODE_ANGULAR_BISECTOR,
-                    EuclidianConstants.MODE_TANGENTS);
+            impl.extendCategory(CATEGORY_EDIT,
+                    EuclidianConstants.MODE_VIEW_IN_FRONT_OF);
+
+            impl.extendCategory(CATEGORY_POINTS,
+                    EuclidianConstants.MODE_POINT_ON_OBJECT,
+                    EuclidianConstants.MODE_ATTACH_DETACH);
+
+            if (app.has(Feature.G3D_IMPROVE_SOLID_TOOLS)) {
+                impl.addCategory(CATEGORY_LINES_AND_POLYGONS,
+                        EuclidianConstants.MODE_SEGMENT,
+                        EuclidianConstants.MODE_SEGMENT_FIXED,
+                        EuclidianConstants.MODE_JOIN,
+                        EuclidianConstants.MODE_RAY,
+                        EuclidianConstants.MODE_VECTOR,
+                        EuclidianConstants.MODE_POLYGON,
+                        EuclidianConstants.MODE_REGULAR_POLYGON,
+                        EuclidianConstants.MODE_ORTHOGONAL_THREE_D,
+                        EuclidianConstants.MODE_PARALLEL,
+                        EuclidianConstants.MODE_ANGULAR_BISECTOR,
+                        EuclidianConstants.MODE_TANGENTS);
+            } else {
+                impl.addCategory(CATEGORY_LINES_AND_POLYGONS,
+                        EuclidianConstants.MODE_SEGMENT,
+                        EuclidianConstants.MODE_SEGMENT_FIXED,
+                        EuclidianConstants.MODE_JOIN,
+                        EuclidianConstants.MODE_RAY,
+                        EuclidianConstants.MODE_VECTOR,
+                        EuclidianConstants.MODE_POLYGON,
+                        EuclidianConstants.MODE_ORTHOGONAL_THREE_D,
+                        EuclidianConstants.MODE_PARALLEL,
+                        EuclidianConstants.MODE_ANGULAR_BISECTOR,
+                        EuclidianConstants.MODE_TANGENTS);
+            }
+
+            impl.addCategory(CATEGORY_SOLIDS,
+                    EuclidianConstants.MODE_PYRAMID,
+                    EuclidianConstants.MODE_PRISM,
+                    EuclidianConstants.MODE_TETRAHEDRON,
+                    EuclidianConstants.MODE_CUBE,
+                    EuclidianConstants.MODE_SPHERE_TWO_POINTS,
+                    EuclidianConstants.MODE_SPHERE_POINT_RADIUS,
+                    EuclidianConstants.MODE_CONE_TWO_POINTS_RADIUS,
+                    EuclidianConstants.MODE_CYLINDER_TWO_POINTS_RADIUS,
+                    EuclidianConstants.MODE_CONIFY,
+                    EuclidianConstants.MODE_EXTRUSION,
+                    EuclidianConstants.MODE_NET);
+
+            impl.extendCategory(CATEGORY_PLANES);
+
+            impl.extendCategory(CATEGORY_CIRCLES,
+                    EuclidianConstants.MODE_CIRCLE_AXIS_POINT,
+                    EuclidianConstants.MODE_CIRCLE_POINT_RADIUS_DIRECTION,
+                    EuclidianConstants.MODE_CIRCLE_THREE_POINTS,
+                    EuclidianConstants.MODE_CIRCLE_ARC_THREE_POINTS,
+                    EuclidianConstants.MODE_CIRCUMCIRCLE_ARC_THREE_POINTS,
+                    EuclidianConstants.MODE_CIRCLE_SECTOR_THREE_POINTS,
+                    EuclidianConstants.MODE_CIRCUMCIRCLE_SECTOR_THREE_POINTS);
+
+            impl.extendCategory(CATEGORY_CURVES,
+                    EuclidianConstants.MODE_ELLIPSE_THREE_POINTS,
+                    EuclidianConstants.MODE_CONIC_FIVE_POINTS,
+                    EuclidianConstants.MODE_PARABOLA,
+                    EuclidianConstants.MODE_HYPERBOLA_THREE_POINTS,
+                    EuclidianConstants.MODE_LOCUS,
+                    EuclidianConstants.MODE_INTERSECTION_CURVE);
+
+            impl.extendCategory(CATEGORY_TRANSFORM,
+                    EuclidianConstants.MODE_DILATE_FROM_POINT,
+                    EuclidianConstants.MODE_MIRROR_AT_LINE);
+
+            impl.extendCategory(CATEGORY_MEASURE);
+
+            List<Integer> others = new ArrayList<>(Arrays.asList(
+                    EuclidianConstants.MODE_ROTATEVIEW,
+                    EuclidianConstants.MODE_TRANSLATEVIEW,
+                    EuclidianConstants.MODE_COPY_VISUAL_STYLE
+            ));
+            if (!isPhoneApp) {
+                others.add(EuclidianConstants.MODE_TEXT);
+            }
+            impl.extendCategory(CATEGORY_OTHERS, others);
+
+            impl.extendCategory(CATEGORY_SPECIAL_LINES,
+                    EuclidianConstants.MODE_VECTOR_FROM_POINT,
+                    EuclidianConstants.MODE_POLYLINE,
+                    // EuclidianConstants.MODE_FITLINE,
+                    EuclidianConstants.MODE_POLAR_DIAMETER);
         }
-
-        impl.addCategory(CATEGORY_SOLIDS,
-                EuclidianConstants.MODE_PYRAMID,
-                EuclidianConstants.MODE_PRISM,
-                EuclidianConstants.MODE_TETRAHEDRON,
-                EuclidianConstants.MODE_CUBE,
-                EuclidianConstants.MODE_SPHERE_TWO_POINTS,
-                EuclidianConstants.MODE_SPHERE_POINT_RADIUS,
-                EuclidianConstants.MODE_CONE_TWO_POINTS_RADIUS,
-                EuclidianConstants.MODE_CYLINDER_TWO_POINTS_RADIUS,
-                EuclidianConstants.MODE_CONIFY,
-                EuclidianConstants.MODE_EXTRUSION,
-                EuclidianConstants.MODE_NET);
-
-        impl.extendCategory(CATEGORY_PLANES);
-
-        impl.extendCategory(CATEGORY_CIRCLES,
-                EuclidianConstants.MODE_CIRCLE_AXIS_POINT,
-                EuclidianConstants.MODE_CIRCLE_POINT_RADIUS_DIRECTION,
-                EuclidianConstants.MODE_CIRCLE_THREE_POINTS,
-                EuclidianConstants.MODE_CIRCLE_ARC_THREE_POINTS,
-                EuclidianConstants.MODE_CIRCUMCIRCLE_ARC_THREE_POINTS,
-                EuclidianConstants.MODE_CIRCLE_SECTOR_THREE_POINTS,
-                EuclidianConstants.MODE_CIRCUMCIRCLE_SECTOR_THREE_POINTS);
-
-        impl.extendCategory(CATEGORY_CURVES,
-                EuclidianConstants.MODE_ELLIPSE_THREE_POINTS,
-                EuclidianConstants.MODE_CONIC_FIVE_POINTS,
-                EuclidianConstants.MODE_PARABOLA,
-                EuclidianConstants.MODE_HYPERBOLA_THREE_POINTS,
-                EuclidianConstants.MODE_LOCUS,
-                EuclidianConstants.MODE_INTERSECTION_CURVE);
-
-        impl.extendCategory(CATEGORY_TRANSFORM,
-                EuclidianConstants.MODE_DILATE_FROM_POINT,
-                EuclidianConstants.MODE_MIRROR_AT_LINE);
-
-        impl.extendCategory(CATEGORY_MEASURE);
-
-        List<Integer> others = new ArrayList<>(Arrays.asList(
-                EuclidianConstants.MODE_ROTATEVIEW,
-                EuclidianConstants.MODE_TRANSLATEVIEW,
-                EuclidianConstants.MODE_COPY_VISUAL_STYLE
-        ));
-        if (!isPhoneApp) {
-            others.add(EuclidianConstants.MODE_TEXT);
-        }
-        impl.extendCategory(CATEGORY_OTHERS, others);
-
-        impl.extendCategory(CATEGORY_SPECIAL_LINES,
-                EuclidianConstants.MODE_VECTOR_FROM_POINT,
-                EuclidianConstants.MODE_POLYLINE,
-                // EuclidianConstants.MODE_FITLINE,
-                EuclidianConstants.MODE_POLAR_DIAMETER);
     }
 }
