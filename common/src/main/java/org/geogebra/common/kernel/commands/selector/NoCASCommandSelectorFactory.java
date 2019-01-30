0aa548c3879d51e7428b635be5c1a933d0b4ddcb
Michael Borcherds <michael@geogebra.org>
2019-01-30 11:56:05 +0000

    Revert r66671 (also have Asymptote(conic))
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@66675 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/kernel/commands/selector/NoCASCommandSelectorFactory.java
+++ common/src/main/java/org/geogebra/common/kernel/commands/selector/NoCASCommandSelectorFactory.java
@@ -23,8 +23,7 @@ public class NoCASCommandSelectorFactory {
 				Commands.CompleteSquare, Commands.PartialFractions,
 				Commands.SolveODE, Commands.ImplicitDerivative,
 				Commands.NextPrime, Commands.PreviousPrime, Commands.Solve,
-				Commands.Solutions, Commands.NSolutions, Commands.NSolve,
-				Commands.Asymptote);
+				Commands.Solutions, Commands.NSolutions, Commands.NSolve);
 		return commandSelector;
 	}
 }
