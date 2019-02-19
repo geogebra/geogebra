d0837351c8f090aaa8f4ac6c34e837b4fc5c7e4a
Zbyněk Konečný <zbynek@geogebra.org>
2019-02-19 17:13:31 +0000

    App class for testing Classic in common-jre
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67003 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common-jre/src/test/java/org/geogebra/common/main/AppCommon3D.java
@@ -0,0 +1,40 @@
+package org.geogebra.common.main;
+
+import org.geogebra.common.geogebra3D.euclidianForPlane.EuclidianViewForPlaneCompanion;
+import org.geogebra.common.geogebra3D.main.App3DCompanion;
+import org.geogebra.common.gui.layout.DockPanel;
+import org.geogebra.common.jre.kernel.commands.CommandDispatcher3DJre;
+import org.geogebra.common.kernel.Kernel;
+import org.geogebra.common.kernel.commands.CommandDispatcher;
+import org.geogebra.common.kernel.kernelND.ViewCreator;
+import org.geogebra.common.main.settings.EuclidianSettings;
+
+public class AppCommon3D extends AppCommon {
+
+	@Override
+	protected AppCompanion newAppCompanion() {
+		return new App3DCompanion(this) {
+
+			@Override
+			protected EuclidianViewForPlaneCompanion createEuclidianViewForPlane(
+					ViewCreator plane, EuclidianSettings evSettings,
+					boolean panelSettings) {
+				return null;
+			}
+
+			@Override
+			public DockPanel getPanelForPlane() {
+				return null;
+			}
+		};
+	}
+
+	@Override
+	public CommandDispatcher getCommand3DDispatcher(Kernel k) {
+		return new CommandDispatcher3DJre(k);
+	}
+	
+	public boolean is3D(){
+		return true;
+	}
+}
