b1a9881df2d03df48bebae324c3e24d5e7ed961b
Jan Borovsky <jan.borovsky@geogebra.org>
2019-02-20 12:22:26 +0000

    G3D-245:ARManagerInterface refactored to ARManager. ARCoreManager modelMatrix interaction pulled to ARManager.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67014 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ ar-base/src/main/java/org/geogebra/arbase/ARManager.java
@@ -0,0 +1,174 @@
+package org.geogebra.arbase;
+
+
+import org.geogebra.common.geogebra3D.euclidian3D.openGL.Renderer;
+import org.geogebra.common.kernel.Matrix.CoordMatrix4x4;
+import org.geogebra.common.kernel.Matrix.Coords;
+
+public class ARManager<TouchEventType> {
+
+    protected CoordMatrix4x4 viewMatrix = new CoordMatrix4x4();
+    protected CoordMatrix4x4 projectMatrix = new CoordMatrix4x4();
+    protected CoordMatrix4x4 mModelMatrix = new CoordMatrix4x4();
+    protected CoordMatrix4x4 mAnchorMatrix = new CoordMatrix4x4();
+    protected CoordMatrix4x4 scaleMatrix = new CoordMatrix4x4();
+    protected float mScaleFactor = 1;
+    protected float rotateAngel = 0;
+    protected Coords hittingFloor = Coords.createInhomCoorsInD3();
+    protected boolean hittingFloorOk;
+    protected double hittingDistance;
+
+    protected float[] hitMatrix = new float[16];
+    protected float[] lastHitOrigin = new float[3];
+    protected float[] rayEndOrigin = new float[3];
+    protected float[] translationOffset = new float[3];
+    protected double[] previousTranslationOffset = new double[3];
+
+    protected float mDistance;
+    protected boolean objectIsRendered = false;
+    protected boolean mDrawing = false;
+    protected boolean mARIsRendering = false;
+
+    protected float[] projectionFloats = new float[3];
+    protected CoordMatrix4x4 cHitMatrix = new CoordMatrix4x4();
+    protected Coords rayOrigin = new Coords(4);
+    protected Coords rayDirection = new Coords(4);
+    protected Coords projection = Coords.createInhomCoorsInD3();
+
+    public void onSurfaceCreated() {
+
+    }
+
+    public void onSurfaceChanged(int width, int height) {
+
+    }
+
+    public void onResume() {
+
+    }
+
+    public void onPause() {
+
+    }
+
+    public void onDrawFrame() {
+
+    }
+
+    public void arButtonClicked() throws ARException {
+
+    }
+
+    public void setSession() throws ARException {
+
+    }
+
+    public boolean getARIsRendering(){
+        return false;
+    }
+
+    public boolean hasSessionStarted(){
+        return false;
+    }
+
+    public boolean isDrawing(){
+        return false;
+    }
+
+    public CoordMatrix4x4 getViewMatrix() {
+        return viewMatrix;
+    }
+
+    public CoordMatrix4x4 getProjectMatrix() {
+        return projectMatrix;
+    }
+
+    public CoordMatrix4x4 getAnchorMatrixForGGB() {
+        return null;
+    }
+
+    public float getDistance() {
+        return mDistance;
+    }
+
+    public float getScaleFactor() {
+        return mScaleFactor;
+    }
+
+    public void freezeScreenRotation() {
+
+    }
+
+    public void setBackgroundColor() {
+
+    }
+
+    public void setBackgroundStyle(Renderer.BackgroundStyle backgroundStyle) {
+
+    }
+
+    public  Renderer.BackgroundStyle getBackgroundStyle() {
+        return null;
+    }
+
+    public Coords getHittingDirection() {
+        return rayDirection;
+    }
+
+    public Coords getHittingOrigin() {
+        return rayOrigin;
+    }
+
+    public Coords getHittingFloor() {
+        return hittingFloorOk ? hittingFloor : null;
+    }
+
+    public double getHittingDistance() {
+        return hittingDistance;
+    }
+
+    public void setHittingOriginAndDirection(float x, float y) {
+
+    }
+
+    public void setHittingOriginAndDirectionFromScreenCenter() {
+
+    }
+
+    public void proceed(TouchEventType event) {
+
+    }
+
+    protected void updateModelMatrixFields() {
+        /* translating */
+        translationOffset[0] = rayEndOrigin[0] - lastHitOrigin[0];
+        translationOffset[1] = rayEndOrigin[1] - lastHitOrigin[1];
+        translationOffset[2] = rayEndOrigin[2] - lastHitOrigin[2];
+    }
+
+    protected void updateModelMatrix() {
+
+        // Rotating not implemented - temporary
+
+        /* Scaling */
+        mModelMatrix.setMul(mAnchorMatrix, scaleMatrix);
+
+        /* translating */
+        Coords modelOrigin = mModelMatrix.getOrigin();
+        Coords anchorOrigin = mAnchorMatrix.getOrigin();
+        modelOrigin.setX(anchorOrigin.getX() + translationOffset[0] + previousTranslationOffset[0]);
+        modelOrigin.setY(anchorOrigin.getY());
+        modelOrigin.setZ(anchorOrigin.getZ() + translationOffset[2] + previousTranslationOffset[2]);
+    }
+
+    protected float[] setRay() {
+        cHitMatrix.setFromGL(hitMatrix);
+
+        viewMatrix.solve(Coords.O, rayOrigin);
+        rayDirection.setSub3(cHitMatrix.getOrigin(), rayOrigin);
+        rayOrigin.projectPlane(mModelMatrix.getVx(), mModelMatrix.getVz(), rayDirection,
+                mModelMatrix.getOrigin(), projection);
+        projection.get3ForGL(projectionFloats);
+        return projectionFloats;
+    }
+}
