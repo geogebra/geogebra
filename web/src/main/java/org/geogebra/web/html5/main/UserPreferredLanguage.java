ae1d5de24ec990bc57fa641d9c1538bbe560c90f
Laszlo Gal <laszlo@geogebra.org>
2019-02-20 09:54:42 +0000

    APPS-91 missing file
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67009 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/main/java/org/geogebra/web/html5/main/UserPreferredLanguage.java
@@ -0,0 +1,48 @@
+package org.geogebra.web.html5.main;
+
+import org.geogebra.common.util.StringUtil;
+import org.geogebra.web.html5.Browser;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Node;
+import com.google.gwt.storage.client.Storage;
+import com.google.gwt.user.client.Cookies;
+import com.google.gwt.user.client.Window.Location;
+
+public class UserPreferredLanguage {
+	public static String get(AppW app) {
+		String cookieLang = Cookies.getCookie("GeoGebraLangUI");
+		if (!StringUtil.empty(cookieLang)) {
+			return cookieLang;
+		}
+
+		Storage localStorage = Storage.getLocalStorageIfSupported();
+		String storageLang = localStorage.getItem("GeoGebraLangUI");
+		if (!StringUtil.empty(storageLang)) {
+			return storageLang;
+		}
+
+		String urlLang = app.getArticleElement().getDataParamApp() ? Location.getParameter("lang") : "";
+		if (!StringUtil.empty(urlLang) && !app.getLoginOperation().isLoggedIn()) {
+			return urlLang;
+		}
+
+		return Browser.navigatorLanguage();
+	}
+	
+	public static void translate(AppW app, Element elem) {
+		for (int i = 0; i < elem.getChildCount(); i++) {
+			Node child = elem.getChild(i);
+			if (child.getNodeType() == Node.ELEMENT_NODE) {
+				Element e = (Element) child;
+				if (child.getChildCount() == 1) {
+					if (e.hasAttribute("data-trans-key")) {
+						e.setInnerText(app.getLocalization().getMenu(e.getAttribute("data-trans-key")));
+					}
+				} else {
+					translate(app, e);
+				}
+			}
+		}
+	}
+}
