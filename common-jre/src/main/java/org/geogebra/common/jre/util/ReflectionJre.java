546149b71f5f64376654065444a2f3dd299506ae
Balazs Bencze <megette_a@yahoo.com>
2019-02-25 11:11:16 +0000

    APPS-308 Implement standard JRE reflection
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67072 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common-jre/src/main/java/org/geogebra/common/jre/util/ReflectionJre.java
@@ -0,0 +1,51 @@
+package org.geogebra.common.jre.util;
+
+import org.geogebra.common.util.Reflection;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Implements general reflection utilities.
+ */
+public class ReflectionJre implements Reflection {
+
+	private final Method[] methods;
+	private final Map<String, Method> cache;
+
+	/**
+	 * Create a reflection for reflecting clazz methods.
+	 *
+	 * @param clazz methods to reflect
+	 */
+	public ReflectionJre(Class clazz) {
+		this.methods = clazz.getMethods();
+		this.cache = new HashMap<>();
+	}
+
+	@Override
+	public void call(Object object, String methodName, Object[] parameters) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+		Method method = getMethod(methodName);
+		method.invoke(object, parameters);
+	}
+
+	private Method getMethod(String methodName) throws NoSuchMethodException {
+		Method method = cache.get(methodName);
+		if (method == null) {
+			method = findMethod(methodName);
+			cache.put(methodName, method);
+		}
+		return method;
+	}
+
+	private Method findMethod(String methodName) throws NoSuchMethodException {
+		for (int i = 0; i < methods.length; i++) {
+			if (methods[i].getName().equals(methodName)) {
+				return methods[i];
+			}
+		}
+		throw new NoSuchMethodException(methodName);
+	}
+}
