1c101e7043a92c952035476ffd454b55bdd58d58
Laszlo Gal <laszlo@geogebra.org>
2019-02-26 13:13:43 +0000

    APPS-491 missings
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67120 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ web/src/main/java/org/geogebra/web/full/gui/dialog/image/WebcamDialogInterface.java
@@ -0,0 +1,18 @@
+package org.geogebra.web.full.gui.dialog.image;
+
+/**
+ * Interface for handling WebCam dialogs
+ * @author laszlo
+ *
+ */
+public interface WebcamDialogInterface {
+	/** Show the WebCam dialog*/ 
+	void show();
+	
+	/** resize the WebCam dialog */ 
+	void resize();
+	
+	/** center WebCam dialog */
+	void center();
+
+}
