3448b04b9c34c919c625bffafc1d4aacfb5bef3d
Balazs Bencze <megette_a@yahoo.com>
2019-02-26 08:03:32 +0000

    APPS-308 Add tests for BatchedUpdateWrapper
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67106 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ common-jre/src/test/java/org/geogebra/common/kernel/batch/BatchedUpdateWrapperTest.java
@@ -0,0 +1,118 @@
+package org.geogebra.common.kernel.batch;
+
+import org.geogebra.common.BaseUnitTest;
+import org.geogebra.common.jre.util.UtilFactoryJre;
+import org.geogebra.common.kernel.View;
+import org.geogebra.common.kernel.geos.GProperty;
+import org.geogebra.common.kernel.geos.GeoElement;
+import org.geogebra.common.util.GTimer;
+import org.geogebra.common.util.GTimerListener;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BatchedUpdateWrapperTest extends BaseUnitTest {
+
+	@Mock
+	private View wrappedView;
+
+	@Mock
+	private GTimer timer;
+
+	@Spy
+	private UtilFactoryJre utilFactory;
+
+	private BatchedUpdateWrapper wrapper;
+
+	@Before
+	public void setupBatchedUpdateWrapperTest() {
+		when(utilFactory.newTimer(any(GTimerListener.class), anyInt())).then(new Answer<GTimer>() {
+			@Override
+			public GTimer answer(InvocationOnMock invocation) {
+				return timer;
+			}
+		});
+		wrapper = new BatchedUpdateWrapper(wrappedView, utilFactory);
+	}
+
+	@Test
+	public void testCallsTimerStart() {
+		GeoElement line = getElementFactory().createGeoLine();
+
+		wrapper.add(line);
+		wrapper.update(line);
+
+		verify(timer, atLeast(1)).start();
+	}
+
+	@Test
+	public void testCallsMethodsWhenTimerRuns() {
+		GeoElement line = getElementFactory().createGeoLine();
+
+		wrapper.add(line);
+		wrapper.update(line);
+		wrapper.rename(line);
+		wrapper.updateVisualStyle(line, GProperty.LABEL_STYLE);
+		wrapper.updateHighlight(line);
+		wrapper.updateAuxiliaryObject(line);
+		wrapper.reset();
+		wrapper.repaintView();
+
+		// Timer fires
+		wrapper.onRun();
+
+		verify(wrappedView).add(line);
+		verify(wrappedView).update(line);
+		verify(wrappedView).rename(line);
+		verify(wrappedView).updateVisualStyle(line, GProperty.LABEL_STYLE);
+		verify(wrappedView).updateHighlight(line);
+		verify(wrappedView).updateAuxiliaryObject(line);
+		verify(wrappedView).reset();
+		verify(wrappedView).repaintView();
+	}
+
+	@Test
+	public void testOptimizeUpdateCalls() {
+		GeoElement line = getElementFactory().createGeoLine();
+		wrapper.update(line);
+		wrapper.update(line);
+
+		wrapper.onRun();
+
+		verify(wrappedView, times(1)).update(line);
+	}
+
+	@Test
+	public void testOptimizeRemove() {
+		GeoElement line = getElementFactory().createGeoLine();
+		wrapper.add(line);
+		wrapper.update(line);
+		wrapper.update(line);
+		wrapper.remove(line);
+
+		wrapper.onRun();
+
+		verify(wrappedView, times(1)).remove(line);
+		verify(wrappedView, times(0)).add(line);
+		verify(wrappedView, times(0)).update(line);
+	}
+
+	@Test
+	public void testUpdatesAreDoneInBatch() {
+		GeoElement line = getElementFactory().createGeoLine();
+		wrapper.add(line);
+
+		wrapper.onRun();
+
+		verify(wrappedView, times(1)).startBatchUpdate();
+		verify(wrappedView, times(1)).endBatchUpdate();
+	}
+}
