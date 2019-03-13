e3b0852d9f269d70e21cdef26096c6dedc784f71
Mathieu Blossier <mathieublossier@free.fr>
2019-03-13 20:19:59 +0000

    G3D-245: ARGestureManager fields to private, make most of the methods synchronized
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67354 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- ar-base/src/main/java/org/geogebra/arbase/ARGestureManager.java
+++ ar-base/src/main/java/org/geogebra/arbase/ARGestureManager.java
@@ -10,12 +10,11 @@ abstract public class ARGestureManager{
 
     private EuclidianView3D mView;
     protected float mScaleFactor = 1.0f;
-    protected Coords mPos = new Coords(2);
-    protected boolean isTouched = false;
-    protected boolean mUpdateOriginIsWanted = false;
-    protected float mAngle;
+    private Coords mPos = new Coords(2);
+    private boolean isTouched = false;
+    private boolean mUpdateOriginIsWanted = false;
+    private float mAngle;
     private boolean actionPointerLeftPreviously = false;
-    private float x, y;
     private boolean mTaped;
 
     public ARGestureManager(EuclidianView3D view) {
@@ -32,34 +31,36 @@ abstract public class ARGestureManager{
         mView.setCoordSystemFromMouseMove((int) angle, 0, EuclidianController.MOVE_ROTATE_VIEW);
     }
 
-    public float getScaleFactor() {
+    synchronized public float getScaleFactor() {
         return mScaleFactor;
     }
 
-    public void copyXYPosition(Coords ret) {
-        synchronized (mPos) {
-            ret.setX(mPos.getX());
-            ret.setY(mPos.getY());
-        }
+    synchronized public void copyXYPosition(Coords ret) {
+        ret.setX(mPos.getX());
+        ret.setY(mPos.getY());
     }
 
-    public boolean getIsTouched() {
+    synchronized public boolean getIsTouched() {
         return isTouched;
     }
 
-    public boolean getUpdateOriginIsWanted() {
+    synchronized public boolean getUpdateOriginIsWanted() {
         return mUpdateOriginIsWanted;
     }
 
-    public void setUpdateOriginIsWanted(boolean updateOriginIsWanted) {
+    synchronized public void setUpdateOriginIsWanted(boolean updateOriginIsWanted) {
         mUpdateOriginIsWanted = updateOriginIsWanted;
     }
 
-    public float getDAngle() {
+    synchronized public float getDAngle() {
         return mAngle;
     }
 
-    protected void firstFingerDown(ARMotionEvent event, App app){
+    synchronized public void setDAngle(float value) {
+        mAngle = value;
+    }
+
+    synchronized protected void firstFingerDown(ARMotionEvent event, App app){
         mUpdateOriginIsWanted = true;
         if (app.has(Feature.G3D_AR_REGULAR_TOOLS)) {
             isTouched = false;
@@ -69,13 +70,13 @@ abstract public class ARGestureManager{
         updatePos(event);
     }
 
-    protected void secondFingerDown(ARMotionEvent event){
+    synchronized protected void secondFingerDown(ARMotionEvent event){
         isTouched = true;
         mUpdateOriginIsWanted = true;
         updatePos(event);
     }
 
-    protected void onMove(ARMotionEvent event, App app){
+    synchronized protected void onMove(ARMotionEvent event, App app){
         if (app.has(Feature.G3D_AR_REGULAR_TOOLS)) {
             isTouched = event.getPointerCount() > 1;
         } else {
@@ -89,14 +90,14 @@ abstract public class ARGestureManager{
         updatePos(event);
     }
 
-    protected void firstFingerUp(App app){
+    synchronized protected void firstFingerUp(App app){
         isTouched = false;
         if (!app.has(Feature.G3D_AR_REGULAR_TOOLS)) {
             mView.getEuclidianController().clearSelections();
         }
     }
 
-    protected void secondFingerUp(App app){
+    synchronized protected void secondFingerUp(App app){
         if (app.has(Feature.G3D_AR_REGULAR_TOOLS)) {
             isTouched = false;
         } else {
@@ -106,11 +107,12 @@ abstract public class ARGestureManager{
         actionPointerLeftPreviously = true;
     }
 
-    protected void actionCancelled(){
+    synchronized protected void actionCancelled(){
         isTouched = false;
     }
 
-    protected void updatePos(ARMotionEvent event) {
+    private void updatePos(ARMotionEvent event) {
+        float x, y;
         if (event.getPointerCount() == 2) {
             float nfX, nfY, nsX, nsY;
             nsX = event.getX(0);
@@ -121,27 +123,25 @@ abstract public class ARGestureManager{
             x = (nfX + nsX) / 2;
             y = (nfY + nsY) / 2;
 
-        } else if (event.getPointerCount() == 1) {
+        } else {
             x = event.getX(0);
             y = event.getY(0);
         }
         setPos(x, y);
     }
 
-    protected void setPos(double x, double y) {
-        synchronized (mPos) {
-            mPos.setX(x);
-            mPos.setY(y);
-        }
+    synchronized protected void setPos(double x, double y) {
+        mPos.setX(x);
+        mPos.setY(y);
     }
 
     abstract public void addGestureRecognizers();
 
-    public boolean isTaped() {
+    synchronized public boolean isTaped() {
         return mTaped;
     }
 
-    public void setTaped(boolean taped) {
+    synchronized public void setTaped(boolean taped) {
         mTaped = taped;
     }
 }
