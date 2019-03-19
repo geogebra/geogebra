f3a6a6fc556795c84c2c526359fdf7b322669fe0
Jan Borovsky <jan.borovsky@geogebra.org>
2019-03-19 09:37:51 +0000

    G3D-266: ARMotionEvent implemented in MouseTouchGestureQueueHelper. Added ARMotionEvent3DA, ARMotionEventHelper.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67442 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- ar-base/src/main/java/org/geogebra/arbase/ARMotionEvent.java
+++ ar-base/src/main/java/org/geogebra/arbase/ARMotionEvent.java
@@ -4,6 +4,14 @@ public class ARMotionEvent {
 
     private int pointerCount;
     private float fingerPositions[][];
+    protected int mAction;
+
+    public static int FIRST_FINGER_DOWN = 0;
+    public static int FIRST_FINGER_UP = 1;
+    public static int ON_MOVE = 2;
+    public static int ACTION_CANCEL = 3;
+    public static int SECOND_FINGER_DOWN = 5;
+    public static int SECOND_FINGER_UP = 6;
 
     public ARMotionEvent(float firstFingerX, float firstFingerY){
         pointerCount = 1;
@@ -22,6 +30,14 @@ public class ARMotionEvent {
         fingerPositions[1][1] = secondFingerY;
     }
 
+    public float getX() {
+        return getX(0);
+    }
+
+    public float getY() {
+        return getY(0);
+    }
+
     public float getX(int index) {
         return fingerPositions[index][0];
     }
@@ -32,5 +48,22 @@ public class ARMotionEvent {
 
     public int getPointerCount() {
         return pointerCount;
+    }
+
+    public int getAction() {
+        return mAction;
+    }
+
+    public void setAction(int action) {
+        mAction = action;
+    }
+
+    public void setLocation(int x, int y) {
+        fingerPositions[0][0] = x;
+        fingerPositions[0][1] = y;
+    }
+
+    public Object getAREvent(){
+        return null;
     }
 }
