118dc20703c113e02163c3c82cb0010dd7bbcc1d
Barna Karacsony <barna.karacsony@geogebra.org>
2019-03-27 15:48:33 +0000

    APPS-348: hack removed from the Layout.setEVsettingsFromPerspective
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67598 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/gui/Layout.java
+++ common/src/main/java/org/geogebra/common/gui/Layout.java
@@ -479,14 +479,6 @@ public abstract class Layout implements SettingListener {
 				changed = ev.setShowAxes(perspective.getShowAxes(), false);
 				changed |= ev.showGrid(perspective.getShowGrid());
 			}
-
-			if (app.isUnbundled()
-					&& perspective.getDefaultID() == Perspective.GEOMETRY
-					&& app.getEuclidianView1() == ev) {
-				app.setLabelingStyle(ConstructionDefaults.LABEL_VISIBLE_POINTS_ONLY);
-			}
-
-			// ev.setUnitAxesRatio(perspective.isUnitAxesRatio());
 		}
 
 		return changed;
