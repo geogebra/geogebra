440475472ea25705195e6a3958d2c722b93b6b2e
Jan Borovsky <jan.borovsky@geogebra.org>
2019-04-03 11:28:06 +0000

    G3D_AR_REGULAR_TOOLS, G3D_AR_ROTATE_3D_VIEW_TOOL, G3D_AR_TRANSLATE_3D_VIEW_TOOL set to prerelease.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67720 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -3905,15 +3905,15 @@ public abstract class App implements UpdateSelection, AppInterface {
 
 		/** G3D-42*/
 			case G3D_AR_REGULAR_TOOLS:
-				return false;
+				return prerelease;
 
         /** G3D-95*/
 			case G3D_AR_ROTATE_3D_VIEW_TOOL:
-                return false;
+                return prerelease;
 
         /** G3D-97*/
 			case G3D_AR_TRANSLATE_3D_VIEW_TOOL:
-				return false;
+				return prerelease;
 
 		/** G3D-160 */
 		case G3D_AR_TARGET:
