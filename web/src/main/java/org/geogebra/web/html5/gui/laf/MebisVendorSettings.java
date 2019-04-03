0c64bfc45c897c045c56ddd05296e1911af1a6c6
Zbyněk Konečný <zbynek@geogebra.org>
2019-04-03 18:33:17 +0000

    MOW-715 use online license in installer
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67730 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/html5/gui/laf/MebisVendorSettings.java
+++ web/src/main/java/org/geogebra/web/html5/gui/laf/MebisVendorSettings.java
@@ -2,11 +2,18 @@ package org.geogebra.web.html5.gui.laf;
 
 import org.geogebra.common.main.AppConfig;
 
+import com.google.gwt.user.client.Window.Location;
+
 public class MebisVendorSettings extends VendorSettings {
+
+	private static final String MEBIS_LICENSE_PATH = "/static/license.html?";
 
 	@Override
 	public String getLicenseURL() {
-		return "/static/license.html?";
+		if (!Location.getProtocol().startsWith("http")) {
+			return "https://tafel.mebis.bayern.de" + MEBIS_LICENSE_PATH;
+		}
+		return MEBIS_LICENSE_PATH;
 	}
 
 	@Override
