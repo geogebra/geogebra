21a1669859583e92e2f6fea055db5e941ffc6feb
Mathieu Blossier <mathieublossier@free.fr>
2019-04-05 11:21:49 +0000

    Make static fields final
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67754 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- ar-base/src/main/java/org/geogebra/arbase/ARMotionEvent.java
+++ ar-base/src/main/java/org/geogebra/arbase/ARMotionEvent.java
@@ -2,16 +2,16 @@ package org.geogebra.arbase;
 
 abstract public class ARMotionEvent {
 
-    public static int FIRST_FINGER_DOWN = 0;
-    public static int FIRST_FINGER_UP = 1;
-    public static int ON_MOVE = 2;
-    public static int ACTION_CANCEL = 3;
-    public static int SECOND_FINGER_DOWN = 5;
-    public static int SECOND_FINGER_UP = 6;
-    public static int TOUCHES_BEGAN = 7;
-    public static int TOUCHES_MOVED = 8;
-    public static int TOUCHES_ENDED = 9;
-    public static int TOUCHES_CANCELLED = 10;
+    final public static int FIRST_FINGER_DOWN = 0;
+    final public static int FIRST_FINGER_UP = 1;
+    final public static int ON_MOVE = 2;
+    final public static int ACTION_CANCEL = 3;
+    final public static int SECOND_FINGER_DOWN = 5;
+    final public static int SECOND_FINGER_UP = 6;
+    final public static int TOUCHES_BEGAN = 7;
+    final public static int TOUCHES_MOVED = 8;
+    final public static int TOUCHES_ENDED = 9;
+    final public static int TOUCHES_CANCELLED = 10;
 
     public abstract float getX();
 
