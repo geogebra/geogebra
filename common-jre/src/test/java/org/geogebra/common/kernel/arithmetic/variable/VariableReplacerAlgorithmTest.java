6091e91e460f56d0363f57edd50dc1cb449072e1
Zbyněk Konečný <zbynek@geogebra.org>
2019-04-09 13:41:14 +0000

    APPS-678 more tests
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67798 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common-jre/src/test/java/org/geogebra/common/kernel/arithmetic/variable/VariableReplacerAlgorithmTest.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/arithmetic/variable/VariableReplacerAlgorithmTest.java
@@ -1,11 +1,15 @@
 package org.geogebra.common.kernel.arithmetic.variable;
 
 import org.geogebra.common.BaseUnitTest;
+import org.geogebra.common.jre.util.TestStringUtil;
+import org.geogebra.common.kernel.StringTemplate;
 import org.geogebra.common.kernel.arithmetic.ExpressionValue;
 import org.geogebra.common.kernel.arithmetic.variable.power.Base;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.himamis.retex.editor.share.util.Unicode;
 
 public class VariableReplacerAlgorithmTest extends BaseUnitTest {
 
@@ -18,9 +22,22 @@ public class VariableReplacerAlgorithmTest extends BaseUnitTest {
 	}
 
 	@Test
+	public void testPower() {
+		shouldReplaceAs("pixxyyy",
+				Unicode.PI_STRING + TestStringUtil.unicode(" x^2 y^3"));
+	}
+
+	@Test
 	public void testLog() {
-		ExpressionValue replacement = variableReplacerAlgorithm.replace("log_{2}2");
-		Assert.assertEquals("log(2, 2)", replacement.toString());
+		shouldReplaceAs("log_{2}2", "log(2, 2)");
+		// TODO shouldReplaceAs("log_22", "log(2, 2)");
+		// TODO shouldReplaceAs("log_{2}xx", "log(2, x^2)");
+	}
+
+	private void shouldReplaceAs(String in, String out) {
+		ExpressionValue replacement = variableReplacerAlgorithm.replace(in);
+		Assert.assertEquals(out,
+				replacement.toString(StringTemplate.defaultTemplate));
 	}
 
 	@Test
