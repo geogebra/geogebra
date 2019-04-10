df2e8781b79957f70900439e3092a6610cfb8995
Zbyněk Konečný <zbynek@geogebra.org>
2019-04-10 09:01:58 +0000

    Remove dead code
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@67806 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/main/settings/ToolbarSettings.java
+++ common/src/main/java/org/geogebra/common/main/settings/ToolbarSettings.java
@@ -2,8 +2,6 @@ package org.geogebra.common.main.settings;
 
 import java.util.LinkedList;
 
-import org.geogebra.common.GeoGebraConstants.Versions;
-import org.geogebra.common.gui.toolcategorization.ToolCategorization;
 import org.geogebra.common.gui.toolcategorization.ToolCategorization.AppType;
 import org.geogebra.common.gui.toolcategorization.ToolCategorization.ToolsetLevel;
 import org.geogebra.common.main.AppConfig;
@@ -93,48 +91,6 @@ public class ToolbarSettings extends AbstractSettings {
 
 	public boolean isPhoneApp() {
 		return phoneApp;
-	}
-
-	/**
-	 * Update toolbar type for app version.
-	 * 
-	 * @param version
-	 *            app version
-	 */
-	public void setFrom(Versions version) {
-		switch (version) {
-		case ANDROID_NATIVE_GRAPHING:
-		case ANDROID_CAS:
-		case IOS_NATIVE:
-		case IOS_CAS:
-			type = ToolCategorization.AppType.GRAPHING_CALCULATOR;
-			phoneApp = true;
-			break;
-		case ANDROID_GEOMETRY:
-		case IOS_GEOMETRY:
-			type = ToolCategorization.AppType.GEOMETRY_CALC;
-			phoneApp = true;
-			break;
-		case ANDROID_NATIVE_3D:
-			type = ToolCategorization.AppType.GRAPHER_3D;
-			phoneApp = true;
-			break;
-		case WEB_GRAPHING:
-		case WEB_GRAPHING_OFFLINE:
-			type = ToolCategorization.AppType.GRAPHING_CALCULATOR;
-			break;
-		case WEB_GEOMETRY:
-		case WEB_GEOMETRY_OFFLINE:
-		case WINDOWS_STORE:
-			type = ToolCategorization.AppType.GEOMETRY_CALC;
-			break;
-		case WEB_3D_GRAPHING:
-			type = ToolCategorization.AppType.GRAPHER_3D;
-			break;
-		default:
-			type = ToolCategorization.AppType.GRAPHING_CALCULATOR;
-			break;
-		}
 	}
 
 	/**
