57c1d73891a27b4a0d7e53aab566945578757b65
Jan Borovsky <jan.borovsky@geogebra.org>
2019-05-09 11:16:48 +0000

    G3D-327: scale not set into modelMatrix in ARManager. Scale set in Renderer.
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@68234 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- ar-base/src/main/java/org/geogebra/arbase/ARManager.java
+++ ar-base/src/main/java/org/geogebra/arbase/ARManager.java
@@ -20,7 +20,6 @@ abstract public class ARManager<TouchEventType> implements ARManagerInterface<To
     protected CoordMatrix4x4 projectMatrix = new CoordMatrix4x4();
     protected CoordMatrix4x4 mModelMatrix = new CoordMatrix4x4();
     protected CoordMatrix4x4 mAnchorMatrix = new CoordMatrix4x4();
-    private CoordMatrix4x4 scaleMatrix = CoordMatrix4x4.identity();
     private CoordMatrix4x4 tmpMatrix1 = new CoordMatrix4x4();
     private CoordMatrix4x4 tmpMatrix2 = new CoordMatrix4x4();
     protected float mScaleFactor = 1;
@@ -244,14 +243,6 @@ abstract public class ARManager<TouchEventType> implements ARManagerInterface<To
     }
 
     protected void updateModelMatrixFields() {
-        /* Scaling */
-        if (arGestureManager != null) {
-            mScaleFactor = arGestureManager.getScaleFactor();
-        }
-
-        /* Scaling */
-        scaleMatrix.setDiag(mScaleFactor);
-
         /* translating */
         translationOffset.setSub3(rayEndOrigin, lastHitOrigin);
     }
@@ -264,8 +255,7 @@ abstract public class ARManager<TouchEventType> implements ARManagerInterface<To
     }
 
     protected void updateModelMatrix(App app) {
-        /* Scaling */
-        mModelMatrix.setMul(mAnchorMatrix, scaleMatrix);
+        mModelMatrix.set(mAnchorMatrix);
 
         updateModelMatrixForRotation(app);
 
@@ -359,7 +349,6 @@ abstract public class ARManager<TouchEventType> implements ARManagerInterface<To
         // used in iOS
         translationOffset.set(0,0,0);
         previousTranslationOffset.set(0,0,0);
-        scaleMatrix = CoordMatrix4x4.identity();
     }
 
     /**
@@ -379,6 +368,10 @@ abstract public class ARManager<TouchEventType> implements ARManagerInterface<To
 
     private void setHittingOriginAndDirection(ARMotionEvent arMotionEvent) {
             setHittingOriginAndDirection(arMotionEvent.getX(), arMotionEvent.getY());
+    }
+
+    public float getGestureScaleFactor() {
+        return arGestureManager.getScaleFactor();
     }
 
 }
