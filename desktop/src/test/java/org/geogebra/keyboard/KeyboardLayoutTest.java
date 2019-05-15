721035c2a9480a2439ebf092c2d4956e53e06bc6
Zbyněk Konečný <zbynek@geogebra.org>
2019-05-15 16:09:46 +0000

    APPS-856 fix failing test
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@68316 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/test/java/org/geogebra/keyboard/KeyboardLayoutTest.java
+++ desktop/src/test/java/org/geogebra/keyboard/KeyboardLayoutTest.java
@@ -25,10 +25,10 @@ public class KeyboardLayoutTest {
 		}
 
 		Assert.assertEquals(
-				"\u221E,\u225F,\u2260,\u2227,\u2228,\u2192,\u00AC,\u2297,\u2225,\u27c2,EMPTY_IMAGE,\u2208,\u2282,\u2286,\u2220,FLOOR,CEIL,&,@,#,EMPTY_IMAGE,EMPTY_IMAGE,[,],:,\",Translate.currency,\u2032,\u2033,EMPTY_IMAGE,BACKSPACE_DELETE,ABC,,,\', ,LEFT_ARROW,RIGHT_ARROW,RETURN_ENTER,",
+				"\u221E,\u225F,\u2260,\u2227,\u2228,\u2192,\u00AC,\u2297,\u2225,\u27c2,\u2208,\u2282,\u2286,\u2220,FLOOR,CEIL,&,@,#,Translate.currency,EMPTY_IMAGE,(,),[,],:,\",\u2032,\u2033,EMPTY_IMAGE,BACKSPACE_DELETE,ABC,,,\', ,LEFT_ARROW,RIGHT_ARROW,RETURN_ENTER,",
 				resources.toString());
 		Assert.assertEquals(
-				"\u221E,\u225F,\u2260,\u2227,\u2228,\u2192,\u00AC,\u2297,\u2225,\u27c2,NONE,\u2208,\u2282,\u2286,\u2220,\u230A,\u2308,&,@,#,NONE,NONE,[,],:,\",Translate.currency,\u2032,\u2033,NONE,BACKSPACE_DELETE,SWITCH_TO_ABC,,,\', ,LEFT_CURSOR,RIGHT_CURSOR,RETURN_ENTER,",
+				"\u221E,\u225F,\u2260,\u2227,\u2228,\u2192,\u00AC,\u2297,\u2225,\u27c2,\u2208,\u2282,\u2286,\u2220,\u230A,\u2308,&,@,#,Translate.currency,NONE,(,),[,],:,\",\u2032,\u2033,NONE,BACKSPACE_DELETE,SWITCH_TO_ABC,,,\', ,LEFT_CURSOR,RIGHT_CURSOR,RETURN_ENTER,",
 				actions.toString());
 
 	}
