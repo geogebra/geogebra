d6d27cbd276f71d7b9ffe61ccc207cd1209f078d
Laszlo Gal <laszlo@geogebra.org>
2019-05-27 07:12:44 +0000

    MOW-754 javadoc for LoadSpinner
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@68483 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/html5/gui/laf/LoadSpinner.java
+++ web/src/main/java/org/geogebra/web/html5/gui/laf/LoadSpinner.java
@@ -5,26 +5,44 @@ import org.geogebra.web.html5.util.Dom;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style;
 
+/**
+ * Class to wrap load spinner
+ *
+ * @author laszlo
+ */
 public class LoadSpinner {
 	private final Element spinner;
 
+	/**
+	 * Constructor to wrap existsing spinner on the page.
+	 *
+	 * @param className
+	 * 					The classname of the existsing spinner.
+	 */
 	public LoadSpinner(String className) {
 		spinner = Dom.querySelector(className);
 	}
 
+	/**
+	 * Show spinner.
+	 */
 	public void show() {
-		if (spinner == null) {
-			return;
-		}
-
- 		spinner.getStyle().setDisplay(Style.Display.BLOCK);
+		setDisplay(Style.Display.BLOCK);
 	}
 
+	/**
+	 * Hide spinner.
+	 */
 	public void hide() {
+		setDisplay(Style.Display.NONE);
+	}
+
+	private void setDisplay(Style.Display display) {
 		if (spinner == null) {
 			return;
 		}
 
-		spinner.getStyle().setDisplay(Style.Display.NONE);
+		spinner.getStyle().setDisplay(display);
+
 	}
 }
