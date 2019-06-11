d4e1ecc558513f13ad983027835a10bce731ac0d
Michael Borcherds <michael@geogebra.org>
2019-06-11 10:43:09 +0000

    Quick fix for compile error
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@68660 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ retex/renderer-base/src/main/java/com/himamis/retex/renderer/share/ColorUtil.java
@@ -0,0 +1,10 @@
+package com.himamis.retex.renderer.share;
+
+import com.himamis.retex.renderer.share.platform.graphics.Color;
+
+public abstract class ColorUtil {
+
+	public static final Color BLACK = Colors.BLACK;
+	public static final Color WHITE = Colors.WHITE;
+
+}
