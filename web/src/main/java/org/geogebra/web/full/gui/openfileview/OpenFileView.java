c15fb0521b0b6c1aceefafedde76a70f3616db35
Zbyněk Konečný <zbynek@geogebra.org>
2019-06-20 13:50:10 +0000

    Remove unused import
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@68818 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/full/gui/openfileview/OpenFileView.java
+++ web/src/main/java/org/geogebra/web/full/gui/openfileview/OpenFileView.java
@@ -34,7 +34,6 @@ import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Widget;
 
