89c363bad49e19b895554eaa023a8ba9d233f3cf
Michael Borcherds <michael@geogebra.org>
2019-06-23 05:30:02 +0000

    add missing implementation of MatrixTransformable to GeoRay (fixes AttachCopyToView(ray,1))
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@68839 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- common/src/main/java/org/geogebra/common/kernel/geos/GeoRay.java
+++ common/src/main/java/org/geogebra/common/kernel/geos/GeoRay.java
@@ -428,4 +428,20 @@ final public class GeoRay extends GeoLine implements LimitedPath, GeoRayND {
 		return algo.getRay();
 	}
 
+	@Override
+	public void matrixTransform(double p, double q, double r, double s) {
+
+		super.matrixTransform(p, q, r, s);
+
+		startPoint.matrixTransform(p, q, r, s);
+	}
+
+	@Override
+	public void matrixTransform(double a00, double a01, double a02, double a10, double a11,
+			double a12, double a20, double a21, double a22) {
+		super.matrixTransform(a00, a01, a02, a10, a11, a12, a20, a21, a22);
+
+		startPoint.matrixTransform(a00, a01, a02, a10, a11, a12, a20, a21, a22);
+	}
+
 }
