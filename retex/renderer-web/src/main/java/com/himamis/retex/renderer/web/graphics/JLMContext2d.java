912592ec18073d77c61eb0083c3a53896dd34f96
Zbyněk Konečný <zbynek@geogebra.org>
2019-06-26 12:37:01 +0000

    Remove dead code, empty lines, add Javadoc
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@68890 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- retex/renderer-web/src/main/java/com/himamis/retex/renderer/web/graphics/JLMContext2d.java
+++ retex/renderer-web/src/main/java/com/himamis/retex/renderer/web/graphics/JLMContext2d.java
@@ -47,62 +47,12 @@ public class JLMContext2d extends Context2d {
 
 	/**
 	 * Fills the current path.
+	 * 
+	 * @param windingRule
+	 *            winding rule, either "nonzero" or "evenodd"
 	 */
 	public final native void fill(String windingRule) /*-{
 		this.fill(windingRule);
-	}-*/;
-
-	public final native boolean isSVGCanvas() /*-{
-		return !!this.createPatternSVG;
-	}-*/;
-
-	public final native boolean isCanvgLoaded() /*-{
-		return !!$wnd.canvg;
-	}-*/;
-
-	/**
-	 * draw with canvg
-	 *
-	 * @param SVG_XML_OR_PATH_TO_SVG
-	 * @param dx
-	 * @param dy
-	 * @param dw
-	 * @param dh
-	 */
-	public final native void drawSvg(Canvas canvas,
-			String SVG_XML_OR_PATH_TO_SVG, double dx, double dy, double dw,
-			double dh) /*-{
-		if ($wnd.canvg) {
-			console.log("drawing with canvg");
-			//this.drawSvg(SVG_XML_OR_PATH_TO_SVG, dx, dy, dw, dh);
-			//			$wnd.canvg(canvas, SVG_XML_OR_PATH_TO_SVG, {
-			//				ignoreMouse : true,
-			//				ignoreAnimation : true,
-			//				ignoreDimensions : true,
-			//				ignoreClear : true,
-			//				offsetX : dx,
-			//				offsetY : dy,
-			//				scaleWidth : dw,
-			//				scaleHeight : dh
-			//			});
-
-			var cOpts = {
-				ignoreMouse : true,
-				ignoreAnimation : true,
-				ignoreDimensions : true,
-				ignoreClear : true,
-				offsetX : 0,//dx,
-				offsetY : 0,//dy,
-				scaleWidth : dw,
-				scaleHeight : dh
-			}
-
-			this.translate(-dx, -dy);
-			$wnd.canvg(this, SVG_XML_OR_PATH_TO_SVG, cOpts);
-			this.translate(dx, dy);
-		} else {
-			console.log("canvg not available");
-		}
 	}-*/;
 
 	/**
@@ -121,11 +71,9 @@ public class JLMContext2d extends Context2d {
 		};
 		//console.log("createPatternSVG", svg);
 		return this.createPatternSVG(svg, "repeating");
-
 	}-*/;
 
 	public final native void initTransform() /*-{
-
 		if (!this) {
 			// 3D View, ignore
 			return;
@@ -142,30 +90,22 @@ public class JLMContext2d extends Context2d {
 		this.m12_ = 0;
 
 		this.ggbTransformCache = [];
-
 	}-*/;
 
 	public final native void setDevicePixelRatio(double devicePixelRatio) /*-{
-
 		this.ggbDevicePixelRatio = devicePixelRatio;
-
 	}-*/;
 
 	public final native double getDevicePixelRatio() /*-{
-
 		return this.ggbDevicePixelRatio || 1;
-
 	}-*/;
 
 	public final native void saveTransform(double m00, double m10, double m01,
 			double m11, double m02, double m12) /*-{
-
 		this.ggbTransformCache.push([ m00, m10, m01, m11, m02, m12 ]);
-
 	}-*/;
 
 	public final native void saveTransform() /*-{
-
 		if (!this.ggbTransformCache) {
 			this.ggbTransformCache = [];
 		}
@@ -176,7 +116,6 @@ public class JLMContext2d extends Context2d {
 		this.ggbTransformCache.push([ t[0], t[1], t[2], t[3], t[4], t[5] ]);
 
 		this.save();
-
 	}-*/;
 
 	public final native void restoreTransform() /*-{
@@ -191,8 +130,6 @@ public class JLMContext2d extends Context2d {
 			return;
 		}
 
-		//console.log("t = ", t);
-
 		this.m00_ = t[0];
 		this.m10_ = t[1];
 		this.m01_ = t[2];
@@ -201,47 +138,33 @@ public class JLMContext2d extends Context2d {
 		this.m12_ = t[5];
 
 		this.restore();
-
 	}-*/;
 
 	public final native double getScaleX() /*-{
-
 		return this.m00_;
-
 	}-*/;
 
 	public final native double getShearY() /*-{
-
 		return this.m10_;
-
 	}-*/;
 
 	public final native double getShearX() /*-{
-
 		return this.m01_;
-
 	}-*/;
 
 	public final native double getScaleY() /*-{
-
 		return this.m11_;
-
 	}-*/;
 
 	public final native double getTranslateX() /*-{
-
 		return this.m02_;
-
 	}-*/;
 
 	public final native double getTranslateY() /*-{
-
 		return this.m12_;
-
 	}-*/;
 
 	public final native void scale2(double sx, double sy) /*-{
-
 		this.m00_ *= sx;
 		this.m10_ *= sx;
 		this.m01_ *= sy;
@@ -256,7 +179,6 @@ public class JLMContext2d extends Context2d {
 		this.m12_ += dx * this.m10_ + dy * this.m11_;
 
 		this.translate(dx, dy);
-
 	}-*/;
 
 	// adapted from TransformW.java
@@ -300,12 +222,10 @@ public class JLMContext2d extends Context2d {
 		}
 
 		this.rotate(theta);
-
 	}-*/;
 
 	public final native void setTransform2(double m00, double m10, double m01,
 			double m11, double m02, double m12) /*-{
-
 		this.m00_ = m00;
 		this.m10_ = m10;
 		this.m01_ = m01;
@@ -327,7 +247,6 @@ public class JLMContext2d extends Context2d {
 
 		this.setTransform(dp * this.m00_, dp * this.m10_, dp * this.m01_, dp
 				* this.m11_, dp * this.m02_, dp * this.m12_);
-
 	}-*/;
 
 	// adapted from goog.graphics.AffineTransform.prototype.concatenate
@@ -348,26 +267,22 @@ public class JLMContext2d extends Context2d {
 
 		// XXX should this be setTransform()?
 		this.transform(m00, m10, m01, m11, m02, m12);
-
 	}-*/;
 
-	final public void fill(Shape s) {
-		if (s instanceof Rectangle2D) {
-			Rectangle2D rect = (Rectangle2D) s;
-			this.fillRect(rect.getX(), rect.getY(), rect.getWidth(),
+	/**
+	 * @param shape
+	 *            shape
+	 */
+	final public void fill(Shape shape) {
+		if (shape instanceof Rectangle2D) {
+			Rectangle2D rect = (Rectangle2D) shape;
+			fillRect(rect.getX(), rect.getY(), rect.getWidth(),
 					rect.getHeight());
-		} else if (s instanceof AreaW) {
-
-			AreaW area = (AreaW) s;
-
+		} else if (shape instanceof AreaW) {
+			AreaW area = (AreaW) shape;
 			area.fill(this);
-
-		} else if (s instanceof ShapeW) {
-
-			ShapeW shape = (ShapeW) s;
-
-			shape.fill(this);
-
+		} else if (shape instanceof ShapeW) {
+			((ShapeW) shape).fill(this);
 		}
 	}
 
