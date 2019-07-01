282371ceb2ad0b9b51a1eba865ac539fad22bd87
Michael Borcherds <michael@geogebra.org>
2019-07-01 08:57:12 +0000

    fix warnings (missing @override)
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@68959 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- web/src/main/java/org/geogebra/web/full/css/MebisResources.java
+++ web/src/main/java/org/geogebra/web/full/css/MebisResources.java
@@ -1,8 +1,9 @@
 package org.geogebra.web.full.css;
 
+import org.geogebra.web.resources.SassResource;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
-import org.geogebra.web.resources.SassResource;
 
 /**
  * Resources related to the mebis Notes app.
@@ -11,15 +12,19 @@ public interface MebisResources extends ClientBundle, StylesProvider {
 
 	MebisResources INSTANCE = GWT.create(MebisResources.class);
 
+	@Override
 	@ClientBundle.Source("org/geogebra/web/resources/scss/mebis/mow.scss")
 	SassResource mowStyle();
 
+	@Override
 	@ClientBundle.Source("org/geogebra/web/resources/scss/mebis/mow-toolbar.scss")
 	SassResource mowToolbarStyle();
 
+	@Override
 	@ClientBundle.Source("org/geogebra/web/resources/scss/mebis/open-screen.scss")
 	SassResource openScreenStyle();
 
+	@Override
 	@ClientBundle.Source("org/geogebra/web/resources/scss/mebis/dialog-styles.scss")
 	SassResource dialogStylesScss();
 }
