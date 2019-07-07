ef8547e902f0d8a0bdbab1df429ee0dd1a9d7e5c
Zbyněk Konečný <zbynek@geogebra.org>
2019-07-07 15:10:00 +0000

    Make localization work in common-jre
    git-svn-id: http://dev.geogebra.org/svn/trunk/geogebra@69090 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- desktop/src/test/java/org/geogebra/common/kernel/commands/SyntaxLocalizationTest.java
+++ desktop/src/test/java/org/geogebra/common/kernel/commands/SyntaxLocalizationTest.java
@@ -1,40 +1,36 @@
 package org.geogebra.common.kernel.commands;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import org.geogebra.common.jre.headless.AppCommon;
-import org.geogebra.common.main.Localization;
+import org.geogebra.common.jre.headless.LocalizationCommon;
 import org.geogebra.desktop.factories.AwtFactoryD;
-import org.geogebra.desktop.main.LocalizationD;
 import org.geogebra.test.commands.CommandSignatures;
 import org.junit.Assert;
 import org.junit.Test;
+
 
 public class SyntaxLocalizationTest {
 
 	@Test
 	public void checkSyntaxTranslations() {
-		LocalizationD loc = new LocalizationD(3);
-		loc.setApp(new AppCommon(loc, new AwtFactoryD()));
+		LocalizationCommon loc = new LocalizationCommon(3);
+		AppCommon app = new AppCommon(loc, new AwtFactoryD());
+		loc.setApp(app);
 
 		for (Commands cmd : Commands.values()) {
-			String syntax = loc.getCommand(cmd + Localization.syntaxStr);
-			if (!syntax.contains(Localization.syntaxStr)
-					&& !cmd.name().equals("ExportImage")) {
-				ArrayList<Integer> signature = new ArrayList<>();
-				for (String line : syntax.split("\n")) {
-					if ("[]".equals(line) || "[ ]".equals(line)) {
-						signature.add(0);
-					} else {
-						signature.add(
-								line.replace("x, y", "xy").split(",").length);
-					}
-				}
-				Assert.assertArrayEquals(cmd.name(), signature.toArray(),
-						CommandSignatures.getSigneture(cmd.name()).toArray());
-
+			if (cmd == Commands.ShowSteps
+					|| cmd.getTable() == CommandsConstants.TABLE_ENGLISH
+					|| cmd.getTable() == CommandsConstants.TABLE_CAS
+					|| cmd.name().toLowerCase(Locale.US).equals(cmd.name())
+					|| NoExceptionsTest.betaCommand(cmd, app)) {
+				continue;
 			}
-
+			List<Integer> signature = CommandSignatures
+					.getSigneture(cmd.name());
+			int size = signature == null ? 0 : signature.size();
+			Assert.assertNotEquals(cmd.name() + " has no syntax", 0, size);
 		}
 	}
 }
