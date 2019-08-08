f5781218a2e24b87c89640b8d1bedbb2a15b1a9f
Jenkins <jenkins@geogebra.org>
2019-08-08 12:32:07 +0200

    Version bump 5.0.555.0

--- common/src/main/java/org/geogebra/common/GeoGebraConstants.java
+++ common/src/main/java/org/geogebra/common/GeoGebraConstants.java
@@ -10,9 +10,9 @@ public interface GeoGebraConstants {
 	// as it is read by the build system
 	// and updated automatically by the "Version Bump" task
 	/** last build date */
-	public static final String BUILD_DATE = "05 August 2019";
+	public static final String BUILD_DATE = "08 August 2019";
 	/** complete version string */
-	public static final String VERSION_STRING = "5.0.554.0";
+	public static final String VERSION_STRING = "5.0.555.0";
 
 	/** proper noun, should NOT be translated / transliterated */
 	public static final String APPLICATION_NAME = "GeoGebra";
