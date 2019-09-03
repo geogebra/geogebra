85190498b480344a702f526e4b27b2fced4db39b
kjozsef84 <kjozsef84.jk@gmail.com>
2019-09-03 09:43:20 +0200

    APPS-1155: The failing test corrected

--- common-jre/src/test/java/org/geogebra/common/kernel/geos/GeoInputboxTest.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/geos/GeoInputboxTest.java
@@ -11,9 +11,10 @@ public class GeoInputboxTest extends BaseUnitTest {
     public void inputboxTest() {
         add("f = x+1");
         add("g = 2f(x+2)+1");
-        GeoElement inputBox1 = add("InputBox(f)");
-        GeoElement inputBox2 = add("InputBox(g)");
-        Assert.assertEquals(((GeoInputBox) inputBox1).getText(), "x + 1");
-        Assert.assertEquals(((GeoInputBox)inputBox2).getText(), "2f(x + 2) + 1");
+        GeoInputBox inputBox1 = (GeoInputBox) add("InputBox(f)");
+        GeoInputBox inputBox2 = (GeoInputBox) add("InputBox(g)");
+        inputBox2.setSymbolicMode(true, false);
+        Assert.assertEquals(inputBox1.getText(), "x + 1");
+        Assert.assertEquals(inputBox2.getTextForEditor(), "2f(x + 2) + 1");
     }
 }
