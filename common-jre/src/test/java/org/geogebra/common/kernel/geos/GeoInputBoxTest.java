af1ebb4e2334c54bd84ad52550c8d546df763b5c
kjozsef84 <kjozsef84.jk@gmail.com>
2019-09-04 14:53:10 +0200

    APPS-1155: Some changes in the test

--- common-jre/src/test/java/org/geogebra/common/kernel/geos/GeoInputBoxTest.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/geos/GeoInputBoxTest.java
@@ -1,18 +1,13 @@
 package org.geogebra.common.kernel.geos;
 
-import com.himamis.retex.renderer.share.TeXFormula;
-import com.himamis.retex.renderer.share.platform.FactoryProvider;
-import com.himamis.retex.renderer.share.serialize.TeXAtomSerializer;
 import org.geogebra.common.BaseUnitTest;
-import org.geogebra.common.io.FactoryProviderCommon;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class LinkedGeosForInputBoxTest extends BaseUnitTest {
-
+public class GeoInputBoxTest extends BaseUnitTest {
 
     @Test
-    public void inputboxOutputTest() {
+    public void symbolicInputBoxUseDefinitionForFunctions() {
         add("f = x+1");
         add("g = 2f(x+2)+1");
         GeoInputBox inputBox1 = (GeoInputBox) add("InputBox(f)");
@@ -20,18 +15,14 @@ public class LinkedGeosForInputBoxTest extends BaseUnitTest {
         inputBox2.setSymbolicMode(true, false);
         Assert.assertEquals("x + 1", inputBox1.getText());
         Assert.assertEquals("2f(x + 2) + 1", inputBox2.getTextForEditor());
-}
-
+    }
 
     @Test
-    public void inputboxTextIsInLaTeX() {
+    public void symbolicInputBoxTextShouldBeInLaTeX() {
         add("f = x + 12");
         add("g = 2f(x + 1) + 2");
         GeoInputBox inputBox2 = (GeoInputBox) add("InputBox(g)");
         inputBox2.setSymbolicMode(true, false);
-        FactoryProvider.setInstance(new FactoryProviderCommon());
-        TeXFormula teXFormula = new TeXFormula(inputBox2.getText());
-        String s = new TeXAtomSerializer(null).serialize(teXFormula.root);
-        Assert.assertEquals("2 f(x+1)+2", s);
+        Assert.assertEquals("2 \\; f\\left(x + 1 \\right) + 2", inputBox2.getText());
     }
 }
