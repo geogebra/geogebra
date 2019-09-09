4c4eda917e467234f2943c3eb02355438e0e110d
agoston <agoston@geogebra.org>
2019-09-09 10:05:21 +0200

    APPS-798: BUTTON_HIGHLIGHTING feature flag removed, MyButton class simplified

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -3900,10 +3900,6 @@ public abstract class App implements UpdateSelection, AppInterface, EuclidianHos
 		case GEOMETRIC_DISCOVERY:
 			return prerelease;
 
-		/** APPS-634 */
-		case BUTTON_HIGHLIGHTING:
-			return true;
-
 		/** APPS-890 */
 		case AUTOLABEL_CAS_SETTINGS:
 			return true;
