a4d4c1c06b437bcbdfe56e3f62fc94e4cad36083
krisztina.frank <krisztina.frank@geogebra.org>
2019-09-18 08:44:47 +0200

    APPS-1246 Fix warning

--- common/src/main/java/org/geogebra/common/kernel/geos/GeoInputBox.java
+++ common/src/main/java/org/geogebra/common/kernel/geos/GeoInputBox.java
@@ -484,7 +484,7 @@ public class GeoInputBox extends GeoButton implements HasSymbolicMode, HasAlignm
 			textForGeo = getNonSymbolicNumberValue(linkedGeo);
 		}
 
-		if (!linkedGeo.isGeoText() && textForGeo.equals("?")) {
+		if ((linkedGeo == null || !linkedGeo.isGeoText()) && "?".equals(textForGeo)) {
 			textFieldToUpdate.setText("");
 			textForGeo = "";
 		}
