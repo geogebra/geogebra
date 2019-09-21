c1669772bcd760fc06b0069855f3e6feea342355
Zoltán Kovács <zoltan@geogebra.org>
2019-09-21 17:18:16 +0200

    Fix regexp for 0*...

--- common/src/main/java/org/geogebra/common/kernel/prover/AlgoCompare.java
+++ common/src/main/java/org/geogebra/common/kernel/prover/AlgoCompare.java
@@ -231,7 +231,7 @@ public class AlgoCompare extends AlgoElement {
                 "$1 " + Unicode.CENTER_DOT + " " + inp2 +
                         " $2 " + inp1 + " $3 $4 " + Unicode.CENTER_DOT + " " + inp2);
         // Remove "0*inp2 Less" from the beginning (it's trivial)
-        result = result.replaceAll("(^0 " + Unicode.CENTER_DOT + " \\w+ Less )", "");
+        result = result.replaceAll("^0 " + Unicode.CENTER_DOT + " .*? Less ", "");
         // m >= 1/2
         result = result.replaceAll("m >= (.*)",
                 inp1 + " GreaterEqual $1 " + Unicode.CENTER_DOT + " " + inp2);
