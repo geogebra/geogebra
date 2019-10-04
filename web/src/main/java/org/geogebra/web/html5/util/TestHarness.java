b380b753454cc7f9719c234a6754cd0879f3963a
Zbynek Konecny <zbynek@geogebra.at>
2019-10-04 20:03:59 +0200

    Add NPE check

--- web/src/main/java/org/geogebra/web/html5/util/TestHarness.java
+++ web/src/main/java/org/geogebra/web/html5/util/TestHarness.java
@@ -4,8 +4,17 @@ import com.google.gwt.user.client.ui.UIObject;
 
 public class TestHarness {
 
+	/**
+	 * Set data-test attribute
+	 * As using XPaths and CSS selectors is unstable and brittle
+	 * please add this attribute to any widget you want to refer to
+	 * in your UI tests
+	 * @param widget widget to set
+	 * @param value value of data-test attribute
+	 */
 	public static void setAttr(UIObject widget, String value) {
-		widget.getElement().setAttribute("data-test", value);
+		if (widget != null) {
+			widget.getElement().setAttribute("data-test", value);
+		}
 	}
-
 }
