15848337212d0c1445a3edec837900160707e000
Zbynek Konecny <zbynek@geogebra.at>
2019-10-15 19:42:55 +0200

    Fix compile error

--- common-jre/src/test/java/org/geogebra/common/gui/toolcategorization/impl/GraphingGraphingToolCollectionFactoryTest.java
+++ common-jre/src/test/java/org/geogebra/common/gui/toolcategorization/impl/GraphingGraphingToolCollectionFactoryTest.java
@@ -1,6 +1,9 @@
 package org.geogebra.common.gui.toolcategorization.impl;
 
+import java.util.List;
+
 import org.geogebra.common.gui.toolcategorization.GraphingToolSet;
+import org.geogebra.common.gui.toolcategorization.ToolCategory;
 import org.geogebra.common.gui.toolcategorization.ToolCollection;
 import org.junit.Assert;
 import org.junit.Before;
@@ -8,29 +11,28 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.List;
-
 /**
  * Test class for GraphingTools.
  */
 @RunWith(MockitoJUnitRunner.class)
 public class GraphingGraphingToolCollectionFactoryTest {
 
-    private ToolCollection toolCollection;
+	private ToolCollection toolCollection;
 
-    @Before
-    public void setupTest() {
-        toolCollection = new GraphingToolCollectionFactory().createToolCollection();
-    }
+	@Before
+	public void setupTest() {
+		toolCollection = new GraphingToolCollectionFactory()
+				.createToolCollection();
+	}
 
-    @Test
-    public void testGraphingTools() {
-        List<String> categories = toolCollection.getCategories();
-        for (int i = 0; i < categories.size(); i++) {
-            for (int tool : toolCollection.getTools(i)) {
-                Assert.assertFalse("Should not be available" + tool,
-                        GraphingToolSet.isInGraphingToolSet(tool));
-            }
-        }
-    }
+	@Test
+	public void testGraphingTools() {
+		List<ToolCategory> categories = toolCollection.getCategories();
+		for (int i = 0; i < categories.size(); i++) {
+			for (int tool : toolCollection.getTools(i)) {
+				Assert.assertFalse("Should not be available" + tool,
+						GraphingToolSet.isInGraphingToolSet(tool));
+			}
+		}
+	}
 }
