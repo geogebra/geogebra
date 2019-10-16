8505ec7895cf7ba452b4ea7bea6cb6b61d5957e6
agoston <agoston@geogebra.org>
2019-10-16 13:08:06 +0200

    APPS-1346: fix typo

--- common/src/main/java/org/geogebra/common/gui/toolcategorization/ToolsetLevel.java
+++ common/src/main/java/org/geogebra/common/gui/toolcategorization/ToolsetLevel.java
@@ -35,7 +35,7 @@ public enum ToolsetLevel {
     }
 
     public ToolsetLevel getPrevious() {
-        if (ordinal() > 1) {
+        if (ordinal() > 0) {
             return ToolsetLevel.values()[ordinal() - 1];
         }
 
