c8d6f01d1271368aaf4bc4b186fd3d52eaa613d9
Zbyněk Konečný <zbynek@geogebra.org>
2019-10-16 13:12:44 +0200

    APPS-1283 Accessibility elements shouldn't obscure external UI

--- web/src/main/java/org/geogebra/web/html5/gui/accessibility/AccessibilityView.java
+++ web/src/main/java/org/geogebra/web/html5/gui/accessibility/AccessibilityView.java
@@ -19,7 +19,6 @@ import org.geogebra.web.html5.gui.BaseWidgetFactory;
 import org.geogebra.web.html5.main.AppW;
 import org.geogebra.web.html5.util.sliderPanel.SliderW;
 
-import com.google.gwt.dom.client.Style.Position;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -45,7 +44,6 @@ public class AccessibilityView implements View {
 		this.controls = sliderFactory.newPanel();
 		controls.addStyleName("accessibilityView");
 		this.sliderFactory = sliderFactory;
-		hideUIElement(controls);
 		widgets = new HashMap<>();
 		app.getKernel().attach(this);
 		new Timer() {
@@ -278,11 +276,4 @@ public class AccessibilityView implements View {
 		range.getElement().setAttribute("aria-valuetext",
 				app.getKernel().format(value, StringTemplate.screenReader) + " " + unit);
 	}
-
-	/** For buttons we need to make sure click handler still works */
-	private static void hideUIElement(Widget range) {
-		range.getElement().getStyle().setOpacity(.01);
-		range.getElement().getStyle().setPosition(Position.FIXED);
-	}
-
 }
