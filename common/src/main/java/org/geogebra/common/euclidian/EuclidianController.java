086c837b7d8b89712ebc4b69508be1124f216a63
Zbynek Konecny <zbynek@geogebra.at>
2019-11-13 20:17:48 +0100

    Javadoc

--- common/src/main/java/org/geogebra/common/euclidian/EuclidianController.java
+++ common/src/main/java/org/geogebra/common/euclidian/EuclidianController.java
@@ -10027,17 +10027,20 @@ public abstract class EuclidianController implements SpecialPointsListener {
 		kernel.notifyRepaint();
 	}
 
+	/**
+	 * Update bounding box to match selection
+	 * 
+	 * @param crop
+	 *            whether the box should be in crop mode
+	 */
 	public void updateBoundingBoxFromSelection(boolean crop) {
 		List<GeoElement> sel = selection.getSelectedGeos();
 		if (specialBoundingBoxNeeded(crop)) {
 			Drawable dr = ((Drawable) view.getDrawableFor(sel.get(0)));
 			BoundingBox boundingBox = dr.getBoundingBox();
-
 			view.setBoundingBox(boundingBox);
 			view.repaintView();
-		}
-		// multi-selection
-		else {
+		} else { // multi-selection
 			setBoundingBoxFromList(sel);
 		}
 	}
