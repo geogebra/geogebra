55f3f9e2da7c747369a8489b27a2211f1e432133
Zbynek Konecny <zbynek@geogebra.at>
2019-11-13 15:27:53 +0100

    Remove unused import

--- web/src/main/java/org/geogebra/web/full/main/activity/EvaluatorActivity.java
+++ web/src/main/java/org/geogebra/web/full/main/activity/EvaluatorActivity.java
@@ -6,7 +6,6 @@ import org.geogebra.web.full.evaluator.EvaluatorEditor;
 import org.geogebra.web.html5.gui.GeoGebraFrameW;
 import org.geogebra.web.html5.main.ApiExporter;
 import org.geogebra.web.html5.main.AppW;
-import org.geogebra.web.html5.util.ArticleElement;
 
 /**
  * Evaluator Activity.
