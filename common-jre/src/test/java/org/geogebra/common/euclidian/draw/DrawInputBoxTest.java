f1c3ff856b2c702bf6e2cd21221b2dbdc790af94
krisztina.frank <krisztina.frank@geogebra.org>
2019-11-25 14:31:17 +0100

    APPS-1407 Add tests

--- common-jre/src/test/java/org/geogebra/common/euclidian/draw/DrawInputBoxTest.java
+++ common-jre/src/test/java/org/geogebra/common/euclidian/draw/DrawInputBoxTest.java
@@ -1,11 +1,38 @@
 package org.geogebra.common.euclidian.draw;
 
 import org.geogebra.common.BaseUnitTest;
+import org.geogebra.common.euclidian.EuclidianView;
 import org.geogebra.common.kernel.geos.GeoInputBox;
+import org.geogebra.common.util.TextObject;
+import org.geogebra.test.euclidian.TextFieldCommonJre;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class DrawInputBoxTest extends BaseUnitTest {
+
+	private TextObject textObject = new TextObject() {
+		String content;
+
+		@Override
+		public String getText() {
+			return content;
+		}
+
+		@Override
+		public void setText(String s) {
+			content = s;
+		}
+
+		@Override
+		public void setVisible(boolean b) {
+
+		}
+
+		@Override
+		public void setEditable(boolean b) {
+
+		}
+	};
 
 	@Test
 	public void testConsistentHeight() {
@@ -16,6 +43,63 @@ public class DrawInputBoxTest extends BaseUnitTest {
 		int symbolicInputBoxHeight = getHeightOfInputBox(inputBoxDrawer, true);
 		int nonSymbolicInputBoxHeight = getHeightOfInputBox(inputBoxDrawer, false);
 		Assert.assertEquals(symbolicInputBoxHeight, nonSymbolicInputBoxHeight);
+	}
+
+	@Test
+	public void testDefaultHeightForEmptyInput() {
+		add("a=1");
+		GeoInputBox inputBox = (GeoInputBox) add("InputBox(a)");
+		inputBox.setSymbolicMode(true, false);
+		textObject.setText("");
+		inputBox.textObjectUpdated(textObject);
+
+		DrawInputBox inputBoxDrawer = new DrawInputBox(getApp().getActiveEuclidianView(), inputBox);
+
+		int symbolicInputBoxHeight = getHeightOfInputBox(inputBoxDrawer, true);
+		int nonSymbolicInputBoxHeight = getHeightOfInputBox(inputBoxDrawer, false);
+
+		Assert.assertEquals(symbolicInputBoxHeight, nonSymbolicInputBoxHeight);
+	}
+
+	@Test
+	public void testDefaultHeightForFocusedInput() {
+		add("a=1");
+		GeoInputBox inputBox = (GeoInputBox) add("InputBox(a)");
+		inputBox.setSymbolicMode(true, false);
+
+		EuclidianView ev = getApp().getActiveEuclidianView();
+		ev.setViewTextField(new TextFieldCommonJre(ev));
+
+		DrawInputBox inputBoxDrawer = new DrawInputBox(getApp().getActiveEuclidianView(), inputBox);
+		int symbolicInputBoxHeightNotFocused = getHeightOfInputBox(inputBoxDrawer, true);
+
+		inputBoxDrawer.getTextField().requestFocus();
+
+		int symbolicInputBoxHeightFocused = getHeightOfInputBox(inputBoxDrawer, true);
+
+		Assert.assertEquals(symbolicInputBoxHeightNotFocused, symbolicInputBoxHeightFocused);
+	}
+
+	@Test
+	public void testHeightWontChangeAfterFirstCharacter() {
+		add("a=1");
+		GeoInputBox inputBox = (GeoInputBox) add("InputBox(a)");
+		inputBox.setSymbolicMode(true, false);
+
+		EuclidianView ev = getApp().getActiveEuclidianView();
+		ev.setViewTextField(new TextFieldCommonJre(ev));
+
+		textObject.setText("");
+		inputBox.textObjectUpdated(textObject);
+
+		DrawInputBox inputBoxDrawer = new DrawInputBox(getApp().getActiveEuclidianView(), inputBox);
+
+		int symbolicInputBoxHeightEmptyInput = getHeightOfInputBox(inputBoxDrawer, true);
+		textObject.setText("2");
+		inputBox.textObjectUpdated(textObject);
+
+		int symbolicInputBoxHeight = getHeightOfInputBox(inputBoxDrawer, true);
+		Assert.assertEquals(symbolicInputBoxHeightEmptyInput, symbolicInputBoxHeight);
 	}
 
 	private int getHeightOfInputBox(DrawInputBox inputBoxDrawer, boolean symbolicMode) {
