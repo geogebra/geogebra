ac7803d6746a80cd327565489bab7061767d5fef
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2019-12-11 10:25:26 +0100

    fix spotbugs

--- common/src/main/java/org/geogebra/common/move/ggtapi/models/JSONParserGGT.java
+++ common/src/main/java/org/geogebra/common/move/ggtapi/models/JSONParserGGT.java
@@ -42,9 +42,15 @@ public class JSONParserGGT {
 		if (getString(obj, "type").length() > 0) {
 			try {
 				switch (getString(obj, "type")) {
-					case "ggs-template" : type = MaterialType.ggsTemplate;
-					case "notes-template" : type = MaterialType.notesTemplate;
-					default: type = MaterialType.valueOf(getString(obj, "type"));
+					case "ggs-template" :
+					    type = MaterialType.ggsTemplate;
+					    break;
+					case "notes-template" :
+					    type = MaterialType.notesTemplate;
+					    break;
+					default:
+					    type = MaterialType.valueOf(getString(obj, "type"));
+					    break;
 				}
 			} catch (Throwable t) {
 				Log.error("Unknown material type:" + getString(obj, "type"));
