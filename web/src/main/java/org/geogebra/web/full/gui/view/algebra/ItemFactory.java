bd2ea28366e6cd3d5a846b87d858f30e10c67c3f
Balázs Bencze <balazs.bencze@geogebra.org>
2020-01-21 10:26:27 +0200

    APPS-1265 Move shouldShowSlider into common

--- web/src/main/java/org/geogebra/web/full/gui/view/algebra/ItemFactory.java
+++ web/src/main/java/org/geogebra/web/full/gui/view/algebra/ItemFactory.java
@@ -1,27 +1,15 @@
 package org.geogebra.web.full.gui.view.algebra;
 
-import org.geogebra.common.kernel.arithmetic.MyDouble;
+import org.geogebra.common.gui.view.algebra.AlgebraItem;
 import org.geogebra.common.kernel.geos.GeoBoolean;
 import org.geogebra.common.kernel.geos.GeoElement;
-import org.geogebra.common.kernel.geos.GeoNumeric;
 
 /**
  * Helper methods for creating new AV items
  */
 public class ItemFactory {
-	private boolean slidersEnabled = true;
 
-	/**
-	 * 
-	 * @param geo
-	 *            geo element
-	 * @return if geo matches to SliderTreeItem.
-	 */
-	public boolean matchSlider(GeoElement geo) {
-		return slidersEnabled && geo instanceof GeoNumeric
-				&& ((GeoNumeric) geo).isShowingExtendedAV() && geo.isSimple()
-				&& MyDouble.isFinite(((GeoNumeric) geo).value);
-	}
+	private boolean slidersEnabled = true;
 
 	/**
 	 * @param geo
@@ -39,7 +27,7 @@ public class ItemFactory {
 	 */
 	public final RadioTreeItem createAVItem(final GeoElement ob) {
 		RadioTreeItem ti = null;
-		if (matchSlider(ob)) {
+		if (slidersEnabled && AlgebraItem.shouldShowSlider(ob)) {
 			ti = new SliderTreeItemRetex(ob);
 		} else if (matchCheckbox(ob)) {
 			ti = new CheckboxTreeItem(ob);
