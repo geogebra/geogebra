0c80a9eb3b405296d8ed078630a0d1a9124644d8
barnakaracsony <git8.XyZ>
2020-01-30 16:30:14 +0100

    APPS-1559: implement and test CreateSliderAction

--- web/src/main/java/org/geogebra/web/full/gui/view/algebra/contextmenu/action/CreateSliderAction.java
+++ web/src/main/java/org/geogebra/web/full/gui/view/algebra/contextmenu/action/CreateSliderAction.java
@@ -1,26 +1,32 @@
 package org.geogebra.web.full.gui.view.algebra.contextmenu.action;
 
-import org.geogebra.common.gui.view.algebra.AlgebraItem;
 import org.geogebra.common.kernel.geos.GeoElement;
-import org.geogebra.common.kernel.geos.GeoSymbolic;
+import org.geogebra.common.kernel.geos.GeoNumeric;
+import org.geogebra.common.scientific.LabelController;
 import org.geogebra.web.full.gui.view.algebra.MenuAction;
 import org.geogebra.web.full.main.AppWFull;
 
 public class CreateSliderAction extends MenuAction<GeoElement> {
 
+	private LabelController labelController;
+
 	public CreateSliderAction() {
 		super("CreateSlider");
+		labelController = new LabelController();
 	}
 
 	@Override
 	public void execute(GeoElement geo, AppWFull app) {
+		if (!(geo instanceof GeoNumeric)) {
+			return;
+		}
+		labelController.ensureHasLabel(geo);
+		((GeoNumeric) geo).setShowExtendedAV(true);
+		geo.getKernel().storeUndoInfo();
 	}
 
 	@Override
 	public boolean isAvailable(GeoElement geo) {
-		if (geo instanceof GeoSymbolic) {
-			return ((GeoSymbolic) geo).canBecomeSlider();
-		}
-		return AlgebraItem.shouldShowSlider(geo);
+		return geo instanceof GeoNumeric && !((GeoNumeric) geo).isShowingExtendedAV();
 	}
 }
