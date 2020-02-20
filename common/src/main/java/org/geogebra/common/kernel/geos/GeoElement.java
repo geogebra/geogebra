a5bfcf5a64e518b6c9e06823ce5da1585aae1d62
barnakaracsony <git8.XyZ>
2020-02-20 13:45:14 +0100

    APPS-1265: make scicalc symbolic definitions visible

--- common/src/main/java/org/geogebra/common/kernel/geos/GeoElement.java
+++ common/src/main/java/org/geogebra/common/kernel/geos/GeoElement.java
@@ -4653,8 +4653,8 @@ public abstract class GeoElement extends ConstructionElement
 		}
 
 		// now handle non-GeoText prefixed with "="
-		else if ((algebraDesc.indexOf("=") > -1) && !geo.isGeoText()) {
-			if (includeLHS) {
+		else if (!geo.isGeoText()) {
+			if (includeLHS && algebraDesc.contains("=")) {
 				sb.append(algebraDesc.split("=")[0] + "\\, = \\,");
 			}
 			sb.append(geo.getFormulaString(tpl, substituteNumbers));
