3b05879659e16786f032baa8e90db4816cc747a7
Balázs Bencze <balazs.bencze@geogebra.org>
2020-02-27 14:06:09 +0200

    APPS-1689 Fix unused imports

--- common/src/main/java/org/geogebra/common/gui/menu/impl/SubmenuItemImpl.java
+++ common/src/main/java/org/geogebra/common/gui/menu/impl/SubmenuItemImpl.java
@@ -2,12 +2,8 @@ package org.geogebra.common.gui.menu.impl;
 
 import org.geogebra.common.gui.menu.ActionableItem;
 import org.geogebra.common.gui.menu.Icon;
-import org.geogebra.common.gui.menu.MenuItem;
 import org.geogebra.common.gui.menu.MenuItemGroup;
 import org.geogebra.common.gui.menu.SubmenuItem;
-
-import java.util.Arrays;
-import java.util.List;
 
 class SubmenuItemImpl extends AbtractMenuItem implements SubmenuItem {
 
