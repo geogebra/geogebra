c42cccc1278dac614d08c75410f8db45892fec3e
Balázs Bencze <balazs.bencze@geogebra.org>
2020-03-09 15:25:37 +0200

    APPS-1684 Remove unused imports

--- web/src/main/java/org/geogebra/web/full/main/AppWFull.java
+++ web/src/main/java/org/geogebra/web/full/main/AppWFull.java
@@ -80,7 +80,6 @@ import org.geogebra.web.full.gui.layout.panels.EuclidianDockPanelW;
 import org.geogebra.web.full.gui.layout.panels.ToolbarDockPanelW;
 import org.geogebra.web.full.gui.menu.MenuViewController;
 import org.geogebra.web.full.gui.menu.MenuViewListener;
-import org.geogebra.web.full.gui.menu.action.MenuActionHandlerFactory;
 import org.geogebra.web.full.gui.menubar.FileMenuW;
 import org.geogebra.web.full.gui.menubar.PerspectivesPopup;
 import org.geogebra.web.full.gui.openfileview.OpenFileView;
