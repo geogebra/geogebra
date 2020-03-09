8b46acb14389e5b180eb0c916660bb12bd320b89
janborovsky <janborovsky01@gmail.com>
2020-03-09 13:45:42 +0100

    APPS-1683 GraphingCommandArgumentFilter Integration test.

--- desktop/src/test/java/org/geogebra/common/kernel/commands/GraphingCommandArgumentFilterTest.java
+++ desktop/src/test/java/org/geogebra/common/kernel/commands/GraphingCommandArgumentFilterTest.java
@@ -6,8 +6,20 @@ import org.junit.Test;
 public class GraphingCommandArgumentFilterTest extends BaseUnitTest {
 
     @Test(expected = NullPointerException.class)
-    public void vhenExceptionThrownAnotherTry() {
+    public void parallelLineWithPointandLine() {
        getApp().setGraphingConfig();
-       addAvInput("Line((1,2), x)");
+       addAvInput("A = (1,1)");
+       addAvInput("B = (2,2)");
+       addAvInput("C = (3,2)");
+       addAvInput("f:Line(B,C)");
+       addAvInput("g:Line(A,f)");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void parallelLineWithPointandFunction() {
+        getApp().setGraphingConfig();
+        addAvInput("A = (1,2)");
+        addAvInput("f(x) = x");
+        addAvInput("g:Line(A,f)");
     }
 }
