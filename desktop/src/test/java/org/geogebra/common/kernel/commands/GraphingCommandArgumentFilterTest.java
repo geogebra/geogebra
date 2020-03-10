9a63b8e5594d3639509f95714fc46a4044fea07a
Balázs Bencze <balazs.bencze@geogebra.org>
2020-03-10 10:35:30 +0200

    APPS-1683 Add working command test

--- desktop/src/test/java/org/geogebra/common/kernel/commands/GraphingCommandArgumentFilterTest.java
+++ desktop/src/test/java/org/geogebra/common/kernel/commands/GraphingCommandArgumentFilterTest.java
@@ -1,13 +1,20 @@
 package org.geogebra.common.kernel.commands;
 
+import com.sun.tools.javac.util.Assert;
 import org.geogebra.common.BaseUnitTest;
+import org.geogebra.common.kernel.geos.GeoLine;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GraphingCommandArgumentFilterTest extends BaseUnitTest {
 
+    @Before
+    public void setUp() {
+        getApp().setGraphingConfig();
+    }
+
     @Test(expected = NullPointerException.class)
     public void testParallelLineWithPointAndLineIsFiltered() {
-       getApp().setGraphingConfig();
        addAvInput("A = (1,1)");
        addAvInput("B = (2,2)");
        addAvInput("C = (3,2)");
@@ -17,9 +24,16 @@ public class GraphingCommandArgumentFilterTest extends BaseUnitTest {
 
     @Test(expected = NullPointerException.class)
     public void testParallelLineWithPointAndFunctionIsFiltered() {
-        getApp().setGraphingConfig();
         addAvInput("A = (1,2)");
         addAvInput("f(x) = x");
         addAvInput("g:Line(A,f)");
+    }
+
+    @Test
+    public void testLineWithTwoPointsAllowed() {
+        addAvInput("A = (1,2)");
+        addAvInput("B = (3,4)");
+        GeoLine element = addAvInput("g: Line(A, B)");
+        Assert.checkNonNull(element);
     }
 }
