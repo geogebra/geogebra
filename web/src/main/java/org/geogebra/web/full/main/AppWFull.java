f633d096338e48cc02ad3b7fe48aac01d169cb50
agoston <agoston@geogebra.org>
2020-03-15 12:10:24 +0100

    APPS-1715: change AppId of Scientific Calculator to `scientific`

--- web/src/main/java/org/geogebra/web/full/main/AppWFull.java
+++ web/src/main/java/org/geogebra/web/full/main/AppWFull.java
@@ -340,7 +340,7 @@ public class AppWFull extends AppW implements HasKeyboard {
 			case "cas":
 				activity = new CASActivity();
 				break;
-			case "calculator":
+			case "scientific":
 				activity = new ScientificActivity();
 				break;
 			case "notes":
