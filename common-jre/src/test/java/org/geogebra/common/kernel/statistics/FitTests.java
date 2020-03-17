998a016ed1620b106f65c286045c0ebd3fbfbccc
krisztina.frank <krisztina.frank@geogebra.org>
2020-03-17 14:53:33 +0100

    APPS-1761 Move Equation visibility tests to FitTests class

--- common-jre/src/test/java/org/geogebra/common/kernel/statistics/FitTests.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/statistics/FitTests.java
@@ -1,14 +1,22 @@
 package org.geogebra.common.kernel.statistics;
 
 import org.geogebra.common.BaseUnitTest;
+import org.geogebra.common.GeoElementFactory;
+import org.geogebra.common.gui.dialog.options.model.LineEqnModel;
+import org.geogebra.common.gui.dialog.options.model.ObjectSettingsModel;
 import org.geogebra.common.kernel.StringTemplate;
 import org.geogebra.common.kernel.geos.DescriptionMode;
 import org.geogebra.common.kernel.geos.GeoElement;
+import org.geogebra.common.kernel.geos.GeoLine;
+import org.geogebra.common.main.settings.AppConfigGeometry;
+import org.geogebra.common.main.settings.AppConfigGraphing;
 import org.junit.Assert;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
+
+import java.util.ArrayList;
 
 public class FitTests extends BaseUnitTest {
 
@@ -133,4 +141,51 @@ public class FitTests extends BaseUnitTest {
         Assert.assertEquals(DescriptionMode.DEFINITION_VALUE, fitSin.getDescriptionMode());
     }
 
+    @Test
+    public void testEquationPropertyVisibilityGraphing() {
+        getApp().setConfig(new AppConfigGraphing());
+        getApp().getSettings().getCasSettings().setEnabled(getApp().getConfig().isCASEnabled());
+
+        GeoElement[] geos = getFitLineGeoElements();
+
+        for (GeoElement geo : geos) {
+            ObjectSettingsModel objectSettingsModel = asList(geo);
+
+            Assert.assertFalse(objectSettingsModel.hasEquationModeSetting());
+            Assert.assertTrue(LineEqnModel.forceInputForm(getApp(), geo));
+        }
+    }
+
+    @Test
+    public void testEquationPropertyVisibilityGeometry() {
+        getApp().setConfig(new AppConfigGeometry());
+        getApp().getSettings().getCasSettings().setEnabled(getApp().getConfig().isCASEnabled());
+
+        GeoElement[] geos = getFitLineGeoElements();
+
+        for (GeoElement geo : geos) {
+            ObjectSettingsModel objectSettingsModel = asList(geo);
+
+            Assert.assertTrue(objectSettingsModel.hasEquationModeSetting());
+            Assert.assertFalse(LineEqnModel.forceInputForm(getApp(), geo));
+        }
+    }
+
+    private GeoElement[] getFitLineGeoElements() {
+        GeoElementFactory factory = getElementFactory();
+        GeoLine fitLine = (GeoLine) factory.create("FitLine({(-1,-1),(0,1),(1,1),(2,5)})");
+        GeoLine fitLineX = (GeoLine) factory.create("FitLineX({(-1,3),(2,1),(3,4),(5,3),(6,5)})");
+
+        return new GeoElement[]{fitLine, fitLineX};
+    }
+
+    private ObjectSettingsModel asList(GeoElement f) {
+        ArrayList<GeoElement> list = new ArrayList<>();
+        list.add(f);
+        ObjectSettingsModel model = new ObjectSettingsModel(getApp()) {
+        };
+        model.setGeoElement(f);
+        model.setGeoElementsList(list);
+        return model;
+    }
 }
