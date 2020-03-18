2c261341d441cabc80fe8fc5b4d7093eccf09500
agoston <agoston@geogebra.org>
2020-03-18 13:38:30 +0100

    WLY-7: creating Box and Field in SymbolicEditorD

--- desktop/src/main/java/org/geogebra/desktop/euclidian/SymbolicEditorD.java
+++ desktop/src/main/java/org/geogebra/desktop/euclidian/SymbolicEditorD.java
@@ -1,5 +1,6 @@
 package org.geogebra.desktop.euclidian;
 
+import com.himamis.retex.editor.desktop.MathFieldD;
 import org.geogebra.common.awt.GPoint;
 import org.geogebra.common.awt.GRectangle;
 import org.geogebra.common.euclidian.EuclidianView;
@@ -7,16 +8,32 @@ import org.geogebra.common.euclidian.SymbolicEditor;
 import org.geogebra.common.euclidian.draw.DrawInputBox;
 import org.geogebra.common.kernel.geos.GeoInputBox;
 import org.geogebra.common.main.App;
+import org.geogebra.common.util.debug.Log;
+import org.geogebra.desktop.awt.GRectangleD;
+
+import javax.swing.Box;
 
 public class SymbolicEditorD extends SymbolicEditor {
 
+	private Box box;
+	private MathFieldD mathField;
+
 	protected SymbolicEditorD(App app, EuclidianView view) {
 		super(app, view);
+
+		box = Box.createHorizontalBox();
+
+		mathField = new MathFieldD();
+		mathField.getInternal().setFieldListener(this);
+		mathField.setVisible(true);
+		box.add(mathField);
 	}
 
 	@Override
 	public void hide() {
+		Log.debug("hide called");
 
+		box.setVisible(false);
 	}
 
 	@Override
@@ -26,8 +43,16 @@ public class SymbolicEditorD extends SymbolicEditor {
 
 	@Override
 	public void attach(GeoInputBox geoInputBox, GRectangle bounds) {
+		Log.debug("attach called");
+
 		this.geoInputBox = geoInputBox;
 		this.drawInputBox = (DrawInputBox) view.getDrawableFor(geoInputBox);
+
+		//mathField.getInternal().parse(geoInputBox.getTextForEditor());
+		mathField.getInternal().parse("alma");
+		mathField.setBounds(GRectangleD.getAWTRectangle(bounds));
+		((EuclidianViewD) view).add(box);
+		box.setVisible(true);
 	}
 
 	@Override
