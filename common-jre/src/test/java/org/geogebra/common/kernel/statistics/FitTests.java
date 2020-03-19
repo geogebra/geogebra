d3812d855d63b8b5bd03881ed274a87b030acde8
janborovsky <janborovsky01@gmail.com>
2020-03-19 10:37:34 +0100

    APPS-1717 Unit test testFitLineRectangleSelectionForTwoPoints moved to FitTests

--- common-jre/src/test/java/org/geogebra/common/kernel/statistics/FitTests.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/statistics/FitTests.java
@@ -7,12 +7,16 @@ import java.util.ArrayList;
 
 import org.geogebra.common.BaseUnitTest;
 import org.geogebra.common.GeoElementFactory;
+import org.geogebra.common.euclidian.EuclidianConstants;
+import org.geogebra.common.euclidian.EuclidianController;
+import org.geogebra.common.euclidian.EuclidianView;
 import org.geogebra.common.gui.dialog.options.model.LineEqnModel;
 import org.geogebra.common.gui.dialog.options.model.ObjectSettingsModel;
 import org.geogebra.common.kernel.StringTemplate;
 import org.geogebra.common.kernel.geos.DescriptionMode;
 import org.geogebra.common.kernel.geos.GeoElement;
 import org.geogebra.common.kernel.geos.GeoLine;
+import org.geogebra.ggbjdk.java.awt.geom.Rectangle;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -167,6 +171,25 @@ public class FitTests extends BaseUnitTest {
             Assert.assertTrue(objectSettingsModel.hasEquationModeSetting());
             Assert.assertFalse(LineEqnModel.forceInputForm(getApp(), geo));
         }
+    }
+
+    @Test
+    public void testFitLineRectangleSelectionForTwoPoints() {
+        EuclidianView view =  getApp().getActiveEuclidianView();
+        EuclidianController controller = view.getEuclidianController();
+
+        addAvInput("A = (1,1)");
+        addAvInput("B = (2,1)");
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setRect(0,0, view.getWidth(), view.getHeight());
+
+        controller.setMode(EuclidianConstants.MODE_FITLINE, null);
+        view.setSelectionRectangle(rectangle);
+        controller.processSelectionRectangle(false, false, false);
+
+        GeoElement geo = getConstruction().getLastGeoElement();
+        Assert.assertTrue(geo instanceof GeoLine);
     }
 
     private GeoElement[] getFitLineGeoElements() {
