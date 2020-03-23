eaf0bbf59906a9c5b82ef2fb7d4b2aea47fe8ee0
barnakaracsony <git8.XyZ>
2020-03-23 18:20:24 +0100

    APPS-1735: return false in AlgebraItem.buildPlainTextItemSimple(..) when the algebra style is ALGEBRA_STYLE_DESCRIPTION

--- common/src/main/java/org/geogebra/common/gui/view/algebra/AlgebraItem.java
+++ common/src/main/java/org/geogebra/common/gui/view/algebra/AlgebraItem.java
@@ -252,7 +252,7 @@ public class AlgebraItem {
 	 *            index builder
 	 * @param stringTemplate
 	 *            string template
-	 * @return whether we did append something to the index builder
+	 * @return whether we need only one row
 	 */
 	public static boolean buildPlainTextItemSimple(GeoElement geo1,
 			IndexHTMLBuilder builder, StringTemplate stringTemplate) {
@@ -284,7 +284,7 @@ public class AlgebraItem {
 				geo1.addLabelTextOrHTML(
 						geo1.getDefinitionDescription(stringTemplate), builder);
 			}
-			return true;
+			return false;
 
 		case Kernel.ALGEBRA_STYLE_DEFINITION:
 			buildDefinitionString(geo1, builder, stringTemplate);
