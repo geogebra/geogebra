fd9d798411e9880a82a9d6a1e11bcddd72bef1a1
janborovsky <janborovsky01@gmail.com>
2020-03-26 06:41:00 +0100

    APPS-1836 Disable whatsup dialog and ui test

--- web/src/main/java/org/geogebra/web/full/main/AppWFull.java
+++ web/src/main/java/org/geogebra/web/full/main/AppWFull.java
@@ -157,6 +157,7 @@ import com.google.gwt.user.client.ui.Widget;
 public class AppWFull extends AppW implements HasKeyboard {
 
 	private static final String RECENT_CHANGES_KEY = "RecentChangesInfo.Graphing";
+	private static final boolean SHOULD_SHOW_RECENT_CHANGES = false;
 	private final static int AUTO_SAVE_PERIOD = 2000;
 
 	private DataCollection dataCollection;
@@ -1135,7 +1136,9 @@ public class AppWFull extends AppW implements HasKeyboard {
 	}
 
 	private void maybeShowRecentChangesDialog() {
-		if (shouldShowRecentChangesDialog(RECENT_CHANGES_KEY) && isUnbundledGraphing()) {
+		if (SHOULD_SHOW_RECENT_CHANGES
+				&& shouldShowRecentChangesDialog(RECENT_CHANGES_KEY)
+				&& isUnbundledGraphing()) {
 			LocalizationW localization = getLocalization();
 			String message = localization.getMenu(RECENT_CHANGES_KEY);
 			String readMore = localization.getMenu("tutorial_apps_comparison");
