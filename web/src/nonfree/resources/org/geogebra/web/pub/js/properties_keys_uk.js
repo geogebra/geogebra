5bb106c404a9c61766e0b02d0654e07458c1fd37
Jenkins <jenkins@geogebra.org>
2020-03-30 01:29:10 +0200

    Update translation files

--- web/src/nonfree/resources/org/geogebra/web/pub/js/properties_keys_uk.js
+++ web/src/nonfree/resources/org/geogebra/web/pub/js/properties_keys_uk.js
@@ -1700,7 +1700,7 @@ __GGB__keysVar["uk"].menu = {
     "DoubleLineEquation": "(a x + b y + c)\xb2 = 0",
     "Down": "\u0412\u043d\u0438\u0437",
     "Download": "\u0417\u0430\u0432\u0430\u043d\u0442\u0430\u0436\u0438\u0442\u0438",
-    "Download.PDFDocument": "PDF document (.pdf)",
+    "Download.PDFDocument": "PDF \u0434\u043e\u043a\u0443\u043c\u0435\u043d\u0442 (.pdf)",
     "Download.PNGImage": "PNG image (.png)",
     "Download.SVGImage": "SVG image (.svg)",
     "Download.SlidesGgs": "Slides (.ggs)",
