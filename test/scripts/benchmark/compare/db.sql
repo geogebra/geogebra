fd8e9b5b23496c3eec5a33c5fd77a13b2d084297
Zoltán Kovács <zoltan@geogebra.org>
2020-04-07 11:46:33 +0200

    Add new test type Classic5-rg

--- test/scripts/benchmark/compare/db.sql
+++ test/scripts/benchmark/compare/db.sql
@@ -9,6 +9,8 @@ create table methods (
  info text);
 
 insert into methods (name, info) values ('default', 'Default prover');
+insert into methods (name, info) values ('noelim', 'Use RealGeomWS only, do not use elimination');
+
 
 create table platforms (
  name text not null primary key,
@@ -17,23 +19,16 @@ create table platforms (
 insert into platforms (name, info) values ('desktop', 'Java native');
 insert into platforms (name, info) values ('web', 'JavaScript');
 
-create table outsourcings (
- name text,
- info text);
-
-insert into outsourcings (name, info) values ('', 'no outsourcing: using internals');
-insert into outsourcings (name, info) values ('RealGeomWS', 'RealGeom WebService');
-
 create table provers (
  name text not null primary key,
  engine text,
  method text,
- outsourcing text,
  platform text
 );
 
-insert into provers (name, engine, method, outsourcing, platform) values ('Classic5', 'GG', 'Auto', '', 'desktop');
-insert into provers (name, engine, method, outsourcing, platform) values ('Classic6', 'GG', 'Auto', '', 'web');
+insert into provers (name, engine, method, platform) values ('Classic5', 'GG', 'default', 'desktop');
+insert into provers (name, engine, method, platform) values ('Classic5-rg', 'GG', 'noelim', 'desktop');
+insert into provers (name, engine, method, platform) values ('Classic6', 'GG', 'default', 'web');
 
 -- Using Jenkins' environment settings, assuming Git being used,
 -- see https://wiki.jenkins-ci.org/display/JENKINS/Building+a+software+project
