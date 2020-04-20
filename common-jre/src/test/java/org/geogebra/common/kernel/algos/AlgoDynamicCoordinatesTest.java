5fa92267c12e07b1d690d83aaee2b202c8686bb3
Zbynek Konecny <zbynek@geogebra.at>
2020-04-20 18:16:07 +0200

    APPS-1229 Move test class to the algos package

--- common-jre/src/test/java/org/geogebra/common/kernel/algos/AlgoDynamicCoordinatesTest.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/algos/AlgoDynamicCoordinatesTest.java
@@ -17,6 +17,9 @@ public class AlgoDynamicCoordinatesTest {
 	static AppCommon app;
 	static Construction cons;
 
+	/**
+	 * Setup the app
+	 */
 	@BeforeClass
 	public static void setup() {
 		app = AppCommonFactory.create();
