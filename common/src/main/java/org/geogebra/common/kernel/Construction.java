86e56ff88d5d010e599c8efe613f491383cf46fb
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2020-04-27 12:27:42 +0200

    fix warnings (remove duplicate imports)

--- common/src/main/java/org/geogebra/common/kernel/Construction.java
+++ common/src/main/java/org/geogebra/common/kernel/Construction.java
@@ -59,18 +59,7 @@ import org.geogebra.common.plugin.ScriptManager;
 import org.geogebra.common.util.StringUtil;
 import org.geogebra.common.util.debug.Log;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.Stack;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 import com.himamis.retex.editor.share.input.Character;
 
