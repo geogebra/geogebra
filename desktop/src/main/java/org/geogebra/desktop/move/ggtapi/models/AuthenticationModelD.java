2979756b3d080a106a469bbd55c41a3471e128b2
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2020-04-29 13:33:50 +0200

    MOW-882 fix compiler

--- desktop/src/main/java/org/geogebra/desktop/move/ggtapi/models/AuthenticationModelD.java
+++ desktop/src/main/java/org/geogebra/desktop/move/ggtapi/models/AuthenticationModelD.java
@@ -47,4 +47,9 @@ public class AuthenticationModelD extends AuthenticationModel {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public void onRun() {
+		// nothing to do here
+	}
 }
