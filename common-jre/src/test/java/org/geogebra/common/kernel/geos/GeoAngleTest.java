e0ccd83f364b8a32d050051e2bd8576a9f3ea2f4
barnakaracsony <git8.XyZ>
2020-05-05 18:03:18 +0200

    APPS-1922 copy isDrawable value in copy() method of GeoAngle and GeoNumeric

---
+++ common-jre/src/test/java/org/geogebra/common/kernel/geos/GeoAngleTest.java
@@ -0,0 +1,18 @@
+package org.geogebra.common.kernel.geos;
+
+import org.geogebra.common.BaseUnitTest;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class GeoAngleTest extends BaseUnitTest {
+
+	@Test
+	public void copy() {
+		GeoAngle angle = addAvInput("90°");
+		angle.setDrawable(true, false);
+		GeoAngle copy = angle.copy();
+		assertThat(copy.isDrawable, is(true));
+	}
+}
