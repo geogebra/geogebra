8446cdfb9f411fb6288525607a368a40f8f7321a
janborovsky <janborovsky01@gmail.com>
2020-05-14 06:43:37 +0200

    APPS-1729 Unit tests

--- desktop/src/test/java/org/geogebra/common/kernel/commands/GraphingCommandArgumentFilterTest.java
+++ desktop/src/test/java/org/geogebra/common/kernel/commands/GraphingCommandArgumentFilterTest.java
@@ -36,4 +36,52 @@ public class GraphingCommandArgumentFilterTest extends BaseUnitTest {
         GeoLine element = addAvInput("g: Line(A, B)");
         Assert.assertNotNull(element);
     }
+
+    @Test
+    public void testLengthOfListAllowed() {
+        addAvInput("L = {(0,0), (1,1), (2,2)}");
+        addAvInput("Length(L)");
+    }
+
+    @Test
+    public void testLengthOfTextAllowed() {
+        addAvInput("text = Text(\"1234\")");
+        addAvInput("Length(text)");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testLengthOfLocusAllowed() {
+        addAvInput("f(x) = x^2 - 2x - 1");
+        addAvInput("A = (0,-1)");
+        addAvInput("Locus = (x(A), f'(x(A)))");
+        addAvInput("Length(Locus)");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testLengthOfVectorIsFiltered() {
+        addAvInput("vector = (1,2)");
+        addAvInput("Length(vector)");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testLengthFunctionStartXValueEndXValueIsFiltered() {
+        addAvInput("a = Length(2 x, 0, 1)");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testLengthFunctionStartPointEndPointIsFiltered() {
+        addAvInput("a = Length(2 x, (0,0), (1,1))");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testLengthCurveStartTValueEndTValueIsFiltered() {
+        addAvInput("curve = Curve(2 cos(t), 2 sin(t), t, 0, 2π)");
+        addAvInput("Length(curve, 1, 7)");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testLengthCurveStartPointEndPointIsFiltered() {
+        addAvInput("curve = Curve(2 cos(t), 2 sin(t), t, 0, 2π)");
+        addAvInput("Length(curve, (2,0), (0,-2))");
+    }
 }
