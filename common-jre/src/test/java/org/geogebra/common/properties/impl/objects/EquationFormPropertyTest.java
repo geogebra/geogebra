c4d2f9937489b76463d0fd0dcf15b8d6bcf295bc
barnakaracsony <git8.XyZ>
2020-06-02 19:32:24 +0200

    APPS-2011 add sanity tests for constructing a GeoElementProperty of a GeoList

--- common-jre/src/test/java/org/geogebra/common/properties/impl/objects/EquationFormPropertyTest.java
+++ common-jre/src/test/java/org/geogebra/common/properties/impl/objects/EquationFormPropertyTest.java
@@ -4,21 +4,56 @@ import static org.junit.Assert.*;
 
 import org.geogebra.common.BaseUnitTest;
 import org.geogebra.common.kernel.geos.GeoElement;
+import org.geogebra.common.kernel.geos.GeoList;
 import org.junit.Test;
 
 public class EquationFormPropertyTest extends BaseUnitTest {
 
 	@Test
-	public void testConstructorSucceeds() {
+	public void testConstructorForLineInGeometry() {
 		getApp().setGeometryConfig();
 		GeoElement line = addAvInput("Line((1,1),(2,2))");
 		new EquationFormProperty(line);
 	}
 
 	@Test
-	public void testConstructorThrowsError() {
+	public void testConstructorForUserFunctionInGeometry() {
+		getApp().setGeometryConfig();
+		GeoElement f = addAvInput("f(x) = x");
+		assertThrows(NotApplicablePropertyException.class, () -> new EquationFormProperty(f));
+	}
+
+	@Test
+	public void testConstructorForLineListInGeometry() {
+		getApp().setGeometryConfig();
+		addAvInput("f = Line((1,1),(2,2))");
+		addAvInput("g = Line((3,4),(4,3))");
+		GeoList list = addAvInput("{f, g}");
+		new EquationFormProperty(list);
+	}
+
+	@Test
+	public void testConstructorForLineAndUserFunctionListInGeometry() {
+		getApp().setGeometryConfig();
+		addAvInput("f = Line((1,1),(2,2))");
+		addAvInput("g(x) = x");
+		GeoList list = addAvInput("{f, g}");
+		assertThrows(NotApplicablePropertyException.class, () -> new EquationFormProperty(list));
+	}
+
+	@Test
+	public void testConstructorForLineInGraphing() {
 		getApp().setGraphingConfig();
 		GeoElement line = addAvInput("Line((1,1),(2,2))");
 		assertThrows(NotApplicablePropertyException.class, () -> new EquationFormProperty(line));
+	}
+
+	@Test
+	public void testConstructorForLineListInGraphing() {
+		getApp().setGraphingConfig();
+		addAvInput("f = Line((1,1),(2,2))");
+		addAvInput("g = Line((3,4),(4,3))");
+		GeoList list = addAvInput("{f, g}");
+		assertThrows(NotApplicablePropertyException.class, () -> new EquationFormProperty(list));
 	}
 }
