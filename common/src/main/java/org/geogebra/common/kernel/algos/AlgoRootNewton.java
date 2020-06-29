734f907e246363c8cf8d8e1a573d99e671a5d6a8
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2020-06-29 16:17:19 +0200

    remove unused import

--- common/src/main/java/org/geogebra/common/kernel/algos/AlgoRootNewton.java
+++ common/src/main/java/org/geogebra/common/kernel/algos/AlgoRootNewton.java
@@ -29,7 +29,6 @@ import org.geogebra.common.kernel.geos.GeoFunctionable;
 import org.geogebra.common.kernel.geos.GeoNumberValue;
 import org.geogebra.common.kernel.geos.GeoPoint;
 import org.geogebra.common.kernel.roots.RealRootUtil;
-import org.geogebra.common.util.debug.Log;
 
 /**
  * Finds one real root of a function with newtons method. The first derivative
