09877f8ef973f404e8289fe103895bbaa2682605
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2020-07-02 09:31:11 +0200

    APPS-2100 keyboard for cas

--- web/src/main/java/org/geogebra/web/full/cas/view/CASLaTeXEditor.java
+++ web/src/main/java/org/geogebra/web/full/cas/view/CASLaTeXEditor.java
@@ -219,7 +219,7 @@ public class CASLaTeXEditor extends FlowPanel implements CASEditorW,
 	@Override
 	public void ensureEditing() {
 		final GuiManagerInterfaceW gui = app.getGuiManager();
-		app.getAppletFrame().doShowKeyBoard(true, retexListener);
+		app.showKeyboard(retexListener, true);
 		app.getKeyboardManager().setOnScreenKeyboardTextField(retexListener);
 		CancelEventTimer.keyboardSetVisible();
 		ClickStartHandler.init(this, new ClickStartHandler(false, false) {
