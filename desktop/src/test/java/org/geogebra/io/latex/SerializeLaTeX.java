2c17ca49e80e74bfadcacf9e3efe512a8ce2693e
janborovsky <janborovsky01@gmail.com>
2020-08-05 16:24:04 +0200

    APPS-2219 Unit test

--- desktop/src/test/java/org/geogebra/io/latex/SerializeLaTeX.java
+++ desktop/src/test/java/org/geogebra/io/latex/SerializeLaTeX.java
@@ -1,15 +1,27 @@
 package org.geogebra.io.latex;
 
+import static junit.framework.TestCase.fail;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.text.Normalizer;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.himamis.retex.editor.share.io.latex.ParseException;
 import com.himamis.retex.editor.share.io.latex.Parser;
 import com.himamis.retex.editor.share.meta.MetaModel;
 import com.himamis.retex.editor.share.model.Korean;
 import com.himamis.retex.editor.share.model.MathFormula;
+import com.himamis.retex.editor.share.model.MathSequence;
 import com.himamis.retex.editor.share.serializer.GeoGebraSerializer;
 import com.himamis.retex.editor.share.serializer.TeXSerializer;
 import com.himamis.retex.editor.share.util.Unicode;
@@ -21,7 +33,7 @@ import com.himamis.retex.renderer.share.serialize.ListBracketsAdapter;
 import com.himamis.retex.renderer.share.serialize.TeXAtomSerializer;
 
 public class SerializeLaTeX {
-	static Parser parser;
+	private static Parser parser;
 
 	/**
 	 * Initilize parser and serializer.
@@ -324,4 +336,19 @@ public class SerializeLaTeX {
 		return null;
 	}
 
+	@Test
+	public void javaSerializationShouldKeepAllData() {
+		try {
+			ByteArrayOutputStream sourceStream = new ByteArrayOutputStream();
+			MathFormula mf = new Parser(new MetaModel()).parse("()");
+			ObjectOutputStream oos = new ObjectOutputStream(sourceStream);
+			oos.writeObject(mf.getRootComponent());
+			InputStream targetStream = new ByteArrayInputStream(sourceStream.toByteArray());
+			ObjectInputStream ois = new ObjectInputStream(targetStream);
+			Object back = ois.readObject();
+			MatcherAssert.assertThat(back, CoreMatchers.instanceOf(MathSequence.class));
+		} catch (ParseException | IOException | ClassNotFoundException e) {
+			fail("Can't parse: " + e);
+		}
+	}
 }
