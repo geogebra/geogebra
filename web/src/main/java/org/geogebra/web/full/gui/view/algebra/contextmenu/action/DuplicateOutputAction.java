de75c3cdcfd03a1ed2aa34622a4f419fbd87056b
schbence <schbence@yahoo.com>
2020-08-18 12:56:11 +0300

    APPS-2159 Duplicate input/output implemented

---
+++ web/src/main/java/org/geogebra/web/full/gui/view/algebra/contextmenu/action/DuplicateOutputAction.java
@@ -0,0 +1,43 @@
+package org.geogebra.web.full.gui.view.algebra.contextmenu.action;
+
+import org.geogebra.common.gui.view.algebra.AlgebraItem;
+import org.geogebra.common.kernel.StringTemplate;
+import org.geogebra.common.kernel.geos.GeoElement;
+import org.geogebra.common.util.ToStringConverter;
+import org.geogebra.web.full.gui.menubar.DefaultMenuAction;
+import org.geogebra.web.full.gui.view.algebra.AlgebraViewW;
+import org.geogebra.web.full.gui.view.algebra.RadioTreeItem;
+import org.geogebra.web.full.main.AppWFull;
+
+public class DuplicateOutputAction extends DefaultMenuAction<GeoElement> {
+
+	private AlgebraViewW algebraView;
+
+	public DuplicateOutputAction(AlgebraViewW algebraView) {
+		this.algebraView = algebraView;
+	}
+
+	@Override
+	public void execute(GeoElement item, AppWFull app) {
+		ToStringConverter<GeoElement> converter = app.createGeoElementValueConverter();
+		RadioTreeItem input = algebraView.getInputTreeItem();
+		String geoString = converter.convert(item);
+
+		RadioTreeItem currentNode = algebraView.getNode(item);
+		if (currentNode != null) {
+			currentNode.selectItem(false);
+		}
+		input.setText(geoString);
+		input.setFocus(true);
+	}
+
+	@Override
+	public boolean isAvailable(GeoElement item) {
+		if (item.isAlgebraDuplicateable()
+				&& AlgebraItem.shouldShowBothRows(item)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
