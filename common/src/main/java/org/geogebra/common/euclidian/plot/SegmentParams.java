fa05a7233f69d8fcbe935aea234c015ec9eb4230
Laszlo Gal <latz.gal@gmail.com>
2020-09-14 14:51:00 +0200

    APPS-1651 extracting isStepTooBig()

--- common/src/main/java/org/geogebra/common/euclidian/plot/SegmentParams.java
+++ common/src/main/java/org/geogebra/common/euclidian/plot/SegmentParams.java
@@ -1,7 +1,11 @@
 package org.geogebra.common.euclidian.plot;
 
 public class SegmentParams {
-	public static final double MAX_DEFINED_BISECTIONS = 16;
+	public static final int MAX_DEFINED_BISECTIONS = 16;
+
+	// maximum number of times to loop when xDiff, yDiff are both zero
+	// eg Curve[0sin(t), 0t, t, 0, 6]
+	private static final int MAX_ZERO_COUNT = 1000;
 	int dyad;
 	int depth = 0;
 	double[] diff;
@@ -9,17 +13,33 @@ public class SegmentParams {
 	int countDiffZeros = 0;
 	double t;
 	double left;
+	private final double tMin;
+	private double tMax;
+	double[] divisors;
 
-	public SegmentParams(double t, double left, double[] diff, double[] prevDiff) {
+
+	public SegmentParams(double tMin, double tMax, double[] divisors, double[] diff,
+			double[] prevDiff) {
+		this.tMin = tMin;
+		this.tMax = tMax;
+		this.divisors = divisors;
 		dyad = 1;
 		depth = 0;
-		this.t = t;
-		this.left = left;
+		this.t = tMin;
+		this.left = tMin;
 		this.diff = diff;
 		this.prevDiff = prevDiff;
 	}
 
-	public boolean isMaxDepthReached() {
-		return depth == MAX_DEFINED_BISECTIONS;
+	public boolean hasMaxDepthNotReached() {
+		return depth < MAX_DEFINED_BISECTIONS;
+	}
+
+	public boolean isDiffZerosLimitNotReached() {
+		return countDiffZeros < MAX_ZERO_COUNT;
+	}
+
+	public boolean isStepTooBig(double maxParamStep) {
+		return divisors[depth] > maxParamStep;
 	}
 }
