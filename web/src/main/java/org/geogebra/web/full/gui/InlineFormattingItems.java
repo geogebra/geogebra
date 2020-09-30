34e0921a3c16437ee8c9bff73b176cff74356972
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2020-09-30 08:06:41 +0200

    MOW-1130 remove delete from the context menu of table

--- web/src/main/java/org/geogebra/web/full/gui/InlineFormattingItems.java
+++ web/src/main/java/org/geogebra/web/full/gui/InlineFormattingItems.java
@@ -154,7 +154,6 @@ public class InlineFormattingItems {
 
 		addItem("ContextMenu.deleteRow", controller::removeRow);
 		addItem("ContextMenu.deleteColumn", controller::removeColumn);
-		addItem("ContextMenu.deleteTable", table::remove);
 	}
 
 	private void addToolbar() {
