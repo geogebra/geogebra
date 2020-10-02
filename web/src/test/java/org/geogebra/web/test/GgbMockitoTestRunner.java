020bf15eb6633da3a8543705646ecf2885bb1dc5
Zbynek Konecny <zbynek@geogebra.at>
2020-10-02 11:27:47 +0200

    APPS-2340 Update GwtMockito to snapshot, mock Js.asPropertyMap

--- web/src/test/java/org/geogebra/web/test/GgbMockitoTestRunner.java
+++ web/src/test/java/org/geogebra/web/test/GgbMockitoTestRunner.java
@@ -10,6 +10,10 @@ import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwtmockito.GwtMockitoTestRunner;
+import com.google.gwtmockito.impl.StubGenerator;
+
+import jsinterop.base.Js;
+import jsinterop.base.JsPropertyMap;
 
 public class GgbMockitoTestRunner extends GwtMockitoTestRunner {
 
@@ -17,10 +21,12 @@ public class GgbMockitoTestRunner extends GwtMockitoTestRunner {
      * Creates a test runner which allows final GWT classes to be mocked. Works by reloading the test
      * class using a custom classloader and substituting the reference.
      *
-     * @param unitTestClass
+     * @param unitTestClass test class
      */
     public GgbMockitoTestRunner(Class<?> unitTestClass) throws InitializationError {
         super(unitTestClass);
+        StubGenerator.replaceMethodWithMock(Js.class, "asPropertyMap",
+                JsPropertyMap.class);
     }
 
     @Override
