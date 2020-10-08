cdf826a2b4a4d372e386a7927769837ea976f5d3
Bence Schneider <bence.schneider@geogebra.at>
2020-10-08 14:43:27 +0200

    APPS-2348 test improved for AlgoHolesPolynomial

--- desktop/src/test/java/org/geogebra/common/kernel/commands/AlgoHolesPolynomialTest.java
+++ desktop/src/test/java/org/geogebra/common/kernel/commands/AlgoHolesPolynomialTest.java
@@ -1,21 +1,21 @@
 package org.geogebra.common.kernel.commands;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.geogebra.common.kernel.algos.AlgoHolesPolynomial;
 import org.geogebra.common.kernel.geos.GeoFunction;
-import org.geogebra.common.kernel.kernelND.GeoElementND;
+import org.geogebra.suite.BaseSuiteTest;
 import org.junit.Test;
 
-public class AlgoHolesPolynomialTest extends AlgebraTest {
-	private EvalInfo info;
+public class AlgoHolesPolynomialTest extends BaseSuiteTest {
 
 	@Test
-	public void testRegressionAppsApps2348() {
-		GeoElementND[] slider = ap.processAlgebraCommand("a=10",false);
-		GeoFunction func = processComandCreatesFunction("1/(x+a)");
-		GeoElementND[] result = ap.processAlgebraCommand("Holes[f]", false);
-		assertNotNull(result);
-	}
+	public void testRegressionApps2348() {
+		add("a = 1");
+		GeoFunction function = add("1/(x+a)");
+		AlgoHolesPolynomial algo = new AlgoHolesPolynomial(getConstruction(), function, null);
+		assertThat(algo.getOutputLength(), is(1));
 
-	private GeoFunction processComandCreatesFunction(String command) {
-		GeoElementND[] elementNDS = ap.processAlgebraCommand(command, false);
-		return (GeoFunction) elementNDS[0];
 	}
 }
