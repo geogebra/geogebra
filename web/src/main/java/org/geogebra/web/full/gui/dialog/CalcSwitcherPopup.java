98d1975c9097db466205add6cc3f3ee4a932358c
Ricarda <ricarda.winklhofer@geogebra.at>
2020-10-13 13:57:09 +0200

    Apps-2052 add data-trans-key to labels in popup, set max-width for button

--- web/src/main/java/org/geogebra/web/full/gui/dialog/CalcSwitcherPopup.java
+++ web/src/main/java/org/geogebra/web/full/gui/dialog/CalcSwitcherPopup.java
@@ -1,12 +1,17 @@
 package org.geogebra.web.full.gui.dialog;
 
+import org.geogebra.common.main.App;
 import org.geogebra.web.full.gui.images.SvgPerspectiveResources;
 import org.geogebra.web.full.main.AppWFull;
 import org.geogebra.web.html5.gui.GPopupPanel;
+import org.geogebra.web.html5.gui.util.AriaHelper;
 import org.geogebra.web.html5.gui.util.NoDragImage;
+import org.geogebra.web.html5.main.UserPreferredLanguage;
+import org.geogebra.web.html5.util.Dom;
 import org.geogebra.web.resources.SVGResource;
 import org.geogebra.web.shared.SuiteHeaderAppPicker;
 
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -14,6 +19,7 @@ import com.google.gwt.user.client.ui.Label;
 public class CalcSwitcherPopup extends GPopupPanel {
 
 	SuiteHeaderAppPicker appPickerButton;
+	private AppWFull app;
 
 	private final static int X_COORDINATE_OFFSET = 8;
 
@@ -26,6 +32,7 @@ public class CalcSwitcherPopup extends GPopupPanel {
 	public CalcSwitcherPopup(AppWFull app, SuiteHeaderAppPicker pickerButton) {
 		super(true, app.getPanel(), app);
 		this.appPickerButton = pickerButton;
+		this.app = app;
 		addAutoHidePartner(appPickerButton.getElement());
 		setGlassEnabled(false);
 		addStyleName("calcPickerPopup");
@@ -39,11 +46,13 @@ public class CalcSwitcherPopup extends GPopupPanel {
 		} else {
 			this.setPopupPosition(getLeft(), 0);
 			super.show();
+			updateLanguage();
 		}
 	}
 
 	private void buildGUI(AppWFull app) {
 		FlowPanel contentPanel = new FlowPanel();
+		contentPanel.addStyleName("popupPanelForTranslation");
 		SvgPerspectiveResources res = SvgPerspectiveResources.INSTANCE;
 		addElement(app, res.menu_icon_algebra_transparent(), "GraphingCalculator.short",
 				contentPanel);
@@ -62,6 +71,7 @@ public class CalcSwitcherPopup extends GPopupPanel {
 
 		Label label = new Label(app.getLocalization().getMenu(key));
 		label.addStyleName("appPickerLabel");
+		AriaHelper.setAttribute(label, "data-trans-key", key);
 		rowPanel.add(label);
 		rowPanel.setStyleName("appPickerRow");
 		rowPanel.addDomHandler(event -> {
@@ -74,5 +84,12 @@ public class CalcSwitcherPopup extends GPopupPanel {
 
 	private int getLeft() {
 		return appPickerButton.getAbsoluteLeft() - X_COORDINATE_OFFSET ;
+	}
+
+	private void updateLanguage() {
+		Element suitePopup = Dom.querySelector("popupPanelForTranslation");
+		if (suitePopup != null) {
+			UserPreferredLanguage.translate(app, suitePopup);
+		}
 	}
 }
