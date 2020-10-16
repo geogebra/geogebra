8ba8d5620e6cd7a4a61398896e0cb856cbc5da9c
Zbynek Konecny <zbynek@geogebra.at>
2020-10-16 13:15:00 +0200

    WLY-179 Update conics on file load, clean up test code

--- web/src/main/java/org/geogebra/web/util/file/FileIO.java
+++ web/src/main/java/org/geogebra/web/util/file/FileIO.java
@@ -1,14 +1,11 @@
 package org.geogebra.web.util.file;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.geogebra.common.util.Charsets;
+import org.geogebra.common.util.StringUtil;
 import org.geogebra.common.util.debug.Log;
 
 /**
@@ -26,44 +23,14 @@ public final class FileIO {
 	 * @return the content of the file
 	 */
 	public static String load(String filename) {
-		InputStream is = null;
+		Path filePath = Paths.get(filename);
 		try {
-			Path filePath = Paths.get(filename);
-			is = Files.newInputStream(filePath);
-			return load(is);
+			return StringUtil.join("\n",
+					Files.readAllLines(filePath, StandardCharsets.UTF_8));
 		} catch (Exception e) {
-			Log.error("problem loading " + filename);
-		} finally {
-			tryToClose(is);
+			Log.error("problem loading " + filePath.toAbsolutePath());
 		}
-
 		return null;
 	}
 
-	private static String load(InputStream is) {
-		BufferedReader reader = new BufferedReader(
-				new InputStreamReader(is, Charsets.getUtf8()));
-		StringBuilder sb = new StringBuilder();
-
-		String line;
-		try {
-			while ((line = reader.readLine()) != null) {
-				sb.append(line).append("\n");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return sb.toString();
-	}
-
-	private static void tryToClose(InputStream inputStream) {
-		try {
-			if (inputStream != null) {
-				inputStream.close();
-			}
-		} catch (IOException e) {
-			Log.error("problem closing the input stream");
-		}
-	}
 }
