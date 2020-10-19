c855991ea156f0e19bd938a579206ef84ee23a37
Zbynek Konecny <zbynek@geogebra.at>
2020-10-19 19:47:23 +0200

    MOW-1133 Do not open login dialog in iframe

--- web/src/main/java/org/geogebra/web/full/main/FileManager.java
+++ web/src/main/java/org/geogebra/web/full/main/FileManager.java
@@ -212,6 +212,7 @@ public abstract class FileManager extends MaterialsManager {
 			// not logged in and possible to log in
 		} else if (!appw.getLoginOperation().isLoggedIn()) {
 			appw.getGuiManager().listenToLogin();
+			((AppWFull) app).getActivity().markSaveOpen();
 			appw.getLoginOperation().showLoginDialog();
 			// logged in
 		} else {
