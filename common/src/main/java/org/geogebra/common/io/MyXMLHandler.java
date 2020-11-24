614b066cfe3ac4e0c9b41d8adb35459b40483345
Bence Schneider <bence.schneider@geogebra.at>
2020-11-24 13:31:39 +0200

    APPS-1624 inner nested commands not added to construction

--- common/src/main/java/org/geogebra/common/io/MyXMLHandler.java
+++ common/src/main/java/org/geogebra/common/io/MyXMLHandler.java
@@ -3585,7 +3585,7 @@ public class MyXMLHandler implements DocHandler {
 			}
 
 			// Application.debug(""+getAlgProcessor());
-
+			ve.setAsRootNode();
 			GeoElementND[] result = getAlgProcessor()
 					.processValidExpression(ve,
 							new EvalInfo(!cons.isSuppressLabelsActive(), true)
