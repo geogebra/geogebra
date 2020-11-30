c1d3b64fcc610d819efba35b509d5a1a263194bc
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2020-11-30 13:16:18 +0100

    MOW-1117 fix compiler

--- desktop/src/test/java/org/geogebra/resources/ResourceAvailability.java
+++ desktop/src/test/java/org/geogebra/resources/ResourceAvailability.java
@@ -70,7 +70,6 @@ public class ResourceAvailability {
 			case EuclidianConstants.MODE_SURFACE_OF_REVOLUTION:
 			case EuclidianConstants.MODE_FREEHAND_FUNCTION:
 			case EuclidianConstants.MODE_MASK:
-			case EuclidianConstants.MODE_H5P:
 				continue;
 			default:
 				ImageResourceD res = man.getToolImageResource(modeText);
