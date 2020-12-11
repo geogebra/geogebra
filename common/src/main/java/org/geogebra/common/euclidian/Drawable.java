de678ec25250e05460464f4fe4c0f26138e66c8f
Zbynek Konecny <zbynek@geogebra.at>
2020-12-11 13:14:43 +0100

    APPS-2503 Avoid NPE for undefined texts

--- common/src/main/java/org/geogebra/common/euclidian/Drawable.java
+++ common/src/main/java/org/geogebra/common/euclidian/Drawable.java
@@ -44,6 +44,7 @@ import org.geogebra.common.kernel.kernelND.GeoElementND;
 import org.geogebra.common.main.App;
 import org.geogebra.common.plugin.EuclidianStyleConstants;
 import org.geogebra.common.util.StringUtil;
+import org.geogebra.common.util.debug.Log;
 
 import com.google.j2objc.annotations.Weak;
 
@@ -391,12 +392,15 @@ public abstract class Drawable extends DrawableND {
 	 */
 	public final void drawMultilineLaTeX(GGraphics2D g2, GFont font,
 			GColor fgColor, GColor bgColor) {
-		EuclidianStatic.drawMultilineLaTeX(view.getApplication(),
-				view.getTempGraphics2D(font), geo, g2, font, fgColor,
-				bgColor, labelDesc, xLabel, yLabel, isSerif(),
-				view.getCallBack(geo, firstCall),
-				labelRectangle);
-		firstCall = false;
+		if (labelDesc != null) {
+			EuclidianStatic.drawMultilineLaTeX(view.getApplication(),
+					view.getTempGraphics2D(font), geo, g2, font, fgColor,
+					bgColor, labelDesc, xLabel, yLabel, isSerif(),
+					view.getCallBack(geo, firstCall),
+					labelRectangle);
+			firstCall = false;
+		}
+		// else: undefined text, do nothing
 	}
 
 	/**
