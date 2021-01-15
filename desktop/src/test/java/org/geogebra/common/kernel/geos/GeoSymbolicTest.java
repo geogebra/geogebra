b0c79dcf5be6db5a77773e871a3363ea93725e5f
Zbynek Konecny <zbynek@geogebra.at>
2021-01-15 17:10:46 +0100

    APPS-2522 Fix test for Assume

--- desktop/src/test/java/org/geogebra/common/kernel/geos/GeoSymbolicTest.java
+++ desktop/src/test/java/org/geogebra/common/kernel/geos/GeoSymbolicTest.java
@@ -1366,7 +1366,7 @@ public class GeoSymbolicTest extends BaseSymbolicTest {
 	@Test
 	public void testAssumeCommand() {
 		t("Assume(a > 0, Integral(exp(-a x), 0, infinity))", "1 / a");
-		t("Assume(x>0 && n>0, Solve(log(n^2*(x/n)^lg(x))=log(x^2), x))",
+		t("Assume(n>0, Solve(log(n^2*(x/n)^lg(x))=log(x^2), x))",
 				"{x = 100, x = n}");
 		t("Assume(x<2,Simplify(sqrt(x-2sqrt(x-1))))", "-sqrt(x - 1) + 1");
 		t("Assume(x>2,Simplify(sqrt(x-2sqrt(x-1))))", "sqrt(x - 1) - 1");
