ac87dcc23dfb13db52bb48cf3c1ab18641b6d70f
janborovsky <janborovsky01@gmail.com>
2021-02-22 07:54:19 +0100

    APPS-2659 Remove unused import

--- retex/editor-android/src/main/java/com/himamis/retex/editor/android/FormulaEditor.java
+++ retex/editor-android/src/main/java/com/himamis/retex/editor/android/FormulaEditor.java
@@ -37,7 +37,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.text.InputType;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.BaseInputConnection;
 import android.view.inputmethod.EditorInfo;
