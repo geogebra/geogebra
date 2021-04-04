c00e6ced469c33454539a1d5f053499b0a6a9a7c
Barni <barna.karacsony@geogebra.org>
2021-04-04 16:57:24 +0200

    APPS-2731 add open and save burger menu items to exam mode

--- common/src/main/java/org/geogebra/common/gui/menu/impl/ExamDrawerMenuFactory.java
+++ common/src/main/java/org/geogebra/common/gui/menu/impl/ExamDrawerMenuFactory.java
@@ -1,5 +1,7 @@
 package org.geogebra.common.gui.menu.impl;
 
+import java.util.List;
+
 import org.geogebra.common.GeoGebraConstants;
 import org.geogebra.common.gui.menu.Action;
 import org.geogebra.common.gui.menu.DrawerMenu;
@@ -11,6 +13,8 @@ import org.geogebra.common.gui.menu.MenuItemGroup;
  * Creates drawer menus for apps when in exam mode.
  */
 public class ExamDrawerMenuFactory extends AbstractDrawerMenuFactory {
+
+	private GeoGebraConstants.Version version;
 
 	/**
 	 * Create a new ExamDrawerMenuFactory.
@@ -28,20 +32,32 @@ public class ExamDrawerMenuFactory extends AbstractDrawerMenuFactory {
 	public ExamDrawerMenuFactory(GeoGebraConstants.Version version,
 			boolean isSuiteApp) {
 		super(version, isSuiteApp);
+		this.version = version;
 	}
 
 	@Override
 	public DrawerMenu createDrawerMenu() {
-		MenuItemGroup group =
-				new MenuItemGroupImpl(removeNulls(
-						clearConstruction(),
-						openFile(),
-						saveFile(),
-						showSwitchCalculator(),
-						showExamLog(),
-						exitExamMode()));
+		MenuItemGroup group = new MenuItemGroupImpl(collectItems());
 		String title = getMenuTitle();
 		return new DrawerMenuImpl(title, group);
+	}
+
+	private List<MenuItem> collectItems() {
+		if (version == GeoGebraConstants.Version.SCIENTIFIC) {
+			return removeNulls(
+					clearConstruction(),
+					showSwitchCalculator(),
+					showExamLog(),
+					exitExamMode());
+		} else {
+			return removeNulls(
+					clearConstruction(),
+					openFile(),
+					saveFile(),
+					showSwitchCalculator(),
+					showExamLog(),
+					exitExamMode());
+		}
 	}
 
 	private static MenuItem exitExamMode() {
