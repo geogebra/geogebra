f32a00e1dd8de1ff3892675714eb56aafaa234ce
janborovsky <janborovsky01@gmail.com>
2021-04-14 16:27:06 +0200

    APPS-2824 add missing docs

--- common/src/main/java/org/geogebra/common/gui/menu/impl/ExamDrawerMenuFactory.java
+++ common/src/main/java/org/geogebra/common/gui/menu/impl/ExamDrawerMenuFactory.java
@@ -35,6 +35,11 @@ public class ExamDrawerMenuFactory extends AbstractDrawerMenuFactory {
 		return createDrawerMenu(true);
 	}
 
+	/**
+	 * Create a new drawer menu.
+	 * @param showExitExam whether Exit Exam menu item is shown
+	 * @return drawer menu
+	 */
 	public DrawerMenu createDrawerMenu(boolean showExitExam) {
 		MenuItemGroup group;
 		if (showExitExam) {
