431d024169e91938c78baa838ed9fd413d6fed2a
agoston <agoston@geogebra.org>
2021-04-27 10:26:37 +0200

    MOW-975: do not reset selected mode on File -> New

--- web/src/main/java/org/geogebra/web/full/main/AppWFull.java
+++ web/src/main/java/org/geogebra/web/full/main/AppWFull.java
@@ -1259,7 +1259,6 @@ public class AppWFull extends AppW implements HasKeyboard, MenuViewListener {
 		resetMaxLayerUsed();
 		setCurrentFile(null);
 		resetUI();
-		resetPenTool();
 		clearMedia();
 	}
 
