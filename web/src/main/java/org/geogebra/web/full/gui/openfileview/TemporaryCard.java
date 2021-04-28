84855859ac272f1ea60d8541886562ab853316e2
Laszlo Gal <laszlo@geogebra.org>
2021-04-28 13:25:01 +0200

    APPS-2397 TemporaryCard cleanup.

--- web/src/main/java/org/geogebra/web/full/gui/openfileview/TemporaryCard.java
+++ web/src/main/java/org/geogebra/web/full/gui/openfileview/TemporaryCard.java
@@ -1,11 +1,8 @@
 package org.geogebra.web.full.gui.openfileview;
 
 import org.geogebra.common.move.ggtapi.models.Material;
-import org.geogebra.common.util.AsyncOperation;
 import org.geogebra.web.full.gui.CardInfoPanel;
 import org.geogebra.web.full.gui.browser.MaterialCardController;
-import org.geogebra.web.full.gui.images.AppResources;
-import org.geogebra.web.html5.Browser;
 import org.geogebra.web.html5.main.AppW;
 
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -14,10 +11,8 @@ import com.google.gwt.user.client.ui.FlowPanel;
 /**
  * Material card
  */
-public class TemporaryCard extends FlowPanel implements MaterialCardI {
+public class TemporaryCard extends FlowPanel {
 	private final AppW app;
-	// image of material
-	private MaterialImagePanel imgPanel;
 	private final MaterialCardController controller;
 
 	/**
@@ -45,84 +40,10 @@ public class TemporaryCard extends FlowPanel implements MaterialCardI {
 
 	private void initGui() {
 		this.setStyleName("materialCard");
-		// panel containing the preview image of material
-		imgPanel = new MaterialImagePanel(getMaterial());
-		this.add(imgPanel);
-		// panel containing the info regarding the material
-
-		// material information
-		CardInfoPanel infoPanel = new CardInfoPanel(getMaterial().getTitle(), "");
-		this.add(infoPanel);
-	}
-
-	/**
-	 * @return represented material
-	 */
-	Material getMaterial() {
-		return controller.getMaterial();
-	}
-
-	private void setBackgroundImgPanel(Material m) {
-		final String thumb = m.getThumbnail();
-		if (thumb != null && thumb.length() > 0) {
-			imgPanel.getElement().getStyle().setBackgroundImage(
-					"url(" + Browser.normalizeURL(thumb) + ")");
-		} else {
-			imgPanel.getElement().getStyle().setBackgroundImage("url("
-					+ AppResources.INSTANCE.geogebra64().getSafeUri().asString()
-					+ ")");
-		}
-	}
-
-	@Override
-	public void remove() {
-		// not used
-	}
-
-	/**
-	 * Actually delete the file.
-	 */
-	protected void onConfirmDelete() {
-		// not used
-	}
-
-	@Override
-	public void rename(String text) {
-		// not used
-	}
-
-	@Override
-	public void copy() {
-		// not used
-	}
-
-	@Override
-	public void onDelete() {
-		// not used
-	}
-
-	@Override
-	public String getCardTitle() {
-		return getMaterial().getTitle();
-	}
-
-	@Override
-	public void setShare(String groupID, boolean shared,
-			AsyncOperation<Boolean> callback) {
-		// not used
-	}
-
-	@Override
-	public String getMaterialID() {
-		return ""; // not used
-	}
-
-	@Override
-	public void updateVisibility(String visibility) {
-		// not used
-	}
-
-	public void setLabels() {
-		// not used
+		Material material = controller.getMaterial();
+		MaterialImagePanel imgPanel = new MaterialImagePanel(material);
+		CardInfoPanel infoPanel = new CardInfoPanel(material.getTitle(), "");
+		add(imgPanel);
+		add(infoPanel);
 	}
 }
