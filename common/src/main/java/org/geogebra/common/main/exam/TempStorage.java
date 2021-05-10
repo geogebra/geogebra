70439b81ad8e9479502dda674470025fe23513f5
Bence Schneider <bence.schneider@geogebra.at>
2021-05-10 11:26:42 +0300

    APPS-2732 unused import removed

--- common/src/main/java/org/geogebra/common/main/exam/TempStorage.java
+++ common/src/main/java/org/geogebra/common/main/exam/TempStorage.java
@@ -2,7 +2,6 @@ package org.geogebra.common.main.exam;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
