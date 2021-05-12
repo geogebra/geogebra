a6b5a894aeba76b640ca071cea63085d62184f4b
Laszlo Gal <laszlo@geogebra.org>
2021-05-12 12:48:05 +0200

    APPS-2908 IntervalPowerEvaluator refactor - negativePower

--- common/src/main/java/org/geogebra/common/kernel/interval/IntervalPowerEvaluator.java
+++ common/src/main/java/org/geogebra/common/kernel/interval/IntervalPowerEvaluator.java
@@ -60,62 +60,63 @@ public class IntervalPowerEvaluator {
 	}
 
 	private Interval calculateNegPower(ExpressionNode node, Interval base) throws Exception {
-		if (node.isOperation(Operation.DIVIDE)) {
-			return negPower(base, node);
-		} else if (node.getOperation() == Operation.MULTIPLY
-				&& node.getLeft() instanceof MinusOne
-				&& node.getRight().isOperation(Operation.DIVIDE)) {
-			return negPower(base, node.getRight().wrap()).multiplicativeInverse();
+		if (isPositiveFraction(node)) {
+			return negativePower(base, node);
+		} else if (isNegativeFraction(node)) {
+			return negativePower(base, node.getRight().wrap()).multiplicativeInverse();
 		}
 		return undefined();
 	}
 
-	private Interval negPower(Interval base, ExpressionNode node) throws Exception {
+	private boolean isPositiveFraction(ExpressionNode node) {
+		return node.isOperation(Operation.DIVIDE);
+	}
+
+	private boolean isNegativeFraction(ExpressionNode node) {
+		return node.getOperation() == Operation.MULTIPLY
+				&& node.getLeft() instanceof MinusOne
+				&& node.getRight().isOperation(Operation.DIVIDE);
+	}
+
+	private Interval negativePower(Interval base, ExpressionNode node) throws Exception {
 		Interval nominator = IntervalFunction.evaluate(base, node.getLeft());
 		if (nominator.isSingletonInteger()) {
 			Interval denominator = IntervalFunction.evaluate(base, node.getRight());
 			if (denominator.isUndefined()) {
 				return undefined();
 			} else if (denominator.isSingletonInteger()) {
-				return powerFraction(base, nominator, denominator);
+				return powerFraction(base, (long) nominator.getLow(),
+						(long) denominator.getLow());
 
 			}
 		}
 		return undefined();
 	}
 
-	private Interval powerFraction(Interval base, Interval a, Interval b) {
-		long al = (long) a.getLow();
-		long bl = (long) b.getLow();
-		long gcd = Kernel.gcd(al, bl);
-		// fix for java.lang.ArithmeticException: divide by zero
-		// https://play.google.com/apps/publish/?dev_acc=05873811091523087820#ErrorClusterDetailsPlace:p=org.geogebra.android&et=CRASH&lr=LAST_7_DAYS&ecn=java.lang.ArithmeticException&tf=SourceFile&tc=org.geogebra.common.kernel.arithmetic.ExpressionNodeEvaluator&tm=negPower&nid&an&c&s=new_status_desc&ed=0
+	private Interval powerFraction(Interval base, long a, long b) {
+		long gcd = Kernel.gcd(a, b);
 		if (gcd == 0) {
 			return undefined();
 		}
 
-		al = al / gcd;
-		bl = bl / gcd;
+		long nominator = a / gcd;
+		long denominator = b / gcd;
 
-		// we will now evaluate (x^a)^(1/b) instead of
-		// x^(a/b)
-		// set base = x^a
-		if (al != 1) {
-			base = base.pow(al);
+		if (nominator != 1) {
+			base = base.pow(nominator);
 		}
+
 		if (base.isPositive()) {
-			// base > 0 => base^(1/b) is no problem
-			return base.pow(1d / bl);
+			return base.pow(1d / denominator);
 		}
-		boolean oddB = (Math.abs(bl) % 2) == 1;
-		if (oddB) {
-			// base < 0 and b odd: (base)^(1/b) =
-			// -(-base^(1/b))
-			return base.negative().pow(1d / bl);
+
+		if (isOdd(denominator)) {
+			return base.negative().pow(1d / denominator);
 		}
-		// base < 0 and a & b even: (base)^(1/b)
-		// = undefined
 		return undefined();
 	}
 
+	private boolean isOdd(long value) {
+		return (Math.abs(value) % 2) == 1;
+	}
 }
