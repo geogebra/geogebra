66f486dc955b8500d2132b3dfe02c0911f553ee3
Bencze Balazs <megette_a@yahoo.com>
2021-05-26 17:27:20 +0300

    APPS-2610 Use the value of the traverse method, if this is replaced. Fix calculating numeric value for arbitrary constant

--- common/src/main/java/org/geogebra/common/kernel/geos/GeoSymbolic.java
+++ common/src/main/java/org/geogebra/common/kernel/geos/GeoSymbolic.java
@@ -25,6 +25,7 @@ import org.geogebra.common.kernel.arithmetic.MyArbitraryConstant;
 import org.geogebra.common.kernel.arithmetic.MyDouble;
 import org.geogebra.common.kernel.arithmetic.MyList;
 import org.geogebra.common.kernel.arithmetic.MyVecNDNode;
+import org.geogebra.common.kernel.arithmetic.NumberValue;
 import org.geogebra.common.kernel.arithmetic.Traversing;
 import org.geogebra.common.kernel.arithmetic.ValueType;
 import org.geogebra.common.kernel.arithmetic.variable.Variable;
@@ -269,7 +270,7 @@ public class GeoSymbolic extends GeoElement
 	}
 
 	private ExpressionValue maybeComputeNumericValue(ExpressionValue casOutput) {
-		if (casOutput == null || !casOutput.isNumberValue()) {
+		if (!shouldComputeNumericValue(casOutput)) {
 			return null;
 		}
 		Log.debug("GeoSymbolic is a number value, calculating numeric result");
@@ -278,6 +279,15 @@ public class GeoSymbolic extends GeoElement
 		} catch (Exception e) {
 			return null;
 		}
+	}
+
+	private boolean shouldComputeNumericValue(ExpressionValue casOutput) {
+		if (casOutput != null && casOutput.isNumberValue()) {
+			ExpressionValue unwrapped = casOutput.unwrap();
+			return !(unwrapped instanceof NumberValue && !((NumberValue) unwrapped).isDefined())
+					&& !(unwrapped instanceof GeoDummyVariable);
+		}
+		return false;
 	}
 
 	private ExpressionValue computeNumericValue(ExpressionValue casOutput) {
