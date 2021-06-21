261c14123cd0506ef2c7dcd188a93bc2d9761f7f
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2021-06-21 13:07:16 +0200

    MOW-1189 remove unused imports

--- web/src/main/java/org/geogebra/web/full/gui/util/ColorChooserW.java
+++ web/src/main/java/org/geogebra/web/full/gui/util/ColorChooserW.java
@@ -25,13 +25,6 @@ import org.geogebra.web.html5.util.Dom;
 import org.geogebra.web.shared.components.DialogData;
 
 import com.google.gwt.canvas.client.Canvas;
-import com.google.gwt.canvas.dom.client.Context2d;
-import com.google.gwt.canvas.dom.client.Context2d.TextBaseline;
-import com.google.gwt.dom.client.ImageElement;
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
