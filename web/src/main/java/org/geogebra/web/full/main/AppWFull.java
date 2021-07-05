18eedf7e18c27575c8a28c14bd880eb7a17fce50
Zbynek Konecny <zbynek@geogebra.at>
2021-07-05 16:06:45 +0200

    APPS-3049 Hide keyboard on app switch, fix overflow issue

--- web/src/main/java/org/geogebra/web/full/main/AppWFull.java
+++ web/src/main/java/org/geogebra/web/full/main/AppWFull.java
@@ -2397,6 +2397,7 @@ public class AppWFull extends AppW implements HasKeyboard, MenuViewListener {
 			DockPanel avPanel = getGuiManager().getLayout().getDockManager()
 					.getPanel(VIEW_ALGEBRA);
 			if (avPanel instanceof ToolbarDockPanelW) {
+				hideKeyboard();
 				((ToolbarDockPanelW) avPanel).getToolbar().close(true, 0);
 				((ToolbarDockPanelW) avPanel).getToolbar().setAVIconNonSelect(isExam());
 			}
