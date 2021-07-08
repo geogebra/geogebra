0215c98ebbfd537d5052af665d5259c9da1c7157
barni <barna.karacsony@geogebra.org>
2021-07-08 09:34:42 +0200

    APPS-3067 use a DefaultCharProvider object to implement CharacterProviderA.kt

--- keyboard-base/src/main/java/org/geogebra/keyboard/base/model/impl/factory/CharacterProvider.java
+++ keyboard-base/src/main/java/org/geogebra/keyboard/base/model/impl/factory/CharacterProvider.java
@@ -6,13 +6,22 @@ package org.geogebra.keyboard.base.model.impl.factory;
 public interface CharacterProvider {
 
     String xForButton();
+
     String xAsInput();
+
     String yForButton();
+
     String yAsInput();
+
     String zForButton();
+
     String zAsInput();
+
     String eulerForButton();
+
     String eulerAsInput();
+
     String piForButton();
+
     String piAsInput();
 }
