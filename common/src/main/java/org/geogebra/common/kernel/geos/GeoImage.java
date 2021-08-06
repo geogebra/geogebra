3a74aa7bdc9cb318f6d982f28c1cfb1bfab4e6fe
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2021-08-06 07:45:22 +0200

    MOW-1238 fix ruler and protractor size

--- common/src/main/java/org/geogebra/common/kernel/geos/GeoImage.java
+++ common/src/main/java/org/geogebra/common/kernel/geos/GeoImage.java
@@ -1467,11 +1467,11 @@ public class GeoImage extends GeoElement implements Locateable,
 	public void setImagePropertiesIfNecessary() {
 		if (isMeasurementTool) {
 			if (getImageFileName().contains("Ruler.svg")) {
-				app.getActiveEuclidianView().setMeasurementTool(this, 1488, 72, 72);
+				app.getActiveEuclidianView().setMeasurementTool(this, 1472, 72, 72);
 			}
 			if (getImageFileName().contains("Protractor.svg")) {
 				int middle = (app.getActiveEuclidianView().getWidth() - 500) / 2;
-				app.getActiveEuclidianView().setMeasurementTool(this, 500, 250, middle);
+				app.getActiveEuclidianView().setMeasurementTool(this, 558, 296, middle);
 			}
 		}
 	}
