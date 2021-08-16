158af6a3c75b96b89d702d21a461d83582bff7e6
agoston <agoston@geogebra.org>
2021-08-16 16:39:54 +0300

    APPS-3160: fix scrolling of cell editor

--- retex/editor-base/src/main/java/com/himamis/retex/editor/share/editor/MathFieldInternal.java
+++ retex/editor-base/src/main/java/com/himamis/retex/editor/share/editor/MathFieldInternal.java
@@ -396,10 +396,10 @@ public class MathFieldInternal
 	 * @param key key name
 	 */
 	public void notifyAndUpdate(String key) {
+		update();
 		if (listener != null) {
 			listener.onKeyTyped(key);
 		}
-		update();
 	}
 
 	@Override
