a3f81e529bf3188371d8fa8bbed0da695fa18391
Laszlo Gal <laszlo@geogebra.org>
2021-08-22 11:54:21 +0200

    APPS-2929 refactor: IntervalMultiply pt.2

--- common/src/main/java/org/geogebra/common/kernel/interval/IntervalMultiply.java
+++ common/src/main/java/org/geogebra/common/kernel/interval/IntervalMultiply.java
@@ -2,6 +2,8 @@ package org.geogebra.common.kernel.interval;
 
 /**
  * Multiplication of intervals.
+ *
+ * Note: an interval [high, low] is called mixed when low < 0 < high.
  */
 public class IntervalMultiply {
 	private final Interval interval;
@@ -63,31 +65,40 @@ public class IntervalMultiply {
 		if (interval.getHigh() > 0) {
 			if (other.getLow() < 0) {
 				if (other.getHigh() > 0) {
-					// positive * mixed
-					interval
-							.set(RMath.mulLow(interval.getHigh(), other.getLow()),
-									RMath.mulHigh(interval.getHigh(), other.getHigh()));
+					multiplyPositiveWithMixed(other);
 				} else {
-					// positive * negative
-					interval
-							.set(RMath.mulLow(interval.getHigh(), other.getLow()),
-									RMath.mulHigh(interval.getLow(), other.getHigh()));
+					multiplyPositiveWithNegative(other);
 				}
 			} else {
 				if (other.getHigh() > 0) {
-					// positive * positive
-					interval
-							.set(RMath.mulLow(interval.getLow(), other.getLow()),
-									RMath.mulHigh(interval.getHigh(), other.getHigh()));
+					multiplyPositiveWithPositive(other);
 				} else {
-					// positive * zero
-					interval.setZero();
+					multiplyWithZero();
 				}
 			}
 		} else {
-			// zero * any other value
-			interval.setZero();
+			multiplyWithZero();
 		}
+	}
+
+	private void multiplyWithZero() {
+		interval.setZero();
+	}
+
+	private void multiplyPositiveWithPositive(Interval other) {
+		multiply(interval.getLow(), other.getLow(), interval.getHigh(), other.getHigh());
+	}
+
+	private void multiply(double low1, double low2, double high1, double high2) {
+		interval.set(RMath.mulLow(low1, low2), RMath.mulHigh(high1, high2));
+	}
+
+	private void multiplyPositiveWithNegative(Interval other) {
+		multiply(interval.getHigh(), other.getLow(), interval.getLow(), other.getHigh());
+	}
+
+	private void multiplyPositiveWithMixed(Interval other) {
+		multiply(interval.getHigh(), other.getLow(), interval.getHigh(), other.getHigh());
 	}
 
 	private void xlNegative(Interval other) {
@@ -103,57 +114,58 @@ public class IntervalMultiply {
 			if (other.getHigh() > 0) {
 				multiplyNegativeWithMixed(other);
 			} else {
-				// negative * negative
-				interval
-						.set(RMath.mulLow(interval.getHigh(), other.getHigh()),
-								RMath.mulHigh(interval.getLow(), other.getLow()));
+				multiplyNegativeWithNegative(other);
 			}
 		} else {
 			if (other.getHigh() > 0) {
-				// negative * positive
-				interval
-						.set(RMath.mulLow(interval.getLow(), other.getHigh()),
-								RMath.mulHigh(interval.getHigh(), other.getLow()));
+				multiplyNegativeWithPositive(other);
 			} else {
-				// negative * zero
-				interval.setZero();
+				multiplyWithZero();
 			}
 		}
+	}
+
+	private void multiplyNegativeWithPositive(Interval other) {
+		multiply(interval.getLow(), other.getHigh(), interval.getHigh(), other.getLow());
+	}
+
+	private void multiplyNegativeWithNegative(Interval other) {
+		multiply(interval.getHigh(), other.getHigh(), interval.getLow(), other.getLow());
 	}
 
 	private void multiplyNegativeWithMixed(Interval other) {
-		interval
-				.set(RMath.mulLow(interval.getLow(), other.getHigh()),
-						RMath.mulHigh(interval.getLow(), other.getLow()));
+		multiply(interval.getLow(), other.getHigh(), interval.getLow(), other.getLow());
 	}
 
 	private void multiplyMixedWidth(Interval other) {
-
 		if (other.getLow() < 0) {
 			if (other.getHigh() > 0) {
-				// mixed * mixed
-				interval
-						.set(Math.min(RMath.mulLow(interval.getLow(), other.getHigh()),
-										RMath.mulLow(interval.getHigh(), other.getLow())),
-								Math.max(RMath.mulHigh(interval.getLow(), other.getLow()),
-										RMath.mulHigh(interval.getHigh(), other.getHigh())));
+				multiplyMixedWithMixed(other);
 			} else {
-				// mixed * negative
-				interval
-						.set(RMath.mulLow(interval.getHigh(), other.getLow()), RMath.mulHigh(interval.getLow(),
-								other.getLow()));
+				multiplyMixedWithNegative(other);
 			}
 		} else {
 			if (other.getHigh() > 0) {
-				// mixed * positive
-				interval
-						.set(RMath.mulLow(interval.getLow(), other.getHigh()), RMath.mulHigh(
-								interval.getHigh(),
-								other.getHigh()));
+				multiplyMixedWithPositive(other);
 			} else {
-				// mixed * zero
-				interval.setZero();
+				multiplyWithZero();
 			}
 		}
+	}
+
+	private void multiplyMixedWithPositive(Interval other) {
+		multiply(interval.getLow(), other.getHigh(), interval.getHigh(), other.getHigh());
+	}
+
+	private void multiplyMixedWithNegative(Interval other) {
+		multiply(interval.getHigh(), other.getLow(), interval.getLow(), other.getLow());
+	}
+
+	private void multiplyMixedWithMixed(Interval other) {
+		interval
+				.set(Math.min(RMath.mulLow(interval.getLow(), other.getHigh()),
+								RMath.mulLow(interval.getHigh(), other.getLow())),
+						Math.max(RMath.mulHigh(interval.getLow(), other.getLow()),
+								RMath.mulHigh(interval.getHigh(), other.getHigh())));
 	}
 }
