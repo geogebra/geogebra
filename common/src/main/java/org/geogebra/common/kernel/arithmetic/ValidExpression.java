077965b27812ab55ac9cd7bb10dea8f5965ed8ad
Balázs Bencze <balazs.bencze@geogebra.org>
2021-09-02 15:27:32 +0200

    APPS-931 Remove unused import

--- common/src/main/java/org/geogebra/common/kernel/arithmetic/ValidExpression.java
+++ common/src/main/java/org/geogebra/common/kernel/arithmetic/ValidExpression.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-import java.util.Vector;
 
 import org.geogebra.common.kernel.GTemplate;
 import org.geogebra.common.kernel.Kernel;
