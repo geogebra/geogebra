771545d55bd394dc0baaccc47a64cabdaa992875
barni <barna.karacsony@geogebra.org>
2021-09-22 09:25:11 +0200

    APPS-3236 show the correct context menu items in the Suite subapps

--- web/src/main/java/org/geogebra/web/full/main/activity/SuiteActivity.java
+++ web/src/main/java/org/geogebra/web/full/main/activity/SuiteActivity.java
@@ -1,12 +1,20 @@
 package org.geogebra.web.full.main.activity;
 
+import java.util.Objects;
+
 import org.geogebra.common.GeoGebraConstants;
+import org.geogebra.common.kernel.geos.GeoElement;
 import org.geogebra.common.main.AppConfig;
 import org.geogebra.common.main.settings.config.AppConfigCas;
 import org.geogebra.common.main.settings.config.AppConfigGeometry;
 import org.geogebra.common.main.settings.config.AppConfigGraphing3D;
 import org.geogebra.common.main.settings.config.AppConfigProbability;
 import org.geogebra.common.main.settings.config.AppConfigUnrestrictedGraphing;
+import org.geogebra.web.full.gui.view.algebra.AlgebraViewW;
+import org.geogebra.web.full.gui.view.algebra.MenuItemCollection;
+import org.geogebra.web.full.gui.view.algebra.contextmenu.AlgebraMenuItemCollection;
+import org.geogebra.web.full.gui.view.algebra.contextmenu.AlgebraMenuItemCollection3D;
+import org.geogebra.web.full.gui.view.algebra.contextmenu.AlgebraMenuItemCollectionCAS;
 import org.geogebra.web.html5.main.AppW;
 
 /**
@@ -41,5 +49,17 @@ public class SuiteActivity extends BaseActivity {
 	public void start(AppW app) {
 		super.start(app);
 		app.getKernel().getGeoGebraCAS().initCurrentCAS();
+	}
+
+	@Override
+	public MenuItemCollection<GeoElement> getAVMenuItems(AlgebraViewW view) {
+		switch (Objects.requireNonNull(getConfig().getSubAppCode())) {
+		case GeoGebraConstants.CAS_APPCODE:
+			return new AlgebraMenuItemCollectionCAS(view);
+		case GeoGebraConstants.G3D_APPCODE:
+			return new AlgebraMenuItemCollection3D(view);
+		default:
+			return super.getAVMenuItems(view);
+		}
 	}
 }
