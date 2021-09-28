194fed0c19398181dc86b6d52ad3c42c747df1fc
Zbynek Konecny <zbynek@geogebra.at>
2021-09-28 19:23:14 +0200

    Make sure GWTMockito prints fewer exceptions

--- web/src/main/java/org/geogebra/web/cas/giac/CASgiacW.java
+++ web/src/main/java/org/geogebra/web/cas/giac/CASgiacW.java
@@ -154,7 +154,7 @@ public class CASgiacW extends CASgiac {
 	}
 
 	private void setUpInitCAS() {
-		if (Js.isFalsy(GeoGebraGlobal.__ggb__giac)) {
+		if (!"object".equals(Js.typeof(GeoGebraGlobal.__ggb__giac))) {
 			GeoGebraGlobal.__ggb__giac = new GiacNative();
 		}
 
