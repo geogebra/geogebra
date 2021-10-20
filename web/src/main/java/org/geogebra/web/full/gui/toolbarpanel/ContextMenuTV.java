f6db6a3b1361f5d13ff228cfa8e7d79490d4537f
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2021-10-20 10:00:28 +0200

    APPS-3363 reorder contex menu items

--- web/src/main/java/org/geogebra/web/full/gui/toolbarpanel/ContextMenuTV.java
+++ web/src/main/java/org/geogebra/web/full/gui/toolbarpanel/ContextMenuTV.java
@@ -79,6 +79,7 @@ public class ContextMenuTV {
 		wrappedPopup.getPopupPanel().addStyleName("tvContextMenu");
 		if (getColumnIdx() > 0) {
 			GeoEvaluatable column = view.getEvaluatable(getColumnIdx());
+			addShowHidePoints();
 			if (column instanceof GeoList) {
 				buildYColumnMenu();
 			} else {
@@ -101,7 +102,6 @@ public class ContextMenuTV {
 
 	private void buildYColumnMenu() {
 		addDelete();
-		addShowHidePoints();
 		wrappedPopup.addVerticalSeparator();
 		String headerHTMLName = view.getHeaderNameHTML(getColumnIdx());
 		addStats(getStatisticsTransKey(headerHTMLName), view::getStatistics1Var);
@@ -116,7 +116,6 @@ public class ContextMenuTV {
 	}
 
 	private void buildFunctionColumnMenu() {
-		addShowHidePoints();
 		addEdit(() -> {
 			GuiManagerInterfaceW guiManager = getApp().getGuiManager();
 			if (guiManager != null) {
