66747a4c15af2d28575fa45b4b2fbe29dc7c303d
Zbynek Konecny <zbynek1729@gmail.com>
2021-11-05 20:17:07 +0100

    WLY-82 Close dropdown when it becomes fixed

--- common/src/main/java/org/geogebra/common/euclidian/draw/dropdown/DrawDropDownList.java
+++ common/src/main/java/org/geogebra/common/euclidian/draw/dropdown/DrawDropDownList.java
@@ -104,7 +104,9 @@ public final class DrawDropDownList extends CanvasDrawable
 		int fontSize = (int) (view.getFontSize()
 				* geoList.getFontSizeMultiplier());
 		setLabelFontSize(fontSize);
-
+		if (geo.isLocked() || !geo.isSelectionAllowed(view)) {
+			setOptionsVisible(false);
+		}
 		if (!isVisible) {
 			return;
 		}
