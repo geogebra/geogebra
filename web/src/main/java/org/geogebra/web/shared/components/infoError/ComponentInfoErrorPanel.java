ca5af3ac33e18184a5455e919b42668c3aa71b2d
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2021-11-29 11:27:15 +0100

    APPS-3416 extract component part to separate branch

---
+++ web/src/main/java/org/geogebra/web/shared/components/infoError/ComponentInfoErrorPanel.java
@@ -0,0 +1,52 @@
+package org.geogebra.web.shared.components.infoError;
+
+import org.geogebra.common.main.Localization;
+import org.geogebra.web.html5.gui.util.NoDragImage;
+import org.geogebra.web.html5.gui.view.button.StandardButton;
+import org.geogebra.web.resources.SVGResource;
+
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+
+public class ComponentInfoErrorPanel extends FlowPanel {
+	private Localization loc;
+
+	/**
+	 * info/error panel constructor
+	 * @param loc - localization
+	 * @param data - data of the panel including title, subtext and button text
+	 * @param img - image
+	 * @param buttonAction - handler for the button
+	 */
+	public ComponentInfoErrorPanel(Localization loc, InfoErrorData data, SVGResource img,
+			Runnable buttonAction) {
+		this.loc = loc;
+		addStyleName("infoErrorPanel");
+		buildGUI(data, img, buttonAction);
+	}
+
+	private void buildGUI(InfoErrorData data, SVGResource img, Runnable buttonAction) {
+		NoDragImage infoImage = new NoDragImage(img, 56, 56);
+		add(infoImage);
+
+		if (data.getTitle() != null) {
+			Label titleLabel = new Label(loc.getMenu(data.getTitle()));
+			titleLabel.setStyleName("title");
+			add(titleLabel);
+		}
+
+		if (data.getSubtext() != null) {
+			Label subtextLabel = new Label(loc.getMenu(data.getSubtext()));
+			subtextLabel.setStyleName("subtext");
+			add(subtextLabel);
+		}
+
+		if (data.getActionButtonText() != null) {
+			StandardButton actionButton =
+					new StandardButton(loc.getMenu(data.getActionButtonText()));
+			actionButton.addStyleName("dialogContainedButton");
+			actionButton.addFastClickHandler(source -> buttonAction.run());
+			add(actionButton);
+		}
+	}
+}
