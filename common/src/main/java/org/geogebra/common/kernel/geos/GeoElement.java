36189a2d60c8e10a1de6a533cfc645fd4178b0cf
Balazs Bencze <balazs.bencze@geogebra.org>
2021-11-29 10:20:46 +0200

    APPS-3446 Do not remove twin geo when showing special points (in turn removes zoom listener for EV)

--- common/src/main/java/org/geogebra/common/kernel/geos/GeoElement.java
+++ common/src/main/java/org/geogebra/common/kernel/geos/GeoElement.java
@@ -6642,12 +6642,7 @@ public abstract class GeoElement extends ConstructionElement implements GeoEleme
 	// specific input protection
 	/////////////////////////////
 
-	/**
-	 * set this can (not) be removed when input of algo
-	 * 
-	 * @param flag
-	 *            flag
-	 */
+	@Override
 	public void setCanBeRemovedAsInput(boolean flag) {
 		canBeRemovedAsInput = flag;
 	}
