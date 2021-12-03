6b63e0bee99b543382d27d5f83d53692f2ee7f12
Zbynek Konecny <zbynek@geogebra.at>
2021-12-03 15:10:04 +0100

    WLY-214 Handle arb. integers in SolveODE(f',point)

--- common-jre/src/test/java/org/geogebra/common/properties/impl/objects/NamePropertyTest.java
+++ common-jre/src/test/java/org/geogebra/common/properties/impl/objects/NamePropertyTest.java
@@ -1,7 +1,7 @@
 package org.geogebra.common.properties.impl.objects;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import org.geogebra.common.BaseUnitTest;
