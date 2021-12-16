c8e7331d393953bdb9c2047801a432d076e1fe78
Balazs Bencze <balazs.bencze@geogebra.org>
2021-12-16 14:21:07 +0200

    APPS-3502 Remove unused imports

--- common/src/main/java/org/geogebra/common/properties/factory/GeoElementPropertiesFactory.java
+++ common/src/main/java/org/geogebra/common/properties/factory/GeoElementPropertiesFactory.java
@@ -1,8 +1,6 @@
 package org.geogebra.common.properties.factory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.geogebra.common.kernel.commands.AlgebraProcessor;
