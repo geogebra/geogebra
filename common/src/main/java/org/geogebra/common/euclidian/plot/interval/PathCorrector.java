e300ada8d7cb44af202d6c73bb895d9490ae1edb
laszlo <laszlo@geogebra.at>
2022-01-11 21:08:09 +0100

    APPS-3193 simplified PathCorrector

--- common/src/main/java/org/geogebra/common/euclidian/plot/interval/PathCorrector.java
+++ common/src/main/java/org/geogebra/common/euclidian/plot/interval/PathCorrector.java
@@ -3,7 +3,6 @@ package org.geogebra.common.euclidian.plot.interval;
 import org.geogebra.common.kernel.interval.Interval;
 import org.geogebra.common.kernel.interval.IntervalConstants;
 import org.geogebra.common.kernel.interval.IntervalTuple;
-import org.geogebra.common.util.DoubleUtil;
 
 /**
  * Class to correct interval path at limits
@@ -52,6 +51,10 @@ public class PathCorrector {
 		return this.lastY;
 	}
 
+	private boolean isInvertedAround(int idx) {
+		return model.isInvertedAt(idx - 1) && model.isInvertedAt(idx + 1);
+	}
+
 	private void extractAndDraw(IntervalTuple tuple) {
 		Interval extractLow = tuple.y().extractLow();
 		Interval extractHigh = tuple.y().extractHigh();
@@ -64,7 +67,7 @@ public class PathCorrector {
 			Interval sx = bounds.toScreenIntervalX(x);
 			double sValue = bounds.toScreenCoordYd(value);
 			gp.moveTo(sx.getLow(), 0);
-			gp.lineTo(sx.getLow(), sValue);
+			gp.lineTo(sx.getHigh(), sValue);
 		}
 	}
 
@@ -73,100 +76,8 @@ public class PathCorrector {
 			Interval sx = bounds.toScreenIntervalX(x);
 			double sValue = bounds.toScreenCoordYd(value);
 			gp.moveTo(sx.getLow(), bounds.getHeight());
-			gp.lineTo(sx.getLow(), sValue);
+			gp.lineTo(sx.getHigh(), sValue);
 		}
-	}
-
-
-	private void handleFill(IntervalTuple tuple) {
-		Interval sx = toScreenX(tuple);
-		Interval sy = toScreenY(tuple);
-		gp.moveTo(sx.getLow(), 0);
-		gp.lineTo(sx.getHigh(), sy.getLow());
-		Interval y = tuple.y();
-		if (y.containsExclusive(0)) {
-			gp.moveTo(sx.getLow(), bounds.getHeight());
-			gp.lineTo(sx.getLow(), sy.getHigh());
-		}
-		lastY.set(sy);
-	}
-
-	private boolean isInvertedAround(int idx) {
-		return model.isInvertedAt(idx - 1) && model.isInvertedAt(idx + 1);
-	}
-
-	private void handleUnion(int idx) {
-		IntervalTuple point = model.pointAt(idx);
-		IntervalTuple prev = model.pointAt(idx - 1);
-		IntervalTuple next = model.pointAt(idx + 1);
-
-		boolean before = model.isAscendingBefore(idx);
-		Interval x = toScreenX(point);
-		Interval y = toScreenY(point);
-		if (prev != null) {
-			Interval prevX = toScreenX(prev);
-			Interval prevY = toScreenY(prev);
-			gp.lineTo(prevX.getLow(), before ? 0 : bounds.getHeight());
-		}
-		if (next != null) {
-			Interval nextX = toScreenX(next);
-			Interval nextY = toScreenY(next);
-			if (prev != null) {
-				gp.moveTo(nextX.getLow(), before ? bounds.getHeight() : 0);
-			}
-			gp.lineTo(nextX.getHigh(), nextY.getHigh());
-		}
-		lastY.set(IntervalConstants.undefined());
-
-	}
-
-	private boolean hasPointNextTo(int idx) {
-		return !model.isEmptyAt(idx + 1);
-	}
-
-	private void completePathFromLeft(IntervalTuple point, boolean ascending) {
-		Interval x = toScreenX(point);
-		Interval y = toScreenY(point);
-		double xMiddle = x.getLow() + (x .getWidth() / 2);
-		double yLow = y.getLow() < bounds.getHeight()
-				? Math.max(0, y.getLow())
-				: bounds.getHeight();
-
-		if (ascending && yLow >= 0) {
-			gp.lineTo(xMiddle, 0);
-		} else if (lastY.isInverted()) {
-			gp.lineTo(xMiddle, lastY.getLow());
-		} else if (!DoubleUtil.isEqual(lastY.getLow(), yLow) && yLow < bounds.getHeight()) {
-			gp.lineTo(xMiddle, bounds.getHeight());
-		}
-	}
-
-	private Interval toScreenY(IntervalTuple point) {
-		return bounds.toScreenIntervalY(point.y());
-	}
-
-	private Interval toScreenX(IntervalTuple point) {
-		return bounds.toScreenIntervalX(point.x());
-	}
-
-	private Interval completePathFromRight(IntervalTuple point, boolean ascending) {
-		Interval x = toScreenX(point);
-		Interval y = toScreenY(point);
-		double xMiddle = x.getLow() + (x.getWidth() / 2);
-		double yLow = y.getLow() < bounds.getHeight()
-				? Math.max(0, y.getLow())
-				: bounds.getHeight();
-		if (ascending) {
-			if (y.getHigh() > 0) {
-				gp.moveTo(xMiddle, bounds.getHeight());
-				return new Interval(bounds.getHeight());
-			}
-		} else if (yLow < bounds.getHeight()) {
-			gp.moveTo(xMiddle, 0);
-			gp.lineTo(x.getHigh(), yLow);
-			return new Interval(yLow);
-		}
-		return IntervalConstants.undefined();
 	}
 
 	/**
@@ -183,7 +94,7 @@ public class PathCorrector {
 			return IntervalConstants.undefined();
 		}
 
-		Interval nextY = toScreenY(model.pointAt(index + 1));
+		Interval nextY = bounds.toScreenIntervalY(model.pointAt(index + 1).y());
 		if (model.isAscendingAfter(index)) {
 			completeToNegativeInfinity(x, nextY);
 		} else {
