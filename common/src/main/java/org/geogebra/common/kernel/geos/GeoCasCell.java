6f7e42811db699b3ace07d252910aaa3b2a9b97b
Zbynek Konecny <zbynek@geogebra.at>
2022-02-23 18:36:07 +0100

    APPS-3647 Improve handling of surfaces in CAS

--- common/src/main/java/org/geogebra/common/kernel/geos/GeoCasCell.java
+++ common/src/main/java/org/geogebra/common/kernel/geos/GeoCasCell.java
@@ -149,6 +149,7 @@ public class GeoCasCell extends GeoElement
 	private boolean pointList;
 
 	private String tooltip;
+	private boolean twinLoadedFromFile;
 
 	/**
 	 * Creates new CAS cell
@@ -1966,44 +1967,17 @@ public class GeoCasCell extends GeoElement
 						((GeoNumeric) twinGeo)
 								.extendMinMax(lastOutputEvaluationGeo);
 					}
-					if (twinGeo instanceof GeoSurfaceCartesianND
-							&& lastOutputEvaluationGeo instanceof GeoSurfaceCartesianND) {
-						// when we replace twinGeo, dependent geos are also
-						// deleted from cons
-						twinGeo.doRemove();
-						notifyRemove();
-						twinGeo = lastOutputEvaluationGeo;
-						cons.addToConstructionList(twinGeo, true);
-						cons.putLabel(twinGeo);
-						twinGeo.notifyAdd();
-						twinGeo.setCorrespondingCasCell(this);
-						// add to construction casCell and parentAlgo
-						if (this.getParentAlgorithm() != null) {
-							cons.addToConstructionList(
-									this.getParentAlgorithm(), true);
-						}
-						cons.addToGeoSetWithCasCells(this);
-						if (assignmentVar == null) {
-							assignmentVar = twinGeo
-									.getLabel(StringTemplate.defaultTemplate);
-						}
+
+					if (uniformListCommand() && lastOutputEvaluationGeo instanceof GeoList) {
+						makePlotable((GeoList) lastOutputEvaluationGeo);
 					}
-					// switch twinGeo with new evaluation
-					// needed for undo->function wasn't draggable
-					else {
-
-						if (uniformListCommand() && lastOutputEvaluationGeo instanceof GeoList) {
-							makePlotable((GeoList) lastOutputEvaluationGeo);
-						}
-						// if both geos are the same type we can use set safely
-						twinGeo.set(lastOutputEvaluationGeo);
-						// update constants references
-						if (lastOutputEvaluationGeo instanceof GeoFunction) {
-							ExpressionNode expr = ((GeoFunction) lastOutputEvaluationGeo)
-									.getFunctionExpression();
-							expr.inspect(new ArbconstAlgoFixer());
-
-						}
+					// if both geos are the same type we can use set safely
+					twinGeo.set(lastOutputEvaluationGeo);
+					// update constants references
+					if (lastOutputEvaluationGeo instanceof GeoFunction) {
+						ExpressionNode expr = ((GeoFunction) lastOutputEvaluationGeo)
+								.getFunctionExpression();
+						expr.inspect(new ArbconstAlgoFixer());
 					}
 				} else if (!lastOutputEvaluationGeo.isDefined()) {
 					// newly created GeoElement is undefined, we can set our
@@ -3779,4 +3753,16 @@ public class GeoCasCell extends GeoElement
 		return cmd != null && "IntegralSymbolic".equals(cmd.getName());
 	}
 
+	/**
+	 * Hide twin if it was not loaded from XML
+	 */
+	public void updateTwinGeoVisibility() {
+		if (!twinLoadedFromFile && twinGeo != null) {
+			twinGeo.setEuclidianVisible(false);
+		}
+	}
+
+	public void setTwinLoadedFromFile(boolean twinLoadedFromFile) {
+		this.twinLoadedFromFile = twinLoadedFromFile;
+	}
 }
