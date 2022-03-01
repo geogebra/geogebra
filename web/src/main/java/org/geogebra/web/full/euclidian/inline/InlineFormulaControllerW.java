27b58265c7cc58af2b8a6f2383c9a666a39c41f5
laszlo <laszlo@geogebra.at>
2022-03-01 11:40:05 +0100

    MOW-1312 getHeightWithMargin()

--- web/src/main/java/org/geogebra/web/full/euclidian/inline/InlineFormulaControllerW.java
+++ web/src/main/java/org/geogebra/web/full/euclidian/inline/InlineFormulaControllerW.java
@@ -181,7 +181,7 @@ public class InlineFormulaControllerW implements InlineFormulaController {
 				int width = (int) ((mathFieldEditor.getMathField().asWidget().getOffsetWidth()
 						- DrawFormula.PADDING) * formula.getWidth() / formula
 							.getContentWidth());
-				int height = mathFieldEditor.getMathField().asWidget().getOffsetHeight();
+				int height = (int) mathFieldEditor.getMathField().getHeightWithMargin();
 
 				formula.setSize(Math.max(formula.getWidth(), width),
 						Math.max(formula.getHeight(), height));
