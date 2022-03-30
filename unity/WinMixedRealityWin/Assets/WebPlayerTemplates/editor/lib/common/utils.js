e97b0cd863be01ac1a2a92e4f3d85185a979e48d
Zbynek Konecny <zbynek@geogebra.org>
2022-03-31 00:16:00 +0200

    Move unity to a separate repo

--- unity/WinMixedRealityWin/Assets/WebPlayerTemplates/editor/lib/common/utils.js
+++
@@ -1,1 +0,0 @@
-define(["require","exports"],function(e,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.dispatch=function(e,t){document.body.dispatchEvent(new CustomEvent(e,{detail:t}))}});
