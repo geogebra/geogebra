62e501d7795640f52f9ac811d1502ab8462fc22d
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2022-04-15 11:15:38 +0200

    APPS-3924 fix NPE the right way

--- common/src/main/java/org/geogebra/common/geogebra3D/euclidian3D/draw/DrawLabel3D.java
+++ common/src/main/java/org/geogebra/common/geogebra3D/euclidian3D/draw/DrawLabel3D.java
@@ -85,7 +85,7 @@ public class DrawLabel3D {
 
     private CoordMatrix4x4 positionMatrix;
 	protected @CheckForNull CaptionText caption;
-	private final CaptionProperties properties;
+	private CaptionProperties properties;
 
 	/**
 	 * common constructor
@@ -220,6 +220,13 @@ public class DrawLabel3D {
 		this.xOffset = xOffset0;
 		this.yOffset = yOffset0;
 		this.zOffset = zOffset0;
+	}
+
+	public void initCaption(CaptionText caption) {
+		this.caption = caption;
+		if (properties == null) {
+			properties = new CaptionProperties(view);
+		}
 	}
 
 	/**
