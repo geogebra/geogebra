a13b1cf1c1f9004e683497e9944bd4c5ac4d136e
Zbynek Konecny <zbynek@geogebra.at>
2022-05-30 18:53:23 +0200

    MOW-1330 Move editor out of Murok DOM, simplify previews

--- web/src/main/java/org/geogebra/web/full/gui/util/LineStylePreview.java
+++ web/src/main/java/org/geogebra/web/full/gui/util/LineStylePreview.java
@@ -2,9 +2,6 @@ package org.geogebra.web.full.gui.util;
 
 import org.geogebra.common.awt.GColor;
 import org.geogebra.common.euclidian.EuclidianView;
-import org.geogebra.common.euclidian.draw.DrawLine;
-import org.geogebra.common.kernel.geos.GProperty;
-import org.geogebra.common.kernel.geos.GeoLine;
 import org.geogebra.web.html5.main.AppW;
 
 /**
@@ -15,9 +12,6 @@ import org.geogebra.web.html5.main.AppW;
  */
 public class LineStylePreview extends StylePreview {
 
-	private GeoLine line;
-	private DrawLine drawLine;
-
 	/**
 	 * @param app
 	 *            application
@@ -27,14 +21,7 @@ public class LineStylePreview extends StylePreview {
 	 *            height of line
 	 */
 	public LineStylePreview(AppW app, int width, int height) {
-		super(app, width, height);
-	}
-
-	@Override
-	protected void createPreviewGeo() {
-		line = new GeoLine(app.getKernel().getConstruction(), 0, 1, 0);
-		line.setLineType(1);
-		drawLine = new DrawLine(app.getActiveEuclidianView(), line);
+		super(width, height);
 	}
 
 	/**
@@ -48,13 +35,7 @@ public class LineStylePreview extends StylePreview {
 	 *            of the line.
 	 */
 	public void update(int thickness, int typeIdx, GColor color) {
-		line.setObjColor(color);
-		line.setLineThickness(thickness);
-		int lineStyle = EuclidianView.getLineType(typeIdx);
-		line.setLineType(lineStyle);
-		line.updateVisualStyleRepaint(GProperty.LINE_STYLE);
 		clear();
-		drawLine.drawStylePreview(g2, getMarginX(), getMarginY(),
-				getOffsetWidth() == 0 ? 30 : getOffsetWidth());
+		drawStylePreview(color, thickness, EuclidianView.getLineType(typeIdx), 30);
 	}
 }
