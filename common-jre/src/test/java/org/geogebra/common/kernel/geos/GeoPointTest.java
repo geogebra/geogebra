9bc27a899276d00d6d9feedb0f3cd2e18cd3dae7
barni <barna.karacsony@geogebra.org>
2022-06-21 19:05:02 +0200

    APPS-4059 test APPS-3568

--- common-jre/src/test/java/org/geogebra/common/kernel/geos/GeoPointTest.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/geos/GeoPointTest.java
@@ -1,17 +1,26 @@
 package org.geogebra.common.kernel.geos;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 import org.geogebra.common.BaseUnitTest;
+import org.geogebra.common.kernel.Kernel;
 import org.junit.Test;
 
 public class GeoPointTest  extends BaseUnitTest {
 
 	@Test
-	public void testDefinitionForEdit() {
+	public void testDefinitionForEditor() {
 		getApp().setGraphingConfig();
 		GeoPoint point = addAvInput("A=(1,2)");
 		assertThat(point.getDefinitionForEditor(), equalTo("A=(1,2)"));
+	}
+
+	@Test
+	public void testDefinitionForEditorAustrianCoords() {
+		getKernel().setCoordStyle(Kernel.COORD_STYLE_AUSTRIAN);
+		GeoPoint point = getKernel().getAlgoDispatcher().point(1, 2, false);
+		assertThat(point.getDefinitionForEditor().endsWith("(1,2)"), is(true));
 	}
 }
