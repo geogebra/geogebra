b26632b6314fd7efbcba95d00493ce5fd620d6ba
Krisztina Frank <krisztina.frank@geogebra.org>
2022-07-13 13:43:34 +0200

    APPS-3996 Keep value after rotate, change underline to purple

--- common/src/main/java/org/geogebra/common/gui/view/probcalculator/PropertyResultPanel.java
+++ common/src/main/java/org/geogebra/common/gui/view/probcalculator/PropertyResultPanel.java
@@ -156,5 +156,6 @@ public class PropertyResultPanel implements ResultPanel {
 		} else {
 			updateResult(view.getProbabilityText(view.probability));
 		}
+		updateLowHigh(""+view.low, ""+view.high);
 	}
 }
