ba93586f948bb0c6e99f645bbc5008e1bba5afd7
csillasolyom-gecse <csilla.solyom-gecse@geogebra.at>
2022-10-12 08:51:44 +0200

    remove dummy combobox for testing

--- web/src/main/java/org/geogebra/web/full/gui/layout/scientific/ScientificSettingsView.java
+++ web/src/main/java/org/geogebra/web/full/gui/layout/scientific/ScientificSettingsView.java
@@ -8,7 +8,6 @@ import org.geogebra.common.properties.Property;
 import org.geogebra.common.properties.factory.PropertiesArray;
 import org.geogebra.common.properties.factory.ScientificPropertiesFactory;
 import org.geogebra.web.full.gui.HeaderView;
-import org.geogebra.web.full.gui.components.ComponentCombobox;
 import org.geogebra.web.full.gui.components.ComponentDropDown;
 import org.geogebra.web.full.gui.layout.panels.AnimatingPanel;
 import org.geogebra.web.html5.gui.util.Dom;
@@ -79,11 +78,6 @@ public class ScientificSettingsView extends AnimatingPanel implements FastClickH
 				panel.add(cell);
 			}
 		}
-
-		ComponentCombobox combobox = new ComponentCombobox(app, "Label",
-				(EnumerableProperty) properties.getProperties()[properties
-						.getProperties().length - 1]);
-		panel.add(combobox);
 	}
 
 	private void updateGUI() {
