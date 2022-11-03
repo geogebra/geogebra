4d7b3663ac5af6628c3bb30cc150e3ef47f68bcb
laszlo <laszlo@geogebra.at>
2022-11-03 12:59:15 +0100

    APPS-4432 Dividing with whole is whole, process asymptotes when extending domain.

--- common/src/main/java/org/geogebra/common/kernel/interval/operators/IntervalDivide.java
+++ common/src/main/java/org/geogebra/common/kernel/interval/operators/IntervalDivide.java
@@ -42,7 +42,7 @@ public class IntervalDivide {
 			return IntervalConstants.undefined();
 		}
 
-		if (isZeroByZero(numerator, divisor) || isWholeByNonZero(numerator, divisor)) {
+		if (isZeroByZero(numerator, divisor) || divisor.isWhole() || isWholeByNonZero(numerator, divisor)) {
 			return whole();
 		}
 
