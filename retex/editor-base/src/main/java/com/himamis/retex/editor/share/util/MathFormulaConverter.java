0f366c98fc8d16d0842f56f5bac87c537303c673
laszlo <laszlo@geogebra.at>
2022-11-14 13:26:11 +0100

    APPS-4414 MathFormulaConverter for InputBox rendering.

---
+++ retex/editor-base/src/main/java/com/himamis/retex/editor/share/util/MathFormulaConverter.java
@@ -0,0 +1,41 @@
+package com.himamis.retex.editor.share.util;
+
+import com.himamis.retex.editor.share.io.latex.ParseException;
+import com.himamis.retex.editor.share.io.latex.Parser;
+import com.himamis.retex.editor.share.meta.MetaModel;
+import com.himamis.retex.editor.share.model.MathFormula;
+import com.himamis.retex.editor.share.serializer.TeXSerializer;
+
+public class MathFormulaConverter {
+	private final Parser parser;
+	private MathFormula formula;
+	private final TeXSerializer texSerializer;
+
+	/**
+	 * Constructor
+	 */
+	public MathFormulaConverter() {
+		MetaModel model = new MetaModel();
+		formula = MathFormula.newFormula(model);
+		parser = new Parser(model);
+		texSerializer = new TeXSerializer();
+	}
+
+	/**
+	 * Converst from GGB to MathML style latex.
+	 * @param text ggb text.
+	 * @return MathML styled text
+	 */
+	public String convert(String text) {
+		buildFormula(text);
+		return texSerializer.serialize(formula);
+	}
+
+	private void buildFormula(String text) {
+		try {
+			formula = parser.parse(text);
+		} catch (ParseException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
