3c2879fe12b4129caed7ba874ba32c60116c52ca
Zbynek Konecny <zbynek@geogebra.at>
2022-11-29 17:35:35 +0100

    APPS-4466 Allow computing slope of quantile plot

--- common-jre/src/test/java/org/geogebra/common/kernel/commands/CommandsTest.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/commands/CommandsTest.java
@@ -2666,6 +2666,7 @@ public class CommandsTest {
 	public void cmdNormalQuantilePlot() {
 		t("NormalQuantilePlot[ {2,3,4}]",
 				"{(2, -0.8193286198336103), (3, 0), (4, 0.8193286198336103), 2.8284271247461903}");
+		t("Slope(Element(NormalQuantilePlot[ {2,3,4}],4))", "1");
 	}
 
 	@Test
