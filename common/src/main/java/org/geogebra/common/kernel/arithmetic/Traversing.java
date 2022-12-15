1597bb103fc64e044f1cfab9fe96679d1bdfcc88
csillasolyom-gecse <csilla.solyom-gecse@geogebra.at>
2022-12-15 08:35:50 +0100

    APPS-4487 set label before suppressing label creation

--- common/src/main/java/org/geogebra/common/kernel/arithmetic/Traversing.java
+++ common/src/main/java/org/geogebra/common/kernel/arithmetic/Traversing.java
@@ -779,6 +779,7 @@ public interface Traversing {
 				GeoNumeric slider = geoClass == GeoClass.ANGLE
 						? new GeoAngle(kernel.getConstruction(), Math.PI / 4)
 						: new GeoNumeric(kernel.getConstruction(), 1);
+				slider.setLabel(name);
 				kernel.getConstruction().setSuppressLabelCreation(old);
 				undefined.add(slider);
 				boolean visible = !kernel.getApplication()
@@ -786,7 +787,6 @@ public interface Traversing {
 						|| kernel.getApplication().showAutoCreatedSlidersInEV();
 				GeoNumeric.setSliderFromDefault(slider,
 						geoClass == GeoClass.ANGLE, visible);
-				slider.setLabel(name);
 			}
 
 		}
