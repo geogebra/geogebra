ced389c007e0bfe30838e157a406e382abe8fd3c
Karl Traunmüller <karl.traunmueller@geogebra.at>
2023-01-26 10:15:47 +0100

    APPS-4611 minor code formatting change

--- common/src/main/java/org/geogebra/common/util/SymbolicUtil.java
+++ common/src/main/java/org/geogebra/common/util/SymbolicUtil.java
@@ -191,16 +191,13 @@ public class SymbolicUtil {
 	public static boolean shouldComputeNumericValue(ExpressionValue expression) {
 		if (expression != null && expression.isNumberValue()) {
 			ExpressionValue unwrapped = expression.unwrap();
-
 			if (expression.wrap().containsGeoDummyVariable()) {
 				return false;
 			}
-
 			if (unwrapped instanceof NumberValue) {
 				return ((NumberValue) unwrapped).isDefined();
-			} else {
-				return true;
 			}
+			return true;
 		}
 		return false;
 	}
