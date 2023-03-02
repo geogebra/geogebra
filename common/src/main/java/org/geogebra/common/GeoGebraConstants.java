c661e442c37065e9135e7a5ad23d5526ae2c9815
mena.aly <mena.aly@geogebra.at>
2023-03-02 10:59:40 +0100

    apps-4544: bumped ggb version to 5.2

--- common/src/main/java/org/geogebra/common/GeoGebraConstants.java
+++ common/src/main/java/org/geogebra/common/GeoGebraConstants.java
@@ -12,7 +12,7 @@ public interface GeoGebraConstants {
 	/** last build date */
 	public static final String BUILD_DATE = "28 February 2023";
 	/** complete version string */
-	public static final String VERSION_STRING = "5.0.762.0";
+	public static final String VERSION_STRING = "5.2.762.0";
 
 	/** proper noun, should NOT be translated / transliterated */
 	public static final String APPLICATION_NAME = "GeoGebra";
