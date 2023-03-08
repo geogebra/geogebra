061d8386522eb3398eb550c2dcbd2309ed1d243b
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2023-03-08 12:55:07 +0100

    APPS-4756 dont add mms exam

--- web/src/main/java/org/geogebra/web/full/gui/exam/ExamStartDialog.java
+++ web/src/main/java/org/geogebra/web/full/gui/exam/ExamStartDialog.java
@@ -36,9 +36,11 @@ public class ExamStartDialog extends ComponentDialog {
 		if (app.isSuite()) {
 			ArrayList<RadioButtonData<ExamRegion>> data = new ArrayList<>();
 			for (ExamRegion region : ExamRegion.values()) {
-				String displayName = region.getDisplayName(app.getLocalization(),
-						app.getConfig());
-				data.add(new RadioButtonData<>(displayName, region));
+				if (region != ExamRegion.MMS) {
+					String displayName = region.getDisplayName(app.getLocalization(),
+							app.getConfig());
+					data.add(new RadioButtonData<>(displayName, region));
+				}
 			}
 			RadioButtonPanel<ExamRegion> regionPicker = new RadioButtonPanel<>(
 					app.getLocalization(), data, ExamRegion.GENERIC, (selectedRegion) ->
