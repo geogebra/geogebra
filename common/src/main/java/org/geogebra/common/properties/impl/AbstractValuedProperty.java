0f4397aa816bb47cae9dfe5146bc29c41f0fddd6
Balazs Bencze <balazs.bencze@geogebra.org>
2023-03-09 15:13:12 +0200

    APPS-4696 Format code

--- common/src/main/java/org/geogebra/common/properties/impl/AbstractValuedProperty.java
+++ common/src/main/java/org/geogebra/common/properties/impl/AbstractValuedProperty.java
@@ -12,68 +12,66 @@ import org.geogebra.common.properties.ValuedProperty;
  * Helper class for implementing value setting and getting of a property with listeners.
  */
 public abstract class AbstractValuedProperty<S> extends AbstractProperty
-        implements ValuedProperty<S> {
+		implements ValuedProperty<S> {
 
-    private final Set<PropertyObserver> observers = new HashSet<>();
+	private final Set<PropertyObserver> observers = new HashSet<>();
 
-    /**
-     * Constructs an abstract property.
-     *
-     * @param localization this is used to localize the name
-     * @param name         the name to be localized
-     */
-    public AbstractValuedProperty(Localization localization, String name) {
-        super(localization, name);
-    }
+	/**
+	 * Constructs an abstract property.
+	 * @param localization this is used to localize the name
+	 * @param name the name to be localized
+	 */
+	public AbstractValuedProperty(Localization localization, String name) {
+		super(localization, name);
+	}
 
-    @Override
-    public void addObserver(PropertyObserver observer) {
-        observers.add(observer);
-    }
+	@Override
+	public void addObserver(PropertyObserver observer) {
+		observers.add(observer);
+	}
 
-    @Override
-    public void removeObserver(PropertyObserver observer) {
-        observers.remove(observer);
-    }
+	@Override
+	public void removeObserver(PropertyObserver observer) {
+		observers.remove(observer);
+	}
 
-    @Override
-    public final void setValue(S value) {
-        doSetValue(value);
-        notifyObservers(observer -> observer.onChange(this));
-    }
+	@Override
+	public final void setValue(S value) {
+		doSetValue(value);
+		notifyObservers(observer -> observer.onChange(this));
+	}
 
-    @Override
-    public final void startChange() {
-        doStartChange();
-        notifyObservers(observer -> observer.onStartChange(this));
-    }
+	@Override
+	public final void startChange() {
+		doStartChange();
+		notifyObservers(observer -> observer.onStartChange(this));
+	}
 
-    @Override
-    public final void endChange() {
-        doEndChange();
-        notifyObservers(observer -> observer.onEndChange(this));
-    }
+	@Override
+	public final void endChange() {
+		doEndChange();
+		notifyObservers(observer -> observer.onEndChange(this));
+	}
 
-    /**
-     * Implement this to set the value of the property.
-     *
-     * @param value property value
-     */
-    protected abstract void doSetValue(S value);
+	/**
+	 * Implement this to set the value of the property.
+	 * @param value property value
+	 */
+	protected abstract void doSetValue(S value);
 
-    /**
-     * Callback to when property starts changing.
-     */
-    protected void doStartChange() {
-    }
+	/**
+	 * Callback to when property starts changing.
+	 */
+	protected void doStartChange() {
+	}
 
-    /**
-     * Callback to when property ends changing.
-     */
-    protected void doEndChange() {
-    }
+	/**
+	 * Callback to when property ends changing.
+	 */
+	protected void doEndChange() {
+	}
 
-    private void notifyObservers(Consumer<PropertyObserver> eventCall) {
-        observers.forEach(eventCall);
-    }
+	private void notifyObservers(Consumer<PropertyObserver> eventCall) {
+		observers.forEach(eventCall);
+	}
 }
--- common/src/main/java/org/geogebra/common/properties/impl/objects/collection/AbstractTypedPropertyCollection.java
+++ common/src/main/java/org/geogebra/common/properties/impl/objects/collection/AbstractTypedPropertyCollection.java
@@ -10,78 +10,78 @@ import org.geogebra.common.properties.PropertyObserver;
 import org.geogebra.common.properties.ValuedProperty;
 
 abstract class AbstractTypedPropertyCollection<T extends ValuedProperty<S>, S> implements
-        ValuedProperty<S> {
+		ValuedProperty<S> {
 
-    private final T[] properties;
-    private final Set<PropertyObserver> observers = new HashSet<>();
+	private final T[] properties;
+	private final Set<PropertyObserver> observers = new HashSet<>();
 
-    AbstractTypedPropertyCollection(T[] properties) {
-        if (properties.length == 0) {
-            throw new IllegalArgumentException("Properties must have at least a single property");
-        }
-        this.properties = properties;
-    }
+	AbstractTypedPropertyCollection(T[] properties) {
+		if (properties.length == 0) {
+			throw new IllegalArgumentException("Properties must have at least a single property");
+		}
+		this.properties = properties;
+	}
 
-    @Override
-    public String getName() {
-        return getFirstProperty().getName();
-    }
+	@Override
+	public String getName() {
+		return getFirstProperty().getName();
+	}
 
-    protected T getFirstProperty() {
-        return properties[0];
-    }
+	protected T getFirstProperty() {
+		return properties[0];
+	}
 
-    protected T[] getProperties() {
-        return properties;
-    }
+	protected T[] getProperties() {
+		return properties;
+	}
 
-    @Override
-    public boolean isEnabled() {
-        boolean isEnabled = true;
-        for (Property property : properties) {
-            isEnabled = isEnabled && property.isEnabled();
-        }
-        return isEnabled;
-    }
+	@Override
+	public boolean isEnabled() {
+		boolean isEnabled = true;
+		for (Property property : properties) {
+			isEnabled = isEnabled && property.isEnabled();
+		}
+		return isEnabled;
+	}
 
-    @Override
-    public void addObserver(PropertyObserver observer) {
-        observers.add(observer);
-    }
+	@Override
+	public void addObserver(PropertyObserver observer) {
+		observers.add(observer);
+	}
 
-    @Override
-    public void removeObserver(PropertyObserver observer) {
-        observers.remove(observer);
-    }
+	@Override
+	public void removeObserver(PropertyObserver observer) {
+		observers.remove(observer);
+	}
 
-    private void notifyObservers(Consumer<PropertyObserver> observerConsumer) {
-        observers.forEach(observerConsumer);
-    }
+	private void notifyObservers(Consumer<PropertyObserver> observerConsumer) {
+		observers.forEach(observerConsumer);
+	}
 
-    private void callProperty(Consumer<T> propertyConsumer) {
-        Arrays.asList(properties).forEach(propertyConsumer);
-    }
+	private void callProperty(Consumer<T> propertyConsumer) {
+		Arrays.asList(properties).forEach(propertyConsumer);
+	}
 
-    @Override
-    public S getValue() {
-        return getFirstProperty().getValue();
-    }
+	@Override
+	public S getValue() {
+		return getFirstProperty().getValue();
+	}
 
-    @Override
-    public void setValue(S value) {
-        callProperty(property -> property.setValue(value));
-        notifyObservers(observer -> observer.onChange(this));
-    }
+	@Override
+	public void setValue(S value) {
+		callProperty(property -> property.setValue(value));
+		notifyObservers(observer -> observer.onChange(this));
+	}
 
-    @Override
-    public void startChange() {
-        callProperty(ValuedProperty::startChange);
-        notifyObservers(observer -> observer.onStartChange(this));
-    }
+	@Override
+	public void startChange() {
+		callProperty(ValuedProperty::startChange);
+		notifyObservers(observer -> observer.onStartChange(this));
+	}
 
-    @Override
-    public void endChange() {
-        callProperty(ValuedProperty::endChange);
-        notifyObservers(observer -> observer.onEndChange(this));
-    }
+	@Override
+	public void endChange() {
+		callProperty(ValuedProperty::endChange);
+		notifyObservers(observer -> observer.onEndChange(this));
+	}
 }
