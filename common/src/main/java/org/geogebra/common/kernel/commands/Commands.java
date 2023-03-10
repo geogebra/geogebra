e68627bcdd0bd4f189e7029abc5cc6a7fbe45f9f
Bence Schneider <bence.schneider@geogebra.at>
2023-03-10 13:57:14 +0100

    APPS-4615 CAS commands added

--- common/src/main/java/org/geogebra/common/kernel/commands/Commands.java
+++ common/src/main/java/org/geogebra/common/kernel/commands/Commands.java
@@ -1218,7 +1218,19 @@ public enum Commands implements CommandsConstants,
 
 	IsVertexForm(TABLE_FUNCTION),
 
-	TableToChart(TABLE_ENGLISH);
+	TableToChart(TABLE_ENGLISH),
+
+	ExtendedGCD(TABLE_CAS),
+
+	ModularExponent(TABLE_CAS),
+
+	CharacteristicPolynomial(TABLE_CAS),
+
+	MinimalPolynomial(TABLE_CAS),
+
+	LUDecomposition(TABLE_CAS),
+
+	QRDecomposition(TABLE_CAS);
 
 	private static final Commands[] RENAMED = {
 			Commands.Binomial, Commands.BinomialCoefficient, Commands.Mean,
