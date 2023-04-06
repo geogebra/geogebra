a023eb9cf3efd400d955df5e97d9b60e68c5c35a
Balazs Bencze <balazs.bencze@geogebra.org>
2023-04-06 16:13:17 +0300

    APPS-4696 Format file

--- common/src/main/java/org/geogebra/common/properties/ValuedProperty.java
+++ common/src/main/java/org/geogebra/common/properties/ValuedProperty.java
@@ -2,7 +2,6 @@ package org.geogebra.common.properties;
 
 /**
  * A property that holds a value and can be observed for changes.
- *
  * @param <V> the type of the value
  */
 public interface ValuedProperty<V> extends Property {
@@ -14,15 +13,15 @@ public interface ValuedProperty<V> extends Property {
 	 */
 	V getValue();
 
-    /**
-     * Sets the property value.
-     *
-     * @param value value
-     */
-    void setValue(V value);
+	/**
+	 * Sets the property value.
+	 *
+	 * @param value value
+	 */
+	void setValue(V value);
 
-    /**
-     * Marks this property as changing value. Call this method for properties whose values
+	/**
+	 * Marks this property as changing value. Call this method for properties whose values
 	 * might change quickly for some period. After the quick successive changes end,
 	 * call {@link ValuedProperty#endChangingValue()}.
 	 * <p>
@@ -30,29 +29,29 @@ public interface ValuedProperty<V> extends Property {
 	 * adjusting the slider produces a large amount of {@link ValuedProperty#setValue(Object)} calls.
 	 * In order to optimize this, some properties (or value observers) might decide to delay
 	 * the handling of setValue after {@link ValuedProperty#endChangingValue()} has been called.
-     */
-    void startChangingValue();
+	 */
+	void startChangingValue();
 
-    /**
-     * Marks this property as its value changing has ended. For every call of this method,
+	/**
+	 * Marks this property as its value changing has ended. For every call of this method,
 	 * there must be a preceeding {@link ValuedProperty#startChangingValue()} call, otherwise
 	 * the functionality is undefined.
-     */
-    void endChangingValue();
+	 */
+	void endChangingValue();
 
-    /**
-     * Adds a property value observer. Adding an obsever that is
+	/**
+	 * Adds a property value observer. Adding an obsever that is
 	 * already registered with this property has no effect.
-     *
-     * @param observer value observer
-     */
-    void addValueObserver(PropertyValueObserver observer);
+	 *
+	 * @param observer value observer
+	 */
+	void addValueObserver(PropertyValueObserver observer);
 
-    /**
-     * Removes a property value observer. Removing an observer that is
+	/**
+	 * Removes a property value observer. Removing an observer that is
 	 * not registered with this property has no effect.
-     *
-     * @param observer value observer
-     */
-    void removeValueObserver(PropertyValueObserver observer);
+	 * 
+	 * @param observer value observer
+	 */
+	void removeValueObserver(PropertyValueObserver observer);
 }
