9f7c0d07ee86c1384c70378e2cc09712a6e7f7d3
Zbynek Konecny <zbynek@geogebra.at>
2023-05-05 16:25:24 +0200

    APPS-4822 Fix handling of registered function vars

--- common/src/main/java/org/geogebra/common/kernel/Construction.java
+++ common/src/main/java/org/geogebra/common/kernel/Construction.java
@@ -3236,7 +3236,10 @@ public class Construction {
 		} else {
 			registeredFV.add(fv);
 		}
+	}
 
+	public boolean hasRegisteredFunctionVariable() {
+		return !registeredFV.isEmpty();
 	}
 
 	/**
@@ -3247,20 +3250,6 @@ public class Construction {
 	 */
 	public boolean isRegisteredFunctionVariable(String s) {
 		return registeredFV.contains(s);
-	}
-
-	/**
-	 * Returns function variable that should be recognized in If and Function
-	 * commands
-	 * 
-	 * @return local function variable or null if there is none
-	 */
-	public String getRegisteredFunctionVariable() {
-		Iterator<String> it = registeredFV.iterator();
-		if (it.hasNext()) {
-			return it.next();
-		}
-		return null;
 	}
 
 	/**
