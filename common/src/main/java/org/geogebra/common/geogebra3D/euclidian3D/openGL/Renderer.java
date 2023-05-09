6a46daf18e5e454517c9e73b6a7679838f922dda
Jan <jan.borovsky@geogebra.at>
2023-05-09 14:51:00 +0200

    HOL-106 Make some Renderer method protected instead private. Embed clip plane logic from drawScene() to protected method.

--- common/src/main/java/org/geogebra/common/geogebra3D/euclidian3D/openGL/Renderer.java
+++ common/src/main/java/org/geogebra/common/geogebra3D/euclidian3D/openGL/Renderer.java
@@ -342,16 +342,7 @@ public abstract class Renderer {
 
 		rendererImpl.useShaderProgram();
 
-		// clip planes
-		if (waitForUpdateClipPlanes) {
-			// Application.debug(enableClipPlanes);
-			if (enableClipPlanes) {
-				rendererImpl.enableClipPlanes();
-			} else {
-				rendererImpl.disableClipPlanes();
-			}
-			waitForUpdateClipPlanes = false;
-		}
+		maybeEnableOrDisableClipPlanes();
 
 		// update 3D controller
 		((EuclidianController3D) view3D.getEuclidianController()).update();
@@ -416,6 +407,18 @@ public abstract class Renderer {
 		rendererImpl.setColorMask(ColorMask.ALL);
 
         endOfDrawScene();
+	}
+
+	protected void maybeEnableOrDisableClipPlanes() {
+		if (waitForUpdateClipPlanes) {
+			// Application.debug(enableClipPlanes);
+			if (enableClipPlanes) {
+				rendererImpl.enableClipPlanes();
+			} else {
+				rendererImpl.disableClipPlanes();
+			}
+			waitForUpdateClipPlanes = false;
+		}
 	}
 
 	/**
@@ -791,7 +794,7 @@ public abstract class Renderer {
 		rendererImpl.enableLighting();
 	}
 
-	private void draw() {
+	protected void draw() {
 		rendererImpl.draw();
 		drawLabels();
 		setMatrixAndLight();
