e48c4f4c81fa2acfbd847ebf732d3db7cb7e8b87
Zbyněk Konečný <zbynek@geogebra.org>
2023-05-11 14:50:30 +0000

    Add Lighthouse to UI tests

--- uitest/runtest.sh
+++ uitest/runtest.sh
@@ -1,4 +1,5 @@
-npm install || exit 1
+npm install --force || exit 1
 npx http-server -p 8888 content & npx wait-on http://localhost:8888
 npx cypress run --env userAgent="Chrome iPad" --spec="cypress/integration/**/*-iPad.spec.js" || true
 npx cypress run || true
+npx lighthouse http://localhost:8888/editor.html --output json --output-path=./lighthouse.report.json --chrome-flags="--headless --no-sandbox"
