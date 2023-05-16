e620db155ba77d161eea64385d39560bf27acc4b
Balazs Bencze <balazs.bencze@geogebra.org>
2023-05-16 11:59:54 +0300

    APPS-4696 Note sort requirement

--- common/src/main/java/org/geogebra/common/properties/GroupedEnumeratedProperty.java
+++ common/src/main/java/org/geogebra/common/properties/GroupedEnumeratedProperty.java
@@ -8,10 +8,11 @@ public interface GroupedEnumeratedProperty<V> extends EnumeratedProperty<V> {
 	/**
 	 * Gets the array of indices where a divider must be inserted.
 	 * Please note that indices refer to the unmodified values array.
+	 * The array is sorted in ascending order.
 	 * <p>
 	 * For example, with values {"a", "b", "c", "d", "e"}, if this method returns
 	 * {1, 3}, it must be understood as the following {"a", |, "b", "c", |, "d", "e"}
-	 * @return an array of indices where a divider must be inserted
+	 * @return a sorted array of indices where a divider must be inserted
 	 */
 	int[] getGroupDividerIndices();
 }
