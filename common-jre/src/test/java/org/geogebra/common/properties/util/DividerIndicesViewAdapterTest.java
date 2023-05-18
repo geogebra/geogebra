b6a9c22097b4f57df9fd773e923be2b8446c44a7
Balazs Bencze <balazs.bencze@geogebra.org>
2023-05-18 11:58:53 +0300

    APPS-4696 Format file

--- common-jre/src/test/java/org/geogebra/common/properties/util/DividerIndicesViewAdapterTest.java
+++ common-jre/src/test/java/org/geogebra/common/properties/util/DividerIndicesViewAdapterTest.java
@@ -12,7 +12,7 @@ public class DividerIndicesViewAdapterTest {
 
 	@Before
 	public void setUp() {
-		adapter = new DividerIndicesViewAdapter(new int[] {1, 3}, 5);
+		adapter = new DividerIndicesViewAdapter(new int[]{1, 3}, 5);
 	}
 
 	@Test
@@ -29,19 +29,19 @@ public class DividerIndicesViewAdapterTest {
 
 	@Test
 	public void testConvertViewIndexToModel() {
-		assertThat(adapter.convertViewIndexToModel(0), is (0));
-		assertThat(adapter.convertViewIndexToModel(2), is (1));
-		assertThat(adapter.convertViewIndexToModel(3), is (2));
-		assertThat(adapter.convertViewIndexToModel(5), is (3));
-		assertThat(adapter.convertViewIndexToModel(6), is (4));
+		assertThat(adapter.convertViewIndexToModel(0), is(0));
+		assertThat(adapter.convertViewIndexToModel(2), is(1));
+		assertThat(adapter.convertViewIndexToModel(3), is(2));
+		assertThat(adapter.convertViewIndexToModel(5), is(3));
+		assertThat(adapter.convertViewIndexToModel(6), is(4));
 	}
 
 	@Test
 	public void testConvertModelIndexToView() {
-		assertThat(adapter.convertModelIndexToView(0), is (0));
-		assertThat(adapter.convertModelIndexToView(1), is (2));
-		assertThat(adapter.convertModelIndexToView(2), is (3));
-		assertThat(adapter.convertModelIndexToView(3), is (5));
-		assertThat(adapter.convertModelIndexToView(4), is (6));
+		assertThat(adapter.convertModelIndexToView(0), is(0));
+		assertThat(adapter.convertModelIndexToView(1), is(2));
+		assertThat(adapter.convertModelIndexToView(2), is(3));
+		assertThat(adapter.convertModelIndexToView(3), is(5));
+		assertThat(adapter.convertModelIndexToView(4), is(6));
 	}
 }
