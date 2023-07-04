1f7c804ca064c6bc4e24b7f8cdc75c12124f83a7
mena.aly <mena.aly@geogebra.at>
2023-07-04 17:34:52 +0200

    mow-1365: chore-UnnecessaryImport

--- common/src/main/java/org/geogebra/common/euclidian/LayerManager.java
+++ common/src/main/java/org/geogebra/common/euclidian/LayerManager.java
@@ -2,7 +2,6 @@ package org.geogebra.common.euclidian;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import org.geogebra.common.kernel.Kernel;
