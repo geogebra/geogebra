77a0e97d277d2ac3e046c894daad639049e30372
Laszlo Gal <latz.gal@gmail.com>
2023-08-25 09:47:46 +0200

    APPS-4955 Lazy initialization of prototype.

--- desktop/src/main/java/org/geogebra/desktop/euclidian/event/MouseEventUtil.java
+++ desktop/src/main/java/org/geogebra/desktop/euclidian/event/MouseEventUtil.java
@@ -5,11 +5,17 @@ import java.awt.event.MouseEvent;
 import org.geogebra.desktop.main.AppD;
 
 public class MouseEventUtil {
-	private static final MouseEventPrototype prototype = AppD.MAC_OS
-			? new MacOSMouseEventPrototype()
-			: new DefaultMouseEventPrototype();
-
+	private static MouseEventPrototype prototype = null;
 	public static boolean isRightClick(MouseEvent event) {
-		return prototype.isRightClick(event);
+		return prototype().isRightClick(event);
+	}
+
+	private static MouseEventPrototype prototype() {
+		if (prototype == null) {
+			prototype = AppD.MAC_OS
+					? new MacOSMouseEventPrototype()
+					: new DefaultMouseEventPrototype();
+		}
+		return prototype;
 	}
 }
