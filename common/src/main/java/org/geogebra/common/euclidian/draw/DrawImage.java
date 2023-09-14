913484bfcd319e214bb542677908bf830bd4e2e1
Zbynek Konecny <zbynek@geogebra.at>
2023-09-14 13:56:57 +0200

    APPS-5107 Only apply optimization in new files

--- common/src/main/java/org/geogebra/common/euclidian/draw/DrawImage.java
+++ common/src/main/java/org/geogebra/common/euclidian/draw/DrawImage.java
@@ -145,7 +145,7 @@ public class DrawImage extends Drawable {
 							// make sure old files work
 							// https://dev.geogebra.org/trac/changeset/57611
 							geo.getKernel().getApplication().fileVersionBefore(
-									new int[] { 5, 0, 397, 0 })
+									5, 0, 397, 0)
 											? -view.getInvXscale()
 											: -view.getInvYscale());
 				}
