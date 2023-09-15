52fc90620f9b1abc846356dde52db4dadfe9a38b
schbence <schbence@yahoo.com>
2023-09-15 12:06:26 +0200

    APPS-5098 test fix after saving enabled

--- common-jre/src/test/java/org/geogebra/common/gui/menu/impl/ExamDrawerMenuFactoryTest.java
+++ common-jre/src/test/java/org/geogebra/common/gui/menu/impl/ExamDrawerMenuFactoryTest.java
@@ -35,6 +35,6 @@ public class ExamDrawerMenuFactoryTest {
 		DrawerMenu menu = factory.createDrawerMenu();
 		Assert.assertEquals(1, menu.getMenuItemGroups().size());
 		MenuItemGroup group = menu.getMenuItemGroups().get(0);
-		Assert.assertEquals(5, group.getMenuItems().size());
+		Assert.assertEquals(6, group.getMenuItems().size());
 	}
 }
