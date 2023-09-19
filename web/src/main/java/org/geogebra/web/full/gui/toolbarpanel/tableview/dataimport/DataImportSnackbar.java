a3a54027be4daeb763e98249d8b1a432ca717f0d
csillasolyom-gecse <csilla.solyom-gecse@geogebra.at>
2023-09-19 12:07:36 +0200

    APPS-5010 more style

--- web/src/main/java/org/geogebra/web/full/gui/toolbarpanel/tableview/dataimport/DataImportSnackbar.java
+++ web/src/main/java/org/geogebra/web/full/gui/toolbarpanel/tableview/dataimport/DataImportSnackbar.java
@@ -1,5 +1,9 @@
 package org.geogebra.web.full.gui.toolbarpanel.tableview.dataimport;
 
+import static org.geogebra.common.main.GeoGebraColorConstants.NEUTRAL_300;
+import static org.geogebra.common.main.GeoGebraColorConstants.NEUTRAL_700;
+
+import org.geogebra.common.awt.GColor;
 import org.geogebra.common.gui.view.table.importer.DataImporterError;
 import org.geogebra.web.full.css.MaterialDesignResources;
 import org.geogebra.web.full.gui.components.ComponentProgressBar;
@@ -10,8 +14,10 @@ import org.gwtproject.user.client.ui.Image;
 import org.gwtproject.user.client.ui.Label;
 
 public class DataImportSnackbar extends FlowPanel {
+	protected AppW appW;
 
 	public DataImportSnackbar(AppW appW, String title) {
+		this.appW = appW;
 		addStyleName("dataImporter");
 		buildGui(title);
 		appW.getAppletFrame().add(this);
@@ -19,6 +25,7 @@ public class DataImportSnackbar extends FlowPanel {
 	}
 
 	public DataImportSnackbar(AppW appW, String title, DataImporterError error) {
+		this.appW = appW;
 		addStyleName("dataImporter");
 		addStyleName("error");
 		buildErrorGui(title, error);
@@ -27,21 +34,21 @@ public class DataImportSnackbar extends FlowPanel {
 	}
 
 	private void buildGui(String title) {
-		addTitleHolder(title);
+		addTitleHolder(title, NEUTRAL_300);
 
 		ComponentProgressBar progressBar = new ComponentProgressBar(true, false);
 		add(progressBar);
 	}
 
-	private void addTitleHolder(String title) {
+	private void addTitleHolder(String title, GColor svgFiller) {
 		FlowPanel titleHolder = new FlowPanel();
 		titleHolder.addStyleName("titleHolder");
 
 		Image dataImg = new Image(MaterialDesignResources.INSTANCE.upload_file().withFill(
-				"#E6E6EB").getSafeUri());
+				svgFiller.toString()).getSafeUri());
 		Label titleLbl = new Label(title);
 		StandardButton xButton = new StandardButton(MaterialDesignResources.INSTANCE.clear()
-				.withFill("#E6E6EB"), 24);
+				.withFill(NEUTRAL_300.toString()), 24);
 
 		titleHolder.add(dataImg);
 		titleHolder.add(titleLbl);
@@ -50,11 +57,11 @@ public class DataImportSnackbar extends FlowPanel {
 	}
 
 	private void buildErrorGui(String title, DataImporterError error) {
-		addTitleHolder(title);
+		addTitleHolder(title, NEUTRAL_700);
 
 		FlowPanel errorHolder = new FlowPanel();
 		errorHolder.addStyleName("errorHolder");
-		Label errorLbl = new Label("erroooooorrrr");
+		Label errorLbl = new Label(error.name());
 		errorLbl.addStyleName("errorMsg");
 		StandardButton tryAgain = new StandardButton("TRY AGAIN");
 		errorHolder.add(errorLbl);
