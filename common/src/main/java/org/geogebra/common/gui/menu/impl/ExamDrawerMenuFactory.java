181622d83c72be114a25454a7e0664a3a4f618ab
mena.aly <mena.aly@geogebra.at>
2023-09-20 09:51:35 +0200

    APPS-5078: chore - added missing import

--- common/src/main/java/org/geogebra/common/gui/menu/impl/ExamDrawerMenuFactory.java
+++ common/src/main/java/org/geogebra/common/gui/menu/impl/ExamDrawerMenuFactory.java
@@ -1,5 +1,7 @@
 package org.geogebra.common.gui.menu.impl;
 
+import static org.geogebra.common.gui.menu.impl.DefaultDrawerMenuFactory.share;
+
 import org.geogebra.common.GeoGebraConstants;
 import org.geogebra.common.gui.menu.Action;
 import org.geogebra.common.gui.menu.DrawerMenu;
