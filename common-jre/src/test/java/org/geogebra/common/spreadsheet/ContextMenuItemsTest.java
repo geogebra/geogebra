d48a647db72eab0e0c938d111c6a6a210ce70aab
laszlogal <latz.gal@gmail.com>
2023-10-09 10:07:11 +0200

    APPS-5093 ContextMenuItemsTest cell, column and row keys

--- common-jre/src/test/java/org/geogebra/common/spreadsheet/ContextMenuItemsTest.java
+++ common-jre/src/test/java/org/geogebra/common/spreadsheet/ContextMenuItemsTest.java
@@ -14,12 +14,27 @@ public class ContextMenuItemsTest {
 
 	@Test
 	public void testCellMenuKeys() {
-		items = new ContextMenuItems(null, null);
-		Map<String, Runnable> map = items.get(0, 1);
-		shouldHaveKeys(map, "Delete", "Insert", "Copy", "Cut", "Paste");
+		testMenuKeys(1, 1,
+				"Delete", "Insert", "Copy", "Cut", "Paste");
 	}
 
-	private void shouldHaveKeys(Map<String, Runnable> map, String... keys) {
+	private void testMenuKeys(int column, int row, String... keys) {
+		items = new ContextMenuItems(null, null);
+		Map<String, Runnable> map = items.get(column, row);
 		assertEquals(map.keySet(), Set.of(keys));
+	}
+
+	@Test
+	public void testRowMenuKeys() {
+		testMenuKeys(-1, 1,
+				"ContextMenu.insertRowAbove",
+				"ContextMenu.insertRowBelow", "ContextMenu.deleteRow");
+	}
+
+	@Test
+	public void testColumnMenuKeys() {
+		testMenuKeys(1, -1,
+				"ContextMenu.insertColumnLeft", "ContextMenu.insertColumnRight",
+					"ContextMenu.deleteColumn");
 	}
 }
