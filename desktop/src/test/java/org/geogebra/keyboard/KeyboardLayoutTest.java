2f26db1d8b06dab5c7a42514ca97b84083a80c53
Keertiraj DJ <keertiraj.dj@geogebra.at>
2023-10-11 00:11:38 +0530

    Adding missing import for DefaultKeyboardFactory

--- desktop/src/test/java/org/geogebra/keyboard/KeyboardLayoutTest.java
+++ desktop/src/test/java/org/geogebra/keyboard/KeyboardLayoutTest.java
@@ -15,6 +15,7 @@ import org.geogebra.common.jre.headless.LocalizationCommon;
 import org.geogebra.common.keyboard.KeyboardRowDefinitionProvider;
 import org.geogebra.common.util.lang.Language;
 import org.geogebra.keyboard.base.KeyboardFactory;
+import org.geogebra.keyboard.base.impl.DefaultKeyboardFactory;
 import org.geogebra.keyboard.base.model.KeyboardModel;
 import org.geogebra.keyboard.base.model.Row;
 import org.geogebra.keyboard.base.model.WeightedButton;
