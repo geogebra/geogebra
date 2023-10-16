67cb76a8a8d4d3412767363ef78254072b87ee96
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2023-10-16 16:30:02 +0200

    APPS-5095 Fix CheckStyle warning

--- common-jre/src/test/java/org/geogebra/common/spreadsheet/core/SpreadsheetControllerTest.java
+++ common-jre/src/test/java/org/geogebra/common/spreadsheet/core/SpreadsheetControllerTest.java
@@ -8,7 +8,8 @@ import org.junit.Test;
 
 public class SpreadsheetControllerTest {
 
-	private final SpreadsheetController controller = new SpreadsheetController(new TestTabularData());
+	private final SpreadsheetController controller =
+			new SpreadsheetController(new TestTabularData());
 
 	@Test
 	public void testMove() {
