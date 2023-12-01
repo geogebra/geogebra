7ba54f28a2ceb58a1107ff875f7bdb245f61b437
Zbynek Konecny <zbynek@geogebra.at>
2023-12-01 16:39:21 +0100

    APPS-5288 Add Matomo, remove remnants of old analytics

--- web-common/src/main/java/org/geogebra/web/html5/main/AppW.java
+++ web-common/src/main/java/org/geogebra/web/html5/main/AppW.java
@@ -81,7 +81,6 @@ import org.geogebra.common.util.GTimerListener;
 import org.geogebra.common.util.MD5EncrypterGWTImpl;
 import org.geogebra.common.util.NormalizerMinimal;
 import org.geogebra.common.util.StringUtil;
-import org.geogebra.common.util.debug.Analytics;
 import org.geogebra.common.util.debug.Log;
 import org.geogebra.common.util.lang.Language;
 import org.geogebra.common.util.profiler.FpsProfiler;
@@ -149,7 +148,6 @@ import org.geogebra.web.html5.util.GeoGebraElement;
 import org.geogebra.web.html5.util.GlobalHandlerRegistry;
 import org.geogebra.web.html5.util.ImageManagerW;
 import org.geogebra.web.html5.util.UUIDW;
-import org.geogebra.web.html5.util.debug.AnalyticsW;
 import org.geogebra.web.html5.util.debug.LoggerW;
 import org.geogebra.web.html5.util.keyboard.KeyboardManagerInterface;
 import org.gwtproject.core.client.Scheduler;
@@ -295,9 +293,6 @@ public abstract class AppW extends App implements SetLabels, HasLanguage {
 					Browser.addMutationObserver(getParent(
 							getAppletParameters().getParamScaleContainerClass()),
 							this::checkScaleContainer));
-		}
-		if (getAppletParameters().getDataParamApp()) {
-			initializeAnalytics();
 		}
 	}
 
@@ -3554,14 +3549,6 @@ public abstract class AppW extends App implements SetLabels, HasLanguage {
 
 	public GlobalHandlerRegistry getGlobalHandlers() {
 		return dropHandlers;
-	}
-
-	private void initializeAnalytics() {
-		try {
-			Analytics.setInstance(new AnalyticsW());
-		} catch (Throwable e) {
-			Log.debug("Could not initialize analytics object." + e);
-		}
 	}
 
 	/**
