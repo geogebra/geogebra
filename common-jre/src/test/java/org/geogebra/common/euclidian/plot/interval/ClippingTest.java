303d2f48c5377aa49203b26c4c0a5128be5038b1
laszlogal <latz.gal@gmail.com>
2023-12-09 10:20:34 +0100

    APPS-5271 intersect test

--- common-jre/src/test/java/org/geogebra/common/euclidian/plot/interval/ClippingTest.java
+++ common-jre/src/test/java/org/geogebra/common/euclidian/plot/interval/ClippingTest.java
@@ -1,43 +1,38 @@
 package org.geogebra.common.euclidian.plot.interval;
 
-import static org.junit.Assert.assertNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 import org.geogebra.common.BaseUnitTest;
-import org.geogebra.common.awt.GGraphicsCommon;
+import org.geogebra.common.awt.GRectangle;
 import org.geogebra.common.euclidian.Drawable;
 import org.geogebra.common.euclidian.EuclidianView;
-import org.geogebra.common.euclidian.GeneralPathClipped;
+import org.geogebra.common.factories.AwtFactory;
 import org.geogebra.common.kernel.geos.GeoFunction;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ClippingTest extends BaseUnitTest {
-	private GGraphicsCommon graphics;
 	private EuclidianView view;
+	private GRectangle clipRectangle;
 
 	@Before
 	public void setupApp() {
-		graphics = spy(new GGraphicsCommon());
 		view = getApp().getActiveEuclidianView();
+		clipRectangle = AwtFactory.getPrototype()
+				.newRectangle(0, 0, view.getViewWidth(), view.getViewHeight());
 	}
 
 	@Test
 	public void testNoArtifact() {
-		doAnswer(inv -> {
-			assertNull(((GeneralPathClipped) inv.getArgument(0)).firstPoint());
-			return null;
-		}).when(graphics).draw(any());
 		GeoFunction f = add("sec(cot(x))");
 		view.update(f);
 		view.setRealWorldCoordSystem(-10, 10, -0.5, 0.5);
 		Drawable drawable = (Drawable) view.getDrawableFor(f);
-		drawable.draw(graphics);
+		if (drawable == null) {
+			fail();
+		}
 
-		verify(graphics, times(1)).draw(any());
+		assertFalse(drawable.intersectsRectangle(clipRectangle));
 	}
 }
