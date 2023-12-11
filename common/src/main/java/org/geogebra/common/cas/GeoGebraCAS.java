fe4eb192f6d67e5aaceaa9ee7a2dd8275d819dcd
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2023-12-11 14:41:01 +0100

    APPS-5233 Do not force '?' output for invalid Asymptote command

--- common/src/main/java/org/geogebra/common/cas/GeoGebraCAS.java
+++ common/src/main/java/org/geogebra/common/cas/GeoGebraCAS.java
@@ -1,7 +1,6 @@
 package org.geogebra.common.cas;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
@@ -171,21 +170,7 @@ public class GeoGebraCAS implements GeoGebraCasInterface {
 		}
 
 		resetCounter();
-
-		if (resultContainsCommandName(result)) {
-			return "?";
-		}
-
 		return result;
-	}
-
-	/**
-	 * @param result CAS result
-	 * @return True if the CAS result contains a Command name, false else
-	 */
-	private boolean resultContainsCommandName(String result) {
-		return Arrays.stream(Commands.values())
-				.anyMatch(command -> result.contains(command.toString()));
 	}
 
 	@Override
