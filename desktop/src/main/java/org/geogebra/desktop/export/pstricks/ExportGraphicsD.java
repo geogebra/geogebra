670e653fdc11deb418643577469976a2e9cf9728
Zbynek Konecny <zbynek@geogebra.at>
2024-01-05 16:58:31 +0100

    Make PSTricks code less platform-dependent

--- desktop/src/main/java/org/geogebra/desktop/export/pstricks/ExportGraphicsD.java
+++ desktop/src/main/java/org/geogebra/desktop/export/pstricks/ExportGraphicsD.java
@@ -3,31 +3,33 @@ package org.geogebra.desktop.export.pstricks;
 import java.awt.image.BufferedImage;
 
 import org.geogebra.common.awt.GShape;
+import org.geogebra.common.export.pstricks.GeoGebraExport;
 import org.geogebra.common.kernel.arithmetic.FunctionalNVar;
 import org.geogebra.common.kernel.arithmetic.Inequality;
-import org.geogebra.common.kernel.geos.GeoElement;
 import org.geogebra.desktop.awt.GGraphics2DD;
 
 // To avoid duplicate inequalities drawing algorithms replacing Graphics.
 // In the three implementations (pstricks, pgf, asymptote) print the
 // appropriate commands
-abstract class ExportGraphicsD extends GGraphics2DD {
+final class ExportGraphicsD extends GGraphics2DD {
 
-	protected double[] ds;
-	protected Inequality ineq;
-	protected FunctionalNVar geo;
+	private final GeoGebraExport export;
+	private final Inequality ineq;
+	private final FunctionalNVar geo;
 
-	public ExportGraphicsD(FunctionalNVar geo, Inequality ineq) {
+	public ExportGraphicsD(FunctionalNVar geo, Inequality ineq, GeoGebraExport export) {
 
 		// dummy canvas
 		super(new BufferedImage(5, 5, BufferedImage.TYPE_INT_RGB)
 				.createGraphics());
 
 		this.geo = geo;
-		this.ds = geo.getKernel().getViewBoundsForGeo((GeoElement) geo);
 		this.ineq = ineq;
+		this.export = export;
 	}
 
 	@Override
-	public abstract void fill(GShape s);
+	public void fill(GShape s) {
+		export.fillIneq(s, ineq, geo);
+	}
 }
