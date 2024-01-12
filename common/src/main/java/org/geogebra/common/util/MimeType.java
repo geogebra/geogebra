294e3e633cb73f5a450bcf750393f6cb692191cc
Zbynek Konecny <zbynek@geogebra.at>
2024-01-12 17:38:49 +0100

    APPS-5265 Provide file type description for local save (needed on Linux)

--- common/src/main/java/org/geogebra/common/util/MimeType.java
+++ common/src/main/java/org/geogebra/common/util/MimeType.java
@@ -1,20 +1,25 @@
 package org.geogebra.common.util;
 
 import org.geogebra.common.main.App;
+import org.geogebra.common.main.Localization;
 
 public enum MimeType {
-	GEOGEBRA("application/vnd.geogebra.file", FileExtensions.GEOGEBRA.toString()),
-	GEOGEBRA_TOOL("application/vnd.geogebra.tool", FileExtensions.GEOGEBRA_TOOL.toString()),
-	GEOGEBRA_NOTES("application/vnd.geogebra.slides", "ggs"),
+	GEOGEBRA("application/vnd.geogebra.file", FileExtensions.GEOGEBRA.toString(),
+			"Download.GeoGebraFile"),
+	GEOGEBRA_TOOL("application/vnd.geogebra.tool", FileExtensions.GEOGEBRA_TOOL.toString(),
+			"GeoGebra Tool"), // description not needed,
+	GEOGEBRA_NOTES("application/vnd.geogebra.slides", "ggs", "Download.SlidesGgs"),
 	;
 
 	private final String type;
 	private final String extension;
 	private final String dotExtension;
+	private String descriptionKey;
 
-	MimeType(String type, String extension) {
+	MimeType(String type, String extension, String descriptionKey) {
 		this.type = type;
 		this.extension = extension;
+		this.descriptionKey = descriptionKey;
 		dotExtension = "." + extension;
 	}
 
@@ -54,5 +59,14 @@ public enum MimeType {
 			return MimeType.GEOGEBRA_TOOL;
 		}
 		return MimeType.GEOGEBRA;
+	}
+
+	/**
+	 * @param loc localization
+	 * @return human readable description
+	 */
+	public String getDescription(Localization loc) {
+		// "GeoGebra file (.ggb)" => "GeoGebra file"
+		return loc.getMenu(descriptionKey).split("\\(")[0].trim();
 	}
 }
