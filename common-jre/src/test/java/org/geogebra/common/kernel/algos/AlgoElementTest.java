ebaae9f5034f0adf7603ab70476c318384542c61
laszlogal <latz.gal@gmail.com>
2024-02-07 13:49:26 +0100

    APPS-5355 Tests for sequence value and the composite of its elements are equal

--- common-jre/src/test/java/org/geogebra/common/kernel/algos/AlgoElementTest.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/algos/AlgoElementTest.java
@@ -1,12 +1,16 @@
 package org.geogebra.common.kernel.algos;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 import org.geogebra.common.BaseUnitTest;
 import org.geogebra.common.kernel.StringTemplate;
 import org.geogebra.common.kernel.geos.GeoElement;
+import org.geogebra.common.kernel.geos.GeoList;
 import org.hamcrest.TypeSafeMatcher;
 import org.junit.Test;
+
+import com.himamis.retex.editor.share.util.Unicode;
 
 public class AlgoElementTest extends BaseUnitTest {
 
@@ -21,6 +25,27 @@ public class AlgoElementTest extends BaseUnitTest {
 		assertThat(add("Integral(f)"), hasLaTeXDefinition("\\int f\\,\\mathrm{d}n"));
 		assertThat(add("Sequence(Integral(x^k),k,1,2)"), hasLaTeXDefinition(
 				"Sequence\\left(\\int x^{k}\\,\\mathrm{d}x, k, 1, 2 \\right)"));
+	}
+
+	@Test
+	public void testSequenceInequality() {
+		add("a: y" + Unicode.LESS_EQUAL + "x^(2) " + Unicode.AND
+				+ " y" + Unicode.GREATER_EQUAL + " 0 " + Unicode.AND + " x" + Unicode.GREATER_EQUAL
+				+ "-0 " + Unicode.AND + " x" + Unicode.LESS_EQUAL + "1");
+		GeoList seq = add("l1=Sequence(a(x+i,y),i,1,2)");
+		GeoList list = add("{Element[l1,1], Element[l1,2]}");
+		assertEquals(list.toValueString(StringTemplate.defaultTemplate),
+				seq.toValueString(StringTemplate.defaultTemplate));
+	}
+
+	@Test
+	public void testSequenceInequality2() {
+		add("a: y" + Unicode.LESS_EQUAL + "x^(2) " + Unicode.AND
+				+ " y" + Unicode.GREATER_EQUAL + " 0 " + Unicode.AND + " x" + Unicode.GREATER_EQUAL
+				+ "-0 " + Unicode.AND + " x" + Unicode.LESS_EQUAL + "1");
+		GeoList list = add("{a(x+1,y), a(x+2,y)}");
+		assertEquals(list.toValueString(StringTemplate.defaultTemplate),
+				add("Sequence(a(x+i,y),i,1,2)").toValueString(StringTemplate.defaultTemplate));
 	}
 
 	private TypeSafeMatcher<GeoElement> hasLaTeXDefinition(String def) {
