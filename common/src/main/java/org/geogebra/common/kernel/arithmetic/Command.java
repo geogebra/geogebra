2f29b553815ebf167df3fe70d7c0276895653c76
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2024-02-08 12:39:00 +0100

    APPS-5357 Enable using internal command names for scripts

--- common/src/main/java/org/geogebra/common/kernel/arithmetic/Command.java
+++ common/src/main/java/org/geogebra/common/kernel/arithmetic/Command.java
@@ -115,8 +115,7 @@ public class Command extends ValidExpression
 		 * other languages to use English names for different commands
 		 */
 
-		if (translateName && !kernel.isUsingInternalCommandNames()
-				&& canCommandBeTranslatedSafely(name)) {
+		if (translateName && !kernel.isUsingInternalCommandNames()) {
 			// translate command name to internal name
 			this.name = app.getReverseCommand(name);
 			// in CAS functions get parsed as commands as well and we want to
@@ -127,21 +126,6 @@ public class Command extends ValidExpression
 		} else {
 			this.name = name;
 		}
-	}
-
-	/**
-	 * If a command and its internal translation are not identical, it can only be translated
-	 * safely if the <b>non-internal command is an invalid internal command</b>
-	 * <br/>
-	 * E.g.: Trying to translate the french command 'Intersection' to the internal counterpart
-	 * 'Intersect' should be prohibited, since both are valid, internal commands (avoid clashes)
-	 * @param commandName The command's name
-	 * @return True if this command can be translated safely, false else
-	 */
-	private boolean canCommandBeTranslatedSafely(String commandName) {
-		String internalCommandName = app.getReverseCommand(commandName);
-		return internalCommandName.equals(commandName)
-				|| Commands.stringToCommand(commandName) == null;
 	}
 
 	/**
