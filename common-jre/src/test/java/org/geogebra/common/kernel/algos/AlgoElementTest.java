4a77f030f86c1d596f98561b02630c28a196b14f
Zbynek Konecny <zbynek@geogebra.at>
2024-02-12 15:13:01 +0100

    APPS-5355 Use constants in tests

--- common-jre/src/test/java/org/geogebra/common/kernel/algos/AlgoElementTest.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/algos/AlgoElementTest.java
@@ -1,7 +1,7 @@
 package org.geogebra.common.kernel.algos;
 
+import static org.geogebra.test.TestStringUtil.unicode;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
 
 import org.geogebra.common.BaseUnitTest;
 import org.geogebra.common.kernel.StringTemplate;
@@ -9,11 +9,8 @@ import org.geogebra.common.kernel.arithmetic.IneqTree;
 import org.geogebra.common.kernel.geos.GeoElement;
 import org.geogebra.common.kernel.geos.GeoFunctionNVar;
 import org.geogebra.common.kernel.geos.GeoList;
-import org.geogebra.common.kernel.kernelND.GeoElementND;
 import org.hamcrest.TypeSafeMatcher;
 import org.junit.Test;
-
-import com.himamis.retex.editor.share.util.Unicode;
 
 public class AlgoElementTest extends BaseUnitTest {
 
@@ -32,39 +29,25 @@ public class AlgoElementTest extends BaseUnitTest {
 
 	@Test
 	public void testSequenceInequality() {
-		add("a: y" + Unicode.LESS_EQUAL + "x^(2) " + Unicode.AND
-				+ " y" + Unicode.GREATER_EQUAL + " 0 " + Unicode.AND + " x" + Unicode.GREATER_EQUAL
-				+ "-0 " + Unicode.AND + " x" + Unicode.LESS_EQUAL + "1");
-		GeoList seq = add("l1=Sequence(a(x+i,y),i,1,2)");
-		GeoList list = add("{Element[l1,1], Element[l1,2]}");
-		assertEquals(list.toValueString(StringTemplate.defaultTemplate),
-				seq.toValueString(StringTemplate.defaultTemplate));
-	}
-
-	@Test
-	public void testSequenceInequality2() {
-		add("a: y" + Unicode.LESS_EQUAL + "x^(2) " + Unicode.AND
-				+ " y" + Unicode.GREATER_EQUAL + " 0 " + Unicode.AND + " x" + Unicode.GREATER_EQUAL
-				+ "-0 " + Unicode.AND + " x" + Unicode.LESS_EQUAL + "1");
+		add("a: y<=x^(2) && y>= 0 && x>=0 && x<=1");
 		GeoList list = add("{a(x+1,y), a(x+2,y)}");
-		assertEquals(list.toValueString(StringTemplate.defaultTemplate),
-				add("Sequence(a(x+i,y),i,1,2)").toValueString(StringTemplate.defaultTemplate));
+		GeoList seq = add("l1=Sequence(a(x+i,y),i,1,2)");
+		GeoList elements = add("{Element[l1,1], Element[l1,2]}");
+		String listValue = list.toValueString(StringTemplate.defaultTemplate);
+		assertThat(seq, hasValue(listValue));
+		assertThat(elements, hasValue(listValue));
 	}
 
 	@Test
 	public void testSequenceIneqTree() {
-		GeoElementND f = add("a: y" + Unicode.LESS_EQUAL + "x^(2) " + Unicode.AND
-				+ " y" + Unicode.GREATER_EQUAL + " 0 " + Unicode.AND + " x" + Unicode.GREATER_EQUAL
-				+ "-0 " + Unicode.AND + " x" + Unicode.LESS_EQUAL + "1");
-		GeoList list = add("{a(x+1,y), a(x+2,y)}");
-		IneqTree ineqs1 = ((GeoFunctionNVar) list.get(0)).getIneqs();
+		add("a: y<=x^(2) && y>= 0 && x>=0 && x<=1");
 		GeoList sequence = add("Sequence(a(x+i,y),i,1,2)");
-		f.updateRepaint();
-		IneqTree ineqs2 = ((GeoFunctionNVar) sequence.get(0)).getIneqs();
-		assertEquals(ineqs1.getLeft().getRight().getIneq().getBorder()
-						.toValueString(StringTemplate.defaultTemplate),
-				ineqs2.getLeft().getRight().getIneq().getBorder()
-						.toValueString(StringTemplate.defaultTemplate));
+		IneqTree inequalities1 = ((GeoFunctionNVar) sequence.get(0)).getIneqs();
+		IneqTree inequalities2 = ((GeoFunctionNVar) sequence.get(1)).getIneqs();
+		assertThat(inequalities1.getLeft().getLeft().getLeft().getIneq().getBorder(),
+				hasValue(unicode("((x + 1)^2) / 1")));
+		assertThat(inequalities2.getLeft().getLeft().getLeft().getIneq().getBorder(),
+				hasValue(unicode("((x + 2)^2) / 1")));
 	}
 
 	private TypeSafeMatcher<GeoElement> hasLaTeXDefinition(String def) {
