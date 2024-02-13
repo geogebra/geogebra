69e2086be2f4ef398810adb548018e84650c2747
Zbynek Konecny <zbynek@geogebra.at>
2024-02-13 13:58:13 +0100

    APPS-5402 c(x+3) should be multiplication if command c does not exist

--- common/src/main/java/org/geogebra/common/kernel/parser/FunctionParser.java
+++ common/src/main/java/org/geogebra/common/kernel/parser/FunctionParser.java
@@ -164,20 +164,17 @@ public class FunctionParser {
 			}
 			Localization loc = kernel.getLocalization();
 			if (!inputBoxParsing || "If".equals(loc.getReverseCommand(funcName))) {
-				if (topLevelExpression && !isCommand(funcName)
+				if (!isCommand(funcName)
 						&& !forceCommand && funcName.length() == 1
 						&& kernel.getAlgebraProcessor().enableStructures()) {
-					if (myList.size() == 2) {
-						ExpressionNode ret = new ExpressionNode(kernel, new MyVecNode(kernel,
-								myList.getListElement(0), myList.getListElement(1)));
-						ret.setLabel(funcName);
-						return ret;
-					} else if (myList.size() == 3) {
-						ExpressionNode ret = new ExpressionNode(kernel, new MyVec3DNode(kernel,
-								myList.getListElement(0), myList.getListElement(1),
-								myList.getListElement(2)));
-						ret.setLabel(funcName);
-						return ret;
+					if (topLevelExpression) {
+						ExpressionNode point = asPoint(myList, funcName);
+						if (point != null) {
+							return point;
+						}
+					} if (kernel.getSymbolicMode() == SymbolicMode.NONE && myList.size() == 1) {
+						return new ExpressionNode(kernel, new Variable(kernel, funcName),
+								Operation.MULTIPLY,	myList.getListElement(0));
 					}
 				}
 
@@ -266,6 +263,22 @@ public class FunctionParser {
 		// a(b) becomes a*b because a is not a function, no list, and no curve
 		// e.g. a(1+x) = a*(1+x) when a is a number
 		return multiplication(geoExp, undecided, myList, funcName);
+	}
+
+	private ExpressionNode asPoint(MyList myList, String funcName) {
+		if (myList.size() == 2) {
+			ExpressionNode ret = new ExpressionNode(kernel, new MyVecNode(kernel,
+					myList.getListElement(0), myList.getListElement(1)));
+			ret.setLabel(funcName);
+			return ret;
+		} else if (myList.size() == 3) {
+			ExpressionNode ret = new ExpressionNode(kernel, new MyVec3DNode(kernel,
+					myList.getListElement(0), myList.getListElement(1),
+					myList.getListElement(2)));
+			ret.setLabel(funcName);
+			return ret;
+		}
+		return null;
 	}
 
 	private static Operation getOperationFor(GeoSymbolic symbolic) {
