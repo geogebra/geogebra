1c00448c97c87ece6f94f90e8e67e8cf26fa8616
csillasolyom-gecse <csilla.solyom-gecse@geogebra.at>
2024-02-26 05:28:23 +0100

    MOW-1433 move code

--- web/src/main/java/org/geogebra/web/full/gui/toolbar/mow/toolbox/ToolboxMow.java
+++ web/src/main/java/org/geogebra/web/full/gui/toolbar/mow/toolbox/ToolboxMow.java
@@ -1,12 +1,6 @@
 package org.geogebra.web.full.gui.toolbar.mow.toolbox;
 
-import org.geogebra.common.euclidian.EuclidianView;
-import org.geogebra.common.main.App;
-import org.geogebra.common.plugin.Event;
-import org.geogebra.common.plugin.EventListener;
-import org.geogebra.common.plugin.EventType;
 import org.geogebra.web.full.css.ToolbarSvgResources;
-import org.geogebra.web.full.gui.layout.DockPanelW;
 import org.geogebra.web.html5.css.ZoomPanelResources;
 import org.geogebra.web.html5.main.AppW;
 import org.geogebra.web.resources.SVGResource;
@@ -17,6 +11,7 @@ import org.gwtproject.user.client.ui.SimplePanel;
 public class ToolboxMow extends FlowPanel {
 	private final AppW appW;
 	private ToolboxDecorator decorator;
+	private ToolboxController controller;
 	private IconButton spotlightBtn;
 
 	/**
@@ -26,6 +21,7 @@ public class ToolboxMow extends FlowPanel {
 	public ToolboxMow(AppW appW) {
 		this.appW = appW;
 		decorator = new ToolboxDecorator(this);
+		controller = new ToolboxController(appW, this);
 		RootPanel.get().add(this);
 		buildGui();
 	}
@@ -33,43 +29,14 @@ public class ToolboxMow extends FlowPanel {
 	private void buildGui() {
 		decorator.positionLeft();
 
-		spotlightBtn = addToggleButton(ZoomPanelResources.INSTANCE.target(), "Spotlight.Tool",
-				"Spotlight.Tool", "spotlightTool", () -> {
-					DockPanelW dp = (DockPanelW) appW.getGuiManager().getLayout().getDockManager()
-							.getPanel(App.VIEW_EUCLIDIAN);
-					dp.getComponent().addStyleName("graphicsWithSpotlight");
-					appW.getActiveEuclidianView().getEuclidianController().spotlightOn();
-					initSpotlightOff();
-					appW.hideMenu();
-				}, () -> {});
+		addSpotlightButton();
 
 		addDivider();
+
+		addRulerButton();
+
 		addPressButton(ToolbarSvgResources.INSTANCE.mode_pen(), "move mode", "moveBtn",
 				() -> appW.setMoveMode());
-	}
-
-	private void initSpotlightOff() {
-		appW.getEventDispatcher().addEventListener(new EventListener() {
-			@Override
-			public void sendEvent(Event evt) {
-				if (evt.getType() == EventType.REMOVE
-						&& evt.getTarget() != null && evt.getTarget().isSpotlight()
-				) {
-					EuclidianView view = appW.getActiveEuclidianView();
-					DockPanelW dp = (DockPanelW) appW.getGuiManager().getLayout().getDockManager()
-							.getPanel(App.VIEW_EUCLIDIAN);
-					dp.getComponent().removeStyleName("graphicsWithSpotlight");
-					view.clearSpotlight();
-					spotlightBtn.setActive(false);
-					appW.getEventDispatcher().removeEventListener(this);
-				}
-			}
-
-			@Override
-			public void reset() {
-				// not needed
-			}
-		});
 	}
 
 	private void addPressButton(SVGResource image, String ariaLabel, String dataTest,
@@ -81,7 +48,7 @@ public class ToolboxMow extends FlowPanel {
 
 	private IconButton addToggleButton(SVGResource image, String ariaLabel, String dataTitle,
 			String dataTest, Runnable onHandler, Runnable offHandler) {
-		IconButton iconButton = new IconButton(appW.getLocalization(), image, ariaLabel, dataTitle,
+		IconButton iconButton = new IconButton(appW, image, ariaLabel, dataTitle,
 				dataTest, onHandler, offHandler);
 		add(iconButton);
 		return iconButton;
@@ -106,5 +73,19 @@ public class ToolboxMow extends FlowPanel {
 		divider.setStyleName("divider");
 
 		add(divider);
+	}
+
+	public void switchSpotlightOff() {
+		spotlightBtn.setActive(false, appW.getDarkColor());
+	}
+
+	private void addSpotlightButton() {
+		spotlightBtn = addToggleButton(ZoomPanelResources.INSTANCE.target(), "Spotlight.Tool",
+				"Spotlight.Tool", "spotlightTool",
+				controller.getSpotlightOnHandler(), () -> {});
+	}
+
+	private void addRulerButton() {
+
 	}
 }
