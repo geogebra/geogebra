b7589be75e3bbc3971314e297771548befc8819f
Jenkins <jenkins@geogebra.org>
2024-02-27 06:33:35 +0000

    Version bump 5.2.828.0

--- common/src/main/java/org/geogebra/common/GeoGebraConstants.java
+++ common/src/main/java/org/geogebra/common/GeoGebraConstants.java
@@ -10,10 +10,10 @@ public interface GeoGebraConstants {
 	// as it is read by the build system
 	// and updated automatically by the "Version Bump" task
 	/** last build date */
-	public static final String BUILD_DATE = "20 February 2024";
+	public static final String BUILD_DATE = "27 February 2024";
 	/** complete version string */
 
-	public static final String VERSION_STRING = "5.2.827.0";
+	public static final String VERSION_STRING = "5.2.828.0";
 	/** proper noun, should NOT be translated / transliterated */
 	public static final String APPLICATION_NAME = "GeoGebra";
 
