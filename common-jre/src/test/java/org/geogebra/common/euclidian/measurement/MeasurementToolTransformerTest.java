8f03cd32328b1851c757bb08570513506f9f03e6
laszlogal <latz.gal@gmail.com>
2024-03-13 12:32:00 +0100

    MOW-1434 transformer test

---
+++ common-jre/src/test/java/org/geogebra/common/euclidian/measurement/MeasurementToolTransformerTest.java
@@ -0,0 +1,35 @@
+package org.geogebra.common.euclidian.measurement;
+
+import static org.geogebra.common.euclidian.measurement.MeasurementToolId.RULER;
+
+import java.util.List;
+
+import org.geogebra.common.BaseUnitTest;
+import org.geogebra.common.awt.GPoint;
+import org.geogebra.common.euclidian.EuclidianView;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MeasurementToolTransformerTest extends BaseUnitTest {
+
+	private EuclidianView view;
+	private MeasurementController mc;
+
+	@Before
+	public void setUp() {
+		view = getApp().getActiveEuclidianView();
+		mc = getApp().getKernel().getConstruction().getMeasurementController();
+	}
+
+	@Test
+	public void testRuler() {
+		mc.selectTool(RULER);
+		MeasurementToolTransformer transformer = createTransformer(RULER);
+		transformer.reset(view, List.of(new GPoint(0,0)));
+
+	}
+
+	private static MeasurementToolTransformer createTransformer(MeasurementToolId id) {
+		return new MeasurementToolTransformer(id.getEdges());
+	}
+}
