eff3eb7ff035fc1be34f00df2eedddefc662d4ff
laszlogal <latz.gal@gmail.com>
2024-03-14 16:35:05 +0100

    MOW-1434 transformer test failing

--- common-jre/src/test/java/org/geogebra/common/euclidian/measurement/MeasurementToolTransformerTest.java
+++ common-jre/src/test/java/org/geogebra/common/euclidian/measurement/MeasurementToolTransformerTest.java
@@ -2,13 +2,16 @@ package org.geogebra.common.euclidian.measurement;
 
 import static org.geogebra.common.euclidian.EuclidianConstants.MODE_RULER;
 import static org.geogebra.common.euclidian.EuclidianConstants.MODE_TRIANGLE_PROTRACTOR;
-import static org.geogebra.common.euclidian.measurement.MeasurementToolId.RULER;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.geogebra.common.AppCommonFactory;
 import org.geogebra.common.BaseUnitTest;
 import org.geogebra.common.awt.GPoint;
 import org.geogebra.common.euclidian.EuclidianView;
+import org.geogebra.common.jre.headless.AppCommon;
 import org.geogebra.common.kernel.geos.GeoImage;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +20,11 @@ public class MeasurementToolTransformerTest extends BaseUnitTest {
 
 	private EuclidianView view;
 	private MeasurementController mc;
+
+	@Override
+	public AppCommon createAppCommon() {
+		return AppCommonFactory.create3D();
+	}
 
 	@Before
 	public void setUp() {
@@ -27,7 +35,7 @@ public class MeasurementToolTransformerTest extends BaseUnitTest {
 	private GeoImage createToolImage(int mode, String fileName) {
 		GeoImage image = new GeoImage(getKernel().getConstruction());
 		if (mode == MODE_RULER) {
-			image.setSize(400, 40);
+			image.setImageFileName("Ruler", 400, 40);
 		} else if (mode == MODE_TRIANGLE_PROTRACTOR) {
 			image.setSize(200, 200);
 		}
@@ -36,13 +44,12 @@ public class MeasurementToolTransformerTest extends BaseUnitTest {
 
 	@Test
 	public void testRuler() {
-		mc.selectTool(RULER);
-		MeasurementToolTransformer transformer = createTransformer(RULER);
-		transformer.reset(view, List.of(new GPoint(0, 0)));
-
+		mc.toggleActiveTool(MODE_RULER);
+		GPoint point = new GPoint(200, 275);
+		List<GPoint> previewPoints = new ArrayList<>();
+		previewPoints.add(new GPoint(199, 275));
+		mc.applyTransformer(view, point, previewPoints);
+		assertEquals(1, previewPoints.size());
 	}
 
-	private MeasurementToolTransformer createTransformer(MeasurementToolId id) {
-		return new MeasurementToolTransformer(mc, id.getEdges());
-	}
 }
