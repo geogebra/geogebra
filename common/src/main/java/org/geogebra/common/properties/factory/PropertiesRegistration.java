d4f54c495d1b71ace910a54d962e8375f1799339
Karl Traunmüller <karl.traunmueller@geogebra.at>
2024-04-03 09:47:26 +0200

    APPS-4967 fixed potential NPE

--- common/src/main/java/org/geogebra/common/properties/factory/PropertiesRegistration.java
+++ common/src/main/java/org/geogebra/common/properties/factory/PropertiesRegistration.java
@@ -2,35 +2,41 @@ package org.geogebra.common.properties.factory;
 
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import org.geogebra.common.properties.PropertiesRegistry;
 import org.geogebra.common.properties.Property;
 import org.geogebra.common.properties.PropertyCollection;
 
 final class PropertiesRegistration {
 
-	static PropertiesArray registerProperties(PropertiesRegistry registry,
+	static PropertiesArray registerProperties(@Nullable PropertiesRegistry registry,
 			PropertiesArray properties) {
-		registerProperties(registry, properties.getProperties());
+		if (registry != null) {
+			registerProperties(registry, properties.getProperties());
+		}
 		return properties;
 	}
 
-	static Property registerProperty(PropertiesRegistry registry, Property property) {
+	static Property registerProperty(@Nullable PropertiesRegistry registry, Property property) {
 		if (property instanceof PropertyCollection<?>) {
 			PropertyCollection collection = (PropertyCollection) property;
 			registerProperties(registry, collection.getProperties());
 		}
-		registry.register(property);
+		if (registry != null) {
+			registry.register(property);
+		}
 		return property;
 	}
 
-	static Property[] registerProperties(PropertiesRegistry registry, Property... properties) {
+	static Property[] registerProperties(@Nullable PropertiesRegistry registry, Property... properties) {
 		for (Property property : properties) {
 			registerProperty(registry, property);
 		}
 		return properties;
 	}
 
-	static List<Property> registerProperties(PropertiesRegistry registry,
+	static List<Property> registerProperties(@Nullable PropertiesRegistry registry,
 			List<Property> properties) {
 		for (Property property : properties) {
 			registerProperty(registry, property);
