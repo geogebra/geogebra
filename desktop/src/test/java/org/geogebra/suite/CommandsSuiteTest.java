60de7896f1971f528f3f32b1737f07b2defddcd5
laszlogal <latz.gal@gmail.com>
2024-04-09 16:31:23 +0200

    APPS-5507 Test to reproduce the bug

--- desktop/src/test/java/org/geogebra/suite/CommandsSuiteTest.java
+++ desktop/src/test/java/org/geogebra/suite/CommandsSuiteTest.java
@@ -3,8 +3,10 @@ package org.geogebra.suite;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.geogebra.common.jre.headless.AppCommon;
 import org.geogebra.common.kernel.StringTemplate;
 import org.geogebra.common.kernel.geos.GeoElement;
+import org.geogebra.test.UndoRedoTester;
 import org.junit.Test;
 
 public class CommandsSuiteTest extends BaseSuiteTest {
@@ -12,12 +14,31 @@ public class CommandsSuiteTest extends BaseSuiteTest {
 	@Test
 	public void testSolveEnabled() {
 		GeoElement element = add("Solve(x)");
-		assertThat(element.toString(StringTemplate.defaultTemplate), is("l1 = {x = 0}"));
+		outputShouldBe(element, "l1 = {x = 0}");
+	}
+
+	private static void outputShouldBe(GeoElement element, String value) {
+		assertThat(element.toString(StringTemplate.defaultTemplate), is(value));
 	}
 
 	@Test
 	public void testIntegralEnabled() {
 		GeoElement element = add("Integral(x)");
-		assertThat(element.toString(StringTemplate.defaultTemplate), is("f(x) = 1 / 2 x²"));
+		outputShouldBe(element, "f(x) = 1 / 2 x²");
 	}
+
+	@Test
+	public void testNSolveShouldBeDefined() {
+		AppCommon app = getApp();
+		UndoRedoTester undoRedo = new UndoRedoTester(app);
+		undoRedo.setupUndoRedo();
+
+		add("V(h) = 4h^3 - 102h^2 +630h");
+		add("l1 = NSolve(V(h))");
+		app.storeUndoInfo();
+		undoRedo.undo();
+		GeoElement element = undoRedo.getAfterRedo("l1");
+		outputShouldBe(element, "l1 = {h = 0, h = 10.5, h = 15}");
+	}
+
 }
