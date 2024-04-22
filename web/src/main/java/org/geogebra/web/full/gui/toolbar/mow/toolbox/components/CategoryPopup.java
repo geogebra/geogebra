6530494b262189bcb3b8e6d7bfafa2b9b3703a19
csillasolyom-gecse <csilla.solyom-gecse@geogebra.at>
2024-04-22 10:01:53 +0200

    MOW-1466 fix disabling

--- web/src/main/java/org/geogebra/web/full/gui/toolbar/mow/toolbox/components/CategoryPopup.java
+++ web/src/main/java/org/geogebra/web/full/gui/toolbar/mow/toolbox/components/CategoryPopup.java
@@ -74,8 +74,8 @@ public class CategoryPopup extends GPopupPanel implements SetLabels {
 		IconButton button = new IconButton(mode, (AppW) app);
 		button.addFastClickHandler(source -> {
 			app.setMode(mode);
-			updateParentCallback.accept(mode);
 			updateButtonSelection(button);
+			updateParentCallback.accept(mode);
 			if (!preventHide) {
 				hide();
 			}
