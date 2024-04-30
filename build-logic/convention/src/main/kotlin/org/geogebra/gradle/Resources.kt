040c70bfeab42037c4b8f66fbe1eb999bf8b8971
Balázs Bencze <balazs.bencze@geogebra.org>
2024-04-30 12:47:43 +0300

    APPS-5589 Simplfy resource reading

--- build-logic/convention/src/main/kotlin/org/geogebra/gradle/Resources.kt
+++ build-logic/convention/src/main/kotlin/org/geogebra/gradle/Resources.kt
@@ -1,26 +1,13 @@
 package org.geogebra.gradle
 
-import java.io.BufferedReader
 import java.io.IOException
-import java.io.InputStreamReader
-import java.nio.charset.StandardCharsets
 
 class Resources {
     companion object {
         @Throws(IOException::class)
         fun getString(resourcePath: String): String {
             val ins = Resources::class.java.getResourceAsStream(resourcePath)
-
-            val sb = StringBuilder()
-            BufferedReader(InputStreamReader(ins!!, StandardCharsets.UTF_8)).use { reader ->
-                var c: Int
-                while ((reader.read().also { c = it }) != -1) {
-                    sb.append(c.toChar())
-                }
-            }
-            return sb.toString()
+            return ins!!.bufferedReader().use { it.readText() }
         }
     }
-
-
 }
