1f55eec72c91f0dc282a57c5af93cbd008330864
csillasolyom-gecse <csilla.solyom-gecse@geogebra.at>
2024-06-13 12:15:03 +0200

    MOW-1491 add undo/redo buttons to topbar

--- web/src/main/java/org/geogebra/web/shared/mow/header/NotesTopbar.java
+++ web/src/main/java/org/geogebra/web/shared/mow/header/NotesTopbar.java
@@ -1,46 +1,79 @@
 package org.geogebra.web.shared.mow.header;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.geogebra.common.gui.SetLabels;
+import org.geogebra.common.kernel.Kernel;
+import org.geogebra.common.main.Localization;
 import org.geogebra.web.full.css.MaterialDesignResources;
 import org.geogebra.web.full.gui.toolbar.mow.toolbox.components.IconButton;
 import org.geogebra.web.html5.main.AppW;
+import org.geogebra.web.html5.util.AppletParameters;
+import org.geogebra.web.resources.SVGResource;
 import org.geogebra.web.shared.GlobalHeader;
 import org.gwtproject.user.client.ui.FlowPanel;
 import org.gwtproject.user.client.ui.SimplePanel;
 
 public class NotesTopbar extends FlowPanel implements SetLabels {
-	private final AppW appW;
+	private final Localization loc;
+	private final AppletParameters appletParams;
+	private TopbarController controller;
+	private final List<IconButton> buttons = new ArrayList<>();
+	private IconButton undoBtn;
+	private IconButton redoBtn;
 
 	/**
 	 * constructor
+	 * @param appW - application
 	 */
 	public NotesTopbar(AppW appW) {
-		this.appW = appW;
+		this.loc = appW.getLocalization();
+		this.appletParams = appW.getAppletParameters();
+		controller = new TopbarController(appW);
 		addStyleName("topbar");
 		buildGui();
 	}
 
 	private void buildGui() {
 		addMenuButton();
+		addUndoRedo();
 	}
 
 	private void addMenuButton() {
-		if (!GlobalHeader.isInDOM() && appW.getAppletParameters().getDataParamShowMenuBar(false)) {
-			IconButton menuBtn = new IconButton(appW.getLocalization(), MaterialDesignResources
-					.INSTANCE.toolbar_menu_black(), "Menu", null);
-			menuBtn.addFastClickHandler((event) -> toggleMenu());
-			menuBtn.addStyleName("small");
-			add(menuBtn);
+		if (!GlobalHeader.isInDOM() && appletParams.getDataParamShowMenuBar(false)) {
+			addSmallPressButton(MaterialDesignResources.INSTANCE.toolbar_menu_black(), "Menu",
+					() -> controller.onMenuToggle());
+			addDivider();
+		}
+	}
+
+	private void addUndoRedo() {
+		if (appletParams.getDataParamEnableUndoRedo()) {
+			undoBtn = addSmallPressButton(MaterialDesignResources.INSTANCE.undo_border(), "Undo",
+					() -> controller.onUndo());
+			redoBtn = addSmallPressButton(MaterialDesignResources.INSTANCE.redo_border(), "Redo",
+					() -> controller.onRedo());
 			addDivider();
 		}
 	}
 
 	/**
-	 * Toggle open/closed state of the menu
+	 * update style of undo+redo buttons
 	 */
-	protected void toggleMenu() {
-		appW.hideKeyboard();
-		appW.toggleMenu();
+	public void updateUndoRedoActions(Kernel kernel) {
+		kernel.getConstruction().getUndoManager().setAllowCheckpoints(
+				appletParams.getParamAllowUndoCheckpoints());
+		undoBtn.setDisabled(!kernel.undoPossible());
+		redoBtn.setDisabled(!kernel.redoPossible());
+	}
+
+	private IconButton addSmallPressButton(SVGResource image, String ariaLabel, Runnable clickHandler) {
+		IconButton button = new IconButton(loc, clickHandler, image, ariaLabel);
+		add(button);
+		buttons.add(button);
+
+		return button;
 	}
 
 	private void addDivider() {
@@ -51,6 +84,6 @@ public class NotesTopbar extends FlowPanel implements SetLabels {
 
 	@Override
 	public void setLabels() {
-		// for later
+		buttons.forEach(SetLabels::setLabels);
 	}
 }
