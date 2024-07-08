30189e39654e8f899362efb62750ac6ac552c087
Zbynek Konecny <zbynek@geogebra.at>
2024-07-08 17:44:39 +0200

    APPS-5677 Specify role for error snackbars

---
+++ web-common/src/main/java/org/geogebra/web/html5/gui/tooltip/ToolTip.java
@@ -0,0 +1,42 @@
+package org.geogebra.web.html5.gui.tooltip;
+
+import javax.annotation.CheckForNull;
+
+public class ToolTip {
+	public final String title;
+	public final String helpText;
+	public final String buttonTransKey;
+	public final String url;
+	public final Role role;
+
+	public enum Role {
+		INFO, ALERT
+	}
+
+	/**
+	 * @param title localized title
+	 * @param helpText localized additional text
+	 * @param buttonTransKey optional button text (translation key)
+	 * @param url url to be opened when button clicked
+	 */
+	public ToolTip(String title, String helpText, @CheckForNull String buttonTransKey,
+			@CheckForNull String url) {
+		this(title, helpText, buttonTransKey, url, Role.INFO);
+	}
+
+	/**
+	 * @param title localized title
+	 * @param helpText localized additional text
+	 * @param buttonTransKey optional button text (translation key)
+	 * @param url url to be opened when button clicked
+	 * @param role role for accessibility purposes
+	 */
+	public ToolTip(String title, String helpText, @CheckForNull String buttonTransKey,
+			@CheckForNull String url, Role role) {
+		this.title = title;
+		this.helpText = helpText;
+		this.buttonTransKey = buttonTransKey;
+		this.url = url;
+		this.role = role;
+	}
+}
