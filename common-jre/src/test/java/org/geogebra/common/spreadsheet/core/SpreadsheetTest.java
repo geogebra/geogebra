568eb7a41704b4b30409aeec692058bb8ecd780c
Zbynek Konecny <zbynek@geogebra.at>
2024-07-16 18:31:48 +0200

    Fix test

--- common-jre/src/test/java/org/geogebra/common/spreadsheet/core/SpreadsheetTest.java
+++ common-jre/src/test/java/org/geogebra/common/spreadsheet/core/SpreadsheetTest.java
@@ -33,8 +33,7 @@ public class SpreadsheetTest extends BaseUnitTest {
 				new TestCellRenderableFactory(), null);
 		spreadsheet.setHeightForRows(20, 0, 5);
 		spreadsheet.setWidthForColumns(40, 0, 5);
-		Rectangle viewport = new Rectangle(0, 100, 0, 120);
-		spreadsheet.setViewport(viewport);
+		resetViewport();
 		spreadsheet.setViewportAdjustmentHandler(new ViewportAdjusterDelegate() {
 			@Override
 			public void setScrollPosition(int x, int y) {
@@ -51,6 +50,11 @@ public class SpreadsheetTest extends BaseUnitTest {
 				// no UI to update
 			}
 		});
+	}
+
+	private void resetViewport() {
+		Rectangle viewport = new Rectangle(0, 100, 0, 120);
+		spreadsheet.setViewport(viewport);
 	}
 
 	@Test
@@ -143,6 +147,7 @@ public class SpreadsheetTest extends BaseUnitTest {
 		spreadsheet.handlePointerDown(15, colHeader + 20, Modifiers.NONE);
 		spreadsheet.handlePointerMove(15, colHeader + 45, Modifiers.NONE);
 		spreadsheet.handlePointerUp(15, colHeader + 45, Modifiers.NONE);
+		resetViewport();
 		graphics = new StringCapturingGraphics();
 		spreadsheet.draw(graphics);
 		// after resize, we have 3
