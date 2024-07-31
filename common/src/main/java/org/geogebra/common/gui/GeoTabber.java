86f7fef34202c3e5250064ead56fdd09730adace
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2024-07-31 10:42:13 +0200

    APPS-5712 Prevent tabbing from stealing AV focus

--- common/src/main/java/org/geogebra/common/gui/GeoTabber.java
+++ common/src/main/java/org/geogebra/common/gui/GeoTabber.java
@@ -1,18 +1,21 @@
 package org.geogebra.common.gui;
 
 import org.geogebra.common.main.App;
+import org.geogebra.common.main.GuiManagerInterface;
 import org.geogebra.common.main.SelectionManager;
 
 public class GeoTabber implements MayHaveFocus {
 
 	private boolean selected;
 	private final SelectionManager selectionManager;
+	private final App app;
 
 	/**
 	 * @param app Application
 	 */
 	public GeoTabber(App app) {
 		selectionManager = app.getSelectionManager();
+		this.app = app;
 	}
 
 	@Override
@@ -33,10 +36,29 @@ public class GeoTabber implements MayHaveFocus {
 		return selected;
 	}
 
+	/**
+	 * Focuses the next available GeoElement<br/>
+	 * If the AlgebraView is currently focused, this method ensures that the focused panel,
+	 * after the next GeoElement has been selected, remains the AlgebraView
+	 * @return True if the selection of the next geo was successful, false else
+	 */
 	@Override
 	public boolean focusNext() {
+		boolean isAlgebraViewFocused = isAlgebraViewFocused();
 		selected = selectionManager.selectNextGeo();
+		if (isAlgebraViewFocused) {
+			app.getGuiManager().getLayout().getDockManager().setFocusedPanel(App.VIEW_ALGEBRA);
+		}
 		return selected;
+	}
+
+	private boolean isAlgebraViewFocused() {
+		GuiManagerInterface guiManager = app.getGuiManager();
+		if (guiManager == null || guiManager.getLayout() == null
+				|| guiManager.getLayout().getDockManager() == null) {
+			return false;
+		}
+		return guiManager.getLayout().getDockManager().getFocusedViewId() == App.VIEW_ALGEBRA;
 	}
 
 	@Override
