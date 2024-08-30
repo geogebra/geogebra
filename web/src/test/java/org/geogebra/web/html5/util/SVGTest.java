412cb6f839fcbbc7d2de29e8bf7e896835ce483d
Zbynek Konecny <zbynek@geogebra.at>
2024-08-30 16:46:46 +0200

    SVG icon names should fit PNG icon names

--- web/src/test/java/org/geogebra/web/html5/util/SVGTest.java
+++ web/src/test/java/org/geogebra/web/html5/util/SVGTest.java
@@ -3,14 +3,17 @@ package org.geogebra.web.html5.util;
 import static com.ibm.icu.impl.Assert.fail;
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.geogebra.common.util.StringUtil;
+import org.geogebra.common.euclidian.EuclidianConstants;
 import org.geogebra.web.full.css.ToolbarSvgResources;
 import org.geogebra.web.full.css.ToolbarSvgResourcesSync;
 import org.gwtproject.resources.client.ClientBundle;
@@ -18,10 +21,10 @@ import org.junit.Test;
 
 public class SVGTest {
 
+	private static String SVG_PATH = "../common/src/nonfree/resources/org/geogebra/common/icons/svg/web/toolIcons";
 	@Test
 	public void rmExtraSVGS() {
-		Path svgs = Paths.get(
-				"../common/src/nonfree/resources/org/geogebra/common/icons/svg/web/toolIcons");
+		Path svgs = Paths.get(SVG_PATH);
 		Path root = Paths.get("../common/src/nonfree/resources");
 		TreeSet<String> disk = new TreeSet<>();
 		try {
@@ -39,6 +42,36 @@ public class SVGTest {
 				disk.remove(src);
 			}
 		});
-		assertEquals("", StringUtil.join(",", disk));
+		// photolibrary duplicates camera (for mobile)
+		// freehandfunction duplicates freehandshape (for mobile)
+		assertEquals("mode_freehandfunction.svg,mode_photolibrary.svg", disk.stream()
+				.map(s -> new File(s).getName()).collect(Collectors.joining(",")));
+	}
+
+	@Test
+	public void checkToolIcons() {
+		StringBuilder missing = new StringBuilder();
+
+		for (int i = 0; i < 3000; i++) {
+			String modeText = EuclidianConstants.getModeTextSimple(i).toLowerCase(Locale.ROOT);
+
+			if (modeText.isEmpty()) {
+				continue;
+			}
+			switch (i) {
+			case EuclidianConstants.MODE_SELECTION_LISTENER:
+			case EuclidianConstants.MODE_GRASPABLE_MATH:
+			case EuclidianConstants.MODE_CALCULATOR:
+				continue;
+			default:
+				File icon = new File(SVG_PATH + "/mode_" + modeText + ".svg");
+				if (!icon.exists()) {
+					missing.append(modeText).append(",");
+				}
+			}
+
+		}
+
+		assertEquals("", missing.toString());
 	}
 }
