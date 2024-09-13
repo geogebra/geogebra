43f4bcb1c637eb64e2ec2fd59b1123bc89b8a022
csillasolyom-gecse <csilla@geogebra.at>
2024-09-13 17:10:07 +0200

    MOW-1524 fix warning

--- web/src/main/java/org/geogebra/web/full/gui/ContextMenuGraphicsWindowW.java
+++ web/src/main/java/org/geogebra/web/full/gui/ContextMenuGraphicsWindowW.java
@@ -18,7 +18,6 @@ import org.geogebra.web.full.javax.swing.GCollapseMenuItem;
 import org.geogebra.web.html5.css.GuiResourcesSimple;
 import org.geogebra.web.html5.gui.menu.AriaMenuBar;
 import org.geogebra.web.html5.gui.menu.AriaMenuItem;
-import org.geogebra.web.html5.gui.view.button.StandardButton;
 import org.geogebra.web.html5.main.AppW;
 import org.geogebra.web.shared.components.dialog.DialogData;
 import org.gwtproject.resources.client.ResourcePrototype;
