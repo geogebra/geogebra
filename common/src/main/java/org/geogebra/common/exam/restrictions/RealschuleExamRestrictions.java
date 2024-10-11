be23a2579cbf8fb4541d45e9f8681a2238af7edf
Zoltán Finta <zoltan.finta@geogebra.at>
2024-10-11 14:06:50 +0300

    APPS-5887 Added tool restriction

---
+++ common/src/main/java/org/geogebra/common/exam/restrictions/RealschuleExamRestrictions.java
@@ -0,0 +1,81 @@
+package org.geogebra.common.exam.restrictions;
+
+import java.util.Set;
+
+import org.geogebra.common.SuiteSubApp;
+import org.geogebra.common.euclidian.EuclidianConstants;
+import org.geogebra.common.exam.ExamType;
+import org.geogebra.common.gui.toolcategorization.ToolCollectionFilter;
+import org.geogebra.common.gui.toolcategorization.impl.ToolCollectionSetFilter;
+
+final class RealschuleExamRestrictions extends ExamRestrictions {
+
+	RealschuleExamRestrictions() {
+		super(ExamType.REALSCHULE,
+				Set.of(SuiteSubApp.CAS, SuiteSubApp.GEOMETRY, SuiteSubApp.G3D,
+						SuiteSubApp.PROBABILITY, SuiteSubApp.SCIENTIFIC),
+				SuiteSubApp.GRAPHING,
+				null,
+				null,
+				null,
+				null,
+				null,
+				null,
+				null,
+				createToolsFilter(),
+				null);
+	}
+
+	private static ToolCollectionFilter createToolsFilter() {
+		return new ToolCollectionSetFilter(
+				EuclidianConstants.MODE_FITLINE,
+				EuclidianConstants.MODE_IMAGE,
+				EuclidianConstants.MODE_TEXT,
+				EuclidianConstants.MODE_ANGLE,
+				EuclidianConstants.MODE_DISTANCE,
+				EuclidianConstants.MODE_AREA,
+				EuclidianConstants.MODE_ANGLE_FIXED,
+				EuclidianConstants.MODE_SLOPE,
+				EuclidianConstants.MODE_COMPLEX_NUMBER,
+				EuclidianConstants.MODE_MIDPOINT,
+				EuclidianConstants.MODE_ORTHOGONAL,
+				EuclidianConstants.MODE_LINE_BISECTOR,
+				EuclidianConstants.MODE_PARALLEL,
+				EuclidianConstants.MODE_ANGULAR_BISECTOR,
+				EuclidianConstants.MODE_TANGENTS,
+				EuclidianConstants.MODE_LOCUS,
+				EuclidianConstants.MODE_SEGMENT,
+				EuclidianConstants.MODE_SEGMENT_FIXED,
+				EuclidianConstants.MODE_POLAR_DIAMETER,
+				EuclidianConstants.MODE_POLYLINE,
+				EuclidianConstants.MODE_POLYGON,
+				EuclidianConstants.MODE_REGULAR_POLYGON,
+				EuclidianConstants.MODE_VECTOR_POLYGON,
+				EuclidianConstants.MODE_RIGID_POLYGON,
+				EuclidianConstants.MODE_CIRCLE_TWO_POINTS,
+				EuclidianConstants.MODE_COMPASSES,
+				EuclidianConstants.MODE_SEMICIRCLE,
+				EuclidianConstants.MODE_CIRCLE_POINT_RADIUS,
+				EuclidianConstants.MODE_CIRCLE_THREE_POINTS,
+				EuclidianConstants.MODE_CIRCLE_ARC_THREE_POINTS,
+				EuclidianConstants.MODE_CIRCUMCIRCLE_ARC_THREE_POINTS,
+				EuclidianConstants.MODE_CIRCLE_SECTOR_THREE_POINTS,
+				EuclidianConstants.MODE_CIRCUMCIRCLE_SECTOR_THREE_POINTS,
+				EuclidianConstants.MODE_ELLIPSE_THREE_POINTS,
+				EuclidianConstants.MODE_CONIC_FIVE_POINTS,
+				EuclidianConstants.MODE_PARABOLA,
+				EuclidianConstants.MODE_HYPERBOLA_THREE_POINTS,
+				EuclidianConstants.MODE_MIRROR_AT_LINE,
+				EuclidianConstants.MODE_MIRROR_AT_POINT,
+				EuclidianConstants.MODE_TRANSLATE_BY_VECTOR,
+				EuclidianConstants.MODE_MOVE_ROTATE,
+				EuclidianConstants.MODE_DILATE_FROM_POINT,
+				EuclidianConstants.MODE_MIRROR_AT_CIRCLE,
+				EuclidianConstants.MODE_RELATION,
+				EuclidianConstants.MODE_BUTTON_ACTION,
+				EuclidianConstants.MODE_SHOW_HIDE_CHECKBOX,
+				EuclidianConstants.MODE_TEXTFIELD_ACTION
+
+		);
+	}
+}
