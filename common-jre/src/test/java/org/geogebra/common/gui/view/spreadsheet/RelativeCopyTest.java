1e276cab3cdab207b8fa0126c3ea8eb17c5366c3
laszlo <laszlo@geogebra.at>
2024-11-14 18:35:31 +0100

    APPS-6058 apply patch for extra decimals and adding regression tests

--- common-jre/src/test/java/org/geogebra/common/gui/view/spreadsheet/RelativeCopyTest.java
+++ common-jre/src/test/java/org/geogebra/common/gui/view/spreadsheet/RelativeCopyTest.java
@@ -1,5 +1,6 @@
 package org.geogebra.common.gui.view.spreadsheet;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 
@@ -8,10 +9,15 @@ import org.geogebra.common.kernel.CircularDefinitionException;
 import org.geogebra.common.kernel.geos.GeoBoolean;
 import org.geogebra.common.kernel.geos.GeoElement;
 import org.geogebra.common.kernel.kernelND.GeoElementND;
+import org.geogebra.common.spreadsheet.kernel.DefaultSpreadsheetCellDataSerializer;
 import org.geogebra.test.TestErrorHandler;
+import org.geogebra.test.annotation.Issue;
 import org.junit.Test;
 
 public class RelativeCopyTest extends BaseUnitTest {
+
+	private DefaultSpreadsheetCellDataSerializer serializer =
+			new DefaultSpreadsheetCellDataSerializer();
 
 	@Test
 	public void conditionToShowRelative() throws CircularDefinitionException {
@@ -19,7 +25,7 @@ public class RelativeCopyTest extends BaseUnitTest {
 		GeoBoolean b1 = add("B1=true");
 		add("B2=false");
 		a1.setShowObjectCondition(b1);
-		new RelativeCopy(getKernel()).doDragCopy(0, 0, 0, 0,
+		newRelativeCopy().doDragCopy(0, 0, 0, 0,
 				0, 1, 0, 1);
 		assertEquals(lookup("A2").getShowObjectCondition(), lookup("B2"));
 	}
@@ -29,7 +35,7 @@ public class RelativeCopyTest extends BaseUnitTest {
 		GeoElement a1 = add("A1=(1,1)");
 		GeoBoolean b = add("b=true");
 		a1.setShowObjectCondition(b);
-		new RelativeCopy(getKernel()).doDragCopy(0, 0, 0, 0,
+		newRelativeCopy().doDragCopy(0, 0, 0, 0,
 				0, 1, 0, 1);
 		assertEquals(lookup("A2").getShowObjectCondition(), b);
 	}
@@ -37,7 +43,7 @@ public class RelativeCopyTest extends BaseUnitTest {
 	@Test
 	public void numericCopy() {
 		add("A1=1");
-		new RelativeCopy(getKernel(), TestErrorHandler.INSTANCE).doDragCopy(0, 0, 0, 0,
+		newRelativeCopy().doDragCopy(0, 0, 0, 0,
 				0, 1, 0, 1);
 		assertThat(lookup("A2"), hasValue("1"));
 	}
@@ -46,10 +52,48 @@ public class RelativeCopyTest extends BaseUnitTest {
 	public void numericCopyHasCorrectPrecision() {
 		add("C3 = 1 / 8");
 		getApp().setRounding("2");
-		new RelativeCopy(getKernel(), TestErrorHandler.INSTANCE).doDragCopy(2, 2, 2, 2,
+		newRelativeCopy().doDragCopy(2, 2, 2, 2,
 				3, 2, 3, 2);
 		getApp().setRounding("3");
 		assertThat(lookup("D3"), hasValue("0.125"));
+	}
+
+	@Test
+	public void functionCopyHasCorrectValue() {
+		add("C2 = 3 / 8 ");
+		add("D2 = 5 / 8 ");
+		add("C3 = C2 + x");
+		getApp().setRounding("2");
+		newRelativeCopy().doDragCopy(2, 2, 2, 2,
+				3, 2, 3, 2);
+		getApp().setRounding("3");
+		assertThat(lookup("D3"), hasValue("0.625 + x"));
+	}
+
+	@Issue("APPS-6058")
+	@Test
+	public void dragCopyRelativeExpression() {
+		for (int i = 1; i < 11; i++) {
+			add("A" + i + " = " + i);
+			add("B" + i + " = (A" + i + ", 0)");
+			assertThat(lookup("B" + i), hasValue("(" + i + ", 0)"));
+		}
+
+		add("C1 = Circle(B1, 0.4)");
+		newRelativeCopy().doDragCopy(2, 0, 2, 0,
+				2, 1, 2, 10);
+		for (int row = 2; row < 10; row++) {
+			assertThat(lookup("C" + row), hasValue("(x - " + row + ")\u00B2 + y\u00B2 = 0.16"));
+			shouldBeInEditor("C" + row, "=Circle(B" + row + "," + "0.4)");
+		}
+	}
+
+	private RelativeCopy newRelativeCopy() {
+		return new RelativeCopy(getKernel(), TestErrorHandler.INSTANCE);
+	}
+
+	private void shouldBeInEditor(String CellName, String value) {
+		assertThat(serializer.getStringForEditor(lookup(CellName)), is(value));
 	}
 
 	@Test
@@ -61,8 +105,7 @@ public class RelativeCopyTest extends BaseUnitTest {
 	}
 
 	private GeoElementND prepareAddingValue(String inputText) {
-		return new RelativeCopy(getKernel(),
-				TestErrorHandler.INSTANCE).prepareAddingValueToTableNoStoringUndoInfo(
+		return newRelativeCopy().prepareAddingValueToTableNoStoringUndoInfo(
 				inputText, null, 1, 1, false);
 	}
 }
