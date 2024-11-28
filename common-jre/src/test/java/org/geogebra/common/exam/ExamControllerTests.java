015ec7d6ef47f43fe909a4f63f389a7862eaa7d7
Zoltán Finta <zoltan.finta@geogebra.at>
2024-11-28 11:25:00 +0200

    Exam test refactor: remove previously moved test

--- common-jre/src/test/java/org/geogebra/common/exam/ExamControllerTests.java
+++ common-jre/src/test/java/org/geogebra/common/exam/ExamControllerTests.java
@@ -154,12 +154,6 @@ public final class ExamControllerTests extends BaseExamTests {
         // TODO commandArgumentFilters
         // expression restrictions
         assertNull(evaluate("true || false"));
-        // property restrictions
-        Property angleUnit = propertiesRegistry.lookup("AngleUnit", app);
-        assertNotNull(angleUnit);
-        assertTrue(angleUnit.isFrozen());
-        assertEquals(List.of(Kernel.ANGLE_DEGREE, Kernel.ANGLE_RADIANT),
-                ((NamedEnumeratedProperty<?>) angleUnit).getValues());
         // context menu restrictions
         assertEquals(List.of(Text, Help), contextMenuFactory.makeInputContextMenu(true));
 
