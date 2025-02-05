ce4d90aab29e54a50b070118664d020909de2dc9
Balázs Bencze <balazs.bencze@geogebra.org>
2025-02-05 12:17:39 +0000

    Merge branch 'apps-6271' into 'main'
    APPS-6271: Add Giac tests to Android Tests
    
    See merge request ggb/geogebra!8901
    
    GitOrigin-RevId: 3f6877ffdd1d5c27493d7bc6de427ee841c08c88

--- source/shared/common-jre/src/testFixtures/java/org/geogebra/common/io/XmlTestUtil.java
+++ source/shared/common-jre/src/testFixtures/java/org/geogebra/common/io/XmlTestUtil.java
@@ -20,29 +20,24 @@ import org.xml.sax.SAXParseException;
 
 public class XmlTestUtil {
 
+	private static final String GEOGEBRA_XSD_URL = "https://www.geogebra.org/apps/xsd/ggb.xsd";
+	private static Validator VALIDATOR;
+
 	/**
 	 * Validate app's state against XML schema.
-	 * 
+	 *
 	 * @param application
 	 *            app
 	 */
 	public static void checkCurrentXML(AppCommon application) {
-		checkCurrentXMLWithURL(application, "https://www.geogebra.org/apps/xsd/ggb.xsd");
-	}
-
-	/**
-	 * Validate app's state against XML schema at a specified url.
-	 *
-	 * @param application app
-	 * @param xsdUrl url to the XML schema
-	 */
-	public static void checkCurrentXMLWithURL(AppCommon application, String xsdUrl) {
 		String xml = application.getXML();
 		try {
-			xml = application.getXML();
+			Validator validator = getValidator();
+			if (validator == null) {
+				return;
+			}
 			Source xmlFile = new StreamSource(new StringReader(xml));
-			String url = System.getProperty("xsdUrl", xsdUrl);
-			getValidator(url).validate(xmlFile);
+			validator.validate(xmlFile);
 		} catch (SAXParseException se) {
 			int l = se.getLineNumber();
 			String[] rows = xml.split("\\n");
@@ -56,12 +51,19 @@ public class XmlTestUtil {
 		}
 	}
 
-	private static Validator getValidator(String url) throws MalformedURLException, SAXException {
-		URL schemaFile = new URL(url);
-		SchemaFactory schemaFactory = SchemaFactory
-				.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-		Schema schema = schemaFactory.newSchema(schemaFile);
-		return schema.newValidator();
+	private static Validator getValidator() throws MalformedURLException, SAXException {
+		if (VALIDATOR != null) {
+			return VALIDATOR;
+		}
+		URL schemaFile = new URL(GEOGEBRA_XSD_URL);
+		try {
+			SchemaFactory schemaFactory = SchemaFactory
+					.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			Schema schema = schemaFactory.newSchema(schemaFile);
+			VALIDATOR = schema.newValidator();
+		} catch (IllegalArgumentException exception) {
+			// Ignore
+		}
+		return VALIDATOR;
 	}
-
 }
