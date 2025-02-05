78a1729ba968853b08890be679bc72d27e9a0a8d
Balázs Bencze <balazs.bencze@geogebra.org>
2025-02-05 07:12:48 +0000

    Merge branch 'apps-5752' into 'main'
    APPS-5752: common - Intersect command restrictions
    
    See merge request ggb/geogebra!8688
    
    GitOrigin-RevId: 2c96ab4b1c4c9fd005bf5978eda3856b45490b7b

--- source/shared/common/src/main/java/org/geogebra/common/exam/restrictions/cvte/CvteCommandArgumentFilter.java
+++ source/shared/common/src/main/java/org/geogebra/common/exam/restrictions/cvte/CvteCommandArgumentFilter.java
@@ -1,15 +1,11 @@
 package org.geogebra.common.exam.restrictions.cvte;
 
+import org.geogebra.common.exam.restrictions.CvteExamRestrictions;
 import org.geogebra.common.kernel.arithmetic.Command;
 import org.geogebra.common.kernel.commands.CommandProcessor;
 import org.geogebra.common.kernel.commands.Commands;
 import org.geogebra.common.kernel.commands.filter.CommandArgumentFilter;
-import org.geogebra.common.kernel.geos.GeoConic;
-import org.geogebra.common.kernel.geos.GeoCurveCartesian;
 import org.geogebra.common.kernel.geos.GeoElement;
-import org.geogebra.common.kernel.geos.GeoFunction;
-import org.geogebra.common.kernel.geos.GeoLine;
-import org.geogebra.common.kernel.implicit.GeoImplicitCurve;
 import org.geogebra.common.main.MyError;
 
 public final class CvteCommandArgumentFilter implements CommandArgumentFilter {
@@ -86,26 +82,22 @@ public final class CvteCommandArgumentFilter implements CommandArgumentFilter {
 	}
 
 	private void checkIntersect(Command command, CommandProcessor commandProcessor) throws MyError {
-		// For Intersect( <Object>, <Object> ) and Intersect( <Object>, <Object>, <Number> ),
-		// the only Objects allowed are those that can be displayed in 2D graphics.
+		// Intersection commands are only allowed if the visibility of the arguments are not
+		// restricted. There are always at least 2 arguments,
+		// and the first 2 are always the geo elements that are potentially restricted:
+		// Intersect(<Object>, <Object>)
+		// Intersect(<Object>, <Object>, <Unrelated parameter>)
+		// Intersect(<Object>, <Object>, <Unrelated parameter>, <Unrelated parameter>)
 		GeoElement[] arguments = commandProcessor.resArgs(command);
-		if (arguments.length == 2 || arguments.length == 3) {
+		if (arguments.length >= 2) {
 			GeoElement firstArgument = arguments[0];
-			if (!isDisplayableIn2DGraphics(firstArgument)) {
+			if (!CvteExamRestrictions.isVisibilityEnabled(firstArgument)) {
 				throw commandProcessor.argErr(command, firstArgument);
 			}
-
 			GeoElement secondArgument = arguments[1];
-			if (!isDisplayableIn2DGraphics(secondArgument)) {
+			if (!CvteExamRestrictions.isVisibilityEnabled(secondArgument)) {
 				throw commandProcessor.argErr(command, secondArgument);
 			}
 		}
-	}
-
-	private boolean isDisplayableIn2DGraphics(GeoElement geoElement) {
-		return geoElement instanceof GeoLine
-				|| geoElement instanceof GeoFunction
-				|| geoElement instanceof GeoImplicitCurve || geoElement instanceof GeoCurveCartesian
-				|| geoElement instanceof GeoConic;
 	}
 }
