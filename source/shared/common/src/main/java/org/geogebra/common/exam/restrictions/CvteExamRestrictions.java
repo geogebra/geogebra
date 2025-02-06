9568bce79ac2df4104d538cc60079c25656e988f
Zbyněk Konečný <zbynek@geogebra.org>
2025-02-06 14:43:06 +0000

    Merge branch 'APPS-6186' into 'main'
    APPS-6186 disable equation form change in dutch exam mode
    
    Closes APPS-6186
    
    See merge request ggb/geogebra!8900
    
    GitOrigin-RevId: 4376740379bfd0191266e96b7105323c68ceae95

--- source/shared/common/src/main/java/org/geogebra/common/exam/restrictions/CvteExamRestrictions.java
+++ source/shared/common/src/main/java/org/geogebra/common/exam/restrictions/CvteExamRestrictions.java
@@ -144,7 +144,8 @@ public final class CvteExamRestrictions extends ExamRestrictions {
 	private static Set<ExamFeatureRestriction> createFeatureRestrictions() {
 		return Set.of(
 				ExamFeatureRestriction.AUTOMATIC_GRAPH_SELECTION_FOR_FUNCTIONS,
-				ExamFeatureRestriction.HIDE_CALCULATED_EQUATION);
+				ExamFeatureRestriction.HIDE_CALCULATED_EQUATION,
+				ExamFeatureRestriction.RESTRICT_CHANGING_EQUATION_FORM);
 	}
 
 	private static Set<CommandFilter> createCommandFilters() {
