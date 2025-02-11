db087fa2286a066f0c229fce480ec8076ea304f4
Zbynek Konecny <zbynek@geogebra.at>
2025-02-11 18:09:22 +0100

    Fix UI tests
    GitOrigin-RevId: 0231db4aa8c2c22c8dc0b760b8769772506f0655

--- source/web/uitest/runtest.sh
+++ source/web/uitest/runtest.sh
@@ -1,4 +1,4 @@
-npm install --force || exit 1
+npm install || exit 1
 find content -type f | xargs gzip -k
 npx http-server -p 8888 content --gzip & npx wait-on http://localhost:8888
 npx cypress run --env userAgent="Chrome iPad" --spec="cypress/integration/**/*-iPad.spec.js" || true
