a45d5646d9d3da4cf001811367671ef9c91ffe50
Zbynek Konecny <zbynek@geogebra.at>
2025-05-23 13:02:57 +0200

    Move ParserInfo to java folder
    GitOrigin-RevId: 5bff2855353066c3c5191055045c7e3f210f6cc5

--- source/shared/common/src/main/java/org/geogebra/common/kernel/parser/ParserInfo.java
+++ source/shared/common/src/main/java/org/geogebra/common/kernel/parser/ParserInfo.java
@@ -1,18 +1,31 @@
 package org.geogebra.common.kernel.parser;
 
+/**
+ * Flags for parsing expressions.
+ */
 public class ParserInfo {
 	private final boolean inputBox;
 	private final boolean decimalComma;
 
+	/**
+	 * @param inputBox whether the expression is parsed for input box
+	 * @param decimalComma whether to prefer decimal comma to dot
+	 */
 	public ParserInfo(boolean inputBox, boolean decimalComma) {
 		this.inputBox = inputBox;
 		this.decimalComma = decimalComma;
 	}
 
+	/**
+	 * @return whether the expression is parsed for input box
+	 */
 	public boolean isInputBox() {
 		return inputBox;
 	}
 
+	/**
+	 * @return whether to prefer decimal comma to dot
+	 */
 	public boolean isDecimalComma() {
 		return decimalComma;
 	}
