3f00febdd15d4e6ee8ffa0a29299df8691c6ae1b
Zbynek Konecny <zbynek@geogebra.at>
2025-07-28 13:10:37 +0200

    Correct error message for Triangular
    GitOrigin-RevId: 62e8c744e04eac641f3a2df0f1ec6f5a8e8ce778

--- source/shared/common/src/main/java/org/geogebra/common/kernel/statistics/CmdTriangular.java
+++ source/shared/common/src/main/java/org/geogebra/common/kernel/statistics/CmdTriangular.java
@@ -7,7 +7,6 @@ import org.geogebra.common.kernel.commands.CommandProcessor;
 import org.geogebra.common.kernel.commands.EvalInfo;
 import org.geogebra.common.kernel.geos.GeoBoolean;
 import org.geogebra.common.kernel.geos.GeoElement;
-import org.geogebra.common.kernel.geos.GeoFunction;
 import org.geogebra.common.kernel.geos.GeoNumberValue;
 import org.geogebra.common.main.MyError;
 
@@ -29,7 +28,7 @@ public class CmdTriangular extends CommandProcessor {
 	@Override
 	public GeoElement[] process(Command c2, EvalInfo info) throws MyError {
 		int n = c2.getArgumentNumber();
-		boolean ok, ok2 = true;
+		boolean[] ok = new boolean[n];
 		GeoElement[] arg;
 
 		GeoBoolean cumulative = null; // default for n=3
@@ -45,10 +44,10 @@ public class CmdTriangular extends CommandProcessor {
 
 			// fall through
 		case 4:
-			if ((ok = arg[0] instanceof GeoNumberValue)
-					&& (ok2 = arg[1] instanceof GeoNumberValue)
-					&& (arg[2] instanceof GeoNumberValue)) {
-				if (arg[3].isGeoFunction() && ((GeoFunction) arg[3])
+			if ((ok[0] = arg[0] instanceof GeoNumberValue)
+					&& (ok[1] = arg[1] instanceof GeoNumberValue)
+					&& (ok[2] = arg[2] instanceof GeoNumberValue)) {
+				if (arg[3].isGeoFunction() && arg[3]
 						.toString(StringTemplate.defaultTemplate).equals("x")) {
 
 					AlgoTriangularDF algo = new AlgoTriangularDF(cons,
@@ -67,11 +66,10 @@ public class CmdTriangular extends CommandProcessor {
 					return algo.getResult().asArray();
 
 				} else {
-					throw argErr(c2, arg[2]);
+					throw argErr(c2, arg[3]);
 				}
 			}
-			throw argErr(c2,
-					!ok ? arg[0] : (ok2 ? arg[2] : arg[0]));
+			throw argErr(c2, getBadArg(ok, arg));
 
 		default:
 			throw argNumErr(c2);
