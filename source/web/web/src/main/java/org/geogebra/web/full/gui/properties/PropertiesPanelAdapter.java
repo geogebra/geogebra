8b9b6c9e90db76fa9ed4d6e2dc06b99358897f5c
Zbyněk Konečný <zbynek@geogebra.org>
2025-08-01 10:26:08 +0000

    APPS-6713 Revert test code
    GitOrigin-RevId: 2cb9077e545391d5eabd69d5f072b67c8f2011cd

--- source/web/web/src/main/java/org/geogebra/web/full/gui/properties/PropertiesPanelAdapter.java
+++ source/web/web/src/main/java/org/geogebra/web/full/gui/properties/PropertiesPanelAdapter.java
@@ -17,7 +17,6 @@ import org.geogebra.common.properties.impl.general.RestoreSettingsAction;
 import org.geogebra.common.properties.impl.general.SaveSettingsAction;
 import org.geogebra.web.full.euclidian.quickstylebar.PropertiesIconAdapter;
 import org.geogebra.web.full.gui.components.ComponentCheckbox;
-import org.geogebra.web.full.gui.components.ComponentComboBox;
 import org.geogebra.web.full.gui.components.ComponentDropDown;
 import org.geogebra.web.full.gui.components.ComponentExpandableList;
 import org.geogebra.web.full.gui.components.ComponentInputField;
@@ -109,11 +108,6 @@ public class PropertiesPanelAdapter {
 			}
 			return iconPanel;
 
-		}
-		if (property instanceof NamedEnumeratedProperty) {
-			ComponentComboBox comboBox = new ComponentComboBox(app, property.getName(),
-					(NamedEnumeratedProperty<?>) property);
-			return comboBox;
 		}
 		if (property instanceof NamedEnumeratedProperty) {
 			ComponentDropDown dropDown = new ComponentDropDown(app, property.getName(),
