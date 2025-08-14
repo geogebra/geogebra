be412bd1db5b3a082908477f1ad77f6273146058
Jenkins <jenkins@geogebra.org>
2025-08-14 13:04:51 +0200

    Version bump 5.2.897.0 [ci skip]
    GitOrigin-RevId: 61266a52d32c834fac9c7cf07de679950fac538c

--- source/shared/common/src/main/java/org/geogebra/common/GeoGebraConstants.java
+++ source/shared/common/src/main/java/org/geogebra/common/GeoGebraConstants.java
@@ -10,10 +10,10 @@ public interface GeoGebraConstants {
 	// as it is read by the build system
 	// and updated automatically by the "Version Bump" task
 	/** last build date */
-	public static final String BUILD_DATE = "05 August 2025";
+	public static final String BUILD_DATE = "14 August 2025";
 	/** complete version string */
 
-	public static final String VERSION_STRING = "5.2.896.0";
+	public static final String VERSION_STRING = "5.2.897.0";
 	/** proper noun, should NOT be translated / transliterated */
 	public static final String APPLICATION_NAME = "GeoGebra";
 
